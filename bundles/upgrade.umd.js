0dbe0102df73fe1ac52eac45d46b2a8e23190020
Alex Eagle <alexeagle@google.com>
2016-07-13 19:50:34 +0000

    4c762a6 chore(lint): enable lint check for duplicate vars

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -226,7 +226,7 @@
         if (!selector.match(COMPONENT_SELECTOR)) {
             throw new Error('Only selectors matching element names are supported, got: ' + selector);
         }
-        var selector = selector.replace(SKEWER_CASE, function (all /** TODO #9100 */, letter) { return letter.toUpperCase(); });
+        selector = selector.replace(SKEWER_CASE, function (all /** TODO #9100 */, letter) { return letter.toUpperCase(); });
         return {
             type: type,
             selector: selector,
