c56af4ce6a80e6498d78eb553fc22bffde659f20
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-01 18:32:23 +0000

    480ef20 docs(changelog): add changelog for 2.2.2

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-c066281');
+export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-480ef20');
 export { UpgradeAdapter, UpgradeAdapterRef } from './src/upgrade';
 //# sourceMappingURL=index.js.map
