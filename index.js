f3aa29de879c82d070cb58a06bf24047a5458784
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-06 18:54:35 +0000

    2893c2c fix(router): validate nested routes (#13224)

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-f31c947');
+export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-2893c2c');
 export { UpgradeAdapter, UpgradeAdapterRef } from './src/upgrade';
 //# sourceMappingURL=index.js.map
