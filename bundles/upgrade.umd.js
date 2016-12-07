cb609fa6037a3cae4b2ee4e3c935706852b04093
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-07 01:23:41 +0000

    4a09c81 fix(language-service): do not throw for invalid metadata (#13261)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-rc.0-16efb13
+ * @license Angular v2.3.0-rc.0-4a09c81
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1297,7 +1297,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-rc.0-16efb13');
+  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-rc.0-4a09c81');
 
   exports.UpgradeAdapter = UpgradeAdapter;
   exports.UpgradeAdapterRef = UpgradeAdapterRef;
