350a22f081023a86365acf2439301e9508f8e4f9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-07 22:08:07 +0000

    13b41bd chore(release): update version of tsc-wrapped to 0.4.2

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-rc.0-cf52284
+ * @license Angular v2.3.0-13b41bd
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1297,7 +1297,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-rc.0-cf52284');
+  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-13b41bd');
 
   exports.UpgradeAdapter = UpgradeAdapter;
   exports.UpgradeAdapterRef = UpgradeAdapterRef;
