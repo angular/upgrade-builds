6019864e49b0831c8031eda83852052126def1bc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-07 00:33:13 +0000

    986abbe fix(http): set the default Accept header (#12989)

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-25c2141');
+export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-986abbe');
 export { UpgradeAdapter, UpgradeAdapterRef } from './src/upgrade';
 //# sourceMappingURL=index.js.map
