be37ca6a1587ee82753993f2c8a4c3dfcf686343
Victor Berchet <victor@suumit.com>
2016-12-08 23:55:33 +0000

    bf93389 doc: update triage owners for language service and router (#13325)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-4398056
+ * @license Angular v2.3.0-bf93389
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1297,7 +1297,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-4398056');
+  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-bf93389');
 
   exports.UpgradeAdapter = UpgradeAdapter;
   exports.UpgradeAdapterRef = UpgradeAdapterRef;
