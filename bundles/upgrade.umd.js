16cb7b53d9058e518f099d2d860ceb31e1db5d84
GitHub <noreply@github.com>
2016-12-09 19:35:50 +0000

    4022173 fix(compiler): fix PR 13322 (#13331)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-c8baf51
+ * @license Angular v2.3.0-4022173
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1297,7 +1297,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-c8baf51');
+  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-4022173');
 
   exports.UpgradeAdapter = UpgradeAdapter;
   exports.UpgradeAdapterRef = UpgradeAdapterRef;
