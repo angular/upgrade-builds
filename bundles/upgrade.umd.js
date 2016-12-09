d73826d012b943b42a2b2600b29823c8cd3c03f8
Victor Berchet <victor@suumit.com>
2016-12-09 23:50:49 +0000

    a006c14 feat(router): routerLink add tabindex attribute (#13094)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-aaf6e05
+ * @license Angular v2.3.0-a006c14
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1297,7 +1297,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-aaf6e05');
+  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-a006c14');
 
   exports.UpgradeAdapter = UpgradeAdapter;
   exports.UpgradeAdapterRef = UpgradeAdapterRef;
