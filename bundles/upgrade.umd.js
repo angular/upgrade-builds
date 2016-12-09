31221ef681d3fdfa8631a0f17753570d0d589e76
Victor Berchet <victor@suumit.com>
2016-12-09 23:54:12 +0000

    8a8c532 fix(dom_adapter): remove logError from logGroup (#12925)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-08ff2e5
+ * @license Angular v2.3.0-8a8c532
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1297,7 +1297,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-08ff2e5');
+  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-8a8c532');
 
   exports.UpgradeAdapter = UpgradeAdapter;
   exports.UpgradeAdapterRef = UpgradeAdapterRef;
