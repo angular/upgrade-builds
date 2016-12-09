d3d693588018128743c9b54588744731fb8be292
Victor Berchet <victor@suumit.com>
2016-12-09 02:55:38 +0000

    5c6ec20 refactor(router): simplify regexp

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-4e3d58a
+ * @license Angular v2.3.0-5c6ec20
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1297,7 +1297,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-4e3d58a');
+  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-5c6ec20');
 
   exports.UpgradeAdapter = UpgradeAdapter;
   exports.UpgradeAdapterRef = UpgradeAdapterRef;
