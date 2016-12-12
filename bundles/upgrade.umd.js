97fbc8e26ebe6199aad107e87bc1fef937d96e21
Victor Berchet <victor@suumit.com>
2016-12-12 19:30:13 +0000

    2e500cc fix(http): create a copy of headers when merge options (#13365)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-56dce0e
+ * @license Angular v2.3.0-2e500cc
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1297,7 +1297,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-56dce0e');
+  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-2e500cc');
 
   exports.UpgradeAdapter = UpgradeAdapter;
   exports.UpgradeAdapterRef = UpgradeAdapterRef;
