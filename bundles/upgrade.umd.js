7e4004b142abc402ebccced8b5fd62117cbc0987
Victor Berchet <victor@suumit.com>
2016-12-12 19:30:43 +0000

    4c35be3 feat(forms): add novalidate by default (#13092)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-2e500cc
+ * @license Angular v2.3.0-4c35be3
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1297,7 +1297,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-2e500cc');
+  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-4c35be3');
 
   exports.UpgradeAdapter = UpgradeAdapter;
   exports.UpgradeAdapterRef = UpgradeAdapterRef;
