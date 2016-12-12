6ce9e388b25a84c72303304030611f673c8ff6db
Victor Berchet <victor@suumit.com>
2016-12-12 19:58:41 +0000

    7295a5e refactor: format and lint code

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-4c35be3
+ * @license Angular v2.3.0-7295a5e
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1297,7 +1297,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-4c35be3');
+  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-7295a5e');
 
   exports.UpgradeAdapter = UpgradeAdapter;
   exports.UpgradeAdapterRef = UpgradeAdapterRef;
