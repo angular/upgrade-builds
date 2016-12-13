76857a9089bcad3c3ef3ad238ab5e51cc1d598c4
GitHub <noreply@github.com>
2016-12-13 03:22:48 +0000

    4b3d135 fix(compiler): xmb `<ph>` tags should not self close (#13413)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-1d0ed6f
+ * @license Angular v2.3.0-4b3d135
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1297,7 +1297,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-1d0ed6f');
+  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-4b3d135');
 
   exports.UpgradeAdapter = UpgradeAdapter;
   exports.UpgradeAdapterRef = UpgradeAdapterRef;
