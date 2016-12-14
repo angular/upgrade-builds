24f3285855f9e1498a109896f40e09f4303c985a
Victor Berchet <victor@suumit.com>
2016-12-14 01:41:15 +0000

    f5f1d5f fix(compiler): make sure provider values with `name` property don’t break.

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-42203ea
+ * @license Angular v2.3.0-f5f1d5f
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1297,7 +1297,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-42203ea');
+  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-f5f1d5f');
 
   exports.UpgradeAdapter = UpgradeAdapter;
   exports.UpgradeAdapterRef = UpgradeAdapterRef;
