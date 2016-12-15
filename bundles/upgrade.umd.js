54b462c10c7562eddf3063eed5d40ebc0c517f6f
Victor Berchet <victor@suumit.com>
2016-12-15 02:46:44 +0000

    540b119 fix(form): fix merge errors

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-6cefccb
+ * @license Angular v2.3.1-540b119
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1508,7 +1508,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.0-6cefccb');
+  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.1-540b119');
 
   exports.UpgradeAdapter = UpgradeAdapter;
   exports.UpgradeAdapterRef = UpgradeAdapterRef;
