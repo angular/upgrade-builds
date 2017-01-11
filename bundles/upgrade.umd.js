3f7506b93a18e7c12508783a3dd5b84d89ef708c
Matias Niemelä <matias@yearofmoo.com>
2017-01-11 22:20:58 +0000

    a7d2804 docs(changelog): add changelog for 2.4.3

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.2-4b56f79
+ * @license Angular v2.4.3-a7d2804
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1519,7 +1519,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new _angular_core.Version('2.4.2-4b56f79');
+  var /** @type {?} */ VERSION = new _angular_core.Version('2.4.3-a7d2804');
 
   exports.UpgradeAdapter = UpgradeAdapter;
   exports.UpgradeAdapterRef = UpgradeAdapterRef;
