800ba9284218f051b7ba1c9b8bc0e000bd517c32
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-24 23:06:20 +0000

    fc55018 ci: disable broken commit message validation

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.4-0c7726d
+ * @license Angular v4.0.0-beta.4-fc55018
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1546,7 +1546,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.4-0c7726d');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.4-fc55018');
 
     exports.UpgradeAdapter = UpgradeAdapter;
     exports.UpgradeAdapterRef = UpgradeAdapterRef;
