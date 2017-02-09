4acc78563957137d3799e84b7327ea515845113f
Miško Hevery <misko@hevery.com>
2017-02-09 18:26:31 +0000

    09b4bd0 docs(changelog): add changelog for 4.0.0-beta.7

---
+++ src/common/content_projection_helper.js
@@ -0,0 +1,25 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+var ContentProjectionHelper = (function () {
+    function ContentProjectionHelper() {
+    }
+    /**
+     * @param {?} $injector
+     * @param {?} component
+     * @param {?} nodes
+     * @return {?}
+     */
+    ContentProjectionHelper.prototype.groupProjectableNodes = function ($injector, component, nodes) {
+        // By default, do not support multi-slot projection,
+        // as `upgrade/static` does not support it yet.
+        return [nodes];
+    };
+    return ContentProjectionHelper;
+}());
+export { ContentProjectionHelper };
+//# sourceMappingURL=content_projection_helper.js.map
