c11391198bc004f6a67e13cb0cc7555b5be02705
Igor Minar <igor@angularjs.org>
2017-02-18 23:37:14 +0000

    1bdf706 release: cut v4.0.0-beta.8

---
+++ static.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './public_api_static';
+export { ContentProjectionHelper as ɵf } from './src/common/content_projection_helper';
+export { angular1Providers as ɵa, compileFactory as ɵd, injectorFactory as ɵb, parseFactory as ɵe, rootScopeFactory as ɵc } from './src/static/angular1_providers';
