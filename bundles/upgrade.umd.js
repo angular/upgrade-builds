27a28209d5da940bcb230bbc0b3cfcaa31e930e5
Victor Berchet <victor@suumit.com>
2017-02-21 02:20:22 +0000

    b658fa9 fix(http): Make ResponseOptionsArgs an interface

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.8-2a12346
+ * @license Angular v2.4.8-b658fa9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1525,7 +1525,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.8-2a12346');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.8-b658fa9');
 
     exports.UpgradeAdapter = UpgradeAdapter;
     exports.UpgradeAdapterRef = UpgradeAdapterRef;
