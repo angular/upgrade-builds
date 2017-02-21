89868b0518a1cb98cb1b6bc543ff5ce49b9a4592
Victor Berchet <victor@suumit.com>
2017-02-21 04:08:08 +0000

    2ddd1c3 Revert "fix(http): Make ResponseOptionsArgs an interface (#14607)"

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-2a191ca
+ * @license Angular v4.0.0-beta.8-2ddd1c3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -532,7 +532,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.8-2a191ca');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.8-2ddd1c3');
 
     /**
      * @license
