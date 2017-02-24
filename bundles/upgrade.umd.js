1c472af6f023fa3237aa223fd3b46ba78f5ab691
GitHub <noreply@github.com>
2017-02-24 21:26:35 +0000

    3c9a46c fix(core): fix directive instantiation (#14715)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -114,7 +114,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-436a179');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-3c9a46c');
 
     /**
      * @license
