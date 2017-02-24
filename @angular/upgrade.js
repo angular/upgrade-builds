05ec04fca85f1fc5541f05b3752069f3fec0b8c1
Igor Minar <iminar@google.com>
2017-02-24 17:58:42 +0000

    93ddd38 refactor(animations): make modules and bundles into plural Animations (#14695)

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-32c2fd5
+ * @license Angular v4.0.0-beta.8-93ddd38
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-beta.8-32c2fd5');
+const /** @type {?} */ VERSION = new Version('4.0.0-beta.8-93ddd38');
 
 /**
  * @license
