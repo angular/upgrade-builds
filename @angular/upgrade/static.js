9c796204adcabe258cefea0ccd53f7cc3b052b1a
Igor Minar <iminar@google.com>
2017-02-24 20:13:25 +0000

    5094aef fix(compiler-cli): fix flat module import on Windows (#14712)

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -446,7 +446,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-beta.8-93ddd38');
+var VERSION = new Version('4.0.0-beta.8-5094aef');
 
 /**
  * @license
--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -114,7 +114,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-93ddd38');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-5094aef');
 
     /**
      * @license
