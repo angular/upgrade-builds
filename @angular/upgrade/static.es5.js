07d5651bcbfa9b1ec5dfd7de56ef2ac333f7d0d7
Igor Minar <iminar@google.com>
2017-02-24 01:44:46 +0000

    a23634d fix(common): do not reference deprecated classes in providers (#14523) (#14523)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -451,7 +451,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-beta.8-c53621b');
+var VERSION = new Version('4.0.0-beta.8-a23634d');
 
 /**
  * @license
