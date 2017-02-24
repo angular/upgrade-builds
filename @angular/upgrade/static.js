3cc18505378b9ba113804f4b2d3b67b500d4df90
Igor Minar <iminar@google.com>
2017-02-24 07:47:10 +0000

    1f3198c feat(aio): copy example code from angular.io

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -446,7 +446,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-beta.8-2da3844');
+var VERSION = new Version('4.0.0-beta.8-1f3198c');
 
 /**
  * @license
