48189579de4e551d728dd15dea6a204a4db5c278
GitHub <noreply@github.com>
2017-02-25 02:44:29 +0000

    f32f4de docs: more changelog fixes

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-26ed262
+ * @license Angular v4.0.0-rc.1-f32f4de
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-26ed262');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-f32f4de');
 
 /**
  * @license
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -451,7 +451,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.1-26ed262');
+var VERSION = new Version('4.0.0-rc.1-f32f4de');
 
 /**
  * @license
