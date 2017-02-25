ec1e264de33baa323a14a409a98cafcc1ea56e8c
Igor Minar <igor@angularjs.org>
2017-02-25 02:29:34 +0000

    c7d1b36 docs: update release schedule due to bad 4.0.0-rc.0 release

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -451,7 +451,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-beta.8-2602b03');
+var VERSION = new Version('4.0.0-rc.1-c7d1b36');
 
 /**
  * @license
