9386c95a41085c545e03f66bc67ea3f63272f224
Igor Minar <iminar@google.com>
2017-02-27 22:52:33 +0000

    de36f8a revert: fix(router): do not finish bootstrap until all the routes are resolved (#14327)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.8-b658fa9
+ * @license Angular v2.4.8-de36f8a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1525,7 +1525,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.8-b658fa9');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.8-de36f8a');
 
     exports.UpgradeAdapter = UpgradeAdapter;
     exports.UpgradeAdapterRef = UpgradeAdapterRef;
