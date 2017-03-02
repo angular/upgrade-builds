c017b22d04b75d1e338473d4624618ae13306408
Igor Minar <iminar@google.com>
2017-03-02 08:42:07 +0000

    207298c build(aio): generate marketing docs JSON files

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -446,7 +446,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.2-a24e652');
+var VERSION = new Version('4.0.0-rc.2-207298c');
 
 /**
  * @license
--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -114,7 +114,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-a24e652');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-207298c');
 
     /**
      * @license
