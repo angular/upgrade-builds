a439076570b99ab16cfc652f2b9b1c9dcd536d5f
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 18:45:03 +0000

    6559425 fix(core): ErrorHandler should not rethrow an error by default (#15077)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-df914ef
+ * @license Angular v4.0.0-rc.3-6559425
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-rc.3-df914ef');
+    var VERSION = new _angular_core.Version('4.0.0-rc.3-6559425');
     /**
      * @license
      * Copyright Google Inc. All Rights Reserved.
