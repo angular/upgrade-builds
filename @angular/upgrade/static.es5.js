373cea4b3e5eff7a60011d0fd2290c9dce21088b
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 16:54:41 +0000

    fa1920a feat(aio): enable deep-linking on deployed apps (until prerendering is done) (#15049)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-71cd295
+ * @license Angular v4.0.0-rc.3-fa1920a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -447,7 +447,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-71cd295');
+var VERSION = new Version('4.0.0-rc.3-fa1920a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
