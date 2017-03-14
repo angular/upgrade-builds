a86f47e55bb10cc0ccccd00b42d82808c71f47d5
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 01:03:43 +0000

    eedca09 build(aio): generate the api-list.json file from the API docs

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-ab40fcb
+ * @license Angular v4.0.0-rc.3-eedca09
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-ab40fcb');
+var VERSION = new Version('4.0.0-rc.3-eedca09');
 
 /**
  * @license
