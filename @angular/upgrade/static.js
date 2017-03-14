0c09d73a09d0ddd2ade0fc4ba978e2dbcaf9aba7
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 23:44:36 +0000

    13686bb fix: element injector vs module injector (#15044)

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-f093501
+ * @license Angular v4.0.0-rc.3-13686bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1003,7 +1003,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-f093501');
+var VERSION = new Version('4.0.0-rc.3-13686bb');
 
 /**
  * @license
