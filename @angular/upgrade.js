62ca2fc71ee602390c1f17cf40017b4bdd67754e
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 21:25:51 +0000

    0aad270 refactor(platform-browser): move platform-browser/animations to animations/browser (#15130)

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-221899a
+ * @license Angular v4.0.0-rc.3-0aad270
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-221899a');
+var VERSION = new Version('4.0.0-rc.3-0aad270');
 
 /**
  * @license
