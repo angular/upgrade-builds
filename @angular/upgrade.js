54689c4f9ee17ddf7f8fc23780cfd56df7fe841d
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 00:11:50 +0000

    8850098 build(aio): ensure API docs are generated in the correct place (#15103)

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-e6c81d2
+ * @license Angular v4.0.0-rc.3-8850098
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-e6c81d2');
+var VERSION = new Version('4.0.0-rc.3-8850098');
 
 /**
  * @license
--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-e6c81d2
+ * @license Angular v4.0.0-rc.3-8850098
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -452,7 +452,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-rc.3-e6c81d2');
+    var VERSION = new _angular_core.Version('4.0.0-rc.3-8850098');
     /**
      * @license
      * Copyright Google Inc. All Rights Reserved.
