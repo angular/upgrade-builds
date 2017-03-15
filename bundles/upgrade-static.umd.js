577fbe457f8bad909c9f848c3051a050f4368bb8
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-15 23:24:46 +0000

    bf98d9d fix(aio): scroll to hash fragment element on URL change

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-a9b1880
+ * @license Angular v4.0.0-rc.3-bf98d9d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1004,7 +1004,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-rc.3-a9b1880');
+    var VERSION = new _angular_core.Version('4.0.0-rc.3-bf98d9d');
     /**
      * @license
      * Copyright Google Inc. All Rights Reserved.
