07f45bafd2a99217d18deae815dc37eacc33a0d4
GitHub <noreply@github.com>
2017-03-15 20:55:08 +0000

    795638e ci: remove unused import from integration test (#15175)

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-322bf7a
+ * @license Angular v4.0.0-rc.3-795638e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-322bf7a');
+var VERSION = new Version('4.0.0-rc.3-795638e');
 
 /**
  * @license
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-322bf7a
+ * @license Angular v4.0.0-rc.3-795638e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-322bf7a');
+var VERSION = new Version('4.0.0-rc.3-795638e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
