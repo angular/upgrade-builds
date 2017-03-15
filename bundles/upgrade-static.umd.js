dcfb9e8f22e37a1517f5934553e240e4f104946a
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 03:08:37 +0000

    5c34066 fix(compiler): only warn for `@Injectable` classes with invalid args.

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-3c15916
+ * @license Angular v4.0.0-rc.3-5c34066
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1004,7 +1004,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-rc.3-3c15916');
+    var VERSION = new _angular_core.Version('4.0.0-rc.3-5c34066');
     /**
      * @license
      * Copyright Google Inc. All Rights Reserved.
