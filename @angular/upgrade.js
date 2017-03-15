f6909d18a75f2c1130f403eaca7d10d5b29decac
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 22:45:41 +0000

    f1b33ab fix(http): Make ResponseOptionsArgs an interface (#14607) (#14623)

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-029d0f2
+ * @license Angular v4.0.0-rc.3-f1b33ab
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-029d0f2');
+var VERSION = new Version('4.0.0-rc.3-f1b33ab');
 
 /**
  * @license
--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-029d0f2
+ * @license Angular v4.0.0-rc.3-f1b33ab
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-rc.3-029d0f2');
+    var VERSION = new _angular_core.Version('4.0.0-rc.3-f1b33ab');
     /**
      * @license
      * Copyright Google Inc. All Rights Reserved.
