60b5ace2e05fb83a46c876b917260b6d1a7b0cdf
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-15 22:42:55 +0000

    a9b1880 feat(aio): top bar typography, sidenav size

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-ec548ad
+ * @license Angular v4.0.0-rc.3-a9b1880
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-ec548ad');
+var VERSION = new Version('4.0.0-rc.3-a9b1880');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
