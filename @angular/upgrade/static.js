11d6d85b51172a105103e19c80e705b6af256e57
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 20:17:01 +0000

    77fd91d fix(core): ErrorHandler should not rethrow an error by default (#15077) (#15208)

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-492153a
+ * @license Angular v4.0.0-rc.3-77fd91d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1003,7 +1003,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-492153a');
+var VERSION = new Version('4.0.0-rc.3-77fd91d');
 
 /**
  * @license
