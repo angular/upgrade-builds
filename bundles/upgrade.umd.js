7c919ea95af2e4cda385caf48228d48f3f66ef25
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 23:26:22 +0000

    b0ae464 ci: changing deployment token

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.9-80fe41a
+ * @license Angular v2.4.9-b0ae464
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1525,7 +1525,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.9-80fe41a');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.9-b0ae464');
 
     exports.UpgradeAdapter = UpgradeAdapter;
     exports.UpgradeAdapterRef = UpgradeAdapterRef;
