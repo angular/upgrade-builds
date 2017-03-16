f3647937996dbdc323e6fbf6ab14a129e74dd868
Igor Minar <iminar@google.com>
2017-03-16 06:50:56 +0000

    480a407 build(aio): implement prerendering

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-923d0c5
+ * @license Angular v4.0.0-rc.3-480a407
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-923d0c5');
+var VERSION = new Version('4.0.0-rc.3-480a407');
 
 /**
  * @license
