993c18c6368425b385b53a765fe9a85ead717e2d
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 00:29:18 +0000

    923d0c5 fix: correct UMD resolutions for platform-browser_animations (#15190)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-013d806
+ * @license Angular v4.0.0-rc.3-923d0c5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-013d806');
+var VERSION = new Version('4.0.0-rc.3-923d0c5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
