482d9705b23bd48e10249e6533617907057565dc
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 20:42:21 +0000

    d2fbbb4 fix(core): update peer dep on zone.js to ^0.8.4 (#15209)

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-77fd91d
+ * @license Angular v4.0.0-rc.3-d2fbbb4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-77fd91d');
+var VERSION = new Version('4.0.0-rc.3-d2fbbb4');
 
 /**
  * @license
