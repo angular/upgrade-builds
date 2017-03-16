d9dfabaef0b2d51b5a40aa2229eba3d2bca7b72c
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 17:31:46 +0000

    26d4ce2 fix(forms): ensure observable validators are properly canceled (#15132)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-41f61b0
+ * @license Angular v4.0.0-rc.3-26d4ce2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1004,7 +1004,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-rc.3-41f61b0');
+    var VERSION = new _angular_core.Version('4.0.0-rc.3-26d4ce2');
     /**
      * @license
      * Copyright Google Inc. All Rights Reserved.
