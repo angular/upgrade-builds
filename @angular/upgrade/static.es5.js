a6fb93886b01bba7b1e6ecf1bc2323f287c786e5
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 00:28:42 +0000

    013d806 fix(platform-server): handle styles with extra ':'s correctly (#15189)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-dd36d41
+ * @license Angular v4.0.0-rc.3-013d806
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.3-dd36d41');
+var VERSION = new Version('4.0.0-rc.3-013d806');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
