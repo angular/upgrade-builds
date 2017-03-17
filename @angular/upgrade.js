662e6356acc6c809fdb541023e2d3bdb67b3094c
Miško Hevery <misko@hevery.com>
2017-03-17 21:33:53 +0000

    6e9264a fix(tsc-wrapped): emit flat module format correctly on Windows (#15215)

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-f925910
+ * @license Angular v4.0.0-rc.5-6e9264a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-f925910');
+const VERSION = new Version('4.0.0-rc.5-6e9264a');
 
 /**
  * @license
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-f925910
+ * @license Angular v4.0.0-rc.5-6e9264a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -462,7 +462,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.5-f925910');
+var VERSION = new Version('4.0.0-rc.5-6e9264a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
