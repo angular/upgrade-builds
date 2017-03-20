52ec9f5425deb4d8167e53c9244138d2dc6a1e7b
Victor Berchet <victor@suumit.com>
2017-03-20 18:06:19 +0000

    d3eda7a feat(router): add `ParamMap.keys` to get a list of parameters

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-5da021c
+ * @license Angular v4.0.0-rc.5-d3eda7a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.5-5da021c');
+var VERSION = new Version('4.0.0-rc.5-d3eda7a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-5da021c
+ * @license Angular v4.0.0-rc.5-d3eda7a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.5-5da021c');
+var VERSION = new Version('4.0.0-rc.5-d3eda7a');
 
 /**
  * @license
