71bd7aafe649d120c42cdafba4260cc38c4c33ef
Miško Hevery <misko@hevery.com>
2017-03-21 20:18:15 +0000

    64beae9 fix(core): mark components for check when host events trigger. (#15359)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-8e6995c
+ * @license Angular v4.0.0-rc.5-64beae9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.0.0-rc.5-8e6995c');
+var VERSION = new Version('4.0.0-rc.5-64beae9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
