3213d5bd85f44804eb03cbe8f3552b4c9972525d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-21 17:16:45 +0000

    f40f915 build(aio): upgrade to rxjs@5.2.0

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-11d5309
+ * @license Angular v4.0.0-rc.5-f40f915
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.5-11d5309');
+var VERSION = new Version('4.0.0-rc.5-f40f915');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
