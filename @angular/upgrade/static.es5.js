5c18960e0ceeb4003764824ff7b2c6003bbb56c6
Igor Minar <iminar@google.com>
2017-03-22 20:45:15 +0000

    941f194 docs: spelling errors (#15375)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-8b4edcc
+ * @license Angular v4.0.0-rc.5-941f194
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.5-8b4edcc');
+var VERSION = new Version('4.0.0-rc.5-941f194');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
