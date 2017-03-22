ca1412e901acd4b252f9bbf6df8437a28f6e7e93
Igor Minar <iminar@google.com>
2017-03-22 20:45:42 +0000

    64285a2 docs: update 4.0.0-rc.5 release schedule (#15288)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-941f194
+ * @license Angular v4.0.0-rc.5-64285a2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.5-941f194');
+var VERSION = new Version('4.0.0-rc.5-64285a2');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
