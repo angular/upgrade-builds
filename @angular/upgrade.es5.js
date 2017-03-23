dd5f225bc756ec8075e321fd8a81d5c37058b768
Igor Minar <igor@angularjs.org>
2017-03-23 06:31:32 +0000

    ee03418 release: cut the 4.0.0-rc.6 release

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-da700d1
+ * @license Angular v4.0.0-rc.6-ee03418
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.0.0-rc.5-da700d1');
+var VERSION = new Version('4.0.0-rc.6-ee03418');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
