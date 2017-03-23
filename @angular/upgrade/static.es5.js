f6f4333e045a41ac068a30e03df20c9d6f7e5076
Victor Berchet <victor@suumit.com>
2017-03-23 20:56:26 +0000

    0dda01e fix(compiler): correctly handle when `toString` is exported (#15430)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.6-92084f2
+ * @license Angular v4.0.0-rc.6-0dda01e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.6-92084f2');
+var VERSION = new Version('4.0.0-rc.6-0dda01e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
