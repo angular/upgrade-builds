7dfff3dfb567290febb8f35fd098ba7966e77eec
Victor Berchet <victor@suumit.com>
2017-03-23 18:04:45 +0000

    08f2f08 fix(router): should pass new data to Observable when query params change (#15387)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.6-376088d
+ * @license Angular v4.0.0-rc.6-08f2f08
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.0.0-rc.6-376088d');
+var VERSION = new Version('4.0.0-rc.6-08f2f08');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.6-376088d
+ * @license Angular v4.0.0-rc.6-08f2f08
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-rc.6-376088d');
+var VERSION = new Version('4.0.0-rc.6-08f2f08');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
