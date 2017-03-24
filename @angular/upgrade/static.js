4809ad2628ca70461f721fc9ea06e290bff6f96c
GitHub <noreply@github.com>
2017-03-24 00:37:42 +0000

    db0dca3 docs: add a note about exclusion of symbols prefixed with ɵ from our public api surface (#15440)

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-7a715b2
+ * @license Angular v4.0.0-db0dca3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-7a715b2');
+var VERSION = new Version('4.0.0-db0dca3');
 
 /**
  * @license
