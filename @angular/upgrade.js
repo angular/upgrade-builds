87af1689d8716656eccf36edbcd7b3d367f52712
Igor Minar <iminar@google.com>
2017-03-24 15:38:17 +0000

    d92930e docs: fixed broken links (#15455)

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-c2892da
+ * @license Angular v4.0.0-d92930e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-c2892da');
+const VERSION = new Version('4.0.0-d92930e');
 
 /**
  * @license
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-c2892da
+ * @license Angular v4.0.0-d92930e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-c2892da');
+var VERSION = new Version('4.0.0-d92930e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
