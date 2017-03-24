0092d56cacadbb0c682cabadedf0e29562642d08
Igor Minar <igor@angularjs.org>
2017-03-24 04:14:33 +0000

    c2892da docs: revert the move of CONTRIBUTING.md to docs/CONTRIBUTING.md

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-e1b09e3
+ * @license Angular v4.0.0-c2892da
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.0.0-e1b09e3');
+var VERSION = new Version('4.0.0-c2892da');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
