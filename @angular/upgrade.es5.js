1c9cca62b40104dfbc39c68ab87bdb0b7570ad4e
GitHub <noreply@github.com>
2017-03-24 02:42:27 +0000

    e1b09e3 docs: update CONTRIBUTING link in README.md

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-b2b1195
+ * @license Angular v4.0.0-e1b09e3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.0.0-b2b1195');
+var VERSION = new Version('4.0.0-e1b09e3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
