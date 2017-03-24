08aff38f67c56c85c51abe5bd4233642fd52c1b3
Victor Berchet <victor@suumit.com>
2017-03-24 21:54:46 +0000

    ea848f7 docs: clarify querying all descendants (#15400)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-b8d5f87
+ * @license Angular v4.0.0-ea848f7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-b8d5f87');
+var VERSION = new Version('4.0.0-ea848f7');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-b8d5f87
+ * @license Angular v4.0.0-ea848f7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-b8d5f87');
+var VERSION = new Version('4.0.0-ea848f7');
 
 /**
  * @license
