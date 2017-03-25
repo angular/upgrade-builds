1bb59b20d35524c9a3cc50bee61be7f6fc81d516
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-25 21:50:48 +0000

    1616cae build(aio): add renderExamples processor

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-c9e51d9
+ * @license Angular v4.0.0-1616cae
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-c9e51d9');
+var VERSION = new Version('4.0.0-1616cae');
 
 /**
  * @license
