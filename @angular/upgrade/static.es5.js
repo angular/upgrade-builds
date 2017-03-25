8b24df7b93b2d656f23999820b9ac92b9d32b1a3
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-25 07:12:41 +0000

    afb17a5 feat(aio): block code examples style fix

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-53b89ec
+ * @license Angular v4.0.0-afb17a5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-53b89ec');
+var VERSION = new Version('4.0.0-afb17a5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
