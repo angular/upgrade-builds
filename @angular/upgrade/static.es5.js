55355cea3fc3c2ecb58c58a08bf2ee0b15e87130
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-25 14:39:57 +0000

    c9e51d9 feat(aio): add new classes, table and images styling (#15478)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-b283038
+ * @license Angular v4.0.0-c9e51d9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-b283038');
+var VERSION = new Version('4.0.0-c9e51d9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
