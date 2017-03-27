01fc64a93e16a43956eb442aa768bbb8cf748ed7
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-27 13:54:20 +0000

    6605dd1 docs(aio): correct spelling in overview

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-837ed78
+ * @license Angular v4.0.0-6605dd1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.0.0-837ed78');
+var VERSION = new Version('4.0.0-6605dd1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-837ed78
+ * @license Angular v4.0.0-6605dd1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.0.0-837ed78
+ * @license Angular v4.0.0-6605dd1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.0.0-837ed78');
+var VERSION = new _angular_core.Version('4.0.0-6605dd1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
