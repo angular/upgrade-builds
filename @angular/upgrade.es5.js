e5ca0ab5c25cdb13d5bd5b52895f9763eee56d73
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-27 09:31:21 +0000

    52ea193 build(aio): left align code regions

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-228238e
+ * @license Angular v4.0.0-52ea193
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.0.0-228238e');
+var VERSION = new Version('4.0.0-52ea193');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-228238e
+ * @license Angular v4.0.0-52ea193
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-228238e');
+var VERSION = new Version('4.0.0-52ea193');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
