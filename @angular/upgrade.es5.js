0d735fbf2ecf4b55445ba1c60760bcfa87c47cae
Victor Berchet <victor@suumit.com>
2017-03-28 17:06:29 +0000

    b7fa5de fix(core): improve error msg for invalid KeyValueDiffer.diff arg (#15489)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-d05aa70
+ * @license Angular v4.0.0-b7fa5de
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.0.0-d05aa70');
+var VERSION = new Version('4.0.0-b7fa5de');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-d05aa70
+ * @license Angular v4.0.0-b7fa5de
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-d05aa70');
+var VERSION = new Version('4.0.0-b7fa5de');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
