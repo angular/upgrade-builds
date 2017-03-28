bf9c671150574176d3f5caf6dbfa89d6524174ea
Victor Berchet <victor@suumit.com>
2017-03-28 20:50:47 +0000

    d438b88 fix(compiler): ignore errors when evaluating base classes (#15560)

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-6269d28
+ * @license Angular v4.0.0-d438b88
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-6269d28');
+var VERSION = new Version('4.0.0-d438b88');
 
 /**
  * @license
