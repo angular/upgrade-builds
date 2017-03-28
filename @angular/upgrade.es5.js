3c655957a503a20c342a16544fcf5b5cccc4cddf
Victor Berchet <victor@suumit.com>
2017-03-28 00:55:15 +0000

    a9ae4da ci: add tbosch and vicb as approvers of the language service (#15530)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-15662ef
+ * @license Angular v4.0.0-a9ae4da
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.0.0-15662ef');
+var VERSION = new Version('4.0.0-a9ae4da');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-15662ef
+ * @license Angular v4.0.0-a9ae4da
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-15662ef');
+var VERSION = new Version('4.0.0-a9ae4da');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
