11ef81d3cc5dd8507259aa594a42b59b08f6c135
Victor Berchet <victor@suumit.com>
2017-03-28 23:33:29 +0000

    0c36f23 perf(router): don't create new serializer every time UrlTree.toString is called (#15565)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-a580f8c
+ * @license Angular v4.0.0-0c36f23
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.0.0-a580f8c
+ * @license Angular v4.0.0-0c36f23
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.0.0-a580f8c');
+var VERSION = new _angular_core.Version('4.0.0-0c36f23');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
