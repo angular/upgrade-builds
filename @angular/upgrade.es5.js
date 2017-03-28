6e0aad79f0c3b3bf28dd8d72bb28640bd1b77546
Igor Minar <iminar@google.com>
2017-03-28 15:42:09 +0000

    eac99c1 build(aio): do not HTML format code-example contents (#15554)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-b7a89ce
+ * @license Angular v4.0.0-eac99c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.0.0-b7a89ce');
+var VERSION = new Version('4.0.0-eac99c1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
