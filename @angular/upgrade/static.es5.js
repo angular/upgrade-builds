04a5247ba4b620a0e21ba20e36e65ae59bf78370
GitHub <noreply@github.com>
2017-03-28 23:07:31 +0000

    93d48f1 fix(core): fix the key/value differ (#15539)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-861953c
+ * @license Angular v4.0.0-93d48f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-861953c');
+var VERSION = new Version('4.0.0-93d48f1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-861953c
+ * @license Angular v4.0.0-93d48f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-861953c');
+var VERSION = new Version('4.0.0-93d48f1');
 
 /**
  * @license
