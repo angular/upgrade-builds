e2745c1e0cbfe0d491cfbd89a0491b28a8aebe84
Victor Berchet <victor@suumit.com>
2017-03-28 21:59:18 +0000

    861953c feat(aio): add LiveExampleComponent (#15544)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-aa16ccd
+ * @license Angular v4.0.0-861953c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.0.0-aa16ccd');
+var VERSION = new Version('4.0.0-861953c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
