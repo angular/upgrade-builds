d2c65062116e4515c468256dd13fccc21e57214f
Victor Berchet <victor@suumit.com>
2017-03-29 17:47:28 +0000

    9c77a7c fix(compiler): throw when a component defines both template and templateUrl (#15572)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-331b9f6
+ * @license Angular v4.0.0-9c77a7c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-331b9f6');
+var VERSION = new Version('4.0.0-9c77a7c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-331b9f6
+ * @license Angular v4.0.0-9c77a7c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-331b9f6');
+var VERSION = new Version('4.0.0-9c77a7c');
 
 /**
  * @license
