b94430103875c9b14798e3a38f9eac83d4e3f7ea
Victor Berchet <victor@suumit.com>
2017-03-29 00:13:48 +0000

    4916278 docs(core): fix API docs for `Injector.get`

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-0c36f23
+ * @license Angular v4.0.0-4916278
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-0c36f23');
+var VERSION = new Version('4.0.0-4916278');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-0c36f23
+ * @license Angular v4.0.0-4916278
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-0c36f23');
+var VERSION = new Version('4.0.0-4916278');
 
 /**
  * @license
