748b1ec26b5a036a2802ccc4bf74b81696e6c8f0
Victor Berchet <victor@suumit.com>
2017-03-29 23:31:41 +0000

    14fd78f fix(core): fix inheritance in JIT mode for TS 2.1 (#15599)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-a9321b1
+ * @license Angular v4.0.0-14fd78f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-a9321b1');
+var VERSION = new Version('4.0.0-14fd78f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
