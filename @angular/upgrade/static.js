be50503eb5b64521d53fcf0ca82d0f261264cb8d
Victor Berchet <victor@suumit.com>
2017-03-29 17:34:55 +0000

    23bf348 refactor(router): cleanup & simplification (#15436)

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-95afaf4
+ * @license Angular v4.0.0-23bf348
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-95afaf4');
+var VERSION = new Version('4.0.0-23bf348');
 
 /**
  * @license
