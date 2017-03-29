c65873c7a789c0230859fe30da84a1cc3ea5a5ac
Victor Berchet <victor@suumit.com>
2017-03-29 23:46:46 +0000

    1dda05a docs: add changelog for 4.0.1

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-14fd78f
+ * @license Angular v4.0.0-1dda05a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-14fd78f');
+var VERSION = new Version('4.0.0-1dda05a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
