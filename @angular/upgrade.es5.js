f533c5991ce2a5a2847c04c529218577d9bf0214
Victor Berchet <victor@suumit.com>
2017-03-29 17:37:19 +0000

    331b9f6 fix(language-service): don't require `reflect-metadata` module to be provided (#15569)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-d58a242
+ * @license Angular v4.0.0-331b9f6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.0.0-d58a242');
+var VERSION = new Version('4.0.0-331b9f6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-d58a242
+ * @license Angular v4.0.0-331b9f6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-d58a242');
+var VERSION = new Version('4.0.0-331b9f6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
