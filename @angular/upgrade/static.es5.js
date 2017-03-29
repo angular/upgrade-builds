987c95f8660fd1b238b5b57d7582513243f2c8cb
Victor Berchet <victor@suumit.com>
2017-03-29 22:28:30 +0000

    19cb503 docs(http): remove deprecated stuff and outdated plunkrs (#15598)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-9f2acf5
+ * @license Angular v4.0.0-19cb503
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-9f2acf5');
+var VERSION = new Version('4.0.0-19cb503');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
