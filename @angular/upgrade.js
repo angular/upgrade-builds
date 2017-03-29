92823c93aaab707f491c95b55decf5e55e0bae10
Victor Berchet <victor@suumit.com>
2017-03-29 23:31:19 +0000

    a9321b1 fix(language-service): correctly determine base members of types (#15600)

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-19cb503
+ * @license Angular v4.0.0-a9321b1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-19cb503');
+const VERSION = new Version('4.0.0-a9321b1');
 
 /**
  * @license
