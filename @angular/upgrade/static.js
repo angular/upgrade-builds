f9cda5012da544ef9ea2c20d6436e05aa5f40155
Victor Berchet <victor@suumit.com>
2017-03-30 00:22:26 +0000

    67719f2 docs: add changelog for 4.1.0-beta.0

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-1dda05a
+ * @license Angular v4.1.0-beta.0-67719f2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.0.0-1dda05a');
+var VERSION = new Version('4.1.0-beta.0-67719f2');
 
 /**
  * @license
