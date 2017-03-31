e2b9da13a1a44fa7e0e86a2871e1b5e5ec3d8bda
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-31 08:55:59 +0000

    4c21114 feat(aio): remove google feedback

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-65bb237
+ * @license Angular v4.1.0-beta.0-4c21114
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-65bb237');
+const VERSION = new Version('4.1.0-beta.0-4c21114');
 
 /**
  * @license
