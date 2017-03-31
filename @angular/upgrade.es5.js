41f673c617e8e19e2f664f21b9a64e3c3af2bc29
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-31 07:40:22 +0000

    65bb237 fix(aio): remove "v.2" from index.html "title"

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-8c925bc
+ * @license Angular v4.1.0-beta.0-65bb237
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.0-8c925bc');
+var VERSION = new Version('4.1.0-beta.0-65bb237');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
