612b51615f4a1787a8e4cc395ee124c887847582
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-03-31 18:55:28 +0000

    5fbb0d0 fix(language-service): resolve any parameter types to any result

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-7c2ce29
+ * @license Angular v4.1.0-beta.0-5fbb0d0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.0-7c2ce29');
+var VERSION = new Version('4.1.0-beta.0-5fbb0d0');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-7c2ce29
+ * @license Angular v4.1.0-beta.0-5fbb0d0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.0-7c2ce29
+ * @license Angular v4.1.0-beta.0-5fbb0d0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.0-7c2ce29');
+var VERSION = new _angular_core.Version('4.1.0-beta.0-5fbb0d0');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
