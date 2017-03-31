9d8c04882710fffd7ed8798bf312a690c4dd25b7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-03-31 18:54:10 +0000

    7c2ce29 feat(aio): add footer background

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-5c38012
+ * @license Angular v4.1.0-beta.0-7c2ce29
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.0-5c38012');
+var VERSION = new Version('4.1.0-beta.0-7c2ce29');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
