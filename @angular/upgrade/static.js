298a250921f029472eec74ac8e0a00bc637ed8a1
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-01 07:01:40 +0000

    ad639d7 feat(aio): add contribute marketing page

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-e1c2e50
+ * @license Angular v4.1.0-beta.0-ad639d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-e1c2e50');
+var VERSION = new Version('4.1.0-beta.0-ad639d7');
 
 /**
  * @license
