5ab48c6a208d86c7d054cd768fa0deb8f7f33bdf
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-01 11:04:31 +0000

    c8be396 style(aio): fix minor syntax error

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-97deb01
+ * @license Angular v4.1.0-beta.0-c8be396
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.0-97deb01');
+var VERSION = new Version('4.1.0-beta.0-c8be396');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
