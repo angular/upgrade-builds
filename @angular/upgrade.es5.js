e91c581f33c8b6f6c7cb93d6dd34b36110364c97
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-01 07:05:43 +0000

    97deb01 feat(aio): add about component and data

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-ad639d7
+ * @license Angular v4.1.0-beta.0-97deb01
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.0-ad639d7');
+var VERSION = new Version('4.1.0-beta.0-97deb01');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
