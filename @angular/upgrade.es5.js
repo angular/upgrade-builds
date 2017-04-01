01503270e5b91086d9536e6bf7cd51e545af7b7a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-01 17:37:50 +0000

    8c726ea fix(aio): preserve space after anchor elements

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-c8be396
+ * @license Angular v4.1.0-beta.0-8c726ea
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.0-c8be396');
+var VERSION = new Version('4.1.0-beta.0-8c726ea');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-c8be396
+ * @license Angular v4.1.0-beta.0-8c726ea
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-c8be396');
+var VERSION = new Version('4.1.0-beta.0-8c726ea');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
