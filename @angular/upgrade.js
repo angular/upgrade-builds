dc060f73db026387c8831d286f94e7a33ba8b3b6
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-01 06:32:51 +0000

    e1c2e50 docs(aio): move bio images into content folder

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-08941aa
+ * @license Angular v4.1.0-beta.0-e1c2e50
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-08941aa');
+const VERSION = new Version('4.1.0-beta.0-e1c2e50');
 
 /**
  * @license
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-08941aa
+ * @license Angular v4.1.0-beta.0-e1c2e50
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-08941aa');
+var VERSION = new Version('4.1.0-beta.0-e1c2e50');
 
 /**
  * @license
