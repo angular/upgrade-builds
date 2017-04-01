368079ffbe1dad5d697e03deffc182e3604da78c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-04-01 00:29:22 +0000

    2454a3b build(aio): process contributor.json file

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-03fb766
+ * @license Angular v4.1.0-beta.0-2454a3b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-03fb766');
+var VERSION = new Version('4.1.0-beta.0-2454a3b');
 
 /**
  * @license
