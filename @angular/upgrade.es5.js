073157c6d5d000a02ad5c358dcb50486a786dc74
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-04-03 22:57:27 +0000

    0e38bf9 build(aio): new migration

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-309bae0
+ * @license Angular v4.1.0-beta.0-0e38bf9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.0-309bae0');
+var VERSION = new Version('4.1.0-beta.0-0e38bf9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-309bae0
+ * @license Angular v4.1.0-beta.0-0e38bf9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-309bae0');
+var VERSION = new Version('4.1.0-beta.0-0e38bf9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
