b3e1c7a60b99f8ee7a91a76d85540ef0f04a46be
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-04-04 21:18:20 +0000

    fe0d02f fix(language-service): initialize static reflector correctly

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-7b005aa
+ * @license Angular v4.1.0-beta.0-fe0d02f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.0-7b005aa
+ * @license Angular v4.1.0-beta.0-fe0d02f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.0-7b005aa');
+var VERSION = new _angular_core.Version('4.1.0-beta.0-fe0d02f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
