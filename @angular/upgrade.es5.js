25af88210286d3f5445b3e68d208b01ddc048355
Matias Niemelä <matias@yearofmoo.com>
2017-04-04 18:07:14 +0000

    64f1bf6 feat(animations): wip: introduce a wave of new animation features

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-691cb98
+ * @license Angular v4.1.0-beta.0-64f1bf6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.0-691cb98');
+var VERSION = new Version('4.1.0-beta.0-64f1bf6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
