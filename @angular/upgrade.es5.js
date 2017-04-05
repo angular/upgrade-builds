e435315f17faa5c20c5b9228df3253fab1e8d469
Matias Niemelä <matias@yearofmoo.com>
2017-04-05 06:09:32 +0000

    294a205 feat(animations): wip: introduce a wave of new animation features

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-f4f621a
+ * @license Angular v4.1.0-beta.0-294a205
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.0-f4f621a');
+var VERSION = new Version('4.1.0-beta.0-294a205');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-f4f621a
+ * @license Angular v4.1.0-beta.0-294a205
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-f4f621a');
+var VERSION = new Version('4.1.0-beta.0-294a205');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
