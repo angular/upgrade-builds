98bc88bd154acc39a30740eb37fff34353df2bf4
Hans <hans@hansl.ca>
2017-04-10 22:18:53 +0000

    258d539 fix(language-service): detect when there isn't a tsconfig.json

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-09d9f5f
+ * @license Angular v4.1.0-beta.0-258d539
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-09d9f5f');
+var VERSION = new Version('4.1.0-beta.0-258d539');
 
 /**
  * @license
