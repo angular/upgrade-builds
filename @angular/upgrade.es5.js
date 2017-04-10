3da2496a8e8e0166afe62e6fbc18b3bb92712844
Hans <hans@hansl.ca>
2017-04-10 22:51:48 +0000

    83527fd test(router): test preloading when a module is already loaded

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-96aa236
+ * @license Angular v4.1.0-beta.0-83527fd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.0-96aa236');
+var VERSION = new Version('4.1.0-beta.0-83527fd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
