f1d1526fcb2086f237480bc6c3740cf70dd13e53
Hans <hans@hansl.ca>
2017-04-11 23:18:57 +0000

    04fb29b test(aio): add test for escaped newline in CodeComponent

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-3ced940
+ * @license Angular v4.1.0-beta.0-04fb29b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.0-3ced940');
+var VERSION = new Version('4.1.0-beta.0-04fb29b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-3ced940
+ * @license Angular v4.1.0-beta.0-04fb29b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-3ced940');
+var VERSION = new Version('4.1.0-beta.0-04fb29b');
 
 /**
  * @license
