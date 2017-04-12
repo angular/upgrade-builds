c85aa462d66c08efdcd0d14f616da6bba779969c
Igor Minar <iminar@google.com>
2017-04-12 19:44:10 +0000

    8659bd2 docs(aio): template -> ng-template (#15912)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-4a052cd
+ * @license Angular v4.1.0-beta.0-8659bd2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.0-4a052cd
+ * @license Angular v4.1.0-beta.0-8659bd2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -29,7 +29,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.0-4a052cd');
+var VERSION = new _angular_core.Version('4.1.0-beta.0-8659bd2');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
