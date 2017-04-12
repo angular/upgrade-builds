701af391f548b128f03b9a0083eaea0e2fb20889
Igor Minar <iminar@google.com>
2017-04-12 19:40:33 +0000

    28c2a71 docs(aio): revert file-trees back to original structure

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-540581d
+ * @license Angular v4.1.0-beta.0-28c2a71
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.0-540581d
+ * @license Angular v4.1.0-beta.0-28c2a71
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -29,7 +29,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.0-540581d');
+var VERSION = new _angular_core.Version('4.1.0-beta.0-28c2a71');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
