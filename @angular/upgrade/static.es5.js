c438dab9d28c98b061aa43d110e07674d94d49e8
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-12 13:48:58 +0000

    2798c1b feat(aio): code block styles with header

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-38d75d4
+ * @license Angular v4.1.0-beta.0-2798c1b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-38d75d4');
+var VERSION = new Version('4.1.0-beta.0-2798c1b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-38d75d4
+ * @license Angular v4.1.0-beta.0-2798c1b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.0-38d75d4
+ * @license Angular v4.1.0-beta.0-2798c1b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.0-38d75d4');
+var VERSION = new _angular_core.Version('4.1.0-beta.0-2798c1b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
