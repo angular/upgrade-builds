1acd2c05f9c00df0a53d0ca0e8caad7a883d94d9
Hans Larsen <hans@hansl.ca>
2017-04-12 23:13:27 +0000

    9cb5964 docs: add changelog for 4.1.0-beta.1

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-f09fd6e
+ * @license Angular v4.1.0-beta.1-9cb5964
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-f09fd6e');
+var VERSION = new Version('4.1.0-beta.1-9cb5964');
 
 /**
  * @license
