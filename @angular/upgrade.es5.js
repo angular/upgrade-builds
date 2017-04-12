f5d7e8782fe8cf81607925f03c36e1948b0c4c87
Hans <hans@hansl.ca>
2017-04-12 21:32:55 +0000

    f09fd6e test(aio): add sidenav tests and refactor related tests

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-909264f
+ * @license Angular v4.1.0-beta.0-f09fd6e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.0-909264f');
+var VERSION = new Version('4.1.0-beta.0-f09fd6e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
