592ee2857099df1feddb023b1d983844558ebc11
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-12 20:18:42 +0000

    0614754 docs(aio): final migration from old site

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-8f1359d
+ * @license Angular v4.1.0-beta.0-0614754
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-8f1359d');
+const VERSION = new Version('4.1.0-beta.0-0614754');
 
 /**
  * @license
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-8f1359d
+ * @license Angular v4.1.0-beta.0-0614754
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.0-8f1359d');
+var VERSION = new Version('4.1.0-beta.0-0614754');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
