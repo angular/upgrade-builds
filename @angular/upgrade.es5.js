d75d6c6099a427abada4a163295b2a8c97c80389
Tobias Bosch <tbosch1009@gmail.com>
2017-04-13 19:13:23 +0000

    fdb3f26 refactor(compiler): cleanup (#15960)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-14b7dfa
+ * @license Angular v4.1.0-beta.1-fdb3f26
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.1-14b7dfa');
+var VERSION = new Version('4.1.0-beta.1-fdb3f26');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
