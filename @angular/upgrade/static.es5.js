13293c8ee6bfb0b304a21e4667d46863010913e2
Tobias Bosch <tbosch1009@gmail.com>
2017-04-13 19:13:08 +0000

    14b7dfa fix(aio): create a proper commit link on preview comments (#15941)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-70b1d6d
+ * @license Angular v4.1.0-beta.1-14b7dfa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-70b1d6d');
+var VERSION = new Version('4.1.0-beta.1-14b7dfa');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
