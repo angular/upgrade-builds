090088944a0dbc4aeee1826226ed6deef4bb990a
GitHub <noreply@github.com>
2017-04-14 01:19:30 +0000

    2e47a0d Revert "fix(forms): Update types for TypeScript nullability support (#15859)" (#15977)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-6a2e08d
+ * @license Angular v4.1.0-beta.1-2e47a0d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-6a2e08d');
+var VERSION = new Version('4.1.0-beta.1-2e47a0d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
