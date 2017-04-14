2b324eb6e3ac7d1b667bba863eddc8649fe6dc21
Tobias Bosch <tbosch1009@gmail.com>
2017-04-14 16:18:50 +0000

    ce47d33 fix(compiler): ignore calls to unresolved symbols in metadata (#15970)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-2e47a0d
+ * @license Angular v4.1.0-beta.1-ce47d33
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.1-2e47a0d
+ * @license Angular v4.1.0-beta.1-ce47d33
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.1-2e47a0d');
+var VERSION = new _angular_core.Version('4.1.0-beta.1-ce47d33');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
