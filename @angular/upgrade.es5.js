b13f9ba2ed6152dc7e98c47574bd94ff97e349f1
Tobias Bosch <tbosch1009@gmail.com>
2017-04-14 22:27:16 +0000

    6649743 fix(forms): Update types for TypeScript nullability support

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-56c46d7
+ * @license Angular v4.1.0-beta.1-6649743
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.1-56c46d7');
+var VERSION = new Version('4.1.0-beta.1-6649743');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
