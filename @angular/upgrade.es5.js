8c02e9b4de9bb25f0993267d66f301504504055f
Tobias Bosch <tbosch1009@gmail.com>
2017-04-14 21:10:58 +0000

    c36ec9b fix(http): Update types for TypeScript nullability support

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-86396a4
+ * @license Angular v4.1.0-beta.1-c36ec9b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.1-86396a4');
+var VERSION = new Version('4.1.0-beta.1-c36ec9b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
