91c77fd2582e190749accfe532788ce815668560
Tobias Bosch <tbosch1009@gmail.com>
2017-04-14 21:47:21 +0000

    56c46d7 fix(router): Update types for TypeScript nullability support

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-c36ec9b
+ * @license Angular v4.1.0-beta.1-56c46d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-c36ec9b');
+var VERSION = new Version('4.1.0-beta.1-56c46d7');
 
 /**
  * @license
