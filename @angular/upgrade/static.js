0a119774679bb9d9190936d365fde12fb8643e90
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:55:08 +0000

    7d986ae feat(aio): smaller typography, code header pad

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-a2bdcc9
+ * @license Angular v4.1.0-beta.1-7d986ae
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-a2bdcc9');
+var VERSION = new Version('4.1.0-beta.1-7d986ae');
 
 /**
  * @license
--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-a2bdcc9
+ * @license Angular v4.1.0-beta.1-7d986ae
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.1-a2bdcc9
+ * @license Angular v4.1.0-beta.1-7d986ae
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.1-a2bdcc9');
+var VERSION = new _angular_core.Version('4.1.0-beta.1-7d986ae');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
