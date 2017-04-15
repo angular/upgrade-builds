f8a4d029578b044e3c57dd5a8ca2866c49ce31e5
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:47:00 +0000

    b57c960 feat(aio): pwa bg color and name

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-49dfc9f
+ * @license Angular v4.1.0-beta.1-b57c960
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-49dfc9f');
+var VERSION = new Version('4.1.0-beta.1-b57c960');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-49dfc9f
+ * @license Angular v4.1.0-beta.1-b57c960
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.1-49dfc9f
+ * @license Angular v4.1.0-beta.1-b57c960
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.1-49dfc9f');
+var VERSION = new _angular_core.Version('4.1.0-beta.1-b57c960');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
