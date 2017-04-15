f7a5222344c5ef06258463a067a623568e560593
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:39:57 +0000

    c0b1bbe feat(aio): add log rotation in preview server

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-8479e9e
+ * @license Angular v4.1.0-beta.1-c0b1bbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-8479e9e');
+var VERSION = new Version('4.1.0-beta.1-c0b1bbe');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
