660d08d4210244f2a34465f372a2f22f7bfc2f9b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:48:28 +0000

    624b5a5 feat(aio): filetree and top bar hover styles

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-b57c960
+ * @license Angular v4.1.0-beta.1-624b5a5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-b57c960');
+const VERSION = new Version('4.1.0-beta.1-624b5a5');
 
 /**
  * @license
