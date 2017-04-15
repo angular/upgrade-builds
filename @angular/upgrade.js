de5068ce7d70e6c3f22d452f85944cd2d4c853d4
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:36:16 +0000

    dfc81c3 feat(aio): sort search results by area/title

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-6649743
+ * @license Angular v4.1.0-beta.1-dfc81c3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-6649743');
+const VERSION = new Version('4.1.0-beta.1-dfc81c3');
 
 /**
  * @license
