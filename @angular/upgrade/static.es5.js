1eae87af0c1c43fe5bf6b20c8aa70802225f9bf2
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-16 07:28:55 +0000

    3f307ff style(aio): fix indentation on location service spec

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-7433da5
+ * @license Angular v4.1.0-beta.1-3f307ff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-7433da5');
+var VERSION = new Version('4.1.0-beta.1-3f307ff');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
