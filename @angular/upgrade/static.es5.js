d3dbf7b211726c4598e4b0488885cbd03af82e9f
Tobias Bosch <tbosch1009@gmail.com>
2017-04-17 17:35:15 +0000

    82417b3 fix(router): prevent `RouterLinkActive` from causing an infinite CD loop

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-5b141fb
+ * @license Angular v4.1.0-beta.1-82417b3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-5b141fb');
+var VERSION = new Version('4.1.0-beta.1-82417b3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-5b141fb
+ * @license Angular v4.1.0-beta.1-82417b3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-5b141fb');
+var VERSION = new Version('4.1.0-beta.1-82417b3');
 
 /**
  * @license
