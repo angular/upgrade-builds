2cb2e96476f1b73dd5b2e8f031a7fd3c70b47c9c
Tobias Bosch <tbosch1009@gmail.com>
2017-04-17 17:07:35 +0000

    2ddf3bc fix(language-service): look for type constructors on canonical symbol

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-0a3a9af
+ * @license Angular v4.1.0-beta.1-2ddf3bc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.1-0a3a9af
+ * @license Angular v4.1.0-beta.1-2ddf3bc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -29,7 +29,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.1-0a3a9af');
+var VERSION = new _angular_core.Version('4.1.0-beta.1-2ddf3bc');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
