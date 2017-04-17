3ee4301bb8f6f9646025c5cdb0ff9f8cdebbba9e
Tobias Bosch <tbosch1009@gmail.com>
2017-04-17 17:06:02 +0000

    0a3a9af fix(language-service): infer correct type of `?.` expressions

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-5a88d2f
+ * @license Angular v4.1.0-beta.1-0a3a9af
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.1-5a88d2f
+ * @license Angular v4.1.0-beta.1-0a3a9af
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.1-5a88d2f');
+var VERSION = new _angular_core.Version('4.1.0-beta.1-0a3a9af');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
