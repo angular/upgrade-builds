87ec4993908c5414708fdddbf45b33f95944dc09
Tobias Bosch <tbosch1009@gmail.com>
2017-04-17 17:37:25 +0000

    a77b126 fix(compiler): Inform user where Quoted error was thrown

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-82417b3
+ * @license Angular v4.1.0-beta.1-a77b126
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -512,7 +512,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-82417b3');
+var VERSION = new Version('4.1.0-beta.1-a77b126');
 
 /**
  * @license
