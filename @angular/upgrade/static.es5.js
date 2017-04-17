47fc47d16538fbc7e5fb499d6b088f308588bb81
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-17 13:03:09 +0000

    7165eb1 build(aio): add div with class around content

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-e20a722
+ * @license Angular v4.1.0-beta.1-7165eb1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -502,7 +502,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-e20a722');
+var VERSION = new Version('4.1.0-beta.1-7165eb1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
