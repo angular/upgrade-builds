b7ff4beb337746477ea83482cf77acd18da0e7f8
Tobias Bosch <tbosch1009@gmail.com>
2017-04-18 01:00:17 +0000

    7dfa0b9 fix(aio): about page

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-90814e4
+ * @license Angular v4.1.0-beta.1-7dfa0b9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -563,7 +563,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-90814e4');
+var VERSION = new Version('4.1.0-beta.1-7dfa0b9');
 
 /**
  * @license
--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-90814e4
+ * @license Angular v4.1.0-beta.1-7dfa0b9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.1-90814e4
+ * @license Angular v4.1.0-beta.1-7dfa0b9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -29,7 +29,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.1-90814e4');
+var VERSION = new _angular_core.Version('4.1.0-beta.1-7dfa0b9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
