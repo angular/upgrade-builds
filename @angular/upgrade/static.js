dab1a33d88bfecf42a0caf7fbc97eb62c045c36d
Tobias Bosch <tbosch1009@gmail.com>
2017-04-18 15:51:14 +0000

    bccfaa4 fix(core): benchmarks - enable ng1 benchmark again

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-7dfa0b9
+ * @license Angular v4.1.0-beta.1-bccfaa4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -563,7 +563,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-7dfa0b9');
+var VERSION = new Version('4.1.0-beta.1-bccfaa4');
 
 /**
  * @license
--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-7dfa0b9
+ * @license Angular v4.1.0-beta.1-bccfaa4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.1-7dfa0b9
+ * @license Angular v4.1.0-beta.1-bccfaa4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -562,7 +562,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.1-7dfa0b9');
+var VERSION = new _angular_core.Version('4.1.0-beta.1-bccfaa4');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
