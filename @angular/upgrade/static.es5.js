09111850b8af7fb3964cc4840331ce09336dcaab
Tobias Bosch <tbosch1009@gmail.com>
2017-04-18 22:38:16 +0000

    ecd0348 build(aio): extract the title from the content if necessary

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-5293794
+ * @license Angular v4.1.0-beta.1-ecd0348
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -552,7 +552,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-5293794');
+var VERSION = new Version('4.1.0-beta.1-ecd0348');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
