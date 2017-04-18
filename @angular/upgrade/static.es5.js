b72b2500d3e0061a631969868b8dd8b5c50876ac
Alex Eagle <alexeagle@google.com>
2017-04-18 22:53:06 +0000

    683e657 fix(compiler): suppress another closure warning

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-636858d
+ * @license Angular v4.1.0-beta.1-683e657
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -552,7 +552,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-636858d');
+var VERSION = new Version('4.1.0-beta.1-683e657');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
