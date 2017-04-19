394fcb4da20c8cd39f9033fc6cec41fc2d850506
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 11:19:14 +0000

    4624406 feat(aio): search results accessibility items

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-c9983e6
+ * @license Angular v4.1.0-beta.1-4624406
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -563,7 +563,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-c9983e6');
+var VERSION = new Version('4.1.0-beta.1-4624406');
 
 /**
  * @license
