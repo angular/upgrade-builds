3d1b3b5ee5efcd5f5d5c0e8ac00387a9abf0fd66
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 09:38:08 +0000

    c2fca3d fix(aio): api list container centered

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-8b31178
+ * @license Angular v4.1.0-beta.1-c2fca3d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -552,7 +552,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-8b31178');
+var VERSION = new Version('4.1.0-beta.1-c2fca3d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
