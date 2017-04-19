b15c5d8f5d83f171e6be775635161a60901457a4
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 11:13:21 +0000

    51098c4 fix(aio): allow empty titles for contents pages

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-50174d9
+ * @license Angular v4.1.0-beta.1-51098c4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.1-50174d9');
+var VERSION = new Version('4.1.0-beta.1-51098c4');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
