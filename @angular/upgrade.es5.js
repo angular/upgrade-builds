f27f74a8e309a2ae5613930e343d9c2c6f3ebd35
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 15:12:47 +0000

    e07b7ea docs(aio): fix broken regions

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-73c7503
+ * @license Angular v4.1.0-beta.1-e07b7ea
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.1-73c7503');
+var VERSION = new Version('4.1.0-beta.1-e07b7ea');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-73c7503
+ * @license Angular v4.1.0-beta.1-e07b7ea
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -563,7 +563,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-73c7503');
+var VERSION = new Version('4.1.0-beta.1-e07b7ea');
 
 /**
  * @license
