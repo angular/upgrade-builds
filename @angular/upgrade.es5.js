136e367e0ab65c94a625d293bc33792cee0f24b1
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 09:43:05 +0000

    93d8f8c docs(aio): add VS Code advice to readme.md

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-8fa4453
+ * @license Angular v4.1.0-beta.1-93d8f8c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.1-8fa4453');
+var VERSION = new Version('4.1.0-beta.1-93d8f8c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
