247862ddbe8f310f204928b86c628fd017bdb426
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 08:37:47 +0000

    e951612 fix(aio): set the pageId to the file-not-found URL if the doc is not available

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-b668c2c
+ * @license Angular v4.1.0-beta.1-e951612
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -552,7 +552,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-b668c2c');
+var VERSION = new Version('4.1.0-beta.1-e951612');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
