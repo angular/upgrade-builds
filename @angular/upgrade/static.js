0accfa456273992a25cb40c9dba34c688eefea24
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 10:01:11 +0000

    648de7d build(aio): render titles correctly in content pages

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-9d40ab9
+ * @license Angular v4.1.0-beta.1-648de7d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -563,7 +563,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-9d40ab9');
+var VERSION = new Version('4.1.0-beta.1-648de7d');
 
 /**
  * @license
--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-9d40ab9
+ * @license Angular v4.1.0-beta.1-648de7d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.1-9d40ab9
+ * @license Angular v4.1.0-beta.1-648de7d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -562,7 +562,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.1-9d40ab9');
+var VERSION = new _angular_core.Version('4.1.0-beta.1-648de7d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
