9aee4cb432395dea760275ec58905711b3edcebb
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 09:38:28 +0000

    8fa4453 docs(aio): Testing Guide: Second matcher parameter

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-c2fca3d
+ * @license Angular v4.1.0-beta.1-8fa4453
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.1-c2fca3d');
+var VERSION = new Version('4.1.0-beta.1-8fa4453');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
