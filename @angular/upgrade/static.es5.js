e348d8b4a801c66232091e2c994a499eb1e97cbb
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 11:14:56 +0000

    c9983e6 docs(aio): Fix reference to the right example

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-51098c4
+ * @license Angular v4.1.0-beta.1-c9983e6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -552,7 +552,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-51098c4');
+var VERSION = new Version('4.1.0-beta.1-c9983e6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
