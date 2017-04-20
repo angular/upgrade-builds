1a86313cfe67f0714b72884a08a7a1f6862d78e6
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-20 10:06:53 +0000

    800b1b0 fix(aio): autoscroll & add #top-of-page

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-ab03852
+ * @license Angular v4.1.0-beta.1-800b1b0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.1-ab03852
+ * @license Angular v4.1.0-beta.1-800b1b0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -562,7 +562,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.1-ab03852');
+var VERSION = new _angular_core.Version('4.1.0-beta.1-800b1b0');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
