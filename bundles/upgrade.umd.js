6610094a367512706e485536f8ea331426d71b6a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-20 09:55:45 +0000

    ab03852 fix(aio): accessibility items in search and home

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-f902581
+ * @license Angular v4.1.0-beta.1-ab03852
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.1-f902581
+ * @license Angular v4.1.0-beta.1-ab03852
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -29,7 +29,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.1-f902581');
+var VERSION = new _angular_core.Version('4.1.0-beta.1-ab03852');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
