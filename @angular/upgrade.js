ea816bae56d15b329e1c745655e16db347e686dd
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-20 15:24:49 +0000

    a54fe63 docs(aio): move file to the right folder

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-800b1b0
+ * @license Angular v4.1.0-beta.1-a54fe63
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-800b1b0');
+const VERSION = new Version('4.1.0-beta.1-a54fe63');
 
 /**
  * @license
