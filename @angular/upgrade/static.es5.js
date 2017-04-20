ac6f0b491517b7c94a025e20aa48941b4b0de923
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-20 15:24:54 +0000

    ccb6c45 docs(aio): rename http guide

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-a54fe63
+ * @license Angular v4.1.0-beta.1-ccb6c45
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -552,7 +552,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-beta.1-a54fe63');
+var VERSION = new Version('4.1.0-beta.1-ccb6c45');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-a54fe63
+ * @license Angular v4.1.0-beta.1-ccb6c45
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.1-a54fe63
+ * @license Angular v4.1.0-beta.1-ccb6c45
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -29,7 +29,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.1-a54fe63');
+var VERSION = new _angular_core.Version('4.1.0-beta.1-ccb6c45');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
