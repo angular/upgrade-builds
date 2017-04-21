7bed74f7867a6c6497a1be442d1dced571729e35
Miško Hevery <misko@hevery.com>
2017-04-21 20:56:52 +0000

    7a8bd99 docs(aio): correct some minor typos in the form validation cookbook (#16068)

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-7520ddc
+ * @license Angular v4.1.0-rc.0-7a8bd99
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-7520ddc');
+const VERSION = new Version('4.1.0-rc.0-7a8bd99');
 
 /**
  * @license
