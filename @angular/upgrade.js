c7fea107dee5cbd3634ec568f04d4ae4e5736cd6
Miško Hevery <misko@hevery.com>
2017-04-21 05:43:16 +0000

    d855e90 docs: add changelog for 4.1.0-rc.0

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-9730351
+ * @license Angular v4.1.0-rc.0-d855e90
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-9730351');
+const VERSION = new Version('4.1.0-rc.0-d855e90');
 
 /**
  * @license
