5a9e363530dea9fa879c1ee5f2416d506c211b60
Miško Hevery <misko@hevery.com>
2017-04-21 03:26:40 +0000

    cbe95f3 feat(aio): add code-of-conduct to footer #16105 (#16216)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-134f542
+ * @license Angular v4.1.0-beta.1-cbe95f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.1-134f542
+ * @license Angular v4.1.0-beta.1-cbe95f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -562,7 +562,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.1-134f542');
+var VERSION = new _angular_core.Version('4.1.0-beta.1-cbe95f3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
