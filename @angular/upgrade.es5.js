59a4b6936093e83d8e44754b6743f72b7f961326
Miško Hevery <misko@hevery.com>
2017-04-21 03:34:06 +0000

    9730351 build(aio): compute which examples to include when watching guide docs (#16213)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-5461182
+ * @license Angular v4.1.0-beta.1-9730351
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.1-5461182');
+var VERSION = new Version('4.1.0-beta.1-9730351');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
