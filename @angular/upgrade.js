ff623f976b9b7dd9725c18c9a0cf0a09f88876e1
Miško Hevery <misko@hevery.com>
2017-04-24 17:15:10 +0000

    feda017 docs(aio): Fix decorator JS feature explanation (#16214)

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-2991221
+ * @license Angular v4.1.0-rc.0-feda017
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-2991221');
+const VERSION = new Version('4.1.0-rc.0-feda017');
 
 /**
  * @license
