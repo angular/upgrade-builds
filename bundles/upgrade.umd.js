23b39c93898910e8975015b8eafae44928db09ac
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-24 13:38:04 +0000

    54e587a fix(aio): fix URL redirection for API pages

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-4ac5096
+ * @license Angular v4.1.0-rc.0-54e587a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v4.1.0-rc.0-4ac5096
+ * @license Angular v4.1.0-rc.0-54e587a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -29,7 +29,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.1.0-rc.0-4ac5096');
+var VERSION = new _angular_core.Version('4.1.0-rc.0-54e587a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
