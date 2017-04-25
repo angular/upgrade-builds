685252502ef32a10abf8ab9d938be7c718897505
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-25 13:29:15 +0000

    5331fc1 build(aio): turn on dangling link checking

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-9b7f2ce
+ * @license Angular v4.1.0-rc.0-5331fc1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -563,7 +563,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-rc.0-9b7f2ce');
+var VERSION = new Version('4.1.0-rc.0-5331fc1');
 
 /**
  * @license
