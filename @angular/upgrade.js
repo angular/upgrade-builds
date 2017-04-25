029298e16ca8388caf12934c1c93ac21439104b9
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-25 06:53:33 +0000

    1762047 build(aio): fix other dgeni templates

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-23e6502
+ * @license Angular v4.1.0-rc.0-1762047
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-23e6502');
+const VERSION = new Version('4.1.0-rc.0-1762047');
 
 /**
  * @license
