8b0f7634424f616daf5e5b4126ee155629fb054a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-25 06:55:13 +0000

    e7c37d7 feat(aio): copy code snackbar and design updates

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-1762047
+ * @license Angular v4.1.0-rc.0-e7c37d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -563,7 +563,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-rc.0-1762047');
+var VERSION = new Version('4.1.0-rc.0-e7c37d7');
 
 /**
  * @license
