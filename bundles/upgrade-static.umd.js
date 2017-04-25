c2b597e46d62a4162e3ec0f2d7abd69de92e1427
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-25 06:23:45 +0000

    ff0f53c build(aio): include missing packages in the docs

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-64ef69f
+ * @license Angular v4.1.0-rc.0-ff0f53c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.1.0-rc.0-64ef69f
+ * @license Angular v4.1.0-rc.0-ff0f53c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -562,7 +562,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.1.0-rc.0-64ef69f');
+var VERSION = new _angular_core.Version('4.1.0-rc.0-ff0f53c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
