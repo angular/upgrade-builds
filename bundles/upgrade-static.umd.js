75e988d4fea37ba47dbc525495e623a26511090a
GitHub <noreply@github.com>
2017-04-26 19:29:57 +0000

    a28f5ee docs: update 4.1.0 changelog, remove aio

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-c0c5013
+ * @license Angular v4.1.0-a28f5ee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.1.0-c0c5013
+ * @license Angular v4.1.0-a28f5ee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -562,7 +562,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.1.0-c0c5013');
+var VERSION = new _angular_core.Version('4.1.0-a28f5ee');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
