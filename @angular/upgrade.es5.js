e93b396b420c36237751c1f74c33ceb8c1d3487c
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-26 09:09:19 +0000

    35a2dfc fix(aio): resource nav

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-d8e2829
+ * @license Angular v4.1.0-rc.0-35a2dfc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-rc.0-d8e2829');
+var VERSION = new Version('4.1.0-rc.0-35a2dfc');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-d8e2829
+ * @license Angular v4.1.0-rc.0-35a2dfc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v4.1.0-rc.0-d8e2829
+ * @license Angular v4.1.0-rc.0-35a2dfc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -29,7 +29,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.1.0-rc.0-d8e2829');
+var VERSION = new _angular_core.Version('4.1.0-rc.0-35a2dfc');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
