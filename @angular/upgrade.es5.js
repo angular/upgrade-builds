c1aaf899a9349c5d7e4ccd336eb72b0160feabc9
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-26 08:42:15 +0000

    d8e2829 feat(aio): boilerplate:add cleans (removes) before adding

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-ac5e6ba
+ * @license Angular v4.1.0-rc.0-d8e2829
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-rc.0-ac5e6ba');
+var VERSION = new Version('4.1.0-rc.0-d8e2829');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-ac5e6ba
+ * @license Angular v4.1.0-rc.0-d8e2829
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.1.0-rc.0-ac5e6ba
+ * @license Angular v4.1.0-rc.0-d8e2829
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -562,7 +562,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.1.0-rc.0-ac5e6ba');
+var VERSION = new _angular_core.Version('4.1.0-rc.0-d8e2829');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
