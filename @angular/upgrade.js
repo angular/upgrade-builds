7e76e861bd1b41bd828724828e2c122b95efd8ef
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-26 13:52:49 +0000

    bb52e22 feat(aio): api pages styling

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-35a2dfc
+ * @license Angular v4.1.0-rc.0-bb52e22
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-35a2dfc');
+const VERSION = new Version('4.1.0-rc.0-bb52e22');
 
 /**
  * @license
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-35a2dfc
+ * @license Angular v4.1.0-rc.0-bb52e22
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -552,7 +552,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-rc.0-35a2dfc');
+var VERSION = new Version('4.1.0-rc.0-bb52e22');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
