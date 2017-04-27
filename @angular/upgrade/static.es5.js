4856d637ca816a80688bfb50d4803b71858b44d0
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-27 09:45:40 +0000

    2a7f636 build(aio): update @angular/service-worker to v1.0.0-beta.10

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-6bac4e6
+ * @license Angular v4.1.0-2a7f636
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -552,7 +552,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-6bac4e6');
+var VERSION = new Version('4.1.0-2a7f636');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
