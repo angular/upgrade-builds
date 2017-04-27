1fea04179ebf866dac1081973fa5f236636c5a74
Miško Hevery <misko@hevery.com>
2017-04-27 22:50:07 +0000

    3f46645 feat(aio): add Table of Contents (toc) component. (#16078)

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-2a7f636
+ * @license Angular v4.1.0-3f46645
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -563,7 +563,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-2a7f636');
+var VERSION = new Version('4.1.0-3f46645');
 
 /**
  * @license
