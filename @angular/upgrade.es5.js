c128db966e140416984e8b884eeab265728908d8
Miško Hevery <misko@hevery.com>
2017-04-28 17:07:22 +0000

    8c50457 fix: public API golden files (#16414)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-3f46645
+ * @license Angular v4.1.0-8c50457
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-3f46645');
+var VERSION = new Version('4.1.0-8c50457');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
