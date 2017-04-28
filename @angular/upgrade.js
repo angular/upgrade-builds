5952deab8f52752bb03ec8d8b0919ac8e86c0dd5
Miško Hevery <misko@hevery.com>
2017-04-28 22:52:40 +0000

    b70b960 docs(aio): apply guidelines to ts-to-js - adapt for aio site (#16054)

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-9384061
+ * @license Angular v4.1.0-b70b960
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-9384061');
+const VERSION = new Version('4.1.0-b70b960');
 
 /**
  * @license
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-9384061
+ * @license Angular v4.1.0-b70b960
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -552,7 +552,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-9384061');
+var VERSION = new Version('4.1.0-b70b960');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
