67dd96900d59971f4cebbe370187b7787379ccf1
Matias Niemelä <matias@yearofmoo.com>
2017-04-28 20:09:58 +0000

    a619991 feat(animations): introduce a wave of new animation features

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-ed4eaf3
+ * @license Angular v4.1.0-a619991
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -552,7 +552,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-ed4eaf3');
+var VERSION = new Version('4.1.0-a619991');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
