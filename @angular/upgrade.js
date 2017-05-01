aa21f66df36d387647c9a8872fb86820dc78bd85
Matias Niemelä <matias@yearofmoo.com>
2017-05-01 23:08:14 +0000

    c8fd904 fix(aio): remove max width on marketing pages and footer

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-aaa5628
+ * @license Angular v4.1.0-c8fd904
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-aaa5628');
+const VERSION = new Version('4.1.0-c8fd904');
 
 /**
  * @license
