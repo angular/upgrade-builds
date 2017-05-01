72e2e72c0be3373d07e575b4e5735b0a9b6d0d32
Matias Niemelä <matias@yearofmoo.com>
2017-05-01 23:07:55 +0000

    aaa5628 fix(aio): correct position of sidenav icons

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-65a8f7f
+ * @license Angular v4.1.0-aaa5628
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -563,7 +563,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-65a8f7f');
+var VERSION = new Version('4.1.0-aaa5628');
 
 /**
  * @license
