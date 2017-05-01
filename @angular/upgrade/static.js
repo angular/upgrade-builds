b16e1e5eda423697dd97f6eeb487a92adbf2cd62
Matias Niemelä <matias@yearofmoo.com>
2017-05-01 23:19:11 +0000

    ac220fc docs(aio): fix image size on homepage

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-d32bc5d
+ * @license Angular v4.1.0-ac220fc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -563,7 +563,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-d32bc5d');
+var VERSION = new Version('4.1.0-ac220fc');
 
 /**
  * @license
--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-d32bc5d
+ * @license Angular v4.1.0-ac220fc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v4.1.0-d32bc5d
+ * @license Angular v4.1.0-ac220fc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -29,7 +29,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.1.0-d32bc5d');
+var VERSION = new _angular_core.Version('4.1.0-ac220fc');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
