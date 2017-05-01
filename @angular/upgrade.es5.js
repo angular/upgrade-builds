56e422a827811a3d5fa2f6b842052112636d1769
Matias Niemelä <matias@yearofmoo.com>
2017-05-01 23:08:58 +0000

    ff32c53 docs(aio): fix lots of broken links

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-c8fd904
+ * @license Angular v4.1.0-ff32c53
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-c8fd904');
+var VERSION = new Version('4.1.0-ff32c53');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-c8fd904
+ * @license Angular v4.1.0-ff32c53
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -563,7 +563,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-c8fd904');
+var VERSION = new Version('4.1.0-ff32c53');
 
 /**
  * @license
