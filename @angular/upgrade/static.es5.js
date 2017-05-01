432ec6cc8e92be4e2ba262f9b47a1d51020220df
Matias Niemelä <matias@yearofmoo.com>
2017-05-01 23:14:36 +0000

    efaf502 feat(aio): tell user when no search results

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-235eb17
+ * @license Angular v4.1.0-efaf502
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -552,7 +552,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-235eb17');
+var VERSION = new Version('4.1.0-efaf502');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
