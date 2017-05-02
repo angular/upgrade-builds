9756b5e053785fdc65af04e32634be04cb0f540b
Matias Niemelä <matias@yearofmoo.com>
2017-05-02 22:32:16 +0000

    1c8772a fix(aio): delete guide/index.md which is no longer used

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-a0b9c23
+ * @license Angular v4.1.0-1c8772a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -552,7 +552,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-a0b9c23');
+var VERSION = new Version('4.1.0-1c8772a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
