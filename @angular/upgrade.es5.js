aa29df8786e9bb96c561fcf609a8f05fb08c3495
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 19:32:24 +0000

    9684d78 build: update `concurrently` to latest version

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-b9723f9
+ * @license Angular v4.1.0-9684d78
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-b9723f9');
+var VERSION = new Version('4.1.0-9684d78');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
