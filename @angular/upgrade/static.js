e365564e4cff92b084aed6cf65cbdafb19b959dd
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 21:04:00 +0000

    f29c6bb docs(aio): fix missing title warnings

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-464701a
+ * @license Angular v4.1.0-f29c6bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -563,7 +563,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-464701a');
+var VERSION = new Version('4.1.0-f29c6bb');
 
 /**
  * @license
--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-464701a
+ * @license Angular v4.1.0-f29c6bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.1.0-464701a
+ * @license Angular v4.1.0-f29c6bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -562,7 +562,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.1.0-464701a');
+var VERSION = new _angular_core.Version('4.1.0-f29c6bb');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
