72c3757d95a0f1376a9dca3f2548690e59fc47a2
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-03 08:39:35 +0000

    5ba8c14 docs(aio): remove unnecessary anchor links from styleguide

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-ea9d8a6
+ * @license Angular v4.1.0-5ba8c14
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -563,7 +563,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-ea9d8a6');
+var VERSION = new Version('4.1.0-5ba8c14');
 
 /**
  * @license
