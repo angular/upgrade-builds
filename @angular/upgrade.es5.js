41dbc9bfb749f3c108748504b5366e95d7285897
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 21:08:25 +0000

    55b8de9 build(aio): lint examples

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-eb56ab3
+ * @license Angular v4.1.0-55b8de9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-eb56ab3');
+var VERSION = new Version('4.1.0-55b8de9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
