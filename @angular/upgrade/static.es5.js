0f8fd5c0764cbe3dc70558399d8210d00ca1964f
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 20:59:07 +0000

    5b96fb9 build(aio): rename `src/content` to `src/generated`

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-1cfb263
+ * @license Angular v4.1.0-5b96fb9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -552,7 +552,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-1cfb263');
+var VERSION = new Version('4.1.0-5b96fb9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
