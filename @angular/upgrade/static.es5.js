1024498795af524f50c686be10dd383a4c131ffe
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-03 08:19:30 +0000

    ea9d8a6 docs(aio): move pipes guide under components

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-9650ff0
+ * @license Angular v4.1.0-ea9d8a6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -552,7 +552,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-9650ff0');
+var VERSION = new Version('4.1.0-ea9d8a6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
