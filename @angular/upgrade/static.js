62c7e96b3c197ad410d051e199e1c9ccace0350b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-03 07:35:47 +0000

    9650ff0 docs(aio): add hero “Zero” to toh-6

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-00dce16
+ * @license Angular v4.1.0-9650ff0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -563,7 +563,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-00dce16');
+var VERSION = new Version('4.1.0-9650ff0');
 
 /**
  * @license
