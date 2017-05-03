f4e20a3212be8686bc2825828f5fbb2b2bf3c4af
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 20:50:11 +0000

    9ca2b4c feat(aio): set “avoid” class by convention (filename has “.avoid.”)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-8760bf7
+ * @license Angular v4.1.0-9ca2b4c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-8760bf7');
+var VERSION = new Version('4.1.0-9ca2b4c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
