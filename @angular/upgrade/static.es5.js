8197757574584c6e519ab5ac8ec17de675dce63e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-03 07:23:19 +0000

    00dce16 docs(aio): grammatical fix. remove duplicate word 'our'

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-c946a92
+ * @license Angular v4.1.0-00dce16
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -552,7 +552,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-c946a92');
+var VERSION = new Version('4.1.0-00dce16');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
