62d22602449354b018f6b989efd318164a01d5d7
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 20:32:44 +0000

    1092292 test(compiler-cli): add test for missingTranslation parameter

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-8931e71
+ * @license Angular v4.1.0-1092292
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -584,7 +584,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-8931e71');
+var VERSION = new Version('4.1.0-1092292');
 
 /**
  * @license
