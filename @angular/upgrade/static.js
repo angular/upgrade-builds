6063237270b3380c540fc3bb81c5b99dce65a7d7
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 23:38:08 +0000

    04197ee feat: add .ngsummary.ts files to support AOT unit tests

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-1092292
+ * @license Angular v4.2.0-beta.0-04197ee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -584,7 +584,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-1092292');
+var VERSION = new Version('4.2.0-beta.0-04197ee');
 
 /**
  * @license
