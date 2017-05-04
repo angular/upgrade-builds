9412e97085030f041d4a5f074c771314e01e5a90
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 22:38:38 +0000

    cd28df6 docs: add changelog for 4.2.0-beta.0

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-1092292
+ * @license Angular v4.2.0-beta.0-cd28df6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -573,7 +573,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.0-1092292');
+var VERSION = new Version('4.2.0-beta.0-cd28df6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
