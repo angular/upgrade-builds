c6de38126f60db2910b432eda378a5a9a7632f5c
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 02:51:47 +0000

    919ff12 fix(aio): cheatsheet table layout fix

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-6748aea
+ * @license Angular v4.1.0-919ff12
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-6748aea');
+var VERSION = new Version('4.1.0-919ff12');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
