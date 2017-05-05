ecc138114e8cc20e195af5091dbb10b30f51f30a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-05 10:28:23 +0000

    d27588b feat(aio): api label styles

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-2714644
+ * @license Angular v4.2.0-beta.0-d27588b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.0-2714644');
+var VERSION = new Version('4.2.0-beta.0-d27588b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-2714644
+ * @license Angular v4.2.0-beta.0-d27588b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -584,7 +584,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.0-2714644');
+var VERSION = new Version('4.2.0-beta.0-d27588b');
 
 /**
  * @license
