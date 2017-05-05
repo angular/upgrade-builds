d1da55ce678dc03cca87ba630ed3434aef603b98
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-05 10:04:04 +0000

    b7caa3e fix(aio): do not route `eplnkr` URLs to / from ServiceWorker

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-3e33482
+ * @license Angular v4.2.0-beta.0-b7caa3e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.0-3e33482');
+var VERSION = new Version('4.2.0-beta.0-b7caa3e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
