fbf633a519f1c516c6d88021517f0b62dedb555e
Matias Niemelä <matias@yearofmoo.com>
2017-05-05 21:01:00 +0000

    518eb54 refactor(aio): tweak the CSS to account for new CSS classes on `aio-shell`

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-42dc2c1
+ * @license Angular v4.2.0-beta.0-518eb54
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -573,7 +573,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.0-42dc2c1');
+var VERSION = new Version('4.2.0-beta.0-518eb54');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
