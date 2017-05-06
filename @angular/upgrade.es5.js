6f04eba40e136ba529b9703ceaab45a6381438b5
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-06 13:50:15 +0000

    b9ed97c fix(aio): do not display a Toc initially

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-799be9c
+ * @license Angular v4.2.0-beta.0-b9ed97c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.0-799be9c');
+var VERSION = new Version('4.2.0-beta.0-b9ed97c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-799be9c
+ * @license Angular v4.2.0-beta.0-b9ed97c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -573,7 +573,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.0-799be9c');
+var VERSION = new Version('4.2.0-beta.0-b9ed97c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
