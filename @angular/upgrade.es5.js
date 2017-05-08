0f75bb4d112723224a68d031ed8d8eb08d71d5b8
Miško Hevery <misko@hevery.com>
2017-05-08 18:08:45 +0000

    c5ce040 docs(core): EventEmitter docs for isAsync defaults (#15780)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-98dd609
+ * @license Angular v4.2.0-beta.0-c5ce040
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.0-98dd609');
+var VERSION = new Version('4.2.0-beta.0-c5ce040');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
