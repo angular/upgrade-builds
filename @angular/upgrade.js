83210a150ceb3469061b363a180288e0beb71c0b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-10 07:09:45 +0000

    412ab3f fix(aio): basic fix to the TOC styling

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-954c08e
+ * @license Angular v4.2.0-beta.0-412ab3f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-954c08e');
+const VERSION = new Version('4.2.0-beta.0-412ab3f');
 
 /**
  * @license
