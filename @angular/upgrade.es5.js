c53aaea44c75e91c207f77f6eaeeb75d9ccc7001
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-10 23:52:51 +0000

    d28a3f7 docs(router): Change CanDeactivate to CanLoad (#16237)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-162dffb
+ * @license Angular v4.2.0-beta.1-d28a3f7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-162dffb');
+var VERSION = new Version('4.2.0-beta.1-d28a3f7');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
