96a80e69f552353f9486c048506eb45d5029badd
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-10 14:24:15 +0000

    f148ebe docs(aio): remove warning when testing in the webpack guide

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-f795f64
+ * @license Angular v4.2.0-beta.0-f148ebe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -584,7 +584,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.0-f795f64');
+var VERSION = new Version('4.2.0-beta.0-f148ebe');
 
 /**
  * @license
