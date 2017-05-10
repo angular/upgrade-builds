2c25fcd79483e21c3f953ebcc37d28a28b74cf9a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-10 07:07:33 +0000

    954c08e fix(aio): remove unwanted code styling from styleguide doc

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-a2dcb7b
+ * @license Angular v4.2.0-beta.0-954c08e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -573,7 +573,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.0-a2dcb7b');
+var VERSION = new Version('4.2.0-beta.0-954c08e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-a2dcb7b
+ * @license Angular v4.2.0-beta.0-954c08e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -584,7 +584,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.0-a2dcb7b');
+var VERSION = new Version('4.2.0-beta.0-954c08e');
 
 /**
  * @license
