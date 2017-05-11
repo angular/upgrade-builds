7b3edaf11e9bf85704bc65388a9d5efa536cccac
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-11 17:48:21 +0000

    5afaa39 refactor(compiler): produce more dense generated code (#16666)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-ce1d7c4
+ * @license Angular v4.2.0-beta.1-5afaa39
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -573,7 +573,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-ce1d7c4');
+var VERSION = new Version('4.2.0-beta.1-5afaa39');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
