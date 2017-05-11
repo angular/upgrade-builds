cc62e780de18b68efc471b51625eaa2ccbe86002
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-11 09:50:15 +0000

    c757e57 build(aio): serve gzipped content from the preview server

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-67dc970
+ * @license Angular v4.2.0-beta.1-c757e57
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-67dc970');
+const VERSION = new Version('4.2.0-beta.1-c757e57');
 
 /**
  * @license
