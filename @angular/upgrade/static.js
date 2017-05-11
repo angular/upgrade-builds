4b2351f20497a39b09de9be8356c59877bd42303
GitHub <noreply@github.com>
2017-05-11 20:36:06 +0000

    84c30be docs: add saved replies (#16726)

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-5afaa39
+ * @license Angular v4.2.0-beta.1-84c30be
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -584,7 +584,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-5afaa39');
+var VERSION = new Version('4.2.0-beta.1-84c30be');
 
 /**
  * @license
