cb41796332422e5860eb130f2147c0a3ce14e33d
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-12 21:07:48 +0000

    38c524d feat(core): introduce fixture.whenRenderingDone for testing (#16732)

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-0626464
+ * @license Angular v4.2.0-beta.1-38c524d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -584,7 +584,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-0626464');
+var VERSION = new Version('4.2.0-beta.1-38c524d');
 
 /**
  * @license
