5d0ba88c51c64fd05751626acc2878f2cdc89710
Igor Minar <iminar@google.com>
2017-05-12 07:05:47 +0000

    37f1fcb docs: remove angular.io from changelog (#16723)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-4a599ee
+ * @license Angular v4.2.0-beta.1-37f1fcb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-4a599ee');
+var VERSION = new Version('4.2.0-beta.1-37f1fcb');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-4a599ee
+ * @license Angular v4.2.0-beta.1-37f1fcb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -584,7 +584,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-4a599ee');
+var VERSION = new Version('4.2.0-beta.1-37f1fcb');
 
 /**
  * @license
