adbc781e6d1ac617b19ee9787cf0daed66d29656
Igor Minar <iminar@google.com>
2017-05-12 19:37:14 +0000

    0626464 build: use subshells when changing directories

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-21d213d
+ * @license Angular v4.2.0-beta.1-0626464
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-21d213d');
+const VERSION = new Version('4.2.0-beta.1-0626464');
 
 /**
  * @license
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-21d213d
+ * @license Angular v4.2.0-beta.1-0626464
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -573,7 +573,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-21d213d');
+var VERSION = new Version('4.2.0-beta.1-0626464');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
