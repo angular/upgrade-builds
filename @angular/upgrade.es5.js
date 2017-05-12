e1e0a4af5bd301e7cdd7304e4c38d78f80d60884
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-12 13:47:54 +0000

    b016984 docs(aio): resize images

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-90bc5b2
+ * @license Angular v4.2.0-beta.1-b016984
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-90bc5b2');
+var VERSION = new Version('4.2.0-beta.1-b016984');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
