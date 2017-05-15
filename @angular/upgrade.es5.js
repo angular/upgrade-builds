bef06323d427e1421b604f439497f574c2e11a51
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-15 20:31:18 +0000

    1eba623 fix(compiler-cli): import routing module with forRoot (#16438)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-ea2b24f
+ * @license Angular v4.2.0-beta.1-1eba623
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-ea2b24f');
+var VERSION = new Version('4.2.0-beta.1-1eba623');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-ea2b24f
+ * @license Angular v4.2.0-beta.1-1eba623
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -584,7 +584,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-ea2b24f');
+var VERSION = new Version('4.2.0-beta.1-1eba623');
 
 /**
  * @license
