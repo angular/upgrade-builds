b547d6b9bd72fe06c356bf8d30ab3c5e839fe877
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-15 20:30:25 +0000

    ea2b24f fix: add typescript 2.3.2 typings test (#16738)

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-a9d9aa1
+ * @license Angular v4.2.0-beta.1-ea2b24f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -584,7 +584,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-a9d9aa1');
+var VERSION = new Version('4.2.0-beta.1-ea2b24f');
 
 /**
  * @license
--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-a9d9aa1
+ * @license Angular v4.2.0-beta.1-ea2b24f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v4.2.0-beta.1-a9d9aa1
+ * @license Angular v4.2.0-beta.1-ea2b24f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -29,7 +29,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-beta.1-a9d9aa1');
+var VERSION = new _angular_core.Version('4.2.0-beta.1-ea2b24f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
