2076899a156bed6f12411bd590c67aa6456fefa5
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-15 20:36:59 +0000

    39b92f7 feat: introduce `TestBed.overrideProvider` (#16725)

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-1eba623
+ * @license Angular v4.2.0-beta.1-39b92f7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-1eba623');
+const VERSION = new Version('4.2.0-beta.1-39b92f7');
 
 /**
  * @license
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-1eba623
+ * @license Angular v4.2.0-beta.1-39b92f7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -584,7 +584,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-1eba623');
+var VERSION = new Version('4.2.0-beta.1-39b92f7');
 
 /**
  * @license
