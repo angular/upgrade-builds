84becb25d72d4fc3e0fcbbcea87f9f25333b6f19
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-16 09:22:34 +0000

    a68ad6d feat(aio): sidenav headers should be focusable buttons

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-7ae0440
+ * @license Angular v4.2.0-beta.1-a68ad6d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -584,7 +584,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-7ae0440');
+var VERSION = new Version('4.2.0-beta.1-a68ad6d');
 
 /**
  * @license
