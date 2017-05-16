ce6c0f7ac54faa04e64b669af5bbf587cf42bd1b
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-16 22:33:15 +0000

    c805082 fix(platform-server): wait for async app initializers to complete before removing server side styles (#16712)

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-9a7f5d5
+ * @license Angular v4.2.0-beta.1-c805082
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -585,7 +585,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-9a7f5d5');
+var VERSION = new Version('4.2.0-beta.1-c805082');
 
 /**
  * @license
