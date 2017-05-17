84bd6ff28e55713ffd74c53feea4db31aa0d0085
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-17 16:00:43 +0000

    6416e79 build: do not create the bundles when updating the public API

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-238c523
+ * @license Angular v4.2.0-beta.1-6416e79
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -574,7 +574,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-238c523');
+var VERSION = new Version('4.2.0-beta.1-6416e79');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
