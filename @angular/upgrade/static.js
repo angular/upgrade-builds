d15f1e46477665aef583b5e2b0500307f4a97a02
Igor Minar <iminar@google.com>
2017-05-17 04:24:22 +0000

    593fe5e build(aio): enable HTTP/2 on the preview server (#16826)

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-54a6e4f
+ * @license Angular v4.2.0-beta.1-593fe5e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -585,7 +585,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-54a6e4f');
+var VERSION = new Version('4.2.0-beta.1-593fe5e');
 
 /**
  * @license
