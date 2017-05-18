45e74a4329e3d5474964e35b73aeeca46dcfb56c
Matias Niemelä <matias@yearofmoo.com>
2017-05-18 19:15:08 +0000

    86b7bd9 revert: refactor(router): cleanup, simplification

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-6531806
+ * @license Angular v4.2.0-beta.1-86b7bd9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -574,7 +574,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-6531806');
+var VERSION = new Version('4.2.0-beta.1-86b7bd9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
