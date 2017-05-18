b5c34311ced4b33f2efaf0dc25eb65f08869007b
Jason Aden <jasonaden@google.com>
2017-05-18 00:47:43 +0000

    7edc5e9 docs: add changelog for 4.1.3

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.2-6f039d7
+ * @license Angular v4.1.3-7edc5e9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.2-6f039d7');
+const VERSION = new Version('4.1.3-7edc5e9');
 
 /**
  * @license
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.2-6f039d7
+ * @license Angular v4.1.3-7edc5e9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -584,7 +584,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.1.2-6f039d7');
+var VERSION = new Version('4.1.3-7edc5e9');
 
 /**
  * @license
