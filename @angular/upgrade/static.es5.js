a21b5f3ea9fd5a21cee01994d85f0b1a205b33fd
Matias Niemelä <matias@yearofmoo.com>
2017-05-18 02:48:44 +0000

    198edb3 refactor(router): don't run the change detection every time an outlet is activated

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-712630c
+ * @license Angular v4.2.0-beta.1-198edb3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -574,7 +574,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-712630c');
+var VERSION = new Version('4.2.0-beta.1-198edb3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
