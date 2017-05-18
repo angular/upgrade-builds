e4a6e13dd190087da1255a3510685f1567714be3
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-18 14:03:43 +0000

    3361a7b build(aio): minor `update-preview-server.sh` improvements

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-9466908
+ * @license Angular v4.2.0-beta.1-3361a7b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -574,7 +574,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-9466908');
+var VERSION = new Version('4.2.0-beta.1-3361a7b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
