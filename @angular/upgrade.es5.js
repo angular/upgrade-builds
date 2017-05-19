e897724edbfacd89469421d50b6f51411a6acb28
Matias Niemelä <matias@yearofmoo.com>
2017-05-19 20:12:33 +0000

    b10029c docs(animations): add documentation for new animation features

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-5d4f543
+ * @license Angular v4.2.0-beta.1-b10029c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-5d4f543');
+var VERSION = new Version('4.2.0-beta.1-b10029c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-5d4f543
+ * @license Angular v4.2.0-beta.1-b10029c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -574,7 +574,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-5d4f543');
+var VERSION = new Version('4.2.0-beta.1-b10029c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
