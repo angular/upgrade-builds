1935747ec45caeb7a75a76486c124c7d2aef31a1
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-24 07:40:11 +0000

    9e17a14 fix(aio): align top-level nav-items consistently

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-eba59aa
+ * @license Angular v4.2.0-rc.0-9e17a14
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.0-eba59aa');
+const VERSION = new Version('4.2.0-rc.0-9e17a14');
 
 /**
  * @license
