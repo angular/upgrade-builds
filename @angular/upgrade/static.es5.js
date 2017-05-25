77d9fa91aae2ec29741766cb854354356450a17e
Chuck Jazdzewski <chuckj@google.com>
2017-05-25 17:19:36 +0000

    573b861 fix(compiler): compile `.ngfactory.ts` files even if nobody references them. (#16899)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-966eb2f
+ * @license Angular v4.2.0-rc.0-573b861
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -574,7 +574,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.0-966eb2f');
+var VERSION = new Version('4.2.0-rc.0-573b861');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
