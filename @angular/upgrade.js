b7006267d44b9699813dd67a067be3b0b3acebb9
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 23:19:10 +0000

    0018acf fix(aio): remove es2015 syntax from the onerror webmaster tools debugger (#17056)

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-51d7a65
+ * @license Angular v4.2.0-rc.1-0018acf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.1-51d7a65');
+const VERSION = new Version('4.2.0-rc.1-0018acf');
 
 /**
  * @license
