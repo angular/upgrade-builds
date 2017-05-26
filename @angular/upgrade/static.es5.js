0963f8dc547fef4b7ef880a2e049fed87b3aaf08
Igor Minar <igor@angularjs.org>
2017-05-26 08:46:10 +0000

    9e7e178 build(aio): add google webmaster tools verification file

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-db3113b
+ * @license Angular v4.2.0-rc.0-9e7e178
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -574,7 +574,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.0-db3113b');
+var VERSION = new Version('4.2.0-rc.0-9e7e178');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
