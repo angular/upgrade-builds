e5ac154e99a23ddaadcd01d2091a1432f111f826
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 00:01:25 +0000

    4c5e28e build(aio): less verbose boilerplate generator (#16970)

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-b37a048
+ * @license Angular v4.2.0-rc.0-4c5e28e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.0-b37a048');
+const VERSION = new Version('4.2.0-rc.0-4c5e28e');
 
 /**
  * @license
