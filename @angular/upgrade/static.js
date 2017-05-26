39fd23d34a853f46efd48562c9543f87257f7245
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 01:11:46 +0000

    3ab86bd fix(animations): use a lightweight renderer for non-animation components (#17003)

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-2538094
+ * @license Angular v4.2.0-rc.0-3ab86bd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -585,7 +585,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.0-2538094');
+var VERSION = new Version('4.2.0-rc.0-3ab86bd');
 
 /**
  * @license
