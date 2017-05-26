86b809aeb50f3ba6ace37a58c4aff287b4bc6c33
Igor Minar <iminar@google.com>
2017-05-26 08:38:33 +0000

    db3113b build(aio): externalize @angular/cli patches into a js file

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-0c691af
+ * @license Angular v4.2.0-rc.0-db3113b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -585,7 +585,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.0-0c691af');
+var VERSION = new Version('4.2.0-rc.0-db3113b');
 
 /**
  * @license
