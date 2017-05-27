1bc756d91a964f2b6ce640c9a59d85cb9dab2863
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-27 10:37:13 +0000

    b8b91d3 feat(aio): marketing design refresh

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-0018acf
+ * @license Angular v4.2.0-rc.1-b8b91d3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -574,7 +574,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-0018acf');
+var VERSION = new Version('4.2.0-rc.1-b8b91d3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
