9fcb5c41453f8cf60e1ff752dec3d585cc9ffbda
Victor Berchet <victor@suumit.com>
2017-05-30 22:33:55 +0000

    0d894a1 fix(aio): fix ToC styling (#17110)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-e513808
+ * @license Angular v4.2.0-rc.1-0d894a1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.1-e513808');
+var VERSION = new Version('4.2.0-rc.1-0d894a1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-e513808
+ * @license Angular v4.2.0-rc.1-0d894a1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -585,7 +585,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-e513808');
+var VERSION = new Version('4.2.0-rc.1-0d894a1');
 
 /**
  * @license
