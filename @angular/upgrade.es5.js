5ec46d19e594234a0b5c04ff65a74c8413c31d76
Victor Berchet <victor@suumit.com>
2017-05-30 18:05:59 +0000

    535d9da fix(common): always use 'other' case for locales with no plural rules (#16990)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-18bf772
+ * @license Angular v4.2.0-rc.1-535d9da
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.1-18bf772');
+var VERSION = new Version('4.2.0-rc.1-535d9da');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-18bf772
+ * @license Angular v4.2.0-rc.1-535d9da
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -574,7 +574,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-18bf772');
+var VERSION = new Version('4.2.0-rc.1-535d9da');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
