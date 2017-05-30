9184dc07ee9eca7420438685967a8dc9c5821843
Victor Berchet <victor@suumit.com>
2017-05-30 22:34:40 +0000

    2f35392 fix(aio): make the search-pane larger (and improve the search-box) (#17105)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-b056adc
+ * @license Angular v4.2.0-rc.1-2f35392
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -574,7 +574,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-b056adc');
+var VERSION = new Version('4.2.0-rc.1-2f35392');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
