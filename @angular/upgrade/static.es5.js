a94012d293a16d4255ea6721f4be764961df72bd
Victor Berchet <victor@suumit.com>
2017-05-30 18:08:58 +0000

    ea8a43d feat(tsc-wrapped): always convert shorthand imports (#16898)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-535d9da
+ * @license Angular v4.2.0-rc.1-ea8a43d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -574,7 +574,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-535d9da');
+var VERSION = new Version('4.2.0-rc.1-ea8a43d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
