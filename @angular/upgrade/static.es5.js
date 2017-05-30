fa5b8c4c8d5a99dacf853259388790dd57f15733
Victor Berchet <victor@suumit.com>
2017-05-30 18:00:14 +0000

    160221c test(platform-server): fix target of TS outDir for integration test (#17118)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-6220b49
+ * @license Angular v4.2.0-rc.1-160221c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -574,7 +574,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-6220b49');
+var VERSION = new Version('4.2.0-rc.1-160221c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
