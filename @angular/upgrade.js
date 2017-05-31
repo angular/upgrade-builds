832aea18e8bd257397f0867e1d379ef43aeb889e
Victor Berchet <victor@suumit.com>
2017-05-31 22:00:17 +0000

    c20f60b fix(router): make remove trailing slash consistent with URL params

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-1408357
+ * @license Angular v4.2.0-rc.1-c20f60b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.1-1408357');
+const VERSION = new Version('4.2.0-rc.1-c20f60b');
 
 /**
  * @license
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-1408357
+ * @license Angular v4.2.0-rc.1-c20f60b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -574,7 +574,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-1408357');
+var VERSION = new Version('4.2.0-rc.1-c20f60b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
