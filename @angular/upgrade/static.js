1bafe3a0e684cd4a295dd82bd0fce5e8e5f1decd
GitHub <noreply@github.com>
2017-05-31 21:54:31 +0000

    1408357 docs: add details about the WeakMap polyfill for 4.x in the changelog (#17146)

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-1f9a3dd
+ * @license Angular v4.2.0-rc.1-1408357
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -585,7 +585,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-1f9a3dd');
+var VERSION = new Version('4.2.0-rc.1-1408357');
 
 /**
  * @license
