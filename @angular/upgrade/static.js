16b14cc5d7207f026e0bd1fa948f06b8aa81b8b7
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-31 06:02:49 +0000

    dfbbbb5 build(aio): the the captured h1 as the title for the search index

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-2f35392
+ * @license Angular v4.2.0-rc.1-dfbbbb5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -585,7 +585,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-2f35392');
+var VERSION = new Version('4.2.0-rc.1-dfbbbb5');
 
 /**
  * @license
