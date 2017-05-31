7677b268d7381f12e4e19183be72c389e89204b5
GitHub <noreply@github.com>
2017-05-31 11:05:58 +0000

    1f9a3dd fix(aio): fix typo in web worker check (#17133)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-11505fa
+ * @license Angular v4.2.0-rc.1-1f9a3dd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -574,7 +574,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-11505fa');
+var VERSION = new Version('4.2.0-rc.1-1f9a3dd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
