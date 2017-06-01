892464000d0bb4237c02201266623fe1752afd61
GitHub <noreply@github.com>
2017-06-01 20:44:20 +0000

    598fdad docs: update the release schedule (#17162)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-ad6a57e
+ * @license Angular v4.2.0-rc.1-598fdad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.1-ad6a57e');
+var VERSION = new Version('4.2.0-rc.1-598fdad');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-ad6a57e
+ * @license Angular v4.2.0-rc.1-598fdad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -574,7 +574,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-ad6a57e');
+var VERSION = new Version('4.2.0-rc.1-598fdad');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
