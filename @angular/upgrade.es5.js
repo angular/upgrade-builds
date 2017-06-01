9907288732ee944123f6cefae48c12551abc3e0a
Victor Berchet <victor@suumit.com>
2017-06-01 22:20:02 +0000

    82ec02d docs: add changelog for 4.2.0-rc.2

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-7822187
+ * @license Angular v4.2.0-rc.2-82ec02d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.1-7822187');
+var VERSION = new Version('4.2.0-rc.2-82ec02d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
