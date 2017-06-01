cbcf72c15d6884e46f9d00b1270ae7ada08bd761
Victor Berchet <victor@suumit.com>
2017-06-01 21:24:19 +0000

    7822187 fix(aio): fix scrolling to top (#17102)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-068133e
+ * @license Angular v4.2.0-rc.1-7822187
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -574,7 +574,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-068133e');
+var VERSION = new Version('4.2.0-rc.1-7822187');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
