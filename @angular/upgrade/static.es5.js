3fb7eec355fc8d8bdb82c1aa0e3d3ea1cc2945d3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-02 21:50:45 +0000

    35f714e fix(aio): place progress bar at the top

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-4c32cb9
+ * @license Angular v4.2.0-rc.2-35f714e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -574,7 +574,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-4c32cb9');
+var VERSION = new Version('4.2.0-rc.2-35f714e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
