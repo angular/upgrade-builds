6fb74c0c0e2612d67e1aa29d918389180a8d7d22
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-05 18:36:04 +0000

    02d74ca ci: update pullapprove to reflect animation ownership

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-734f30d
+ * @license Angular v4.2.0-rc.2-02d74ca
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -574,7 +574,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-734f30d');
+var VERSION = new Version('4.2.0-rc.2-02d74ca');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
