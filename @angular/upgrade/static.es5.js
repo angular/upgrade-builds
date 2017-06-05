d031fe847a83087cdbb03cdc9ba91ed6553bc744
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-05 20:02:22 +0000

    7c78282 docs: fix typo in 'Routes' docs

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-47c2a2e
+ * @license Angular v4.2.0-rc.2-7c78282
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -574,7 +574,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-47c2a2e');
+var VERSION = new Version('4.2.0-rc.2-7c78282');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
