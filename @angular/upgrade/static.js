b0843627cc84f7bb16c3840bb0dc78ed5167349f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-05 18:38:34 +0000

    47c2a2e ci: switch CircleCI to 2.0

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-7c78282
+ * @license Angular v4.2.0-rc.2-47c2a2e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -585,7 +585,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-7c78282');
+var VERSION = new Version('4.2.0-rc.2-47c2a2e');
 
 /**
  * @license
