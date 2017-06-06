79c6c55bdf759b7e0d9b999594814a40f0593967
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-06 21:39:06 +0000

    b00b80a feat(compiler-cli): introduce synchronous codegen API

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-269bbe0
+ * @license Angular v4.2.0-rc.2-b00b80a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.2-269bbe0');
+var VERSION = new Version('4.2.0-rc.2-b00b80a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-269bbe0
+ * @license Angular v4.2.0-rc.2-b00b80a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -586,7 +586,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-269bbe0');
+var VERSION = new Version('4.2.0-rc.2-b00b80a');
 
 /**
  * @license
