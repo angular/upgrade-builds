caa01df9dddd2f1190f87fd42f7f5ffc7cbe02b9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-07 19:00:07 +0000

    bb46f54 refactor(aio): use the SelectMenuComponent for all select menus

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-a39f7d6
+ * @license Angular v4.2.0-rc.2-bb46f54
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.2.0-rc.2-a39f7d6
+ * @license Angular v4.2.0-rc.2-bb46f54
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -585,7 +585,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.2-a39f7d6');
+var VERSION = new _angular_core.Version('4.2.0-rc.2-bb46f54');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
