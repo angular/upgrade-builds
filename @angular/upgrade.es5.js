a5be108682e6f1c1872397b86092c5f395811218
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-07 19:58:35 +0000

    cba2b3c docs(aio): add “nougat” to compat chart

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-bb46f54
+ * @license Angular v4.2.0-rc.2-cba2b3c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.2-bb46f54');
+var VERSION = new Version('4.2.0-rc.2-cba2b3c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
