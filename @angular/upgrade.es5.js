9f755bc247897479554e2792846329152138f8c6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-08 21:18:10 +0000

    41a765d fix(aio): re-color code pretty print

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-f370fd3
+ * @license Angular v4.2.0-rc.2-41a765d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.2-f370fd3');
+var VERSION = new Version('4.2.0-rc.2-41a765d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-f370fd3
+ * @license Angular v4.2.0-rc.2-41a765d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-f370fd3');
+var VERSION = new Version('4.2.0-rc.2-41a765d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
