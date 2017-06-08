e2224081d899cc5ed7514b12d972445eff1b9cbc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-08 21:18:46 +0000

    a222c3e fix(forms): fix min and max validator behavior on non-numbers

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-41a765d
+ * @license Angular v4.2.0-rc.2-a222c3e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.2-41a765d');
+var VERSION = new Version('4.2.0-rc.2-a222c3e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-41a765d
+ * @license Angular v4.2.0-rc.2-a222c3e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-41a765d');
+var VERSION = new Version('4.2.0-rc.2-a222c3e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
