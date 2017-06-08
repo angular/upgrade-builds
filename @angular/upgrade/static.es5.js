9829d9a7c5e6681cb98c1e83a070611c67b79e1a
Igor Minar <iminar@google.com>
2017-06-08 19:47:21 +0000

    75a311f fix(aio): style and fix hover color for features cta button (#17342)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-4d2ee51
+ * @license Angular v4.2.0-rc.2-75a311f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-4d2ee51');
+var VERSION = new Version('4.2.0-rc.2-75a311f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
