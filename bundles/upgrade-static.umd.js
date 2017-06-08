1e52b9cfe3eae9ab7e11ac792fbfa3526fe9183b
Igor Minar <iminar@google.com>
2017-06-08 19:48:21 +0000

    391ed63 fix(aio): copy code button overlap fix on mobile (#17302)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-75a311f
+ * @license Angular v4.2.0-rc.2-391ed63
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.2.0-rc.2-75a311f
+ * @license Angular v4.2.0-rc.2-391ed63
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -585,7 +585,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.2-75a311f');
+var VERSION = new _angular_core.Version('4.2.0-rc.2-391ed63');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
