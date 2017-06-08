2a63b36b860fffebc916ccce5a7cca02b3ee2ed5
GitHub <noreply@github.com>
2017-06-08 22:47:51 +0000

    576d6d8 feat(aio): update the home page and docs landing page content (#17325)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-a222c3e
+ * @license Angular v4.2.0-rc.2-576d6d8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-a222c3e');
+var VERSION = new Version('4.2.0-rc.2-576d6d8');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
