f736f1f2e7f626a1f840bbf6becef6d769479413
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-08 21:17:26 +0000

    f370fd3 docs: "TypeScript to JavaScript" code tab renamed “ES6 + Decorators”

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-6a46cab
+ * @license Angular v4.2.0-rc.2-f370fd3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.2-6a46cab');
+var VERSION = new Version('4.2.0-rc.2-f370fd3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-6a46cab
+ * @license Angular v4.2.0-rc.2-f370fd3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -586,7 +586,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-6a46cab');
+var VERSION = new Version('4.2.0-rc.2-f370fd3');
 
 /**
  * @license
