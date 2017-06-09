f8e97b01954189666fac1a64bb27a338ef0d2a8e
Igor Minar <iminar@google.com>
2017-06-09 21:43:46 +0000

    65d49d5 fix(aio): temporarily remove link to source from the API pages (#17371)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-33df13a
+ * @license Angular v4.2.0-65d49d5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-33df13a');
+var VERSION = new Version('4.2.0-65d49d5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-33df13a
+ * @license Angular v4.2.0-65d49d5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-33df13a');
+var VERSION = new Version('4.2.0-65d49d5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
