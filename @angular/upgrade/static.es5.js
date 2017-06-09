e0f5158b2cbde823e2e8bf8f3eabd33c7b82728c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-09 23:41:20 +0000

    15090a8 docs: add changelog for 4.2.1

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-90b0713
+ * @license Angular v4.2.1-15090a8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-90b0713');
+var VERSION = new Version('4.2.1-15090a8');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
