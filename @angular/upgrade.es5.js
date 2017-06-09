e0fbca6037d62fa8b8f20446bd818e2bde25564c
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-09 13:56:19 +0000

    a0b30e5 docs(aio): remove example specific `.gitignore` files

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-e894f5c
+ * @license Angular v4.2.0-a0b30e5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-e894f5c');
+var VERSION = new Version('4.2.0-a0b30e5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-e894f5c
+ * @license Angular v4.2.0-a0b30e5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -586,7 +586,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-e894f5c');
+var VERSION = new Version('4.2.0-a0b30e5');
 
 /**
  * @license
