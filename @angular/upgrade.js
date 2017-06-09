f9f5bef76216fc3007db9c31a3a685c0062c1490
Igor Minar <iminar@google.com>
2017-06-09 20:51:07 +0000

    503fd1f docs(aio): fix image width on the homepage

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-ce18c68
+ * @license Angular v4.2.0-503fd1f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-ce18c68');
+const VERSION = new Version('4.2.0-503fd1f');
 
 /**
  * @license
