bf1d6c214df0c8cf33ade260ca9ba5c1b51658b6
Igor Minar <iminar@google.com>
2017-06-10 01:57:54 +0000

    df7774c docs(aio): restore animation original value (#17385)

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-c8c0cee
+ * @license Angular v4.2.1-df7774c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.1-c8c0cee');
+const VERSION = new Version('4.2.1-df7774c');
 
 /**
  * @license
--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-c8c0cee
+ * @license Angular v4.2.1-df7774c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.2.1-c8c0cee
+ * @license Angular v4.2.1-df7774c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -585,7 +585,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.2.1-c8c0cee');
+var VERSION = new _angular_core.Version('4.2.1-df7774c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
