cfc33d99246ecb51b27863407aa42e753b56de2d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-11 18:06:26 +0000

    d961911 docs(aio): fix broken example in upgrade guide

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-e543272
+ * @license Angular v4.2.1-d961911
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.1-e543272');
+var VERSION = new Version('4.2.1-d961911');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
