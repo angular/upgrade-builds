42749f02dc19ca59dccc3b069c38e74caa4bdecf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-12 22:45:30 +0000

    ed73d4f refactor(compiler): don’t always compile `.ngfactory.ts` files by default

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-451257a
+ * @license Angular v4.2.1-ed73d4f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.1-451257a');
+var VERSION = new Version('4.2.1-ed73d4f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-451257a
+ * @license Angular v4.2.1-ed73d4f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.1-451257a');
+const VERSION = new Version('4.2.1-ed73d4f');
 
 /**
  * @license
