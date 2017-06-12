1bbb2dac0e7eefd945f00fb6fb8b9545ebfc9959
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-12 19:40:44 +0000

    451257a fix(animations): do not treat a `0` animation state as `void`

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-185075d
+ * @license Angular v4.2.1-451257a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.1-185075d');
+var VERSION = new Version('4.2.1-451257a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
