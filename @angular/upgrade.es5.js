d63abbe3faaebaf57802bce441a7b98dfcce75d8
Igor Minar <igor@angularjs.org>
2017-06-12 23:50:34 +0000

    5e79449 build(aio): make deploy-to-firebase.sh executable

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-0268818
+ * @license Angular v4.2.2-5e79449
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.1-0268818');
+var VERSION = new Version('4.2.2-5e79449');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-0268818
+ * @license Angular v4.2.2-5e79449
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.1-0268818');
+var VERSION = new Version('4.2.2-5e79449');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
