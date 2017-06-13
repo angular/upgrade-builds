f25515b23a207ea0e37e9a52cb8a07aa804b16c7
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-13 11:31:47 +0000

    12452d4 build(aio): increase docs integration test timeouts

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-709a3f6
+ * @license Angular v4.2.2-12452d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-709a3f6');
+var VERSION = new Version('4.2.2-12452d4');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-709a3f6
+ * @license Angular v4.2.2-12452d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -586,7 +586,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-709a3f6');
+var VERSION = new Version('4.2.2-12452d4');
 
 /**
  * @license
