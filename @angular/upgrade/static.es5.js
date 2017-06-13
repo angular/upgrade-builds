1307c3235413d0b6b47664cbd9c6cf4dff1e27d3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-13 23:14:03 +0000

    0c07f8c test(platform-server): fix and re-enable integration tests

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-d8d21c7
+ * @license Angular v4.2.2-0c07f8c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-d8d21c7');
+var VERSION = new Version('4.2.2-0c07f8c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
