00352fd6bc3841fe2039dc3bb19385f7386d6909
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-13 13:15:55 +0000

    052331f build(aio): add staging environment

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-541c9a9
+ * @license Angular v4.2.2-052331f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-541c9a9');
+const VERSION = new Version('4.2.2-052331f');
 
 /**
  * @license
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-541c9a9
+ * @license Angular v4.2.2-052331f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -586,7 +586,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-541c9a9');
+var VERSION = new Version('4.2.2-052331f');
 
 /**
  * @license
