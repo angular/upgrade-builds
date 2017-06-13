5164cec1889d8b46c53687b7e7f977797a8ff0e2
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-13 10:51:48 +0000

    7caa0a8 fix(aio): show search results when search box gets focus

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-fc774a1
+ * @license Angular v4.2.2-7caa0a8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -586,7 +586,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-fc774a1');
+var VERSION = new Version('4.2.2-7caa0a8');
 
 /**
  * @license
