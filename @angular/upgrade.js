83b2565723523f48573d27e0d1c05839c4547149
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-13 19:03:03 +0000

    4164369 docs(aio): update typescript for `examples/webpack` to same as cli

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-dc084a5
+ * @license Angular v4.2.2-4164369
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-dc084a5');
+const VERSION = new Version('4.2.2-4164369');
 
 /**
  * @license
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-dc084a5
+ * @license Angular v4.2.2-4164369
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-dc084a5');
+var VERSION = new Version('4.2.2-4164369');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
