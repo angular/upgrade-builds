550e0e18f2adb9c78a4a3cc5c407daffa00a5a38
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-14 14:36:16 +0000

    607fb1f fix(aio): always cover the whole footer with its background

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-38fc2a0
+ * @license Angular v4.2.2-607fb1f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-38fc2a0');
+var VERSION = new Version('4.2.2-607fb1f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
