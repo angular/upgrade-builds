7800a41571c33b50a2b0ae7f14658ad918244e89
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-14 17:10:59 +0000

    d378a29 ci(aio): fail the build if the PWA score is too low

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-2bdf2fe
+ * @license Angular v4.2.2-d378a29
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-2bdf2fe');
+const VERSION = new Version('4.2.2-d378a29');
 
 /**
  * @license
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-2bdf2fe
+ * @license Angular v4.2.2-d378a29
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-2bdf2fe');
+var VERSION = new Version('4.2.2-d378a29');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
