871799d7437eff883b3e0efeda000e00c7f53749
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-14 15:04:30 +0000

    2bdf2fe test(aio): add async beforeEach to prevent Chrome disconnects

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-607fb1f
+ * @license Angular v4.2.2-2bdf2fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-607fb1f');
+const VERSION = new Version('4.2.2-2bdf2fe');
 
 /**
  * @license
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-607fb1f
+ * @license Angular v4.2.2-2bdf2fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-607fb1f');
+var VERSION = new Version('4.2.2-2bdf2fe');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
