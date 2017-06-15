cd74c986cc27dcdd2c92dd3ae39548def62bdb0f
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-15 07:06:04 +0000

    01173b9 fix(aio): do not log messages in production

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-06a0cf2
+ * @license Angular v4.2.2-01173b9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-06a0cf2');
+var VERSION = new Version('4.2.2-01173b9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
