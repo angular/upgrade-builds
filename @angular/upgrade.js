b07bd98bf2674f9bb327addf9a9d0f15b7b4d71d
Hans <hans@hansl.ca>
2017-06-16 19:35:05 +0000

    adc1b12 fix(aio): correctly redirect `/docs/ts/latest` and `/styleguide`

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-f58211d
+ * @license Angular v4.2.2-adc1b12
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-f58211d');
+const VERSION = new Version('4.2.2-adc1b12');
 
 /**
  * @license
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-f58211d
+ * @license Angular v4.2.2-adc1b12
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -586,7 +586,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-f58211d');
+var VERSION = new Version('4.2.2-adc1b12');
 
 /**
  * @license
