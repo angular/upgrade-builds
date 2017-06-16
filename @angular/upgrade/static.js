14394c24a378b73473b819141280f77a9f038e91
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-16 07:27:49 +0000

    956a7e9 docs(aio): re-add biography entry for devversion

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-2cc931e
+ * @license Angular v4.2.2-956a7e9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -586,7 +586,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-2cc931e');
+var VERSION = new Version('4.2.2-956a7e9');
 
 /**
  * @license
