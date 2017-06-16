114075632e5073d558d5f635265c6905c961dede
Hans Larsen <hans@hansl.ca>
2017-06-16 21:14:24 +0000

    ab81c1c build(aio): update @angular/service-worker to 1.0.0-beta.16

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-71a8ef5
+ * @license Angular v4.2.3-ab81c1c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -586,7 +586,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.3-71a8ef5');
+var VERSION = new Version('4.2.3-ab81c1c');
 
 /**
  * @license
