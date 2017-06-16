7adf2ebcd4217f5c98a32b0cec7ad3a9b4559bba
Hans Larsen <hans@hansl.ca>
2017-06-16 17:20:41 +0000

    1ddbddb docs: add changelog for 4.2.3

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-232bd93
+ * @license Angular v4.2.2-1ddbddb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-232bd93');
+var VERSION = new Version('4.2.2-1ddbddb');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
