9b676eb853bf49ab30b2dec44e550aaf506145a2
Peter Bacon Darwin <pete@bacondarwin.com>
2017-06-16 07:28:17 +0000

    9c7a84d docs(aio): re-add biography entry for devversion

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-dbc6a4c
+ * @license Angular v4.2.2-9c7a84d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-dbc6a4c');
+var VERSION = new Version('4.2.2-9c7a84d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
