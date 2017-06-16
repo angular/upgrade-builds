88cac72a940efd772e1017506598675e7b8bf600
Hans Larsen <hans@hansl.ca>
2017-06-16 17:17:06 +0000

    8a547ee docs: add changelog for 4.2.3

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-b8c39cd
+ * @license Angular v4.2.3-8a547ee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-b8c39cd');
+var VERSION = new Version('4.2.3-8a547ee');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
