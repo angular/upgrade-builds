25611615484b93c0c0031b1b4f4cfca693501ab0
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-16 07:11:12 +0000

    ce00fa3 build(aio): do not fail if `check-env` for the main angular project fails

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-e096a85
+ * @license Angular v4.2.2-ce00fa3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.2.2-e096a85
+ * @license Angular v4.2.2-ce00fa3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -585,7 +585,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.2.2-e096a85');
+var VERSION = new _angular_core.Version('4.2.2-ce00fa3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
