2019dc23be41ecdfeb007e8958ae0ee80324db43
Hans <hans@hansl.ca>
2017-06-16 19:40:34 +0000

    5ac3919 feat(core): update zone.js to 0.8.12

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-fe126cb
+ * @license Angular v4.2.2-5ac3919
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-fe126cb');
+var VERSION = new Version('4.2.2-5ac3919');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-fe126cb
+ * @license Angular v4.2.2-5ac3919
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -586,7 +586,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-fe126cb');
+var VERSION = new Version('4.2.2-5ac3919');
 
 /**
  * @license
