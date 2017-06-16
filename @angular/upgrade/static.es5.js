9d819a057bbf3956623fc0407cd3438cbdfa2279
Peter Bacon Darwin <pete@bacondarwin.com>
2017-06-16 07:24:13 +0000

    dbc6a4c build(aio): do not fail if `check-env` for the main angular project fails

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-b192dd5
+ * @license Angular v4.2.2-dbc6a4c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-b192dd5');
+var VERSION = new Version('4.2.2-dbc6a4c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-b192dd5
+ * @license Angular v4.2.2-dbc6a4c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -586,7 +586,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-b192dd5');
+var VERSION = new Version('4.2.2-dbc6a4c');
 
 /**
  * @license
