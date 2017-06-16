b23e888e1e2b217a01dba04e1547b9c19fc05525
Hans <hans@hansl.ca>
2017-06-16 18:42:19 +0000

    9315ab8 test(compiler): fix typo "mamespace"

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-1ddbddb
+ * @license Angular v4.2.2-9315ab8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -586,7 +586,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-1ddbddb');
+var VERSION = new Version('4.2.2-9315ab8');
 
 /**
  * @license
