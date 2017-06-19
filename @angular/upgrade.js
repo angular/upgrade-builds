0c598dae3d8b09e56ea13dbde59045289d21bfda
Hans Larsen <hans@hansl.ca>
2017-06-19 18:47:18 +0000

    afbb6bb docs: remove aio from changelog

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-4f37f86
+ * @license Angular v4.2.2-afbb6bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-4f37f86');
+const VERSION = new Version('4.2.2-afbb6bb');
 
 /**
  * @license
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-4f37f86
+ * @license Angular v4.2.2-afbb6bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -586,7 +586,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-4f37f86');
+var VERSION = new Version('4.2.2-afbb6bb');
 
 /**
  * @license
