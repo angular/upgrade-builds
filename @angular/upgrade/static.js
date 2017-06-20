f0c41be745145ce69c300b67af8efb7b73518dd9
Hans <hans@hansl.ca>
2017-06-20 16:34:03 +0000

    3e685f9 fix(forms): roll back breaking change with min/max directives

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-1c6a252
+ * @license Angular v4.2.2-3e685f9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -586,7 +586,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-1c6a252');
+var VERSION = new Version('4.2.2-3e685f9');
 
 /**
  * @license
