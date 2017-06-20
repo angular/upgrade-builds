0fad5786f02ecbfe7cab15a7198bca7aa04644e2
Hans <hans@hansl.ca>
2017-06-20 19:21:08 +0000

    4e6be15 fix(language-service): rollup `tslib` into the language service package

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-3e685f9
+ * @license Angular v4.2.2-4e6be15
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-3e685f9');
+const VERSION = new Version('4.2.2-4e6be15');
 
 /**
  * @license
