f5d520f80030d858d1d41f8c1485c0224e71015c
Hans Larsen <hans@hansl.ca>
2017-06-20 19:23:59 +0000

    63a5f33 fix(language-service): infer `any` `ngForOf` of type `any`

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-4ab7353
+ * @license Angular v4.2.3-63a5f33
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.3-4ab7353');
+const VERSION = new Version('4.2.3-63a5f33');
 
 /**
  * @license
--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-4ab7353
+ * @license Angular v4.2.3-63a5f33
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v4.2.3-4ab7353
+ * @license Angular v4.2.3-63a5f33
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -29,7 +29,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.3-4ab7353');
+var VERSION = new _angular_core.Version('4.2.3-63a5f33');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
