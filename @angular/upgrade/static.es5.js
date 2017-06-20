e60cab8d75bcf0bdcff8cb841c0ed6abca37ba87
Hans Larsen <hans@hansl.ca>
2017-06-20 20:34:33 +0000

    f997937 docs(aio): update resource listing

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-77860a0
+ * @license Angular v4.2.3-f997937
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.3-77860a0');
+var VERSION = new Version('4.2.3-f997937');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
