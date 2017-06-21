55d7933bb053cf90a006afc15fef88990fffc969
Hans Larsen <hans@hansl.ca>
2017-06-21 00:16:00 +0000

    572885d docs(aio): fix numbered list in testing.md

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-3390648
+ * @license Angular v4.2.3-572885d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.3-3390648');
+var VERSION = new Version('4.2.3-572885d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
