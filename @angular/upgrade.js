815f299a104c0a49ca1085ec559a311dea424789
Hans Larsen <hans@hansl.ca>
2017-06-21 23:59:38 +0000

    74954f1 docs(aio): ward's changes

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-c112232
+ * @license Angular v4.2.3-74954f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.3-c112232');
+const VERSION = new Version('4.2.3-74954f1');
 
 /**
  * @license
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-c112232
+ * @license Angular v4.2.3-74954f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -586,7 +586,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.3-c112232');
+var VERSION = new Version('4.2.3-74954f1');
 
 /**
  * @license
