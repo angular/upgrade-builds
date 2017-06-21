94815bc4a75ad2cf94d90129239392483819d879
Hans <hans@hansl.ca>
2017-06-21 21:24:51 +0000

    97fd248 fix(aio): fix patch script on windows

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-bb804dd
+ * @license Angular v4.2.2-97fd248
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.2.2-bb804dd
+ * @license Angular v4.2.2-97fd248
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -585,7 +585,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.2.2-bb804dd');
+var VERSION = new _angular_core.Version('4.2.2-97fd248');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
