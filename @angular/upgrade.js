dd840b41dbe78779dbbe4f752d70d9829334a77d
Hans <hans@hansl.ca>
2017-06-21 18:57:20 +0000

    0034bb2 fix(aio): restore component-styles/exclude hidden children

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-d168528
+ * @license Angular v4.2.2-0034bb2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-d168528');
+const VERSION = new Version('4.2.2-0034bb2');
 
 /**
  * @license
