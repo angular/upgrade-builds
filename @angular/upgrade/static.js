01c9320197d2775d620e08e1e76540fb3f750b9a
Hans <hans@hansl.ca>
2017-06-21 21:12:36 +0000

    1d93cf2 build(aio): ensure all doc tests are run

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-0034bb2
+ * @license Angular v4.2.2-1d93cf2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -586,7 +586,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-0034bb2');
+var VERSION = new Version('4.2.2-1d93cf2');
 
 /**
  * @license
