bcbfce608f41aa38bfa648ee9e4f220d9650c5bc
Hans <hans@hansl.ca>
2017-06-21 23:52:32 +0000

    37cdc4f docs(aio): ward's changes

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-4352dd2
+ * @license Angular v4.2.2-37cdc4f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-4352dd2');
+var VERSION = new Version('4.2.2-37cdc4f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-4352dd2
+ * @license Angular v4.2.2-37cdc4f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-4352dd2');
+var VERSION = new Version('4.2.2-37cdc4f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
