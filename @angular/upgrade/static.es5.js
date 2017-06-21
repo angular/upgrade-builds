c3bca88e5078d1112979e4e1173c8f5288d3aed8
Hans <hans@hansl.ca>
2017-06-21 21:23:26 +0000

    bb804dd feat(aio): select contributor group with URL “about?group=gde”

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-1d93cf2
+ * @license Angular v4.2.2-bb804dd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-1d93cf2');
+var VERSION = new Version('4.2.2-bb804dd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-1d93cf2
+ * @license Angular v4.2.2-bb804dd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v4.2.2-1d93cf2
+ * @license Angular v4.2.2-bb804dd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -29,7 +29,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-1d93cf2');
+var VERSION = new _angular_core.Version('4.2.2-bb804dd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
