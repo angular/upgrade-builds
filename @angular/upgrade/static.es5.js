e4de84cde4fd7ced5b107fed449fc80340d96e28
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:18:53 +0000

    133b5e6 docs(aio): added link to Russian Angular Courses

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-92bcfef
+ * @license Angular v4.3.0-beta.0-133b5e6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.0-92bcfef');
+var VERSION = new Version('4.3.0-beta.0-133b5e6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
