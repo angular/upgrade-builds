1ecb0e881bcbb89c61d4c4d866b73ffb54cfe534
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:49:04 +0000

    67ffbae docs(aio): fix promises link in toh-pt4

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-5dd5bfd
+ * @license Angular v4.3.0-beta.0-67ffbae
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.0-5dd5bfd');
+var VERSION = new Version('4.3.0-beta.0-67ffbae');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
