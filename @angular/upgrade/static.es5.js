414652c2fe2daab4e0a9d5f36dca59986332451b
Hans Larsen <hans@hansl.ca>
2017-06-22 00:44:19 +0000

    8a09015 docs: add changelog for 4.2.4

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-d203136
+ * @license Angular v4.2.4-8a09015
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.3-d203136');
+var VERSION = new Version('4.2.4-8a09015');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
