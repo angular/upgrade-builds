fe16f425bc64109305188ba0a5e141f42d8cb2e3
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:54:12 +0000

    21018af docs(aio): replace “Angular 1” in upgrade phonecat script

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-67ffbae
+ * @license Angular v4.3.0-beta.0-21018af
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.0-67ffbae');
+var VERSION = new Version('4.3.0-beta.0-21018af');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
