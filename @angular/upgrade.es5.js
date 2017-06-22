1f6486723744113ec2807513578f9b564da8f621
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 21:58:44 +0000

    68f939e docs: Fix Stack Overflow being repeatedly misspelt as a single word

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-1fc0d05
+ * @license Angular v4.3.0-beta.0-68f939e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.0-1fc0d05');
+var VERSION = new Version('4.3.0-beta.0-68f939e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-1fc0d05
+ * @license Angular v4.3.0-beta.0-68f939e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.0-1fc0d05');
+const VERSION = new Version('4.3.0-beta.0-68f939e');
 
 /**
  * @license
