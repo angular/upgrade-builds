a239984542efbaf70d0dfc9bd8d2e6b1d639f90f
Hans Larsen <hans@hansl.ca>
2017-06-22 00:29:33 +0000

    02a38d3 docs: add changelog for 4.2.4

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-37cdc4f
+ * @license Angular v4.2.2-02a38d3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-37cdc4f');
+var VERSION = new Version('4.2.2-02a38d3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-37cdc4f
+ * @license Angular v4.2.2-02a38d3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-37cdc4f');
+var VERSION = new Version('4.2.2-02a38d3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
