9d147106fe50e6c513a09107ef5960226efcf195
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 20:09:54 +0000

    ffbdf74 docs(aio): cleanup rollup-config script

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.4-9344c86
+ * @license Angular v4.2.4-ffbdf74
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.4-9344c86');
+var VERSION = new Version('4.2.4-ffbdf74');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.4-9344c86
+ * @license Angular v4.2.4-ffbdf74
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -586,7 +586,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.4-9344c86');
+var VERSION = new Version('4.2.4-ffbdf74');
 
 /**
  * @license
