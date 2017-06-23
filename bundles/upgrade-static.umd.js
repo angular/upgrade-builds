c25b8f8d673adeb567f89627a8beecf2be071af4
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 22:01:11 +0000

    e80851d fix(core): add needed closure compiler warning suppression

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-30f4fe2
+ * @license Angular v4.3.0-beta.0-e80851d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.3.0-beta.0-30f4fe2
+ * @license Angular v4.3.0-beta.0-e80851d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -585,7 +585,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.0-30f4fe2');
+var VERSION = new _angular_core.Version('4.3.0-beta.0-e80851d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
