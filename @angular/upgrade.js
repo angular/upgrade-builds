26b9cefaf8bdb62f541e713aafdebaf1a43a7883
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 19:20:04 +0000

    849200b docs: remove unnecessary newline

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-87206e1
+ * @license Angular v4.3.0-beta.0-849200b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.0-87206e1');
+const VERSION = new Version('4.3.0-beta.0-849200b');
 
 /**
  * @license
