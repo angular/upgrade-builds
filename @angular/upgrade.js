0bc0bc9ef0b6956f9febb58b267b9e71bd3086bc
Matias Niemelä <matias@yearofmoo.com>
2017-06-26 21:01:03 +0000

    693f79e docs: fix spelling of case variants in naming.md

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-448d9f9
+ * @license Angular v4.3.0-beta.0-693f79e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.0-448d9f9');
+const VERSION = new Version('4.3.0-beta.0-693f79e');
 
 /**
  * @license
