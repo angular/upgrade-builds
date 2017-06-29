2ea5d2418eb3eecdd5a62338787a8817e6000c4b
Matias Niemelä <matias@yearofmoo.com>
2017-06-29 22:58:28 +0000

    7728d7e ci: remove print-logs.sh to try to fix failing builds on Travis

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.4-7205d7a
+ * @license Angular v4.2.4-7728d7e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.4-7205d7a');
+var VERSION = new Version('4.2.4-7728d7e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.4-7205d7a
+ * @license Angular v4.2.4-7728d7e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.4-7205d7a');
+var VERSION = new Version('4.2.4-7728d7e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
