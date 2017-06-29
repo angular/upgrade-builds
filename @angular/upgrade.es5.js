2d20073cf7ce9173d041012d78d1276e76e7714f
Matias Niemelä <matias@yearofmoo.com>
2017-06-29 01:48:50 +0000

    af14b1e test(animations): add proper tests to check multiple combinations of :enter/:leave animation queries

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-6c1a8da
+ * @license Angular v4.3.0-beta.0-af14b1e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.0-6c1a8da');
+var VERSION = new Version('4.3.0-beta.0-af14b1e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
