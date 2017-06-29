c5f3be7d7e0bbe49fa8945fb51a64ae5c2de69ab
Matias Niemelä <matias@yearofmoo.com>
2017-06-29 16:54:55 +0000

    4268c82 feat(aio): use shorter URLs for previews

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-af14b1e
+ * @license Angular v4.3.0-beta.0-4268c82
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -586,7 +586,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.0-af14b1e');
+var VERSION = new Version('4.3.0-beta.0-4268c82');
 
 /**
  * @license
