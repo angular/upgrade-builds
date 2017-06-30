594e8779bda3cf4cde40ed22fb3f2fa4492745cf
Stephen Fluin <stephen.fluin@gmail.com>
2017-06-30 19:13:04 +0000

    69ec333 docs(aio): Add resources contributor info

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-67519eb
+ * @license Angular v4.3.0-beta.1-69ec333
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.0-67519eb');
+var VERSION = new Version('4.3.0-beta.1-69ec333');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
