3ee6199e1b1c4f8b2561a595b0bcb9cafa914fab
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-30 11:48:03 +0000

    154154d docs(aio): fix quickstart-cli bad HTML escape chars

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-b052ef5
+ * @license Angular v4.3.0-beta.1-154154d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -586,7 +586,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-b052ef5');
+var VERSION = new Version('4.3.0-beta.1-154154d');
 
 /**
  * @license
