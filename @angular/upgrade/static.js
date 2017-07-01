41c06ee4a4d9c6dc93870e656dbe49f6d9d662d6
GitHub <noreply@github.com>
2017-07-01 18:00:49 +0000

    b479ed9 perf(core): refactor NgZone, decrease size by 1.2Kb (#17773)

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-d5dc53e
+ * @license Angular v4.3.0-beta.1-b479ed9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -586,7 +586,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-d5dc53e');
+var VERSION = new Version('4.3.0-beta.1-b479ed9');
 
 /**
  * @license
