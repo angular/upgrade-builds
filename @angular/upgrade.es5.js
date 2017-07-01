08a8d4ec5eda2fe4e9a0d1bc9f476d09b7cc00c5
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-01 14:12:20 +0000

    d5dc53e ci(aio): avoid printing too large PWA report (and restore `print-logs.sh`)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-8e00161
+ * @license Angular v4.3.0-beta.1-d5dc53e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-8e00161');
+var VERSION = new Version('4.3.0-beta.1-d5dc53e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-8e00161
+ * @license Angular v4.3.0-beta.1-d5dc53e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.3.0-beta.1-8e00161
+ * @license Angular v4.3.0-beta.1-d5dc53e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -585,7 +585,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.1-8e00161');
+var VERSION = new _angular_core.Version('4.3.0-beta.1-d5dc53e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
