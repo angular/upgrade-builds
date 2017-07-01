eb49cbf9b74d426f33c1aba9fcaac5a49b5200ac
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-01 14:07:00 +0000

    c460757 docs(aio): Add resources contributor info

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.5-1a476b8
+ * @license Angular v4.2.5-c460757
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -586,7 +586,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.5-1a476b8');
+var VERSION = new Version('4.2.5-c460757');
 
 /**
  * @license
