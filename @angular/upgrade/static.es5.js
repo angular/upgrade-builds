5178748b80d2e66e92789006f5b1fa0a825edef5
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-05 22:32:05 +0000

    b51697c ci: add Chuck to the owners of core (#17892)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-92d7ecf
+ * @license Angular v4.3.0-beta.1-b51697c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-92d7ecf');
+var VERSION = new Version('4.3.0-beta.1-b51697c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
