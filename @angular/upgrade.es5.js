e6955933b5572e33f4e9b4e423776b5ba2a70b55
Jason Aden <jasonaden@angular.io>
2017-07-06 17:57:20 +0000

    105e920 fix(animations): properly handle cancelled animation style application

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-71ee0c5
+ * @license Angular v4.3.0-beta.1-105e920
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-71ee0c5');
+var VERSION = new Version('4.3.0-beta.1-105e920');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-71ee0c5
+ * @license Angular v4.3.0-beta.1-105e920
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-71ee0c5');
+const VERSION = new Version('4.3.0-beta.1-105e920');
 
 /**
  * @license
