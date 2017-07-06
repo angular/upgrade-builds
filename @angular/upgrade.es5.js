b400096bb834942c5dcb0d94e4d4320c7a81d5ce
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-06 19:20:20 +0000

    f2f61c9 ci: allow chuck to approve animations (#17940)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-ec58246
+ * @license Angular v4.3.0-beta.1-f2f61c9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-ec58246');
+var VERSION = new Version('4.3.0-beta.1-f2f61c9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-ec58246
+ * @license Angular v4.3.0-beta.1-f2f61c9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-ec58246');
+var VERSION = new Version('4.3.0-beta.1-f2f61c9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
