e924798e4864f3cd7dd4650da50d09b2d2956ed4
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-06 18:32:24 +0000

    6fc5174 build: disable sauce connect logging (#17947)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-105e920
+ * @license Angular v4.3.0-beta.1-6fc5174
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-105e920');
+var VERSION = new Version('4.3.0-beta.1-6fc5174');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
