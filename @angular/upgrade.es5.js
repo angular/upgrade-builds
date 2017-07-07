92f4d687e7247e26ffbe910ecb673d73507b38b3
Jason Aden <jasonaden@angular.io>
2017-07-07 19:34:25 +0000

    37797e2 feat(common): new HttpClient API

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-72747e5
+ * @license Angular v4.3.0-beta.1-37797e2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-72747e5');
+var VERSION = new Version('4.3.0-beta.1-37797e2');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
