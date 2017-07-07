d6297dc2d192a94ba153d517804849518b2d60e9
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 16:04:47 +0000

    227dbbc fix(language-service): ignore hover of symbols not in the TypeScript program (#17969)

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-ae27af7
+ * @license Angular v4.3.0-beta.1-227dbbc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -586,7 +586,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-ae27af7');
+var VERSION = new Version('4.3.0-beta.1-227dbbc');
 
 /**
  * @license
