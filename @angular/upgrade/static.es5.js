5d2988682eaa403a8fb3fa1e5f840c83d68c5d7e
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 23:49:21 +0000

    2ba3ada revert: "refactor(compiler-cli): remove the dependency on fs in codegen.ts (#17738)"

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-c1474f3
+ * @license Angular v4.3.0-beta.1-2ba3ada
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -584,7 +584,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-beta.1-c1474f3');
+var VERSION = new Version('4.3.0-beta.1-2ba3ada');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
