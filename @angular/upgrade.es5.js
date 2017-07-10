b968186628c8cc21d36c1f9b238524a56e61e701
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-10 11:54:00 +0000

    b3d9036 fix(aio): add new common/http package into API list

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-cb76091
+ * @license Angular v4.3.0-rc.0-b3d9036
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-rc.0-cb76091');
+var VERSION = new Version('4.3.0-rc.0-b3d9036');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
