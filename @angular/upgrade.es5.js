ffa0a38f200a5c38f12572164ba2de87e5afb1ae
Igor Minar <iminar@google.com>
2017-07-11 20:36:41 +0000

    20127c1 docs: fix typos and grammar (#17818)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-a50d935
+ * @license Angular v4.3.0-rc.0-20127c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-rc.0-a50d935');
+var VERSION = new Version('4.3.0-rc.0-20127c1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
