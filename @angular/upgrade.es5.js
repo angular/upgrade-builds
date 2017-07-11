442907852b88758af6633a632aba6e60d24e74ca
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-11 10:37:36 +0000

    7c4ac68 build(aio): remove unused script and GitHub token

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-2055634
+ * @license Angular v4.3.0-rc.0-7c4ac68
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-rc.0-2055634');
+var VERSION = new Version('4.3.0-rc.0-7c4ac68');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
