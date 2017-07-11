2da30def28161ad7aa03c907cb7dbdee13020bc0
Igor Minar <iminar@google.com>
2017-07-11 19:17:46 +0000

    d9b03be test: git ignore e2e test folders (#17984)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-b6aad07
+ * @license Angular v4.3.0-rc.0-d9b03be
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-rc.0-b6aad07');
+var VERSION = new Version('4.3.0-rc.0-d9b03be');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
