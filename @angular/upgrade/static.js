a4362254305646fb0fe8a5ec20a964e4082af42d
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-11 11:14:18 +0000

    c6aacf5 build(aio): remove unused script and GitHub token

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.6-bd4ec5c
+ * @license Angular v4.2.6-c6aacf5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -595,7 +595,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.6-bd4ec5c');
+var VERSION = new Version('4.2.6-c6aacf5');
 
 /**
  * @license
