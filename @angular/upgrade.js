2ae2e3c0a94dcc287a94af90349c5e8f8935826b
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-11 11:26:10 +0000

    3f29348 ci: add aio/content to aio group

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.6-09261e6
+ * @license Angular v4.2.6-3f29348
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.6-09261e6');
+const VERSION = new Version('4.2.6-3f29348');
 
 /**
  * @license
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.6-09261e6
+ * @license Angular v4.2.6-3f29348
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -595,7 +595,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.6-09261e6');
+var VERSION = new Version('4.2.6-3f29348');
 
 /**
  * @license
