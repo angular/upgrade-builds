0022c8c8b1b9d8666b7ada5b05868a73d33fefaa
Igor Minar <iminar@google.com>
2017-07-11 19:05:19 +0000

    e131f6b docs: various minor formatting changes (#18046)

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-e28f097
+ * @license Angular v4.3.0-rc.0-e131f6b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-e28f097');
+const VERSION = new Version('4.3.0-rc.0-e131f6b');
 
 /**
  * @license
