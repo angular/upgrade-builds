ab2668faabfc00379028cf577fdc46c7b414dc1c
Igor Minar <iminar@google.com>
2017-07-11 19:14:56 +0000

    b6aad07 build: remove duplicate dependency on TypeScript

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-4d45fe6
+ * @license Angular v4.3.0-rc.0-b6aad07
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -595,7 +595,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-rc.0-4d45fe6');
+var VERSION = new Version('4.3.0-rc.0-b6aad07');
 
 /**
  * @license
