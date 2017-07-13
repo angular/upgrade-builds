bf68bb2862b97b799868efa84dceeb09d46c9bd8
Igor Minar <igor@angularjs.org>
2017-07-13 22:53:54 +0000

    72143e8 revert: ci(aio): exclude changes in aio/content folder

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-45ffe54
+ * @license Angular v4.3.0-rc.0-72143e8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-45ffe54');
+const VERSION = new Version('4.3.0-rc.0-72143e8');
 
 /**
  * @license
