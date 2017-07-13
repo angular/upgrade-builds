1d73add78d9f4d29dfda257577b644c03d3bd779
Igor Minar <iminar@google.com>
2017-07-13 19:34:14 +0000

    a7ea008 ci: fast-forward to master/HEAD on all circle jobs (#18101)

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-edb8375
+ * @license Angular v4.3.0-rc.0-a7ea008
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-edb8375');
+const VERSION = new Version('4.3.0-rc.0-a7ea008');
 
 /**
  * @license
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-edb8375
+ * @license Angular v4.3.0-rc.0-a7ea008
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -584,7 +584,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-rc.0-edb8375');
+var VERSION = new Version('4.3.0-rc.0-a7ea008');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
