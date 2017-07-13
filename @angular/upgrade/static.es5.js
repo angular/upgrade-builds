57c68624f8e2d8110b619eb311cd6a55367ab500
Igor Minar <iminar@google.com>
2017-07-13 19:35:34 +0000

    ffda3e4 build(aio): simplify the primary overload's anchor

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-a7ea008
+ * @license Angular v4.3.0-rc.0-ffda3e4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -584,7 +584,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-rc.0-a7ea008');
+var VERSION = new Version('4.3.0-rc.0-ffda3e4');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
