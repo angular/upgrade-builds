87514318bdf2f784aa244151c04848e1301e802f
Igor Minar <iminar@google.com>
2017-07-13 23:35:05 +0000

    ddb766e feat(compiler): do not evaluate metadata expressions that can use references (#18001)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-72143e8
+ * @license Angular v4.3.0-rc.0-ddb766e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -584,7 +584,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-rc.0-72143e8');
+var VERSION = new Version('4.3.0-rc.0-ddb766e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
