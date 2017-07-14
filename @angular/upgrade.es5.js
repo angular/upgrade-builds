25fe6226a8f26daae42b7f2337b901dff4814cbe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-14 20:40:15 +0000

    a0b06be docs: add changelog for 4.3.0

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-e0fa727
+ * @license Angular v4.3.0-a0b06be
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-rc.0-e0fa727');
+var VERSION = new Version('4.3.0-a0b06be');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
