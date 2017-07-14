226b37fead4c410eb72c0f3957ad957f07f14ad3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-14 22:22:27 +0000

    97e6901 Revert "revert: revert: ci(aio): exclude changes in aio/content folder"

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-30e76fc
+ * @license Angular v4.3.0-97e6901
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -610,7 +610,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-30e76fc');
+var VERSION = new Version('4.3.0-97e6901');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
