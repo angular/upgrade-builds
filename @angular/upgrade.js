368bcac512bf7f8bd3b6b53680ef1e488dbc2e09
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-15 15:13:30 +0000

    969ce9d fix(aio): remove title from callout

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-34834a9
+ * @license Angular v4.3.0-969ce9d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-34834a9');
+const VERSION = new Version('4.3.0-969ce9d');
 
 /**
  * @license
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-34834a9
+ * @license Angular v4.3.0-969ce9d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -581,7 +581,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-34834a9');
+var VERSION = new Version('4.3.0-969ce9d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
