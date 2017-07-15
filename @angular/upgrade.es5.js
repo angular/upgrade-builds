a3a7e7e42af5f4cc1bf0abed0da932377cd66682
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-15 14:53:41 +0000

    34834a9 fix(aio): remove unused `news.html` file

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-6e2ddcc
+ * @license Angular v4.3.0-34834a9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-6e2ddcc');
+var VERSION = new Version('4.3.0-34834a9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
