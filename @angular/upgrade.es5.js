6c015e081d77db245c61630af6336d8568ab567a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-15 08:10:22 +0000

    0bf0c35 build(aio): render type parameters of API function exports

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-97e6901
+ * @license Angular v4.3.0-0bf0c35
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-97e6901');
+var VERSION = new Version('4.3.0-0bf0c35');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-97e6901
+ * @license Angular v4.3.0-0bf0c35
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-97e6901');
+const VERSION = new Version('4.3.0-0bf0c35');
 
 /**
  * @license
