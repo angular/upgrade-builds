bcb3c86726c52f63617a3497da942ed95f572973
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-17 21:39:47 +0000

    c6ad212 docs(aio): fix HttpClient's interceptor sample

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-8c81c62
+ * @license Angular v4.3.0-c6ad212
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -610,7 +610,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-8c81c62');
+var VERSION = new Version('4.3.0-c6ad212');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
