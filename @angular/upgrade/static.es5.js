8919670ef3ebc0b8a8d02478d4ea6c5fab0f3d5f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-17 21:34:18 +0000

    47b3ecd docs(http): fix "Expecting and answering requests" example mistake

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-7e72317
+ * @license Angular v4.3.0-47b3ecd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -610,7 +610,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-7e72317');
+var VERSION = new Version('4.3.0-47b3ecd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
