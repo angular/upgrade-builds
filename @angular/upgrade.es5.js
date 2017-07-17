f8fc9c2931bf0795b0bcecd9c3882cce814b0abb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-17 22:10:40 +0000

    3f331b5 docs(aio): update jelbourn photo

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-375d598
+ * @license Angular v4.3.0-3f331b5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-375d598');
+var VERSION = new Version('4.3.0-3f331b5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-375d598
+ * @license Angular v4.3.0-3f331b5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -610,7 +610,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-375d598');
+var VERSION = new Version('4.3.0-3f331b5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
