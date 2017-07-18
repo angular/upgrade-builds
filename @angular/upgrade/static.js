ff67404ddec062a435c6d996a2ab9ee158c1b54a
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-18 17:09:34 +0000

    c39e7d1 fix(aio): do not wrap `<code-tabs>` tab labels

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-799bffb
+ * @license Angular v4.3.0-c39e7d1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -594,7 +594,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-799bffb');
+var VERSION = new Version('4.3.0-c39e7d1');
 
 /**
  * @license
--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-799bffb
+ * @license Angular v4.3.0-c39e7d1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.3.0-799bffb
+ * @license Angular v4.3.0-c39e7d1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -591,7 +591,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.3.0-799bffb');
+var VERSION = new _angular_core.Version('4.3.0-c39e7d1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
