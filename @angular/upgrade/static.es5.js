de2e016f9cc19883bb13302fab1d43a390f291b3
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-18 17:02:01 +0000

    92179bc fix(aio): do not wrap `<code-tabs>` tab labels

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-cdb069a
+ * @license Angular v4.3.0-92179bc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -610,7 +610,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-cdb069a');
+var VERSION = new Version('4.3.0-92179bc');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
