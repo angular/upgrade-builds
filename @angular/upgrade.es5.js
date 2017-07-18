781bdeb34d26b9dac63f87a3739215797d1b72b8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:18:39 +0000

    3b588fe docs: fix typo

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-e03adb9
+ * @license Angular v4.3.0-3b588fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-e03adb9');
+var VERSION = new Version('4.3.0-3b588fe');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-e03adb9
+ * @license Angular v4.3.0-3b588fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-e03adb9');
+const VERSION = new Version('4.3.0-3b588fe');
 
 /**
  * @license
