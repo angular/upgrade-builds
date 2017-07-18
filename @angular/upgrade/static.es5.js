9a99d70ffccd70b5a68e60f4eae9dfa99056b992
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-18 16:51:49 +0000

    cdb069a docs(aio): fix cheatsheet layout for narrow screens

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-c453b7b
+ * @license Angular v4.3.0-cdb069a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -610,7 +610,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-c453b7b');
+var VERSION = new Version('4.3.0-cdb069a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
