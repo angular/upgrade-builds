deeca57159a735b7fabaeb58ae871c0de05cf4c7
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-18 17:13:23 +0000

    f0a1109 fix(aio): add quote to module

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-c39e7d1
+ * @license Angular v4.3.0-f0a1109
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -581,7 +581,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-c39e7d1');
+var VERSION = new Version('4.3.0-f0a1109');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-c39e7d1
+ * @license Angular v4.3.0-f0a1109
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v4.3.0-c39e7d1
+ * @license Angular v4.3.0-f0a1109
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -29,7 +29,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-c39e7d1');
+var VERSION = new _angular_core.Version('4.3.0-f0a1109');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
