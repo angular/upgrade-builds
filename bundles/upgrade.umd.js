5eb662b9c6eaff96d302ca860aefb2ee065554d9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 17:37:22 +0000

    ad3029e fix(router): should run resolvers for the same route concurrently

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-2a2fe11
+ * @license Angular v4.3.0-ad3029e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v4.3.0-2a2fe11
+ * @license Angular v4.3.0-ad3029e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -29,7 +29,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-2a2fe11');
+var VERSION = new _angular_core.Version('4.3.0-ad3029e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
