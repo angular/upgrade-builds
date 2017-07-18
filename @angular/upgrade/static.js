4a9a0f4d6e69f09941714216446fc9d27c9adbe9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:00:55 +0000

    95635c1 fix(compiler): ensure jit external id arguments names are unique

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-e20cfe1
+ * @license Angular v4.3.0-95635c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -621,7 +621,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-e20cfe1');
+var VERSION = new Version('4.3.0-95635c1');
 
 /**
  * @license
