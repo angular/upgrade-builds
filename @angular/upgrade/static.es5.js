28abadfb9ba71e8892c05ad3c83eef4afbc93608
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 17:49:34 +0000

    e20cfe1 fix(router): canDeactivate guards should run from bottom to top

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-eb6fb5f
+ * @license Angular v4.3.0-e20cfe1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -610,7 +610,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-eb6fb5f');
+var VERSION = new Version('4.3.0-e20cfe1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
