616e8f32194bb4b6075de9f404ef98ab9ee6a5e3
Miško Hevery <misko@hevery.com>
2017-07-20 16:56:50 +0000

    31cb418 docs(aio): Fix http guide

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-8de44cf
+ * @license Angular v5.0.0-beta.0-31cb418
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.0-8de44cf');
+const VERSION = new Version('5.0.0-beta.0-31cb418');
 
 /**
  * @license
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-8de44cf
+ * @license Angular v5.0.0-beta.0-31cb418
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -624,7 +624,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.0-8de44cf');
+var VERSION = new Version('5.0.0-beta.0-31cb418');
 
 /**
  * @license
