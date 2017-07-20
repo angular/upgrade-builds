00002ba5592146adfa26f0a015821f4b19fd0ac8
Miško Hevery <misko@hevery.com>
2017-07-20 16:56:08 +0000

    8de44cf docs: fix wrong link in CONTRIBUTING.md (#18228)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-54ea5b6
+ * @license Angular v5.0.0-beta.0-8de44cf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.0.0-beta.0-54ea5b6
+ * @license Angular v5.0.0-beta.0-8de44cf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -29,7 +29,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.0-54ea5b6');
+var VERSION = new _angular_core.Version('5.0.0-beta.0-8de44cf');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
