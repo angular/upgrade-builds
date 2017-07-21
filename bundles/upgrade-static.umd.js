9a7c52d201636f71c5bbb7fd9063f8effaadd701
Miško Hevery <misko@hevery.com>
2017-07-21 17:41:50 +0000

    a6c635e ci: force precise on Travis (#18282)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-54e0244
+ * @license Angular v5.0.0-beta.0-a6c635e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowser) { 'use strict';
 
 /**
- * @license Angular v5.0.0-beta.0-54e0244
+ * @license Angular v5.0.0-beta.0-a6c635e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -649,7 +649,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.0-54e0244');
+var VERSION = new _angular_core.Version('5.0.0-beta.0-a6c635e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
