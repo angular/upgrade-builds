ff8d1dee07384e496bf352b70875d0cbf9771af5
Miško Hevery <misko@hevery.com>
2017-07-21 17:48:47 +0000

    e0a9625 test: fix bad merge (#18267)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-a6c635e
+ * @license Angular v5.0.0-beta.0-e0a9625
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.0-a6c635e');
+var VERSION = new Version('5.0.0-beta.0-e0a9625');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-a6c635e
+ * @license Angular v5.0.0-beta.0-e0a9625
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.0-a6c635e');
+const VERSION = new Version('5.0.0-beta.0-e0a9625');
 
 /**
  * @license
