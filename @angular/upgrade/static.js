1c9ee1cb4630a71d8149e8f877895b6c22bd0e81
Miško Hevery <misko@hevery.com>
2017-07-22 00:12:18 +0000

    6fc5940 build: Bazel builds ngfactories for packages/core (#18289)

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-b7a6f52
+ * @license Angular v5.0.0-beta.0-6fc5940
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -649,7 +649,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.0-b7a6f52');
+var VERSION = new Version('5.0.0-beta.0-6fc5940');
 
 /**
  * @license
