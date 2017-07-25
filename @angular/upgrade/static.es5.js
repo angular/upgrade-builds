f171d0386bb006356605d7c007995f2601b48bcd
Miško Hevery <misko@hevery.com>
2017-07-25 23:12:03 +0000

    b582e2b docs(aio): update examples to 4.3

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-91ab39c
+ * @license Angular v5.0.0-beta.0-b582e2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -640,7 +640,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.0-91ab39c');
+var VERSION = new Version('5.0.0-beta.0-b582e2b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
