a95e09d05c4689b5ccf75ed3cae3a3af0e835b9e
Miško Hevery <misko@hevery.com>
2017-07-27 21:21:48 +0000

    12b8e1a Revert "fix(router): should throw when lazy loaded module doesn't define any routes (#15001)"

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.1-2245748
+ * @license Angular v4.3.1-12b8e1a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -594,7 +594,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.1-2245748');
+var VERSION = new Version('4.3.1-12b8e1a');
 
 /**
  * @license
