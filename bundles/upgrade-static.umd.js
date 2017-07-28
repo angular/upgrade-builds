3a57b55ed9a9d96b3d1d21b5ca0d9e83e8e03908
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-28 22:26:42 +0000

    0fb7484 refactor(aio): move content-specific images to `content/images/`

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-5808153
+ * @license Angular v5.0.0-beta.1-0fb7484
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowser) { 'use strict';
 
 /**
- * @license Angular v5.0.0-beta.1-5808153
+ * @license Angular v5.0.0-beta.1-0fb7484
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -649,7 +649,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.1-5808153');
+var VERSION = new _angular_core.Version('5.0.0-beta.1-0fb7484');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
