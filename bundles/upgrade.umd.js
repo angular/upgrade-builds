af05612b4197ec79eb8b7c47094e2d65e14d8870
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 19:58:30 +0000

    a11542a docs(aio): fixed list format in FormArray section

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-b6c4af6
+ * @license Angular v5.0.0-beta.1-a11542a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.0.0-beta.1-b6c4af6
+ * @license Angular v5.0.0-beta.1-a11542a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -33,7 +33,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.1-b6c4af6');
+var VERSION = new _angular_core.Version('5.0.0-beta.1-a11542a');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
