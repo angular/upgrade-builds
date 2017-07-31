61c45dc0636216b5b1f979ca90dea78062804c6d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 19:39:26 +0000

    ebef5e6 feat(forms): add options arg to abstract controls

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-d71ae27
+ * @license Angular v5.0.0-beta.1-ebef5e6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.1-d71ae27');
+const VERSION = new Version('5.0.0-beta.1-ebef5e6');
 
 /**
  * @fileoverview added by tsickle
--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-d71ae27
+ * @license Angular v5.0.0-beta.1-ebef5e6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.0.0-beta.1-d71ae27
+ * @license Angular v5.0.0-beta.1-ebef5e6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -33,7 +33,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.1-d71ae27');
+var VERSION = new _angular_core.Version('5.0.0-beta.1-ebef5e6');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
