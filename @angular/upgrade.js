6b3d18b68138ebe93c75898bbd4aa1a670e44416
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 19:47:34 +0000

    381471d fix(compiler): fix for element needing implicit parent placed in top-level ng-container

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-ebef5e6
+ * @license Angular v5.0.0-beta.1-381471d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.1-ebef5e6');
+const VERSION = new Version('5.0.0-beta.1-381471d');
 
 /**
  * @fileoverview added by tsickle
