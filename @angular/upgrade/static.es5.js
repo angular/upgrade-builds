761717384b6e4eab75e86cec8571fa9a5cd33923
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 23:15:19 +0000

    9479a10 build: enable TSLint on the packages folder

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-e64b54b
+ * @license Angular v5.0.0-beta.1-9479a10
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -640,7 +640,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.1-e64b54b');
+var VERSION = new Version('5.0.0-beta.1-9479a10');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
