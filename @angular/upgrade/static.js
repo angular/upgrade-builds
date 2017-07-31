3458e9e64ba99f7961a11a442cd706a14deab8f0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 21:38:51 +0000

    e64b54b fix(compiler): do not consider arguments when determining recursion

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-cc2a4c4
+ * @license Angular v5.0.0-beta.1-e64b54b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -649,7 +649,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.1-cc2a4c4');
+var VERSION = new Version('5.0.0-beta.1-e64b54b');
 
 /**
  * @license
