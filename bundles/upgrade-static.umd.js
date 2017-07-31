6e810ab678c87a15d98afdd7a15918cbcfe6b2c8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 19:54:11 +0000

    b6c4af6 feat(compiler-cli): automatically lower lambda expressions in metadata

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-381471d
+ * @license Angular v5.0.0-beta.1-b6c4af6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowser) { 'use strict';
 
 /**
- * @license Angular v5.0.0-beta.1-381471d
+ * @license Angular v5.0.0-beta.1-b6c4af6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -649,7 +649,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.1-381471d');
+var VERSION = new _angular_core.Version('5.0.0-beta.1-b6c4af6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
