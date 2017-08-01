5aa4799d4a0a0936623811546a49bbf5c75ac88e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-08-01 17:43:47 +0000

    82b067f build: ignore node_modules for tslint

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-9479a10
+ * @license Angular v5.0.0-beta.1-82b067f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -649,7 +649,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.1-9479a10');
+var VERSION = new Version('5.0.0-beta.1-82b067f');
 
 /**
  * @license
