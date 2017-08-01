dd680af418bc66007aa06174566f699f237e3427
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-08-01 18:05:57 +0000

    49cd851 feat(router): add events tracking activation of individual routes

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-82b067f
+ * @license Angular v5.0.0-beta.1-49cd851
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -649,7 +649,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.1-82b067f');
+var VERSION = new Version('5.0.0-beta.1-49cd851');
 
 /**
  * @license
