b4a48fa6ae32d73d90fd1534133e4112d965bd08
Victor Berchet <victor@suumit.com>
2017-08-02 22:58:57 +0000

    89c6161 docs: improve github labels by introducing "PR target" labels (#18436)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-1e1af7f
+ * @license Angular v5.0.0-beta.2-89c6161
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -640,7 +640,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-1e1af7f');
+var VERSION = new Version('5.0.0-beta.2-89c6161');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
