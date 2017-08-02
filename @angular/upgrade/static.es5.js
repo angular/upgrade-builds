d09c350d0855e67d76034b4012a9ec98bfdffd84
Victor Berchet <victor@suumit.com>
2017-08-02 23:23:36 +0000

    b65fe3e ci: remove chromium fold reference (#18445)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-dbc5c58
+ * @license Angular v4.3.3-b65fe3e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -581,7 +581,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.3-dbc5c58');
+var VERSION = new Version('4.3.3-b65fe3e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
