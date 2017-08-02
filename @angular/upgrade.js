baf6889b0b4a3a64151fce0916195f96a0c17f1f
Victor Berchet <victor@suumit.com>
2017-08-02 23:57:49 +0000

    3923c30 docs(aio): fix missing anchor-open in i18n documentation (#18476)

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-99017bf
+ * @license Angular v5.0.0-beta.2-3923c30
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.2-99017bf');
+const VERSION = new Version('5.0.0-beta.2-3923c30');
 
 /**
  * @fileoverview added by tsickle
