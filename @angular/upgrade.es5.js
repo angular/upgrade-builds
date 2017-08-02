b7d42056e5ddd882559be19fed84a1b33647f1e8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-08-02 20:43:48 +0000

    24db1ed docs: add changelog for 4.3.3

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.2-da8bb1b
+ * @license Angular v4.3.3-24db1ed
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.2-da8bb1b');
+var VERSION = new Version('4.3.3-24db1ed');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.2-da8bb1b
+ * @license Angular v4.3.3-24db1ed
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.2-da8bb1b');
+const VERSION = new Version('4.3.3-24db1ed');
 
 /**
  * @license
