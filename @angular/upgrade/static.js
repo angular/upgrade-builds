43f560a8b3a841caf10b4de282f473e2259a6748
Victor Berchet <victor@suumit.com>
2017-08-03 00:20:24 +0000

    6a88659 test(upgrade): fix an IE9 timer issue in downgrade module tests (#18482)

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-44ae6e9
+ * @license Angular v5.0.0-beta.2-6a88659
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -649,7 +649,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-44ae6e9');
+var VERSION = new Version('5.0.0-beta.2-6a88659');
 
 /**
  * @license
