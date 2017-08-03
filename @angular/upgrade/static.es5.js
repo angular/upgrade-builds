f06c8660d7edda16980a9d4f971fc96382d3ee02
Victor Berchet <victor@suumit.com>
2017-08-03 00:42:23 +0000

    ea07856 refactor(upgrade): compile upgrade cleanly with TypeScript 2.4 (#18461)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-7c47b62
+ * @license Angular v5.0.0-beta.2-ea07856
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -640,7 +640,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-7c47b62');
+var VERSION = new Version('5.0.0-beta.2-ea07856');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
