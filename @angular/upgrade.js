e1704e9cfa1a87cdbfc77d200d3381850496cefc
Victor Berchet <victor@suumit.com>
2017-08-03 18:24:48 +0000

    e7e7622 fix(compiler): ignore @import in multi-line css (#18452)

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-4176832
+ * @license Angular v4.3.3-e7e7622
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.3-4176832');
+const VERSION = new Version('4.3.3-e7e7622');
 
 /**
  * @license
