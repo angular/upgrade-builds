2ee0f317ab61ac729cce9fc5e156cfd50b564628
GitHub <noreply@github.com>
2017-08-03 22:00:33 +0000

    ca695e0 fix(compiler-cli): disable buggy expression lowering (#18513)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-9b015a9
+ * @license Angular v5.0.0-beta.2-ca695e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowser) { 'use strict';
 
 /**
- * @license Angular v5.0.0-beta.2-9b015a9
+ * @license Angular v5.0.0-beta.2-ca695e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -649,7 +649,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.2-9b015a9');
+var VERSION = new _angular_core.Version('5.0.0-beta.2-ca695e0');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
