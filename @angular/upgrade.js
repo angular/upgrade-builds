792946a5600728cdbc5fe3751a75e136d5188cc3
Victor Berchet <victor@suumit.com>
2017-08-03 00:44:40 +0000

    1640d2a refactor(platform-browser): compiler platform-browser packages cleanly (#18464)

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-5f501c7
+ * @license Angular v5.0.0-beta.2-1640d2a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.2-5f501c7');
+const VERSION = new Version('5.0.0-beta.2-1640d2a');
 
 /**
  * @fileoverview added by tsickle
--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-5f501c7
+ * @license Angular v5.0.0-beta.2-1640d2a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.0.0-beta.2-5f501c7
+ * @license Angular v5.0.0-beta.2-1640d2a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -33,7 +33,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.2-5f501c7');
+var VERSION = new _angular_core.Version('5.0.0-beta.2-1640d2a');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
