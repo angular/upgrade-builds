c95a543b0230b8ddff099eaee68024ecb152d1fd
Victor Berchet <victor@suumit.com>
2017-08-07 19:40:16 +0000

    04b18a9 docs(common): fix the DatePipe API docs (#18548)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-05472cb
+ * @license Angular v5.0.0-beta.2-04b18a9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -640,7 +640,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-05472cb');
+var VERSION = new Version('5.0.0-beta.2-04b18a9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
