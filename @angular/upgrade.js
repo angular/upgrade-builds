a4abd11bd2a796578032359364b328314e469c05
Victor Berchet <victor@suumit.com>
2017-08-08 21:29:25 +0000

    1e18331 ci(aio): fix deploying to firebase (#18590)

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-6f2038c
+ * @license Angular v5.0.0-beta.2-1e18331
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.2-6f2038c');
+const VERSION = new Version('5.0.0-beta.2-1e18331');
 
 /**
  * @fileoverview added by tsickle
