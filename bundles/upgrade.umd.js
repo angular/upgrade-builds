522ea2bf58f0aac59565f1b377fc1bce71325ebc
Victor Berchet <victor@suumit.com>
2017-08-09 21:44:37 +0000

    71f5e78 test(animations): disable buggy test in Chrome 39 (#18483)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-f0c3ed0
+ * @license Angular v4.3.3-71f5e78
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v4.3.3-f0c3ed0
+ * @license Angular v4.3.3-71f5e78
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -29,7 +29,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.3-f0c3ed0');
+var VERSION = new _angular_core.Version('4.3.3-71f5e78');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
