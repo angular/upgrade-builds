f4b41f3b145dec0ee100ced0d032074e3b584e9e
Hans <hans@hansl.ca>
2017-08-11 03:49:58 +0000

    679608d refactor(compiler-cli): use the transformer based compiler by default

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-06faac8
+ * @license Angular v5.0.0-beta.3-679608d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.3-06faac8');
+const VERSION = new Version('5.0.0-beta.3-679608d');
 
 /**
  * @fileoverview added by tsickle
