b19586e6d0e7f43c2bac3afa3019b0c470e6e340
Hans Larsen <hans@hansl.ca>
2017-08-15 23:07:15 +0000

    a618d6e docs(forms): add api docs for AbstractControlDirective

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.4-b315a84
+ * @license Angular v4.3.4-a618d6e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.4-b315a84');
+var VERSION = new Version('4.3.4-a618d6e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
