15bdb5bf2bf54c63bde79919822492af92dc3c4f
Hans <hans@hansl.ca>
2017-08-15 23:12:04 +0000

    856278c docs(core): correct code examples for ChangeDetectorRef

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-5e840e1
+ * @license Angular v5.0.0-beta.3-856278c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -640,7 +640,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.3-5e840e1');
+var VERSION = new Version('5.0.0-beta.3-856278c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
