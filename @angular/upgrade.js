c095f00ed1f08afe6f514d5e744d662d0109f2ce
Hans <hans@hansl.ca>
2017-08-15 23:25:51 +0000

    38addac build(aio): switch from `@angular/http` to `@angular/common/http`

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-856278c
+ * @license Angular v5.0.0-beta.3-38addac
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.3-856278c');
+const VERSION = new Version('5.0.0-beta.3-38addac');
 
 /**
  * @fileoverview added by tsickle
