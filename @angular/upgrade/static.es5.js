a40c69b725c9ee1d5f02a89d0aaec831e6c4f2ea
Hans Larsen <hans@hansl.ca>
2017-08-15 23:23:03 +0000

    1cb6076 build(aio): switch from `@angular/http` to `@angular/common/http`

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.4-259fc91
+ * @license Angular v4.3.4-1cb6076
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -581,7 +581,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.4-259fc91');
+var VERSION = new Version('4.3.4-1cb6076');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
