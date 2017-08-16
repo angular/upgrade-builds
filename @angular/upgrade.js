08120da4a3a0b4e231dd8d968a0cab7e3063f748
Hans <hans@hansl.ca>
2017-08-16 00:17:14 +0000

    697c6ed docs: remove TypeScript to JavaScript guide & sample

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-64b4be9
+ * @license Angular v5.0.0-beta.3-697c6ed
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.3-64b4be9');
+const VERSION = new Version('5.0.0-beta.3-697c6ed');
 
 /**
  * @fileoverview added by tsickle
