8d6fbb35dd327d7999dd75deb7a304aeea2c3dd7
Hans Larsen <hans@hansl.ca>
2017-08-16 18:14:59 +0000

    7e94405 docs: add changelog for 4.3.5

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.4-a1624f2
+ * @license Angular v4.3.5-7e94405
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -581,7 +581,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.4-a1624f2');
+var VERSION = new Version('4.3.5-7e94405');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.4-a1624f2
+ * @license Angular v4.3.5-7e94405
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -594,7 +594,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.4-a1624f2');
+var VERSION = new Version('4.3.5-7e94405');
 
 /**
  * @license
