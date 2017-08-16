e82b2071582737e889ab3affb96792e2fc01826a
Hans Larsen <hans@hansl.ca>
2017-08-16 00:33:42 +0000

    7662cef docs: remove TypeScript to JavaScript guide & sample

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.4-1cb6076
+ * @license Angular v4.3.4-7662cef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -581,7 +581,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.4-1cb6076');
+var VERSION = new Version('4.3.4-7662cef');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
