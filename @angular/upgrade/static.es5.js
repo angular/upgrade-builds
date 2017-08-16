9994e263268080042df379ba4eeec56465ec9fe0
Hans Larsen <hans@hansl.ca>
2017-08-16 20:58:16 +0000

    9aa0521 docs: add changelog for 5.0.0-beta.4

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-bc22ff1
+ * @license Angular v5.0.0-beta.4-9aa0521
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -640,7 +640,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.3-bc22ff1');
+var VERSION = new Version('5.0.0-beta.4-9aa0521');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
