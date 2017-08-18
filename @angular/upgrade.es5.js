7a38a104c05d1f7f9186d4804a09fb287d061aca
Miško Hevery <misko@hevery.com>
2017-08-18 18:45:44 +0000

    f9b2905 fix(animations): resolve error when using AnimationBuilder with platform-server (#18642)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.5-7e94405
+ * @license Angular v4.3.5-f9b2905
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.5-7e94405');
+var VERSION = new Version('4.3.5-f9b2905');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
