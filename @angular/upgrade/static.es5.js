797fb072082783e91417b6df894e87148e00c3ff
Miško Hevery <misko@hevery.com>
2017-08-21 22:36:34 +0000

    55d151a refactor(common): remove usage of deprecated Renderer (#17528)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-e228f2c
+ * @license Angular v5.0.0-beta.4-55d151a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -640,7 +640,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.4-e228f2c');
+var VERSION = new Version('5.0.0-beta.4-55d151a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
