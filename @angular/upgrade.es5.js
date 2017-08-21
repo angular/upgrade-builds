dc70aabd166601f3ea3a73c33ec44374fd221388
Miško Hevery <misko@hevery.com>
2017-08-21 23:26:55 +0000

    7ce9e06 fix(aio): do not redirect API pages on archive and next deployments (#18791)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-55d151a
+ * @license Angular v5.0.0-beta.4-7ce9e06
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -24,7 +24,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.4-55d151a');
+var VERSION = new Version('5.0.0-beta.4-7ce9e06');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-55d151a
+ * @license Angular v5.0.0-beta.4-7ce9e06
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -649,7 +649,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.4-55d151a');
+var VERSION = new Version('5.0.0-beta.4-7ce9e06');
 
 /**
  * @license
