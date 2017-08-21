e2f4eb9b7f48e84a2e1703242531e4c83ff34c09
Miško Hevery <misko@hevery.com>
2017-08-21 22:23:09 +0000

    55d151a refactor(common): remove usage of deprecated Renderer (#17528)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-5a1b9a3
+ * @license Angular v5.0.0-beta.4-55d151a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -24,7 +24,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.4-5a1b9a3');
+var VERSION = new Version('5.0.0-beta.4-55d151a');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-5a1b9a3
+ * @license Angular v5.0.0-beta.4-55d151a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowser) { 'use strict';
 
 /**
- * @license Angular v5.0.0-beta.4-5a1b9a3
+ * @license Angular v5.0.0-beta.4-55d151a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -649,7 +649,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.4-5a1b9a3');
+var VERSION = new _angular_core.Version('5.0.0-beta.4-55d151a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
