43196b756429f7351270bcac2deb099bb181359e
Miško Hevery <misko@hevery.com>
2017-08-22 02:02:53 +0000

    ec56760 refactor(common): remove deprecated `NgFor` (#18758)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-55d151a
+ * @license Angular v5.0.0-beta.4-ec56760
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -24,7 +24,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.4-55d151a');
+var VERSION = new Version('5.0.0-beta.4-ec56760');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
