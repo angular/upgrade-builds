b035b4f03ec31afc99a6533a79e5f5cda87a83bb
Miško Hevery <misko@hevery.com>
2017-08-23 18:46:24 +0000

    641be64 docs(aio): add info about `--local` option in the readme (#18824)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.5-1f43713
+ * @license Angular v4.3.5-641be64
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -581,7 +581,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.5-1f43713');
+var VERSION = new Version('4.3.5-641be64');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
