1aab31ca541ce531a2e9759c120c659d267cb9c9
Misko Hevery <misko@hevery.com>
2017-08-24 17:05:49 +0000

    1c2f3d4 build: fix build by pinning angular-cli to specific version (#18865)

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-3db84c8
+ * @license Angular v5.0.0-beta.4-1c2f3d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.4-3db84c8');
+const VERSION = new Version('5.0.0-beta.4-1c2f3d4');
 
 /**
  * @fileoverview added by tsickle
