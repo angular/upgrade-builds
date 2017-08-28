fd8e5992e286faa0df18c028e6243b38d09046a3
Miško Hevery <misko@hevery.com>
2017-08-28 21:47:01 +0000

    655b495 build: reenable npm install for air builds

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-92dbaf3
+ * @license Angular v5.0.0-beta.4-655b495
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -640,7 +640,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.4-92dbaf3');
+var VERSION = new Version('5.0.0-beta.4-655b495');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
