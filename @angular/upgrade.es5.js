94df3903a88e2173450e57dfdcc4330fd5327455
Miško Hevery <misko@hevery.com>
2017-08-29 00:12:50 +0000

    72c7b6e feat: deprecate @angular/http in favor of @angular/common/http (#18906)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-2e714f9
+ * @license Angular v5.0.0-beta.5-72c7b6e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -24,7 +24,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.5-2e714f9');
+var VERSION = new Version('5.0.0-beta.5-72c7b6e');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
