2b7f8eaed8cbf2856a5338e2f1617fc719e9310f
Jason Aden <jasonaden@google.com>
2017-08-30 00:46:47 +0000

    8821723 fix(common): fix XSSI prefix stripping by using JSON.parse always (#18466)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-dfe2bad
+ * @license Angular v4.3.6-8821723
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.6-dfe2bad');
+var VERSION = new Version('4.3.6-8821723');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-dfe2bad
+ * @license Angular v4.3.6-8821723
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -581,7 +581,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.6-dfe2bad');
+var VERSION = new Version('4.3.6-8821723');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
