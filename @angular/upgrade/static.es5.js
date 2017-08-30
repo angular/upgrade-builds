fee844445115aa8f4ce2d3fe6f6b167853dba74a
Jason Aden <jasonaden@google.com>
2017-08-30 00:47:40 +0000

    1b1d5f1 feat(common): accept object map for HttpClient headers & params (#18490)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-65e26d7
+ * @license Angular v5.0.0-beta.5-1b1d5f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -640,7 +640,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.5-65e26d7');
+var VERSION = new Version('5.0.0-beta.5-1b1d5f1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
