4b865b4947a5222d737707a95959b159946757ff
Jason Aden <jasonaden@google.com>
2017-08-31 02:32:25 +0000

    c7e1bda Revert "feat(compiler-cli): lower metadata `useValue` and `data` literal fields (#18905)"

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-2f2d5f3
+ * @license Angular v5.0.0-beta.5-c7e1bda
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -24,7 +24,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.5-2f2d5f3');
+var VERSION = new Version('5.0.0-beta.5-c7e1bda');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
