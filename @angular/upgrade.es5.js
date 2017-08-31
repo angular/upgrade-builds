92dde2baf18274471d150547f1d0ff7be63634db
Jason Aden <jasonaden@google.com>
2017-08-31 18:53:20 +0000

    57abe8d refactor(common): export locale data from closure-locale (#18973)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-cf7d47d
+ * @license Angular v5.0.0-beta.5-57abe8d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -24,7 +24,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.5-cf7d47d');
+var VERSION = new Version('5.0.0-beta.5-57abe8d');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
