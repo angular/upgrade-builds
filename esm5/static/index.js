0327c7b7079fe26ae40eb3247c5ad213a7f1a6a7
Jason Aden <jasonaden@google.com>
2017-09-01 17:35:20 +0000

    56238fe feat(compiler): set `enableLegacyTemplate` to false by default (#18756)

--- esm5/static/index.js
+++ esm5/static/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-fd701b0
+ * @license Angular v5.0.0-beta.5-56238fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -649,7 +649,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.5-fd701b0');
+var VERSION = new Version('5.0.0-beta.5-56238fe');
 
 /**
  * @license
