fcf183340fd51609568b0e2c62fa94f89e268234
Miško Hevery <misko@hevery.com>
2017-09-01 20:27:22 +0000

    6af2396 fix changelog release date (#18969)

--- esm15/index.js
+++ esm15/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-450a13d
+ * @license Angular v5.0.0-beta.5-6af2396
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.5-450a13d');
+const VERSION = new Version('5.0.0-beta.5-6af2396');
 
 /**
  * @fileoverview added by tsickle
--- esm15/static/index.js
+++ esm15/static/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-450a13d
+ * @license Angular v5.0.0-beta.5-6af2396
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -632,7 +632,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.0.0-beta.5-450a13d');
+const VERSION = new Version('5.0.0-beta.5-6af2396');
 
 /**
  * @license
