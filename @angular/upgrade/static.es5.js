f31bd48895f71d1f4a2ecd9a14818b6673e724c5
Miško Hevery <misko@hevery.com>
2017-09-02 04:21:46 +0000

    7d137d7 fix(core): complete EventEmitter in QueryList on component destroy (#18902)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-b8b551c
+ * @license Angular v4.3.6-7d137d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -581,7 +581,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.6-b8b551c');
+var VERSION = new Version('4.3.6-7d137d7');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
