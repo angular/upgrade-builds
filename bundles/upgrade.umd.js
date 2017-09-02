ce02ae01aeec01668fea17fbc0edcc8830e4696d
Misko Hevery <misko@hevery.com>
2017-09-02 16:35:04 +0000

    fa6b802 docs: add changelog for 5.0.0-beta.6

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-6ab4966
+ * @license Angular v5.0.0-beta.6-fa6b802
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.0.0-beta.5-6ab4966
+ * @license Angular v5.0.0-beta.6-fa6b802
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -33,7 +33,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.5-6ab4966');
+var VERSION = new _angular_core.Version('5.0.0-beta.6-fa6b802');
 
 /**
  * @fileoverview added by tsickle
