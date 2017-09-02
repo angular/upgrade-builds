85fb33b76acd9f46c27803a2e392eecb7fa2619d
Miško Hevery <misko@hevery.com>
2017-09-02 05:20:25 +0000

    8036d05 release: cut the 4.4.0-RC.0 release

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-7d137d7
+ * @license Angular v4.4.0-RC.0-8036d05
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -581,7 +581,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.6-7d137d7');
+var VERSION = new Version('4.4.0-RC.0-8036d05');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
