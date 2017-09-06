859b67d6a2fe8e0fff412f2baf87c569e7a9df61
Miško Hevery <misko@hevery.com>
2017-09-06 05:00:19 +0000

    d2707f1 build: cleanup duplicated code now in @bazel/typescript (#19004)

--- esm15/static/index.js
+++ esm15/static/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-15945c8
+ * @license Angular v5.0.0-beta.6-d2707f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -632,7 +632,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.0.0-beta.6-15945c8');
+const VERSION = new Version('5.0.0-beta.6-d2707f1');
 
 /**
  * @license
