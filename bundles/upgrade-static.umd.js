00dcfa4cb1c31f97f39a49d55dd50124ca61e7d8
Miško Hevery <misko@hevery.com>
2017-09-06 17:49:01 +0000

    3c480e4 build(aio): support rendering of constructor overloads in API docs (#19042)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-29762f1
+ * @license Angular v5.0.0-beta.6-3c480e4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowser) { 'use strict';
 
 /**
- * @license Angular v5.0.0-beta.6-29762f1
+ * @license Angular v5.0.0-beta.6-3c480e4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -657,7 +657,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.6-29762f1');
+var VERSION = new _angular_core.Version('5.0.0-beta.6-3c480e4');
 
 /**
  * @license
