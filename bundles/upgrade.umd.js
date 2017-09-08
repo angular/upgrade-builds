9d98a3741b42b51aa3eb56452c922a04613158ef
Matias Niemelä <matias@yearofmoo.com>
2017-09-08 17:38:47 +0000

    a69172f fix(compiler): fix bazel integration and make `perform-compile` more flexible

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-5ef6e63
+ * @license Angular v5.0.0-beta.6-a69172f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.0.0-beta.6-5ef6e63
+ * @license Angular v5.0.0-beta.6-a69172f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -33,7 +33,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.6-5ef6e63');
+var VERSION = new _angular_core.Version('5.0.0-beta.6-a69172f');
 
 /**
  * @fileoverview added by tsickle
