12b3434a6ac33af0cbba1252cba2926964786922
Matias Niemelä <matias@yearofmoo.com>
2017-09-08 21:43:57 +0000

    626555c fix(tsc-wrapped): fix metadata symbol reference

--- esm15/index.js
+++ esm15/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-8f79150
+ * @license Angular v5.0.0-beta.6-626555c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.6-8f79150');
+const VERSION = new Version('5.0.0-beta.6-626555c');
 
 /**
  * @fileoverview added by tsickle
