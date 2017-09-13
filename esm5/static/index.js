7774ed6b788039d43353340c6719c348607a706d
Matias Niemelä <matias@yearofmoo.com>
2017-09-13 17:53:34 +0000

    bf94f87 refactor(compiler): use new ngc for i18n (#19095)

--- esm5/static/index.js
+++ esm5/static/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-c8f742e
+ * @license Angular v5.0.0-beta.6-bf94f87
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -654,7 +654,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.6-c8f742e');
+var VERSION = new Version('5.0.0-beta.6-bf94f87');
 
 /**
  * @license
