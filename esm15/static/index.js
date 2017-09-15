2f0e8ce709b9074ec6b69379ad1067029a5a8573
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 23:21:31 +0000

    2373186 docs: update combined changelog for 4.4 stable

--- esm15/static/index.js
+++ esm15/static/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-b6431c6
+ * @license Angular v5.0.0-beta.7-2373186
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -627,7 +627,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.0.0-beta.7-b6431c6');
+const VERSION = new Version('5.0.0-beta.7-2373186');
 
 /**
  * @license
--- esm5/static/index.js
+++ esm5/static/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-b6431c6
+ * @license Angular v5.0.0-beta.7-2373186
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -654,7 +654,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.7-b6431c6');
+var VERSION = new Version('5.0.0-beta.7-2373186');
 
 /**
  * @license
