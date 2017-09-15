06277e8ae20accf2c7f5dd2d37de85c2d235dd33
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 23:18:45 +0000

    0c72f7d docs: update combined changelog for 4.4 stable

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.1-818f4a7
+ * @license Angular v4.4.1-0c72f7d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.1-818f4a7');
+var VERSION = new Version('4.4.1-0c72f7d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
