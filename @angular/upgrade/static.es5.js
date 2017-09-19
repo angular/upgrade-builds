7adf6a00332d0a0b10d8d70ade4eebc8da26c86d
Igor Minar <igor@angularjs.org>
2017-09-19 23:00:10 +0000

    e2e8ba6 release: cut the 4.4.3 release

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.2-0371538
+ * @license Angular v4.4.3-e2e8ba6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.2-0371538');
+var VERSION = new Version('4.4.3-e2e8ba6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
