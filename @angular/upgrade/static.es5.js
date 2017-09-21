5738d669046f4ec0770b41a77f75a7a3c8515329
Igor Minar <igor@angularjs.org>
2017-09-21 00:31:22 +0000

    ae52851 fix(tsc-wrapped): add metadata for `type` declarations (#19040)

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-e2e8ba6
+ * @license Angular v4.4.3-ae52851
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.3-e2e8ba6');
+var VERSION = new Version('4.4.3-ae52851');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
