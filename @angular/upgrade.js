83eba1438986aeeb41f1177828e301eac7b9ae8a
Victor Berchet <victor@suumit.com>
2017-09-25 19:35:14 +0000

    5c99b01 build(aio): run the upload server as a non-previleged user

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-04bc525
+ * @license Angular v4.4.3-5c99b01
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.3-04bc525');
+const VERSION = new Version('4.4.3-5c99b01');
 
 /**
  * @license
