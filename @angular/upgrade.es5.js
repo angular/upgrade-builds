18bf3e641ebf5dc3a2141cae79e3eda2935fb9bf
Victor Berchet <victor@suumit.com>
2017-09-25 21:01:12 +0000

    c7aa8a1 fix(compiler): skip &nbsp; when trimming / removing whitespaces (#19310)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-5c99b01
+ * @license Angular v4.4.3-c7aa8a1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.3-5c99b01');
+var VERSION = new Version('4.4.3-c7aa8a1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
