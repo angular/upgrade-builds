12f66ecb49df1777fa9e6381bd4efba480367451
Victor Berchet <victor@suumit.com>
2017-09-25 19:21:24 +0000

    adb0b76 build(aio): add metadata aliases for directives, components and pipes (#19317)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-0f5c70d
+ * @license Angular v5.0.0-beta.7-adb0b76
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -627,7 +627,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.0.0-beta.7-0f5c70d');
+const VERSION = new Version('5.0.0-beta.7-adb0b76');
 
 /**
  * @license
--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-0f5c70d
+ * @license Angular v5.0.0-beta.7-adb0b76
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -654,7 +654,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.7-0f5c70d');
+var VERSION = new Version('5.0.0-beta.7-adb0b76');
 
 /**
  * @license
