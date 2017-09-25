c13c98842be456557d12d6abd6771bfd42ba459e
Victor Berchet <victor@suumit.com>
2017-09-25 20:56:29 +0000

    bb1665c fix(compiler): make sure to detect paths that start with `rootDir` correctly

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-13613d4
+ * @license Angular v5.0.0-beta.7-bb1665c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -627,7 +627,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.0.0-beta.7-13613d4');
+const VERSION = new Version('5.0.0-beta.7-bb1665c');
 
 /**
  * @license
