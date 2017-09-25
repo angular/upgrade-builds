8b70b8d9098a2fe189094e2db5cc83faed440bef
Victor Berchet <victor@suumit.com>
2017-09-25 20:01:48 +0000

    e224e3d fix(compiler-cli): update ngtools2 EmitFlags (#19375)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-9ad4b3b
+ * @license Angular v5.0.0-beta.7-e224e3d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-9ad4b3b');
+const VERSION = new Version('5.0.0-beta.7-e224e3d');
 
 /**
  * @fileoverview added by tsickle
