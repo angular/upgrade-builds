33c9f2fd08381e1da00406836acf5a8085fef436
Victor Berchet <victor@suumit.com>
2017-09-25 20:03:47 +0000

    4767902 fix(compiler-cli): do not validate metadata from declaration files (#19324)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-e224e3d
+ * @license Angular v5.0.0-beta.7-4767902
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -654,7 +654,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.7-e224e3d');
+var VERSION = new Version('5.0.0-beta.7-4767902');
 
 /**
  * @license
