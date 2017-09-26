e9653a5ef061b1ddf9cd3b7cb90d0722d29d0e31
Victor Berchet <victor@suumit.com>
2017-09-26 19:23:27 +0000

    86ffacf feat(animations): support negative query option values

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-e889c68
+ * @license Angular v5.0.0-beta.7-86ffacf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-e889c68');
+var VERSION = new Version('5.0.0-beta.7-86ffacf');
 
 /**
  * @fileoverview added by tsickle
