7f8bb066b40cdef28dd8885770515d668bea891e
Victor Berchet <victor@suumit.com>
2017-09-26 20:54:56 +0000

    a75040d refactor(compiler): bump metadata version to 4 (#19338)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-86ffacf
+ * @license Angular v5.0.0-beta.7-a75040d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.0.0-beta.7-86ffacf
+ * @license Angular v5.0.0-beta.7-a75040d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -33,7 +33,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.7-86ffacf');
+var VERSION = new _angular_core.Version('5.0.0-beta.7-a75040d');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-86ffacf
+ * @license Angular v5.0.0-beta.7-a75040d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-86ffacf');
+const VERSION = new Version('5.0.0-beta.7-a75040d');
 
 /**
  * @fileoverview added by tsickle
