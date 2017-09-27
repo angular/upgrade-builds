e36743b50d5a5543c5b99cb991822daaaf5ccfc7
Victor Berchet <victor@suumit.com>
2017-09-27 22:44:45 +0000

    334caf8 docs(aio): add copy about NgForOf (#18686)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-48c6ece
+ * @license Angular v5.0.0-beta.7-334caf8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-48c6ece');
+const VERSION = new Version('5.0.0-beta.7-334caf8');
 
 /**
  * @fileoverview added by tsickle
