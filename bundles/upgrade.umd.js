4338b411c79f7f6ed63aab1c36eae73986e8d5ca
Victor Berchet <victor@suumit.com>
2017-09-27 21:05:30 +0000

    48c6ece docs(aio): remove links to top of documents (#16971)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-8d8995f
+ * @license Angular v5.0.0-beta.7-48c6ece
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.0.0-beta.7-8d8995f
+ * @license Angular v5.0.0-beta.7-48c6ece
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -33,7 +33,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.7-8d8995f');
+var VERSION = new _angular_core.Version('5.0.0-beta.7-48c6ece');
 
 /**
  * @fileoverview added by tsickle
