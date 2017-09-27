8ea1803f25d2414eb181b8a00b0ea21486c075b7
Victor Berchet <victor@suumit.com>
2017-09-27 22:53:18 +0000

    0cefb0b docs(aio): add copy about NgForOf (#18686)

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-55a7443
+ * @license Angular v4.4.3-0cefb0b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -588,7 +588,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.3-55a7443');
+var VERSION = new Version('4.4.3-0cefb0b');
 
 /**
  * @license
