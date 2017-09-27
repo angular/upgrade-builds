028c005a58f6de7dcf08870c40cd4a3a6f3483c4
Victor Berchet <victor@suumit.com>
2017-09-27 21:04:37 +0000

    8d8995f docs(aio): edit summary and next step headers (#16962)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-f7c7038
+ * @license Angular v5.0.0-beta.7-8d8995f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowser) { 'use strict';
 
 /**
- * @license Angular v5.0.0-beta.7-f7c7038
+ * @license Angular v5.0.0-beta.7-8d8995f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -662,7 +662,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.7-f7c7038');
+var VERSION = new _angular_core.Version('5.0.0-beta.7-8d8995f');
 
 /**
  * @license
