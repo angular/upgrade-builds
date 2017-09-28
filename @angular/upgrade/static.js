2e9c9e6485fd08c3b9d10c9dec1bd91ffc91f39e
Victor Berchet <victor@suumit.com>
2017-09-28 19:44:50 +0000

    d489ad5 docs: add changelog for 4.4.4

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-7fc2dce
+ * @license Angular v4.4.4-d489ad5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -588,7 +588,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.3-7fc2dce');
+var VERSION = new Version('4.4.4-d489ad5');
 
 /**
  * @license
