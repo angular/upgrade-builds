cb54570406919ef20b498c94b69c6604ad566a9c
Victor Berchet <victor@suumit.com>
2017-09-28 18:13:08 +0000

    ff5b050 fix(compiler): correctly map error message locations (#19424)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-2b84b86
+ * @license Angular v5.0.0-beta.7-ff5b050
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-2b84b86');
+var VERSION = new Version('5.0.0-beta.7-ff5b050');
 
 /**
  * @fileoverview added by tsickle
