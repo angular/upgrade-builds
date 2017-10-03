625da4a2a26c94e5a7a201d6ee4594299105c1bc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-03 15:43:46 +0000

    e3a4ece docs(aio): Fix incorrect wording

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-9783c92
+ * @license Angular v5.0.0-rc.0-e3a4ece
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.0.0-rc.0-9783c92
+ * @license Angular v5.0.0-rc.0-e3a4ece
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -33,7 +33,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.0-9783c92');
+var VERSION = new _angular_core.Version('5.0.0-rc.0-e3a4ece');
 
 /**
  * @fileoverview added by tsickle
--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-9783c92
+ * @license Angular v5.0.0-rc.0-e3a4ece
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.0-9783c92');
+var VERSION = new Version('5.0.0-rc.0-e3a4ece');
 
 /**
  * @license
