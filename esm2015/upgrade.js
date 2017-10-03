5d41750e46ff373a329e49dedbd9177422861964
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-03 15:28:29 +0000

    7cecaf2 docs(aio): add ngVikings to the events page

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-eef7d8a
+ * @license Angular v5.0.0-rc.0-7cecaf2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.0-eef7d8a');
+const VERSION = new Version('5.0.0-rc.0-7cecaf2');
 
 /**
  * @fileoverview added by tsickle
