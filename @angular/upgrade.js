67f266845acbf75442e2ecb1f16bcb5bf65ea8bf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-03 16:04:54 +0000

    544a7ad docs(aio): Fix incorrect wording

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-d489ad5
+ * @license Angular v4.4.4-544a7ad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.4-d489ad5');
+const VERSION = new Version('4.4.4-544a7ad');
 
 /**
  * @license
