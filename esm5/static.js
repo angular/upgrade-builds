d7074e81a324611644f91aae738b6ca76aaf7a15
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 22:21:25 +0000

    696af79 fix(compiler): properly work on windows

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-d4d9009
+ * @license Angular v5.0.0-rc.0-696af79
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.0-d4d9009');
+var VERSION = new Version('5.0.0-rc.0-696af79');
 
 /**
  * @license
