c34b3c32ae3ad36419da874640a8daf8d351c19c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 22:27:59 +0000

    c80e02b docs(aio): document Custom Async validators

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-fbc9537
+ * @license Angular v5.0.0-rc.0-c80e02b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.0-fbc9537');
+var VERSION = new Version('5.0.0-rc.0-c80e02b');
 
 /**
  * @license
