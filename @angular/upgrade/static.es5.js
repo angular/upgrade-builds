6b587c655a5c652c1e09355e4bfd8e792574e39e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 17:42:13 +0000

    df91fd0 build: update npm dependencies

--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-544a7ad
+ * @license Angular v4.4.4-df91fd0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.4-544a7ad');
+var VERSION = new Version('4.4.4-df91fd0');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
