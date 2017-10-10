b3f7089d28ada132f445bccb62d434731114d9f1
Chuck Jazdzewski <chuckj@google.com>
2017-10-10 21:01:00 +0000

    ac50bd6 fix(animations): ensure inner :leave animations do not remove node when skipped (#19532)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-931cf78
+ * @license Angular v5.0.0-rc.1-ac50bd6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.1-931cf78');
+var VERSION = new Version('5.0.0-rc.1-ac50bd6');
 
 /**
  * @fileoverview added by tsickle
