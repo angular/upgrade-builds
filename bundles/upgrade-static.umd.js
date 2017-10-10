89ce9a0300729acdbacb42165d5cadf302369748
Chuck Jazdzewski <chuckj@google.com>
2017-10-10 23:19:37 +0000

    f89ac92 test(aio): temporarily disable failing examples e2e tests (#19600)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-ccc25ee
+ * @license Angular v4.4.4-f89ac92
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core) { 'use strict';
 
 /**
- * @license Angular v4.4.4-ccc25ee
+ * @license Angular v4.4.4-f89ac92
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -585,7 +585,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.4.4-ccc25ee');
+var VERSION = new _angular_core.Version('4.4.4-f89ac92');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
