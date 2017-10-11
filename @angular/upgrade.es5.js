737e52f0c51cadf87df0571c9454f928745721d9
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 21:59:23 +0000

    3e3f918 ci: freeze yarn lockfile when installing dependencies (#19616)

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-734378c
+ * @license Angular v4.4.4-3e3f918
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.4-734378c');
+var VERSION = new Version('4.4.4-3e3f918');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
