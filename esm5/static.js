169a83b32627dae8eba91c3f7bb2d8251d62a85b
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 19:19:46 +0000

    566606e ci: freeze yarn lockfile when installing dependencies (#19616)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-46992b4
+ * @license Angular v5.0.0-rc.1-566606e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.1-46992b4');
+var VERSION = new Version('5.0.0-rc.1-566606e');
 
 /**
  * @license
--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-46992b4
+ * @license Angular v5.0.0-rc.1-566606e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.1-46992b4');
+var VERSION = new Version('5.0.0-rc.1-566606e');
 
 /**
  * @fileoverview added by tsickle
