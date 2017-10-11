498890d15b90085f38e7317ca953e1f998e3199f
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 00:29:56 +0000

    8b57130 build(aio): append information about links in and out of docs (#19583)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-b0befd7
+ * @license Angular v5.0.0-rc.1-8b57130
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.1-b0befd7');
+var VERSION = new Version('5.0.0-rc.1-8b57130');
 
 /**
  * @license
