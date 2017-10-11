f8d5e4b465ea359105c028a1ab91f04bc9f3fd32
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 20:54:00 +0000

    3232125 build(aio): remove unnecessary `--` from yarn commands (#19565)

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-f89ac92
+ * @license Angular v4.4.4-3232125
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -588,7 +588,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.4-f89ac92');
+var VERSION = new Version('4.4.4-3232125');
 
 /**
  * @license
