cd49604885a28afe553628b8ed7181b85cc31fe6
Chuck Jazdzewski <chuckj@google.com>
2017-10-12 00:15:41 +0000

    41fec31 docs(aio): change Material 2 to Material (#19663)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-a22121d
+ * @license Angular v5.0.0-rc.1-41fec31
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.1-a22121d');
+var VERSION = new Version('5.0.0-rc.1-41fec31');
 
 /**
  * @fileoverview added by tsickle
