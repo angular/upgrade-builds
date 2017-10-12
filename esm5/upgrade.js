7bb61d702b6c924739f68980ec19a65dab944dc8
Chuck Jazdzewski <chuckj@google.com>
2017-10-12 21:24:19 +0000

    522ec9a docs: add changelog for 5.0.0-rc.2

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-94a925a
+ * @license Angular v5.0.0-rc.2-522ec9a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.1-94a925a');
+var VERSION = new Version('5.0.0-rc.2-522ec9a');
 
 /**
  * @fileoverview added by tsickle
