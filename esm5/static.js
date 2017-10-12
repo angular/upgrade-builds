1bf2b04d7c132a17a332632581b4f0cb003b0a57
Chuck Jazdzewski <chuckj@google.com>
2017-10-12 17:10:13 +0000

    055b802 docs(aio): improve the Angular Connect homepage banner (#19684)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-41fec31
+ * @license Angular v5.0.0-rc.1-055b802
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.1-41fec31');
+var VERSION = new Version('5.0.0-rc.1-055b802');
 
 /**
  * @license
