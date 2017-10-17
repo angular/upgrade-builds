ff8f9fd6f9ced8a17b90dd0f39b9b4e65c6e0525
Tobias Bosch <tbosch1009@gmail.com>
2017-10-17 23:30:11 +0000

    01e4aa5 build: remove required BrowserStack run as it fails with “Access denied” (#19769)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-64b3619
+ * @license Angular v5.0.0-rc.2-01e4aa5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.2-64b3619');
+var VERSION = new Version('5.0.0-rc.2-01e4aa5');
 
 /**
  * @fileoverview added by tsickle
