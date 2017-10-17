6347b3b75acf11e5e551e851c0b21697595c04d6
Tobias Bosch <tbosch1009@gmail.com>
2017-10-17 22:26:17 +0000

    af8c2fa build: don’t make BrowserStack required as it fails with “Access denied”

--- @angular/upgrade.js
+++ @angular/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-0789601
+ * @license Angular v4.4.5-af8c2fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.5-0789601');
+const VERSION = new Version('4.4.5-af8c2fa');
 
 /**
  * @license
