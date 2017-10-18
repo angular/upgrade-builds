4d5006620afe49793f08dbf37ac9d59838fef8f5
Tobias Bosch <tbosch1009@gmail.com>
2017-10-18 20:36:45 +0000

    269f5ac fix(common): attempt to JSON.parse errors for JSON responses (#19773)

--- @angular/upgrade/static.js
+++ @angular/upgrade/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-3342a82
+ * @license Angular v4.4.5-269f5ac
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -588,7 +588,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.5-3342a82');
+var VERSION = new Version('4.4.5-269f5ac');
 
 /**
  * @license
