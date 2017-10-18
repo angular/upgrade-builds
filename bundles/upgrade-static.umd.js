b08c4b50e0c95fdcfe6e5e51fddce1a21e32469c
Tobias Bosch <tbosch1009@gmail.com>
2017-10-18 04:17:28 +0000

    5a9ed2d fix(animations): always fire inner trigger callbacks even if blocked by parent animations (#19753)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-01e4aa5
+ * @license Angular v5.0.0-rc.2-5a9ed2d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowser) { 'use strict';
 
 /**
- * @license Angular v5.0.0-rc.2-01e4aa5
+ * @license Angular v5.0.0-rc.2-5a9ed2d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -664,7 +664,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.2-01e4aa5');
+var VERSION = new _angular_core.Version('5.0.0-rc.2-5a9ed2d');
 
 /**
  * @license
