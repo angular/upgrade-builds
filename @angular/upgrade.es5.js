31915a3d36f1d0ad5dc9682baa5a63d3f69b0c24
Tobias Bosch <tbosch1009@gmail.com>
2017-10-19 00:46:55 +0000

    7231f5e docs: add changelog for 4.4.6

--- @angular/upgrade.es5.js
+++ @angular/upgrade.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-269f5ac
+ * @license Angular v4.4.6-7231f5e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.5-269f5ac');
+var VERSION = new Version('4.4.6-7231f5e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/upgrade/static.es5.js
+++ @angular/upgrade/static.es5.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-269f5ac
+ * @license Angular v4.4.6-7231f5e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.5-269f5ac');
+var VERSION = new Version('4.4.6-7231f5e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
