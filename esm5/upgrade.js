d80b0dada24c15e362c50bbdd17d73ce7ce10daf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-20 17:04:16 +0000

    820bb7b revert: ci: use chrome stable (#18307)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-14380ff
+ * @license Angular v5.0.0-rc.3-820bb7b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.3-14380ff');
+var VERSION = new Version('5.0.0-rc.3-820bb7b');
 
 /**
  * @fileoverview added by tsickle
