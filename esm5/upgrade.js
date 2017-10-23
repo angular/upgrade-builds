04e385c0d39631f7bf6fef5cbd0753c71339e457
Matias Niemelä <matias@yearofmoo.com>
2017-10-23 23:06:17 +0000

    5da96c7 fix(bazel): don't console.error from the compile helper (#19879)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-90d1423
+ * @license Angular v5.0.0-rc.3-5da96c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.3-90d1423');
+var VERSION = new Version('5.0.0-rc.3-5da96c7');
 
 /**
  * @fileoverview added by tsickle
