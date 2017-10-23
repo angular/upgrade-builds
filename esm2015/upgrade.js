2711dc129316ad9096aa6c1efcfcc61407deec6e
Matias Niemelä <matias@yearofmoo.com>
2017-10-23 23:58:23 +0000

    6bed189 build(service-worker): set skipTemplateCodegen for build (#19875)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-fc0b1d5
+ * @license Angular v5.0.0-rc.3-6bed189
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.3-fc0b1d5');
+const VERSION = new Version('5.0.0-rc.3-6bed189');
 
 /**
  * @fileoverview added by tsickle
