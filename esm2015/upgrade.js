a389c5d2c5a1b41ea6a556308950cfe9fa59041c
Matias Niemelä <matias@yearofmoo.com>
2017-10-24 07:10:57 +0000

    b51d57d docs: add changelog for 5.0.0-rc.5

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.4-a0ae120
+ * @license Angular v5.0.0-rc.5-b51d57d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.4-a0ae120');
+const VERSION = new Version('5.0.0-rc.5-b51d57d');
 
 /**
  * @fileoverview added by tsickle
