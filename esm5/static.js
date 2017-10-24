575f79513acd437aa2441578c775093218967b9d
Matias Niemelä <matias@yearofmoo.com>
2017-10-24 22:19:48 +0000

    7bfeac7 fix(compiler-cli): only use error collector when needed. (#19912)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.5-b51d57d
+ * @license Angular v5.0.0-rc.5-7bfeac7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.5-b51d57d');
+var VERSION = new Version('5.0.0-rc.5-7bfeac7');
 
 /**
  * @license
