950e29498bd783f9d613effb420efe3a0edc2e04
Matias Niemelä <matias@yearofmoo.com>
2017-10-25 20:02:16 +0000

    fde5f2f build: update to rxjs@5.5.2 (#19931)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.5-d567246
+ * @license Angular v5.0.0-rc.5-fde5f2f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.5-d567246');
+var VERSION = new Version('5.0.0-rc.5-fde5f2f');
 
 /**
  * @license
