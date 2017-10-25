6a1143fce4b8ed2ac200d51e825eed17a4e2c83f
Matias Niemelä <matias@yearofmoo.com>
2017-10-25 20:11:23 +0000

    680bcf7 build: update to rxjs@5.5.2 (#19931)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.5-ef08330
+ * @license Angular v5.0.0-rc.5-680bcf7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.5-ef08330');
+var VERSION = new Version('5.0.0-rc.5-680bcf7');
 
 /**
  * @fileoverview added by tsickle
