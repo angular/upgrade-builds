18933b168255330fdd36479679e9713ef512e8ef
Matias Niemelä <matias@yearofmoo.com>
2017-10-26 20:43:55 +0000

    47caebf fix(service-worker): don't block initialization on registration (#19936)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.6-47bc6f1
+ * @license Angular v5.0.0-rc.6-47caebf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.6-47bc6f1');
+var VERSION = new Version('5.0.0-rc.6-47caebf');
 
 /**
  * @license
