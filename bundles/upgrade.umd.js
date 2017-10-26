0fe8501f0c02e15b2155b9e2f4be26ac1a044478
Matias Niemelä <matias@yearofmoo.com>
2017-10-26 22:45:47 +0000

    eca822b fix(service-worker): fix improper call of Observable.merge (#19962)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.6-17142a7
+ * @license Angular v5.0.0-rc.6-eca822b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.0.0-rc.6-17142a7
+ * @license Angular v5.0.0-rc.6-eca822b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -33,7 +33,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.6-17142a7');
+var VERSION = new _angular_core.Version('5.0.0-rc.6-eca822b');
 
 /**
  * @fileoverview added by tsickle
--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.6-17142a7
+ * @license Angular v5.0.0-rc.6-eca822b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.6-17142a7');
+var VERSION = new Version('5.0.0-rc.6-eca822b');
 
 /**
  * @fileoverview added by tsickle
