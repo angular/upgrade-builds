6b224452985868193f4b821e558ba6f48c195dae
Matias Niemelä <matias@yearofmoo.com>
2017-10-27 02:29:16 +0000

    f4d5729 fix(compiler): make watch mode work on windows (#19953)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.6-14016c7
+ * @license Angular v5.0.0-rc.6-f4d5729
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.6-14016c7');
+var VERSION = new Version('5.0.0-rc.6-f4d5729');
 
 /**
  * @fileoverview added by tsickle
