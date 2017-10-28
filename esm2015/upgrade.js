833666b2240a21ae5ac4a08df131553b47c34d73
Matias Niemelä <matias@yearofmoo.com>
2017-10-28 07:08:24 +0000

    85e95cc docs: add changelog for 5.0.0-rc.8

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.7-a01c877
+ * @license Angular v5.0.0-rc.8-85e95cc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.7-a01c877');
+const VERSION = new Version('5.0.0-rc.8-85e95cc');
 
 /**
  * @fileoverview added by tsickle
