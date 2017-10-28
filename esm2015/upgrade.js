f3c25859b516f8dff35b9119a39d494193bfdcf5
Matias Niemelä <matias@yearofmoo.com>
2017-10-28 06:08:53 +0000

    a01c877 docs: ensure examples get rxjs ^5.5.0 (#19985)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.7-5542517
+ * @license Angular v5.0.0-rc.7-a01c877
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.7-5542517');
+const VERSION = new Version('5.0.0-rc.7-a01c877');
 
 /**
  * @fileoverview added by tsickle
