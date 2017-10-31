a9259c8ffc7835dec2f53e10ffb0f04d94e961c8
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 03:33:35 +0000

    13f8648 fix: add missing globals from each rollup configuration (#20028)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-54480f7
+ * @license Angular v5.0.0-rc.8-13f8648
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.8-54480f7');
+const VERSION = new Version('5.0.0-rc.8-13f8648');
 
 /**
  * @fileoverview added by tsickle
--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-54480f7
+ * @license Angular v5.0.0-rc.8-13f8648
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-rc.8-54480f7');
+var VERSION = new Version('5.0.0-rc.8-13f8648');
 
 /**
  * @license
