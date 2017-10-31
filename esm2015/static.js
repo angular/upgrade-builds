a3b8963c765fce6ca56ffc3a3b9de3fe0991d48b
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 01:44:50 +0000

    bf57df3 build: temporarily increase the commit msg limit to 120

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-951bd33
+ * @license Angular v5.0.0-rc.8-bf57df3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -629,7 +629,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.0.0-rc.8-951bd33');
+const VERSION = new Version('5.0.0-rc.8-bf57df3');
 
 /**
  * @license
