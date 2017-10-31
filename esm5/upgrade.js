b2c71f850d945c520722d93824bf846cfb200008
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 04:07:55 +0000

    3bdbb18 build: update rollup lint rule from bad merge (#20047)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-13f8648
+ * @license Angular v5.0.0-rc.8-3bdbb18
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.8-13f8648');
+var VERSION = new Version('5.0.0-rc.8-3bdbb18');
 
 /**
  * @fileoverview added by tsickle
