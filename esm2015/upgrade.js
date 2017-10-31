2e69d58e13df5e860b1412f318b45d47edc9282d
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 00:52:37 +0000

    04eb80c fix(compiler): always use relative paths to refer to generated code

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-308fc8e
+ * @license Angular v5.0.0-rc.8-04eb80c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.8-308fc8e');
+const VERSION = new Version('5.0.0-rc.8-04eb80c');
 
 /**
  * @fileoverview added by tsickle
