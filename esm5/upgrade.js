1145bd1dc830c29c05c4a5a59c524d4bca9971f7
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 03:27:43 +0000

    de24d54 fix(compiler): report errors properly in G3 in certain conditions (#20041)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-c295aee
+ * @license Angular v5.0.0-rc.8-de24d54
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.8-c295aee');
+var VERSION = new Version('5.0.0-rc.8-de24d54');
 
 /**
  * @fileoverview added by tsickle
