cf1c61df3272a9d0e46bffddf2bea540cd4fe68f
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 03:31:45 +0000

    812786f fix: add missing globals from each rollup configuration (#20028)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-de24d54
+ * @license Angular v5.0.0-rc.8-812786f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowser) { 'use strict';
 
 /**
- * @license Angular v5.0.0-rc.8-de24d54
+ * @license Angular v5.0.0-rc.8-812786f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -664,7 +664,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.8-de24d54');
+var VERSION = new _angular_core.Version('5.0.0-rc.8-812786f');
 
 /**
  * @license
