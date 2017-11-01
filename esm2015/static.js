ea67f7995c485a3313c89554a8de023071330e8d
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 22:53:36 +0000

    280dada docs(aio): fix typo in attribute-directives

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-c30eff8
+ * @license Angular v5.0.0-280dada
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -629,7 +629,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.0.0-c30eff8');
+const VERSION = new Version('5.0.0-280dada');
 
 /**
  * @license
--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-c30eff8
+ * @license Angular v5.0.0-280dada
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-c30eff8');
+var VERSION = new Version('5.0.0-280dada');
 
 /**
  * @license
