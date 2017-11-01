83dba2c90cf36d72ff000a0bdefb48be8f7f7020
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 21:46:29 +0000

    ca129ba docs(aio): upgrade to v5 and cli 1.5

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-171ae15
+ * @license Angular v5.0.0-ca129ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -629,7 +629,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.0.0-171ae15');
+const VERSION = new Version('5.0.0-ca129ba');
 
 /**
  * @license
--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-171ae15
+ * @license Angular v5.0.0-ca129ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-171ae15');
+var VERSION = new Version('5.0.0-ca129ba');
 
 /**
  * @license
