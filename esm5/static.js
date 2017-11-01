e46613d60402f9e091dffc3e8d0b686af700e54d
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 22:54:22 +0000

    a1ec65b docs(aio): fix typo in attribute-directives

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-851e1cf
+ * @license Angular v5.0.0-a1ec65b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-851e1cf');
+var VERSION = new Version('5.0.0-a1ec65b');
 
 /**
  * @license
