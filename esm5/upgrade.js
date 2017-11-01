c61a465c10ee6fc46a38b71e7a617ea6402e5a45
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 23:03:46 +0000

    27ae0f9 build(aio): upgrade to @angular/material@2.0.0-beta.12 (#19702)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-280dada
+ * @license Angular v5.0.0-27ae0f9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-280dada');
+var VERSION = new Version('5.0.0-27ae0f9');
 
 /**
  * @fileoverview added by tsickle
