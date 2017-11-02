af5206fc442cddfb94a92143863f5a6df081fad3
Victor Berchet <victor@suumit.com>
2017-11-02 22:40:00 +0000

    068348e build(aio): do not fail the initial doc-gen when running docs-watch

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-5b16ce9
+ * @license Angular v5.1.0-beta.0-068348e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-beta.0-5b16ce9');
+var VERSION = new Version('5.1.0-beta.0-068348e');
 
 /**
  * @license
--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-5b16ce9
+ * @license Angular v5.1.0-beta.0-068348e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-5b16ce9');
+var VERSION = new Version('5.1.0-beta.0-068348e');
 
 /**
  * @fileoverview added by tsickle
