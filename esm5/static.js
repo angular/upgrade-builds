05e14760a987277278a35c728041883eda666f47
Victor Berchet <victor@suumit.com>
2017-11-02 23:23:45 +0000

    65d57a0 build(aio): avoid building before running the local PWA tests

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-9975486
+ * @license Angular v5.1.0-beta.0-65d57a0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-beta.0-9975486');
+var VERSION = new Version('5.1.0-beta.0-65d57a0');
 
 /**
  * @license
