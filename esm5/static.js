de7d5099722102e6a6495b4bf92c03d8fa2b605b
Victor Berchet <victor@suumit.com>
2017-11-02 23:16:44 +0000

    37a740b build: remove local yarn (#19981)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-067e926
+ * @license Angular v5.0.0-37a740b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-067e926');
+var VERSION = new Version('5.0.0-37a740b');
 
 /**
  * @license
