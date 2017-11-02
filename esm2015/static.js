09fca65d4d048d91d4ea83bd67c56e720fcd2891
Victor Berchet <victor@suumit.com>
2017-11-02 22:28:24 +0000

    8639995 fix(compiler-cli): don't report emit diagnostics when --noEmitOnError is off (#20063)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-d550b68
+ * @license Angular v5.0.0-8639995
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -629,7 +629,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.0.0-d550b68');
+const VERSION = new Version('5.0.0-8639995');
 
 /**
  * @license
