43afcc065a13956f87c34110fb1e5dfefe8669d3
Igor Minar <igor@angular.io>
2017-11-02 00:40:22 +0000

    18197bd docs(aio): fix linting for universal (#20086)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-c75740e
+ * @license Angular v5.0.0-18197bd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -629,7 +629,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.0.0-c75740e');
+const VERSION = new Version('5.0.0-18197bd');
 
 /**
  * @license
