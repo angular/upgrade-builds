cb15c232edd0a9461c572bc9ab497614ed9e9bb7
Victor Berchet <victor@suumit.com>
2017-11-02 21:45:46 +0000

    132c071 docs(aio): updated i18n guide and example (#19975)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-3db7112
+ * @license Angular v5.1.0-beta.0-132c071
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-3db7112');
+var VERSION = new Version('5.1.0-beta.0-132c071');
 
 /**
  * @fileoverview added by tsickle
