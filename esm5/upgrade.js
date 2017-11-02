1c5193dc873daf5441b4547c80038b87704b85af
Victor Berchet <victor@suumit.com>
2017-11-02 23:28:52 +0000

    25843fe build(aio): avoid building before running the local PWA tests

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-37a740b
+ * @license Angular v5.0.0-25843fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-37a740b');
+var VERSION = new Version('5.0.0-25843fe');
 
 /**
  * @fileoverview added by tsickle
