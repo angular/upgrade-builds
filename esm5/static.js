4960c9655dd4f9e0a9931cc7c216c3e9605f42a9
Victor Berchet <victor@suumit.com>
2017-11-02 23:43:48 +0000

    678d1cf fix(core): __symbol__ should return __zone_symbol__ without zone.js loaded (#19541)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-25843fe
+ * @license Angular v5.0.0-678d1cf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-25843fe');
+var VERSION = new Version('5.0.0-678d1cf');
 
 /**
  * @license
