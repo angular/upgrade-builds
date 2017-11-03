c2bf359f9594765f40a67985164f9e0d8443542f
Victor Berchet <victor@suumit.com>
2017-11-03 19:35:25 +0000

    567cc26 docs: Component Styles guide for CLI (#19791)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-169cedd
+ * @license Angular v5.1.0-beta.0-567cc26
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-beta.0-169cedd');
+var VERSION = new Version('5.1.0-beta.0-567cc26');
 
 /**
  * @license
