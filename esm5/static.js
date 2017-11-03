a56a949fb23c723e27fbcb82a15b22b990fbb135
Victor Berchet <victor@suumit.com>
2017-11-03 19:03:51 +0000

    e6f16a7 docs(aio): Fix typo in Architecture Overview. (#20073)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-678d1cf
+ * @license Angular v5.0.0-e6f16a7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-678d1cf');
+var VERSION = new Version('5.0.0-e6f16a7');
 
 /**
  * @license
