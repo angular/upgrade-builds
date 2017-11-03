e5f73f90e9e74bed1e405e9e1c98eab16c182a67
Victor Berchet <victor@suumit.com>
2017-11-03 23:14:48 +0000

    3997d97 revert: feat(elements): implement `@angular/elements` #19469 (#20152)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-200d92d
+ * @license Angular v5.1.0-beta.0-3997d97
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-beta.0-200d92d');
+var VERSION = new Version('5.1.0-beta.0-3997d97');
 
 /**
  * @license
