6e68fef956d93c7f5d351f769be09ff02cf21b6f
Victor Berchet <victor@suumit.com>
2017-11-03 22:55:14 +0000

    7083791 fix(core): should support event.stopImmediatePropagation (#19222)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-e4cd3b0
+ * @license Angular v5.0.0-7083791
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-e4cd3b0');
+const VERSION = new Version('5.0.0-7083791');
 
 /**
  * @fileoverview added by tsickle
