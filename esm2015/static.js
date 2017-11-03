c796969525d683c37fa7b0731a7f5f7a4f782212
Victor Berchet <victor@suumit.com>
2017-11-03 22:41:49 +0000

    200d92d fix(core): should support event.stopImmediatePropagation (#19222)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-dbec3ca
+ * @license Angular v5.1.0-beta.0-200d92d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -629,7 +629,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.0-beta.0-dbec3ca');
+const VERSION = new Version('5.1.0-beta.0-200d92d');
 
 /**
  * @license
--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-dbec3ca
+ * @license Angular v5.1.0-beta.0-200d92d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-dbec3ca');
+var VERSION = new Version('5.1.0-beta.0-200d92d');
 
 /**
  * @fileoverview added by tsickle
