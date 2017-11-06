432e0779102004b6e9a7db8c71c048b82943c9b3
Victor Berchet <victor@suumit.com>
2017-11-06 19:51:31 +0000

    b489259 docs(common): fix mis-ordered lines (#20221)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-6b74883
+ * @license Angular v5.1.0-beta.0-b489259
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-6b74883');
+var VERSION = new Version('5.1.0-beta.0-b489259');
 
 /**
  * @fileoverview added by tsickle
