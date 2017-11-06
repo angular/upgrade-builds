ca9576acc3ea23de58fca3822e66910c90bc5be2
Victor Berchet <victor@suumit.com>
2017-11-06 18:33:37 +0000

    049c896 docs(aio): update ToH for CLI (#19811)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-8802016
+ * @license Angular v5.1.0-beta.0-049c896
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-8802016');
+const VERSION = new Version('5.1.0-beta.0-049c896');
 
 /**
  * @fileoverview added by tsickle
