9b684cc7123fe7441eaf8d400acd331a92ae8179
Victor Berchet <victor@suumit.com>
2017-11-08 19:24:26 +0000

    9de45fa docs: add changelog for 5.0.1

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-511e56d
+ * @license Angular v5.0.1-9de45fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-511e56d');
+var VERSION = new Version('5.0.1-9de45fa');
 
 /**
  * @fileoverview added by tsickle
