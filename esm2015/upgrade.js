a4be4df4c8fbe2a602942108de87483f3b879b19
Victor Berchet <victor@suumit.com>
2017-11-08 22:37:24 +0000

    78ba39b docs: add changelog for 5.1.0-beta.0

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-119034c
+ * @license Angular v5.1.0-beta.0-78ba39b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-119034c');
+const VERSION = new Version('5.1.0-beta.0-78ba39b');
 
 /**
  * @fileoverview added by tsickle
--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-119034c
+ * @license Angular v5.1.0-beta.0-78ba39b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-beta.0-119034c');
+var VERSION = new Version('5.1.0-beta.0-78ba39b');
 
 /**
  * @license
