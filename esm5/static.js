bf13c9b8d4b9776e40a1c70e76e996044817544a
Victor Berchet <victor@suumit.com>
2017-11-09 00:15:27 +0000

    9bcd709 test(compiler): do not use `a` as a content selector

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-78ba39b
+ * @license Angular v5.1.0-beta.0-9bcd709
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-beta.0-78ba39b');
+var VERSION = new Version('5.1.0-beta.0-9bcd709');
 
 /**
  * @license
