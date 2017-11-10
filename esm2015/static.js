721f195c1f78ab251b9972720ef48ee268dacb2c
Jason Aden <jasonaden@google.com>
2017-11-10 19:32:51 +0000

    7830d74 docs(aio): empty line between HTML tag and content (#20341) PR Close #20341

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-3257fcd
+ * @license Angular v5.1.0-beta.0-7830d74
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -629,7 +629,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.0-beta.0-3257fcd');
+const VERSION = new Version('5.1.0-beta.0-7830d74');
 
 /**
  * @license
--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-3257fcd
+ * @license Angular v5.1.0-beta.0-7830d74
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-3257fcd');
+var VERSION = new Version('5.1.0-beta.0-7830d74');
 
 /**
  * @fileoverview added by tsickle
