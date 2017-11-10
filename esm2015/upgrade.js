1e341a47af9a059ee24604b0aa17527f574d4b93
Jason Aden <jasonaden@google.com>
2017-11-10 19:35:51 +0000

    fb4b90a docs(aio): typo fix (#20318)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-7830d74
+ * @license Angular v5.1.0-beta.0-fb4b90a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-7830d74');
+const VERSION = new Version('5.1.0-beta.0-fb4b90a');
 
 /**
  * @fileoverview added by tsickle
