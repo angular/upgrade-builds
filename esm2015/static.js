3c01428a2b30a5b57cdfc23081e2c5d7b8607686
Jason Aden <jasonaden@google.com>
2017-11-10 00:24:52 +0000

    424a323 fix(compiler): show explanatory text in template errors (#20313)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-28985cb
+ * @license Angular v5.0.1-424a323
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -629,7 +629,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.0.1-28985cb');
+const VERSION = new Version('5.0.1-424a323');
 
 /**
  * @license
--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-28985cb
+ * @license Angular v5.0.1-424a323
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.1-28985cb');
+const VERSION = new Version('5.0.1-424a323');
 
 /**
  * @fileoverview added by tsickle
