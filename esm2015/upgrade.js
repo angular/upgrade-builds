f1c8c6aac35893a79a162088131c740dedf9af74
Jason Aden <jasonaden@google.com>
2017-11-14 22:06:38 +0000

    12af6d3 docs(core): change from deprecated renderer to renderer2 (#19920)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-b9bd320
+ * @license Angular v5.1.0-beta.0-12af6d3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-b9bd320');
+const VERSION = new Version('5.1.0-beta.0-12af6d3');
 
 /**
  * @fileoverview added by tsickle
