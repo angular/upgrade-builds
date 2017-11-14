f0df71c01e826e8d27b1a42bd7feea2b4e49ee50
Jason Aden <jasonaden@google.com>
2017-11-14 22:07:26 +0000

    20aafff fix(animations): ensure final state() styles are applied within @.disabled animations (#20267)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-12af6d3
+ * @license Angular v5.1.0-beta.0-20aafff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-12af6d3');
+const VERSION = new Version('5.1.0-beta.0-20aafff');
 
 /**
  * @fileoverview added by tsickle
