740d9173586287135691094e4994eb86f7839c8f
Jason Aden <jasonaden@google.com>
2017-11-15 00:45:33 +0000

    d47b2a6 fix(animations): always fire inner trigger callbacks even if blocked by parent animations (#19753)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-20aafff
+ * @license Angular v5.1.0-beta.0-d47b2a6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -629,7 +629,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.0-beta.0-20aafff');
+const VERSION = new Version('5.1.0-beta.0-d47b2a6');
 
 /**
  * @license
