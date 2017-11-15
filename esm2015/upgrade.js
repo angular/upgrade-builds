46e6f151c9b9a48b81fc547841395160c0e5490d
Jason Aden <jasonaden@google.com>
2017-11-15 20:21:23 +0000

    043e408 style(core): fix comment format for linter

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-d47b2a6
+ * @license Angular v5.1.0-beta.0-043e408
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-d47b2a6');
+const VERSION = new Version('5.1.0-beta.0-043e408');
 
 /**
  * @fileoverview added by tsickle
