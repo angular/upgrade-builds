2614d0a4f127c14c8ce7e7ad88d9a8313149ae91
Miško Hevery <misko@hevery.com>
2017-11-16 04:51:09 +0000

    1285792 fix(aio): fix window title on Home page (#20440)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-2d6126e
+ * @license Angular v5.0.1-1285792
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -629,7 +629,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.0.1-2d6126e');
+const VERSION = new Version('5.0.1-1285792');
 
 /**
  * @license
--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-2d6126e
+ * @license Angular v5.0.1-1285792
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.1-2d6126e');
+var VERSION = new Version('5.0.1-1285792');
 
 /**
  * @fileoverview added by tsickle
