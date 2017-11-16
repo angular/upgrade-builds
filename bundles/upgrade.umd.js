62c23326395e2c777df9a4ac1fa4c4ee24591229
Miško Hevery <misko@hevery.com>
2017-11-16 01:01:43 +0000

    f8658cd Revert "fix(animations): always fire inner trigger callbacks even if blocked by parent animations (#19753)"

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-043e408
+ * @license Angular v5.1.0-beta.0-f8658cd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.1.0-beta.0-043e408
+ * @license Angular v5.1.0-beta.0-f8658cd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -33,7 +33,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-043e408');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-f8658cd');
 
 /**
  * @fileoverview added by tsickle
--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-043e408
+ * @license Angular v5.1.0-beta.0-f8658cd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-043e408');
+var VERSION = new Version('5.1.0-beta.0-f8658cd');
 
 /**
  * @fileoverview added by tsickle
