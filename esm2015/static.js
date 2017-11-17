ec636bfde06f6d8207d086304f68d58bfc2598cc
Miško Hevery <misko@hevery.com>
2017-11-17 22:10:25 +0000

    365712e docs(aio): fix typo in Attribute Directives documentation (#20143)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-1861e41
+ * @license Angular v5.1.0-beta.1-365712e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -629,7 +629,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.0-beta.1-1861e41');
+const VERSION = new Version('5.1.0-beta.1-365712e');
 
 /**
  * @license
--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-1861e41
+ * @license Angular v5.1.0-beta.1-365712e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.1-1861e41');
+const VERSION = new Version('5.1.0-beta.1-365712e');
 
 /**
  * @fileoverview added by tsickle
