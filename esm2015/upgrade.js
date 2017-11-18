eea5e8c8c505703314600dc60678c2ea40719988
Miško Hevery <misko@hevery.com>
2017-11-18 00:37:56 +0000

    69c53c3 fix(forms): updateOn should check if change occurred (#20358)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-365712e
+ * @license Angular v5.1.0-beta.1-69c53c3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.1-365712e');
+const VERSION = new Version('5.1.0-beta.1-69c53c3');
 
 /**
  * @fileoverview added by tsickle
