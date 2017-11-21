0b7595ffdb69cf58caf85e6633585d17494e58f7
Miško Hevery <misko@hevery.com>
2017-11-21 05:27:13 +0000

    2a9d2ba docs: update changelog with missing feature in 5.1.0-beta.1 (#20508)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-69c53c3
+ * @license Angular v5.1.0-beta.1-2a9d2ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.1-69c53c3');
+var VERSION = new Version('5.1.0-beta.1-2a9d2ba');
 
 /**
  * @fileoverview added by tsickle
