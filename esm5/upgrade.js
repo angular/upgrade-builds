39ef2e8dcbbc33ac234847fbba813faff6bf7e59
Miško Hevery <misko@hevery.com>
2017-11-22 17:11:26 +0000

    efce396 docs(aio): add a French onsite training ressource (#19889) PR Close #19889

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.2-b53ead4
+ * @license Angular v5.0.2-efce396
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.2-b53ead4');
+var VERSION = new Version('5.0.2-efce396');
 
 /**
  * @fileoverview added by tsickle
