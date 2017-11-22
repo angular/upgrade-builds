4156fdd2e730cd9e09a7d3c64e925c5d650671b5
Miško Hevery <misko@hevery.com>
2017-11-22 20:13:08 +0000

    0444e13 docs(aio): add ngATL header (#20590)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-ffb6dbe
+ * @license Angular v5.1.0-beta.1-0444e13
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.1-ffb6dbe');
+var VERSION = new Version('5.1.0-beta.1-0444e13');
 
 /**
  * @fileoverview added by tsickle
