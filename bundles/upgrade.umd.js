71ad156643906eb337632fb5145c778df719e591
Miško Hevery <misko@hevery.com>
2017-11-28 01:14:34 +0000

    24bf3e2 feat(common): add locale id parameter to `registerLocaleData` (#20623)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-a53a040
+ * @license Angular v5.1.0-beta.2-24bf3e2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.1.0-beta.2-a53a040
+ * @license Angular v5.1.0-beta.2-24bf3e2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -33,7 +33,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.2-a53a040');
+var VERSION = new _angular_core.Version('5.1.0-beta.2-24bf3e2');
 
 /**
  * @fileoverview added by tsickle
--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-a53a040
+ * @license Angular v5.1.0-beta.2-24bf3e2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.2-a53a040');
+var VERSION = new Version('5.1.0-beta.2-24bf3e2');
 
 /**
  * @fileoverview added by tsickle
