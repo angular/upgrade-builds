10755463bc32bc6978c43855d48241c4d5a602c3
Miško Hevery <misko@hevery.com>
2017-11-29 18:31:05 +0000

    65a2cb8 fix(core): should use native addEventListener in ngZone (#20672)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-c2b3792
+ * @license Angular v5.1.0-beta.2-65a2cb8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -629,7 +629,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.0-beta.2-c2b3792');
+const VERSION = new Version('5.1.0-beta.2-65a2cb8');
 
 /**
  * @license
