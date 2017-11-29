43054b2eb06b7b3d3b8a5c945cdbf7cd5feb556b
Miško Hevery <misko@hevery.com>
2017-11-29 22:48:04 +0000

    647ca64 docs(aio): Updating with Ignite UI for Angular (#20663)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.3-c7b211c
+ * @license Angular v5.0.3-647ca64
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.3-c7b211c');
+const VERSION = new Version('5.0.3-647ca64');
 
 /**
  * @fileoverview added by tsickle
