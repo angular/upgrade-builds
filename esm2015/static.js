2f6edf9dd3ac17f78b0af3dfe6abe8fcdfb7f3c0
Miško Hevery <misko@hevery.com>
2017-11-30 04:00:09 +0000

    b450c83 docs(aio): remove services plurality (#20696)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.3-647ca64
+ * @license Angular v5.0.3-b450c83
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -629,7 +629,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.0.3-647ca64');
+const VERSION = new Version('5.0.3-b450c83');
 
 /**
  * @license
