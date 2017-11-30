60af310517cc2221fba1d066ad608ade6ba96635
Misko Hevery <misko@hevery.com>
2017-11-30 04:47:45 +0000

    7a2550a styl: broken build due to missing new lines

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-b9e4d62
+ * @license Angular v5.1.0-beta.2-7a2550a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.2-b9e4d62');
+var VERSION = new Version('5.1.0-beta.2-7a2550a');
 
 /**
  * @fileoverview added by tsickle
