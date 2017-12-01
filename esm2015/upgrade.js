d295efb85b2f8a0633c02adc09b5214f5ce33ecc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-01 23:40:27 +0000

    9dc310e docs: add changelog for 5.0.5

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.4-445d833
+ * @license Angular v5.0.5-9dc310e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.4-445d833');
+const VERSION = new Version('5.0.5-9dc310e');
 
 /**
  * @fileoverview added by tsickle
