8eb62a9868a726219033e8bf25a8dacb95721958
Misko Hevery <misko@hevery.com>
2017-12-01 04:37:29 +0000

    e95b5d7 revert: docs(aio): add service worker guide content and update nav (#20021) (#20716)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.3-9e69d97
+ * @license Angular v5.0.3-e95b5d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.3-9e69d97');
+var VERSION = new Version('5.0.3-e95b5d7');
 
 /**
  * @license
