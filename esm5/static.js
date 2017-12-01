b657d4b7fdb448ca429843d5f73378b6b2c1da23
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-01 22:42:02 +0000

    f841fbe fix(compiler-cli): propagate ts.SourceFile moduleName into metadata

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.0-b9a91a5
+ * @license Angular v5.1.0-rc.0-f841fbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-rc.0-b9a91a5');
+var VERSION = new Version('5.1.0-rc.0-f841fbe');
 
 /**
  * @license
