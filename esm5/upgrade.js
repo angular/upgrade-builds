c45ff245e87f2c0f3781927b72187aa314f879be
Igor Minar <igor@angularjs.org>
2017-12-05 00:35:58 +0000

    6911a25 build: set `preserveWhitespaces` to false by default on Bazel (#20783)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.1-7e7ff2e
+ * @license Angular v5.1.0-rc.1-6911a25
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-rc.1-7e7ff2e');
+var VERSION = new Version('5.1.0-rc.1-6911a25');
 
 /**
  * @fileoverview added by tsickle
