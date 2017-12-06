c189e4a8e5706d958e9e6c63009df6f69cfe2370
Igor Minar <igor@angularjs.org>
2017-12-06 15:06:16 +0000

    be99449 docs(core): add docs for i18n tokens (#17920)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.1-6911a25
+ * @license Angular v5.1.0-rc.1-be99449
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-rc.1-6911a25');
+var VERSION = new Version('5.1.0-rc.1-be99449');
 
 /**
  * @license
--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.1-6911a25
+ * @license Angular v5.1.0-rc.1-be99449
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-rc.1-6911a25');
+var VERSION = new Version('5.1.0-rc.1-be99449');
 
 /**
  * @fileoverview added by tsickle
