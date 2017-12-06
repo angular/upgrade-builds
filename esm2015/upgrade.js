bd93800ad56a559f275713e894a643ecb4840733
Igor Minar <igor@angularjs.org>
2017-12-06 15:20:29 +0000

    d824cc2 ci: upgrade to node 8 and Bazel 0.8 (#20807)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-c91c0d4
+ * @license Angular v5.0.5-d824cc2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.5-c91c0d4');
+const VERSION = new Version('5.0.5-d824cc2');
 
 /**
  * @fileoverview added by tsickle
--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-c91c0d4
+ * @license Angular v5.0.5-d824cc2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.5-c91c0d4');
+var VERSION = new Version('5.0.5-d824cc2');
 
 /**
  * @fileoverview added by tsickle
