2436c7fb337156e5a1bd9794c71c26011537b8fd
Igor Minar <igor@angularjs.org>
2017-12-06 06:19:24 +0000

    860ef8c docs(aio): update myUnless references to appUnless (#20658)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-6d15dd7
+ * @license Angular v5.0.5-860ef8c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.5-6d15dd7');
+const VERSION = new Version('5.0.5-860ef8c');
 
 /**
  * @fileoverview added by tsickle
--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-6d15dd7
+ * @license Angular v5.0.5-860ef8c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.5-6d15dd7');
+var VERSION = new Version('5.0.5-860ef8c');
 
 /**
  * @license
