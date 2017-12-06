0fb503410ac928f577f443d9dc06fb361c33944f
Igor Minar <igor@angularjs.org>
2017-12-06 06:16:09 +0000

    6d15dd7 docs(aio): architecture review for a11y (#17848)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-a2fde2b
+ * @license Angular v5.0.5-6d15dd7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -629,7 +629,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.0.5-a2fde2b');
+const VERSION = new Version('5.0.5-6d15dd7');
 
 /**
  * @license
