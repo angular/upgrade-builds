f42709ebbaba9bcd58a91b569626ed3fb5292fa6
Jason Aden <jasonaden@google.com>
2017-12-07 02:01:43 +0000

    6727336 fix(compiler-cli): Fix swallowed Error messages (#20846)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-c2dbc55
+ * @license Angular v5.1.0-6727336
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-c2dbc55');
+var VERSION = new Version('5.1.0-6727336');
 
 /**
  * @fileoverview added by tsickle
