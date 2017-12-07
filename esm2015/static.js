d963753a58e073bc15d0dac988199475f98da088
Jason Aden <jasonaden@google.com>
2017-12-07 02:02:43 +0000

    073f485 fix(compiler-cli): Fix swallowed Error messages (#20846)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-c2dbc55
+ * @license Angular v5.1.0-073f485
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -629,7 +629,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.0-c2dbc55');
+const VERSION = new Version('5.1.0-073f485');
 
 /**
  * @license
