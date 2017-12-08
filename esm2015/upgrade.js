32d020958e0a3bd9368b300f49222338442033bb
Jason Aden <jasonaden@google.com>
2017-12-08 18:29:21 +0000

    225baf4 docs(aio): fix typo for missing quote (#20888) PR Close #20888

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-46aa0a1
+ * @license Angular v5.1.0-225baf4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-46aa0a1');
+const VERSION = new Version('5.1.0-225baf4');
 
 /**
  * @fileoverview added by tsickle
