5788815b509a76a860fb59852476f4a6ef81f424
Jason Aden <jasonaden@google.com>
2017-12-08 20:32:35 +0000

    d713225 refactor(common): update i18n locale data to CLDR v32 (#20830)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-affa54d
+ * @license Angular v5.1.0-d713225
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-affa54d');
+var VERSION = new Version('5.1.0-d713225');
 
 /**
  * @fileoverview added by tsickle
