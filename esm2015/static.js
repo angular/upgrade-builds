2cc2bfbf8082db446570782bb9fe9d0094d9fed5
Jason Aden <jasonaden@google.com>
2017-12-08 01:29:42 +0000

    baeec4d fix(router): NavigatonError and NavigationCancel should be emitted after resetting the URL (#20803)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-9dd60a5
+ * @license Angular v5.1.0-baeec4d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -629,7 +629,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.0-9dd60a5');
+const VERSION = new Version('5.1.0-baeec4d');
 
 /**
  * @license
--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-9dd60a5
+ * @license Angular v5.1.0-baeec4d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-9dd60a5');
+var VERSION = new Version('5.1.0-baeec4d');
 
 /**
  * @license
