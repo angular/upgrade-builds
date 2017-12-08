9d9923fdfade2f9b719ca7bf9d4916505ec54a70
Jason Aden <jasonaden@google.com>
2017-12-08 18:25:51 +0000

    53b9de3 docs(aio): fix typo for missing quote (#20888) PR Close #20888

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-e6a2805
+ * @license Angular v5.1.0-53b9de3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -629,7 +629,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.0-e6a2805');
+const VERSION = new Version('5.1.0-53b9de3');
 
 /**
  * @license
--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-e6a2805
+ * @license Angular v5.1.0-53b9de3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-e6a2805');
+var VERSION = new Version('5.1.0-53b9de3');
 
 /**
  * @license
