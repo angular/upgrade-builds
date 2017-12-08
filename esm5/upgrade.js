778c9d93cd2f71ac30d39f5885c0a0877959399c
Jason Aden <jasonaden@google.com>
2017-12-08 23:26:21 +0000

    d09d497 fix(animations): ensure multi-level route leave animations are queryable (#20787)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-d713225
+ * @license Angular v5.1.0-d09d497
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-d713225');
+var VERSION = new Version('5.1.0-d09d497');
 
 /**
  * @fileoverview added by tsickle
