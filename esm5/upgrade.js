911271070419c35b94cf1490479e887456474e2b
Jason Aden <jasonaden@google.com>
2017-12-08 18:19:04 +0000

    46aa0a1 fix(animations): properly recover and cleanup DOM when CD failures occur (#20719)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-d8cc09b
+ * @license Angular v5.1.0-46aa0a1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-d8cc09b');
+var VERSION = new Version('5.1.0-46aa0a1');
 
 /**
  * @fileoverview added by tsickle
