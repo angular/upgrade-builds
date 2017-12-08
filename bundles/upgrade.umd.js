99b2f0968b55315774d1a05aeece106d6964ff5a
Jason Aden <jasonaden@google.com>
2017-12-08 01:30:00 +0000

    d8cc09b fix(router): NavigatonError and NavigationCancel should be emitted after resetting the URL (#20803)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-ef534c0
+ * @license Angular v5.1.0-d8cc09b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.1.0-ef534c0
+ * @license Angular v5.1.0-d8cc09b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -33,7 +33,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-ef534c0');
+var VERSION = new _angular_core.Version('5.1.0-d8cc09b');
 
 /**
  * @fileoverview added by tsickle
--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-ef534c0
+ * @license Angular v5.1.0-d8cc09b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-ef534c0');
+var VERSION = new Version('5.1.0-d8cc09b');
 
 /**
  * @fileoverview added by tsickle
