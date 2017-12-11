0bdcbd904d2a54da098c8d5a269ad60c26e1d6d4
Jason Aden <jasonaden@google.com>
2017-12-11 23:59:52 +0000

    70cd124 feat(compiler): add a pseudo $any() function to disable type checking (#20876)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-7363b3d
+ * @license Angular v5.1.0-70cd124
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-7363b3d');
+var VERSION = new Version('5.1.0-70cd124');
 
 /**
  * @license
--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-7363b3d
+ * @license Angular v5.1.0-70cd124
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-7363b3d');
+var VERSION = new Version('5.1.0-70cd124');
 
 /**
  * @fileoverview added by tsickle
