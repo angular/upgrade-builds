0af3f6d1335e78a3b9b515ceb8413267c039f1d2
Jason Aden <jasonaden@google.com>
2017-12-11 19:56:54 +0000

    7363b3d build: remove bazel option --noshow_results (#20943)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-13e663c
+ * @license Angular v5.1.0-7363b3d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-13e663c');
+const VERSION = new Version('5.1.0-7363b3d');
 
 /**
  * @fileoverview added by tsickle
--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-13e663c
+ * @license Angular v5.1.0-7363b3d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-13e663c');
+var VERSION = new Version('5.1.0-7363b3d');
 
 /**
  * @fileoverview added by tsickle
