b134ea9117968245b5c78cca17155dde398859a8
Jason Aden <jasonaden@google.com>
2017-12-12 20:03:40 +0000

    be9a737 fix(compiler-cli): merge @fileoverview comments. (#20870)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-1f46949
+ * @license Angular v5.1.0-be9a737
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-1f46949');
+var VERSION = new Version('5.1.0-be9a737');
 
 /**
  * @license
--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-1f46949
+ * @license Angular v5.1.0-be9a737
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-1f46949');
+var VERSION = new Version('5.1.0-be9a737');
 
 /**
  * @fileoverview added by tsickle
