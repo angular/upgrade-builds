2a3bd0fd7f4dd0bf3c21284c4012dddf496636f4
Jason Aden <jasonaden@google.com>
2017-12-12 00:35:40 +0000

    1f46949 ci: use container version in cache key (#20952)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-8a5f0f7
+ * @license Angular v5.1.0-1f46949
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-8a5f0f7');
+const VERSION = new Version('5.1.0-1f46949');
 
 /**
  * @fileoverview added by tsickle
--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-8a5f0f7
+ * @license Angular v5.1.0-1f46949
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-8a5f0f7');
+var VERSION = new Version('5.1.0-1f46949');
 
 /**
  * @license
