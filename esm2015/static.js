f39c269ed780afa133442f67e28b1d8e3cdf0204
Jason Aden <jasonaden@google.com>
2017-12-12 00:00:08 +0000

    8bcb093 build: update node version number in .nvmrc (#20832)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-9fbf850
+ * @license Angular v5.1.0-8bcb093
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -629,7 +629,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.0-9fbf850');
+const VERSION = new Version('5.1.0-8bcb093');
 
 /**
  * @license
