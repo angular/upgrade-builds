ca73d012e979fe2a7e2c3b06e34e3b84580a4769
Jason Aden <jasonaden@google.com>
2017-12-12 00:26:39 +0000

    81d497c build: pin ChromeDriver version (#20940)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-70cd124
+ * @license Angular v5.1.0-81d497c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-70cd124');
+var VERSION = new Version('5.1.0-81d497c');
 
 /**
  * @license
--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-70cd124
+ * @license Angular v5.1.0-81d497c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-70cd124');
+var VERSION = new Version('5.1.0-81d497c');
 
 /**
  * @fileoverview added by tsickle
