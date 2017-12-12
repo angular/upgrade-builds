42a514599488dc18608016591b47c8cb6f2fccef
Jason Aden <jasonaden@google.com>
2017-12-12 00:28:56 +0000

    add3589 ci: use container version in cache key (#20952)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-81d497c
+ * @license Angular v5.1.0-add3589
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.0-81d497c');
+var VERSION = new Version('5.1.0-add3589');
 
 /**
  * @license
--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-81d497c
+ * @license Angular v5.1.0-add3589
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-81d497c');
+var VERSION = new Version('5.1.0-add3589');
 
 /**
  * @fileoverview added by tsickle
