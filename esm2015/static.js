0b8e06bce2481d91b6e91a04aefdf4098eb8a3f8
Jason Aden <jasonaden@google.com>
2017-12-13 18:03:06 +0000

    d6da798 fix(compiler): support referencing enums in namespaces (#20947)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-be9a737
+ * @license Angular v5.1.0-d6da798
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -629,7 +629,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.0-be9a737');
+const VERSION = new Version('5.1.0-d6da798');
 
 /**
  * @license
