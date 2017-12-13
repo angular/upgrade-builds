d828d9d38657364398808803559e49041bf601bc
Alex Eagle <alexeagle@google.com>
2017-12-13 23:21:59 +0000

    541a391 ci: parallelize bazel build and test

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-d3dbb9d
+ * @license Angular v5.2.0-beta.0-541a391
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -630,7 +630,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.0-beta.0-d3dbb9d');
+const VERSION = new Version('5.2.0-beta.0-541a391');
 
 /**
  * @license
