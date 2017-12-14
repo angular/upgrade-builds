6a948b10462b9f31755f6a3760cd3a7af5e13115
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-14 16:57:56 +0000

    2686219 ci: sync CircleCI config from master to 5.0.x

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-eb57bc3
+ * @license Angular v5.0.5-2686219
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.0.5-eb57bc3
+ * @license Angular v5.0.5-2686219
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -33,7 +33,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.5-eb57bc3');
+var VERSION = new _angular_core.Version('5.0.5-2686219');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-eb57bc3
+ * @license Angular v5.0.5-2686219
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.5-eb57bc3');
+const VERSION = new Version('5.0.5-2686219');
 
 /**
  * @fileoverview added by tsickle
