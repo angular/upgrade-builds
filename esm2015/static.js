dd5d9bafaf85fa71adb86ad4a6ed5a04993622b7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-14 17:08:09 +0000

    b89e7c2 ci(aio): move e2e tests to non-optional job

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-cbe7e39
+ * @license Angular v5.2.0-beta.0-b89e7c2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -630,7 +630,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.0-beta.0-cbe7e39');
+const VERSION = new Version('5.2.0-beta.0-b89e7c2');
 
 /**
  * @license
