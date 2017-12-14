1a281d55f3509182279ddb1af98723b5b0758fc4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-14 16:53:08 +0000

    cbe7e39 build(common): don't generate .d.ts & .metadata.json files for i18n locales

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-6d57cb0
+ * @license Angular v5.2.0-beta.0-cbe7e39
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -630,7 +630,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.0-beta.0-6d57cb0');
+const VERSION = new Version('5.2.0-beta.0-cbe7e39');
 
 /**
  * @license
--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-6d57cb0
+ * @license Angular v5.2.0-beta.0-cbe7e39
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-beta.0-6d57cb0');
+var VERSION = new Version('5.2.0-beta.0-cbe7e39');
 
 /**
  * @fileoverview added by tsickle
