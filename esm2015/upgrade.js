8e3dbf3433a1ecb7a9d490494368309c5e392e9a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-14 17:30:23 +0000

    49c45f3 ci(aio): move e2e tests to non-optional job

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.1-e3140ae
+ * @license Angular v5.1.1-49c45f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.1-e3140ae');
+const VERSION = new Version('5.1.1-49c45f3');
 
 /**
  * @fileoverview added by tsickle
--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.1-e3140ae
+ * @license Angular v5.1.1-49c45f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.1-e3140ae');
+var VERSION = new Version('5.1.1-49c45f3');
 
 /**
  * @license
