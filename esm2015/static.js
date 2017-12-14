7eb78ebc2d8e57781e834b663aa0c67b9172ceef
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-14 16:52:46 +0000

    6d57cb0 ci: parallelize bazel build and test

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-6e2a8a2
+ * @license Angular v5.2.0-beta.0-6d57cb0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -630,7 +630,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.0-beta.0-6e2a8a2');
+const VERSION = new Version('5.2.0-beta.0-6d57cb0');
 
 /**
  * @license
