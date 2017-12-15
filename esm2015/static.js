eb3de797677a7fc3d961441e35994df4728db45a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-15 16:14:04 +0000

    05ff6c0 fix(compiler): make tsx file aot compatible

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-2e7e935
+ * @license Angular v5.2.0-beta.0-05ff6c0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -630,7 +630,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.0-beta.0-2e7e935');
+const VERSION = new Version('5.2.0-beta.0-05ff6c0');
 
 /**
  * @license
