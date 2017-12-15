7a37690689c2fd2bc5feb0044545782f7242d457
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-15 16:15:50 +0000

    267ebf3 fix(common): fix a Closure compilation issue.

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.1-49c45f3
+ * @license Angular v5.1.1-267ebf3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.1.1-49c45f3
+ * @license Angular v5.1.1-267ebf3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -33,7 +33,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.1-49c45f3');
+var VERSION = new _angular_core.Version('5.1.1-267ebf3');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.1-49c45f3
+ * @license Angular v5.1.1-267ebf3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.1-49c45f3');
+const VERSION = new Version('5.1.1-267ebf3');
 
 /**
  * @fileoverview added by tsickle
