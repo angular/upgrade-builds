f3633dc4fbc3fcfc79b3865ea73ec8fc5b5e0af6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-15 16:19:09 +0000

    d91ff17 fix(compiler): generate the correct imports for summary type-check

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-05ff6c0
+ * @license Angular v5.2.0-beta.0-d91ff17
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -630,7 +630,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.0-beta.0-05ff6c0');
+const VERSION = new Version('5.2.0-beta.0-d91ff17');
 
 /**
  * @license
