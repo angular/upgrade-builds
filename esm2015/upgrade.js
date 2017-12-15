5638b9b0e7473d8706ce47be42ef05777679dd14
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-15 16:39:29 +0000

    c011ffa test(aio): correct usage of fakeAsync and inject together in test

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.1-756dd34
+ * @license Angular v5.1.1-c011ffa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.1-756dd34');
+const VERSION = new Version('5.1.1-c011ffa');
 
 /**
  * @fileoverview added by tsickle
--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.1-756dd34
+ * @license Angular v5.1.1-c011ffa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.1-756dd34');
+var VERSION = new Version('5.1.1-c011ffa');
 
 /**
  * @license
