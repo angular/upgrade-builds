73dbc04f1167f6592ab0d8d85c0c37d35269e1b3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-20 22:13:43 +0000

    abca7c0 docs: add changelog for 5.2.0-beta.1

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-135ead6
+ * @license Angular v5.2.0-beta.1-abca7c0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -642,7 +642,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.0-beta.0-135ead6');
+const VERSION = new Version('5.2.0-beta.1-abca7c0');
 
 /**
  * @license
