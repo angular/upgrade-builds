008d17145ca022c4737659ae0179f6db65527521
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-20 21:59:21 +0000

    6353b77 docs: add changelog for 5.1.2

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.1-a36dfd4
+ * @license Angular v5.1.2-6353b77
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.1-a36dfd4');
+const VERSION = new Version('5.1.2-6353b77');
 
 /**
  * @fileoverview added by tsickle
