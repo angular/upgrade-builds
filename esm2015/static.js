7888243079b072a126231e7191f570061711b6d0
Igor Minar <igor@angularjs.org>
2017-12-21 22:24:40 +0000

    4337768 build: fix pullapprove (#21140)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-dc53248
+ * @license Angular v5.1.2-4337768
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -641,7 +641,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.2-dc53248');
+const VERSION = new Version('5.1.2-4337768');
 
 /**
  * @license
--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-dc53248
+ * @license Angular v5.1.2-4337768
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -668,7 +668,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.2-dc53248');
+var VERSION = new Version('5.1.2-4337768');
 
 /**
  * @license
