d8caa5fd27fdd93e289dba3fe76706f152786134
Igor Minar <igor@angularjs.org>
2017-12-22 04:33:00 +0000

    0aa6341 build: make umd.min.js source map paths relative (#21147)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-41abcc3
+ * @license Angular v5.1.2-0aa6341
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -641,7 +641,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.1.2-41abcc3');
+const VERSION = new Version('5.1.2-0aa6341');
 
 /**
  * @license
