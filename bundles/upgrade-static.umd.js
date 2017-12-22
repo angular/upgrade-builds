a3fc2f2b52adacc414148b83d0ae18ae9dd10ba6
Igor Minar <igor@angularjs.org>
2017-12-22 07:01:38 +0000

    5a7bf36 build: fix circular dep between interface and l_node by merging (#20855)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.1-6a58184
+ * @license Angular v5.2.0-beta.1-5a7bf36
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowser) { 'use strict';
 
 /**
- * @license Angular v5.2.0-beta.1-6a58184
+ * @license Angular v5.2.0-beta.1-5a7bf36
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -677,7 +677,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('5.2.0-beta.1-6a58184');
+var VERSION = new _angular_core.Version('5.2.0-beta.1-5a7bf36');
 
 /**
  * @license
