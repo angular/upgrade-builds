c249ddac6e5fa0414d01260e0a0df3f86241db42
Igor Minar <igor@angular.io>
2017-12-27 21:35:50 +0000

    66043e0 docs(aio): Sort in the api type dropdown (#21030) (#21176)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-12702b2
+ * @license Angular v5.1.2-66043e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -668,7 +668,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.2-12702b2');
+var VERSION = new Version('5.1.2-66043e0');
 
 /**
  * @license
--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-12702b2
+ * @license Angular v5.1.2-66043e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.2-12702b2');
+var VERSION = new Version('5.1.2-66043e0');
 
 /**
  * @fileoverview added by tsickle
