7bb0f3c07b84b0cf42208202819c1323fcd030b5
Miško Hevery <misko@hevery.com>
2018-01-02 21:10:47 +0000

    48a1f32 fix(language-service): ignore null metadatas (#20557)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-66043e0
+ * @license Angular v5.1.2-48a1f32
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.2-66043e0');
+var VERSION = new Version('5.1.2-48a1f32');
 
 /**
  * @fileoverview added by tsickle
