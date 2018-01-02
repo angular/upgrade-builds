a1828fdfeeeb583263460c8ba68d6a18a9dfb884
Miško Hevery <misko@hevery.com>
2018-01-02 19:03:30 +0000

    3e47ea2 fix(language-service): ignore null metadatas (#20557)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.1-bc7a6d7
+ * @license Angular v5.2.0-beta.1-3e47ea2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -664,7 +664,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.0-beta.1-bc7a6d7');
+var VERSION = new Version('5.2.0-beta.1-3e47ea2');
 
 /**
  * @license
