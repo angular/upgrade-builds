90c83fdf6fe816a6cb38387fb8a7729161d5fdb4
Misko Hevery <misko@hevery.com>
2018-01-03 22:04:11 +0000

    a7a7bd3 build: update rules_typescript (#21290)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-67630f8
+ * @license Angular v5.1.2-a7a7bd3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.2-67630f8');
+const VERSION = new Version('5.1.2-a7a7bd3');
 
 /**
  * @fileoverview added by tsickle
