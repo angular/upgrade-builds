012cf88990aee4a0606a1693dfac6db5782c8e08
Kara Erickson <karakara@google.com>
2018-01-04 22:36:24 +0000

    81823e8 build(aio): fix example protractor config for Travis (#21309)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-ef78538
+ * @license Angular v5.2.0-rc.0-81823e8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.0-rc.0-ef78538');
+const VERSION = new Version('5.2.0-rc.0-81823e8');
 
 /**
  * @license
