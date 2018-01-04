c0148c1553abddd2edcfa917dc764675fa1f93e6
Kara Erickson <karakara@google.com>
2018-01-04 01:29:26 +0000

    fc217f5 refactor(aio): use one argument for `DocViewer` error reporting (#21293)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-d138b38
+ * @license Angular v5.1.3-fc217f5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.1.3-d138b38
+ * @license Angular v5.1.3-fc217f5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -33,7 +33,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.3-d138b38');
+var VERSION = new _angular_core.Version('5.1.3-fc217f5');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-d138b38
+ * @license Angular v5.1.3-fc217f5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.3-d138b38');
+const VERSION = new Version('5.1.3-fc217f5');
 
 /**
  * @fileoverview added by tsickle
