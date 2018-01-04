2b1f4cf06098e0b206b4ed2db0baf12d3d0c0944
Misko Hevery <misko@hevery.com>
2018-01-04 00:21:52 +0000

    d138b38 docs: add changelog for 5.1.3

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-00308fc
+ * @license Angular v5.1.3-d138b38
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -668,7 +668,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.2-00308fc');
+var VERSION = new Version('5.1.3-d138b38');
 
 /**
  * @license
