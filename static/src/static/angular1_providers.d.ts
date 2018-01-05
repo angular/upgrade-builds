49d9a3d079d015e4a44d856890b40f2494bb05fb
GitHub <noreply@github.com>
2018-01-05 18:50:35 +0000

    7bec6a2 docs: remove broken issuestats.com badges

--- static/src/static/angular1_providers.d.ts
+++ static/src/static/angular1_providers.d.ts
@@ -7,15 +7,12 @@
  */
 import * as angular from '../common/angular1';
 export declare function setTempInjectorRef(injector: angular.IInjectorService): void;
-export declare function injectorFactory(): angular.IInjectorService | null;
+export declare function injectorFactory(): angular.IInjectorService;
 export declare function rootScopeFactory(i: angular.IInjectorService): any;
 export declare function compileFactory(i: angular.IInjectorService): any;
 export declare function parseFactory(i: angular.IInjectorService): any;
-export declare const angular1Providers: ({
-    provide: string;
-    useFactory: () => angular.IInjectorService | null;
-} | {
+export declare const angular1Providers: {
     provide: string;
     useFactory: (i: angular.IInjectorService) => any;
     deps: string[];
-})[];
+}[];
