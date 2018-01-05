b10649ab4068e5068112a0af2b46d40abda53d20
Kara Erickson <karakara@google.com>
2018-01-05 22:44:14 +0000

    839d24e fix(common): export currencies via `getCurrencySymbol` (#20983)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-a1af559
+ * @license Angular v5.1.3-839d24e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -668,7 +668,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.3-a1af559');
+var VERSION = new Version('5.1.3-839d24e');
 
 /**
  * @license
