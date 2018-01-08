55b3c2221778095b85312637be516c0d244b57f0
Kara Erickson <karakara@google.com>
2018-01-08 18:55:17 +0000

    1bd03a4 docs(aio): document the template compiler options (#21333)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-8b197b2
+ * @license Angular v5.1.3-1bd03a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.3-8b197b2');
+const VERSION = new Version('5.1.3-1bd03a4');
 
 /**
  * @fileoverview added by tsickle
--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-8b197b2
+ * @license Angular v5.1.3-1bd03a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -668,7 +668,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.1.3-8b197b2');
+var VERSION = new Version('5.1.3-1bd03a4');
 
 /**
  * @license
