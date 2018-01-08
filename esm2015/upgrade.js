8f8325a48777de0a23a3a35c6b06d320a797d2fd
Kara Erickson <karakara@google.com>
2018-01-08 18:59:19 +0000

    4947454 ci: reformat itegration/_payload-limits.json to be easier to read (#21345)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-1bd03a4
+ * @license Angular v5.1.3-4947454
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.3-1bd03a4');
+const VERSION = new Version('5.1.3-4947454');
 
 /**
  * @fileoverview added by tsickle
