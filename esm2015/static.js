94e63e51a919ecf1cc706251de34b0217fe21439
Kara Erickson <karakara@google.com>
2018-01-08 18:38:48 +0000

    b15d50a build(core): improve payload size failure message (#21346)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-db55e86
+ * @license Angular v5.2.0-rc.0-b15d50a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.0-rc.0-db55e86');
+const VERSION = new Version('5.2.0-rc.0-b15d50a');
 
 /**
  * @license
--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-db55e86
+ * @license Angular v5.2.0-rc.0-b15d50a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -664,7 +664,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.0-rc.0-db55e86');
+var VERSION = new Version('5.2.0-rc.0-b15d50a');
 
 /**
  * @license
