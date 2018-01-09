74aaf44adcd72f67dd47bb49caa1302961467476
Kara Erickson <karakara@google.com>
2018-01-09 19:21:23 +0000

    80a5b91 build(aio): upgrade to zone.js@0.8.19 (#21349)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-9728dce
+ * @license Angular v5.2.0-rc.0-80a5b91
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.0-rc.0-9728dce');
+const VERSION = new Version('5.2.0-rc.0-80a5b91');
 
 /**
  * @license
