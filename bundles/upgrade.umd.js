8de0ff4fec750a033333d095392b16d3372108c4
Kara Erickson <karakara@google.com>
2018-01-09 00:30:22 +0000

    a73b0b6 docs(aio): fix punctuation for clarity (#21325) PR Close #21325

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-fda3815
+ * @license Angular v5.1.3-a73b0b6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.1.3-fda3815
+ * @license Angular v5.1.3-a73b0b6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -33,7 +33,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.3-fda3815');
+var VERSION = new _angular_core.Version('5.1.3-a73b0b6');
 
 /**
  * @fileoverview added by tsickle
--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-fda3815
+ * @license Angular v5.1.3-a73b0b6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -25,7 +25,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.3-fda3815');
+var VERSION = new Version('5.1.3-a73b0b6');
 
 /**
  * @fileoverview added by tsickle
