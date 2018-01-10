077a1493ddd468ce62132cff95e40ea8149da5e1
Alex Eagle <alexeagle@google.com>
2018-01-10 23:53:53 +0000

    6a97b5b docs(ivy): add Ivy as a recognized label (#21428)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-aad1126
+ * @license Angular v5.2.0-6a97b5b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -664,7 +664,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.0-aad1126');
+var VERSION = new Version('5.2.0-6a97b5b');
 
 /**
  * @license
