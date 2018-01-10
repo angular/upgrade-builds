0a47e6027073c517336ce92cfad8101048381f0c
Alex Eagle <alexeagle@google.com>
2018-01-10 20:59:09 +0000

    ff6047c build(aio): upgrade to uglify-es@3.3.5 (#21350)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-a76ab1f
+ * @license Angular v5.2.0-ff6047c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.0-a76ab1f');
+const VERSION = new Version('5.2.0-ff6047c');
 
 /**
  * @license
--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-a76ab1f
+ * @license Angular v5.2.0-ff6047c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -664,7 +664,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.0-a76ab1f');
+var VERSION = new Version('5.2.0-ff6047c');
 
 /**
  * @license
