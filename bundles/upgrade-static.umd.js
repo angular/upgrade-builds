c7c36b865ff24c7809e21f1c8ad4211f74ba0f40
Alex Eagle <alexeagle@google.com>
2018-01-11 15:05:42 +0000

    c0080d7 docs: fix release schedule date (#21469) PR Close #21469

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-c6eb9ee
+ * @license Angular v5.2.0-c0080d7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowser) { 'use strict';
 
 /**
- * @license Angular v5.2.0-c6eb9ee
+ * @license Angular v5.2.0-c0080d7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -672,7 +672,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('5.2.0-c6eb9ee');
+var VERSION = new _angular_core.Version('5.2.0-c0080d7');
 
 /**
  * @license
