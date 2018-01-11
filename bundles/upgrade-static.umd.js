825acebce91bfc6bfec86b45a28db54f85a28f40
Alex Eagle <alexeagle@google.com>
2018-01-11 00:48:35 +0000

    3db02d2 docs: fix lazy-loading example (#20306)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-a931a41
+ * @license Angular v5.2.0-3db02d2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowser) { 'use strict';
 
 /**
- * @license Angular v5.2.0-a931a41
+ * @license Angular v5.2.0-3db02d2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -672,7 +672,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('5.2.0-a931a41');
+var VERSION = new _angular_core.Version('5.2.0-3db02d2');
 
 /**
  * @license
--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-a931a41
+ * @license Angular v5.2.0-3db02d2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.2.0-a931a41
+ * @license Angular v5.2.0-3db02d2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -28,7 +28,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-a931a41');
+var VERSION = new _angular_core.Version('5.2.0-3db02d2');
 
 /**
  * @fileoverview added by tsickle
