1d68a36d4febf8654644fec60c2ed1e959aa9c04
Alex Eagle <alexeagle@google.com>
2018-01-11 15:23:21 +0000

    b49d54e docs(forms): clarify note in reactive forms doc (#21134)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-55fd82e
+ * @license Angular v5.2.0-b49d54e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.0-55fd82e');
+const VERSION = new Version('5.2.0-b49d54e');
 
 /**
  * @license
