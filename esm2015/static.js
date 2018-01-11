1747d26a81f65734e7634d5928c150d88a0c0aaf
Alex Eagle <alexeagle@google.com>
2018-01-11 15:21:58 +0000

    5eaaac3 refactor(ivy): remove `type` from `DirectiveDef` (#21374)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-c0080d7
+ * @license Angular v5.2.0-5eaaac3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.0-c0080d7');
+const VERSION = new Version('5.2.0-5eaaac3');
 
 /**
  * @license
