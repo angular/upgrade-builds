fc2f31d373f13dbefb8c92b4a9f4c04e7a149538
Alex Eagle <alexeagle@google.com>
2018-01-12 22:09:32 +0000

    3e3c22f fix(aio): correctly handle redirects (#21416)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-f6db952
+ * @license Angular v5.2.0-3e3c22f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.0-f6db952');
+const VERSION = new Version('5.2.0-3e3c22f');
 
 /**
  * @license
--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-f6db952
+ * @license Angular v5.2.0-3e3c22f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-f6db952');
+const VERSION = new Version('5.2.0-3e3c22f');
 
 /**
  * @fileoverview added by tsickle
