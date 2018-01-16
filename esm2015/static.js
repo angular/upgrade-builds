064e9f01039192a2d0ee3d6881e3162704a7d6c5
Alex Eagle <alexeagle@google.com>
2018-01-16 15:31:06 +0000

    54bf179 feat(forms): handle string with and without line boundary on pattern validator (#19256)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-7c414fc
+ * @license Angular v5.2.0-54bf179
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.0-7c414fc');
+const VERSION = new Version('5.2.0-54bf179');
 
 /**
  * @license
--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-7c414fc
+ * @license Angular v5.2.0-54bf179
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -664,7 +664,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.0-7c414fc');
+var VERSION = new Version('5.2.0-54bf179');
 
 /**
  * @license
