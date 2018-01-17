d65ee52f707cb4c08710d26c5d885668b16e6289
Alex Eagle <alexeagle@google.com>
2018-01-17 17:28:49 +0000

    420f5c4 build(bazel): remove spurious file (#21455)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-b773a4a
+ * @license Angular v5.2.0-420f5c4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.0-b773a4a');
+const VERSION = new Version('5.2.0-420f5c4');
 
 /**
  * @license
