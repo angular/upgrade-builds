dbe0495d38ef28071d2c7f498cbc81740392e6cc
GitHub <noreply@github.com>
2018-01-17 23:42:16 +0000

    0eabd07 fix: avoid triggering a cli bug (#21611)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.0-8503cc1
+ * @license Angular v5.9.9-6-beta.0-0eabd07
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v6.0.0-beta.0-8503cc1
+ * @license Angular v5.9.9-6-beta.0-0eabd07
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -28,7 +28,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.0-8503cc1');
+var VERSION = new _angular_core.Version('5.9.9-6-beta.0-0eabd07');
 
 /**
  * @fileoverview added by tsickle
