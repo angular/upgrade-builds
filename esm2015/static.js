ba4e89911697dd286f092e8446001ea63a9dc24f
Alex Eagle <alexeagle@google.com>
2018-01-17 15:46:49 +0000

    d3bf54b feat(core): add binding name to content changed error (#20352)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-7e3f9a4
+ * @license Angular v5.2.0-d3bf54b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.0-7e3f9a4');
+const VERSION = new Version('5.2.0-d3bf54b');
 
 /**
  * @license
