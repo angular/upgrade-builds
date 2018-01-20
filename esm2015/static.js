30dc3ca132e02864259bea1b76ddaf99df8b2f76
Misko Hevery <misko@hevery.com>
2018-01-20 04:52:47 +0000

    554129d feat(aio): update metatags to control search engine crawling (#21665)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-c828e56
+ * @license Angular v5.2.1-554129d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.1-c828e56');
+const VERSION = new Version('5.2.1-554129d');
 
 /**
  * @license
