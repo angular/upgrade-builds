710370db270362a7eeb96fca6a5417493ca49c2b
Miško Hevery <misko@hevery.com>
2018-01-22 23:55:21 +0000

    4b68fdc build: Update to latest rules_typescript. (#21675)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-c12ea3a
+ * @license Angular v5.2.1-4b68fdc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.2.1-c12ea3a
+ * @license Angular v5.2.1-4b68fdc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -28,7 +28,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.1-c12ea3a');
+var VERSION = new _angular_core.Version('5.2.1-4b68fdc');
 
 /**
  * @fileoverview added by tsickle
