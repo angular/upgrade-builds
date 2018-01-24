0345aa9df9e6af85b37a48ffee51135451d62f0c
Miško Hevery <misko@hevery.com>
2018-01-24 01:00:37 +0000

    ed670a3 docs: update ICU select messages to use male/female (#21713)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-52970c0
+ * @license Angular v5.2.1-ed670a3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.1-52970c0');
+const VERSION = new Version('5.2.1-ed670a3');
 
 /**
  * @license
