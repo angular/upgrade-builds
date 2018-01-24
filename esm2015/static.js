461c4006b038e18e4a75ed4a305ac9c3c30e9ff5
Miško Hevery <misko@hevery.com>
2018-01-24 05:52:57 +0000

    8b3fbb5 fix(router): don't use ParamsInheritanceStrategy in declarations (#21574)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-ed670a3
+ * @license Angular v5.2.1-8b3fbb5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.1-ed670a3');
+const VERSION = new Version('5.2.1-8b3fbb5');
 
 /**
  * @license
