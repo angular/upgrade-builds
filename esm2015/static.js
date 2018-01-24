b3c6cb70b29d792a65734e7eca746129d0ba6634
Miško Hevery <misko@hevery.com>
2018-01-24 05:54:13 +0000

    2f61d3c fix(aio): remove remaining `plnkr` references (#20165)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-8b3fbb5
+ * @license Angular v5.2.1-2f61d3c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.1-8b3fbb5');
+const VERSION = new Version('5.2.1-2f61d3c');
 
 /**
  * @license
