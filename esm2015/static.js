cd9ef277cd813deda11af984202b5adf10dad980
Miško Hevery <misko@hevery.com>
2018-01-25 22:03:26 +0000

    23596b3 docs(aio): fix missing stylesheet in component-styles example (#21772)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-5269ce2
+ * @license Angular v6.0.0-beta.1-23596b3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-beta.1-5269ce2');
+const VERSION = new Version('6.0.0-beta.1-23596b3');
 
 /**
  * @license
