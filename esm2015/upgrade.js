c12e6815f01b0627ef424b1b9e560f50ea7b6043
Jason Aden <jasonaden@google.com>
2018-01-26 23:20:29 +0000

    e0b31db build: merge-pr now checks that PR status is green before proceeding (#21810)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-8df56fe
+ * @license Angular v6.0.0-beta.1-e0b31db
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.1-8df56fe');
+const VERSION = new Version('6.0.0-beta.1-e0b31db');
 
 /**
  * @fileoverview added by tsickle
