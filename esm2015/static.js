d667ad0a99896dd39a2fe74b6dfd0363c66535e0
Misko Hevery <misko@hevery.com>
2018-01-26 07:00:17 +0000

    08aa54e ci: Add back the CLI integration test with pinning (#21555)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-fac4d8d
+ * @license Angular v6.0.0-beta.1-08aa54e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-beta.1-fac4d8d');
+const VERSION = new Version('6.0.0-beta.1-08aa54e');
 
 /**
  * @license
