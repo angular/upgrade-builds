091dc4a442499ee245cbb15ebd8bde02cda56a24
Miško Hevery <misko@hevery.com>
2018-01-26 06:13:04 +0000

    4db1be0 docs(aio): fix paths to imported CSS stylesheets (#21772)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-27ecd07
+ * @license Angular v5.2.2-4db1be0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.2-27ecd07');
+const VERSION = new Version('5.2.2-4db1be0');
 
 /**
  * @fileoverview added by tsickle
