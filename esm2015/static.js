a3a380d5d7669b8ee23c2c621d1149df3dd63f63
Jason Aden <jasonaden@google.com>
2018-01-27 00:55:23 +0000

    ede9cb7 Revert: "feat(router): add navigationSource and restoredState to NavigationStart event (#21728)"

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-c3fb820
+ * @license Angular v6.0.0-beta.1-ede9cb7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-beta.1-c3fb820');
+const VERSION = new Version('6.0.0-beta.1-ede9cb7');
 
 /**
  * @license
