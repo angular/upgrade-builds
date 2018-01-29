4c23126756d1ddb507e63ef7fc47420686b482bc
Jason Aden <jasonaden@google.com>
2018-01-29 20:17:29 +0000

    3f5ead3 fix(aio): missing plural s in preserveWhiteSpaces example (#21854)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-a89e709
+ * @license Angular v5.2.2-3f5ead3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.2.2-a89e709
+ * @license Angular v5.2.2-3f5ead3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -28,7 +28,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.2-a89e709');
+var VERSION = new _angular_core.Version('5.2.2-3f5ead3');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-a89e709
+ * @license Angular v5.2.2-3f5ead3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.2-a89e709');
+const VERSION = new Version('5.2.2-3f5ead3');
 
 /**
  * @fileoverview added by tsickle
