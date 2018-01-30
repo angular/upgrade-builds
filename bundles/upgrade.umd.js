c9e585bdc6b1957962c9d333121fe270a53c6cac
Jason Aden <jasonaden@google.com>
2018-01-30 20:22:34 +0000

    285dd6b feat(ivy): observable QueryList (#21859)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-b10540a
+ * @license Angular v6.0.0-beta.1-285dd6b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v6.0.0-beta.1-b10540a
+ * @license Angular v6.0.0-beta.1-285dd6b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -28,7 +28,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.1-b10540a');
+var VERSION = new _angular_core.Version('6.0.0-beta.1-285dd6b');
 
 /**
  * @fileoverview added by tsickle
