888f15f282aca19a976ded5281747069bb842e5f
Jason Aden <jasonaden@google.com>
2018-01-31 19:20:00 +0000

    2aa460b docs: add http guide sample and adjust text (#21326)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-e0022ae
+ * @license Angular v5.2.2-2aa460b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.2.2-e0022ae
+ * @license Angular v5.2.2-2aa460b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -28,7 +28,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.2-e0022ae');
+var VERSION = new _angular_core.Version('5.2.2-2aa460b');
 
 /**
  * @fileoverview added by tsickle
