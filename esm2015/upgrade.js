02d0f53f131e3b765361f067b8188546bc44ed84
Jason Aden <jasonaden@google.com>
2018-01-31 19:01:54 +0000

    ccd0298 ci(ivy): enable size tracking of a minimal cli render3 application (#21792)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-eeab433
+ * @license Angular v6.0.0-beta.1-ccd0298
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.1-eeab433');
+const VERSION = new Version('6.0.0-beta.1-ccd0298');
 
 /**
  * @fileoverview added by tsickle
