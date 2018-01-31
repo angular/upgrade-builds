1b4346836ffe83994fd9256cafb6509406106618
Jason Aden <jasonaden@google.com>
2018-01-31 18:44:28 +0000

    9dca5f2 refactor(ivy): save check methods separately (#21795) (#21905)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-06d6c76
+ * @license Angular v6.0.0-beta.1-9dca5f2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowser) { 'use strict';
 
 /**
- * @license Angular v6.0.0-beta.1-06d6c76
+ * @license Angular v6.0.0-beta.1-9dca5f2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -672,7 +672,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.1-06d6c76');
+var VERSION = new _angular_core.Version('6.0.0-beta.1-9dca5f2');
 
 /**
  * @license
--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-06d6c76
+ * @license Angular v6.0.0-beta.1-9dca5f2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.1-06d6c76');
+var VERSION = new Version('6.0.0-beta.1-9dca5f2');
 
 /**
  * @fileoverview added by tsickle
