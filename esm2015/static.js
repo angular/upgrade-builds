88d5ad9a0e9718c5455d87ca2ed36dfe906248c9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-01 16:53:57 +0000

    c4fb696 fix(common): don't convert null to a string when flushing a mock request (#21417)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-72df747
+ * @license Angular v5.2.3-c4fb696
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.3-72df747');
+const VERSION = new Version('5.2.3-c4fb696');
 
 /**
  * @license
