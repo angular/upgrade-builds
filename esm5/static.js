c31eac7b0fc04304a7188ce4aa2b4886e034620c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:15:26 +0000

    75eecdc docs: add missing underline (#21892) PR Close #21892

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-965eecc
+ * @license Angular v5.2.3-75eecdc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -664,7 +664,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.3-965eecc');
+var VERSION = new Version('5.2.3-75eecdc');
 
 /**
  * @license
