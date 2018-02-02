56c9f65655919c64089e45af2e9720027153063e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:13:03 +0000

    3cc1d76 fix(ivy): generate correct interpolations (#21946)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-65cf1ad
+ * @license Angular v6.0.0-beta.2-3cc1d76
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-beta.2-65cf1ad');
+const VERSION = new Version('6.0.0-beta.2-3cc1d76');
 
 /**
  * @license
