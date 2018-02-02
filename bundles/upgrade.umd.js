12ed5d7bbe55e3e26a5160058e633524952e6e53
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:14:15 +0000

    1242839 build(aio): move zip and live-example generation to `yarn predocs` task (#21970)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-3cc1d76
+ * @license Angular v6.0.0-beta.2-1242839
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v6.0.0-beta.2-3cc1d76
+ * @license Angular v6.0.0-beta.2-1242839
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -28,7 +28,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.2-3cc1d76');
+var VERSION = new _angular_core.Version('6.0.0-beta.2-1242839');
 
 /**
  * @fileoverview added by tsickle
