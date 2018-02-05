749d7c8efb93e6f04c28e6c2b5c4ec30bdd50cfa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 22:11:09 +0000

    9d3326c docs: clarify npm/yarn commands, add blank lines to mix md/html in table (#21606)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-0846784
+ * @license Angular v6.0.0-beta.2-9d3326c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-beta.2-0846784');
+const VERSION = new Version('6.0.0-beta.2-9d3326c');
 
 /**
  * @license
