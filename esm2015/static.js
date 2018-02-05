413f0d6d158b80f2f874bc2a0e86c57eb4e9c6a6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 21:53:28 +0000

    0c9ec37 ci: mark PRs with rejection as not green (#21922) PR Close #21922

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-9a0700f
+ * @license Angular v6.0.0-beta.2-0c9ec37
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-beta.2-9a0700f');
+const VERSION = new Version('6.0.0-beta.2-0c9ec37');
 
 /**
  * @license
