b9279c3d43dca3cb3991c8900fb1077fa1c54ce8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 21:47:15 +0000

    973607f ci: mark PRs with rejection as not green (#21922) PR Close #21922

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-b155ae1
+ * @license Angular v5.2.3-973607f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowser) { 'use strict';
 
 /**
- * @license Angular v5.2.3-b155ae1
+ * @license Angular v5.2.3-973607f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -672,7 +672,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('5.2.3-b155ae1');
+var VERSION = new _angular_core.Version('5.2.3-973607f');
 
 /**
  * @license
