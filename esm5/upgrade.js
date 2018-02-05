716ee2b5445b0438c2791af7c74b4b71748304c2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 21:23:10 +0000

    5a624fa feat(aio): dynamically, pre-emptively, add `noindex` (#21992)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-3a86940
+ * @license Angular v6.0.0-beta.2-5a624fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.2-3a86940');
+var VERSION = new Version('6.0.0-beta.2-5a624fa');
 
 /**
  * @fileoverview added by tsickle
