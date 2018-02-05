9dd963a7497c36208e3c7c2b90df396b63558bb4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 22:00:37 +0000

    0846784 fix(ivy): improve `bindV` perf and memory usage (#21881)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-0c9ec37
+ * @license Angular v6.0.0-beta.2-0846784
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.2-0c9ec37');
+var VERSION = new Version('6.0.0-beta.2-0846784');
 
 /**
  * @fileoverview added by tsickle
