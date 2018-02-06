5a0d38c8d460025a0f1ee26958eb80f5b8ee73ee
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-06 16:22:16 +0000

    c6645e7 fix(core): fix proper propagation of subscriptions in EventEmitter (#22016)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-f0396f1
+ * @license Angular v5.2.3-c6645e7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.3-f0396f1');
+const VERSION = new Version('5.2.3-c6645e7');
 
 /**
  * @license
--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-f0396f1
+ * @license Angular v5.2.3-c6645e7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.3-f0396f1');
+const VERSION = new Version('5.2.3-c6645e7');
 
 /**
  * @fileoverview added by tsickle
