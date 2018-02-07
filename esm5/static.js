285103bf56511c21fb81db600c1286aeb3ae92b3
Miško Hevery <misko@hevery.com>
2018-02-07 21:41:28 +0000

    47b73fd fix(core): ensure initial value of QueryList length (#21980) (#21982)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-07769e5
+ * @license Angular v5.2.4-47b73fd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -664,7 +664,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.4-07769e5');
+var VERSION = new Version('5.2.4-47b73fd');
 
 /**
  * @license
