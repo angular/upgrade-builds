26d08fd7caaa249a604016cd335c4b4138125754
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-07 19:42:08 +0000

    530b824 docs: add changelog for 5.2.4

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-c6645e7
+ * @license Angular v5.2.4-530b824
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.2.3-c6645e7
+ * @license Angular v5.2.4-530b824
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -28,7 +28,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.3-c6645e7');
+var VERSION = new _angular_core.Version('5.2.4-530b824');
 
 /**
  * @fileoverview added by tsickle
