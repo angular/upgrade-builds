994f9f74d1a6dbe7f0e0b0259323b0f998738441
Miško Hevery <misko@hevery.com>
2018-02-07 20:25:57 +0000

    44ea80b docs(forms): Custom Validator example selector name incorrect. (#20464)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-530b824
+ * @license Angular v5.2.4-44ea80b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowser) { 'use strict';
 
 /**
- * @license Angular v5.2.4-530b824
+ * @license Angular v5.2.4-44ea80b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -672,7 +672,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('5.2.4-530b824');
+var VERSION = new _angular_core.Version('5.2.4-44ea80b');
 
 /**
  * @license
--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-530b824
+ * @license Angular v5.2.4-44ea80b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.4-530b824');
+const VERSION = new Version('5.2.4-44ea80b');
 
 /**
  * @license
