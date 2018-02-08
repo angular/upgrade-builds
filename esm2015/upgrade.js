227a7c2b05193087a04c439cba865fb2cb12d0aa
Miško Hevery <misko@pangular.mtv.corp.google.com>
2018-02-08 00:30:27 +0000

    cd25939 build(aio): update examples to CLI to 1.6.5 (#21222)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-b58c352
+ * @license Angular v6.0.0-beta.3-cd25939
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-b58c352');
+const VERSION = new Version('6.0.0-beta.3-cd25939');
 
 /**
  * @fileoverview added by tsickle
