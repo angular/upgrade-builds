9bbda719cbe4e451e708de4e85a411d8e9a3b7fa
Miško Hevery <misko@hevery.com>
2018-02-13 00:17:45 +0000

    c6bdc83 fix(common): weaken AsyncPipe transform signature (#22169)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-1a897e4
+ * @license Angular v5.2.4-c6bdc83
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.2.4-1a897e4
+ * @license Angular v5.2.4-c6bdc83
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -28,7 +28,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.4-1a897e4');
+var VERSION = new _angular_core.Version('5.2.4-c6bdc83');
 
 /**
  * @fileoverview added by tsickle
