00cb9abbb4f602d21172082ebae44dea9da854af
Miško Hevery <misko@hevery.com>
2018-02-13 20:04:40 +0000

    6435ecd fix(platform-browser): support 0/false/null values in transfer_state (#22179)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-16d1700
+ * @license Angular v6.0.0-beta.3-6435ecd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v6.0.0-beta.3-16d1700
+ * @license Angular v6.0.0-beta.3-6435ecd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -28,7 +28,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.3-16d1700');
+var VERSION = new _angular_core.Version('6.0.0-beta.3-6435ecd');
 
 /**
  * @fileoverview added by tsickle
