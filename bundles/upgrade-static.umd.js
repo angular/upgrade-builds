51f6823dee98b79f713c16adeb156b30d2fa5d66
Miško Hevery <misko@hevery.com>
2018-02-13 20:12:41 +0000

    20a900b test: Add bundle symbol extractor tool (#22002)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-6435ecd
+ * @license Angular v6.0.0-beta.3-20a900b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowser) { 'use strict';
 
 /**
- * @license Angular v6.0.0-beta.3-6435ecd
+ * @license Angular v6.0.0-beta.3-20a900b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -672,7 +672,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.3-6435ecd');
+var VERSION = new _angular_core.Version('6.0.0-beta.3-20a900b');
 
 /**
  * @license
