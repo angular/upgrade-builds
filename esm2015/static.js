14e7a57639d231af6a1a6232866cf738880da9af
Victor Berchet <victor@suumit.com>
2018-02-16 00:49:37 +0000

    d241532 docs(ivy): add a note about exporting top level variables (#22234)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-f755db7
+ * @license Angular v6.0.0-beta.4-d241532
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-beta.4-f755db7');
+const VERSION = new Version('6.0.0-beta.4-d241532');
 
 /**
  * @license
