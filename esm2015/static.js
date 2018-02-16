ca4a10f2e4101f5c56ffb2a1f85ac8131ac9eea4
Victor Berchet <victor@suumit.com>
2018-02-16 23:31:14 +0000

    ce5e8fa fix(common): correct mapping of Observable methods (#20518)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-185a6ab
+ * @license Angular v5.2.5-ce5e8fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.5-185a6ab');
+const VERSION = new Version('5.2.5-ce5e8fa');
 
 /**
  * @license
