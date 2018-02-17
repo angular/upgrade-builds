33d611566250357329300d4dec7fd51566d1e640
Victor Berchet <victor@suumit.com>
2018-02-17 02:26:14 +0000

    861250b docs: fix changelog errors (#22228)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-81c1e0a
+ * @license Angular v5.2.5-861250b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.2.5-81c1e0a
+ * @license Angular v5.2.5-861250b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -28,7 +28,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.5-81c1e0a');
+var VERSION = new _angular_core.Version('5.2.5-861250b');
 
 /**
  * @fileoverview added by tsickle
--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-81c1e0a
+ * @license Angular v5.2.5-861250b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.5-81c1e0a');
+var VERSION = new Version('5.2.5-861250b');
 
 /**
  * @fileoverview added by tsickle
