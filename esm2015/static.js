afbe2bc1ff5e6b9bb6131d639937c20c520f3d49
Victor Berchet <victor@suumit.com>
2018-02-18 21:34:20 +0000

    396bc0d build: use authenticated mode for the merge script (#22269)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-861250b
+ * @license Angular v5.2.5-396bc0d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.5-861250b');
+const VERSION = new Version('5.2.5-396bc0d');
 
 /**
  * @license
