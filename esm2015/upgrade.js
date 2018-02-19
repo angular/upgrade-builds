7b30d0c12a80abc40583928e3b2482bd2e2a3ef0
Victor Berchet <victor@suumit.com>
2018-02-19 03:12:29 +0000

    a8b5465 fix(ivy): update master with renamings (#22268)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-3ceee99
+ * @license Angular v6.0.0-beta.4-a8b5465
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-3ceee99');
+const VERSION = new Version('6.0.0-beta.4-a8b5465');
 
 /**
  * @fileoverview added by tsickle
