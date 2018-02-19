5091dd7f6d7218af1b81e9c4cacab89417425487
Victor Berchet <victor@suumit.com>
2018-02-19 04:11:08 +0000

    cdfedc1 fix(common): fix merge error in ng_if.ts

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-c726d1d
+ * @license Angular v5.2.5-cdfedc1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.5-c726d1d');
+const VERSION = new Version('5.2.5-cdfedc1');
 
 /**
  * @license
