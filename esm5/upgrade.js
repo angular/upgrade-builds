135146d5795ef79b1b34c4b763fcb9a116c33204
Victor Berchet <victor@suumit.com>
2018-02-21 23:15:25 +0000

    612cfec docs(aio): updates directive event hooks real capabilities (#16654)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-dfdade2
+ * @license Angular v5.2.5-612cfec
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.5-dfdade2');
+var VERSION = new Version('5.2.5-612cfec');
 
 /**
  * @fileoverview added by tsickle
