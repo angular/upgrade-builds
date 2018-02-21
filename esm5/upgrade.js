53d48840a8d6f77c69b084b7c1a08f658c7899d2
Victor Berchet <victor@suumit.com>
2018-02-21 19:28:16 +0000

    484802c build: make git revert messages valid (#22339)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-ee53577
+ * @license Angular v5.2.5-484802c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.5-ee53577');
+var VERSION = new Version('5.2.5-484802c');
 
 /**
  * @fileoverview added by tsickle
