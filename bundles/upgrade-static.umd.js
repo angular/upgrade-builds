ff371fd01395d9bfa167dd857f3f6679e1a80f54
Victor Berchet <victor@suumit.com>
2018-02-22 22:12:26 +0000

    b3ffeaa fix(ivy): OnDestroy hook should not be called twice for a directive on an element (#22350)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-f194d00
+ * @license Angular v6.0.0-beta.5-b3ffeaa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowser) { 'use strict';
 
 /**
- * @license Angular v6.0.0-beta.5-f194d00
+ * @license Angular v6.0.0-beta.5-b3ffeaa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -672,7 +672,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.5-f194d00');
+var VERSION = new _angular_core.Version('6.0.0-beta.5-b3ffeaa');
 
 /**
  * @license
--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-f194d00
+ * @license Angular v6.0.0-beta.5-b3ffeaa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-beta.5-f194d00');
+const VERSION = new Version('6.0.0-beta.5-b3ffeaa');
 
 /**
  * @license
