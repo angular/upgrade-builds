13b942393097aec2977b8460ae396d97edaa0b63
Victor Berchet <victor@suumit.com>
2018-02-22 22:04:15 +0000

    1776239 build: disable bazel-out symlink (#22375)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-da1b4d5
+ * @license Angular v5.2.6-1776239
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.6-da1b4d5');
+const VERSION = new Version('5.2.6-1776239');
 
 /**
  * @license
