a654abfc0d0ca505d61a97c97a2958c4aba054bd
Victor Berchet <victor@suumit.com>
2018-02-22 19:54:38 +0000

    894b098 test(ivy): add missing tests on directive lifecycle hooks (#22368)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-022ad4a
+ * @license Angular v6.0.0-beta.5-894b098
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-beta.5-022ad4a');
+const VERSION = new Version('6.0.0-beta.5-894b098');
 
 /**
  * @license
