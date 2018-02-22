21a04e44b7cb40a27e547d82167ccc331fadac7b
Victor Berchet <victor@suumit.com>
2018-02-22 21:46:03 +0000

    b7544cc feat(core): support metadata reflection for native class types (#22356)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-894b098
+ * @license Angular v6.0.0-beta.5-b7544cc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-beta.5-894b098');
+const VERSION = new Version('6.0.0-beta.5-b7544cc');
 
 /**
  * @license
