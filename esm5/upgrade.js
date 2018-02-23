a5bca1552cba356dcecf2be61a7cebfe12eebe63
Victor Berchet <victor@suumit.com>
2018-02-23 19:40:15 +0000

    cf91906 docs(aio): Essential JS 2 url updated (#19739) PR Close #19739

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-2b71889
+ * @license Angular v5.2.6-cf91906
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.6-2b71889');
+var VERSION = new Version('5.2.6-cf91906');
 
 /**
  * @fileoverview added by tsickle
