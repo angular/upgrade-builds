5e385fca5fd3bc46b53d739786ddaa7afd76f443
Victor Berchet <victor@suumit.com>
2018-02-23 18:17:09 +0000

    17c1577 ci: don't use bazel git_repository rule (#22406)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-150bac3
+ * @license Angular v5.2.6-17c1577
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -637,7 +637,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.6-150bac3');
+const VERSION = new Version('5.2.6-17c1577');
 
 /**
  * @license
