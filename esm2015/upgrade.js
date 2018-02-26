091ad19aa8077939d55ff35175a56f1ace0689f3
Alex Eagle <alexeagle@google.com>
2018-02-26 21:58:12 +0000

    5a32d7e build(aio): render default value for optional parameters (#22435)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-1f59981
+ * @license Angular v6.0.0-beta.5-5a32d7e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.5-1f59981');
+const VERSION = new Version('6.0.0-beta.5-5a32d7e');
 
 /**
  * @fileoverview added by tsickle
