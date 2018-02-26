ca4b27486e91abad9d3d16fb8353baa48a80bf28
Alex Eagle <alexeagle@google.com>
2018-02-26 21:56:50 +0000

    2a38d93 docs(aio): fix doc typo referring to `httpOptions` (#22456)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-a9a0e27
+ * @license Angular v5.2.6-2a38d93
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -641,7 +641,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.6-a9a0e27');
+const VERSION = new Version('5.2.6-2a38d93');
 
 /**
  * @license
