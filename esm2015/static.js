939505cce374a5d9cba7a1acc7867fd3e3bafa9d
Alex Eagle <alexeagle@google.com>
2018-02-27 02:13:07 +0000

    d033106 docs: update i18n guide for projects that don't use the cli (#21767)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-de02a7a
+ * @license Angular v5.2.6-d033106
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -641,7 +641,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.6-de02a7a');
+const VERSION = new Version('5.2.6-d033106');
 
 /**
  * @license
--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-de02a7a
+ * @license Angular v5.2.6-d033106
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -668,7 +668,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.6-de02a7a');
+var VERSION = new Version('5.2.6-d033106');
 
 /**
  * @license
