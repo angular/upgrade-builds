3df5a1fe63cffb037c57a92aaec2422b19044eba
Alex Eagle <alexeagle@google.com>
2018-02-28 19:19:23 +0000

    7d65356 build(aio): add `@usageNotes` tag def for API docs (#22401)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-930ecac
+ * @license Angular v6.0.0-beta.5-7d65356
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.5-930ecac');
+const VERSION = new Version('6.0.0-beta.5-7d65356');
 
 /**
  * @fileoverview added by tsickle
