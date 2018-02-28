c71c138671328752eea972cc7ea082982fc929d8
Alex Eagle <alexeagle@google.com>
2018-02-28 18:08:31 +0000

    930ecac build: update ts-api-guardian version (#22402)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-e95b61d
+ * @license Angular v6.0.0-beta.5-930ecac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -645,7 +645,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-beta.5-e95b61d');
+const VERSION = new Version('6.0.0-beta.5-930ecac');
 
 /**
  * @license
