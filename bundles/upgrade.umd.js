e19254418f965806e44de9d81f55dc2b46af99c5
Alex Eagle <alexeagle@google.com>
2018-02-28 23:20:20 +0000

    288851c release: add ng update package group metadata to angular (#22482)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-9eaf1bb
+ * @license Angular v6.0.0-beta.5-288851c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v6.0.0-beta.5-9eaf1bb
+ * @license Angular v6.0.0-beta.5-288851c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -28,7 +28,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.5-9eaf1bb');
+var VERSION = new _angular_core.Version('6.0.0-beta.5-288851c');
 
 /**
  * @fileoverview added by tsickle
