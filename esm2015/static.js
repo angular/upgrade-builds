6fa924672515cb0c8380b3770618cd0e472af55b
Alex Eagle <alexeagle@google.com>
2018-03-02 22:44:22 +0000

    d7e5d45 release: packageGroup should use scoped package names (#22538)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-ba8df8a
+ * @license Angular v6.0.0-beta.6-d7e5d45
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -645,7 +645,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-beta.6-ba8df8a');
+const VERSION = new Version('6.0.0-beta.6-d7e5d45');
 
 /**
  * @license
