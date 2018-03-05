976ea5a1817fcc73ad6476d795d6c0d12ebaf627
Alex Eagle <alexeagle@google.com>
2018-03-05 21:50:08 +0000

    2c2b62f fix(ivy): preventDefault when listener returns false (#22529)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-58932c7
+ * @license Angular v6.0.0-beta.6-2c2b62f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowser) { 'use strict';
 
 /**
- * @license Angular v6.0.0-beta.6-58932c7
+ * @license Angular v6.0.0-beta.6-2c2b62f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -680,7 +680,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.6-58932c7');
+var VERSION = new _angular_core.Version('6.0.0-beta.6-2c2b62f');
 
 /**
  * @license
