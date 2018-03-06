dbdb394ac07e648f0cf8783d860adfaed8001832
Alex Eagle <alexeagle@google.com>
2018-03-06 15:24:27 +0000

    789a47e fix(router): fix URL serialization so special characters are only encoded where needed (#22337)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-a3f7e30
+ * @license Angular v5.2.7-789a47e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -645,7 +645,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.7-a3f7e30');
+const VERSION = new Version('5.2.7-789a47e');
 
 /**
  * @license
