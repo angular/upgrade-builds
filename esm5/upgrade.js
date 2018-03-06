95152b21f23f1faac8658d1c1a0275223b157ecf
Alex Eagle <alexeagle@google.com>
2018-03-06 23:27:33 +0000

    5876fb0 docs(aio): update deprecated Http reference to HttpClientModule, remove Http reference because another context is used (#21984)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-5b7b208
+ * @license Angular v5.2.7-5876fb0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.7-5b7b208');
+var VERSION = new Version('5.2.7-5876fb0');
 
 /**
  * @fileoverview added by tsickle
