fbb2ae55038fcecca120c7fe0b8265e0ded2c570
Alex Eagle <alexeagle@google.com>
2018-03-07 22:59:40 +0000

    ad8fb84 test: include router in public_api_guard tests (#22628)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-7816791
+ * @license Angular v6.0.0-beta.6-ad8fb84
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v6.0.0-beta.6-7816791
+ * @license Angular v6.0.0-beta.6-ad8fb84
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -28,7 +28,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.6-7816791');
+var VERSION = new _angular_core.Version('6.0.0-beta.6-ad8fb84');
 
 /**
  * @fileoverview added by tsickle
