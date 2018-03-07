08fec51b854c4e9a9b6e98f161166eab0b98c5a2
Alex Eagle <alexeagle@google.com>
2018-03-07 23:32:06 +0000

    8ad4ae0 docs: add changelog for 6.0.0-beta.7

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-ad8fb84
+ * @license Angular v6.0.0-beta.7-8ad4ae0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.6-ad8fb84');
+const VERSION = new Version('6.0.0-beta.7-8ad4ae0');
 
 /**
  * @fileoverview added by tsickle
