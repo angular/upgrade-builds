6876956190e47f2a40fb7a4e30b06d424653df74
Kara Erickson <karakara@google.com>
2018-03-08 19:23:56 +0000

    2ca77d8 docs: refactor revert() and call to lifecylce hook, edit doc to changes (#22094)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-73c203f
+ * @license Angular v6.0.0-beta.7-2ca77d8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.7-73c203f');
+const VERSION = new Version('6.0.0-beta.7-2ca77d8');
 
 /**
  * @fileoverview added by tsickle
