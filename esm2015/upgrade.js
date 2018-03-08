1b0c90ea09556a87b4860b405ee86aa091995e6d
Kara Erickson <karakara@google.com>
2018-03-08 19:19:38 +0000

    c499c8f build: update angular-bot g3sync paths (#22637)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-4c089c1
+ * @license Angular v6.0.0-beta.7-c499c8f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.7-4c089c1');
+const VERSION = new Version('6.0.0-beta.7-c499c8f');
 
 /**
  * @fileoverview added by tsickle
