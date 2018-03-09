f2fe978d5b05f5a568f1f59980daa856edb690c4
Kara Erickson <karakara@google.com>
2018-03-09 01:07:00 +0000

    5412e10 docs(core): replace ancient live demos (#22665)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-489fec1
+ * @license Angular v6.0.0-beta.7-5412e10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.7-489fec1');
+const VERSION = new Version('6.0.0-beta.7-5412e10');
 
 /**
  * @fileoverview added by tsickle
