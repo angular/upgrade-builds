89bd6b53da6a54d42e22259efdbe85b65f533afd
Kara Erickson <karakara@google.com>
2018-03-13 19:35:56 +0000

    34e355a build(bazel): ng_package rxjs/operators rollup config (#22744)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-58b94e6
+ * @license Angular v6.0.0-beta.7-34e355a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -645,7 +645,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-beta.7-58b94e6');
+const VERSION = new Version('6.0.0-beta.7-34e355a');
 
 /**
  * @license
