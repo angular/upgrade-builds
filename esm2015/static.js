311033fef0573c80244e980f4621bfb1f2a65713
Kara Erickson <karakara@google.com>
2018-03-13 16:49:39 +0000

    db56836 feat: tree-shakeable providers API updates (#22655)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-21e44c6
+ * @license Angular v6.0.0-beta.7-db56836
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -645,7 +645,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-beta.7-21e44c6');
+const VERSION = new Version('6.0.0-beta.7-db56836');
 
 /**
  * @license
