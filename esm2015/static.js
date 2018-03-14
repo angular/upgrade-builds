ec520ac8f2247f8127b5f1a28bbc874f93f4220c
Kara Erickson <karakara@google.com>
2018-03-14 05:08:17 +0000

    4ac606b docs(compiler): fix spelling errors (#22704)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-51027d7
+ * @license Angular v6.0.0-beta.7-4ac606b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -645,7 +645,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-beta.7-51027d7');
+const VERSION = new Version('6.0.0-beta.7-4ac606b');
 
 /**
  * @license
