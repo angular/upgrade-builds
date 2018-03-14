73c97d7acd08aebda838d742989058ff4bda1844
Kara Erickson <karakara@google.com>
2018-03-14 17:45:57 +0000

    1f97343 docs: testing - highlight dispatchEvent (#22726)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-6f0dad1
+ * @license Angular v6.0.0-beta.7-1f97343
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -645,7 +645,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-beta.7-6f0dad1');
+const VERSION = new Version('6.0.0-beta.7-1f97343');
 
 /**
  * @license
