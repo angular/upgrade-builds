497b46efd99fab19b4891edbe2cc86dd118b3f35
Miško Hevery <misko@hevery.com>
2018-03-15 19:12:15 +0000

    7d9de17 build: add release helper scripts (#22378) (#22781)

--- esm5/upgrade.js
+++ esm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-db0afa9
+ * @license Angular v5.2.9-7d9de17
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.9-db0afa9');
+var VERSION = new Version('5.2.9-7d9de17');
 
 /**
  * @fileoverview added by tsickle
