cfcce1c2f34d66772337c0a6af4166300bb7ddb2
Misko Hevery <misko@hevery.com>
2018-03-18 19:05:10 +0000

    4f21d37 refactor(ivy): move hostBindings calls out of template (#22833)

--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-beta.7-ce63dc6');
+export var VERSION = new Version('6.0.0-beta.7-4f21d37');
 //# sourceMappingURL=version.js.map
