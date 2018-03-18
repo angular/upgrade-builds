edb824d861f9ebe01096925cee438208f6d3ca0d
Igor Minar <igor@angular.io>
2018-03-18 21:31:50 +0000

    0bede54 style: fix formatting issues on the master branch (#22854)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-beta.7-e27cfd6');
+export const VERSION = new Version('6.0.0-beta.7-0bede54');
 //# sourceMappingURL=version.js.map
--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-beta.7-e27cfd6');
+export var VERSION = new Version('6.0.0-beta.7-0bede54');
 //# sourceMappingURL=version.js.map
