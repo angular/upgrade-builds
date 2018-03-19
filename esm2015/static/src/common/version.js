a458cf5f312a417c46435df7ae700675b16b12c2
Miško Hevery <misko@misko-ubi.cbf.corp.google.com>
2018-03-19 21:59:12 +0000

    bfe077a fix(bazel): correct expected outs for external sources in ng_module (#22755)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-beta.7-1a0cb21');
+export const VERSION = new Version('6.0.0-beta.7-bfe077a');
 //# sourceMappingURL=version.js.map
--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-beta.7-1a0cb21');
+export var VERSION = new Version('6.0.0-beta.7-bfe077a');
 //# sourceMappingURL=version.js.map
