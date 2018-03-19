5215187e9cd5c545ad3dbf5ae11f97387f93b1d5
Miško Hevery <misko@misko-ubi.cbf.corp.google.com>
2018-03-19 21:53:27 +0000

    1a0cb21 refactor(ivy): remove unnecessary binding from hello world (#22848)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-beta.7-0bede54');
+export const VERSION = new Version('6.0.0-beta.7-1a0cb21');
 //# sourceMappingURL=version.js.map
