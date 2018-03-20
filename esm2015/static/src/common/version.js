fbe42e3a70842d7cbcc1455e0df14492834d3c62
Igor Minar <igor@angular.io>
2018-03-20 23:17:12 +0000

    17fb983 fix(ivy): fix type error in newer version of TS (#22897)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-beta.7-3cc5c2e');
+export const VERSION = new Version('6.0.0-beta.7-17fb983');
 //# sourceMappingURL=version.js.map
