f0c50c23d1ed67e588fdbab04dc47876c51daf13
Igor Minar <igor@angular.io>
2018-03-21 20:47:48 +0000

    b12ea30 test: remove gulp public-api:update docs (#22914) PR Close #22914

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-rc.0-8c2a578');
+export const VERSION = new Version('6.0.0-rc.0-b12ea30');
 //# sourceMappingURL=version.js.map
