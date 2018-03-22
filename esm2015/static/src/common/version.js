97c4eddd3861585f75cbd09092f133f20cdc9021
Matias Niemelä <matias@yearofmoo.com>
2018-03-22 22:53:43 +0000

    d77bb46 revert: docs(ivy): update STATUS.md with compiler work breakdown (#22874)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-rc.0-bb58664');
+export const VERSION = new Version('6.0.0-rc.0-d77bb46');
 //# sourceMappingURL=version.js.map
--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-rc.0-bb58664');
+export var VERSION = new Version('6.0.0-rc.0-d77bb46');
 //# sourceMappingURL=version.js.map
