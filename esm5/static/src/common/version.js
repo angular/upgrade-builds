731a9a2ed9b8ac8e2b9637dcb13c3d0d6b03af3f
Matias Niemelä <matias@yearofmoo.com>
2018-03-22 23:27:14 +0000

    e5e1b0d fix(animations): treat numeric state name values as strings (#22923)

--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-rc.0-99711b1');
+export var VERSION = new Version('6.0.0-rc.0-e5e1b0d');
 //# sourceMappingURL=version.js.map
