2e17c9783c90c8218acfaf39e92923b978db3e6b
Matias Niemelä <matsko@matskocloud.c.googlers.com>
2018-03-22 22:44:05 +0000

    49f6d1d build: rm --noimplicit_deps from bazel query (#22912)

--- esm2015/upgrade.js
+++ esm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-641cc49
+ * @license Angular v5.2.9-49f6d1d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.9-641cc49');
+const VERSION = new Version('5.2.9-49f6d1d');
 
 /**
  * @fileoverview added by tsickle
