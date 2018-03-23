5e468eca12603db046843b411f167d478e1aef78
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 18:14:33 +0000

    7966744 fix(common): titlecase pipe (#22600)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-rc.0-d9a0a8f');
+export const VERSION = new Version('6.0.0-rc.0-7966744');
 //# sourceMappingURL=version.js.map
