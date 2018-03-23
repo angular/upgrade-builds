1fb2dcf9072cd210c39b552470a7d2d764e0984e
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 20:54:07 +0000

    d7f4aa6 docs(common): add `HttpParamsOptions` to the public API (#20332)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-4be8b3f
+ * @license Angular v5.2.9-d7f4aa6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.2.9-4be8b3f
+ * @license Angular v5.2.9-d7f4aa6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -28,7 +28,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.9-4be8b3f');
+var VERSION = new _angular_core.Version('5.2.9-d7f4aa6');
 
 /**
  * @fileoverview added by tsickle
