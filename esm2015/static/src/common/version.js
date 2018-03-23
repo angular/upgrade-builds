6b39de24665da34e95e19515251331b3352937d8
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 19:28:44 +0000

    b9cbe83 docs(ivy): update STATUS.md with compiler work breakdown (#22874)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-rc.0-60d9983');
+export const VERSION = new Version('6.0.0-rc.0-b9cbe83');
 //# sourceMappingURL=version.js.map
