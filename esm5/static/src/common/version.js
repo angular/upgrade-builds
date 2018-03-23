3997add43ea4b818dae6cba8d5ca81aebfd5b678
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 17:32:42 +0000

    16f021c docs(aio): fix TS warning error  - filter expects a boolean function param (#22954)

--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-rc.0-3a30f5d');
+export var VERSION = new Version('6.0.0-rc.0-16f021c');
 //# sourceMappingURL=version.js.map
