48a9c9821a4ebc9802d2c207561ca1581847a863
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 01:12:06 +0000

    8ca26a9 ci(compiler): run compiler tests in bazel (#22900)

--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-rc.0-3498106');
+export var VERSION = new Version('6.0.0-rc.0-8ca26a9');
 //# sourceMappingURL=version.js.map
