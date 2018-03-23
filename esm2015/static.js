261819c3519d3bddb423297ec838905039e0f605
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 18:35:29 +0000

    4be8b3f docs: update available platforms for `test.sh` (#22958)

--- esm2015/static.js
+++ esm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-8d1e640
+ * @license Angular v5.2.9-4be8b3f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -642,7 +642,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('5.2.9-8d1e640');
+const VERSION = new Version('5.2.9-4be8b3f');
 
 /**
  * @license
