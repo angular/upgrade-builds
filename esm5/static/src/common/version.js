acb15ea51ac98456d9a810e8a8d4cbf815820c1e
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 18:34:13 +0000

    60d9983 docs: update available platforms for `test.sh` (#22958)

--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-rc.0-7966744');
+export var VERSION = new Version('6.0.0-rc.0-60d9983');
 //# sourceMappingURL=version.js.map
