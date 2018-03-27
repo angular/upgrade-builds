d83e2dfa23466fd9a4335e8d346881b60594b838
Matias Niemelä <matias@yearofmoo.com>
2018-03-27 02:56:55 +0000

    27e14b2 feat(bazel): prefix private-export (barred-latin-o) symbols (#23007)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-rc.0-2388f24');
+export const VERSION = new Version('6.0.0-rc.0-27e14b2');
 //# sourceMappingURL=version.js.map
