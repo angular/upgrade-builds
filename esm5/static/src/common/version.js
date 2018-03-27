a501d74cdefc52fa1b6e7fe957941565d6331d1d
Matias Niemelä <matias@yearofmoo.com>
2018-03-27 19:32:15 +0000

    ab348ee build: update browserstack key (#23026)

--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-rc.0-f739f75');
+export var VERSION = new Version('6.0.0-rc.0-ab348ee');
 //# sourceMappingURL=version.js.map
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-f739f75
+ * @license Angular v6.0.0-rc.0-ab348ee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-f739f75');
+var VERSION = new Version('6.0.0-rc.0-ab348ee');
 
 /**
  * @fileoverview added by tsickle
