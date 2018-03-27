776782783126ef602a95867ae0a73a3a408dda32
Matias Niemelä <matias@yearofmoo.com>
2018-03-27 19:24:44 +0000

    0ebdb3d style: typo fix amendments (#22975)

--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-rc.0-910a16a');
+export var VERSION = new Version('6.0.0-rc.0-0ebdb3d');
 //# sourceMappingURL=version.js.map
