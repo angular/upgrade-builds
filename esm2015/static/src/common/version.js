e4d0269d64525efcb42de1f6c1e4ec573eae2876
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-28 16:57:29 +0000

    e2e80ec refactor(ivy): remove pipe references from the template (#23032)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-rc.0-5a86f71');
+export const VERSION = new Version('6.0.0-rc.0-e2e80ec');
 //# sourceMappingURL=version.js.map
--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-5a86f71
+ * @license Angular v6.0.0-rc.0-e2e80ec
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -642,7 +642,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-const VERSION = new Version('6.0.0-rc.0-5a86f71');
+const VERSION = new Version('6.0.0-rc.0-e2e80ec');
 
 /**
  * @license
