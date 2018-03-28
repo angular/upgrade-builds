47692d45d8c3d6225c883fb8e62d389ff3918228
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-28 15:24:07 +0000

    e1ea7ed docs: update examples for tree-shakeable providers (#22961)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-rc.0-ed53c5c');
+export const VERSION = new Version('6.0.0-rc.0-e1ea7ed');
 //# sourceMappingURL=version.js.map
--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-rc.0-ed53c5c');
+export var VERSION = new Version('6.0.0-rc.0-e1ea7ed');
 //# sourceMappingURL=version.js.map
