535c038e49bfcaed5114f6b84ae303dbfa6449d2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-29 16:20:30 +0000

    4f7c369 fix(compiler): fix support for html-like text in translatable attributes (#23053)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-0d4fe38
+ * @license Angular v5.2.9-4f7c369
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -666,7 +666,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.9-0d4fe38');
+var VERSION = new Version('5.2.9-4f7c369');
 
 /**
  * @license
