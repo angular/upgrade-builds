a8b82501eca65736c13cc939c8603bd86731ae1a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-29 20:36:50 +0000

    9fb08e2 ci(language-service): enable language service tests in bazel (#23001)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-rc.0-de0b13d');
+export const VERSION = new Version('6.0.0-rc.0-9fb08e2');
 //# sourceMappingURL=version.js.map
