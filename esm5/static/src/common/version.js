3faa916d3cec1a8b24ab1685668413426653ae0b
Igor Minar <igor@angular.io>
2018-03-30 21:27:16 +0000

    1527878 release(bazel): change publish-next script to publish bazel artifacts (#23097)

--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-rc.0-22cb2c9');
+export var VERSION = new Version('6.0.0-rc.0-1527878');
 //# sourceMappingURL=version.js.map
