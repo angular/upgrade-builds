72464854a4b0d6ff17d272b546477e7f7ebdf6ea
Igor Minar <igor@angular.io>
2018-03-30 22:15:31 +0000

    a30728c docs: add changelog for 6.0.0-rc.1

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-rc.0-1527878');
+export const VERSION = new Version('6.0.0-rc.1-a30728c');
 //# sourceMappingURL=version.js.map
