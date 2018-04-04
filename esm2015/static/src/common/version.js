053cc1af5b1b3733b19d39473b4344940d80a4f3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 16:07:34 +0000

    412b85b docs: add a link to "Impact of polymorphism [...]" to the perf notes (#23151)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-rc.1-d37064c');
+export const VERSION = new Version('6.0.0-rc.1-412b85b');
 //# sourceMappingURL=version.js.map
