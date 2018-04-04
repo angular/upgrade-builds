3a98bc8870879f6a03c6e82c4747c6be196d167a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 16:13:44 +0000

    9703079 test(router): fix typo in expectation (#23137)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-dd61595
+ * @license Angular v5.2.9-9703079
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -668,7 +668,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.9-dd61595');
+var VERSION = new Version('5.2.9-9703079');
 
 /**
  * @license
