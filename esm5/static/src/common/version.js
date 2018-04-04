b23338c4aedc655968a2dffb1f959aa11fcb61b5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 16:51:21 +0000

    5c8340a refactor(ivy): misc refactoring (#23154)

--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-rc.1-46eadb5');
+export var VERSION = new Version('6.0.0-rc.1-5c8340a');
 //# sourceMappingURL=version.js.map
