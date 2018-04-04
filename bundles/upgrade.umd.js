605f7c905e3d510e1cbb67c2004eccd6b5a713ad
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 17:16:17 +0000

    3900c36 refactor(common): simplify NgClass code, add comments (#21937)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-9703079
+ * @license Angular v5.2.9-3900c36
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.2.9-9703079
+ * @license Angular v5.2.9-3900c36
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -28,7 +28,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.9-9703079');
+var VERSION = new _angular_core.Version('5.2.9-3900c36');
 
 /**
  * @fileoverview added by tsickle
