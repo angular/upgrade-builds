89c87d8034ce674c5b2e5ecf3fc86d662af86ee0
Igor Minar <igor@angular.io>
2018-04-05 16:55:43 +0000

    92821e3 docs(forms): remove unnecessary `{@link Injector}` jsdoc tags (#23186)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-rc.1-8ea15b4');
+export const VERSION = new Version('6.0.0-rc.1-92821e3');
 //# sourceMappingURL=version.js.map
--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-rc.1-8ea15b4');
+export var VERSION = new Version('6.0.0-rc.1-92821e3');
 //# sourceMappingURL=version.js.map
