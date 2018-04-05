037f48b76456ffe54cf67f142b3da23edc1b6fde
Igor Minar <igor@angular.io>
2018-04-05 23:51:49 +0000

    a1231be docs: fixed live example for the lifecycle hooks. (#23201)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-73172dd
+ * @license Angular v5.2.9-a1231be
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowser) { 'use strict';
 
 /**
- * @license Angular v5.2.9-73172dd
+ * @license Angular v5.2.9-a1231be
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -676,7 +676,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('5.2.9-73172dd');
+var VERSION = new _angular_core.Version('5.2.9-a1231be');
 
 /**
  * @license
--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-73172dd
+ * @license Angular v5.2.9-a1231be
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.2.9-73172dd
+ * @license Angular v5.2.9-a1231be
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -28,7 +28,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.9-73172dd');
+var VERSION = new _angular_core.Version('5.2.9-a1231be');
 
 /**
  * @fileoverview added by tsickle
