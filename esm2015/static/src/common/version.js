c6cd5189857ee2668e3a318f719dc47a11657cfa
Igor Minar <igor@angular.io>
2018-04-05 16:52:01 +0000

    8ea15b4 docs(core): remove unnecessary `{@link Injector}` jsdoc tag (#23185)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-rc.1-5db9ab1');
+export const VERSION = new Version('6.0.0-rc.1-8ea15b4');
 //# sourceMappingURL=version.js.map
