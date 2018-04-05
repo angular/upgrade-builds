aac3eae7b19c6fba9251c7b7bef67b527b40faba
Igor Minar <igor@angular.io>
2018-04-05 18:07:38 +0000

    fc813f6 fix(ivy): fix issue with refreshing embedded views (#23164)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-rc.1-3b607e6');
+export const VERSION = new Version('6.0.0-rc.1-fc813f6');
 //# sourceMappingURL=version.js.map
--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-rc.1-3b607e6');
+export var VERSION = new Version('6.0.0-rc.1-fc813f6');
 //# sourceMappingURL=version.js.map
