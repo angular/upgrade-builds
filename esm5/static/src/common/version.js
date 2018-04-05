fab40e3abbb39fb872bfba20e97319bdd4871b04
Igor Minar <igor@angular.io>
2018-04-05 23:54:28 +0000

    b5f41f2 docs: fixed live example for the lifecycle hooks. (#23201)

--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-rc.2-629629d');
+export var VERSION = new Version('6.0.0-rc.2-b5f41f2');
 //# sourceMappingURL=version.js.map
