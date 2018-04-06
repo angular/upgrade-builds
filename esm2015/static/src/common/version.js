d78824319f261c77b47dbe465fa74f68d9f3e3a0
Igor Minar <igor@angular.io>
2018-04-06 18:53:21 +0000

    3a9d916 test(ivy): remove outdated render3 tests (#23229)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-rc.2-eb9968a');
+export const VERSION = new Version('6.0.0-rc.2-3a9d916');
 //# sourceMappingURL=version.js.map
--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-rc.2-eb9968a');
+export var VERSION = new Version('6.0.0-rc.2-3a9d916');
 //# sourceMappingURL=version.js.map
