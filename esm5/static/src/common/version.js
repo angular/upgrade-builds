6db602025a9fabf140f9826b5d7ece93fbd65f14
Igor Minar <igor@angular.io>
2018-04-07 00:12:09 +0000

    33f8120 docs: add changelog for 6.0.0-rc.3

--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-rc.2-da58a55');
+export var VERSION = new Version('6.0.0-rc.3-33f8120');
 //# sourceMappingURL=version.js.map
--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.2-da58a55
+ * @license Angular v6.0.0-rc.3-33f8120
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -667,7 +667,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('6.0.0-rc.2-da58a55');
+var VERSION = new Version('6.0.0-rc.3-33f8120');
 
 /**
  * @license
