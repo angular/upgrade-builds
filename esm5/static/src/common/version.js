bc144d17d9b5eade519ca3a62ae3610eab03b8d3
Igor Minar <igor@angular.io>
2018-04-09 22:41:44 +0000

    30a6861 build: update zone.js to 0.8.26 to fix some regression bugs (#23227)

--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-rc.3-3e8eef6');
+export var VERSION = new Version('6.0.0-rc.3-30a6861');
 //# sourceMappingURL=version.js.map
