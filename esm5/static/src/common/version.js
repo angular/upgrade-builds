c99d1fc60682dbda763271fbe8c7ca922bd476cb
Igor Minar <igor@angular.io>
2018-04-09 22:43:08 +0000

    f4c56f4 feat(ivy): implement some of the ViewContainerRef API (#23189)

--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-rc.3-5a298b1');
+export var VERSION = new Version('6.0.0-rc.3-f4c56f4');
 //# sourceMappingURL=version.js.map
