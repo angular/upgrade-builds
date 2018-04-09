20d027e211270e1ab981a0495bb2834629977614
Igor Minar <igor@angular.io>
2018-04-09 22:42:26 +0000

    5a298b1 fix(common): workaround UMD/webpack limitation (#23271)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-rc.3-30a6861');
+export const VERSION = new Version('6.0.0-rc.3-5a298b1');
 //# sourceMappingURL=version.js.map
--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-rc.3-30a6861');
+export var VERSION = new Version('6.0.0-rc.3-5a298b1');
 //# sourceMappingURL=version.js.map
