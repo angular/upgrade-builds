98c7c831fa46d516a917e840c8ce9f69da7a8f45
Igor Minar <igor@angular.io>
2018-04-09 22:41:16 +0000

    3e8eef6 fix(bazel): set rollup output.name and amd.id (#23274)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-rc.3-1fac5f4');
+export const VERSION = new Version('6.0.0-rc.3-3e8eef6');
 //# sourceMappingURL=version.js.map
