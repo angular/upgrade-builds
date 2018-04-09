0c094b2e8d7b345c7395201bf0dffdabb6229eb9
Igor Minar <igor@angular.io>
2018-04-09 23:36:49 +0000

    37c1634 build: Add sourcemap output from ng_rollup_bundle (#23276)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-5814355
+ * @license Angular v6.0.0-rc.3-37c1634
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v6.0.0-rc.3-5814355
+ * @license Angular v6.0.0-rc.3-37c1634
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -28,7 +28,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.3-5814355');
+var VERSION = new _angular_core.Version('6.0.0-rc.3-37c1634');
 
 /**
  * @fileoverview added by tsickle
--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('6.0.0-rc.3-5814355');
+export var VERSION = new Version('6.0.0-rc.3-37c1634');
 //# sourceMappingURL=version.js.map
