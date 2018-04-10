e580056a72c3cda71631b370004beea2387ae90d
Igor Minar <igor@angular.io>
2018-04-10 20:42:16 +0000

    c973830 refactor(ivy): clean projection support (#23287)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-rc.3-f1db789');
+export const VERSION = new Version('6.0.0-rc.3-c973830');
 //# sourceMappingURL=version.js.map
