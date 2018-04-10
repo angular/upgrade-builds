df57f330ab878f8f6843837b90c482b6345fcb1a
Igor Minar <igor@angular.io>
2018-04-10 20:39:27 +0000

    f1db789 test(ivy): update todo app to http://todomvc.com (#23168)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-rc.3-8555a3a');
+export const VERSION = new Version('6.0.0-rc.3-f1db789');
 //# sourceMappingURL=version.js.map
