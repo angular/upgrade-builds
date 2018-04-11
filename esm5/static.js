eac10214bb6ac8e50b2b591e58e46b8bf5a38117
Igor Minar <igor@angular.io>
2018-04-11 01:54:03 +0000

    22686f8 build(aio): fix scripts/test-production.sh file permission issue

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-bda6908
+ * @license Angular v5.2.9-22686f8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -668,7 +668,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.9-bda6908');
+var VERSION = new Version('5.2.9-22686f8');
 
 /**
  * @license
