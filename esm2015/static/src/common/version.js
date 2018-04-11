846e35bf365f9724505859edc9d309b31db19e2a
Igor Minar <igor@angular.io>
2018-04-11 06:23:33 +0000

    b3a10e0 build: update Bazel dependency to 0.11.1 (#23297)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export const VERSION = new Version('6.0.0-rc.3-0cb4f12');
+export const VERSION = new Version('6.0.0-rc.3-b3a10e0');
 //# sourceMappingURL=version.js.map
--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export var VERSION = new Version('6.0.0-rc.3-0cb4f12');
+export var VERSION = new Version('6.0.0-rc.3-b3a10e0');
 //# sourceMappingURL=version.js.map
