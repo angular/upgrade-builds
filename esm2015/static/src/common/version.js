58a0429a1f0a737f167beccc2cbc2d59eee2a1ac
Victor Berchet <victor@suumit.com>
2018-04-11 22:57:59 +0000

    a4ac872 build: use rxjs@6.0.0-terrific-rc.3 (#23327)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export const VERSION = new Version('6.0.0-rc.3-0d516f1');
+export const VERSION = new Version('6.0.0-rc.3-a4ac872');
 //# sourceMappingURL=version.js.map
--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-0d516f1
+ * @license Angular v6.0.0-rc.3-a4ac872
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -643,7 +643,7 @@ function downgradeInjectable(token) {
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.3-0d516f1');
+const VERSION = new Version('6.0.0-rc.3-a4ac872');
 
 /**
  * @license
