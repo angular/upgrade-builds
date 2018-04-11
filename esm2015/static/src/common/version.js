0c2d9bb7930b534080ff52ef5a5ca4ad8784ebf9
Igor Minar <igor@angular.io>
2018-04-11 01:48:54 +0000

    ee14579 build(aio): fix scripts/test-production.sh file permission issue

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export const VERSION = new Version('6.0.0-rc.3-c973830');
+export const VERSION = new Version('6.0.0-rc.3-ee14579');
 //# sourceMappingURL=version.js.map
