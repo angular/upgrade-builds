2c3f96f990d46a747b2aca339ed26284a158c325
Igor Minar <igor@angular.io>
2018-04-12 08:44:26 +0000

    01975ff docs: add changelog for 6.0.0-rc.4

---
+++ esm5/src/common/downgrade_component_adapter.js
@@ -0,0 +1,398 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { ApplicationRef, ChangeDetectorRef, Injector, SimpleChange, Testability, TestabilityRegistry } from '@angular/core';
+import { PropertyBinding } from './component_info';
+import { $SCOPE } from './constants';
+import { getComponentName, hookupNgModel, strictEquals } from './util';
+var /** @type {?} */ INITIAL_VALUE = {
+    __UNINITIALIZED__: true
+};
+var DowngradeComponentAdapter = /** @class */ (function () {
+    function DowngradeComponentAdapter(element, attrs, scope, ngModel, parentInjector, $injector, $compile, $parse, componentFactory, wrapCallback) {
+        this.element = element;
+        this.attrs = attrs;
+        this.scope = scope;
+        this.ngModel = ngModel;
+        this.parentInjector = parentInjector;
+        this.$injector = $injector;
+        this.$compile = $compile;
+        this.$parse = $parse;
+        this.componentFactory = componentFactory;
+        this.wrapCallback = wrapCallback;
+        this.implementsOnChanges = false;
+        this.inputChangeCount = 0;
+        this.inputChanges = {};
+        this.componentScope = scope.$new();
+    }
+    /**
+     * @return {?}
+     */
+    DowngradeComponentAdapter.prototype.compileContents = /**
+     * @return {?}
+     */
+    function () {
+        var _this = this;
+        var /** @type {?} */ compiledProjectableNodes = [];
+        var /** @type {?} */ projectableNodes = this.groupProjectableNodes();
+        var /** @type {?} */ linkFns = projectableNodes.map(function (nodes) { return _this.$compile(nodes); }); /** @type {?} */
+        ((this.element.empty))();
+        linkFns.forEach(function (linkFn) {
+            linkFn(_this.scope, function (clone) {
+                compiledProjectableNodes.push(clone); /** @type {?} */
+                ((_this.element.append))(clone);
+            });
+        });
+        return compiledProjectableNodes;
+    };
+    /**
+     * @param {?} projectableNodes
+     * @return {?}
+     */
+    DowngradeComponentAdapter.prototype.createComponent = /**
+     * @param {?} projectableNodes
+     * @return {?}
+     */
+    function (projectableNodes) {
+        var /** @type {?} */ providers = [{ provide: $SCOPE, useValue: this.componentScope }];
+        var /** @type {?} */ childInjector = Injector.create({ providers: providers, parent: this.parentInjector, name: 'DowngradeComponentAdapter' });
+        this.componentRef =
+            this.componentFactory.create(childInjector, projectableNodes, this.element[0]);
+        this.viewChangeDetector = this.componentRef.injector.get(ChangeDetectorRef);
+        this.changeDetector = this.componentRef.changeDetectorRef;
+        this.component = this.componentRef.instance;
+        // testability hook is commonly added during component bootstrap in
+        // packages/core/src/application_ref.bootstrap()
+        // in downgraded application, component creation will take place here as well as adding the
+        // testability hook.
+        var /** @type {?} */ testability = this.componentRef.injector.get(Testability, null);
+        if (testability) {
+            this.componentRef.injector.get(TestabilityRegistry)
+                .registerApplication(this.componentRef.location.nativeElement, testability);
+        }
+        hookupNgModel(this.ngModel, this.component);
+    };
+    /**
+     * @param {?} needsNgZone
+     * @param {?=} propagateDigest
+     * @return {?}
+     */
+    DowngradeComponentAdapter.prototype.setupInputs = /**
+     * @param {?} needsNgZone
+     * @param {?=} propagateDigest
+     * @return {?}
+     */
+    function (needsNgZone, propagateDigest) {
+        var _this = this;
+        if (propagateDigest === void 0) { propagateDigest = true; }
+        var /** @type {?} */ attrs = this.attrs;
+        var /** @type {?} */ inputs = this.componentFactory.inputs || [];
+        var _loop_1 = function (i) {
+            var /** @type {?} */ input = new PropertyBinding(inputs[i].propName, inputs[i].templateName);
+            var /** @type {?} */ expr = null;
+            if (attrs.hasOwnProperty(input.attr)) {
+                var /** @type {?} */ observeFn_1 = (function (prop) {
+                    var /** @type {?} */ prevValue = INITIAL_VALUE;
+                    return function (currValue) {
+                        // Initially, both `$observe()` and `$watch()` will call this function.
+                        if (!strictEquals(prevValue, currValue)) {
+                            if (prevValue === INITIAL_VALUE) {
+                                prevValue = currValue;
+                            }
+                            _this.updateInput(prop, prevValue, currValue);
+                            prevValue = currValue;
+                        }
+                    };
+                })(input.prop);
+                attrs.$observe(input.attr, observeFn_1);
+                // Use `$watch()` (in addition to `$observe()`) in order to initialize the input in time
+                // for `ngOnChanges()`. This is necessary if we are already in a `$digest`, which means that
+                // `ngOnChanges()` (which is called by a watcher) will run before the `$observe()` callback.
+                var /** @type {?} */ unwatch_1 = this_1.componentScope.$watch(function () {
+                    /** @type {?} */ ((unwatch_1))();
+                    unwatch_1 = null;
+                    observeFn_1(attrs[input.attr]);
+                });
+            }
+            else if (attrs.hasOwnProperty(input.bindAttr)) {
+                expr = attrs[input.bindAttr];
+            }
+            else if (attrs.hasOwnProperty(input.bracketAttr)) {
+                expr = attrs[input.bracketAttr];
+            }
+            else if (attrs.hasOwnProperty(input.bindonAttr)) {
+                expr = attrs[input.bindonAttr];
+            }
+            else if (attrs.hasOwnProperty(input.bracketParenAttr)) {
+                expr = attrs[input.bracketParenAttr];
+            }
+            if (expr != null) {
+                var /** @type {?} */ watchFn = (function (prop) {
+                    return function (currValue, prevValue) {
+                        return _this.updateInput(prop, prevValue, currValue);
+                    };
+                })(input.prop);
+                this_1.componentScope.$watch(expr, watchFn);
+            }
+        };
+        var this_1 = this;
+        for (var /** @type {?} */ i = 0; i < inputs.length; i++) {
+            _loop_1(i);
+        }
+        // Invoke `ngOnChanges()` and Change Detection (when necessary)
+        var /** @type {?} */ detectChanges = function () { return _this.changeDetector.detectChanges(); };
+        var /** @type {?} */ prototype = this.componentFactory.componentType.prototype;
+        this.implementsOnChanges = !!(prototype && (/** @type {?} */ (prototype)).ngOnChanges);
+        this.componentScope.$watch(function () { return _this.inputChangeCount; }, this.wrapCallback(function () {
+            // Invoke `ngOnChanges()`
+            if (_this.implementsOnChanges) {
+                var /** @type {?} */ inputChanges = _this.inputChanges;
+                _this.inputChanges = {};
+                (/** @type {?} */ (_this.component)).ngOnChanges(/** @type {?} */ ((inputChanges)));
+            }
+            _this.viewChangeDetector.markForCheck();
+            // If opted out of propagating digests, invoke change detection when inputs change.
+            if (!propagateDigest) {
+                detectChanges();
+            }
+        }));
+        // If not opted out of propagating digests, invoke change detection on every digest
+        if (propagateDigest) {
+            this.componentScope.$watch(this.wrapCallback(detectChanges));
+        }
+        // If necessary, attach the view so that it will be dirty-checked.
+        // (Allow time for the initial input values to be set and `ngOnChanges()` to be called.)
+        if (needsNgZone || !propagateDigest) {
+            var /** @type {?} */ unwatch_2 = this.componentScope.$watch(function () {
+                /** @type {?} */ ((unwatch_2))();
+                unwatch_2 = null;
+                var /** @type {?} */ appRef = _this.parentInjector.get(ApplicationRef);
+                appRef.attachView(_this.componentRef.hostView);
+            });
+        }
+    };
+    /**
+     * @return {?}
+     */
+    DowngradeComponentAdapter.prototype.setupOutputs = /**
+     * @return {?}
+     */
+    function () {
+        var /** @type {?} */ attrs = this.attrs;
+        var /** @type {?} */ outputs = this.componentFactory.outputs || [];
+        for (var /** @type {?} */ j = 0; j < outputs.length; j++) {
+            var /** @type {?} */ output = new PropertyBinding(outputs[j].propName, outputs[j].templateName);
+            var /** @type {?} */ bindonAttr = output.bindonAttr.substring(0, output.bindonAttr.length - 6);
+            var /** @type {?} */ bracketParenAttr = "[(" + output.bracketParenAttr.substring(2, output.bracketParenAttr.length - 8) + ")]";
+            // order below is important - first update bindings then evaluate expressions
+            if (attrs.hasOwnProperty(bindonAttr)) {
+                this.subscribeToOutput(output, attrs[bindonAttr], true);
+            }
+            if (attrs.hasOwnProperty(bracketParenAttr)) {
+                this.subscribeToOutput(output, attrs[bracketParenAttr], true);
+            }
+            if (attrs.hasOwnProperty(output.onAttr)) {
+                this.subscribeToOutput(output, attrs[output.onAttr]);
+            }
+            if (attrs.hasOwnProperty(output.parenAttr)) {
+                this.subscribeToOutput(output, attrs[output.parenAttr]);
+            }
+        }
+    };
+    /**
+     * @param {?} output
+     * @param {?} expr
+     * @param {?=} isAssignment
+     * @return {?}
+     */
+    DowngradeComponentAdapter.prototype.subscribeToOutput = /**
+     * @param {?} output
+     * @param {?} expr
+     * @param {?=} isAssignment
+     * @return {?}
+     */
+    function (output, expr, isAssignment) {
+        var _this = this;
+        if (isAssignment === void 0) { isAssignment = false; }
+        var /** @type {?} */ getter = this.$parse(expr);
+        var /** @type {?} */ setter = getter.assign;
+        if (isAssignment && !setter) {
+            throw new Error("Expression '" + expr + "' is not assignable!");
+        }
+        var /** @type {?} */ emitter = /** @type {?} */ (this.component[output.prop]);
+        if (emitter) {
+            emitter.subscribe({
+                next: isAssignment ? function (v) { return ((setter))(_this.scope, v); } :
+                    function (v) { return getter(_this.scope, { '$event': v }); }
+            });
+        }
+        else {
+            throw new Error("Missing emitter '" + output.prop + "' on component '" + getComponentName(this.componentFactory.componentType) + "'!");
+        }
+    };
+    /**
+     * @return {?}
+     */
+    DowngradeComponentAdapter.prototype.registerCleanup = /**
+     * @return {?}
+     */
+    function () {
+        var _this = this;
+        var /** @type {?} */ destroyComponentRef = this.wrapCallback(function () { return _this.componentRef.destroy(); });
+        var /** @type {?} */ destroyed = false; /** @type {?} */
+        ((this.element.on))('$destroy', function () { return _this.componentScope.$destroy(); });
+        this.componentScope.$on('$destroy', function () {
+            if (!destroyed) {
+                destroyed = true;
+                _this.componentRef.injector.get(TestabilityRegistry)
+                    .unregisterApplication(_this.componentRef.location.nativeElement);
+                destroyComponentRef();
+            }
+        });
+    };
+    /**
+     * @return {?}
+     */
+    DowngradeComponentAdapter.prototype.getInjector = /**
+     * @return {?}
+     */
+    function () { return this.componentRef.injector; };
+    /**
+     * @param {?} prop
+     * @param {?} prevValue
+     * @param {?} currValue
+     * @return {?}
+     */
+    DowngradeComponentAdapter.prototype.updateInput = /**
+     * @param {?} prop
+     * @param {?} prevValue
+     * @param {?} currValue
+     * @return {?}
+     */
+    function (prop, prevValue, currValue) {
+        if (this.implementsOnChanges) {
+            this.inputChanges[prop] = new SimpleChange(prevValue, currValue, prevValue === currValue);
+        }
+        this.inputChangeCount++;
+        this.component[prop] = currValue;
+    };
+    /**
+     * @return {?}
+     */
+    DowngradeComponentAdapter.prototype.groupProjectableNodes = /**
+     * @return {?}
+     */
+    function () {
+        var /** @type {?} */ ngContentSelectors = this.componentFactory.ngContentSelectors;
+        return groupNodesBySelector(ngContentSelectors, /** @type {?} */ ((this.element.contents))());
+    };
+    return DowngradeComponentAdapter;
+}());
+export { DowngradeComponentAdapter };
+function DowngradeComponentAdapter_tsickle_Closure_declarations() {
+    /** @type {?} */
+    DowngradeComponentAdapter.prototype.implementsOnChanges;
+    /** @type {?} */
+    DowngradeComponentAdapter.prototype.inputChangeCount;
+    /** @type {?} */
+    DowngradeComponentAdapter.prototype.inputChanges;
+    /** @type {?} */
+    DowngradeComponentAdapter.prototype.componentScope;
+    /** @type {?} */
+    DowngradeComponentAdapter.prototype.componentRef;
+    /** @type {?} */
+    DowngradeComponentAdapter.prototype.component;
+    /** @type {?} */
+    DowngradeComponentAdapter.prototype.changeDetector;
+    /** @type {?} */
+    DowngradeComponentAdapter.prototype.viewChangeDetector;
+    /** @type {?} */
+    DowngradeComponentAdapter.prototype.element;
+    /** @type {?} */
+    DowngradeComponentAdapter.prototype.attrs;
+    /** @type {?} */
+    DowngradeComponentAdapter.prototype.scope;
+    /** @type {?} */
+    DowngradeComponentAdapter.prototype.ngModel;
+    /** @type {?} */
+    DowngradeComponentAdapter.prototype.parentInjector;
+    /** @type {?} */
+    DowngradeComponentAdapter.prototype.$injector;
+    /** @type {?} */
+    DowngradeComponentAdapter.prototype.$compile;
+    /** @type {?} */
+    DowngradeComponentAdapter.prototype.$parse;
+    /** @type {?} */
+    DowngradeComponentAdapter.prototype.componentFactory;
+    /** @type {?} */
+    DowngradeComponentAdapter.prototype.wrapCallback;
+}
+/**
+ * Group a set of DOM nodes into `ngContent` groups, based on the given content selectors.
+ * @param {?} ngContentSelectors
+ * @param {?} nodes
+ * @return {?}
+ */
+export function groupNodesBySelector(ngContentSelectors, nodes) {
+    var /** @type {?} */ projectableNodes = [];
+    var /** @type {?} */ wildcardNgContentIndex;
+    for (var /** @type {?} */ i = 0, /** @type {?} */ ii = ngContentSelectors.length; i < ii; ++i) {
+        projectableNodes[i] = [];
+    }
+    for (var /** @type {?} */ j = 0, /** @type {?} */ jj = nodes.length; j < jj; ++j) {
+        var /** @type {?} */ node = nodes[j];
+        var /** @type {?} */ ngContentIndex = findMatchingNgContentIndex(node, ngContentSelectors);
+        if (ngContentIndex != null) {
+            projectableNodes[ngContentIndex].push(node);
+        }
+    }
+    return projectableNodes;
+}
+/**
+ * @param {?} element
+ * @param {?} ngContentSelectors
+ * @return {?}
+ */
+function findMatchingNgContentIndex(element, ngContentSelectors) {
+    var /** @type {?} */ ngContentIndices = [];
+    var /** @type {?} */ wildcardNgContentIndex = -1;
+    for (var /** @type {?} */ i = 0; i < ngContentSelectors.length; i++) {
+        var /** @type {?} */ selector = ngContentSelectors[i];
+        if (selector === '*') {
+            wildcardNgContentIndex = i;
+        }
+        else {
+            if (matchesSelector(element, selector)) {
+                ngContentIndices.push(i);
+            }
+        }
+    }
+    ngContentIndices.sort();
+    if (wildcardNgContentIndex !== -1) {
+        ngContentIndices.push(wildcardNgContentIndex);
+    }
+    return ngContentIndices.length ? ngContentIndices[0] : null;
+}
+var /** @type {?} */ _matches;
+/**
+ * @param {?} el
+ * @param {?} selector
+ * @return {?}
+ */
+function matchesSelector(el, selector) {
+    if (!_matches) {
+        var /** @type {?} */ elProto = /** @type {?} */ (Element.prototype);
+        _matches = elProto.matches || elProto.matchesSelector || elProto.mozMatchesSelector ||
+            elProto.msMatchesSelector || elProto.oMatchesSelector || elProto.webkitMatchesSelector;
+    }
+    return el.nodeType === Node.ELEMENT_NODE ? _matches.call(el, selector) : false;
+}
+//# sourceMappingURL=downgrade_component_adapter.js.map
---
+++ esm5/upgrade.js
@@ -0,0 +1,9 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+/**
+ * Generated bundle index. Do not edit.
+ */
+export { VERSION, UpgradeAdapter, UpgradeAdapterRef } from './public_api';
+//# sourceMappingURL=upgrade.js.map
