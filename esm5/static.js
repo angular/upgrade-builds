3ab26ff10867c8e6d0c31ae4f0f4d23a2e62c42f
Igor Minar <igor@angular.io>
2018-04-12 06:38:36 +0000

    c5b6e31 build: fix aio size tracking, we need to use node_modules local to aio (#23328)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-22686f8
+ * @license Angular v5.2.9-c5b6e31
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -668,7 +668,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.9-22686f8');
+var VERSION = new Version('5.2.9-c5b6e31');
 
 /**
  * @license
