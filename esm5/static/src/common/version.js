e718116c8003283618250243e1783a4e400b7136
Igor Minar <igor@angular.io>
2018-04-12 06:40:40 +0000

    7e8cee6 build: fix aio size tracking, we need to use node_modules local to aio (#23328)

--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export var VERSION = new Version('6.0.0-rc.3-37d2cb4');
+export var VERSION = new Version('6.0.0-rc.3-7e8cee6');
 //# sourceMappingURL=version.js.map
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-37d2cb4
+ * @license Angular v6.0.0-rc.3-7e8cee6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.3-37d2cb4');
+const VERSION = new Version('6.0.0-rc.3-7e8cee6');
 
 /**
  * @fileoverview added by tsickle
