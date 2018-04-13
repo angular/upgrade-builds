c6046acc8dcc4620bd890bf35a947dcb82578c9c
Igor Minar <igor@angular.io>
2018-04-13 21:43:38 +0000

    490772e docs(aio): Update file tree to match Angular CLI V6 structure (#23367)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export const VERSION = new Version('6.0.0-rc.4-e7ef027');
+export const VERSION = new Version('6.0.0-rc.4-490772e');
 //# sourceMappingURL=version.js.map
