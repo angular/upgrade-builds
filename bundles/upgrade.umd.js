cbd182730194668e866caa526be3e2784b343a34
Igor Minar <igor@angular.io>
2018-04-13 07:31:18 +0000

    a113330 refactor(language-service): fix typo on type.ts language-service

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-3cf5719
+ * @license Angular v5.2.9-a113330
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.2.9-3cf5719
+ * @license Angular v5.2.9-a113330
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -28,7 +28,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.9-3cf5719');
+var VERSION = new _angular_core.Version('5.2.9-a113330');
 
 /**
  * @fileoverview added by tsickle
