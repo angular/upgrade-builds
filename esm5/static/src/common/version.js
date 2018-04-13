fca09cafb32425e2a16330341ecefb716e6e5534
Igor Minar <igor@angular.io>
2018-04-13 15:39:38 +0000

    eac36d7 test(aio): fix `DocViewerComponent` tests (#23359)

--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export var VERSION = new Version('6.0.0-rc.4-74b203a');
+export var VERSION = new Version('6.0.0-rc.4-eac36d7');
 //# sourceMappingURL=version.js.map
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-74b203a
+ * @license Angular v6.0.0-rc.4-eac36d7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.4-74b203a');
+const VERSION = new Version('6.0.0-rc.4-eac36d7');
 
 /**
  * @fileoverview added by tsickle
