4c0d8c736eba85562e0406e116545a4d0a9dd29d
Igor Minar <igor@angular.io>
2018-04-13 06:44:34 +0000

    75febe7 docs(upgrade): fix detail regarding bootstrapping order (#23225) (#23270)

--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export var VERSION = new Version('6.0.0-rc.4-993eeab');
+export var VERSION = new Version('6.0.0-rc.4-75febe7');
 //# sourceMappingURL=version.js.map
