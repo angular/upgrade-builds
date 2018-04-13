08c970c70bfecb95ebd2978fe5cc706f1fe40acf
Igor Minar <igor@angular.io>
2018-04-13 21:05:38 +0000

    e7ef027 fix(ivy): local directives and pipes should be applied to TemplateRef (#23312)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-d5e7f60
+ * @license Angular v6.0.0-rc.4-e7ef027
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.4-d5e7f60');
+var VERSION = new Version('6.0.0-rc.4-e7ef027');
 
 /**
  * @fileoverview added by tsickle
