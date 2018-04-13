e60548b45f998bf4cf6b00b29b983158f6d3e9ab
Igor Minar <igor@angular.io>
2018-04-13 20:53:02 +0000

    6e73300 refactor(ivy): cleanup directives & pipes import (#23369)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export const VERSION = new Version('6.0.0-rc.4-6c2c958');
+export const VERSION = new Version('6.0.0-rc.4-6e73300');
 //# sourceMappingURL=version.js.map
--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export var VERSION = new Version('6.0.0-rc.4-6c2c958');
+export var VERSION = new Version('6.0.0-rc.4-6e73300');
 //# sourceMappingURL=version.js.map
