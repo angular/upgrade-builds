e48aad88c20cd48fdb9228d0fc42203c7463f56b
Igor Minar <igor@angular.io>
2018-04-15 04:03:43 +0000

    b64a276 refactor(ivy): make return value of define(Component|Directive|Pipe|Injector|Injectable) private (#23371) (#23383)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export const VERSION = new Version('6.0.0-rc.5-815ae29');
+export const VERSION = new Version('6.0.0-rc.5-b64a276');
 //# sourceMappingURL=version.js.map
--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export var VERSION = new Version('6.0.0-rc.5-815ae29');
+export var VERSION = new Version('6.0.0-rc.5-b64a276');
 //# sourceMappingURL=version.js.map
