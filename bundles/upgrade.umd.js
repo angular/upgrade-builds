41fc682d78e46db06fbd4162bfea6421b393e233
Igor Minar <igor@angular.io>
2018-04-16 06:47:41 +0000

    7b23983 fix(service-worker): add badge to NOTIFICATION_OPTION_NAMES (#23241)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-21f3301
+ * @license Angular v5.2.9-7b23983
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v5.2.9-21f3301
+ * @license Angular v5.2.9-7b23983
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -28,7 +28,7 @@
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.9-21f3301');
+var VERSION = new _angular_core.Version('5.2.9-7b23983');
 
 /**
  * @fileoverview added by tsickle
