8b169dc2a975b17620a4cfdc261d2e4e9e133a22
Jason Aden <jasonaden@google.com>
2018-04-17 21:08:41 +0000

    010a4ef docs(aio): Fixed typo in 'comparing observables -> Observabled compared (#23407) to arrays' section.

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export const VERSION = new Version('6.0.0-rc.5-c3280b2');
+export const VERSION = new Version('6.0.0-rc.5-010a4ef');
 //# sourceMappingURL=version.js.map
--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export var VERSION = new Version('6.0.0-rc.5-c3280b2');
+export var VERSION = new Version('6.0.0-rc.5-010a4ef');
 //# sourceMappingURL=version.js.map
