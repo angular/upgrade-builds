db9c5eee6fa5111417e2bec67d4716a424f4c423
Victor Berchet <victor@suumit.com>
2018-04-22 19:21:21 +0000

    ba47997 style(compiler): fix lint issues (#23480)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export const VERSION = new Version('6.0.0-rc.5-6761a64');
+export const VERSION = new Version('6.0.0-rc.5-ba47997');
 //# sourceMappingURL=version.js.map
--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export var VERSION = new Version('6.0.0-rc.5-6761a64');
+export var VERSION = new Version('6.0.0-rc.5-ba47997');
 //# sourceMappingURL=version.js.map
