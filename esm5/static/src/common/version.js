7b948a15dc37d583e34106df0e4c2434a08f0951
Victor Berchet <victor@suumit.com>
2018-04-23 21:07:28 +0000

    60e5507 docs(aio): Add UpgradingAngularJS to education resources (#23169)

--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export var VERSION = new Version('6.0.0-rc.5-4cfa571');
+export var VERSION = new Version('6.0.0-rc.5-60e5507');
 //# sourceMappingURL=version.js.map
--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-4cfa571
+ * @license Angular v6.0.0-rc.5-60e5507
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -667,7 +667,7 @@ function downgradeInjectable(token) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-4cfa571');
+var VERSION = new Version('6.0.0-rc.5-60e5507');
 
 /**
  * @license
