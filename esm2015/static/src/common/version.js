91e8215a05ca6443bc74fa8fc0a0f7e323a01ac8
Victor Berchet <victor@suumit.com>
2018-04-24 18:42:57 +0000

    db17231 ci(aio): fix `deploy-to-firebase` script (#23470)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export const VERSION = new Version('6.0.0-rc.5-540626a');
+export const VERSION = new Version('6.0.0-rc.5-db17231');
 //# sourceMappingURL=version.js.map
--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export var VERSION = new Version('6.0.0-rc.5-540626a');
+export var VERSION = new Version('6.0.0-rc.5-db17231');
 //# sourceMappingURL=version.js.map
