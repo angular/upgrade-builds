e1c3c60818b4ab3a0b92f0d55c63f6072dbfd744
Victor Berchet <victor@suumit.com>
2018-04-24 00:34:12 +0000

    1c9200e ci: require green integration tests to publish snapshot (#23517)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+73.sha-ace6440
+ * @license Angular v6.0.0-rc.5+74.sha-1c9200e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -670,7 +670,7 @@ function downgradeInjectable(token) {
 /**
  *
  */
-var VERSION = new core.Version('6.0.0-rc.5+73.sha-ace6440');
+var VERSION = new core.Version('6.0.0-rc.5+74.sha-1c9200e');
 
 /**
  * @license
