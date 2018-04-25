7fee6bb8ddab29aeabf01ed52330f37f91af06bf
Igor Minar <igor@angular.io>
2018-04-25 23:19:19 +0000

    eb031c6 refactor(core): tree-shake application_module providers (#23477)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+86.sha-b4c252b
+ * @license Angular v6.0.0-rc.5+87.sha-eb031c6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5+86.sha-b4c252b');
+const VERSION = new Version('6.0.0-rc.5+87.sha-eb031c6');
 
 /**
  * @fileoverview added by tsickle
