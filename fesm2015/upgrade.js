73e07310c9f305e625421112f709075a3a0ea1f3
Igor Minar <igor@angular.io>
2018-04-26 02:23:29 +0000

    b1f040f fix(compiler-cli): don't rely on incompatible TS method (#23550)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+87.sha-eb031c6
+ * @license Angular v6.0.0-rc.5+88.sha-b1f040f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5+87.sha-eb031c6');
+const VERSION = new Version('6.0.0-rc.5+88.sha-b1f040f');
 
 /**
  * @fileoverview added by tsickle
