56b94e447688d5adf79712bfcdd9828b3da83609
Igor Minar <igor@angular.io>
2018-04-26 02:25:42 +0000

    1a44a0b feat(ivy): support lifecycle hooks of ViewContainerRef (#23396)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+88.sha-b1f040f
+ * @license Angular v6.0.0-rc.5+89.sha-1a44a0b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5+88.sha-b1f040f');
+const VERSION = new Version('6.0.0-rc.5+89.sha-1a44a0b');
 
 /**
  * @fileoverview added by tsickle
