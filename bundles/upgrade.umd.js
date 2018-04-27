29322878ea3c0602b0fb1e27dbc847a740df460c
Igor Minar <igor@angular.io>
2018-04-27 14:56:23 +0000

    098a3ef docs: correct more typos (#23565)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-5021bdd
+ * @license Angular v6.0.0-rc.5-098a3ef
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_platformBrowserDynamic) { 'use strict';
 
 /**
- * @license Angular v6.0.0-rc.5-5021bdd
+ * @license Angular v6.0.0-rc.5-098a3ef
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -28,7 +28,7 @@
 /**
  *
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.5-5021bdd');
+var VERSION = new _angular_core.Version('6.0.0-rc.5-098a3ef');
 
 /**
  * @fileoverview added by tsickle
--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export var VERSION = new Version('6.0.0-rc.5-5021bdd');
+export var VERSION = new Version('6.0.0-rc.5-098a3ef');
 //# sourceMappingURL=version.js.map
