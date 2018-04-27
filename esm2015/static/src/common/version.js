9f37640cdfa75e1201f3fcee01f275b51fb9c49b
Igor Minar <igor@angular.io>
2018-04-27 15:18:48 +0000

    91bf598 docs(aio): update docs error in guide/http (#23567)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export const VERSION = new Version('6.0.0-rc.5-08e9290');
+export const VERSION = new Version('6.0.0-rc.5-91bf598');
 //# sourceMappingURL=version.js.map
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-08e9290
+ * @license Angular v6.0.0-rc.5-91bf598
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5-08e9290');
+const VERSION = new Version('6.0.0-rc.5-91bf598');
 
 /**
  * @fileoverview added by tsickle
