65011c934f6f494491c8be708e14a3f2d2a0c2b0
Igor Minar <igor@angular.io>
2018-04-27 14:48:56 +0000

    5021bdd ci: add Brandon Roberts as an aio approver (#23417)

--- esm2015/static/src/common/version.js
+++ esm2015/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export const VERSION = new Version('6.0.0-rc.5-64bf6ed');
+export const VERSION = new Version('6.0.0-rc.5-5021bdd');
 //# sourceMappingURL=version.js.map
--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export var VERSION = new Version('6.0.0-rc.5-64bf6ed');
+export var VERSION = new Version('6.0.0-rc.5-5021bdd');
 //# sourceMappingURL=version.js.map
