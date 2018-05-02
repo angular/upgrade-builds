5406a7fbb11a2f37673c0e0c23d7d8cd19f853c6
Igor Minar <igor@angular.io>
2018-05-02 22:41:15 +0000

    22eb8e2 build(aio): align stackblitz files with Angular CLI V6 (#23521)

--- esm5/static/src/common/version.js
+++ esm5/static/src/common/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export var VERSION = new Version('6.0.0-rc.6-14138f6');
+export var VERSION = new Version('6.0.0-rc.6-22eb8e2');
 //# sourceMappingURL=version.js.map
