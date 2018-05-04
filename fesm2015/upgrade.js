07f109b0922689162fd5437bc5156de4f428c51b
Igor Minar <igor@angular.io>
2018-05-04 22:47:21 +0000

    e3e1577 build: update bazel to 0.13 (#23623)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+134.sha-b25e15c
+ * @license Angular v6.0.0-rc.5+135.sha-e3e1577
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5+134.sha-b25e15c');
+const VERSION = new Version('6.0.0-rc.5+135.sha-e3e1577');
 
 /**
  * @fileoverview added by tsickle
