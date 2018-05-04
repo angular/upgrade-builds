cf64c8f05f6bbf6207489cb14f8de3b20add9e6f
Igor Minar <igor@angular.io>
2018-05-04 22:54:09 +0000

    7e9649b build: update to latest nodejs bazel rules (#23683)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+135.sha-e3e1577
+ * @license Angular v6.0.0-rc.5+136.sha-7e9649b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5+135.sha-e3e1577');
+const VERSION = new Version('6.0.0-rc.5+136.sha-7e9649b');
 
 /**
  * @fileoverview added by tsickle
