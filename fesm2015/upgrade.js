2ca1f1c19242e742b0272a718c123b6e96f9ddb7
Igor Minar <igor@angular.io>
2018-05-05 15:56:43 +0000

    77ff72f Revert "style(animations): fix short param names (#23668)"

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+138.sha-e351896
+ * @license Angular v6.0.0-rc.5+140.sha-77ff72f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5+138.sha-e351896');
+const VERSION = new Version('6.0.0-rc.5+140.sha-77ff72f');
 
 /**
  * @fileoverview added by tsickle
