fba276268c218b983e5f671b2fe930a2f041604e
Igor Minar <igor@angular.io>
2018-05-08 21:20:07 +0000

    b0eca85 refactor(compiler): compile{Component,Directive} take only local information (#23545)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+141.sha-fc03427
+ * @license Angular v6.0.0-rc.5+144.sha-b0eca85
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5+141.sha-fc03427');
+const VERSION = new Version('6.0.0-rc.5+144.sha-b0eca85');
 
 /**
  * @fileoverview added by tsickle
