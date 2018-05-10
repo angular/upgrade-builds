3e943d2de8b958e53d7853845264dc278ba75c11
Igor Minar <igor@angular.io>
2018-05-10 15:29:02 +0000

    89f64e5 fix(router): avoid freezing queryParams in-place (#22663)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+156.sha-858e48a
+ * @license Angular v6.0.0-rc.5+158.sha-89f64e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5+156.sha-858e48a');
+const VERSION = new Version('6.0.0-rc.5+158.sha-89f64e5');
 
 /**
  * @fileoverview added by tsickle
