c31557c539dca398313fe56b979e5da09833782f
Igor Minar <igor@angular.io>
2018-05-10 23:27:38 +0000

    db2d67c docs: change release_schedule.md to link to new angular release page in docs (#23808)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+159.sha-117c7ee
+ * @license Angular v6.0.0-rc.5+160.sha-db2d67c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5+159.sha-117c7ee');
+const VERSION = new Version('6.0.0-rc.5+160.sha-db2d67c');
 
 /**
  * @fileoverview added by tsickle
