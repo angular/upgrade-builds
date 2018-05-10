01f2ad16183a133562940c22896bd6c86b543306
Igor Minar <igor@angular.io>
2018-05-10 15:20:34 +0000

    553a680 fix(router): correct the segment parsing so it won't break on ampersand (#23684)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+154.sha-f1e4a15
+ * @license Angular v6.0.0-rc.5+157.sha-553a680
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -21,7 +21,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5+154.sha-f1e4a15');
+const VERSION = new Version('6.0.0-rc.5+157.sha-553a680');
 
 /**
  * @fileoverview added by tsickle
