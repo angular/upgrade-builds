27878ef4ab733e433722faa5af71c87549ebbbd7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-15 19:37:18 +0000

    bc27d4a fix(service-worker): correctly handle requests with empty `clientId` (#23625)

--- esm5/static.js
+++ esm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.10-65dba9d
+ * @license Angular v5.2.10-bc27d4a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -668,7 +668,7 @@ function downgradeInjectable(token) {
 /**
  * @stable
  */
-var VERSION = new Version('5.2.10-65dba9d');
+var VERSION = new Version('5.2.10-bc27d4a');
 
 /**
  * @license
