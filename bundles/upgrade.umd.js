818f56222b84e94f15847119dc3ccbfd919af5a9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-15 22:59:56 +0000

    95fca24 feat(aio): add brand and concept icons, img style class more flexible (#23589)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2
+ * @license Angular v6.0.2+1.sha-95fca24
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new core.Version('6.0.2');
+var VERSION = new core.Version('6.0.2+1.sha-95fca24');
 
 /*! *****************************************************************************
 Copyright (c) Microsoft Corporation. All rights reserved.
