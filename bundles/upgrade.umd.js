a0c174ad81a76241a1555ee1166044d4bd68bed4
Victor Berchet <victor@suumit.com>
2018-06-04 17:56:15 +0000

    a3d9878 docs(aio): remove an extraneous apostrophe (#24293) PR Close #24293

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+52.sha-0e1919c
+ * @license Angular v6.0.3+54.sha-a3d9878
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.0.3+52.sha-0e1919c');
+var VERSION = new core.Version('6.0.3+54.sha-a3d9878');
 
 /*! *****************************************************************************
 Copyright (c) Microsoft Corporation. All rights reserved.
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+52.sha-0e1919c
+ * @license Angular v6.0.3+54.sha-a3d9878
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.3+52.sha-0e1919c');
+var VERSION = new Version('6.0.3+54.sha-a3d9878');
 
 /**
  * @license
