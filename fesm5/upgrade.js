c18d807e33f6dfb15682d1425016a554ebc871d2
Victor Berchet <victor@suumit.com>
2018-06-04 17:56:33 +0000

    ccaa199 docs(aio): remove an extraneous apostrophe (#24293) PR Close #24293

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+304.sha-0690622
+ * @license Angular v6.0.0-rc.5+305.sha-ccaa199
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+304.sha-0690622');
+var VERSION = new Version('6.0.0-rc.5+305.sha-ccaa199');
 
 /**
  * @license
