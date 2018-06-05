33d7e1534a3bc3ead9551d27b878c82688965856
Victor Berchet <victor@suumit.com>
2018-06-05 21:04:44 +0000

    ea3127e build(bazel): ran format (#24279)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+58.sha-faf60d9
+ * @license Angular v6.0.3+62.sha-ea3127e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.0.3+58.sha-faf60d9');
+var VERSION = new core.Version('6.0.3+62.sha-ea3127e');
 
 /*! *****************************************************************************
 Copyright (c) Microsoft Corporation. All rights reserved.
