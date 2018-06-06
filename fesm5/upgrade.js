50fa131758feab62746e5aebbc88e07351b430d7
Victor Berchet <victor@suumit.com>
2018-06-06 17:50:40 +0000

    700e55c build(docs-infra): log warning rather than error if content errors are not fatal (#24320)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+324.sha-c494d3c
+ * @license Angular v6.0.0-rc.5+337.sha-700e55c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+324.sha-c494d3c');
+var VERSION = new Version('6.0.0-rc.5+337.sha-700e55c');
 
 /**
  * @license
