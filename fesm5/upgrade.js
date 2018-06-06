998f459dfb66367012292728a5cca208ecb14c76
Victor Berchet <victor@suumit.com>
2018-06-06 05:10:25 +0000

    22b58a7 feat(ivy): add support of ApplicationRef.bootstrapModuleFactory (#23811)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+322.sha-86b13cc
+ * @license Angular v6.0.0-rc.5+323.sha-22b58a7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+322.sha-86b13cc');
+var VERSION = new Version('6.0.0-rc.5+323.sha-22b58a7');
 
 /**
  * @license
