2f44ed60749257592bc0cd418c2debcbc5b6c280
Victor Berchet <victor@suumit.com>
2018-06-06 00:56:10 +0000

    87b1671 docs(aio): Add null type to form validation example (#23949)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+318.sha-20c463e
+ * @license Angular v6.0.0-rc.5+319.sha-87b1671
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.0.0-rc.5+318.sha-20c463e');
+var VERSION = new core.Version('6.0.0-rc.5+319.sha-87b1671');
 
 /*! *****************************************************************************
 Copyright (c) Microsoft Corporation. All rights reserved.
