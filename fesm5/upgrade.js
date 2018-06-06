32932300bacd14f8f80b062074c75e009dcf6054
Victor Berchet <victor@suumit.com>
2018-06-06 01:53:09 +0000

    86b13cc refactor(ivy): move static parts of LView.cleanup to TView (#24301)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+320.sha-9367e91
+ * @license Angular v6.0.0-rc.5+322.sha-86b13cc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+320.sha-9367e91');
+var VERSION = new Version('6.0.0-rc.5+322.sha-86b13cc');
 
 /**
  * @license
