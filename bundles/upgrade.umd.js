659ad28718a33010bce4dd2056f8e99f7e4e45d7
Miško Hevery <misko@hevery.com>
2018-06-07 23:11:17 +0000

    ce1543f docs(aio): Added resource link to Amexio Canvas Web Based IDE (#24336)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+16.sha-a6e797b
+ * @license Angular v6.1.0-beta.0+17.sha-ce1543f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.1.0-beta.0+16.sha-a6e797b');
+var VERSION = new core.Version('6.1.0-beta.0+17.sha-ce1543f');
 
 /*! *****************************************************************************
 Copyright (c) Microsoft Corporation. All rights reserved.
