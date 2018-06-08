c965f7306e208b5b84afdae7bc49572f141dde6f
Miško Hevery <misko@hevery.com>
2018-06-08 21:29:25 +0000

    e876535 fix(docs-infra): use script nomodule to load IE polyfills, skip other polyfills (#24317)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+3.sha-0d81151
+ * @license Angular v6.0.4+5.sha-e876535
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.0.4+3.sha-0d81151');
+var VERSION = new core.Version('6.0.4+5.sha-e876535');
 
 /*! *****************************************************************************
 Copyright (c) Microsoft Corporation. All rights reserved.
