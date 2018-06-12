3b8d45904af93036a26e99b6d3a3867f61811f32
Miško Hevery <misko@hevery.com>
2018-06-12 19:20:18 +0000

    c2b5ebf build: update buildifier to latest (#24296)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+40.sha-1316024
+ * @license Angular v6.1.0-beta.0+42.sha-c2b5ebf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.0+40.sha-1316024');
+const VERSION = new Version('6.1.0-beta.0+42.sha-c2b5ebf');
 
 /**
  * @license
