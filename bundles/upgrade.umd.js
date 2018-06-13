f69b3efc3602f2a177c1953703d10207e2c5bb96
Miško Hevery <misko@hevery.com>
2018-06-13 21:13:45 +0000

    a577c9e docs: edit api doc comments for new template and style (#23682)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+43.sha-70ef061
+ * @license Angular v6.1.0-beta.0+55.sha-a577c9e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.1.0-beta.0+43.sha-70ef061');
+var VERSION = new core.Version('6.1.0-beta.0+55.sha-a577c9e');
 
 /*! *****************************************************************************
 Copyright (c) Microsoft Corporation. All rights reserved.
--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+43.sha-70ef061
+ * @license Angular v6.1.0-beta.0+55.sha-a577c9e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -663,7 +663,7 @@ function downgradeInjectable(token) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.0+43.sha-70ef061');
+var VERSION = new Version('6.1.0-beta.0+55.sha-a577c9e');
 
 /**
  * @license
