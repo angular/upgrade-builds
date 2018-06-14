d9eb00366e9c134ff47e3efd31bc1a20d26850b1
Miško Hevery <misko@hevery.com>
2018-06-14 22:10:19 +0000

    27bc7dc feat(ivy): ngtsc compiles @Component, @Directive, @NgModule (#24427)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+29.sha-0f7e4fa
+ * @license Angular v6.1.0-beta.1+30.sha-27bc7dc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.1+29.sha-0f7e4fa');
+const VERSION = new Version('6.1.0-beta.1+30.sha-27bc7dc');
 
 /**
  * @license
