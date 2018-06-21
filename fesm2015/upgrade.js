63bd600cec54843fce99cd18ab972f590131f11e
Miško Hevery <misko@hevery.com>
2018-06-21 00:26:34 +0000

    8b81682 fix(ivy): nested ngFor should be supported (#24564)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+3.sha-b91254f
+ * @license Angular v6.1.0-beta.2+2.sha-8b81682
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.2+3.sha-b91254f');
+const VERSION = new Version('6.1.0-beta.2+2.sha-8b81682');
 
 /**
  * @license
