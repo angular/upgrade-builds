8f37501c550f39252d7cf7c879c08fed2faee85f
Miško Hevery <misko@hevery.com>
2018-06-21 00:32:14 +0000

    c11e84e build(docs-infra): redirect removed webpack guide to `v5.angular.io` (#24595)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+2.sha-1ce5a49
+ * @license Angular v6.0.6+3.sha-c11e84e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.0.6+2.sha-1ce5a49');
+var VERSION = new core.Version('6.0.6+3.sha-c11e84e');
 
 /*! *****************************************************************************
 Copyright (c) Microsoft Corporation. All rights reserved.
