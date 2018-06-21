515beb7643fd3f435b7cfe04ee03889359ba9ba2
Miško Hevery <misko@hevery.com>
2018-06-21 20:47:09 +0000

    84272e2 feat(ivy): runtime i18n (#24037)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+6.sha-cb31381
+ * @license Angular v6.1.0-beta.2+7.sha-84272e2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.2+6.sha-cb31381');
+const VERSION = new Version('6.1.0-beta.2+7.sha-84272e2');
 
 /**
  * @license
