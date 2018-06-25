08c5756fa38621f1931b19312f3c91e08a34bb03
Jason Aden <jasonaden@google.com>
2018-06-25 20:35:36 +0000

    ae01c70 ci: fix broken build do to bad merge (#24662)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+12.sha-393db94
+ * @license Angular v6.0.6+16.sha-ae01c70
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.0.6+12.sha-393db94');
+var VERSION = new core.Version('6.0.6+16.sha-ae01c70');
 
 /*! *****************************************************************************
 Copyright (c) Microsoft Corporation. All rights reserved.
