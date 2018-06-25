8afc70a354b2aa479c48aa18bd920f6ed75b4de6
Miško Hevery <misko@hevery.com>
2018-06-25 17:43:23 +0000

    2a68ba4 docs: fix misdirected group links (#24569)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+25.sha-941d2cd
+ * @license Angular v6.1.0-beta.2+27.sha-2a68ba4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.2+25.sha-941d2cd');
+var VERSION = new Version('6.1.0-beta.2+27.sha-2a68ba4');
 
 /**
  * @license
