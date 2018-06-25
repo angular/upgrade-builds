86ef9fef1a60891cddd03f06a55b0cb1d5faee7c
Miško Hevery <misko@hevery.com>
2018-06-25 16:12:28 +0000

    1e139d4 refactor(ivy): rename, limit usage of global vars (#24604)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+19.sha-fba3f10
+ * @license Angular v6.1.0-beta.2+20.sha-1e139d4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.2+19.sha-fba3f10');
+const VERSION = new Version('6.1.0-beta.2+20.sha-1e139d4');
 
 /**
  * @license
