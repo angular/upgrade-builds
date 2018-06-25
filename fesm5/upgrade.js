b5eeef9ba2593406c1df190cd0ac0ee69477f5b8
Miško Hevery <misko@hevery.com>
2018-06-25 19:07:47 +0000

    166d90d ci: fix broken build

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+28.sha-7d31874
+ * @license Angular v6.1.0-beta.2+29.sha-166d90d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.2+28.sha-7d31874');
+var VERSION = new Version('6.1.0-beta.2+29.sha-166d90d');
 
 /**
  * @license
