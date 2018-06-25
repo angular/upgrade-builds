a8c2021a5d3e70e23f9b473c0c76458479a639ca
Pete Bacon Darwin <pete@bacondarwin.com>
2018-06-25 13:34:25 +0000

    368a7ed feat(ivy): implement initial ngcc fesm2015 parser

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+38.sha-d9c4acf
+ * @license Angular v6.1.0-beta.1+44.sha-368a7ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.1+38.sha-d9c4acf');
+const VERSION = new Version('6.1.0-beta.1+44.sha-368a7ed');
 
 /**
  * @license
