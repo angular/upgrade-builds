8217549b0927944704eaa1a0c47605b445b868c3
Jason Aden <jasonaden@google.com>
2018-06-27 21:53:27 +0000

    fe8fcc8 refactor(ivy): remove dynamicParent from LNode (#24678)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+38.sha-5c0e681
+ * @license Angular v6.1.0-beta.2+39.sha-fe8fcc8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.2+38.sha-5c0e681');
+const VERSION = new Version('6.1.0-beta.2+39.sha-fe8fcc8');
 
 /**
  * @license
