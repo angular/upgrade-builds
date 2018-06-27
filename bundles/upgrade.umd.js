1e35dda3b473dde17efc9e7aef2214db83fefba8
Jason Aden <jasonaden@google.com>
2018-06-27 22:14:00 +0000

    32da3e1 docs: add explanation for `enableResourceInlining` (#24644)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+41.sha-6d68f3e
+ * @license Angular v6.1.0-beta.2+42.sha-32da3e1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -22,7 +22,7 @@
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.1.0-beta.2+41.sha-6d68f3e');
+var VERSION = new core.Version('6.1.0-beta.2+42.sha-32da3e1');
 
 /*! *****************************************************************************
 Copyright (c) Microsoft Corporation. All rights reserved.
