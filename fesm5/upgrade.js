3dc51d1d4c5e6e042b2fe011805dd4f846c4a84a
Jason Aden <jasonaden@google.com>
2018-06-27 22:01:53 +0000

    50fb13f fix(ivy): report results to appropriate content queries (#24673)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+39.sha-fe8fcc8
+ * @license Angular v6.1.0-beta.2+40.sha-50fb13f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.2+39.sha-fe8fcc8');
+var VERSION = new Version('6.1.0-beta.2+40.sha-50fb13f');
 
 /**
  * @license
