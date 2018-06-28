99efd9459e3ab3a971d4e20a21971e0dacf5478c
Miško Hevery <misko@hevery.com>
2018-06-28 22:41:44 +0000

    d89f57f build(docs-infra): render short description of parameters in API docs (#24537)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+3.sha-ac5b69f
+ * @license Angular v6.0.7+4.sha-d89f57f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -18,7 +18,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.7+3.sha-ac5b69f');
+const VERSION = new Version('6.0.7+4.sha-d89f57f');
 
 /**
  * @fileoverview added by tsickle
