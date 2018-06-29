e242f7bc8048a5226acc44f9df77a3f4242a9d3a
Misko Hevery <misko@hevery.com>
2018-06-29 14:30:52 +0000

    0f1de35 docs(core): rephrase doc for Injector.get (#24670)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+4.sha-d89f57f
+ * @license Angular v6.0.7+5.sha-0f1de35
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.7+4.sha-d89f57f');
+var VERSION = new Version('6.0.7+5.sha-0f1de35');
 
 /**
  * @license
