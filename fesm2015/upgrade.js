b327da234d5d4c66485a06bcfe70113cbaa58be3
Miško Hevery <misko@hevery.com>
2018-07-02 22:44:39 +0000

    a294e0d fix(ivy): correct position for re-projected containers (#24721)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+20.sha-d243baf
+ * @license Angular v6.1.0-beta.3+25.sha-a294e0d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.3+20.sha-d243baf');
+const VERSION = new Version('6.1.0-beta.3+25.sha-a294e0d');
 
 /**
  * @license
