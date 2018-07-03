c11fab6932c6583d2b877663a4fb95eb18977c01
Miško Hevery <misko@hevery.com>
2018-07-03 21:02:56 +0000

    e3064d5 feat: typescript 2.9 support (#24652)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+28.sha-0922228
+ * @license Angular v6.1.0-beta.3+30.sha-e3064d5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -648,7 +648,7 @@ function downgradeInjectable(token) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.3+28.sha-0922228');
+const VERSION = new Version('6.1.0-beta.3+30.sha-e3064d5');
 
 /**
  * @license
