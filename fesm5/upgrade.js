eadd72516083051007fe89c9a235d89e848cbaeb
Miško Hevery <misko@hevery.com>
2018-07-06 18:17:53 +0000

    25b532e docs: clarify faqs about services (#24079)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+19.sha-b51ce62
+ * @license Angular v6.0.7+18.sha-25b532e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { platformBrowserDynamic as platformBrowserDynamic$1 } from '@angular/pla
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.7+19.sha-b51ce62');
+var VERSION = new Version('6.0.7+18.sha-25b532e');
 
 /**
  * @license
