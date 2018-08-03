14ffaecb0c348db5f53838011dd5baf742401c76
Kara Erickson <karakara@google.com>
2018-08-03 23:35:28 +0000

    0c9c2ac refactor(bazel): dont rely on language target to downlevel for loop (#24534)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+3.sha-0fb41e5
+ * @license Angular v6.1.1+4.sha-0c9c2ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12,7 +12,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.1+3.sha-0fb41e5');
+const VERSION = new Version('6.1.1+4.sha-0c9c2ac');
 
 /**
  * @fileoverview added by tsickle
