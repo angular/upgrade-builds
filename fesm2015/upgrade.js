28d0bc675329d4d8256cb39be21437c8345ecb7f
Kara Erickson <karakara@google.com>
2018-08-07 21:37:08 +0000

    732026c feat(core): add DoBootstrap interface. (#24558)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+45.sha-fefc860
+ * @license Angular v7.0.0-beta.0+50.sha-732026c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12,7 +12,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.0+45.sha-fefc860');
+const VERSION = new Version('7.0.0-beta.0+50.sha-732026c');
 
 /**
  * @fileoverview added by tsickle
