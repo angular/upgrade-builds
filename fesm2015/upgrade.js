2ea041e9cd61061d3ab001c597ed4a723ba37bc6
Misko Hevery <misko@hevery.com>
2018-09-05 19:28:46 +0000

    1e7a873 build(bazel): remove unused angular.tsconfig.json integration/bazel test (#25778)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+50.sha-13b8399
+ * @license Angular v7.0.0-beta.4+51.sha-1e7a873
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12,7 +12,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.4+50.sha-13b8399');
+const VERSION = new Version('7.0.0-beta.4+51.sha-1e7a873');
 
 /**
  * @fileoverview added by tsickle
