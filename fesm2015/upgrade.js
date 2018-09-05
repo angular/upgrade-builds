9b52799d9cf026d4c5e751929f059a545ace6c23
Misko Hevery <misko@hevery.com>
2018-09-05 20:51:38 +0000

    2c1f550 build: update ngcontainer to node 10.9.0 (#25812)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+24.sha-e72f741
+ * @license Angular v6.1.6+25.sha-2c1f550
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12,7 +12,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.6+24.sha-e72f741');
+const VERSION = new Version('6.1.6+25.sha-2c1f550');
 
 /**
  * @fileoverview added by tsickle
