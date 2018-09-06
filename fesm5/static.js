9733dafbe74edecf6f3105e6f9f95068308f08b8
Igor Minar <igor@angular.io>
2018-09-06 23:22:29 +0000

    b9a5ce1 docs(service-worker): update http-server command (#25845) PR Close #25845

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+28.sha-d1063c6
+ * @license Angular v6.1.7+8.sha-b9a5ce1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ function downgradeInjectable(token) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.6+28.sha-d1063c6');
+var VERSION = new Version('6.1.7+8.sha-b9a5ce1');
 
 /**
  * @license
