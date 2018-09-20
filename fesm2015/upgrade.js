9ec18a43a4dc775f0b6229f7f2dd262423b34527
Kara Erickson <karakara@google.com>
2018-09-20 02:06:40 +0000

    41ac58a docs: copy-edit (#25732)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+6.sha-d7e5bbf
+ * @license Angular v7.0.0-beta.6+12.sha-41ac58a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12,7 +12,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.6+6.sha-d7e5bbf');
+const VERSION = new Version('7.0.0-beta.6+12.sha-41ac58a');
 
 /**
  * @fileoverview added by tsickle
