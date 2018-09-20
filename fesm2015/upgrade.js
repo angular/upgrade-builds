c8be514c8abb90c2765c8e6d4b5c9ca48e91334d
Kara Erickson <karakara@google.com>
2018-09-20 18:31:42 +0000

    549de1e fix(core): add missing `peerDependency ` to `@angular/compiler` (#26033)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+13.sha-48e73c1
+ * @license Angular v7.0.0-beta.6+14.sha-549de1e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12,7 +12,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.6+13.sha-48e73c1');
+const VERSION = new Version('7.0.0-beta.6+14.sha-549de1e');
 
 /**
  * @fileoverview added by tsickle
