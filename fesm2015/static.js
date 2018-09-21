88cefbaefa581da2713438e0f42f494762d0d7c8
Kara Erickson <karakara@google.com>
2018-09-21 01:56:57 +0000

    64aa670 refactor(router): cleanup to navigation stream for readability and documentation (#25740)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+10.sha-927323f
+ * @license Angular v7.0.0-beta.6+25.sha-64aa670
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -808,7 +808,7 @@ function downgradeInjectable(token) {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.6+10.sha-927323f');
+const VERSION = new Version('7.0.0-beta.6+25.sha-64aa670');
 
 /**
  * @fileoverview added by tsickle
