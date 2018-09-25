c9a355ead971361b6fcdc22085ff3dc56aade1d5
Kara Erickson <karakara@google.com>
2018-09-25 19:14:44 +0000

    42e2e7c build: remove obsolete comment in `env.sh` (#25819)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+39.sha-adad170
+ * @license Angular v6.1.8+40.sha-42e2e7c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12,7 +12,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.8+39.sha-adad170');
+const VERSION = new Version('6.1.8+40.sha-42e2e7c');
 
 /**
  * @fileoverview added by tsickle
