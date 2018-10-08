538cbf182073cd734842fe9bc4d4e9a020fef455
Jason Aden <jasonaden@google.com>
2018-10-08 22:07:33 +0000

    b22c376 Revert "fix(upgrade): properly destroy upgraded component elements and descendants (#26209)"

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+78.sha-8a6f372
+ * @license Angular v6.1.9+79.sha-b22c376
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.9+78.sha-8a6f372');
+const VERSION = new Version('6.1.9+79.sha-b22c376');
 
 /**
  * @license
@@ -30,7 +30,7 @@ function noNg() {
 let angular = {
     bootstrap: noNg,
     module: noNg,
-    element: Object.assign(() => noNg(), { cleanData: noNg }),
+    element: noNg,
     version: undefined,
     resumeBootstrap: noNg,
     getTestability: noNg
@@ -45,7 +45,7 @@ catch (e) {
 }
 const bootstrap = (e, modules, config) => angular.bootstrap(e, modules, config);
 const module$1 = (prefix, dependencies) => angular.module(prefix, dependencies);
-const element = Object.assign((e) => angular.element(e), { cleanData: (nodes) => angular.element.cleanData(nodes) });
+const element = e => angular.element(e);
 let version = angular.version;
 
 /**
@@ -711,14 +711,7 @@ class UpgradeHelper {
             controllerInstance.$onDestroy();
         }
         $scope.$destroy();
-        // Clean the jQuery/jqLite data on the component+child elements.
-        // Equivelent to how jQuery/jqLite invoke `cleanData` on an Element (this.element)
-        //  https://github.com/jquery/jquery/blob/e743cbd28553267f955f71ea7248377915613fd9/src/manipulation.js#L223
-        //  https://github.com/angular/angular.js/blob/26ddc5f830f902a3d22f4b2aab70d86d4d688c82/src/jqLite.js#L306-L312
-        // `cleanData` will invoke the AngularJS `$destroy` DOM event
-        //  https://github.com/angular/angular.js/blob/26ddc5f830f902a3d22f4b2aab70d86d4d688c82/src/Angular.js#L1911-L1924
-        element.cleanData([this.element]);
-        element.cleanData(this.element.querySelectorAll('*'));
+        this.$element.triggerHandler('$destroy');
     }
     prepareTransclusion() {
         const transclude = this.directive.transclude;
