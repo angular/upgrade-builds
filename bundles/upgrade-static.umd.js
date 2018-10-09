4e8d946f39135bd0f80a6978a9f2d774fc55150b
Jason Aden <jasonaden@google.com>
2018-10-09 00:06:00 +0000

    83302d1 fix(ivy): ensure ngcc compiles `@angular/core` with correct internal imports (#26236)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+63.sha-55d54c7
+ * @license Angular v7.0.0-rc.0+79.sha-83302d1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -23,7 +23,7 @@
     var angular = {
         bootstrap: noNg,
         module: noNg,
-        element: noNg,
+        element: Object.assign(function () { return noNg(); }, { cleanData: noNg }),
         version: undefined,
         resumeBootstrap: noNg,
         getTestability: noNg
@@ -69,7 +69,7 @@
     var module$1 = function (prefix, dependencies) {
         return angular.module(prefix, dependencies);
     };
-    var element = function (e) { return angular.element(e); };
+    var element = Object.assign(function (e) { return angular.element(e); }, { cleanData: function (nodes) { return angular.element.cleanData(nodes); } });
     var version = angular.version;
 
     /**
@@ -655,7 +655,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('7.0.0-rc.0+63.sha-55d54c7');
+    var VERSION = new i0.Version('7.0.0-rc.0+79.sha-83302d1');
 
     /**
      * @license
@@ -936,7 +936,14 @@
                 controllerInstance.$onDestroy();
             }
             $scope.$destroy();
-            this.$element.triggerHandler('$destroy');
+            // Clean the jQuery/jqLite data on the component+child elements.
+            // Equivelent to how jQuery/jqLite invoke `cleanData` on an Element (this.element)
+            //  https://github.com/jquery/jquery/blob/e743cbd28553267f955f71ea7248377915613fd9/src/manipulation.js#L223
+            //  https://github.com/angular/angular.js/blob/26ddc5f830f902a3d22f4b2aab70d86d4d688c82/src/jqLite.js#L306-L312
+            // `cleanData` will invoke the AngularJS `$destroy` DOM event
+            //  https://github.com/angular/angular.js/blob/26ddc5f830f902a3d22f4b2aab70d86d4d688c82/src/Angular.js#L1911-L1924
+            element.cleanData([this.element]);
+            element.cleanData(this.element.querySelectorAll('*'));
         };
         UpgradeHelper.prototype.prepareTransclusion = function () {
             var _this = this;
