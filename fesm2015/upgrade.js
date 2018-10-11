4fd2d867da3c9fe61380c8676085b85c3e9ae264
Miško Hevery <misko@hevery.com>
2018-10-11 21:48:11 +0000

    1e69d60 fix(ivy): ensure that `ɵNgModuleDefWithMeta` is exported (#26082)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+6.sha-7d0e175
+ * @license Angular v7.0.0-rc.1+16.sha-1e69d60
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12,7 +12,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+6.sha-7d0e175');
+const VERSION = new Version('7.0.0-rc.1+16.sha-1e69d60');
 
 /**
  * @fileoverview added by tsickle
