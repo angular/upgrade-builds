9c4ed3332d02bd98e40e97e6cfe2a2c9ed43ea6c
Miško Hevery <misko@hevery.com>
2018-10-17 20:47:38 +0000

    a4ebf3f docs: fix formatting of PULL_REQUEST_TEMPLATE.md (#26512)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+74.sha-b49b274
+ * @license Angular v7.0.0-rc.1+73.sha-a4ebf3f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12,7 +12,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+74.sha-b49b274');
+const VERSION = new Version('7.0.0-rc.1+73.sha-a4ebf3f');
 
 /**
  * @fileoverview added by tsickle
