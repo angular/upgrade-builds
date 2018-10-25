790c9d31d2d4027027c6f22ead244f5e50e03a2d
Matias Niemelä <matias@yearofmoo.com>
2018-10-25 00:13:24 +0000

    13a803d build(docs-infra): fix parameter type rendering (#26688)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+5.sha-0cc9842
+ * @license Angular v7.1.0-beta.0+2.sha-13a803d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.0+5.sha-0cc9842');
+const VERSION = new Version('7.1.0-beta.0+2.sha-13a803d');
 
 /**
  * @fileoverview added by tsickle
