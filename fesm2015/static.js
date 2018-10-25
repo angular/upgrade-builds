249585e186dd6746068aa4deb8ea97fd51a9b112
Matias Niemelä <matias@yearofmoo.com>
2018-10-25 17:20:42 +0000

    b0476f3 feat(ivy): support providers and viewProviders (#25803)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+6.sha-297dc2b
+ * @license Angular v7.1.0-beta.0+8.sha-b0476f3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -825,7 +825,7 @@ function downgradeInjectable(token) {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.0+6.sha-297dc2b');
+const VERSION = new Version('7.1.0-beta.0+8.sha-b0476f3');
 
 /**
  * @fileoverview added by tsickle
