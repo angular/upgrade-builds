d441c1d4aa5f73558475a64731c7fd7f61c23f8e
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 23:00:35 +0000

    332394d build(bazel): generalize fix for AMD name for flat module out file (#26837)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+73.sha-62b4ff52
+ * @license Angular v7.1.0-beta.0+74.sha-332394d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.0+73.sha-62b4ff52');
+const VERSION = new Version('7.1.0-beta.0+74.sha-332394d');
 
 /**
  * @fileoverview added by tsickle
