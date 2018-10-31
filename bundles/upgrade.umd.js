15b13e670bc509ae0f014bfb02843a55d5a81f18
Matias Niemelä <matias@yearofmoo.com>
2018-10-31 23:44:05 +0000

    77f531b release: cut the v7.0.2 release

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.1+56.sha-3041240
+ * @license Angular v7.0.2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.0.1+56.sha-3041240');
+    var VERSION = new core.Version('7.0.2');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.1+56.sha-3041240
+ * @license Angular v7.0.2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.0.1+56.sha-3041240');
+const VERSION = new Version('7.0.2');
 
 /**
  * @fileoverview added by tsickle
