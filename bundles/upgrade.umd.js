c2339c8b45fe9c27cd0a926baebb5b6374e91e31
Matias Niemelä <matias@yearofmoo.com>
2018-10-31 23:25:56 +0000

    3041240 build: remove problematic `yarn` prefix for running bazel

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.1+55.sha-257ac83
+ * @license Angular v7.0.1+56.sha-3041240
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.0.1+55.sha-257ac83');
+    var VERSION = new core.Version('7.0.1+56.sha-3041240');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
