b78bff4928bc007f147512b9aa56a19b0f741f9d
Kara Erickson <karakara@google.com>
2018-11-02 18:09:11 +0000

    beabfb7 ci: remove redundant `start-xvfb` step (#26869)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+52.sha-496372d
+ * @license Angular v7.1.0-beta.1+60.sha-beabfb7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -667,7 +667,7 @@ function downgradeInjectable(token) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+52.sha-496372d');
+var VERSION = new Version('7.1.0-beta.1+60.sha-beabfb7');
 
 /**
  * @license
