406710bdedac5ba435febff4bf8274c34e4d0ec7
Kara Erickson <karakara@google.com>
2018-11-02 18:52:27 +0000

    9c2d0d0 docs: edit file structure page (#26552)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+62.sha-64647af
+ * @license Angular v7.1.0-beta.1+63.sha-9c2d0d0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -668,7 +668,7 @@ function downgradeInjectable(token) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+62.sha-64647af');
+var VERSION = new Version('7.1.0-beta.1+63.sha-9c2d0d0');
 
 /**
  * @license
