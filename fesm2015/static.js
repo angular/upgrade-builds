d8aff7854c1bad951144c1c2275088370c2fed76
Kara Erickson <karakara@google.com>
2018-11-07 20:30:35 +0000

    9263671 release: cut the v7.0.3 release

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.2+25.sha-14ff340
+ * @license Angular v7.0.3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -825,7 +825,7 @@ function downgradeInjectable(token) {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.0.2+25.sha-14ff340');
+const VERSION = new Version('7.0.3');
 
 /**
  * @fileoverview added by tsickle
