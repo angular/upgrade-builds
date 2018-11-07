7380eb449a4f42316527d62c18041fd577d16777
Kara Erickson <karakara@google.com>
2018-11-07 01:31:31 +0000

    931a363 fix(ivy): use `window.ng` instead of `window.ngDev` for debug tooling (#26951)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+80.sha-3ca1a57
+ * @license Angular v7.1.0-beta.1+81.sha-931a363
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -746,7 +746,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+80.sha-3ca1a57');
+var VERSION = new Version('7.1.0-beta.1+81.sha-931a363');
 
 /**
  * @license
