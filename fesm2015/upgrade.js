92cedadfe3721226060b3c21fd004fab53443146
Andrew Kushnir <akushnir@google.com>
2018-11-09 18:09:39 +0000

    f33307b docs(router): add RxJS filter pipe operator to example (#25686)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3+11.sha-b00588a
+ * @license Angular v7.0.3+12.sha-f33307b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.0.3+11.sha-b00588a');
+const VERSION = new Version('7.0.3+12.sha-f33307b');
 
 /**
  * @fileoverview added by tsickle
