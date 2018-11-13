6c24efe7e336c2459b496f68c48229627252db6b
Andrew Kushnir <akushnir@google.com>
2018-11-13 19:29:03 +0000

    9741f5b docs: new doc for core directives (#26923)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+37.sha-7695dbd
+ * @license Angular v7.1.0-beta.2+36.sha-9741f5b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -18,7 +18,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+37.sha-7695dbd');
+var VERSION = new Version('7.1.0-beta.2+36.sha-9741f5b');
 
 /**
  * @license
