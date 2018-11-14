19362607d16e49fb3b771c76f9b58fa1520698c0
Andrew Kushnir <akushnir@google.com>
2018-11-14 00:42:44 +0000

    a752971 fix(router): add `relativeLinkResolution` to `recognize` operator (#26990)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+43.sha-a63fd2d
+ * @license Angular v7.1.0-beta.2+45.sha-a752971
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -746,7 +746,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+43.sha-a63fd2d');
+var VERSION = new Version('7.1.0-beta.2+45.sha-a752971');
 
 /**
  * @license
