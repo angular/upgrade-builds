7aa116110543be292cbda9604e6703f848d6a5b1
Igor Minar <igor@angular.io>
2018-11-30 06:10:33 +0000

    013c0ae style: change style css syntax (#27253)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+6.sha-56254b5
+ * @license Angular v7.1.1+8.sha-013c0ae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.1+6.sha-56254b5');
+    var VERSION = new core.Version('7.1.1+8.sha-013c0ae');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
