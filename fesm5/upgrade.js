fc7622b7272d90e3ef53c721bb7b32c1200cc68c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 23:11:52 +0000

    8eb102a fix(ivy): avoid counting style/class bindings in component/directive `hostBindings` (#27388)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+115.sha-6552471
+ * @license Angular v7.1.0+111.sha-8eb102a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -18,7 +18,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+115.sha-6552471');
+var VERSION = new Version('7.1.0+111.sha-8eb102a');
 
 /**
  * @license
