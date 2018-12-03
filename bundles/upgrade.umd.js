dcfdda6c4163834287c5050f603cfddb86908fca
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 17:45:11 +0000

    6f5c124 fix(ivy): proper accounting of host vars in case of inherited Directives (#27392)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+107.sha-931a636
+ * @license Angular v7.1.0+108.sha-6f5c124
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.0+107.sha-931a636');
+    var VERSION = new core.Version('7.1.0+108.sha-6f5c124');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
