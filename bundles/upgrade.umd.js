93534b33176a8ed9114a94dc1d9388440bd2edab
Igor Minar <igor@angular.io>
2018-12-04 22:40:41 +0000

    8e644d9 fix(ivy): taking "interpolation" config option into account (FW-723) (#27363)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+121.sha-d3c08e7
+ * @license Angular v7.1.0+128.sha-8e644d9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.0+121.sha-d3c08e7');
+    var VERSION = new core.Version('7.1.0+128.sha-8e644d9');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
