ad082e9b78611f5cee4fae1cb0cc03e727ac2ba2
Igor Minar <igor@angular.io>
2018-12-05 04:42:11 +0000

    b2d6f43 fix(ivy): Implement remaining methods for DebugNode (#27387)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+127.sha-1597886
+ * @license Angular v7.1.0+130.sha-b2d6f43
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -921,7 +921,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+127.sha-1597886');
+const VERSION = new Version('7.1.0+130.sha-b2d6f43');
 
 /**
  * @fileoverview added by tsickle
