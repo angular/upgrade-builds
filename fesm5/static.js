2a0d395e07745ceac6fc078c506daea8e2a7b1a8
Igor Minar <igor@angular.io>
2018-12-05 07:43:06 +0000

    4b9948c fix(core): More precise return type for `InjectableDecorator` (#27360)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+149.sha-faca8b1
+ * @license Angular v7.1.0+150.sha-4b9948c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -746,7 +746,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+149.sha-faca8b1');
+var VERSION = new Version('7.1.0+150.sha-4b9948c');
 
 /**
  * @license
