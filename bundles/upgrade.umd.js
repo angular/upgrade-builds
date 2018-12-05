d90a5553e7b7916cc7859c67971b995b0cb324d8
Igor Minar <igor@angular.io>
2018-12-05 04:47:21 +0000

    5c57920 fix(docs-infra): log the successful creation of previews (#27436)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+24.sha-9b4d959
+ * @license Angular v7.1.1+25.sha-5c57920
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.1+24.sha-9b4d959');
+    var VERSION = new core.Version('7.1.1+25.sha-5c57920');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
