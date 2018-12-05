cfff7df555fbb143987b532607c725690787f145
Igor Minar <igor@angular.io>
2018-12-05 21:16:16 +0000

    24ebdbd ci: test ts-api-guardian on windows (#27205)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+163.sha-821fecb
+ * @license Angular v7.1.0+164.sha-24ebdbd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -745,7 +745,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+163.sha-821fecb');
+var VERSION = new Version('7.1.0+164.sha-24ebdbd');
 
 /**
  * @license
