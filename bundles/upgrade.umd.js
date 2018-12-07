73970a222359c7fe2d6da5d39caf11675327a3e2
Igor Minar <igor@angular.io>
2018-12-07 06:06:52 +0000

    11d2a8c release: cut the v7.1.2 release

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+57.sha-ad106ab
+ * @license Angular v7.1.2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.1+57.sha-ad106ab');
+    var VERSION = new core.Version('7.1.2');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
