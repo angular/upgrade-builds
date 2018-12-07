50f605d411e6e6b5be60a8f702ff0b682b2ce2d7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 20:20:01 +0000

    255e672 fix(ivy): define correct SVG namespace (FW-757) (#27529)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+13.sha-f034114
+ * @license Angular v7.2.0-beta.1+15.sha-255e672
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -746,7 +746,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.1+13.sha-f034114');
+var VERSION = new Version('7.2.0-beta.1+15.sha-255e672');
 
 /**
  * @license
