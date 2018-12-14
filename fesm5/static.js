ecbca62b18fe4bac4cf055e988ff510a09af0b10
Miško Hevery <misko@hevery.com>
2018-12-14 23:58:36 +0000

    b224057 docs(common): fix documentation for getLocaleCurrencyName (#27087) PR Close #27087

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+27.sha-453589f
+ * @license Angular v7.1.3+29.sha-b224057
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -746,7 +746,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.3+27.sha-453589f');
+var VERSION = new Version('7.1.3+29.sha-b224057');
 
 /**
  * @license
