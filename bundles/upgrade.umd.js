651bb3b409f24236f6aea3ddd9570b7ecb27260b
Miško Hevery <misko@hevery.com>
2018-12-14 23:51:50 +0000

    c220328 docs: adding ngAtlanta 2019 (#27522)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+26.sha-fa3af8b
+ * @license Angular v7.1.3+25.sha-c220328
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.3+26.sha-fa3af8b');
+    var VERSION = new core.Version('7.1.3+25.sha-c220328');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
