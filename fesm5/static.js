6753428a41b162227f3fb7dd0859c424e4b96204
Miško Hevery <misko@hevery.com>
2018-12-14 23:59:22 +0000

    78bd3c7 docs(animations): rename animateChild() to animate() (#26184)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+29.sha-b224057
+ * @license Angular v7.1.3+30.sha-78bd3c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -746,7 +746,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.3+29.sha-b224057');
+var VERSION = new Version('7.1.3+30.sha-78bd3c7');
 
 /**
  * @license
