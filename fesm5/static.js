1478fed7ab815ac61d4f095aa6045350084abb46
Miško Hevery <misko@hevery.com>
2018-12-17 18:16:45 +0000

    a726338 docs: Add Smart UI to UI Components section in resources (#27515)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+62.sha-94f17e9
+ * @license Angular v7.2.0-beta.2+55.sha-a726338
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -746,7 +746,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+62.sha-94f17e9');
+var VERSION = new Version('7.2.0-beta.2+55.sha-a726338');
 
 /**
  * @license
