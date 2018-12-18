1039e1de24603a878b61181cee45e512b41423be
Miško Hevery <misko@hevery.com>
2018-12-18 21:44:39 +0000

    b04bc5d test(ivy): re-enable passing upgrade tests (#27736)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+82.sha-1c93afe
+ * @license Angular v7.2.0-beta.2+83.sha-b04bc5d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -746,7 +746,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+82.sha-1c93afe');
+var VERSION = new Version('7.2.0-beta.2+83.sha-b04bc5d');
 
 /**
  * @license
