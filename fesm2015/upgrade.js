24ebf7524aaf91ecd121c867010334d92d2128ec
Miško Hevery <misko@hevery.com>
2018-12-18 04:37:42 +0000

    4bf8d64 fix(ivy): restore `fixmeIvy` imports for Router-related tests (#27720)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+70.sha-8042140
+ * @license Angular v7.2.0-beta.2+74.sha-4bf8d64
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-beta.2+70.sha-8042140');
+const VERSION = new Version('7.2.0-beta.2+74.sha-4bf8d64');
 
 /**
  * @fileoverview added by tsickle
