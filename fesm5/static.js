7b2d2ec55d5213c80ba6a133e53ba00d3326f624
Matias Niemelä <matias@yearofmoo.com>
2018-12-20 21:48:57 +0000

    07ada7f fix(router): ensure URL is updated after second redirect with UrlUpdateStrategy="eager" (#27523)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+13.sha-880e8a5
+ * @license Angular v7.2.0-rc.0+14.sha-07ada7f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -745,7 +745,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+13.sha-880e8a5');
+var VERSION = new Version('7.2.0-rc.0+14.sha-07ada7f');
 
 /**
  * @license
