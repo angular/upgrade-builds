70ccf0604bd20bd4f8d5571160dc82437a08a37f
Kara Erickson <karakara@google.com>
2019-01-04 20:49:15 +0000

    38b4d15 fix(ivy): implement 'TestBed.overrideTemplateUsingTestingModule' function (#27717)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+53.sha-8a05199
+ * @license Angular v7.2.0-rc.0+55.sha-38b4d15
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0-rc.0+53.sha-8a05199');
+    var VERSION = new core.Version('7.2.0-rc.0+55.sha-38b4d15');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
