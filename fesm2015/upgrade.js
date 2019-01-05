b822f88df925a069f421babb2a20e5767158b8c9
Filipe Silva <filipematossilva@gmail.com>
2019-01-05 22:42:34 +0000

    784453c ci: use image based cache

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+53.sha-9576e48
+ * @license Angular v7.2.0-rc.0+53.sha-784453c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+53.sha-9576e48');
+const VERSION = new Version('7.2.0-rc.0+53.sha-784453c');
 
 /**
  * @fileoverview added by tsickle
