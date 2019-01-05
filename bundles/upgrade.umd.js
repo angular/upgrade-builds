b41fcca528d8397b7e0fa1cfefd4da9f6ad45cd4
Filipe Silva <filipematossilva@gmail.com>
2019-01-05 22:01:42 +0000

    9576e48 ci: use image based cache

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+53.sha-8acaed1
+ * @license Angular v7.2.0-rc.0+53.sha-9576e48
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0-rc.0+53.sha-8acaed1');
+    var VERSION = new core.Version('7.2.0-rc.0+53.sha-9576e48');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
