83b0202389803770e7b43b906304e99e8c066878
Kara Erickson <karakara@google.com>
2019-01-07 16:25:28 +0000

    0efbb37 release: cut the v7.2.0 release

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+71.sha-6b0c1a7
+ * @license Angular v7.2.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0-rc.0+71.sha-6b0c1a7');
+    var VERSION = new core.Version('7.2.0');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
