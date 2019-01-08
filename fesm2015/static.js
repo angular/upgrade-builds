fd55a9ae6cd506d2793acd2e9bcd18d2be871c6d
Kara Erickson <karakara@google.com>
2019-01-08 00:02:35 +0000

    4613864 test(ivy): add i18n integration tests via TestBed (#27852)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+65.sha-b61dafa
+ * @license Angular v7.2.0+15.sha-4613864
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -991,7 +991,7 @@ function downgradeInjectable(token, downgradedModule = '') {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+65.sha-b61dafa');
+const VERSION = new Version('7.2.0+15.sha-4613864');
 
 /**
  * @fileoverview added by tsickle
