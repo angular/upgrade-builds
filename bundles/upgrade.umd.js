91cc2d6b962e1c0ec993a79c69aed774507b0eec
Andrew Kushnir <akushnir@google.com>
2019-01-09 23:10:56 +0000

    9b2b9b3 docs(core): add missing closing backtick (`) (#27908) PR Close #27908

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+56.sha-c3aa24c
+ * @license Angular v7.2.0+74.sha-9b2b9b3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0+56.sha-c3aa24c');
+    var VERSION = new core.Version('7.2.0+74.sha-9b2b9b3');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
