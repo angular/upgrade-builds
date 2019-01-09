8679890f1bc615fe991776c6ea10d20fde716403
Andrew Kushnir <akushnir@google.com>
2019-01-09 21:00:00 +0000

    a06469c docs(forms): remove duplicated link (#27884) PR Close #27884

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+70.sha-ecc304a
+ * @license Angular v7.2.0-rc.0+72.sha-a06469c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0-rc.0+70.sha-ecc304a');
+    var VERSION = new core.Version('7.2.0-rc.0+72.sha-a06469c');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
