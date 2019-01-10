b78fe0098d53437e79780eb7a4dfdd5d6e4df3f9
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:28:31 +0000

    142553a feat(ivy): accept multiple values for exportAs in the compiler (#28001)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+87.sha-6003145
+ * @license Angular v7.2.0+88.sha-142553a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0+87.sha-6003145');
+    var VERSION = new core.Version('7.2.0+88.sha-142553a');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
