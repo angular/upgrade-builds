12a758f7c080f26f3c70dc0744fb161440cec36f
Andrew Kushnir <akushnir@google.com>
2019-01-10 01:59:43 +0000

    69ca7cd fix(bazel): Add @bazel/bazel to dev deps (#28032)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+74.sha-e5f5ad3
+ * @license Angular v7.2.0-rc.0+75.sha-69ca7cd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -792,7 +792,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+74.sha-e5f5ad3');
+var VERSION = new Version('7.2.0-rc.0+75.sha-69ca7cd');
 
 /**
  * @license
