e3e518be1dfd247eca36efdef035f98f71d6588c
Andrew Kushnir <akushnir@google.com>
2019-01-10 21:52:50 +0000

    feebe03 fix(ivy): pass ngContentSelectors through to `defineComponent()` calls (#27867)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+98.sha-a6ba789
+ * @license Angular v7.2.0+100.sha-feebe03
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -990,7 +990,7 @@ function downgradeInjectable(token, downgradedModule = '') {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+98.sha-a6ba789');
+const VERSION = new Version('7.2.0+100.sha-feebe03');
 
 /**
  * @fileoverview added by tsickle
@@ -1008,7 +1008,7 @@ const VERSION = new Version('7.2.0+98.sha-a6ba789');
 // Then we "get" the ng1 injector from the module injector, which triggers the provider to read
 // the stored injector and release the reference to it.
 /** @type {?} */
-let tempInjectorRef;
+let tempInjectorRef = null;
 /**
  * @param {?} injector
  * @return {?}
