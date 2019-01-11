c612d5e007d587bf9656cc0d7eec6748bf8c4a89
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:26:14 +0000

    aebec4b refactor(core): improve type for EventEmitter (#27228)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+105.sha-9277142
+ * @license Angular v7.2.0+106.sha-aebec4b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -792,7 +792,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+105.sha-9277142');
+var VERSION = new Version('7.2.0+106.sha-aebec4b');
 
 /**
  * @license
