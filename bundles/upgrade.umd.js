5796e3fbd8103787a75a4d28796c07f0071b7d37
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:36:40 +0000

    b0f3c20 test(ivy): resolve root cause for core animation tests (#28062)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+109.sha-bccf8da
+ * @license Angular v7.2.0+111.sha-b0f3c20
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0+109.sha-bccf8da');
+    var VERSION = new core.Version('7.2.0+111.sha-b0f3c20');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
