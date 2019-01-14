31ca8e75983c393e2b2d8483443392a042891ee3
Andrew Kushnir <akushnir@google.com>
2019-01-14 18:34:33 +0000

    c5b67b9 docs: update link in universal guide (#26628)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+148.sha-78bc21c
+ * @license Angular v7.2.0+149.sha-c5b67b9
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0+148.sha-78bc21c');
+    var VERSION = new core.Version('7.2.0+149.sha-c5b67b9');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
