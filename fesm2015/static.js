1c8aad10fbaa890dac3fc0ee0aa38aa1f9e4595b
Andrew Kushnir <akushnir@google.com>
2019-01-15 01:34:52 +0000

    fa53150 test(ivy): NodeInjector should know how to get itself (INJECTOR) (#28009)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+173.sha-b5c2ef2
+ * @license Angular v7.2.0+174.sha-fa53150
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -990,7 +990,7 @@ function downgradeInjectable(token, downgradedModule = '') {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+173.sha-b5c2ef2');
+const VERSION = new Version('7.2.0+174.sha-fa53150');
 
 /**
  * @fileoverview added by tsickle
