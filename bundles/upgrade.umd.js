1553b1d35b0551fdf02b7aaf737005f03e69ddef
Andrew Kushnir <akushnir@google.com>
2019-01-15 18:05:40 +0000

    6930451 refactor(ivy): remove def.attributes in favor of the `elementHostAttrs` instruction (#28089)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+178.sha-e62eeed
+ * @license Angular v7.2.0+179.sha-6930451
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0+178.sha-e62eeed');
+    var VERSION = new core.Version('7.2.0+179.sha-6930451');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
