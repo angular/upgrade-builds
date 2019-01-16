243b59854cebeff0394e4a20b7fba0a6e550261e
Andrew Kushnir <akushnir@google.com>
2019-01-16 18:14:46 +0000

    bac71ef fix(ivy): componentFactories not populated in ModuleWithComponentFactories (#28112)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+196.sha-9a81f0d
+ * @license Angular v7.2.0+198.sha-bac71ef
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0+196.sha-9a81f0d');
+    var VERSION = new core.Version('7.2.0+198.sha-bac71ef');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
