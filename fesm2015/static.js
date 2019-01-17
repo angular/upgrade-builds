eaa87c42438ac335ad2786245d9423de61420d03
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 18:20:39 +0000

    e172e97 fix(ivy): ensure animation @trigger ordering is correctly delivered to the renderer (#28165)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+9.sha-896cf35
+ * @license Angular v8.0.0-beta.0+11.sha-e172e97
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -990,7 +990,7 @@ function downgradeInjectable(token, downgradedModule = '') {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.0+9.sha-896cf35');
+const VERSION = new Version('8.0.0-beta.0+11.sha-e172e97');
 
 /**
  * @fileoverview added by tsickle
