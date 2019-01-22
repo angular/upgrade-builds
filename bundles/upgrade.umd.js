4aa39a52cddfb23042e3d92b48492090d54be311
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 20:01:47 +0000

    d336bff fix(ivy): add missging getDirectives export (#28259)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+41.sha-f99082f
+ * @license Angular v8.0.0-beta.0+42.sha-d336bff
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.0+41.sha-f99082f');
+    var VERSION = new core.Version('8.0.0-beta.0+42.sha-d336bff');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
