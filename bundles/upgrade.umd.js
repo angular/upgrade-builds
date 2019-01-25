b29d5f8c6b950ef248dc3f24a7545eff129e0949
Jason Aden <jasonaden@google.com>
2019-01-25 21:14:25 +0000

    b2811e5 refactor(router): initalize browserUrlTree to empty tree (#28376)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+28.sha-e2c98fb
+ * @license Angular v8.0.0-beta.1+29.sha-b2811e5
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.1+28.sha-e2c98fb');
+    var VERSION = new core.Version('8.0.0-beta.1+29.sha-b2811e5');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
