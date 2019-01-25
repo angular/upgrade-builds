dc9adb61d7276309d473af0db1c953fd20556edc
Jason Aden <jasonaden@google.com>
2019-01-25 08:21:13 +0000

    e2c98fb fix(docs-infra): change the key used to find out the cli section (#28293)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+22.sha-465abab
+ * @license Angular v8.0.0-beta.1+28.sha-e2c98fb
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.1+22.sha-465abab');
+var VERSION = new Version('8.0.0-beta.1+28.sha-e2c98fb');
 
 /**
  * @license
