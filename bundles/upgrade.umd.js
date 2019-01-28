adcb4ad243620a93f6741d6edb87964480e3c8f0
Jason Aden <jasonaden@google.com>
2019-01-28 20:25:59 +0000

    2f19ad9 fix(docs-infra): upgrade codelyzer to 4.5.0 (#28389)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.2+16.sha-a1b9995
+ * @license Angular v7.2.2+25.sha-2f19ad9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.2+16.sha-a1b9995');
+    var VERSION = new core.Version('7.2.2+25.sha-2f19ad9');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
