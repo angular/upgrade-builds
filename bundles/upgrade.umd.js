a676ca8d6bbd69cac3ee107663fb2946af7a0896
Jason Aden <jasonaden@google.com>
2019-01-28 20:44:20 +0000

    fd8dbd5 test(ivy): @Injectable is required on type providers (#28331)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+35.sha-fdc2b0b
+ * @license Angular v8.0.0-beta.1+56.sha-fd8dbd5
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.1+35.sha-fdc2b0b');
+    var VERSION = new core.Version('8.0.0-beta.1+56.sha-fd8dbd5');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
