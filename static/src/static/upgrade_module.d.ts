3902b09758eda11550508915e3c78d40b284aa59
Jason Aden <jasonaden@google.com>
2019-01-29 23:34:53 +0000

    dabcb3e docs: fix Redundant "See also:" link in ContentChild (#28334)

--- static/src/static/upgrade_module.d.ts
+++ static/src/static/upgrade_module.d.ts
@@ -1,4 +1,3 @@
-import * as i0 from '@angular/core';
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -7,6 +6,7 @@ import * as i0 from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 import { Injector, NgZone } from '@angular/core';
+import * as i0 from "@angular/core";
 /**
  * @description
  *
