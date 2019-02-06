ac60c64cb50f3c3adb54d6f7fe58ccbb73c1157a
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 04:57:17 +0000

    2ea4f69 test(ivy): add root cause for failing hello world e2e tests (#28540)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+10.sha-d42f32c
+ * @license Angular v8.0.0-beta.3+9.sha-2ea4f69
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+10.sha-d42f32c');
+var VERSION = new Version('8.0.0-beta.3+9.sha-2ea4f69');
 
 /**
  * @license
