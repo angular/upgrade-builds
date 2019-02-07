cffee69d6ec91e2346357d13b7b800e7071eab1e
Miško Hevery <misko@hevery.com>
2019-02-07 20:55:37 +0000

    264ef72 build: update render3 section of integration test readme (#28372)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+35.sha-50732e1
+ * @license Angular v8.0.0-beta.3+42.sha-264ef72
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -792,7 +792,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+35.sha-50732e1');
+var VERSION = new Version('8.0.0-beta.3+42.sha-264ef72');
 
 /**
  * @license
