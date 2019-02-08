108c716bb0250c7f8d2e77eae2960d3575bd3ef4
Miško Hevery <misko@hevery.com>
2019-02-08 01:24:17 +0000

    4b7264f ci: use fallback caches (#28545)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+49.sha-6f9dd1b
+ * @license Angular v8.0.0-beta.3+50.sha-4b7264f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.3+49.sha-6f9dd1b');
+    var VERSION = new core.Version('8.0.0-beta.3+50.sha-4b7264f');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
