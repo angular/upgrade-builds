def18c1e51da02b38ae068f1e041cc60d8aca062
Miško Hevery <misko@hevery.com>
2019-02-11 17:30:47 +0000

    aa163be refactor(compiler): allow disabling external symbol factory reexports (#28594)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+44.sha-2f73c55
+ * @license Angular v7.2.4+45.sha-aa163be
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18,7 +18,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.4+44.sha-2f73c55');
+var VERSION = new Version('7.2.4+45.sha-aa163be');
 
 /**
  * @license
