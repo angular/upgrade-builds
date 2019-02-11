119b166a4a6bf4326c30bf2dd11d20c9312e9c88
Miško Hevery <misko@hevery.com>
2019-02-11 17:36:05 +0000

    570f735 docs: clarify library types integration (#28410)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+88.sha-9ec8aa5
+ * @license Angular v8.0.0-beta.3+90.sha-570f735
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.3+88.sha-9ec8aa5');
+    var VERSION = new core.Version('8.0.0-beta.3+90.sha-570f735');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
