36ee368805b44451e844b8f6230ddd6383694861
Misko Hevery <misko@hevery.com>
2019-02-14 04:01:30 +0000

    1df3aef fix(forms): mark form as pristine before emitting value and status change events (#28395)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+162.sha-3842dd6
+ * @license Angular v8.0.0-beta.3+167.sha-1df3aef
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.3+162.sha-3842dd6');
+    var VERSION = new core.Version('8.0.0-beta.3+167.sha-1df3aef');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
