a9fed5eccf9dd45c8eaaff366defd6a86642c39c
Misko Hevery <misko@hevery.com>
2019-02-14 03:53:11 +0000

    3842dd6 fix(ivy): OnChanges should support updating one Input among many (#28693)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+172.sha-841a1d3
+ * @license Angular v8.0.0-beta.3+162.sha-3842dd6
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.3+172.sha-841a1d3');
+    var VERSION = new core.Version('8.0.0-beta.3+162.sha-3842dd6');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
