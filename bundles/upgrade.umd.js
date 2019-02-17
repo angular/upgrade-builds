a8b865a3725640b0131a77d9ed04842d709c1ace
Misko Hevery <misko@hevery.com>
2019-02-17 05:27:55 +0000

    04a1923 ci: add /packages/* to the @angular/fw-dev-infra CODEOWNERS group (#28745)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+2.sha-c1094cf
+ * @license Angular v8.0.0-beta.4+4.sha-04a1923
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.4+2.sha-c1094cf');
+    var VERSION = new core.Version('8.0.0-beta.4+4.sha-04a1923');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+2.sha-c1094cf
+ * @license Angular v8.0.0-beta.4+4.sha-04a1923
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4+2.sha-c1094cf');
+var VERSION = new Version('8.0.0-beta.4+4.sha-04a1923');
 
 /**
  * @license
