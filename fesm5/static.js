0a97ea23ccb9a1692bef7adfd9adb04cde713f33
Igor Minar <igor@angular.io>
2019-02-19 21:09:37 +0000

    00a8b07 ci: combine "define_env_vars" and "download_yarn" anchor (#28788)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+20.sha-80c7aff
+ * @license Angular v8.0.0-beta.4+21.sha-00a8b07
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -792,7 +792,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4+20.sha-80c7aff');
+var VERSION = new Version('8.0.0-beta.4+21.sha-00a8b07');
 
 /**
  * @license
