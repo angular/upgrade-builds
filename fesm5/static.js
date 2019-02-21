f5bd996c81009f0e6018b5229e37fa8b5f2472a3
Igor Minar <igor@angular.io>
2019-02-21 20:59:09 +0000

    d8704a3 test(ivy): turn on passing Material tests for paginator (#28890)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+18.sha-64e6aaa
+ * @license Angular v8.0.0-beta.5+19.sha-d8704a3
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+18.sha-64e6aaa');
+var VERSION = new Version('8.0.0-beta.5+19.sha-d8704a3');
 
 /**
  * @license
