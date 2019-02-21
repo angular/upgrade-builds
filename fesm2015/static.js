262207198a270f836032a7d30ce870f545104a96
Ben Lesh <ben@benlesh.com>
2019-02-21 20:57:32 +0000

    2817764 ci: make yarn globally available and independent of `$BASH_ENV` (#28889)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+6.sha-145639d
+ * @license Angular v7.2.6+7.sha-2817764.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -991,7 +991,7 @@ function downgradeInjectable(token, downgradedModule = '') {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.6+6.sha-145639d');
+const VERSION = new Version('7.2.6+7.sha-2817764.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
