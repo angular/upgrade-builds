7b945c2c8765c3da341fb417f52cacba9b3817fb
Igor Minar <igor@angular.io>
2019-02-22 02:20:22 +0000

    9defc00 build(bazel): cleanup the jasmine bootstrap code (#28906)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+23.sha-1145bdb.with-local-changes
+ * @license Angular v8.0.0-beta.5+24.sha-9defc00.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+23.sha-1145bdb.with-local-changes');
+var VERSION = new Version('8.0.0-beta.5+24.sha-9defc00.with-local-changes');
 
 /**
  * @license
