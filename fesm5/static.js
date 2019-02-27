3c489ecad7232adccd573eb5a45dbf8d5956a053
Ben Lesh <ben@benlesh.com>
2019-02-27 20:18:00 +0000

    40833ba fix(ivy): process property bindings in i18n blocks similar to non-i18n bindings (#28969)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+12.sha-34bdebc.with-local-changes
+ * @license Angular v8.0.0-beta.6+13.sha-40833ba.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -792,7 +792,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.6+12.sha-34bdebc.with-local-changes');
+var VERSION = new Version('8.0.0-beta.6+13.sha-40833ba.with-local-changes');
 
 /**
  * @license
