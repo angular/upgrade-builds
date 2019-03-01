9df01177057b82194bab953a81cda945cc956212
Andrew Kushnir <akushnir@google.com>
2019-03-01 23:35:45 +0000

    ff8e4dd test(animations): fix unit-based delays within the animation DSL (#28993)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+43.sha-7ac58be.with-local-changes
+ * @license Angular v8.0.0-beta.6+42.sha-ff8e4dd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -792,7 +792,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.6+43.sha-7ac58be.with-local-changes');
+var VERSION = new Version('8.0.0-beta.6+42.sha-ff8e4dd.with-local-changes');
 
 /**
  * @license
