8420dc9ea971eff61bb261ca288fd4eaa401a441
Andrew Kushnir <akushnir@google.com>
2019-03-06 05:35:33 +0000

    84406e4 fix(ivy): avoid innerHTML usage in exports test (to make it work in IE11) (#29127)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+89.sha-c29d2a4.with-local-changes
+ * @license Angular v8.0.0-beta.6+90.sha-84406e4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.6+89.sha-c29d2a4.with-local-changes');
+var VERSION = new Version('8.0.0-beta.6+90.sha-84406e4.with-local-changes');
 
 /**
  * @license
