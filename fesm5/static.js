4c68e108e525eb476375e824ef004837a1e8156f
Andrew Kushnir <akushnir@google.com>
2019-03-06 00:33:55 +0000

    25166d4 fix(ivy): support property values changed in ngOnChanges (forward rref case) (#29054)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+74.sha-3063547.with-local-changes
+ * @license Angular v8.0.0-beta.6+84.sha-25166d4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -792,7 +792,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.6+74.sha-3063547.with-local-changes');
+var VERSION = new Version('8.0.0-beta.6+84.sha-25166d4.with-local-changes');
 
 /**
  * @license
