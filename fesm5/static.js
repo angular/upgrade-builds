4108046f0172f4d1e3b61d246210debcdefabe46
Kara Erickson <karakara@google.com>
2019-03-07 23:49:51 +0000

    aa6db0d docs: clarify intro and examples (#29012)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+34.sha-d95e059.with-local-changes
+ * @license Angular v8.0.0-beta.7+37.sha-aa6db0d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -792,7 +792,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.7+34.sha-d95e059.with-local-changes');
+var VERSION = new Version('8.0.0-beta.7+37.sha-aa6db0d.with-local-changes');
 
 /**
  * @license
