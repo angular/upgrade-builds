27e1bc4bb96189971f14b8a73c00da938260cb2c
Kara Erickson <karakara@google.com>
2019-03-11 22:55:05 +0000

    ed4675e docs: update Universal guide (#28956)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+61.sha-146256a.with-local-changes
+ * @license Angular v8.0.0-beta.7+63.sha-ed4675e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -792,7 +792,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.7+61.sha-146256a.with-local-changes');
+var VERSION = new Version('8.0.0-beta.7+63.sha-ed4675e.with-local-changes');
 
 /**
  * @license
