351e4d3ebc6b2742e61faa7693c523a45a5fe666
Kara Erickson <karakara@google.com>
2019-03-12 18:53:50 +0000

    e6117a3 ci: rebase PRs on target branch (#29215)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+71.sha-37b9f06.with-local-changes
+ * @license Angular v8.0.0-beta.7+73.sha-e6117a3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.7+71.sha-37b9f06.with-local-changes');
+var VERSION = new Version('8.0.0-beta.7+73.sha-e6117a3.with-local-changes');
 
 /**
  * @license
