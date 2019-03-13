12b5d2cee0c7f06c8f81f00c5ac4bee3b60ac826
Kara Erickson <karakara@google.com>
2019-03-13 18:00:56 +0000

    eb00a37 build: user bazelrc should be able to overwrite existing flags (#29279)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+11.sha-7b20cec.with-local-changes
+ * @license Angular v8.0.0-beta.8+10.sha-eb00a37.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.8+11.sha-7b20cec.with-local-changes');
+var VERSION = new Version('8.0.0-beta.8+10.sha-eb00a37.with-local-changes');
 
 /**
  * @license
