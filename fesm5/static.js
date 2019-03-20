b0566428b7c815d87b5973c063c39f9c3597bd55
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 19:30:40 +0000

    68a9fe8 test: remove symlink workaround (#29426)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+42.sha-10734ac.with-local-changes
+ * @license Angular v8.0.0-beta.9+41.sha-68a9fe8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.9+42.sha-10734ac.with-local-changes');
+var VERSION = new Version('8.0.0-beta.9+41.sha-68a9fe8.with-local-changes');
 
 /**
  * @license
