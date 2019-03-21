21f024770bd67a56f7ecc0db7162f189bad85848
Matias Niemelä <matias@yearofmoo.com>
2019-03-21 07:04:13 +0000

    41737bb docs: fix html tags in changelog (#29411)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+47.sha-afe3e72.with-local-changes
+ * @license Angular v8.0.0-beta.9+48.sha-41737bb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.9+47.sha-afe3e72.with-local-changes');
+var VERSION = new Version('8.0.0-beta.9+48.sha-41737bb.with-local-changes');
 
 /**
  * @license
