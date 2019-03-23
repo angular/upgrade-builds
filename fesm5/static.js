d1d0b84d6370a9e02a65ccc37cb7edd237bb91db
Miško Hevery <misko@hevery.com>
2019-03-23 00:01:26 +0000

    bef5043 fix(ivy): TestBed overriding custom ErrorHandler (#29482)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+82.sha-b5295ad.with-local-changes
+ * @license Angular v8.0.0-beta.9+83.sha-bef5043.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.9+82.sha-b5295ad.with-local-changes');
+var VERSION = new Version('8.0.0-beta.9+83.sha-bef5043.with-local-changes');
 
 /**
  * @license
