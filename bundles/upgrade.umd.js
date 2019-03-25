e6f07d9c36daaee9d4677d5cc1906aa02cfb7807
Misko Hevery <misko@hevery.com>
2019-03-25 16:42:27 +0000

    3612ddb refactor: remove extra line break in gulpfile (#29489)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+85.sha-b3102b9.with-local-changes
+ * @license Angular v8.0.0-beta.9+87.sha-3612ddb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.9+85.sha-b3102b9.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.9+87.sha-3612ddb.with-local-changes');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
