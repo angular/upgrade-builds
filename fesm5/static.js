8df4fa9165bbc0437c7f1a6b01dc0aa4da39672d
Miško Hevery <misko@hevery.com>
2019-03-26 20:02:52 +0000

    9724247 fix(ivy): generate empty QueryList for root component content queries (#29514)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+94.sha-7951c4f.with-local-changes
+ * @license Angular v8.0.0-beta.9+96.sha-9724247.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.9+94.sha-7951c4f.with-local-changes');
+var VERSION = new Version('8.0.0-beta.9+96.sha-9724247.with-local-changes');
 
 /**
  * @license
