707fcd69b1b4f68b725aed6c6ae3e51cfffb35d0
Miško Hevery <misko@hevery.com>
2019-03-27 17:28:27 +0000

    ac3ce0d docs: replaced hero with heroes component property (#29487) PR Close #29487

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+16.sha-b17d1a9.with-local-changes
+ * @license Angular v8.0.0-beta.10+12.sha-ac3ce0d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+16.sha-b17d1a9.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+12.sha-ac3ce0d.with-local-changes');
 
 /**
  * @license
