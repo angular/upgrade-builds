17ebefbdaf12fd310abb7e29e0a29901ec3798b4
Miško Hevery <misko@hevery.com>
2019-03-28 17:29:37 +0000

    e958447 build(docs-infra): upgrade cli command docs sources to 42e87428d (#29562)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+32.sha-931d356.with-local-changes
+ * @license Angular v8.0.0-beta.10+33.sha-e958447.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+32.sha-931d356.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+33.sha-e958447.with-local-changes');
 
 /**
  * @license
