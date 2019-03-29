46428b95073bfc4fb0c530aa0041be440d5eb014
Jason Aden <jasonaden@google.com>
2019-03-29 18:00:21 +0000

    303eae9 fix(ivy): querying should be possible on any debug element (#29534)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+51.sha-22b89ea.with-local-changes
+ * @license Angular v8.0.0-beta.10+53.sha-303eae9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+51.sha-22b89ea.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+53.sha-303eae9.with-local-changes');
 
 /**
  * @license
