7cc7f8dda53f05d340981fca085eaf8151188a22
Jason Aden <jasonaden@google.com>
2019-04-01 22:33:40 +0000

    7041e61 perf(ivy): basic incremental compilation for ngtsc (#29380)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+87.sha-3e56976.with-local-changes
+ * @license Angular v8.0.0-beta.10+90.sha-7041e61.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+87.sha-3e56976.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+90.sha-7041e61.with-local-changes');
 
 /**
  * @license
