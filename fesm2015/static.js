4874f3553145b29c8ed29cbf6cd3aa18c642f9ed
Jason Aden <jasonaden@google.com>
2019-04-02 18:04:26 +0000

    6bf8e37 docs: add info regarding injecting custom pipes (#28291)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+55.sha-c7075fe.with-local-changes
+ * @license Angular v7.2.11+57.sha-6bf8e37.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -991,7 +991,7 @@ function downgradeInjectable(token, downgradedModule = '') {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.11+55.sha-c7075fe.with-local-changes');
+const VERSION = new Version('7.2.11+57.sha-6bf8e37.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
