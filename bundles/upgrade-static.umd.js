ed3b185204c8e8ba1451be62b38c2e1da6230acc
Jason Aden <jasonaden@google.com>
2019-04-02 18:12:58 +0000

    98f8b0f fix(ivy): ngcc - properly handle aliases class expressions (#29119)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+104.sha-e5201f9.with-local-changes
+ * @license Angular v8.0.0-beta.10+108.sha-98f8b0f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,8 +20,7 @@
     function noNg() {
         throw new Error('AngularJS v1.x is not loaded!');
     }
-    var ɵ0 = function () { return noNg(); };
-    var noNgElement = (ɵ0);
+    var noNgElement = (function () { return noNg(); });
     noNgElement.cleanData = noNg;
     var angular = {
         bootstrap: noNg,
@@ -843,7 +842,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.10+104.sha-e5201f9.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.10+108.sha-98f8b0f.with-local-changes');
 
     /**
      * @license
@@ -1833,7 +1832,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    // This file only re-exports content of the `src` folder. Keep it that way.
+    // This file only re-exports items to appear in the public api. Keep it that way.
 
     /**
      * @license
