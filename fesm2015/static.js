301c1526a37a45332a6266fbccdd4b06de7b738d
Jason Aden <jasonaden@google.com>
2019-04-02 18:02:04 +0000

    c7075fe refactor(compiler): rename INTERPOLATION_BLACKLIST_REGEXPS (#29593)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+53.sha-f6864cc.with-local-changes
+ * @license Angular v7.2.11+55.sha-c7075fe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -991,7 +991,7 @@ function downgradeInjectable(token, downgradedModule = '') {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.11+53.sha-f6864cc.with-local-changes');
+const VERSION = new Version('7.2.11+55.sha-c7075fe.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
