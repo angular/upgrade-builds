405d0af39562a50d38a4e1a93fa352052bf807d6
Jason Aden <jasonaden@google.com>
2019-04-03 22:58:39 +0000

    b9fead7 docs: update announcements for home page (#29671)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+4.sha-9810c6c.with-local-changes
+ * @license Angular v8.0.0-beta.11+6.sha-b9fead7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -767,7 +767,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('8.0.0-beta.11+4.sha-9810c6c.with-local-changes');
+const VERSION = new Version('8.0.0-beta.11+6.sha-b9fead7.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+4.sha-9810c6c.with-local-changes
+ * @license Angular v8.0.0-beta.11+6.sha-b9fead7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.11+4.sha-9810c6c.with-local-changes');
+var VERSION = new Version('8.0.0-beta.11+6.sha-b9fead7.with-local-changes');
 
 /**
  * @license
