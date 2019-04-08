607ad590cb8be756148d9ac018d494c1262bd036
Igor Minar <igor@angular.io>
2019-04-08 17:04:42 +0000

    6d35c50 test(core): add test for template variable object property assignment (#29708)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+32.sha-8bbf481.with-local-changes
+ * @license Angular v8.0.0-beta.11+34.sha-6d35c50.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.11+32.sha-8bbf481.with-local-changes');
+var VERSION = new Version('8.0.0-beta.11+34.sha-6d35c50.with-local-changes');
 
 /**
  * @license
