53e214cb076eb86fcbd0ec6c38aecee267e493ad
Igor Minar <igor@angular.io>
2019-04-10 19:43:24 +0000

    19fe45d docs: update path mappings and exports (#29810)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+25.sha-7f1c5f6.with-local-changes
+ * @license Angular v7.2.12+27.sha-19fe45d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.12+25.sha-7f1c5f6.with-local-changes');
+var VERSION = new Version('7.2.12+27.sha-19fe45d.with-local-changes');
 
 /**
  * @license
