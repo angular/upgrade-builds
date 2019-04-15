eb297d848ef69455e24a8a25688854e84a2dcb9f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-15 23:18:55 +0000

    8027b3e ci(docs-infra): temporarily lower the min required PWA score for localhost tests (#29911)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+15.sha-3a836c3.with-local-changes
+ * @license Angular v8.0.0-beta.12+16.sha-8027b3e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.12+15.sha-3a836c3.with-local-changes');
+var VERSION = new Version('8.0.0-beta.12+16.sha-8027b3e.with-local-changes');
 
 /**
  * @license
