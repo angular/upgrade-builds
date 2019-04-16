83827d15a8e7c278721f4fd7fa6cdef8ec13b4a1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-16 17:04:33 +0000

    2deac0a perf(ivy): cache multiple reads to an element's stylingContext (#29818)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+16.sha-8027b3e.with-local-changes
+ * @license Angular v8.0.0-beta.12+18.sha-2deac0a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.12+16.sha-8027b3e.with-local-changes');
+var VERSION = new Version('8.0.0-beta.12+18.sha-2deac0a.with-local-changes');
 
 /**
  * @license
