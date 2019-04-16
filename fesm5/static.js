52362a7160ef94a7c39e5c11f24c2fe939ed3a05
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-16 23:04:20 +0000

    ab60362 release: cut the v8.0.0-beta.13 release

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+25.sha-86a3f90.with-local-changes
+ * @license Angular v8.0.0-beta.13.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.12+25.sha-86a3f90.with-local-changes');
+var VERSION = new Version('8.0.0-beta.13.with-local-changes');
 
 /**
  * @license
