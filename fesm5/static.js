7159dda3ef856c9f494dca824d47f5323e06e15e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-16 17:04:19 +0000

    5d824c4 fix(ivy): DebugNode.properties not preserving type of values (#29914)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+18.sha-2deac0a.with-local-changes
+ * @license Angular v8.0.0-beta.12+20.sha-5d824c4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.12+18.sha-2deac0a.with-local-changes');
+var VERSION = new Version('8.0.0-beta.12+20.sha-5d824c4.with-local-changes');
 
 /**
  * @license
