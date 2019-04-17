e4974bef7669792c6e50d5cc75f2dac0cff2b25b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-17 03:29:11 +0000

    d9c39dc fix(ivy): directiveInject should fall back to inject (#29948)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+26.sha-1794a8e.with-local-changes
+ * @license Angular v8.0.0-beta.13+2.sha-d9c39dc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.12+26.sha-1794a8e.with-local-changes');
+var VERSION = new Version('8.0.0-beta.13+2.sha-d9c39dc.with-local-changes');
 
 /**
  * @license
