6b48c4023a57b41e3dc044ae1f5fc4502893f26d
Ben Lesh <ben@benlesh.com>
2019-04-19 17:00:25 +0000

    f9bb53a fix(ivy): allow TestBed.createComponent to create components in isolation (#29981)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+28.sha-c1d5fbd.with-local-changes
+ * @license Angular v8.0.0-beta.13+29.sha-f9bb53a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.13+28.sha-c1d5fbd.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.13+29.sha-f9bb53a.with-local-changes');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
