2adbb1ad56ee38537bb97aec9ec6402684b1802b
Ben Lesh <ben@benlesh.com>
2019-04-19 17:16:26 +0000

    0c4430b build(docs-infra): make tsconfig path detection in `switch-to-ivy` more robust (#29989)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.13+12.sha-4994527.with-local-changes
+ * @license Angular v7.2.13+17.sha-0c4430b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.13+12.sha-4994527.with-local-changes');
+var VERSION = new Version('7.2.13+17.sha-0c4430b.with-local-changes');
 
 /**
  * @license
