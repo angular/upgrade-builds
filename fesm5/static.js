762182ab843cf0f59d953591a88fe16687c9eab7
Andrew Kushnir <akushnir@google.com>
2019-04-25 00:18:37 +0000

    a9379e0 docs(bazel): add aio/guide/bazel (#30042)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+36.sha-6da1959.with-local-changes
+ * @license Angular v8.0.0-beta.14+37.sha-a9379e0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -791,7 +791,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.14+36.sha-6da1959.with-local-changes');
+var VERSION = new Version('8.0.0-beta.14+37.sha-a9379e0.with-local-changes');
 
 /**
  * @license
