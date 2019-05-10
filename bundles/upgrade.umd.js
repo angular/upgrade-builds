7f36782a51ecd1fef3c4e8687850efd150460644
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-10 16:41:00 +0000

    24e172d test(ivy): use `TestBed` for render3 component tests (#30282) PR Close #30282

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+136.sha-9abf114.with-local-changes
+ * @license Angular v8.0.0-rc.0+138.sha-24e172d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.0+136.sha-9abf114.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.0+138.sha-24e172d.with-local-changes');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
