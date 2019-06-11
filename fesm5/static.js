d03ee065b45f21c3bba1fc7094afd56c618bc786
Andrew Kushnir <akushnir@google.com>
2019-06-11 21:47:50 +0000

    338e58c docs: use const in dynamic-component-loader example (#30888)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+60.sha-56a3dcf.with-local-changes
+ * @license Angular v8.0.0+62.sha-338e58c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -793,7 +793,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0+60.sha-56a3dcf.with-local-changes');
+var VERSION = new Version('8.0.0+62.sha-338e58c.with-local-changes');
 
 /**
  * @license
