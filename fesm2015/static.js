2548cad41b423c5dff2c28b6833309dd2fc2db79
Kara Erickson <karakara@google.com>
2019-07-23 23:11:00 +0000

    9eefe25e2 fix(ivy): components created with wrong context when passing root node to component factory (#31661)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+50.sha-b31a292.with-local-changes
+ * @license Angular v8.2.0-next.2+53.sha-9eefe25.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -770,7 +770,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('8.2.0-next.2+50.sha-b31a292.with-local-changes');
+const VERSION = new Version('8.2.0-next.2+53.sha-9eefe25.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
