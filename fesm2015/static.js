e0435eff316593dbffe41702e445a4965a5c1b47
Miško Hevery <misko@hevery.com>
2019-07-24 22:05:10 +0000

    c73b75f27 fix(zone.js): should remove on symbol property after removeAllListeners (#31644)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+25.sha-0440dc9.with-local-changes
+ * @license Angular v8.1.2+28.sha-c73b75f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -770,7 +770,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('8.1.2+25.sha-0440dc9.with-local-changes');
+const VERSION = new Version('8.1.2+28.sha-c73b75f.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
