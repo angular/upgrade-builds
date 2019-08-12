d548c7d3d110cbc8fc82f81e84d3023739155658
Kara Erickson <karakara@google.com>
2019-08-12 20:38:51 +0000

    76854287e Revert "fix(compiler): do not remove whitespace wrapping i18n expansions (#31962)" (#32110)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.1+11.sha-972550e.with-local-changes
+ * @license Angular v8.2.1+12.sha-7685428.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.1+11.sha-972550e.with-local-changes');
+    var VERSION = new core.Version('8.2.1+12.sha-7685428.with-local-changes');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
