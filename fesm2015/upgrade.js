9e3381c2d15b68417f119b8500af69a738428f3d
Kara Erickson <karakara@google.com>
2019-08-13 19:29:21 +0000

    3d1b82be6 docs: correct description of animation example (#32009)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+8.sha-7b3bd21.with-local-changes
+ * @license Angular v8.2.2+5.sha-3d1b82b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('8.2.2+8.sha-7b3bd21.with-local-changes');
+const VERSION = new Version('8.2.2+5.sha-3d1b82b.with-local-changes');
 
 /**
  * @license
--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+8.sha-7b3bd21.with-local-changes
+ * @license Angular v8.2.2+5.sha-3d1b82b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -871,7 +871,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.2+8.sha-7b3bd21.with-local-changes');
+var VERSION = new Version('8.2.2+5.sha-3d1b82b.with-local-changes');
 
 /**
  * @license
