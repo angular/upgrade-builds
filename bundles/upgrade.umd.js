c5eef11d961a6bd1a152f335aac7fb382f57765d
atscott <atscott@google.com>
2019-08-26 22:25:17 +0000

    514271ba9 docs: reference articles specifically (#32221)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+18.sha-6b0b7d0.with-local-changes
+ * @license Angular v8.2.3+20.sha-514271b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.3+18.sha-6b0b7d0.with-local-changes');
+    var VERSION = new core.Version('8.2.3+20.sha-514271b.with-local-changes');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
