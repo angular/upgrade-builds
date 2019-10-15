ff5e7277aff26e7262a1f3e43a7617b6c361cf8d
Miško Hevery <misko@hevery.com>
2019-10-15 21:31:04 +0000

    b71ccc295 docs: fix JDK url (#33168)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.10+13.sha-9409dce.with-local-changes
+ * @license Angular v8.2.10+18.sha-b71ccc2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.10+13.sha-9409dce.with-local-changes');
+    var VERSION = new core.Version('8.2.10+18.sha-b71ccc2.with-local-changes');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
