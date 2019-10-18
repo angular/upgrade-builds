e4e27044c328d94b746494c2f9d1d2dd980c3f9d
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 20:50:16 +0000

    d9581bd6a4 ci: fix dev-infra incorrectly matching all ".bzl" files as codeowner (#33249)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.11+8.sha-19ada81.with-local-changes
+ * @license Angular v8.2.11+9.sha-d9581bd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.11+8.sha-19ada81.with-local-changes');
+    var VERSION = new core.Version('8.2.11+9.sha-d9581bd.with-local-changes');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
