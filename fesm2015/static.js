4a5456e59de41300b4f97bae47eb9f1b7be4111e
atscott <atscott01@gmail.com>
2019-11-06 20:52:53 +0000

    41560b47c4 refactor(ivy): move all styling configurations into `TNodeFlags` (#33540)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+77.sha-2ed04ff.with-local-changes
+ * @license Angular v9.0.0-rc.0+69.sha-41560b4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -840,7 +840,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('9.0.0-rc.0+77.sha-2ed04ff.with-local-changes');
+const VERSION = new Version('9.0.0-rc.0+69.sha-41560b4.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
@@ -2161,5 +2161,5 @@ if (false) {
  * Generated bundle index. Do not edit.
  */
 
-export { UpgradeComponent, UpgradeModule, VERSION, downgradeComponent, downgradeInjectable, downgradeModule, getAngularJSGlobal, getAngularLib, setAngularJSGlobal, setAngularLib, injectorFactory as ɵangular_packages_upgrade_static_static_a, rootScopeFactory as ɵangular_packages_upgrade_static_static_b, compileFactory as ɵangular_packages_upgrade_static_static_c, parseFactory as ɵangular_packages_upgrade_static_static_d, angular1Providers as ɵangular_packages_upgrade_static_static_e };
+export { angular1Providers as ɵangular_packages_upgrade_static_static_e, compileFactory as ɵangular_packages_upgrade_static_static_c, injectorFactory as ɵangular_packages_upgrade_static_static_a, parseFactory as ɵangular_packages_upgrade_static_static_d, rootScopeFactory as ɵangular_packages_upgrade_static_static_b, getAngularJSGlobal, getAngularLib, setAngularJSGlobal, setAngularLib, downgradeComponent, downgradeInjectable, VERSION, downgradeModule, UpgradeComponent, UpgradeModule };
 //# sourceMappingURL=static.js.map
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+77.sha-2ed04ff.with-local-changes
+ * @license Angular v9.0.0-rc.0+69.sha-41560b4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('9.0.0-rc.0+77.sha-2ed04ff.with-local-changes');
+const VERSION = new Version('9.0.0-rc.0+69.sha-41560b4.with-local-changes');
 
 /**
  * @license
@@ -2422,5 +2422,5 @@ if (false) {
  * Generated bundle index. Do not edit.
  */
 
-export { UpgradeAdapter, UpgradeAdapterRef, VERSION };
+export { VERSION, UpgradeAdapter, UpgradeAdapterRef };
 //# sourceMappingURL=upgrade.js.map
