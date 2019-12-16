f1704a3ffd720db7fa27b9e1c12451cdaf2291ed
Kara Erickson <karakara@google.com>
2019-12-16 16:10:10 +0000

    12444a8afc test(ngcc): cleanup entry-point bundle testcases (#34415)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+478.sha-04ab036
+ * @license Angular v9.0.0-rc.1+480.sha-12444a8
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -814,6 +814,9 @@
      *
      * {@example upgrade/static/ts/full/module.ts region="ng2-heroes-wrapper"}
      *
+     * For more details and examples on downgrading Angular components to AngularJS components please
+     * visit the [Upgrade guide](guide/upgrade#using-angular-components-from-angularjs-code).
+     *
      * @param info contains information about the Component that is being downgraded:
      *
      * - `component: Type<any>`: The type of the Component that will be downgraded
@@ -1068,7 +1071,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+478.sha-04ab036');
+    var VERSION = new core.Version('9.0.0-rc.1+480.sha-12444a8');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+478.sha-04ab036
+ * @license Angular v9.0.0-rc.1+480.sha-12444a8
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18,7 +18,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+478.sha-04ab036');
+var VERSION = new Version('9.0.0-rc.1+480.sha-12444a8');
 
 /**
  * @license
@@ -626,6 +626,9 @@ var SyncPromise = /** @class */ (function () {
  * can use to define the AngularJS directive that wraps the "downgraded" component.
  *
  * {@example upgrade/static/ts/full/module.ts region="ng2-heroes-wrapper"}
+ *
+ * For more details and examples on downgrading Angular components to AngularJS components please
+ * visit the [Upgrade guide](guide/upgrade#using-angular-components-from-angularjs-code).
  *
  * @param info contains information about the Component that is being downgraded:
  *
