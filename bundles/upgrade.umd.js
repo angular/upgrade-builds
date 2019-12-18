48555674bc3f2ddaa1c3b1185bdc24700bee0a17
Kara Erickson <karakara@google.com>
2019-12-18 17:38:46 +0000

    1ebb405ef0 build(docs-infra): upgrade cli command docs sources to 30d7b1353 (#34465)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.6+68.sha-82dce68
+ * @license Angular v9.0.0-rc.6+72.sha-1ebb405
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.6+68.sha-82dce68');
+    var VERSION = new core.Version('9.0.0-rc.6+72.sha-1ebb405');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
