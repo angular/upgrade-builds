4e06252e43ae22d336c155cdcc9e86348abefa13
atscott <atscott@google.com>
2020-01-09 23:36:38 +0000

    f7a2ed7d66 docs(ivy): document breaking change with <select> and binding order (#34709)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+18.sha-f295240
+ * @license Angular v9.0.0-rc.8+29.sha-f7a2ed7
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('9.0.0-rc.8+18.sha-f295240');
+const VERSION = new Version('9.0.0-rc.8+29.sha-f7a2ed7');
 
 /**
  * @license
