e34cc3b29d8541c3b870657cd4704a9ff0d6c473
atscott <atscott@google.com>
2020-01-14 23:50:21 +0000

    c4d035e886 refactor(compiler): refactor and export Bazel host creation. (#34686)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+116.sha-3113bb7
+ * @license Angular v9.0.0-rc.8+118.sha-c4d035e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18,7 +18,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.8+116.sha-3113bb7');
+var VERSION = new Version('9.0.0-rc.8+118.sha-c4d035e');
 
 /**
  * @license
