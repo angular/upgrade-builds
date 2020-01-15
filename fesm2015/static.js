cead1ef32f2905ddb74be14821857855e387d852
atscott <atscott@google.com>
2020-01-15 00:13:06 +0000

    fa39a8c026 feat(language-service): provide completion for $event variable (#34566)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+118.sha-c4d035e
+ * @license Angular v9.0.0-rc.8+119.sha-fa39a8c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -843,7 +843,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('9.0.0-rc.8+118.sha-c4d035e');
+const VERSION = new Version('9.0.0-rc.8+119.sha-fa39a8c');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+118.sha-c4d035e
+ * @license Angular v9.0.0-rc.8+119.sha-fa39a8c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -872,7 +872,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.8+118.sha-c4d035e');
+var VERSION = new Version('9.0.0-rc.8+119.sha-fa39a8c');
 
 /**
  * @license
