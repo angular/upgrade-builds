fab362f542e04e1fe85e9cbc8de088f211d24ae9
GitHub <noreply@github.com>
2020-01-15 12:14:41 +0000

    50e52c8e1b fixup! docs: fix public api tool name

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+689.sha-ae9c061
+ * @license Angular v9.0.0-rc.1+691.sha-50e52c8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18,7 +18,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+689.sha-ae9c061');
+var VERSION = new Version('9.0.0-rc.1+691.sha-50e52c8');
 
 /**
  * @license
