a2fd9d9845a902e15d0a527b4df36c6014dd5a55
Miško Hevery <misko@hevery.com>
2020-01-22 00:52:02 +0000

    ad68b618e2 refactor(ivy): Implement `computeStaticStyling` (#34418)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,6 +1,6 @@
 /**
- * @license Angular v9.0.0-rc.1+636.sha-142363a
- * (c) 2010-2019 Google LLC. https://angular.io/
+ * @license Angular v9.0.0-rc.1+745.sha-ad68b61
+ * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
 
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('9.0.0-rc.1+636.sha-142363a');
+const VERSION = new Version('0.0.0');
 
 /**
  * @license
