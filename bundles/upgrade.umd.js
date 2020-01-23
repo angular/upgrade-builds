9c3cfcb5bf7832ee196ecea1e4f330bee7c28541
Andrew Kushnir <akushnir@google.com>
2020-01-23 20:03:05 +0000

    6a4186c017 build: vendor yarn using the .yarnrc yarn-path value (#34902)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+34.sha-e69d02c
+ * @license Angular v9.0.0-rc.10+37.sha-6a4186c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.10+34.sha-e69d02c');
+    var VERSION = new core.Version('9.0.0-rc.10+37.sha-6a4186c');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
