ff7e903834027b9f9150c9d18c15c062c4b1dd3a
Andrew Kushnir <akushnir@google.com>
2020-01-23 00:50:10 +0000

    3999d7adf0 docs: update payload size limits for angular.io application (#34916)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+15.sha-a4cbb86
+ * @license Angular v9.0.0-rc.10+22.sha-3999d7a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.10+15.sha-a4cbb86');
+    var VERSION = new core.Version('9.0.0-rc.10+22.sha-3999d7a');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
