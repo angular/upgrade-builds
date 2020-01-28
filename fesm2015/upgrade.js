2e48b7dee6eb3a62585f3a3b4748db96b63e1ce1
Andrew Kushnir <akushnir@google.com>
2020-01-28 21:25:27 +0000

    a5a598104f docs: change p to pointer for better understanding (#34927)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+836.sha-1cee9b3
+ * @license Angular v9.0.0-rc.1+837.sha-a5a5981
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('9.0.0-rc.1+836.sha-1cee9b3');
+const VERSION = new Version('9.0.0-rc.1+837.sha-a5a5981');
 
 /**
  * @license
