89232e4af758fd327cef986bed95523c3e8d85bc
Kara Erickson <karakara@google.com>
2020-02-07 21:28:52 +0000

    dea1b962c7 fix(ivy): repeat template guards to narrow types in event handlers (#35193)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+30.sha-dce230e
+ * @license Angular v9.0.0+31.sha-dea1b96
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -843,7 +843,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('9.0.0+30.sha-dce230e');
+const VERSION = new Version('9.0.0+31.sha-dea1b96');
 
 /**
  * @fileoverview added by tsickle
