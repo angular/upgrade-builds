9bf8f131d09dc5d5a90b903bb34bffd67bf71c14
Kara Erickson <karakara@google.com>
2020-02-07 22:16:46 +0000

    5b5dc811c1 docs: add asset info to library guide (#34217)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+31.sha-dea1b96
+ * @license Angular v9.0.0+32.sha-5b5dc81
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -872,7 +872,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0+31.sha-dea1b96');
+var VERSION = new Version('9.0.0+32.sha-5b5dc81');
 
 /**
  * @license
