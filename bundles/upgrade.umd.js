bcaa64d61aa18d65f868e60d52d466448776cc28
Kara Erickson <karakara@google.com>
2020-02-13 01:11:06 +0000

    a84093a971 docs: remove service from region where it was added before it was created (#35354)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.1
+ * @license Angular v9.0.1+6.sha-a84093a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.1');
+    var VERSION = new core.Version('9.0.1+6.sha-a84093a');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
