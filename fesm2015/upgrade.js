96d6a3c404a8916010ce78c865746500c3676450
atscott <atscott@google.com>
2020-02-27 19:14:59 +0000

    30f29ee8e7 docs: correct spelling of 'detection' (#35723)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+2.sha-0b97d07
+ * @license Angular v9.1.0-next.2+4.sha-30f29ee
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('9.1.0-next.2+2.sha-0b97d07');
+const VERSION = new Version('9.1.0-next.2+4.sha-30f29ee');
 
 /**
  * @license
