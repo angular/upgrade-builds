a00155d44374dd599931150cde7169cb54ac7890
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 18:02:55 +0000

    44f9fa6ce5 docs: add missing closing bracket (#35890)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+85.sha-b42e2e0
+ * @license Angular v9.1.0-next.2+87.sha-44f9fa6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('9.1.0-next.2+85.sha-b42e2e0');
+const VERSION = new Version('9.1.0-next.2+87.sha-44f9fa6');
 
 /**
  * @license
