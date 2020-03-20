1ad35c85903fdb856650fa96000c1ac0ba38595d
Misko Hevery <misko@hevery.com>
2020-03-20 20:13:20 +0000

    05eeb7d279 test(compiler): remove whitespace in spans (#36169)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0
+ * @license Angular v9.1.0-rc.0+8.sha-05eeb7d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('9.1.0-rc.0');
+const VERSION = new Version('9.1.0-rc.0+8.sha-05eeb7d');
 
 /**
  * @license
