3a4b9f9a8cbd8c47b97720b5729d7abaa9b633f8
Kara Erickson <karakara@google.com>
2020-04-07 22:46:57 +0000

    fd2ef74a31 docs: update reactive forms page (#35969)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+99.sha-b02c950
+ * @license Angular v9.1.0+106.sha-fd2ef74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -843,7 +843,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('9.1.0+99.sha-b02c950');
+const VERSION = new Version('9.1.0+106.sha-fd2ef74');
 
 /**
  * @fileoverview added by tsickle
