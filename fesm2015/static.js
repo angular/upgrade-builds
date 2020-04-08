5988caefe901e846b81da0647e95250fce6891d4
atscott <atscott@google.com>
2020-04-08 22:13:07 +0000

    cbed582a1a style(compiler): reformat of codebase with new clang-format version (#36520)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+9.sha-33eee43
+ * @license Angular v9.1.1+11.sha-cbed582
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -843,7 +843,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('9.1.1+9.sha-33eee43');
+const VERSION = new Version('9.1.1+11.sha-cbed582');
 
 /**
  * @fileoverview added by tsickle
