ecda93b7f6d0b28b61fd4f9cac5bd99be24e4cd6
Joey Perrott <josephperrott@gmail.com>
2020-04-14 01:05:37 +0000

    870657ca93 build: reformat repo to new clang@1.4.0

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+35.sha-97c1f02
+ * @license Angular v9.1.1+35.sha-870657c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('9.1.1+35.sha-97c1f02');
+const VERSION = new Version('9.1.1+35.sha-870657c');
 
 /**
  * @license
