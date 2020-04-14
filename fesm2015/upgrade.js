7b9215c00c815cdcadae4a3377c3a5616c2ffcf6
atscott <atscott@google.com>
2020-04-14 17:37:42 +0000

    29122c518b style: lint (#36580)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+27.sha-0cc53fb
+ * @license Angular v10.0.0-next.1+30.sha-29122c5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-next.1+27.sha-0cc53fb');
+const VERSION = new Version('10.0.0-next.1+30.sha-29122c5');
 
 /**
  * @license
