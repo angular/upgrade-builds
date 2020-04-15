0b79e2faf5c9957170522fc0798afd05d7b10410
atscott <atscott@google.com>
2020-04-15 16:44:17 +0000

    663b768780 fix(ngcc): force ngcc to exit on error (#36622)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+35.sha-0147de9
+ * @license Angular v10.0.0-next.1+36.sha-663b768
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.1+35.sha-0147de9');
+    var VERSION = new core.Version('10.0.0-next.1+36.sha-663b768');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
