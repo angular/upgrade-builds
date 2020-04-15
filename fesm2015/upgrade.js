dd5ae5685b7ac27569e7cb4f3ea4f9512f91ac1e
atscott <atscott@google.com>
2020-04-15 16:49:01 +0000

    e041ac6f0d fix(ngcc): display output from the unlocker process on Windows (#36569)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+36.sha-663b768
+ * @license Angular v10.0.0-next.1+38.sha-e041ac6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-next.1+36.sha-663b768');
+const VERSION = new Version('10.0.0-next.1+38.sha-e041ac6');
 
 /**
  * @license
