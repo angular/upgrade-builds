2c169b6e8461467978accd013ec4aa736a611cea
Matias Niemelä <matias@yearofmoo.com>
2020-04-16 21:35:39 +0000

    4480ba3e29 ci: remove reliance on Github API for CI setup (#36500)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+31.sha-5e79799
+ * @license Angular v10.0.0-next.2+32.sha-4480ba3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.2+31.sha-5e79799');
+    var VERSION = new core.Version('10.0.0-next.2+32.sha-4480ba3');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+31.sha-5e79799
+ * @license Angular v10.0.0-next.2+32.sha-4480ba3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-next.2+31.sha-5e79799');
+const VERSION = new Version('10.0.0-next.2+32.sha-4480ba3');
 
 /**
  * @license
