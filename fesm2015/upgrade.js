cb6a0b431329e973b6233997dd0124444c07c1bb
Matias Niemelä <matias@yearofmoo.com>
2020-04-20 20:44:38 +0000

    254b9ea44c docs: place download section in accessibility to the top (#36561)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+46.sha-2a53f47
+ * @license Angular v9.1.2+47.sha-254b9ea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('9.1.2+46.sha-2a53f47');
+const VERSION = new Version('9.1.2+47.sha-254b9ea');
 
 /**
  * @license
