bc0f48d467f14a10eb3f269093282d3c13c5a49c
Matias Niemelä <matias@yearofmoo.com>
2020-04-21 17:19:15 +0000

    28995dba19 fix(core): missing-injectable migration should not migrate `@NgModule` classes (#36369)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+51.sha-51b0924
+ * @license Angular v10.0.0-next.2+54.sha-28995db
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.2+51.sha-51b0924');
+    var VERSION = new i0.Version('10.0.0-next.2+54.sha-28995db');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
