edc99d7f53b12817c78c8971b457baa68aa1e5ad
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 20:57:15 +0000

    781f5611d1 build: update to @bazel/bazelisk@^1.4.0 (#36729)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+58.sha-5db8d93
+ * @license Angular v10.0.0-next.3+1.sha-781f561
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.2+58.sha-5db8d93');
+    var VERSION = new i0.Version('10.0.0-next.3+1.sha-781f561');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
