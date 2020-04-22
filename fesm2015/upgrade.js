8c2c34a04228ebee82b0365a2222162ab2091856
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 19:17:34 +0000

    e1071615c6 release: cut the v9.1.3 release

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+56.sha-8bd5374
+ * @license Angular v9.1.3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('9.1.2+56.sha-8bd5374');
+const VERSION = new Version('9.1.3');
 
 /**
  * @license
