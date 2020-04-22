3915d189d400399e2982c1015a737b01a99a5ade
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 02:17:53 +0000

    942b986ef3 fix(core): properly identify modules affected by overrides in TestBed (#36649)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+55.sha-acf6075
+ * @license Angular v10.0.0-next.2+56.sha-942b986
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -876,7 +876,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.2+55.sha-acf6075');
+var VERSION = new Version('10.0.0-next.2+56.sha-942b986');
 
 /**
  * @license
