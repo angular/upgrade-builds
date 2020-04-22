44f19ce8aa739d86a54d82f8b67ff101dd1fbcfa
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 04:47:59 +0000

    5db8d93fe5 build: fix the compare master and patch script output (#36749)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+57.sha-535c249
+ * @license Angular v10.0.0-next.2+58.sha-5db8d93
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-next.2+57.sha-535c249');
+const VERSION = new Version('10.0.0-next.2+58.sha-5db8d93');
 
 /**
  * @license
