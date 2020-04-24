4bb367c36cd834b4e0279819c8400a1d62cea72d
Andrew Kushnir <akushnir@google.com>
2020-04-24 16:26:10 +0000

    dd049caf0a fix(language-service): disable update the `@angular/core` module (#36783)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+24.sha-e6161ca
+ * @license Angular v10.0.0-next.3+26.sha-dd049ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-next.3+24.sha-e6161ca');
+const VERSION = new Version('10.0.0-next.3+26.sha-dd049ca');
 
 /**
  * @license
