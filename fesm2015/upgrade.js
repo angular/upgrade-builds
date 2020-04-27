d5972ba91f73d1f79cfd1896e7d96c785596e1f0
Andrew Kushnir <akushnir@google.com>
2020-04-27 21:33:50 +0000

    2a27f69522 docs: fix typo (#36786)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+26.sha-180a328
+ * @license Angular v9.1.3+27.sha-2a27f69
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('9.1.3+26.sha-180a328');
+const VERSION = new Version('9.1.3+27.sha-2a27f69');
 
 /**
  * @license
