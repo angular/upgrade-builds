f188c5523ce78d91b56a1b440cef1f385f48e4a2
Andrew Kushnir <akushnir@google.com>
2020-04-28 16:43:15 +0000

    0ce96f1d78 refactor(localize): remove unused code (#36834)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+28.sha-0f389fa
+ * @license Angular v9.1.3+34.sha-0ce96f1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('9.1.3+28.sha-0f389fa');
+const VERSION = new Version('9.1.3+34.sha-0ce96f1');
 
 /**
  * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+28.sha-0f389fa
+ * @license Angular v9.1.3+34.sha-0ce96f1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18,7 +18,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.3+28.sha-0f389fa');
+var VERSION = new Version('9.1.3+34.sha-0ce96f1');
 
 /**
  * @license
