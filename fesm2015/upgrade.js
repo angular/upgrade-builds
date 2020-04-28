fd01c3efb087cd0cafbad0ee7150c2fc6f2786fb
Andrew Kushnir <akushnir@google.com>
2020-04-28 16:35:18 +0000

    1d9375d115 ci: fix bad reference to `head` property in rebase-pr script (#36825)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+36.sha-f27deea
+ * @license Angular v10.0.0-next.3+37.sha-1d9375d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-next.3+36.sha-f27deea');
+const VERSION = new Version('10.0.0-next.3+37.sha-1d9375d');
 
 /**
  * @license
