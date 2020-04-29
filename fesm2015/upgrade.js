b466ce3d2a26390b576e012aff7ce14b7e45a557
Andrew Kushnir <akushnir@google.com>
2020-04-29 21:51:18 +0000

    62ba0acfb5 test(language-service): do not invalidate @angular/core (#36845)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4
+ * @license Angular v10.0.0-next.4+1.sha-62ba0ac
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-next.4');
+const VERSION = new Version('10.0.0-next.4+1.sha-62ba0ac');
 
 /**
  * @license
