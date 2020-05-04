7e6ae298ae448882de03c66cd265b749a9cdafa8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:10:30 +0000

    297dab845d build: migrate bazel related formatting/linting to ng-dev format (#36842)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+34.sha-0f3831b
+ * @license Angular v10.0.0-next.4+36.sha-297dab8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-next.4+34.sha-0f3831b');
+const VERSION = new Version('10.0.0-next.4+36.sha-297dab8');
 
 /**
  * @license
