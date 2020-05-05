205dce99863bb8b1c43953a530abeaee17678369
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-05 19:25:24 +0000

    b95a336f12 build: fix and re-enable elements tests on saucelabs (#36929)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+7.sha-306f46c
+ * @license Angular v10.0.0-next.5+18.sha-b95a336
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.5+7.sha-306f46c');
+    var VERSION = new core.Version('10.0.0-next.5+18.sha-b95a336');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
