fb7c0a03446b99ed5bc9f0cc07b8cb3f26549dd5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 01:05:18 +0000

    1b8752e595 refactor(docs-infra): take advantage of latest Jasmine features in preview server tests (#36837)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+24.sha-fbd281c
+ * @license Angular v10.0.0-next.5+37.sha-1b8752e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18,7 +18,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.5+24.sha-fbd281c');
+var VERSION = new Version('10.0.0-next.5+37.sha-1b8752e');
 
 /**
  * @license
