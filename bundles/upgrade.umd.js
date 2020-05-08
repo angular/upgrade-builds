5de8b30ceffe9f537a150b2ae3f241ca20eca19a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 17:09:29 +0000

    178e750f16 docs: Fix typo in navigation.json (#36955)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+5.sha-14be55c
+ * @license Angular v10.0.0-next.6+6.sha-178e750
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.6+5.sha-14be55c');
+    var VERSION = new i0.Version('10.0.0-next.6+6.sha-178e750');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
