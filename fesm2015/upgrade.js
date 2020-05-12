91e7c701658e2e1cef8dc56f9f54661ab6e627f8
Misko Hevery <misko@hevery.com>
2020-05-12 18:11:37 +0000

    8fe532e593 build(dev-infra): cleanup package dependencies for shared package (#36980)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+28.sha-681741c
+ * @license Angular v10.0.0-next.6+27.sha-8fe532e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-next.6+28.sha-681741c');
+const VERSION = new Version('10.0.0-next.6+27.sha-8fe532e');
 
 /**
  * @license
