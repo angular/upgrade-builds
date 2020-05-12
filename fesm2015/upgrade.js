403202c8193c93cb06c96a4ed9e19341c3b9312b
Misko Hevery <misko@hevery.com>
2020-05-12 18:13:15 +0000

    681741cd13 docs: minor typo fix in the bazel documentation (#37043)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+24.sha-1e208e8
+ * @license Angular v10.0.0-next.6+28.sha-681741c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-next.6+24.sha-1e208e8');
+const VERSION = new Version('10.0.0-next.6+28.sha-681741c');
 
 /**
  * @license
