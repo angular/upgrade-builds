23d06e263d969d85c29e06ba527c2f419b3a6d1a
Misko Hevery <misko@hevery.com>
2020-05-13 23:22:51 +0000

    45f4a47286 refactor(core): remove style sanitization code for `[style]`/`[style.prop]` bindings (#36965)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+8.sha-14c0ec9
+ * @license Angular v10.0.0-next.7+7.sha-45f4a47
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.7+8.sha-14c0ec9');
+    var VERSION = new core.Version('10.0.0-next.7+7.sha-45f4a47');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
