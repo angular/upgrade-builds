4307ea09adb94b58d57ddd894aa5a20be729f057
Kara Erickson <karakara@google.com>
2020-05-14 20:26:02 +0000

    719ca5283b fix(dev-infra): do not require a commit body for release commits (#37110)

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+35.sha-3813f54
+ * @license Angular v9.1.6+36.sha-719ca52
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -876,7 +876,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.6+35.sha-3813f54');
+var VERSION = new Version('9.1.6+36.sha-719ca52');
 
 /**
  * @license
