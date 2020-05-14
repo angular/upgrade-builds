be4bc6430aa6f7560b2fa2982b635f83f6fab15a
Kara Erickson <karakara@google.com>
2020-05-14 19:20:28 +0000

    5f64e52ed4 docs: clarifies the service limitation (#36349)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+44.sha-95f429c
+ * @license Angular v10.0.0-next.7+46.sha-5f64e52
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -847,7 +847,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-next.7+44.sha-95f429c');
+const VERSION = new Version('10.0.0-next.7+46.sha-5f64e52');
 
 /**
  * @fileoverview added by tsickle
