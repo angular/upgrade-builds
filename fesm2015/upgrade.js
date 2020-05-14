0d03680da3715903dbbb16766e1fe8e827f04515
Kara Erickson <karakara@google.com>
2020-05-14 22:47:20 +0000

    7a30153aa1 test(core): verify that Ivy i18n works correctly with HTML namespaces (#36943)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+49.sha-7acd330
+ * @license Angular v10.0.0-next.7+50.sha-7a30153
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-next.7+49.sha-7acd330');
+const VERSION = new Version('10.0.0-next.7+50.sha-7a30153');
 
 /**
  * @license
