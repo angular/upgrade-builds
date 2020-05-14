ec2ca0bf35faa5a125dbffa1fc8585a6669df13d
Kara Erickson <karakara@google.com>
2020-05-14 18:08:03 +0000

    2418c6aca1 refactor(docs-infra): refactors `addImageDimensionsImpl` (#37046)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+16.sha-9020507
+ * @license Angular v10.0.0-next.7+17.sha-2418c6a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -847,7 +847,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-next.7+16.sha-9020507');
+const VERSION = new Version('10.0.0-next.7+17.sha-2418c6a');
 
 /**
  * @fileoverview added by tsickle
