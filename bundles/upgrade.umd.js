35d0d8e13b1cfae028c62334a03fa33c3f6df99f
Kara Erickson <karakara@google.com>
2020-05-18 17:02:56 +0000

    1754beb59c release: cut the v10.0.0-next.8 release

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+70.sha-844208f
+ * @license Angular v10.0.0-next.8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.7+70.sha-844208f');
+    var VERSION = new core.Version('10.0.0-next.8');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
