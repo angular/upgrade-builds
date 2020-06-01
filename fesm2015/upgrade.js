83c36037643f9c5a2263999016d0bc9ce4e3331a
Matias Niemelä <matias@yearofmoo.com>
2020-06-01 21:43:12 +0000

    3b52f7066b docs: fix typo in deprecations (#37379)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+37.sha-8bb7411
+ * @license Angular v10.0.0-rc.0+38.sha-3b52f70
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+37.sha-8bb7411');
+const VERSION = new Version('10.0.0-rc.0+38.sha-3b52f70');
 
 /**
  * @license
