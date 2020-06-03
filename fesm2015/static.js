8ae809306dd98764d0635da8ce9c351d90d73ea5
atscott <atscott@google.com>
2020-06-03 22:51:14 +0000

    e35269dd87 docs: update file header to be correct (#37425)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+21.sha-60a03b7
+ * @license Angular v10.0.0-rc.2+22.sha-e35269d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -847,7 +847,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.2+21.sha-60a03b7');
+const VERSION = new Version('10.0.0-rc.2+22.sha-e35269d');
 
 /**
  * @license
