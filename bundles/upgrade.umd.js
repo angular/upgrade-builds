9627c447470d3741edcbea4b2102147d7571af90
atscott <atscott@google.com>
2020-06-08 16:52:24 +0000

    d63ecf4c5f fix(service-worker): Don't stay locked in EXISTING_CLIENTS_ONLY if corrupted data (#37453)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+89.sha-5e64c2b
+ * @license Angular v10.0.0-rc.0+88.sha-d63ecf4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+89.sha-5e64c2b');
+    var VERSION = new core.Version('10.0.0-rc.0+88.sha-d63ecf4');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
