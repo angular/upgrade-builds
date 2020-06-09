36f29b638d152556c5f37e67e8dbd3dfa972edda
atscott <atscott@google.com>
2020-06-09 15:54:37 +0000

    7edb026619 fix(common): prevent duplicate URL change notifications (#37459)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+104.sha-7301e70
+ * @license Angular v10.0.0-rc.0+103.sha-7edb026
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -847,7 +847,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+104.sha-7301e70');
+const VERSION = new Version('10.0.0-rc.0+103.sha-7edb026');
 
 /**
  * @license
