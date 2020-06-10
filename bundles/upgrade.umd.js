38f4536605c1b8988aeed58331a71888a0184616
atscott <atscott@google.com>
2020-06-10 19:25:53 +0000

    bd9c1e6cfc build: add commit message scope for migration changes (#36390)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+120.sha-e836eaf
+ * @license Angular v10.0.0-rc.0+123.sha-bd9c1e6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+120.sha-e836eaf');
+    var VERSION = new core.Version('10.0.0-rc.0+123.sha-bd9c1e6');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
