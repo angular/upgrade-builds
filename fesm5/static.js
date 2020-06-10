7838b8cbf009bdda078ff84e119d8663c313a2db
atscott <atscott@google.com>
2020-06-10 18:50:20 +0000

    f7997256fc release: cut the v9.1.11 release

--- fesm5/static.js
+++ fesm5/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.10+1.sha-dc9da17
+ * @license Angular v9.1.11
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -876,7 +876,7 @@ function downgradeInjectable(token, downgradedModule) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.10+1.sha-dc9da17');
+var VERSION = new Version('9.1.11');
 
 /**
  * @license
