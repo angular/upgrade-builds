6451fd3f99dcf4c8b6bcf03a7ed4297e3af35827
Misko Hevery <misko@hevery.com>
2020-06-11 20:55:18 +0000

    97dc85ba5e feat(core): support injection token as predicate in queries (#37506)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+134.sha-a937889
+ * @license Angular v10.0.0-rc.0+135.sha-97dc85b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+134.sha-a937889');
+const VERSION = new Version('10.0.0-rc.0+135.sha-97dc85b');
 
 /**
  * @license
