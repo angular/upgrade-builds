15f7bafdf43557dd4b953dbb4ab72ae18519167e
crisbeto <crisbeto@abv.bg>
2020-06-12 23:05:37 +0000

    6c1ab479ee fix(core): infinite loop if injectable using inheritance has a custom decorator

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.11
+ * @license Angular v9.1.11+1.sha-6c1ab47
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -847,7 +847,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('9.1.11');
+const VERSION = new Version('9.1.11+1.sha-6c1ab47');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.11
+ * @license Angular v9.1.11+1.sha-6c1ab47
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('9.1.11');
+const VERSION = new Version('9.1.11+1.sha-6c1ab47');
 
 /**
  * @license
