2898a043e69ec5b176cec3191d6298ddcff48e17
Andrew Kushnir <akushnir@google.com>
2020-06-25 21:59:32 +0000

    64f2ffa166 fix(core): cleanup DOM elements when root view is removed (#37600)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0+15.sha-a5d5f67
+ * @license Angular v10.0.0+20.sha-64f2ffa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0+15.sha-a5d5f67');
+    var VERSION = new core.Version('10.0.0+20.sha-64f2ffa');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0+15.sha-a5d5f67
+ * @license Angular v10.0.0+20.sha-64f2ffa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0+15.sha-a5d5f67');
+const VERSION = new Version('10.0.0+20.sha-64f2ffa');
 
 /**
  * @license
