bce46d10871f452ee821b16ae349c3f07e47a1b3
Andrew Kushnir <akushnir@google.com>
2020-06-25 22:46:43 +0000

    a9a4edebe2 fix(docs-infra): fix typo in the deploy-to-firebase.sh script (#37754)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0+20.sha-64f2ffa
+ * @license Angular v10.0.0+21.sha-a9a4ede
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0+20.sha-64f2ffa');
+    var VERSION = new core.Version('10.0.0+21.sha-a9a4ede');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
