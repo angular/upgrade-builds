9111bd2602a17869eee40bb4f35ba6420a23771e
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:02:29 +0000

    29490cce30 build: set up caretaker note label in merge tooling (#37675)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+230.sha-a0595f4
+ * @license Angular v10.0.0-rc.0+232.sha-29490cc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+230.sha-a0595f4');
+    var VERSION = new core.Version('10.0.0-rc.0+232.sha-29490cc');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
