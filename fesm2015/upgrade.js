f78bdbc8762203bfed6b754956fbb3e6bf2dbd65
Andrew Kushnir <akushnir@google.com>
2020-06-26 22:20:21 +0000

    2038568f3e ci: decrease expected AIO and integration payload sizes (#36578) (#36578)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+283.sha-ae5257c
+ * @license Angular v10.0.0-rc.0+287.sha-2038568
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+283.sha-ae5257c');
+const VERSION = new Version('10.0.0-rc.0+287.sha-2038568');
 
 /**
  * @license
