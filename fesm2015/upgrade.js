0d7ec2e55469277ef2acc9c30e83e0b0f54dd0e7
Andrew Kushnir <akushnir@google.com>
2020-06-27 00:10:04 +0000

    78460c1848 test(core): update symbols used in the test app (#37785)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.1
+ * @license Angular v10.0.1+4.sha-78460c1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.1');
+const VERSION = new Version('10.0.1+4.sha-78460c1');
 
 /**
  * @license
