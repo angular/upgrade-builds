e7269e56b2c6b4d6e2b8d516d085d1b1c89bffa6
Andrew Kushnir <akushnir@google.com>
2020-06-29 19:45:22 +0000

    7a91b23cb5 fix(core): fake_async_fallback should have the same logic with fake-async (#37680)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.1+10.sha-89e16ed
+ * @license Angular v10.0.1+16.sha-7a91b23
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1106,7 +1106,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.1+10.sha-89e16ed');
+    var VERSION = new core.Version('10.0.1+16.sha-7a91b23');
 
     /**
      * @license
