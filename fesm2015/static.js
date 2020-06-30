d0ae1b9dcb51c28f2cd9a7c674d8639da6f46064
Andrew Kushnir <akushnir@google.com>
2020-06-30 00:00:20 +0000

    57a518a36d perf(compiler-cli): fix memory leak in retained incremental state (#37835)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.1+20.sha-29b8318
+ * @license Angular v10.0.1+21.sha-57a518a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -847,7 +847,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.1+20.sha-29b8318');
+const VERSION = new Version('10.0.1+21.sha-57a518a');
 
 /**
  * @license
