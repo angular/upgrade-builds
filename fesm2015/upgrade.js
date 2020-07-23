f541afee36ed601c4be005a49f781ba85a15303e
Misko Hevery <misko@hevery.com>
2020-07-23 18:30:48 +0000

    dd09fb5348 build(docs-infra): upgrade cli command docs sources to b0b27361d (#38182)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+19.sha-ee22aa5
+ * @license Angular v10.1.0-next.2+18.sha-dd09fb5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+19.sha-ee22aa5');
+const VERSION = new Version('10.1.0-next.2+18.sha-dd09fb5');
 
 /**
  * @license
