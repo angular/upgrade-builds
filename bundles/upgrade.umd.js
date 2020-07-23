05a93d656c5627acc403f74317f581694b8db1e2
Misko Hevery <misko@hevery.com>
2020-07-23 18:31:37 +0000

    f411c9e5b9 build(docs-infra): simplify `ExampleZipper` by removing `PackageJsonCustomizer` (#38192)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+8.sha-a50a688
+ * @license Angular v10.0.5+15.sha-f411c9e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.5+8.sha-a50a688');
+    var VERSION = new core.Version('10.0.5+15.sha-f411c9e');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
