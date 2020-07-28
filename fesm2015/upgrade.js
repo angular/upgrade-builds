dbd58ab6ebba3c896384bdc8902f9b0874062e3e
Misko Hevery <misko@hevery.com>
2020-07-28 22:29:36 +0000

    cdda60a430 release: cut the v10.0.6 release

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+51.sha-7570356
+ * @license Angular v10.0.6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.5+51.sha-7570356');
+const VERSION = new Version('10.0.6');
 
 /**
  * @license
