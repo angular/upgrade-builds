191e7286d6b3d61492214ee822e8571e4263dc0c
Misko Hevery <misko@hevery.com>
2020-07-29 16:55:58 +0000

    0469d9240a Revert "refactor(platform-browser): specify return type of parseEventName (#38089)"

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.6
+ * @license Angular v10.0.6+1.sha-0469d92
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -847,7 +847,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.6');
+const VERSION = new Version('10.0.6+1.sha-0469d92');
 
 /**
  * @license
