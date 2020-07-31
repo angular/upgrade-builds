14248e37b3ae9b58091a7a7e0931362344064e2a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-31 23:01:13 +0000

    cea46786a2 fix(compiler): update unparsable character reference entity error messages (#38319)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.7+17.sha-9c5fc16
+ * @license Angular v10.0.7+18.sha-cea4678
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -847,7 +847,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.7+17.sha-9c5fc16');
+const VERSION = new Version('10.0.7+18.sha-cea4678');
 
 /**
  * @license
