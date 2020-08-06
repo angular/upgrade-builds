391855fcbf5c3c59ce28b0fad5fdf9f026456246
Andrew Kushnir <akushnir@google.com>
2020-08-06 19:09:57 +0000

    ad16e2f97a docs(service-worker): describe how asset-/data-group order affects request handling (#38364)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+14.sha-016a41b
+ * @license Angular v10.0.8+16.sha-ad16e2f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.8+14.sha-016a41b');
+const VERSION = new Version('10.0.8+16.sha-ad16e2f');
 
 /**
  * @license
