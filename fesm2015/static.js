d10033ce108d7b5d1172f3104274b0c8942e5052
Andrew Kushnir <akushnir@google.com>
2020-08-10 20:37:16 +0000

    e34c33cd46 fix(platform-server): remove styles added by ServerStylesHost on destruction (#38367)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+29.sha-6d8c73a
+ * @license Angular v10.1.0-next.4+30.sha-e34c33c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -847,7 +847,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+29.sha-6d8c73a');
+const VERSION = new Version('10.1.0-next.4+30.sha-e34c33c');
 
 /**
  * @license
