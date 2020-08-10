689913032145d97806d339eef30677dbd4403077
Andrew Kushnir <akushnir@google.com>
2020-08-10 22:56:21 +0000

    32109dc414 fix(core): queries not matching string injection tokens (#38321)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+28.sha-7f11149
+ * @license Angular v10.0.8+31.sha-32109dc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -847,7 +847,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.8+28.sha-7f11149');
+const VERSION = new Version('10.0.8+31.sha-32109dc');
 
 /**
  * @license
