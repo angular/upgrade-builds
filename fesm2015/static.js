d5075accb7369da8dfb3b4e1a51b8dbb27fa179e
Andrew Kushnir <akushnir@google.com>
2020-08-10 22:29:09 +0000

    250e299dc3 refactor(core): break `i18n.ts` into smaller files (#38368)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+31.sha-8f708b5
+ * @license Angular v10.1.0-next.4+32.sha-250e299
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -847,7 +847,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+31.sha-8f708b5');
+const VERSION = new Version('10.1.0-next.4+32.sha-250e299');
 
 /**
  * @license
