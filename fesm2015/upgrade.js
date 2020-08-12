8a2dc25637e8e520b5ef014e9ee928f57b66e8a2
Andrew Kushnir <akushnir@google.com>
2020-08-12 15:48:57 +0000

    2590a9b535 docs: delete one superfluous sentence (#38339)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+39.sha-f68a1d3
+ * @license Angular v10.0.8+40.sha-2590a9b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.8+39.sha-f68a1d3');
+const VERSION = new Version('10.0.8+40.sha-2590a9b');
 
 /**
  * @license
