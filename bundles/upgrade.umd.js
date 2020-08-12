08111d86455f07c6afb0e03275f254dcd7e6a2da
Andrew Scott <atscott01@gmail.com>
2020-08-12 18:54:08 +0000

    aa3520eb7d refactor(dev-infra): use promptConfirm util in ng-dev's formatter (#38419)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5
+ * @license Angular v10.1.0-next.5+2.sha-aa3520e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.5');
+    var VERSION = new i0.Version('10.1.0-next.5+2.sha-aa3520e');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
