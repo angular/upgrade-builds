586c70bb52b8b2643e8bc4bb6f37d5043bbf4cf1
Andrew Scott <atscott01@gmail.com>
2020-08-12 23:34:04 +0000

    afd4417a7b refactor(dev-infra): extract the commit message parsing function into its own file (#38429)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+3.sha-2f53bbb
+ * @license Angular v10.0.9+4.sha-afd4417
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.9+3.sha-2f53bbb');
+    var VERSION = new core.Version('10.0.9+4.sha-afd4417');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
