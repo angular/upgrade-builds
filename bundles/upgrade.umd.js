0083ccb5f4b805d60991d05158060b3e0a2f6875
Andrew Scott <atscott01@gmail.com>
2020-08-14 19:07:20 +0000

    b32126c335 fix(common): Allow scrolling when browser supports scrollTo (#38468)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+16.sha-d5e09f4
+ * @license Angular v10.0.9+17.sha-b32126c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.9+16.sha-d5e09f4');
+    var VERSION = new core.Version('10.0.9+17.sha-b32126c');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
