6a9eb6a9490ca896ff39b9a97828d57b3e38e690
atscott <atscott01@gmail.com>
2020-08-17 22:57:34 +0000

    e472f5f688 refactor(ngcc): update yargs and typings for yargs (#38470)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6
+ * @license Angular v10.1.0-next.6+3.sha-e472f5f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -847,7 +847,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.6');
+const VERSION = new Version('10.1.0-next.6+3.sha-e472f5f');
 
 /**
  * @license
