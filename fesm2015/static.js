b7b8a76d1cd83098d8158b6fbe074012b7a416ee
atscott <atscott01@gmail.com>
2020-08-17 20:45:37 +0000

    fec9dcbeb0 docs: release notes for the v10.0.10 release

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+28.sha-e0e5c9f
+ * @license Angular v10.1.0-next.5+29.sha-fec9dcb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -847,7 +847,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5+28.sha-e0e5c9f');
+const VERSION = new Version('10.1.0-next.5+29.sha-fec9dcb');
 
 /**
  * @license
