c26cc78c0713876e8a4608ab7280a6ecf3a94803
atscott <atscott01@gmail.com>
2020-08-19 16:39:51 +0000

    a3f3082bf0 release: cut the v10.0.11 release

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.10+9.sha-0570c24
+ * @license Angular v10.0.11
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -847,7 +847,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.10+9.sha-0570c24');
+const VERSION = new Version('10.0.11');
 
 /**
  * @license
