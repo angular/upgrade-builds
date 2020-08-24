1576d17ef48d43e25c488d089dc8cc71f41a37aa
Misko Hevery <misko@hevery.com>
2020-08-24 22:55:04 +0000

    81db6a3171 release: cut the v10.0.12 release

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.11+16.sha-8224e65
+ * @license Angular v10.0.12
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -847,7 +847,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.11+16.sha-8224e65');
+const VERSION = new Version('10.0.12');
 
 /**
  * @license
