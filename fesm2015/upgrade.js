846df12ec452ee0ca6591651ccd9ec51937a2b0f
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:10:03 +0000

    4e8592e610 feat(dev-infra): write outputs of command runs to ng-dev log file (#38599)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.14+13.sha-cb6bf40
+ * @license Angular v10.0.14+19.sha-4e8592e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.14+13.sha-cb6bf40');
+const VERSION = new Version('10.0.14+19.sha-4e8592e');
 
 /**
  * @license
