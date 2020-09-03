c842152ed62092b90ffedb10d1e822ddae38cb69
atscott <atscott01@gmail.com>
2020-09-03 00:11:46 +0000

    fdea1804d6 fix(core): remove CollectionChangeRecord symbol (#38668)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+2.sha-5da1934
+ * @license Angular v11.0.0-next.0+6.sha-fdea180
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.0+2.sha-5da1934');
+const VERSION = new Version('11.0.0-next.0+6.sha-fdea180');
 
 /**
  * @license
