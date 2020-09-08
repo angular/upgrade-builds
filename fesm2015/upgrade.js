36c729154abebdd31ed83381c080962b50851802
atscott <atscott01@gmail.com>
2020-09-08 17:31:47 +0000

    d9da7e5a18 docs: fix result of sanitization example (#38724)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+24.sha-97475d7
+ * @license Angular v11.0.0-next.0+19.sha-d9da7e5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.0+24.sha-97475d7');
+const VERSION = new Version('11.0.0-next.0+19.sha-d9da7e5');
 
 /**
  * @license
