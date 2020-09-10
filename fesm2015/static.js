bed5daff1d55a9c26a353d1d18c61260ab74452b
Andrew Kushnir <akushnir@google.com>
2020-09-10 18:22:38 +0000

    a47383d1e8 fix(localize): ensure that `formatOptions` is optional (#38787)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+11.sha-9078187
+ * @license Angular v10.1.1+12.sha-a47383d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -852,7 +852,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.1+11.sha-9078187');
+const VERSION = new Version('10.1.1+12.sha-a47383d');
 
 /**
  * @license
