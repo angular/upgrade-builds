57a490990bb7047eba7269dbbb1884a7b67b1273
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:56:11 +0000

    06525cfed3 test(compiler-cli): fix tests to have at least one component (#39011)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+77.sha-3ba97ab
+ * @license Angular v11.0.0-next.3+72.sha-06525cf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+77.sha-3ba97ab');
+const VERSION = new Version('11.0.0-next.3+72.sha-06525cf');
 
 /**
  * @license
