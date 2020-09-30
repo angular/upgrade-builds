80080b23d7e41c2e5d1147a6e5100aeb3db689a9
Joey Perrott <josephperrott@gmail.com>
2020-09-30 20:11:21 +0000

    eec9f4a84e test(language-service): add test for multiple bound attributes in microsyntax (#39062)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4
+ * @license Angular v11.0.0-next.4+3.sha-eec9f4a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.4');
+    var VERSION = new core.Version('11.0.0-next.4+3.sha-eec9f4a');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
