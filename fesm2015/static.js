1136116bad538897d55541739e2f7703403718c7
Joey Perrott <josephperrott@gmail.com>
2020-10-01 16:58:30 +0000

    50e83e2566 refactor(docs-infra): leave TODO comments for aligning tslint with CLI (#39018)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+7.sha-bb11fd9
+ * @license Angular v10.1.4+14.sha-50e83e2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -852,7 +852,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.4+7.sha-bb11fd9');
+const VERSION = new Version('10.1.4+14.sha-50e83e2');
 
 /**
  * @license
