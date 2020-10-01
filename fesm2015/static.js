a87bcf46800ef83c0e1797f9b2b5fd6f483cd464
Joey Perrott <josephperrott@gmail.com>
2020-10-01 19:55:06 +0000

    bb19e61848 feat(dev-infra): add a command to verify NgBot YAML config syntax (#39071)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+14.sha-50e83e2
+ * @license Angular v10.1.4+15.sha-bb19e61
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -852,7 +852,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.4+14.sha-50e83e2');
+const VERSION = new Version('10.1.4+15.sha-bb19e61');
 
 /**
  * @license
