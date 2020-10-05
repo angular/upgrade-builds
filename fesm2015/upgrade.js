e025e87fb7a912ac02d98b462ad4e6dbb0e5085c
Joey Perrott <josephperrott@gmail.com>
2020-10-05 15:29:57 +0000

    4beaddcb83 docs(upgrade): expand upon change detection implications for downgraded components (#39100)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+36.sha-89c5255
+ * @license Angular v11.0.0-next.4+37.sha-4beaddc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+36.sha-89c5255');
+const VERSION = new Version('11.0.0-next.4+37.sha-4beaddc');
 
 /**
  * @license
