d2549a68f0aa650b249739bb15e21d71084defd7
atscott <atscott01@gmail.com>
2020-10-09 16:00:42 +0000

    52a0c6b36e fix(compiler): incorrectly encapsulating @import containing colons and semicolons (#38716)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+7.sha-8913aee
+ * @license Angular v10.1.5+8.sha-52a0c6b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.5+7.sha-8913aee');
+const VERSION = new Version('10.1.5+8.sha-52a0c6b');
 
 /**
  * @license
