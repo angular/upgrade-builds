d41bd68dfbb3feed28b09e239fd9d5c994de6c91
atscott <atscott01@gmail.com>
2020-10-09 15:23:38 +0000

    8913aee527 build: bump Chromium to next stable version: 84.0.4147 (#39179)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+6.sha-7628c36
+ * @license Angular v10.1.5+7.sha-8913aee
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -852,7 +852,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.5+6.sha-7628c36');
+const VERSION = new Version('10.1.5+7.sha-8913aee');
 
 /**
  * @license
