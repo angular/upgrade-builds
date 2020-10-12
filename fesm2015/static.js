cfece548bb53b196ef73e191ba587b60b2616ce4
atscott <atscott01@gmail.com>
2020-10-12 16:05:49 +0000

    2d37e47e95 build(docs-infra): support passing args to `ng serve` via `serve-and-sync` (#39201)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+10.sha-952fd86
+ * @license Angular v10.1.5+12.sha-2d37e47
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -852,7 +852,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.5+10.sha-952fd86');
+const VERSION = new Version('10.1.5+12.sha-2d37e47');
 
 /**
  * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+10.sha-952fd86
+ * @license Angular v10.1.5+12.sha-2d37e47
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.5+10.sha-952fd86');
+const VERSION = new Version('10.1.5+12.sha-2d37e47');
 
 /**
  * @license
