b9c746e54c051e6f852e593751c6136aad655924
atscott <atscott01@gmail.com>
2020-10-13 22:22:43 +0000

    584f37cfca updated sajee's profile (#39019)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+53.sha-b642f0b
+ * @license Angular v11.0.0-next.5+55.sha-584f37c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.5+53.sha-b642f0b');
+const VERSION = new Version('11.0.0-next.5+55.sha-584f37c');
 
 /**
  * @license
