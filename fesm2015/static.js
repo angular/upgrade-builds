db112d4ef3cce66795d68bf3ea321a32b776c31f
atscott <atscott01@gmail.com>
2020-10-13 19:23:10 +0000

    0875fd2360 feat(core): create internal Trusted Types module (#39207)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+49.sha-5ce71e0
+ * @license Angular v11.0.0-next.5+51.sha-0875fd2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -852,7 +852,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.5+49.sha-5ce71e0');
+const VERSION = new Version('11.0.0-next.5+51.sha-0875fd2');
 
 /**
  * @license
