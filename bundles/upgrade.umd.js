65c4a2a4e19447174744d63545a65b95f62252b4
atscott <atscott01@gmail.com>
2020-10-14 21:35:14 +0000

    aee2d3f994 docs: fix broken link (#39256)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6
+ * @license Angular v11.0.0-next.6+7.sha-aee2d3f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6');
+    var VERSION = new i0.Version('11.0.0-next.6+7.sha-aee2d3f');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
