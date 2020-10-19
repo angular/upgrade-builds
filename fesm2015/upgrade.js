6fa7d3d94c295f257280bb2816ae6716d8f8c9a3
Andrew Kushnir <akushnir@google.com>
2020-10-19 21:56:16 +0000

    02b87505ee docs: clarify grammatical error (#39279)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+26.sha-32a8e72
+ * @license Angular v10.1.6+27.sha-02b8750
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.6+26.sha-32a8e72');
+const VERSION = new Version('10.1.6+27.sha-02b8750');
 
 /**
  * @license
