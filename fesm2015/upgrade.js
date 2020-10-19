901cb92f8146caa4769a8303794a6770a6ed848a
Andrew Kushnir <akushnir@google.com>
2020-10-19 16:19:13 +0000

    38db976617 docs: add Johannes Hoppe to GDE resources (#34694)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+22.sha-10db995
+ * @license Angular v10.1.6+24.sha-38db976
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.6+22.sha-10db995');
+const VERSION = new Version('10.1.6+24.sha-38db976');
 
 /**
  * @license
