150c6b7df425fbb9a721b971afd0d10c82d6f4e8
Andrew Kushnir <akushnir@google.com>
2020-10-20 23:54:37 +0000

    ac53101fc6 build: upgrade karma to version 4.4.0 (#39180)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+49.sha-161b278
+ * @license Angular v11.0.0-next.6+50.sha-ac53101
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+49.sha-161b278');
+const VERSION = new Version('11.0.0-next.6+50.sha-ac53101');
 
 /**
  * @license
