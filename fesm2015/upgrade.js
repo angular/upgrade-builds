701c0670d8b4bbe0d5e3fe5355e7ffd6a2ae3afd
Joey Perrott <josephperrott@gmail.com>
2020-10-28 22:04:52 +0000

    6b3db15080 docs: move template ref vars doc to concepts section clarify, add scope section, and update headers (#31195)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1
+ * @license Angular v10.2.1+2.sha-6b3db15
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('10.2.1');
+const VERSION = new Version('10.2.1+2.sha-6b3db15');
 
 /**
  * @license
