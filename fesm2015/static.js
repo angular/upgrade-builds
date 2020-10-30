595e10c50115ab4604e46199cdc494485fabbabb
Joey Perrott <josephperrott@gmail.com>
2020-10-30 18:15:13 +0000

    a8e0db73ba fix(compiler): preserve this.$event and this.$any accesses in expressions (#39323)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+16.sha-3091534
+ * @license Angular v11.0.0-rc.1+18.sha-a8e0db7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -852,7 +852,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-rc.1+16.sha-3091534');
+const VERSION = new Version('11.0.0-rc.1+18.sha-a8e0db7');
 
 /**
  * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+16.sha-3091534
+ * @license Angular v11.0.0-rc.1+18.sha-a8e0db7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-rc.1+16.sha-3091534');
+const VERSION = new Version('11.0.0-rc.1+18.sha-a8e0db7');
 
 /**
  * @license
