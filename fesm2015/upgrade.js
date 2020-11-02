ddd0454df2d52b023aad50bc7b2b43315da1891d
Joey Perrott <josephperrott@gmail.com>
2020-11-02 21:01:12 +0000

    a0b6548a94 build(docs-infra): ensure that deployment works on CI (#39535)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+33.sha-d386fb3
+ * @license Angular v11.0.0-rc.1+35.sha-a0b6548
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-rc.1+33.sha-d386fb3');
+const VERSION = new Version('11.0.0-rc.1+35.sha-a0b6548');
 
 /**
  * @license
