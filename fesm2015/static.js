057b603800da7187f46aea66309d70a27e65367d
Misko Hevery <misko@hevery.com>
2020-11-06 18:00:04 +0000

    290ea57a93 fix(core): Access injected parent values using SelfSkip (#39464)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+217.sha-b9b9178
+ * @license Angular v11.0.0-next.6+218.sha-290ea57
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -852,7 +852,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+217.sha-b9b9178');
+const VERSION = new Version('11.0.0-next.6+218.sha-290ea57');
 
 /**
  * @license
