74ffd8e23c14bae9fa53bd6b38e3e6c463d4aa5d
Misko Hevery <misko@hevery.com>
2020-11-06 23:59:45 +0000

    c68ca49d0b fix(compiler-cli): avoid duplicate diagnostics about unknown pipes (#39517)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.2+14.sha-dd28855
+ * @license Angular v11.0.0-rc.2+15.sha-c68ca49
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -852,7 +852,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-rc.2+14.sha-dd28855');
+const VERSION = new Version('11.0.0-rc.2+15.sha-c68ca49');
 
 /**
  * @license
