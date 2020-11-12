0f9e5968aedafea008b55ced7c044d52adfe98dc
atscott <atscott01@gmail.com>
2020-11-12 22:25:20 +0000

    8e17dc0278 fix(core): remove deprecated wtfZoneSpec from NgZone (#37864)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+244.sha-c8ed497
+ * @license Angular v11.0.0-next.6+245.sha-8e17dc0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18,7 +18,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+244.sha-c8ed497');
+const VERSION = new Version('11.0.0-next.6+245.sha-8e17dc0');
 
 /**
  * @license
