7dbcc3803b90f22115e0c7612349388a9a924714
atscott <atscott01@gmail.com>
2020-11-12 23:45:05 +0000

    8a1c98c5e8 refactor(compiler-cli): add keySpan to reference nodes (#39616)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+249.sha-c33326c
+ * @license Angular v11.0.0-next.6+250.sha-8a1c98c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18,7 +18,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+249.sha-c33326c');
+const VERSION = new Version('11.0.0-next.6+250.sha-8a1c98c');
 
 /**
  * @license
