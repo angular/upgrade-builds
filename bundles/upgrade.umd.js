e63e35f1d999ecd8b8fe3c2781051a5db73aee48
atscott <atscott01@gmail.com>
2020-11-18 19:42:52 +0000

    cc509d609f build: update to husky@5 (#39739)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+1.sha-3a344d4
+ * @license Angular v11.1.0-next.0+3.sha-cc509d6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.0+1.sha-3a344d4');
+    var VERSION = new i0.Version('11.1.0-next.0+3.sha-cc509d6');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
