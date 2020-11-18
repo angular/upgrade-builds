14b90a8898646a751265b0844db292e7f1e5c69d
atscott <atscott01@gmail.com>
2020-11-18 20:35:26 +0000

    26533a0954 docs: fixes on paragraph (#39664)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.1
+ * @license Angular v11.0.1+3.sha-26533a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.1');
+const VERSION = new Version('11.0.1+3.sha-26533a0');
 
 /**
  * @license
