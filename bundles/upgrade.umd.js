bd366d6dde7ad089327e827c6c72e4e813f5fa57
Andrew Kushnir <akushnir@google.com>
2020-11-19 17:23:28 +0000

    61506404a9 docs: move di-in-action doc to conceptual ref section (#39544)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+15.sha-1951342
+ * @license Angular v11.1.0-next.0+16.sha-6150640
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.0+15.sha-1951342');
+    var VERSION = new core.Version('11.1.0-next.0+16.sha-6150640');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
