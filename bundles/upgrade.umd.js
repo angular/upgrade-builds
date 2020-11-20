d0f61122f9a928bec481c8c0939db3d82660f848
Andrew Kushnir <akushnir@google.com>
2020-11-20 17:48:15 +0000

    156f9f30ae docs: edit di providers doc (#39403)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+43.sha-c33a823
+ * @license Angular v11.1.0-next.0+45.sha-156f9f3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.0+43.sha-c33a823');
+    var VERSION = new i0.Version('11.1.0-next.0+45.sha-156f9f3');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
