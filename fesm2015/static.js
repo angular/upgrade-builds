8e47b9e6fd248b2dabaa7e0fb5a8126e1dcd04cb
Andrew Kushnir <akushnir@google.com>
2020-11-20 16:54:07 +0000

    fe1ada86e8 docs: add missing return to example storage.service.ts (#31434)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+35.sha-17d5266
+ * @license Angular v11.1.0-next.0+36.sha-fe1ada8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -852,7 +852,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+35.sha-17d5266');
+const VERSION = new Version('11.1.0-next.0+36.sha-fe1ada8');
 
 /**
  * @license
