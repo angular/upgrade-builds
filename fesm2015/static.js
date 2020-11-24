26c3757856575c542443666e72047484bce00bf9
Andrew Kushnir <akushnir@google.com>
2020-11-24 21:33:24 +0000

    75b53690f6 docs: explain foreign function/value constraints in Ivy compatibility examples (#36092)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+49.sha-9cb12ca
+ * @license Angular v11.0.2+51.sha-75b5369
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -852,7 +852,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.2+49.sha-9cb12ca');
+const VERSION = new Version('11.0.2+51.sha-75b5369');
 
 /**
  * @license
