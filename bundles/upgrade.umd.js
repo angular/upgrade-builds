215293e48091766165bdbc1b44b0956da7f8eacf
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 20:56:21 +0000

    4cfa2098d0 docs(docs-infra): display angulararchitects.io in trainings (#37139)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+59.sha-2bfa8ee
+ * @license Angular v11.0.2+61.sha-4cfa209
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.2+59.sha-2bfa8ee');
+    var VERSION = new core.Version('11.0.2+61.sha-4cfa209');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
