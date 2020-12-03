2d1586b65fbf01d733e5f3f0adc4a9010e3836e5
Misko Hevery <misko@hevery.com>
2020-12-03 22:10:03 +0000

    f01c713ee2 ci: fix AIO deployment to multiple Firebase sites (#39948)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+37.sha-3b70098
+ * @license Angular v11.1.0-next.1+34.sha-f01c713
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+37.sha-3b70098');
+const VERSION = new Version('11.1.0-next.1+34.sha-f01c713');
 
 /**
  * @license
