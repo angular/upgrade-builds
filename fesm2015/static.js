efcbf2111c5c57f23287ab0f8d9403168fcf934e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:58:04 +0000

    b08fd0c18c docs: remove unused CSS rules (#40069)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+23.sha-91fb830
+ * @license Angular v11.1.0-next.2+24.sha-b08fd0c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.2+23.sha-91fb830');
+const VERSION = new Version('11.1.0-next.2+24.sha-b08fd0c');
 
 /**
  * @license
