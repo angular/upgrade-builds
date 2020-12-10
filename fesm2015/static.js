7448075c7e2497b5c10c83d70e2084b39565aea2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:57:50 +0000

    91fb830c86 docs: remove prefixed border-radius related properties (#40066)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+21.sha-79a42d9
+ * @license Angular v11.1.0-next.2+23.sha-91fb830
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.2+21.sha-79a42d9');
+const VERSION = new Version('11.1.0-next.2+23.sha-91fb830');
 
 /**
  * @license
