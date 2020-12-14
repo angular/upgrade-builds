46432e4f205ad01e29e6df5e2cb1e4177c1975b5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 20:16:08 +0000

    7d9ac37ce0 docs: fix typo (#40106)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.4+15.sha-e90a7f7
+ * @license Angular v11.0.4+22.sha-7d9ac37
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.4+15.sha-e90a7f7');
+const VERSION = new Version('11.0.4+22.sha-7d9ac37');
 
 /**
  * @license
