c0553f8455fc481573a6c03e06419ea6646a8545
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-15 22:02:48 +0000

    245dccc478 build: update config flags for snapshot builds (#40095)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+50.sha-dcb3d17
+ * @license Angular v11.1.0-next.2+52.sha-245dccc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.2+50.sha-dcb3d17');
+    var VERSION = new core.Version('11.1.0-next.2+52.sha-245dccc');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
