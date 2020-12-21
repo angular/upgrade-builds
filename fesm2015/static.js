b8c35f2e0c5e360c1880e8fab7e55a7e0577657c
Joey Perrott <josephperrott@gmail.com>
2020-12-21 18:46:34 +0000

    f7642ceff3 docs: update App Shell guide to reflect its actual behavior (#40210) PR Close #40210

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+7.sha-10a468a
+ * @license Angular v11.1.0-next.3+9.sha-f7642ce
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.3+7.sha-10a468a');
+const VERSION = new Version('11.1.0-next.3+9.sha-f7642ce');
 
 /**
  * @license
