77458af19acaac24c8d36847d21d40ddf44f168c
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:21:22 +0000

    c1d7805acc docs(core): Fix spelling (#40296) PR Close #40296

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+50.sha-e43f7e2
+ * @license Angular v11.1.0-next.3+52.sha-c1d7805
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.3+50.sha-e43f7e2');
+    var VERSION = new i0.Version('11.1.0-next.3+52.sha-c1d7805');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
