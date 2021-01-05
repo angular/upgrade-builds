c6be00ceaf0adf1f3afeeb3e0cab68f544f2d670
Joey Perrott <josephperrott@gmail.com>
2021-01-05 20:54:56 +0000

    b473bc226c ci: remove autolabeling from pullapprove config (#40314)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+43.sha-a384961
+ * @license Angular v11.1.0-next.3+44.sha-b473bc2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.3+43.sha-a384961');
+const VERSION = new Version('11.1.0-next.3+44.sha-b473bc2');
 
 /**
  * @license
