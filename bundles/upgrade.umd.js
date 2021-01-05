f78d7b0ac92e8a625466a3599e1bb1f55f11bdbb
Joey Perrott <josephperrott@gmail.com>
2021-01-05 18:36:28 +0000

    805b4f936b feat(core): Add schematic to fix invalid `Route` configs (#40067)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+38.sha-d466db8
+ * @license Angular v11.1.0-next.3+40.sha-805b4f9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.3+38.sha-d466db8');
+    var VERSION = new core.Version('11.1.0-next.3+40.sha-805b4f9');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
