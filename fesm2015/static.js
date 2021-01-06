86c6e9d43047ab818e17b03e4fa57c96ed8b4fe3
atscott <atscott01@gmail.com>
2021-01-06 22:17:48 +0000

    48526ccdf1 docs: fix instructions for setting up `ProductDetailsComponent` in `start-routing.md` (#40197)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+56.sha-5b9c981
+ * @license Angular v11.0.6+5.sha-48526cc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.5+56.sha-5b9c981');
+const VERSION = new Version('11.0.6+5.sha-48526cc');
 
 /**
  * @license
