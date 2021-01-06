56f0ca5f689da931f8ea915cf2941ed451a00e5e
Joey Perrott <josephperrott@gmail.com>
2021-01-06 20:19:31 +0000

    b6a9717597 test(core): fix invalid symbol used in html sanitizier spec (#40325)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+51.sha-bad1717
+ * @license Angular v11.0.5+55.sha-b6a9717
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.5+51.sha-bad1717');
+const VERSION = new Version('11.0.5+55.sha-b6a9717');
 
 /**
  * @license
