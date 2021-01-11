1e0a5ee69d9c139a21ffdedc75ce1c1cc8076294
atscott <atscott01@gmail.com>
2021-01-11 19:36:58 +0000

    adf42daed6 fix(router): better ngZone checking for warning (#25839)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.8+6.sha-6438a29
+ * @license Angular v11.0.8+19.sha-adf42da
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.8+6.sha-6438a29');
+const VERSION = new Version('11.0.8+19.sha-adf42da');
 
 /**
  * @license
