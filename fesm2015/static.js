5aefe2b14f8e92d2d4919d56ec864c1927717e34
atscott <atscott01@gmail.com>
2021-01-11 18:21:01 +0000

    6438a29eb4 docs(router): fix minor grammatical issue (#40380)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.7+13.sha-c44dd84
+ * @license Angular v11.0.8+6.sha-6438a29
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.7+13.sha-c44dd84');
+const VERSION = new Version('11.0.8+6.sha-6438a29');
 
 /**
  * @license
