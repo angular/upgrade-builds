b2ded79ae92efd6326dadd297267cbc90d8f34f6
GitHub <noreply@github.com>
2021-01-13 22:29:17 +0000

    e6ac54282c docs: cherry-pick changelog from the 11.1.0-rc.0 release (#40421)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+84.sha-47b3b08
+ * @license Angular v11.1.0-next.4+85.sha-e6ac542
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+84.sha-47b3b08');
+const VERSION = new Version('11.1.0-next.4+85.sha-e6ac542');
 
 /**
  * @license
