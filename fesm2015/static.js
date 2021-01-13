bc57d43b6076d7e183b53155b6a1cdb6ae206f42
atscott <atscott01@gmail.com>
2021-01-13 22:33:27 +0000

    25892c1bcf docs: add links to TypeScript (#40410)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+85.sha-e6ac542
+ * @license Angular v11.1.0-next.4+86.sha-25892c1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+85.sha-e6ac542');
+const VERSION = new Version('11.1.0-next.4+86.sha-25892c1');
 
 /**
  * @license
