6407097c0e7372d6b68b27a019f56b530819c991
Andrew Kushnir <akushnir@google.com>
2021-01-15 20:22:14 +0000

    524415ef8a fix(compiler): correct the `KeySpan` for animation events and properties (#40347)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-rc.0+21.sha-d5f696c
+ * @license Angular v11.1.0-rc.0+28.sha-524415e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-rc.0+21.sha-d5f696c');
+const VERSION = new Version('11.1.0-rc.0+28.sha-524415e');
 
 /**
  * @license
