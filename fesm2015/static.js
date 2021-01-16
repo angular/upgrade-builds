83dac69b576051d78acf713d17d527a3d23dd9de
Andrew Kushnir <akushnir@google.com>
2021-01-16 01:10:47 +0000

    4e8198d60f feat(language-service): Implement `getRenameInfo` (#40439)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+114.sha-40e0bfd
+ * @license Angular v11.1.0-next.4+115.sha-4e8198d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+114.sha-40e0bfd');
+const VERSION = new Version('11.1.0-next.4+115.sha-4e8198d');
 
 /**
  * @license
