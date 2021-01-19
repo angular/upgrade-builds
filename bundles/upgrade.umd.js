599313d71963bae6045099500ed66e515b39cc12
Andrew Kushnir <akushnir@google.com>
2021-01-19 17:43:32 +0000

    f49447df20 fix(core): fix fakeAsync() error messages (#40442)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+115.sha-4e8198d
+ * @license Angular v11.1.0-next.4+116.sha-f49447d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+115.sha-4e8198d');
+    var VERSION = new i0.Version('11.1.0-next.4+116.sha-f49447d');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
