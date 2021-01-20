5622e78ae42ad5402600ade27aabc1173af02232
Andrew Kushnir <akushnir@google.com>
2021-01-20 17:26:43 +0000

    d684e5a8d3 docs: add embedded videos to error guides (#40453)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.9+26.sha-96e6f20
+ * @license Angular v11.0.9+27.sha-d684e5a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.9+26.sha-96e6f20');
+const VERSION = new Version('11.0.9+27.sha-d684e5a');
 
 /**
  * @license
