4f6da2fc8882f12956d9253fe4603ccf2e47c99f
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-22 22:42:41 +0000

    bb3b315eee fix(core): fix possible XSS attack in development through SSR (#40525)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+157.sha-c5599cc
+ * @license Angular v11.1.0-next.4+158.sha-bb3b315
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+157.sha-c5599cc');
+const VERSION = new Version('11.1.0-next.4+158.sha-bb3b315');
 
 /**
  * @license
