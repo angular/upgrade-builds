cf8d0eaaaa838080839a6114d4edffea4f09425d
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-23 01:11:31 +0000

    dfe4200c2e Revert "fix(compiler): support multiple `:host-context()` selectors (#40494)" (#40531)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+24.sha-3bf6a99
+ * @license Angular v11.1.0+26.sha-dfe4200
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0+24.sha-3bf6a99');
+const VERSION = new Version('11.1.0+26.sha-dfe4200');
 
 /**
  * @license
