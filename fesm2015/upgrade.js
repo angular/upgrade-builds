8b66084427d37bd00023764e78179903fc0d314c
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-27 23:46:59 +0000

    de124856f6 docs: update release notes for the 11.1.1 release (#40602)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+60.sha-63275ae
+ * @license Angular v11.1.1+1.sha-de12485
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0+60.sha-63275ae');
+const VERSION = new Version('11.1.1+1.sha-de12485');
 
 /**
  * @license
