ebce2a4dea0b3af7e5871d4e51de53604a772235
Misko Hevery <misko@hevery.com>
2021-02-02 20:26:11 +0000

    21994ee985 refactor(docs-infra): remove unused styles for `.heading` CSS class (#40427)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0-next.0+18.sha-caafac2
+ * @license Angular v11.2.0-next.0+30.sha-21994ee
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.0-next.0+18.sha-caafac2');
+const VERSION = new Version('11.2.0-next.0+30.sha-21994ee');
 
 /**
  * @license
