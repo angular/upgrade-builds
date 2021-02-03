135980fbf4db0a355e344a04e2f0ef0485203947
Misko Hevery <misko@hevery.com>
2021-02-03 23:28:38 +0000

    9517ae3f23 release: cut the v11.1.2 release

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.1+37.sha-0ee9ea8
+ * @license Angular v11.1.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.1+37.sha-0ee9ea8');
+    var VERSION = new core.Version('11.1.2');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
