262c27034df2fba44c9838bd87b41c9aad31c98b
Misko Hevery <misko@hevery.com>
2021-02-04 00:22:44 +0000

    836b1764eb docs: release notes for the v11.2.0-rc.0 release

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+239.sha-a9322b7
+ * @license Angular v11.1.0-next.4+240.sha-836b176
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18,7 +18,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+239.sha-a9322b7');
+const VERSION = new Version('11.1.0-next.4+240.sha-836b176');
 
 /**
  * @license
