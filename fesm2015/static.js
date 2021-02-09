061e45c1cef43df53b331080a2d25b272a28c4be
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 19:14:25 +0000

    4ce44eac33 fix(animations): properly track listeners for a removed element (#40712)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+284.sha-089a634
+ * @license Angular v11.1.0-next.4+302.sha-4ce44ea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+284.sha-089a634');
+const VERSION = new Version('11.1.0-next.4+302.sha-4ce44ea');
 
 /**
  * @license
