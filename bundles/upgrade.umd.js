0027f61b907c670fa6c1f79c1e26b2a4c2cde2cf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 19:19:33 +0000

    4e4fcacb6d fix(docs-infra): correctly handle entry-points with no public exports (#40737)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+304.sha-e3213f6
+ * @license Angular v11.1.0-next.4+305.sha-4e4fcac
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+304.sha-e3213f6');
+    var VERSION = new i0.Version('11.1.0-next.4+305.sha-4e4fcac');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
