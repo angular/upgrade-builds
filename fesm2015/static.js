204ff80150a9772c2bcb1e5edf7e1bc2c310c46e
Joey Perrott <josephperrott@gmail.com>
2021-02-10 23:11:10 +0000

    06ee07b15f docs: release notes for the v11.2.0 release (#40791)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+322.sha-ddff6b6
+ * @license Angular v11.1.0-next.4+325.sha-06ee07b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+322.sha-ddff6b6');
+const VERSION = new Version('11.1.0-next.4+325.sha-06ee07b');
 
 /**
  * @license
