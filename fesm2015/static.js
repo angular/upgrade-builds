f8880d5d3fe651c2fccb89f0edda38fcebc8d963
Joey Perrott <josephperrott@gmail.com>
2021-02-12 17:51:00 +0000

    ebbcf0cc04 docs: Update start-deployment.md (#40817)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0+20.sha-8225bb5
+ * @license Angular v11.2.0+21.sha-ebbcf0c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.0+20.sha-8225bb5');
+const VERSION = new Version('11.2.0+21.sha-ebbcf0c');
 
 /**
  * @license
