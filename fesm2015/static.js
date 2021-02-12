e50925bde243ebff694eee7d8a0d6a798662ae33
Joey Perrott <josephperrott@gmail.com>
2021-02-12 17:21:04 +0000

    8225bb5b57 refactor(compiler-cli): implement `ɵɵngDeclarePipe()` (#40803)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0+18.sha-9482a4d
+ * @license Angular v11.2.0+20.sha-8225bb5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.0+18.sha-9482a4d');
+const VERSION = new Version('11.2.0+20.sha-8225bb5');
 
 /**
  * @license
