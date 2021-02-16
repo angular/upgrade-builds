fb0884da0fb63eaeb9a4b984ac1d4c9538a8298e
Joey Perrott <josephperrott@gmail.com>
2021-02-16 19:59:58 +0000

    95ad452e54 fix(router): fix load interaction of navigation and preload strategies (#40389)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+44.sha-7fa8819
+ * @license Angular v12.0.0-next.0+66.sha-95ad452
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.0+44.sha-7fa8819');
+const VERSION = new Version('12.0.0-next.0+66.sha-95ad452');
 
 /**
  * @license
