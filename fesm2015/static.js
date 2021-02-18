b2ffa7d144f8e3c82faf0c9ddb311ef2c559811d
Zach Arend <zachzach@google.com>
2021-02-18 01:24:29 +0000

    f57cd19e0c refactor(localize): avoid computing source-maps in extraction unnecessarily (#40891)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+4.sha-1de999b
+ * @license Angular v12.0.0-next.1+6.sha-f57cd19
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.1+4.sha-1de999b');
+const VERSION = new Version('12.0.0-next.1+6.sha-f57cd19');
 
 /**
  * @license
