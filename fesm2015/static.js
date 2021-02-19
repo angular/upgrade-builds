e6dbf5fd3ef74e52034fd2dbd5dc09b70aad2cc1
atscott <atscott01@gmail.com>
2021-02-19 22:57:46 +0000

    ff86f919ab refactor(core): use RuntimeError to throw provider not found error (#40901)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.1+32.sha-3b884ed
+ * @license Angular v11.2.1+33.sha-ff86f91
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.1+32.sha-3b884ed');
+const VERSION = new Version('11.2.1+33.sha-ff86f91');
 
 /**
  * @license
