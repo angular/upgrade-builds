192aff23639bb21b8ed80caa381594bf4a63e38d
Andrew Scott <atscott@google.com>
2021-02-19 16:53:32 +0000

    4e84b2f3ac docs: edit attribute-directives.md, move best practice to styleguide (#39849)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.1+9.sha-170692c
+ * @license Angular v11.2.1+10.sha-4e84b2f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.1+9.sha-170692c');
+const VERSION = new Version('11.2.1+10.sha-4e84b2f');
 
 /**
  * @license
