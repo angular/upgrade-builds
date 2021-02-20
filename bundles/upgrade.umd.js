7bd04a5e7ba64a4fd7d59f86dfb4acc15af2b37b
JoostK <joost.koehoorn@gmail.com>
2021-02-20 22:17:52 +0000

    44ffa8cba7 make it compile, cleanup and added some comments

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+37.sha-1646f8d
+ * @license Angular v12.0.0-next.1+38.sha-44ffa8c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.0+37.sha-1646f8d');
+    var VERSION = new core.Version('12.0.0-next.1+38.sha-44ffa8c');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
