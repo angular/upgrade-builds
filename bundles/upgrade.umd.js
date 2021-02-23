1b00ed96ac9928cfddeab7155dca7dec3c853ded
atscott <atscott01@gmail.com>
2021-02-23 19:06:38 +0000

    ad38cbbe09 perf(language-service): Skip Angular analysis when quick info requested outside a template (#40956)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+53.sha-bd6f377
+ * @license Angular v12.0.0-next.1+49.sha-ad38cbb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.1+53.sha-bd6f377');
+    var VERSION = new core.Version('12.0.0-next.1+49.sha-ad38cbb');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
