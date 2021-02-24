1a2215f9bcb0ea8be2ac4ad9c1477286fa333968
GitHub <noreply@github.com>
2021-02-24 17:53:47 +0000

    d7133a8e4e release: cut the v11.2.3 release (#40981)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.2+25.sha-3fe759e
+ * @license Angular v11.2.2+26.sha-d7133a8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.2+25.sha-3fe759e');
+const VERSION = new Version('11.2.2+26.sha-d7133a8');
 
 /**
  * @license
