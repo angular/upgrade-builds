858450b228df8e9f37ea04822d3da98cca673a0e
Andrew Scott <atscott@google.com>
2021-02-24 16:17:16 +0000

    03de2f2b78 docs: fix ch 4 so it compiles in strict mode (#40970)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+59.sha-011a527
+ * @license Angular v12.0.0-next.1+60.sha-03de2f2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.1+59.sha-011a527');
+const VERSION = new Version('12.0.0-next.1+60.sha-03de2f2');
 
 /**
  * @license
