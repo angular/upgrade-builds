9fd06b1633d990e504b21115b8c99841b4df8324
atscott <atscott01@gmail.com>
2021-02-24 17:47:06 +0000

    d1535a1a77 test(language-service): remove circular dependency (#40966)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+64.sha-f93eae7
+ * @license Angular v12.0.0-next.1+74.sha-d1535a1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.1+64.sha-f93eae7');
+const VERSION = new Version('12.0.0-next.1+74.sha-d1535a1');
 
 /**
  * @license
