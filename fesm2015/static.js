0bd45dab3398737db37db62a5619a90d1839b7e9
Zach Arend <zachzach@google.com>
2021-02-26 16:45:23 +0000

    953e98e211 docs: Change grammatically in security guide (#40995)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+7.sha-6c05c80
+ * @license Angular v12.0.0-next.2+9.sha-953e98e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.2+7.sha-6c05c80');
+const VERSION = new Version('12.0.0-next.2+9.sha-953e98e');
 
 /**
  * @license
