e54758991c4007d4927594abd4694be83b03a58b
JoostK <joost.koehoorn@gmail.com>
2021-02-26 23:23:59 +0000

    8d159b0942 fixup! perf(compiler-cli): detect semantic changes and their effect on an incremental rebuild

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+16.sha-3df1582
+ * @license Angular v12.0.0-next.2+16.sha-8d159b0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.2+16.sha-3df1582');
+const VERSION = new Version('12.0.0-next.2+16.sha-8d159b0');
 
 /**
  * @license
