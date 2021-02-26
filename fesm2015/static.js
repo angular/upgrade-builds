623910b8ca62fdaadca38a09477353dc78cfee41
Zach Arend <zachzach@google.com>
2021-02-26 20:59:27 +0000

    91cdc11aa0 fix(common): allow number or boolean as http params (#40663)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+13.sha-383d226
+ * @license Angular v12.0.0-next.2+14.sha-91cdc11
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.2+13.sha-383d226');
+const VERSION = new Version('12.0.0-next.2+14.sha-91cdc11');
 
 /**
  * @license
