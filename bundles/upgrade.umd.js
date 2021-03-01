6a9a6afc8d989eec6e816e27f54ca9df9216f904
Zach Arend <zachzach@google.com>
2021-03-01 23:50:12 +0000

    54b088967a refactor(compiler): remove unreachable code (#40984)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+38.sha-a31da48
+ * @license Angular v12.0.0-next.2+39.sha-54b0889
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.2+38.sha-a31da48');
+    var VERSION = new core.Version('12.0.0-next.2+39.sha-54b0889');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
