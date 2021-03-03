013da73678977904923d509a49cca4ed6db092ee
Zach Arend <zachzach@google.com>
2021-03-03 23:13:30 +0000

    2028a77dc8 release: cut the v11.2.4 release

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.3+49.sha-9322e6a
+ * @license Angular v11.2.4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.3+49.sha-9322e6a');
+    var VERSION = new core.Version('11.2.4');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
