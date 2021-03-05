a644598093c54e038255fc8cead4124e55103ee2
Andrew Kushnir <akushnir@google.com>
2021-03-05 23:28:24 +0000

    c676ec1ce5 docs: fix some typos in angular.io examples (#41093)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+17.sha-e7453f1
+ * @license Angular v12.0.0-next.3+18.sha-c676ec1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.3+17.sha-e7453f1');
+    var VERSION = new i0.Version('12.0.0-next.3+18.sha-c676ec1');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
