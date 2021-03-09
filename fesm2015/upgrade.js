a527170c865f2744234ab9fc45aa935c077f8cb6
Andrew Kushnir <akushnir@google.com>
2021-03-09 19:02:53 +0000

    16f90cac4b perf(compiler-cli): ensure module resolution cache is reused for type-check program (#39693)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.4+50.sha-e7ce857
+ * @license Angular v11.2.4+51.sha-16f90ca
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.4+50.sha-e7ce857');
+const VERSION = new Version('11.2.4+51.sha-16f90ca');
 
 /**
  * @license
