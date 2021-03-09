5776502426d49898ef67d0fbd3523e143e0c4448
Andrew Kushnir <akushnir@google.com>
2021-03-09 21:44:37 +0000

    01c1677151 build: update the lock closed sha to the latest sha (#41140)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.4+52.sha-c4ea0ab
+ * @license Angular v11.2.4+53.sha-01c1677
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.4+52.sha-c4ea0ab');
+    var VERSION = new core.Version('11.2.4+53.sha-01c1677');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
