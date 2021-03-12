a01385f366760fadd4bd3b378d1263d58ef1c854
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-12 21:48:30 +0000

    b37296ae64 ci: remove `CHANGELOG.md` from the `public-api` group (#41135)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+9.sha-74aba13
+ * @license Angular v11.2.5+16.sha-b37296a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.5+9.sha-74aba13');
+    var VERSION = new core.Version('11.2.5+16.sha-b37296a');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
