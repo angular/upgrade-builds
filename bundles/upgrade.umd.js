02000deb8c8ecbf49e41d560e044ee6975906745
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-15 17:53:12 +0000

    b4484125d6 docs: document `help wanted` and `good first issue` labels (#41195)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+18.sha-5908eda
+ * @license Angular v11.2.5+19.sha-b448412
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.5+18.sha-5908eda');
+    var VERSION = new core.Version('11.2.5+19.sha-b448412');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
