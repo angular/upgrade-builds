eb5e62bb89bcbc413606beedbddac332e7e0e0a9
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-15 19:35:12 +0000

    cc75e1d17b docs: add contributors guide to aio (#41061)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+25.sha-3bd1992
+ * @license Angular v12.0.0-next.4+26.sha-cc75e1d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.4+25.sha-3bd1992');
+    var VERSION = new core.Version('12.0.0-next.4+26.sha-cc75e1d');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
