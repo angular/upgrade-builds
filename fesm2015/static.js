1b97910bf68bf44ac03bcfa7530a22b018b6a552
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-16 18:26:54 +0000

    6bdad5ef6c build(docs-infra): upgrade cli command docs sources to 46538eea1 (#41227)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+38.sha-e88a9c6
+ * @license Angular v12.0.0-next.4+40.sha-6bdad5e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.4+38.sha-e88a9c6');
+const VERSION = new Version('12.0.0-next.4+40.sha-6bdad5e');
 
 /**
  * @license
