1320129aeee40429ef96bbd07d036b1e0e4f515d
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-17 16:25:58 +0000

    0f0f34fb8a docs: minor update to what is Angular topic (#41123)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+46.sha-6bc22a7
+ * @license Angular v12.0.0-next.4+44.sha-0f0f34f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.4+46.sha-6bc22a7');
+const VERSION = new Version('12.0.0-next.4+44.sha-0f0f34f');
 
 /**
  * @license
