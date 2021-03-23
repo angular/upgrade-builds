d03dff80d00ff0dc8410476afc13e6a74ba83782
Joey Perrott <josephperrott@gmail.com>
2021-03-23 21:40:07 +0000

    d8d77ed00d refactor(dev-infra): use conventional-commits-parser for commit parsing (#41329)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.6+25.sha-a68f6b0
+ * @license Angular v11.2.6+26.sha-d8d77ed
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.6+25.sha-a68f6b0');
+    var VERSION = new core.Version('11.2.6+26.sha-d8d77ed');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
