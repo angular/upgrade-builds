9f51c8f976c5d6382e0460654be78f1c55c8a5e8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-24 21:03:46 +0000

    b61c009c54 build: emit performance JSON file for each ng_module() (#41125)

--- bundles/upgrade-static.umd.js
+++ bundles/upgrade-static.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+1.sha-dd82c8e
+ * @license Angular v12.0.0-next.6+3.sha-b61c009
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1235,7 +1235,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.6+1.sha-dd82c8e');
+    var VERSION = new core.Version('12.0.0-next.6+3.sha-b61c009');
 
     /**
      * @license
