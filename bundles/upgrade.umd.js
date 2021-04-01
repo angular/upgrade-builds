4444fbf67e6e29f0243beb8908084b57a5151c29
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 18:45:32 +0000

    18bc9ffb51 ci: do not try to publish directories that are not packages (#41393)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+3.sha-b61c009
+ * @license Angular v12.0.0-next.6+36.sha-18bc9ff
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.6+3.sha-b61c009');
+    var VERSION = new core.Version('12.0.0-next.6+36.sha-18bc9ff');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
