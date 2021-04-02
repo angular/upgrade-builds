03e6e867ac3a21bce48ad08f485e7a8e981f2893
atscott <atscott01@gmail.com>
2021-04-02 17:49:49 +0000

    dc8a0818e7 fix(dev-infra): require npm login for all publishes via wombat proxy (#41422)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.7+33.sha-154129b
+ * @license Angular v11.2.8+2.sha-dc8a081
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.7+33.sha-154129b');
+const VERSION = new Version('11.2.8+2.sha-dc8a081');
 
 /**
  * @license
