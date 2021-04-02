2567e7149e1119b337fbc52960301233654df631
atscott <atscott01@gmail.com>
2021-04-02 18:12:23 +0000

    d4f739fb70 docs: Including Visual Studio (#41414)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.8+2.sha-dc8a081
+ * @license Angular v11.2.8+3.sha-d4f739f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.8+2.sha-dc8a081');
+const VERSION = new Version('11.2.8+3.sha-d4f739f');
 
 /**
  * @license
