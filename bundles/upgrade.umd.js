eb622291f935e86cddd27b85747f2b6d2b6f1dec
atscott <atscott01@gmail.com>
2021-04-07 17:03:02 +0000

    f98f379dd8 fix(dev-infra): make scopes for `test` and `refactor` commit types optional (#41486)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+34.sha-dd66da1
+ * @license Angular v12.0.0-next.7+37.sha-f98f379
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.7+34.sha-dd66da1');
+    var VERSION = new core.Version('12.0.0-next.7+37.sha-f98f379');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
