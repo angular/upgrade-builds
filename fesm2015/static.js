0e12fc9579c4e2623a70ad4824ef73e943d94825
Zach Arend <zachzach@google.com>
2021-04-07 23:04:37 +0000

    646f4a1fe6 fix(router): Remove information about attached component when deactivating route (#41381)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.9+8.sha-41636e6
+ * @license Angular v11.2.9+12.sha-646f4a1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.9+8.sha-41636e6');
+const VERSION = new Version('11.2.9+12.sha-646f4a1');
 
 /**
  * @license
