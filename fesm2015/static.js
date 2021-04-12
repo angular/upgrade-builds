2bd0b34ccbee26bf92fa6cabf38fc9e4e9829b25
Zach Arend <zachzach@google.com>
2021-04-12 18:08:05 +0000

    c20db69f9f refactor(compiler-cli): introduce declaration function to declare class metadata (#41200)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+33.sha-6ba67c6
+ * @license Angular v12.0.0-next.8+36.sha-c20db69
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+33.sha-6ba67c6');
+const VERSION = new Version('12.0.0-next.8+36.sha-c20db69');
 
 /**
  * @license
