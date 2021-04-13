506acaac051541d1053ca28bc9028686c80471a4
Zach Arend <zachzach@google.com>
2021-04-13 20:29:24 +0000

    04235fa079 docs: replace Codelyzer recommendation with Angular-ESLint in accessibility guide (#41294)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.9+36.sha-4847c14
+ * @license Angular v11.2.9+37.sha-04235fa
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.9+36.sha-4847c14');
+const VERSION = new Version('11.2.9+37.sha-04235fa');
 
 /**
  * @license
