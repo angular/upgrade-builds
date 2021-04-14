8df3968613c10bf886b991a75bd645089126e44d
Zach Arend <zachzach@google.com>
2021-04-14 17:13:45 +0000

    beafa22de9 feat(upgrade): update supported range of node versions (#41544)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+78.sha-0bc539a
+ * @license Angular v12.0.0-next.8+95.sha-beafa22
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+78.sha-0bc539a');
+const VERSION = new Version('12.0.0-next.8+95.sha-beafa22');
 
 /**
  * @license
