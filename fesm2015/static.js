6371949ea18fa52ce3601044d3364f2836a5bc93
Zach Arend <zachzach@google.com>
2021-04-14 23:20:25 +0000

    dde81ba0cd perf(compiler): reduce amount of generated code for safe accesses and nullish coalescing (#41563)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+107.sha-b649273
+ * @license Angular v12.0.0-next.8+106.sha-dde81ba
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+107.sha-b649273');
+const VERSION = new Version('12.0.0-next.8+106.sha-dde81ba');
 
 /**
  * @license
