27657053569b27a384d1b4061def8d0c4bd3271f
Andrew Kushnir <akushnir@google.com>
2021-04-15 19:12:06 +0000

    f4376fc0d4 fix(router): Only retrieve stored route when reuse strategy indicates it should reattach (#30263)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+5.sha-c4b0753
+ * @license Angular v11.2.10+6.sha-f4376fc
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.10+5.sha-c4b0753');
+    var VERSION = new core.Version('11.2.10+6.sha-f4376fc');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+5.sha-c4b0753
+ * @license Angular v11.2.10+6.sha-f4376fc
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.10+5.sha-c4b0753');
+const VERSION = new Version('11.2.10+6.sha-f4376fc');
 
 /**
  * @license
