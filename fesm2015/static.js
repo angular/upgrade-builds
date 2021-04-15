98063d7d60617cb63ee13512f480d2993c7195bc
Andrew Kushnir <akushnir@google.com>
2021-04-15 19:00:41 +0000

    dd4c3dba3f refactor(dev-infra): move `getRepoBaseDir()` to `GitClient` (#41527)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+109.sha-0d55d45
+ * @license Angular v12.0.0-next.8+111.sha-dd4c3db
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+109.sha-0d55d45');
+const VERSION = new Version('12.0.0-next.8+111.sha-dd4c3db');
 
 /**
  * @license
