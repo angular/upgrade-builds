7fcc0d3546d3c7c3194b15a735aff80175d3cd34
Andrew Kushnir <akushnir@google.com>
2021-04-16 16:55:28 +0000

    62e3f3279d fix(compiler): non-literal inline templates incorrectly processed in partial compilation (#41583)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+130.sha-d3edc5c
+ * @license Angular v12.0.0-next.8+132.sha-62e3f32
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+130.sha-d3edc5c');
+const VERSION = new Version('12.0.0-next.8+132.sha-62e3f32');
 
 /**
  * @license
