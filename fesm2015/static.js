444d6888fae8df0231963a9871a23725db4e0247
Andrew Kushnir <akushnir@google.com>
2021-04-16 16:13:40 +0000

    5d73b33ba2 build: update angular packages (#41633)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+121.sha-391d08f
+ * @license Angular v12.0.0-next.8+123.sha-5d73b33
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+121.sha-391d08f');
+const VERSION = new Version('12.0.0-next.8+123.sha-5d73b33');
 
 /**
  * @license
