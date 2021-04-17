ad4de3ab7538d544bc458e625ac667f9326de2fc
GitHub <noreply@github.com>
2021-04-17 10:25:23 +0000

    aa35a1a082 ci: disable automatic updates of rollup

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+133.sha-d5b13ce
+ * @license Angular v12.0.0-next.8+139.sha-aa35a1a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+133.sha-d5b13ce');
+const VERSION = new Version('12.0.0-next.8+139.sha-aa35a1a');
 
 /**
  * @license
