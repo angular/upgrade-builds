ce0f35006e45ec9c3492ed4b7da89f2d0d7e4271
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:23:21 +0000

    6a153a2a6f build: update diff to version 5.0.0 (#41684)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+147.sha-1391323
+ * @license Angular v12.0.0-next.8+149.sha-6a153a2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+147.sha-1391323');
+const VERSION = new Version('12.0.0-next.8+149.sha-6a153a2');
 
 /**
  * @license
