46708c44e49eb5d870b819fa4b88344683886b0e
Andrew Kushnir <akushnir@google.com>
2021-04-19 21:05:26 +0000

    d58747de8a Revert "fix(zone.js): should continue to executue listeners when throw error (#41562)" (#41707)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+165.sha-e256d92
+ * @license Angular v12.0.0-next.8+167.sha-d58747d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+165.sha-e256d92');
+const VERSION = new Version('12.0.0-next.8+167.sha-d58747d');
 
 /**
  * @license
