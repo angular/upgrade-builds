a5e5cc66278431a4289628dcd6f1970a91841fed
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:15:31 +0000

    d0b1f99598 docs: fix broken links to CLI wiki in the "Component styles" guide (#41516)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+138.sha-de25cda
+ * @license Angular v12.0.0-next.8+140.sha-d0b1f99
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+138.sha-de25cda');
+const VERSION = new Version('12.0.0-next.8+140.sha-d0b1f99');
 
 /**
  * @license
