e7c919778a02729cad33cd9fc2fa1334b3a2a100
Andrew Kushnir <akushnir@google.com>
2021-04-19 23:18:59 +0000

    a76f07bd13 docs: documentation of descendants property of @ContentChildren (#35927)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+31.sha-3631934
+ * @license Angular v11.2.10+35.sha-a76f07b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.10+31.sha-3631934');
+const VERSION = new Version('11.2.10+35.sha-a76f07b');
 
 /**
  * @license
