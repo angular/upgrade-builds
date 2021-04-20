5027ede9871375d99fa553eced7f335faf8729f6
Andrew Kushnir <akushnir@google.com>
2021-04-20 18:56:50 +0000

    87b5026e48 docs: add margin to button boilerplate CSS for examples (#41413)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.9+86.sha-2bcbbda
+ * @license Angular v12.0.0-next.9+87.sha-87b5026
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.9+86.sha-2bcbbda');
+    var VERSION = new core.Version('12.0.0-next.9+87.sha-87b5026');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
