16041d808fd4fd384d8a29fa9a4a35265df0ba1c
Andrew Kushnir <akushnir@google.com>
2021-04-20 20:33:41 +0000

    8f3e5a5982 build(docs-infra): update docs examples to Angular v11.2.10 (#41689) (#41716)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+39.sha-20e0917
+ * @license Angular v11.2.10+41.sha-8f3e5a5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.10+39.sha-20e0917');
+const VERSION = new Version('11.2.10+41.sha-8f3e5a5');
 
 /**
  * @license
