844c83f20e48748e7f3828c26c74dd2a27c20e4d
Andrew Kushnir <akushnir@google.com>
2021-04-20 23:23:06 +0000

    c0eabb2e70 build: update io_bazel_rules_sass to version 1.32.11 (#41713)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+187.sha-a2b671e
+ * @license Angular v12.0.0-next.8+188.sha-c0eabb2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+187.sha-a2b671e');
+const VERSION = new Version('12.0.0-next.8+188.sha-c0eabb2');
 
 /**
  * @license
