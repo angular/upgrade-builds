3c5fcd918151e78780692ee7c43afaae94275629
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 18:02:19 +0000

    c3a512a9e9 fix(compiler-cli): match string indexed partial declarations (#41747)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+200.sha-6ca6302
+ * @license Angular v12.0.0-next.8+202.sha-c3a512a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+200.sha-6ca6302');
+const VERSION = new Version('12.0.0-next.8+202.sha-c3a512a');
 
 /**
  * @license
