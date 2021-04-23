13cc4f19a4d9e33d99eecb4c0782692ed8f18b89
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-23 18:35:59 +0000

    097fdffc91 build: use platform from shared dev-infra package for remote execution (#41767)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.0+18.sha-87dc851
+ * @license Angular v12.0.0-rc.0+22.sha-097fdff
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-rc.0+18.sha-87dc851');
+const VERSION = new Version('12.0.0-rc.0+22.sha-097fdff');
 
 /**
  * @license
