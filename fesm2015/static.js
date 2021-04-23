21aaf22e1c58aa2818007e080cfa77d9376c43de
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-23 19:13:45 +0000

    73105aa7d6 ci: remove trailing comma from renovate config (#41790)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+213.sha-9f88da2
+ * @license Angular v12.0.0-next.8+214.sha-73105aa
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+213.sha-9f88da2');
+const VERSION = new Version('12.0.0-next.8+214.sha-73105aa');
 
 /**
  * @license
