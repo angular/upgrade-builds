3afde3e7c502b3ba635068f7f826eafb94d24992
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 16:49:41 +0000

    6d9e3400e1 test(service-worker): replace `global || window` with `globalThis` (#41739)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+219.sha-6465b50
+ * @license Angular v12.0.0-next.8+223.sha-6d9e340
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+219.sha-6465b50');
+const VERSION = new Version('12.0.0-next.8+223.sha-6d9e340');
 
 /**
  * @license
