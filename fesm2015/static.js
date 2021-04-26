1a5b2c596e6245f4dbf5d86cf6949fb163ad3bc9
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 16:37:30 +0000

    01e546f116 docs: improve accessibility of architecture example (#41314)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+216.sha-3268847
+ * @license Angular v12.0.0-next.8+217.sha-01e546f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+216.sha-3268847');
+const VERSION = new Version('12.0.0-next.8+217.sha-01e546f');
 
 /**
  * @license
