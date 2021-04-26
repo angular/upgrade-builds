753230155d94c8bd30af4debb9d7e73a97848a33
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 16:52:19 +0000

    990067a0c6 perf(core): avoid storing LView in __ngContext__ (#41358)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.0+24.sha-e6ec22f
+ * @license Angular v12.0.0-rc.0+34.sha-990067a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-rc.0+24.sha-e6ec22f');
+const VERSION = new Version('12.0.0-rc.0+34.sha-990067a');
 
 /**
  * @license
