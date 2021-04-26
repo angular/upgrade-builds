a680743f7b7fbfdfaa58403e45dd0c047cbe7652
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 16:49:37 +0000

    6465b5056a build(docs-infra): fix e2e testing SystemJS-based docs examples (#41796)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+220.sha-bd986cb
+ * @license Angular v12.0.0-next.8+219.sha-6465b50
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+220.sha-bd986cb');
+const VERSION = new Version('12.0.0-next.8+219.sha-6465b50');
 
 /**
  * @license
