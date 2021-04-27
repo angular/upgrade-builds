76f2d86367d8f077e26c6bca1ad7e0833d504a83
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 18:56:18 +0000

    cd004d4a23 ci: add Zach Arend to pullapprove groups (#41839)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+256.sha-b1ae31c
+ * @license Angular v12.0.0-next.8+257.sha-cd004d4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+256.sha-b1ae31c');
+const VERSION = new Version('12.0.0-next.8+257.sha-cd004d4');
 
 /**
  * @license
