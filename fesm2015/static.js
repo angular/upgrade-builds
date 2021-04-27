0f5dd00ef0a6b0ab02832d7017360fe809b48012
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 17:42:30 +0000

    b1ae31c712 fix(dev-infra): add github token requirement for discover-new-conflicts (#41825)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+255.sha-3783976
+ * @license Angular v12.0.0-next.8+256.sha-b1ae31c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+255.sha-3783976');
+const VERSION = new Version('12.0.0-next.8+256.sha-b1ae31c');
 
 /**
  * @license
