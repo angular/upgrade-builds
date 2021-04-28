39f06d94b79e233a6076e530a66f49ad55769274
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 18:00:12 +0000

    336fe9124e Revert "ci: re-enable the `@angular/components` unit tests (#41816)" (#41863)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+36.sha-7219d1b
+ * @license Angular v11.2.11+38.sha-336fe91
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.11+36.sha-7219d1b');
+    var VERSION = new core.Version('11.2.11+38.sha-336fe91');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+36.sha-7219d1b
+ * @license Angular v11.2.11+38.sha-336fe91
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.11+36.sha-7219d1b');
+const VERSION = new Version('11.2.11+38.sha-336fe91');
 
 /**
  * @license
