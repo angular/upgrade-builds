3614f6c1ffc9e55cc7ef4a9e45dcfffa05131a0c
Misko Hevery <misko@hevery.com>
2021-04-29 17:45:11 +0000

    f3331a8350 build(docs-infra): upgrade cli command docs sources to ac6fc449e (#41878)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+284.sha-d80ad95
+ * @license Angular v12.0.0-next.8+288.sha-f3331a8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+284.sha-d80ad95');
+const VERSION = new Version('12.0.0-next.8+288.sha-f3331a8');
 
 /**
  * @license
