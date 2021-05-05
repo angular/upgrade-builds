1c488bcad6d86164d6a1af317fd6cfa354700fa4
Misko Hevery <misko@hevery.com>
2021-05-05 04:38:29 +0000

    19b12d7286 refactor(dev-infra): switch away from deprecated platform execution properties (#41941)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+329.sha-5947239
+ * @license Angular v12.0.0-next.8+331.sha-19b12d7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+329.sha-5947239');
+    var VERSION = new core.Version('12.0.0-next.8+331.sha-19b12d7');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
