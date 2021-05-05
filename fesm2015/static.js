fdfc9c64d4e6acfee30c78f192f31c5cc6eed681
Misko Hevery <misko@hevery.com>
2021-05-05 04:35:08 +0000

    378bb04d7b docs(common): Corrected the wrong formatting (#41945)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+327.sha-43cc5a1
+ * @license Angular v12.0.0-next.8+330.sha-378bb04
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+327.sha-43cc5a1');
+const VERSION = new Version('12.0.0-next.8+330.sha-378bb04');
 
 /**
  * @license
