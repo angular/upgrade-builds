94c6d269875c045067ba6743e31111142eea075e
Misko Hevery <misko@hevery.com>
2021-05-05 21:04:56 +0000

    fb38175a40 docs: remove ts-node and tslint form dev deps (#41950)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+333.sha-753fb06
+ * @license Angular v12.0.0-next.8+334.sha-fb38175
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+333.sha-753fb06');
+const VERSION = new Version('12.0.0-next.8+334.sha-fb38175');
 
 /**
  * @license
