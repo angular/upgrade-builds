a65cec600d8a1378f5e98cf65395ae5c215436ad
Misko Hevery <misko@hevery.com>
2021-05-05 04:35:34 +0000

    33c9579212 refactor(dev-infra): switch away from deprecated platform execution properties (#41941)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.1+44.sha-b5b09dc
+ * @license Angular v12.0.0-rc.1+58.sha-33c9579
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-rc.1+44.sha-b5b09dc');
+const VERSION = new Version('12.0.0-rc.1+58.sha-33c9579');
 
 /**
  * @license
