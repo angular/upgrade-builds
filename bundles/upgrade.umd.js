db6820878959f13dd975cc908fd6a06668997cdd
Misko Hevery <misko@hevery.com>
2021-05-05 21:10:26 +0000

    df2ff0497d docs: initial commit of content projection topic. (#41143)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.1+58.sha-33c9579
+ * @license Angular v12.0.0-rc.1+64.sha-df2ff04
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-rc.1+58.sha-33c9579');
+    var VERSION = new core.Version('12.0.0-rc.1+64.sha-df2ff04');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
