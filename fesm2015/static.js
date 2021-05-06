fa2d17803432c72bceee941dd2d1feab0c506e23
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 14:01:38 +0000

    35fbd4f411 build: update dependency fs-extra to v10 (#41937)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+344.sha-abcd4bb
+ * @license Angular v12.0.0-next.8+345.sha-35fbd4f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+344.sha-abcd4bb');
+const VERSION = new Version('12.0.0-next.8+345.sha-35fbd4f');
 
 /**
  * @license
