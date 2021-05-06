97b042b54b3d349cf634310145f77bca24e988ea
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 22:00:48 +0000

    83bb0effc2 docs: use relative path for the feature request process diagram (#41970)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.13+6.sha-e090a6d
+ * @license Angular v11.2.13+8.sha-83bb0ef
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.13+6.sha-e090a6d');
+    var VERSION = new core.Version('11.2.13+8.sha-83bb0ef');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
