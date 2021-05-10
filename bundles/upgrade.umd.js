be0467ceb61aaf102963cb59eb4e83360ac26abe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 14:57:42 +0000

    ddf2a6c975 build: update node version to stop aio from breaking while generating docs (#42009)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+386.sha-a529d4f
+ * @license Angular v12.0.0-next.8+387.sha-ddf2a6c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+386.sha-a529d4f');
+    var VERSION = new core.Version('12.0.0-next.8+387.sha-ddf2a6c');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
