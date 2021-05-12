5b680afe13261d74e421317c1874f444ea7cc929
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-12 21:28:09 +0000

    5f7a401fef release: cut the v11.2.14 release (#42070)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.13+24.sha-b075481
+ * @license Angular v11.2.13+29.sha-5f7a401
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.13+24.sha-b075481');
+const VERSION = new Version('11.2.13+29.sha-5f7a401');
 
 /**
  * @license
