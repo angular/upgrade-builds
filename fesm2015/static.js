f40898ff4bfff0dec71d4cea1bbb01f149bd151a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-13 18:26:47 +0000

    04f41554dc build(docs-infra): show a dist-tag when installing the CLI on non-stable versions (#41991) (#42041)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.14
+ * @license Angular v11.2.14+1.sha-04f4155
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.14');
+const VERSION = new Version('11.2.14+1.sha-04f4155');
 
 /**
  * @license
