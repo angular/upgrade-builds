dc4bfca29a546a9dcbf2cba52bc5f3e2e4238c42
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-13 20:36:00 +0000

    fdb3d3076e build: update angular (#42035)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+403.sha-45083bc
+ * @license Angular v12.0.0-next.8+409.sha-fdb3d30
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+403.sha-45083bc');
+const VERSION = new Version('12.0.0-next.8+409.sha-fdb3d30');
 
 /**
  * @license
