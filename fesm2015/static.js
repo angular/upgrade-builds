5ae4e013a6b31dd7ed3ba91fee76a191d0086363
Andrew Scott <atscott@google.com>
2021-05-18 18:39:43 +0000

    c58e15a4b0 ci: add pullapprove entries for devtools (#42152)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0+26.sha-6861911
+ * @license Angular v12.0.0+32.sha-c58e15a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0+26.sha-6861911');
+const VERSION = new Version('12.0.0+32.sha-c58e15a');
 
 /**
  * @license
