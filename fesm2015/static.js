85ade9cff776e1c99d4b971cca363ae279a13beb
Andrew Scott <atscott@google.com>
2021-05-18 23:00:03 +0000

    aa9a86cfd5 build: remove publishConfig entry from package.json entries (#42104)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0+34.sha-a20ef16
+ * @license Angular v12.0.0+37.sha-aa9a86c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0+34.sha-a20ef16');
+const VERSION = new Version('12.0.0+37.sha-aa9a86c');
 
 /**
  * @license
