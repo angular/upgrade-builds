348b0eb1ed7482bb92ddd2a94a47f4c8c4082cb0
Zach Arend <zachzach@google.com>
2021-05-20 17:16:28 +0000

    a01fe8d9b4 ci: add Teri to global approvers for docs changes list (#42156)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+3.sha-04ab5ed
+ * @license Angular v12.1.0-next.2+6.sha-a01fe8d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+3.sha-04ab5ed');
+const VERSION = new Version('12.1.0-next.2+6.sha-a01fe8d');
 
 /**
  * @license
