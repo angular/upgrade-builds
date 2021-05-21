81098fbb5924b5a34bb81860204acd451f365976
Joey Perrott <josephperrott@gmail.com>
2021-05-21 21:02:43 +0000

    30c461d862 ci: update to latest feature request triage bot commit (#42229)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+17.sha-03825cf
+ * @license Angular v12.1.0-next.2+18.sha-30c461d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+17.sha-03825cf');
+const VERSION = new Version('12.1.0-next.2+18.sha-30c461d');
 
 /**
  * @license
