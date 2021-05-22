436d40c7adca97ad50990d9f125718d3a935c101
Zach Arend <zachzach@google.com>
2021-05-22 08:29:23 +0000

    3293dd34ca docs: add missing `Product` interface to example (#42147)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+19.sha-4d56c10
+ * @license Angular v12.1.0-next.2+21.sha-3293dd3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.2+19.sha-4d56c10');
+    var VERSION = new i0.Version('12.1.0-next.2+21.sha-3293dd3');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
