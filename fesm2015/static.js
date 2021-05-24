adc782be6fe9ffdb0f5bccd306cda2a9e7842c1b
Zach Arend <zachzach@google.com>
2021-05-24 16:20:01 +0000

    4ac55ca676 fix(dev-infra): use template strings for release note templates (#42224)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+22.sha-d7a1da9
+ * @license Angular v12.1.0-next.2+24.sha-4ac55ca
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+22.sha-d7a1da9');
+const VERSION = new Version('12.1.0-next.2+24.sha-4ac55ca');
 
 /**
  * @license
