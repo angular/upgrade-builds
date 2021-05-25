68c1b71672438eb69ac27ae2aa17e64762d7deb8
Zach Arend <zachzach@google.com>
2021-05-25 22:33:30 +0000

    9f50495f28 docs: update date pipe api reference to indicate it is a pure pipe (#42320)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+77.sha-4a2cc3e
+ * @license Angular v12.1.0-next.2+73.sha-9f50495
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+77.sha-4a2cc3e');
+const VERSION = new Version('12.1.0-next.2+73.sha-9f50495');
 
 /**
  * @license
