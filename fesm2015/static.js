0b3565040a8e8e178f1dacb76dc994eaf323977d
Zach Arend <zachzach@google.com>
2021-05-26 15:24:44 +0000

    54b23aef42 docs: document shallow equality emit for ActivatedRoute (#42280)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.1+66.sha-c01da21
+ * @license Angular v12.0.1+83.sha-54b23ae
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.1+66.sha-c01da21');
+const VERSION = new Version('12.0.1+83.sha-54b23ae');
 
 /**
  * @license
