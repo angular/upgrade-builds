7da4bb777ff6fdde3e0b317eb7118b3951e20ca0
Andrew Kushnir <akushnir@google.com>
2021-05-26 23:08:10 +0000

    75a33e5fa9 docs: remove lightbox gif from Use Directives to Watch the DOM section. (#42369) This gif is out of date and causing reader confusion.

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.2+21.sha-765854e
+ * @license Angular v12.0.2+23.sha-75a33e5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.2+21.sha-765854e');
+const VERSION = new Version('12.0.2+23.sha-75a33e5');
 
 /**
  * @license
