6ea4472338453d7bbd196c5e8ab7f207151d64d4
Zach Arend <zachzach@google.com>
2021-05-26 20:24:41 +0000

    74ebdf6fdc docs: refactor `HeroDetailComponent` and unit test (#42349)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3
+ * @license Angular v12.1.0-next.3+1.sha-74ebdf6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3');
+const VERSION = new Version('12.1.0-next.3+1.sha-74ebdf6');
 
 /**
  * @license
