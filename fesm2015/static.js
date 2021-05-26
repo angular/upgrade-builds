ccc66bbef038ce877e888ae62bd7b65ac44b81c4
Zach Arend <zachzach@google.com>
2021-05-26 20:37:47 +0000

    12e79686dc docs: clarify providedIn options (#42355)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.2+2.sha-5a51884
+ * @license Angular v12.0.2+15.sha-12e7968
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.2+2.sha-5a51884');
+const VERSION = new Version('12.0.2+15.sha-12e7968');
 
 /**
  * @license
