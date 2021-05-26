57692674de79a2eefb31bf4fb9a9602bb7c058b1
Zach Arend <zachzach@google.com>
2021-05-26 20:34:47 +0000

    a787f78074 test: clean up internal testing utilities (#42177)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+12.sha-33e1a18
+ * @license Angular v12.1.0-next.3+11.sha-a787f78
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.3+12.sha-33e1a18');
+    var VERSION = new i0.Version('12.1.0-next.3+11.sha-a787f78');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
