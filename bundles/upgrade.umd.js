829e8f89291a3b658b1dfed6f7d1fdff51059e1d
Andrew Kushnir <akushnir@google.com>
2021-05-26 21:45:51 +0000

    765854ed8f build: allow node v15 and above for aio (#42367)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.2+15.sha-12e7968
+ * @license Angular v12.0.2+21.sha-765854e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.2+15.sha-12e7968');
+    var VERSION = new core.Version('12.0.2+21.sha-765854e');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.2+15.sha-12e7968
+ * @license Angular v12.0.2+21.sha-765854e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.2+15.sha-12e7968');
+const VERSION = new Version('12.0.2+21.sha-765854e');
 
 /**
  * @license
