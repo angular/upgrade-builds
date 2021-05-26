a1bb181d46548e832dabab2a7ce659d9de8bee0a
Andrew Kushnir <akushnir@google.com>
2021-05-26 21:43:56 +0000

    15726594a9 ci: update the github feature request process action and run on cron (#42337)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+17.sha-bcfe4af
+ * @license Angular v12.1.0-next.3+16.sha-1572659
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+17.sha-bcfe4af');
+const VERSION = new Version('12.1.0-next.3+16.sha-1572659');
 
 /**
  * @license
