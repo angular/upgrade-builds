6441febf106bf8cacd21ce97a45ff14ef085281a
Zach Arend <zachzach@google.com>
2021-05-26 18:55:47 +0000

    5a51884069 fix(dev-infra): build with stamping for release builds (#42354)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.1+84.sha-037b636
+ * @license Angular v12.0.2+2.sha-5a51884
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.1+84.sha-037b636');
+const VERSION = new Version('12.0.2+2.sha-5a51884');
 
 /**
  * @license
