56cf54bd03cb243037272f5cc8cdbc546b2395f9
Andrew Kushnir <akushnir@google.com>
2021-05-27 21:54:21 +0000

    d33fdea1e3 docs: clarify behavior of strict templates flags (#42392)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+39.sha-fa0144f
+ * @license Angular v12.1.0-next.3+41.sha-d33fdea
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+39.sha-fa0144f');
+const VERSION = new Version('12.1.0-next.3+41.sha-d33fdea');
 
 /**
  * @license
