bc4dd7129185b2b0af094a3d81a83decab338a34
Andrew Kushnir <akushnir@google.com>
2021-05-27 22:54:56 +0000

    3a959152f9 docs: Edited to remove jargon, fixed issue (#42026)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+41.sha-d33fdea
+ * @license Angular v12.1.0-next.3+44.sha-3a95915
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+41.sha-d33fdea');
+const VERSION = new Version('12.1.0-next.3+44.sha-3a95915');
 
 /**
  * @license
