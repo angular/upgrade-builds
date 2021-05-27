ad4334fd3df163288fe3c69bb7f82357376d60b0
Andrew Kushnir <akushnir@google.com>
2021-05-27 16:15:01 +0000

    e87dbafba1 docs: fix a typo (#42384)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+25.sha-38378ce
+ * @license Angular v12.1.0-next.3+26.sha-e87dbaf
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+25.sha-38378ce');
+const VERSION = new Version('12.1.0-next.3+26.sha-e87dbaf');
 
 /**
  * @license
