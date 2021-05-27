a6859f38b19b4826f9923d51588a42d101bf0c95
Andrew Kushnir <akushnir@google.com>
2021-05-27 16:19:13 +0000

    deb893095f build(docs-infra): remove unused `spy-directive.gif` image (#42388)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+26.sha-e87dbaf
+ * @license Angular v12.1.0-next.3+29.sha-deb8930
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+26.sha-e87dbaf');
+const VERSION = new Version('12.1.0-next.3+29.sha-deb8930');
 
 /**
  * @license
