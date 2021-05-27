7532bb08f22e00f13e0f9f0e3fd4bc73d3b0f4bc
Andrew Kushnir <akushnir@google.com>
2021-05-27 16:14:23 +0000

    38378ce50a docs: explain what a deploy url is and how it differs from a base href (#42363)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+24.sha-d74d130
+ * @license Angular v12.1.0-next.3+25.sha-38378ce
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+24.sha-d74d130');
+const VERSION = new Version('12.1.0-next.3+25.sha-38378ce');
 
 /**
  * @license
