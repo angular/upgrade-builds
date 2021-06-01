fe480b1d561bde05206a27e19f86ce42a5cc2357
Andrew Kushnir <akushnir@google.com>
2021-06-01 19:36:22 +0000

    e039075a28 fix(compiler-cli): better detect classes that are indirectly exported (#42207)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+60.sha-3e192bf
+ * @license Angular v12.1.0-next.3+62.sha-e039075
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+60.sha-3e192bf');
+const VERSION = new Version('12.1.0-next.3+62.sha-e039075');
 
 /**
  * @license
