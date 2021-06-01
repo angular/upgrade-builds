1e19b893796cef8a2909da8c14d269ce9e422f29
Andrew Kushnir <akushnir@google.com>
2021-06-01 17:32:03 +0000

    c54643f00e docs(forms): make the forms validation guide specify more clearly what happens when mixing HTML5 native validators with Angular forms. (#42377)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+49.sha-e798bfa
+ * @license Angular v12.1.0-next.3+53.sha-c54643f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+49.sha-e798bfa');
+const VERSION = new Version('12.1.0-next.3+53.sha-c54643f');
 
 /**
  * @license
