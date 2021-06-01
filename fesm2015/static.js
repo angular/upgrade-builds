24f2f15f8c48e9dc71c40b2c9c8b09f99ab55282
Andrew Kushnir <akushnir@google.com>
2021-06-01 17:33:32 +0000

    1b8bbddff5 docs: list supported values for `read` in decorator metadata (#42370)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+53.sha-c54643f
+ * @license Angular v12.1.0-next.3+50.sha-1b8bbdd
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+53.sha-c54643f');
+const VERSION = new Version('12.1.0-next.3+50.sha-1b8bbdd');
 
 /**
  * @license
