5c12d388e880bf191978e982619d4520d7b8e3cd
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-04 22:41:53 +0000

    30059173f4 docs: add node.js version info into the universal guide (#42375)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+46.sha-4fc24ad
+ * @license Angular v12.1.0-next.4+47.sha-3005917
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+46.sha-4fc24ad');
+const VERSION = new Version('12.1.0-next.4+47.sha-3005917');
 
 /**
  * @license
