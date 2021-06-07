a8ee66475a23035c29eb87cf7c7db734de7f97d1
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 16:52:00 +0000

    cd22c7a1f5 docs: Updated the code snippet path (#42494) PR Close #42494

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+47.sha-3005917
+ * @license Angular v12.1.0-next.4+48.sha-cd22c7a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+47.sha-3005917');
+const VERSION = new Version('12.1.0-next.4+48.sha-cd22c7a');
 
 /**
  * @license
