1041bb69326578a4b545e7b57887ae5f074a5bd5
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:03:05 +0000

    5c0541f351 docs: fix issue 24571 in ngOnChanges (#42517)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+82.sha-386550c
+ * @license Angular v12.1.0-next.4+84.sha-5c0541f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+82.sha-386550c');
+const VERSION = new Version('12.1.0-next.4+84.sha-5c0541f');
 
 /**
  * @license
