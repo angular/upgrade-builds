f393942e8e555649947754886f08531bb881b5d5
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 18:03:13 +0000

    d10c38a8f8 docs: add Roman Sedov to contributors page (#42515)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+73.sha-645cad5
+ * @license Angular v12.1.0-next.4+72.sha-d10c38a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+73.sha-645cad5');
+const VERSION = new Version('12.1.0-next.4+72.sha-d10c38a');
 
 /**
  * @license
