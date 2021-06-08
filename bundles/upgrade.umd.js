d90da3b8ccf312de3273f313aeef800aad8c7f6a
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 18:01:43 +0000

    d10c38a8f8 docs: add Roman Sedov to contributors page (#42515)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+73.sha-645cad5
+ * @license Angular v12.1.0-next.4+72.sha-d10c38a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.4+73.sha-645cad5');
+    var VERSION = new core.Version('12.1.0-next.4+72.sha-d10c38a');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
