0ac41d2ad84286e110f531ccca192545337595da
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:02:10 +0000

    386550cdf3 docs: improve accessibility of reactive-forms example (#41252)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+81.sha-0031c8c
+ * @license Angular v12.1.0-next.4+82.sha-386550c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+81.sha-0031c8c');
+const VERSION = new Version('12.1.0-next.4+82.sha-386550c');
 
 /**
  * @license
