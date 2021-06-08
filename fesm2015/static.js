5b820b8ab9f29796b264f3fb3762c4aa7d6cecc1
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 21:20:45 +0000

    34ce635e3a feat(forms): undo revert and add ng-submitted class to forms that have been submitted. (#42132) (#42132)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+88.sha-2a28a1d
+ * @license Angular v12.1.0-next.4+90.sha-34ce635
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+88.sha-2a28a1d');
+const VERSION = new Version('12.1.0-next.4+90.sha-34ce635');
 
 /**
  * @license
