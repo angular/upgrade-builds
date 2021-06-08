d0612c7a6090a95cfc23ff65ce9c6dd9ad81c4db
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:05:02 +0000

    f3fde4bbb5 docs: Add selector for `PeekABooDirective` (#42146)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+84.sha-5c0541f
+ * @license Angular v12.1.0-next.4+86.sha-f3fde4b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+84.sha-5c0541f');
+const VERSION = new Version('12.1.0-next.4+86.sha-f3fde4b');
 
 /**
  * @license
