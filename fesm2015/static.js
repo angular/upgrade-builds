9abf3b7473777056a433bf96cbb5aacebe69524b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-09 23:29:43 +0000

    22bda2226b fix(compiler-cli): prevent prior compilations from being retained in watch builds (#42537)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+2.sha-3961b3c
+ * @license Angular v12.1.0-next.5+3.sha-22bda22
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+2.sha-3961b3c');
+const VERSION = new Version('12.1.0-next.5+3.sha-22bda22');
 
 /**
  * @license
