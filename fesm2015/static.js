4d4c3472bf6db9099134d98cc5cf5eb038a8bcb7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 21:28:09 +0000

    efb440eb2f refactor(router): compute correct history restoration when navigation is cancelled (#38884)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+11.sha-52e0987
+ * @license Angular v12.1.0-next.5+12.sha-efb440e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+11.sha-52e0987');
+const VERSION = new Version('12.1.0-next.5+12.sha-efb440e');
 
 /**
  * @license
