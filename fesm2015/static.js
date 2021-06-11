d206ca7355047f7fa3fe4dbc382d5df2a9c8d1f7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-11 15:20:04 +0000

    0c13e2bf97 build(packaging): remove rehype (#42557)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+12.sha-efb440e
+ * @license Angular v12.1.0-next.5+13.sha-0c13e2b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+12.sha-efb440e');
+const VERSION = new Version('12.1.0-next.5+13.sha-0c13e2b');
 
 /**
  * @license
