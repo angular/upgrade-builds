d598e163001f624c7d400d2cf3b2f4aed7a74895
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 17:17:15 +0000

    7180ec9e7c fix(forms): changes to status not always being emitted to statusChanges observable for async validators. (#42553)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+18.sha-828fde6
+ * @license Angular v12.1.0-next.5+20.sha-7180ec9
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+18.sha-828fde6');
+const VERSION = new Version('12.1.0-next.5+20.sha-7180ec9');
 
 /**
  * @license
