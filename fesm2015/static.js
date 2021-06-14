67fbf3fac52819865f58fe6f48fb3ac496007f7b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 21:32:28 +0000

    228beeabd1 fix(language-service): Use last child end span for parent without close tag (#42554)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+28.sha-044e022
+ * @license Angular v12.1.0-next.5+30.sha-228beea
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+28.sha-044e022');
+const VERSION = new Version('12.1.0-next.5+30.sha-228beea');
 
 /**
  * @license
