89702a047c54ae9bedaaee9ffbda62c8861de381
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:22:44 +0000

    3b81528ddd fix(docs-infra): improve card layout for different browser font sizes (#42533)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+42.sha-f3a7987
+ * @license Angular v12.1.0-next.5+43.sha-3b81528
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+42.sha-f3a7987');
+const VERSION = new Version('12.1.0-next.5+43.sha-3b81528');
 
 /**
  * @license
