3c944be54cfef775410a23920fdf4062545d55ce
Dylan Hunn <dylhunn@gmail.com>
2021-06-17 18:30:51 +0000

    07c1ddc487 fix(router): error if module is destroyed before location is initialized (#42560)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.6+3.sha-166e98a
+ * @license Angular v12.1.0-next.6+4.sha-07c1ddc
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.6+3.sha-166e98a');
+const VERSION = new Version('12.1.0-next.6+4.sha-07c1ddc');
 
 /**
  * @license
