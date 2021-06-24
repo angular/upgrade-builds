fdac0f1cc8198225eee0513fafebc5dea7c9de78
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-24 21:58:42 +0000

    6e84ede412 release: cut the v12.2.0-next.0 release

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0
+ * @license Angular v12.2.0-next.0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0');
+    var VERSION = new core.Version('12.2.0-next.0');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
