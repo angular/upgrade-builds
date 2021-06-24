e2fee73cce71b9f7be5075d42859f1c7a3769471
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-24 20:15:53 +0000

    b31e905754 build(docs-infra): disambiguate doc paths for global APIs (#42648)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.5+39.sha-4459741
+ * @license Angular v12.0.5+42.sha-b31e905
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.5+39.sha-4459741');
+const VERSION = new Version('12.0.5+42.sha-b31e905');
 
 /**
  * @license
