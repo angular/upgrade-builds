54aa737196bad493737af2e268d09dbe10e2d50b
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-29 18:34:42 +0000

    f84d826a3a fix(dev-infra): remove bots from special thanks section (#42697)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0+15.sha-78cf10b
+ * @license Angular v12.1.0+16.sha-f84d826
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0+15.sha-78cf10b');
+const VERSION = new Version('12.1.0+16.sha-f84d826');
 
 /**
  * @license
