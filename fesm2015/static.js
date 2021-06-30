1480fdda39b2baac37936db758872fa7a6750dfe
GitHub <noreply@github.com>
2021-06-30 20:44:08 +0000

    24624e607d docs: release notes for the v12.1.1 release (#42722)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+27.sha-59fe159
+ * @license Angular v12.2.0-next.0+28.sha-24624e6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.0+27.sha-59fe159');
+const VERSION = new Version('12.2.0-next.0+28.sha-24624e6');
 
 /**
  * @license
