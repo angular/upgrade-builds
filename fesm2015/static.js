08957a85081e952a69a700423c1a64c3622980e9
atscott <atscott01@gmail.com>
2021-07-01 22:42:58 +0000

    97c18f4527 fix(language-service): Do not override TS LS methods not supported by VE NgLS (#42727)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.1+4.sha-df5cc1f
+ * @license Angular v12.1.1+5.sha-97c18f4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.1+4.sha-df5cc1f');
+const VERSION = new Version('12.1.1+5.sha-97c18f4');
 
 /**
  * @license
