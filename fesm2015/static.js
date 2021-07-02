aaec99692a638471984f6899aaeb5db8c671410b
atscott <atscott01@gmail.com>
2021-07-02 19:51:58 +0000

    6da24e6451 build: update API goldens after api-extractor update (#42749)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.1+7.sha-aa261e7
+ * @license Angular v12.1.1+12.sha-6da24e6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.1+7.sha-aa261e7');
+const VERSION = new Version('12.1.1+12.sha-6da24e6');
 
 /**
  * @license
