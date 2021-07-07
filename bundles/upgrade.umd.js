9a09ba363f9fbbffbbd600dd5827225c11f878bd
atscott <atscott01@gmail.com>
2021-07-07 22:40:15 +0000

    0a17e98ae2 fix(compiler-cli): inline type checking instructions no longer prevent incremental reuse (#42759)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.1+25.sha-fe602c9
+ * @license Angular v12.1.1+26.sha-0a17e98
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.1+25.sha-fe602c9');
+    var VERSION = new core.Version('12.1.1+26.sha-0a17e98');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
