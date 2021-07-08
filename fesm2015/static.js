fdac76821b760fc2a2f8baa5d0e6bbd82389e5f1
atscott <atscott01@gmail.com>
2021-07-08 22:26:50 +0000

    a8d5a31356 docs(core): initializeApp method should return function (#42743)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.1+41.sha-f6bce31
+ * @license Angular v12.1.1+43.sha-a8d5a31
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.1+41.sha-f6bce31');
+const VERSION = new Version('12.1.1+43.sha-a8d5a31');
 
 /**
  * @license
