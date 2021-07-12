43d302b7662ee22d262034a906813b82fcb2bf8c
Andrew Kushnir <akushnir@google.com>
2021-07-12 20:43:24 +0000

    4f8a7280c1 docs: move start tutorial alert for clarity (#42764)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.1+60.sha-91a576d
+ * @license Angular v12.1.1+83.sha-4f8a728
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.1+60.sha-91a576d');
+const VERSION = new Version('12.1.1+83.sha-4f8a728');
 
 /**
  * @license
