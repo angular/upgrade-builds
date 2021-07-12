7c6598325e542684e3c0b06b0b0cda96611c5b52
Andrew Kushnir <akushnir@google.com>
2021-07-12 19:12:39 +0000

    4c482bf3f1 fix(compiler-cli): properly emit literal types when recreating type parameters in a different file (#42761)

--- bundles/upgrade.umd.js
+++ bundles/upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+62.sha-40da386
+ * @license Angular v12.2.0-next.1+63.sha-4c482bf
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.2.0-next.1+62.sha-40da386');
+    var VERSION = new core.Version('12.2.0-next.1+63.sha-4c482bf');
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
