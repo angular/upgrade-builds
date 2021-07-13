e0dd2bd5dd00d6c9528fe0eb3d61fa5896966605
Andrew Kushnir <akushnir@google.com>
2021-07-13 18:55:49 +0000

    d654c7933a fix(common): re-sort output of `KeyValuePipe` when `compareFn` changes (#42821)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.1+92.sha-b101ad0
+ * @license Angular v12.1.1+93.sha-d654c79
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.1+92.sha-b101ad0');
+const VERSION = new Version('12.1.1+93.sha-d654c79');
 
 /**
  * @license
