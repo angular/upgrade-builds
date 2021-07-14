d88b6d958f53c5574324c9175af026bf1170d6f0
GitHub <noreply@github.com>
2021-07-14 18:12:03 +0000

    2a61728137 release: cut the v12.1.2 release (#42856)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.1+96.sha-6301354
+ * @license Angular v12.1.1+97.sha-2a61728
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.1+96.sha-6301354');
+const VERSION = new Version('12.1.1+97.sha-2a61728');
 
 /**
  * @license
