1ee61b4ecbd7e11a4ca6a100effab844589cf265
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:57:45 +0000

    52dc65dd1b build(docs-infra): improve @usageNotes message (#42888)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+39.sha-307dac6
+ * @license Angular v12.2.0-next.2+40.sha-52dc65d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.2+39.sha-307dac6');
+const VERSION = new Version('12.2.0-next.2+40.sha-52dc65d');
 
 /**
  * @license
