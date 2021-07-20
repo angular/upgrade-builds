fd2896cdffe83e0ce76fca2fc74c9a158fd933fa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 19:19:54 +0000

    e2aa7652e5 fix(docs-infra): fix keyword color in CLI pages code blocks (#42889)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+41.sha-445fee4
+ * @license Angular v12.2.0-next.2+45.sha-e2aa765
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.2+41.sha-445fee4');
+const VERSION = new Version('12.2.0-next.2+45.sha-e2aa765');
 
 /**
  * @license
