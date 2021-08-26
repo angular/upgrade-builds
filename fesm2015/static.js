b5f953fff1159910376e64269ad266e116639421
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-26 17:27:34 +0000

    fdcb420bd7 docs(router): Fix incorrect name for url match options (#43268)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.3+3.sha-fb64f71.with-local-changes
+ * @license Angular v12.2.3+5.sha-fdcb420.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.3+3.sha-fb64f71.with-local-changes');
+const VERSION = new Version('12.2.3+5.sha-fdcb420.with-local-changes');
 
 /**
  * @license
