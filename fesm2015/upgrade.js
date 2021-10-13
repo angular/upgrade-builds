79f661c5d02ac6dd3b72fa329327c61527df87c9
GitHub <noreply@github.com>
2021-10-13 18:20:42 +0000

    c7c285cec7 release: cut the v12.2.10 release (#43826)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.9+13.sha-fced3e8.with-local-changes
+ * @license Angular v12.2.9+17.sha-c7c285c.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.9+13.sha-fced3e8.with-local-changes');
+const VERSION = new Version('12.2.9+17.sha-c7c285c.with-local-changes');
 
 /**
  * @license
