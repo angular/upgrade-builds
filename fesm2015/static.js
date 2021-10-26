c042ea3fbe4f4d1a455ad86ad2fe408b5b4e62c0
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-26 01:48:15 +0000

    5f32863a74 docs(docs-infra): change deprecated keyboardEvent.keyCode example to use keyboardEvent.code (#43934)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.11+8.sha-c831c67.with-local-changes
+ * @license Angular v12.2.11+10.sha-5f32863.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.11+8.sha-c831c67.with-local-changes');
+const VERSION = new Version('12.2.11+10.sha-5f32863.with-local-changes');
 
 /**
  * @license
