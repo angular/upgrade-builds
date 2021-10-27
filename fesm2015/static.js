221ffa79160d5ec7fa0b1004256e7a735c12b473
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-27 16:37:27 +0000

    515949e7e6 docs: review tag added (#43465)

--- fesm2015/static.js
+++ fesm2015/static.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.11+14.sha-8fcfd87.with-local-changes
+ * @license Angular v12.2.11+20.sha-515949e.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -908,7 +908,7 @@ function downgradeInjectable(token, downgradedModule = '') {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.11+14.sha-8fcfd87.with-local-changes');
+const VERSION = new Version('12.2.11+20.sha-515949e.with-local-changes');
 
 /**
  * @license
