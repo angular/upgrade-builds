ed1e575331001b22a27ea556c7b5f4a78f11a420
Andrew Kushnir <akushnir@google.com>
2025-03-07 19:10:05 +0000

    bde1b37 build: properly compile tests in `core` with Angular compiler (#60268)

--- fesm2022/upgrade_helper-05369591.mjs
+++ fesm2022/upgrade_helper-05369591.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.1+sha-70fb3bb
+ * @license Angular v19.2.1+sha-bde1b37
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version, ɵNG_MOD_DEF, Injector, ChangeDetectorRef, Testability, Testab
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.1+sha-70fb3bb');
+const VERSION = new Version('19.2.1+sha-bde1b37');
 
 function noNg() {
     throw new Error('AngularJS v1.x is not loaded!');
