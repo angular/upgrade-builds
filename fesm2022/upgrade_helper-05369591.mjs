4e3f4b40cd54f4a893e0babdf5b58953a6462bed
Andrew Kushnir <akushnir@google.com>
2025-03-10 17:07:59 +0000

    23ca885 fix(compiler-cli): handle transformed classes when generating HMR code (#60298)

--- fesm2022/upgrade_helper-05369591.mjs
+++ fesm2022/upgrade_helper-05369591.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.1+sha-44155c9
+ * @license Angular v19.2.1+sha-23ca885
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version, ɵNG_MOD_DEF, Injector, ChangeDetectorRef, Testability, Testab
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.1+sha-44155c9');
+const VERSION = new Version('19.2.1+sha-23ca885');
 
 function noNg() {
     throw new Error('AngularJS v1.x is not loaded!');
