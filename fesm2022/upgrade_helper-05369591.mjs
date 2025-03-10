3a58896f782d78b972ed3a3e280fea1236298cfa
Andrew Kushnir <akushnir@google.com>
2025-03-10 16:40:27 +0000

    07dc806 refactor(bazel): remove primary_bundle_name as attr for ng_package (#60306)

--- fesm2022/upgrade_helper-05369591.mjs
+++ fesm2022/upgrade_helper-05369591.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.1+sha-338818c
+ * @license Angular v20.0.0-next.1+sha-07dc806
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version, ɵNG_MOD_DEF, Injector, ChangeDetectorRef, Testability, Testab
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.1+sha-338818c');
+const VERSION = new Version('20.0.0-next.1+sha-07dc806');
 
 function noNg() {
     throw new Error('AngularJS v1.x is not loaded!');
