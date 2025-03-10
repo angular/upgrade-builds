ed6440e2a3e8253ece54eb867648b6e7f1a514a3
Andrew Kushnir <akushnir@google.com>
2025-03-10 16:30:35 +0000

    5e2f312 fix(docs-infra): terminal light mode text color (#60246)

--- fesm2022/upgrade_helper-05369591.mjs
+++ fesm2022/upgrade_helper-05369591.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.1+sha-bde1b37
+ * @license Angular v19.2.1+sha-5e2f312
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version, ɵNG_MOD_DEF, Injector, ChangeDetectorRef, Testability, Testab
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.1+sha-bde1b37');
+const VERSION = new Version('19.2.1+sha-5e2f312');
 
 function noNg() {
     throw new Error('AngularJS v1.x is not loaded!');
