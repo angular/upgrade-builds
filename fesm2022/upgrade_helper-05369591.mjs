d4829ea2b7966046a2322aa31be3c45b34393ec9
Andrew Kushnir <akushnir@google.com>
2025-03-11 18:39:49 +0000

    8be6e38 refactor(core): replace internal usages of InjectFlags (#60318)

--- fesm2022/upgrade_helper-05369591.mjs
+++ fesm2022/upgrade_helper-05369591.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.1+sha-9a124c8
+ * @license Angular v20.0.0-next.1+sha-8be6e38
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version, ɵNG_MOD_DEF, Injector, ChangeDetectorRef, Testability, Testab
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.1+sha-9a124c8');
+const VERSION = new Version('20.0.0-next.1+sha-8be6e38');
 
 function noNg() {
     throw new Error('AngularJS v1.x is not loaded!');
