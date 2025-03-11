e578bd9f4d54740d2c10f3c8771352d2534423bf
Andrew Kushnir <akushnir@google.com>
2025-03-11 21:34:00 +0000

    705fb0d docs: add mention of GH issue for devPreview feedback (#60010)

--- fesm2022/upgrade_helper-05369591.mjs
+++ fesm2022/upgrade_helper-05369591.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.1+sha-b6f22a7
+ * @license Angular v19.2.1+sha-705fb0d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version, ɵNG_MOD_DEF, Injector, ChangeDetectorRef, Testability, Testab
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.1+sha-b6f22a7');
+const VERSION = new Version('19.2.1+sha-705fb0d');
 
 function noNg() {
     throw new Error('AngularJS v1.x is not loaded!');
