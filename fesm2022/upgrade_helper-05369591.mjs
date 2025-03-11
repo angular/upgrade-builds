edb0ebdbae9d52ae9d795bcbc960c2fe2c8bc451
Andrew Kushnir <akushnir@google.com>
2025-03-11 20:12:21 +0000

    b6f22a7 refactor(core): inline standalone check to be dropped (#60305)

--- fesm2022/upgrade_helper-05369591.mjs
+++ fesm2022/upgrade_helper-05369591.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.1+sha-f9b6bf9
+ * @license Angular v19.2.1+sha-b6f22a7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version, ɵNG_MOD_DEF, Injector, ChangeDetectorRef, Testability, Testab
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.1+sha-f9b6bf9');
+const VERSION = new Version('19.2.1+sha-b6f22a7');
 
 function noNg() {
     throw new Error('AngularJS v1.x is not loaded!');
