52d8cf5231f3f3b3708e96376c77adee883e1309
Andrew Kushnir <akushnir@google.com>
2025-03-12 17:47:42 +0000

    0d2b964 refactor(core): add equal option to the signal and computed creation (#60300)

--- fesm2022/upgrade_helper-BOLCivYz.mjs
+++ fesm2022/upgrade_helper-BOLCivYz.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.2+sha-7529512
+ * @license Angular v19.2.2+sha-0d2b964
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version, ɵNG_MOD_DEF as _NG_MOD_DEF, Injector, ChangeDetectorRef, Test
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.2+sha-7529512');
+const VERSION = new Version('19.2.2+sha-0d2b964');
 
 function noNg() {
     throw new Error('AngularJS v1.x is not loaded!');
