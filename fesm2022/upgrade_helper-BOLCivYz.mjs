e4a7425edeb8e0ff4be9553127aaab5fa97b8f2c
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-03-12 16:59:22 +0000

    bc472b6 release: cut the v20.0.0-next.2 release

--- fesm2022/upgrade_helper-BOLCivYz.mjs
+++ fesm2022/upgrade_helper-BOLCivYz.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.1+sha-0788b0c
+ * @license Angular v20.0.0-next.2+sha-bc472b6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version, ɵNG_MOD_DEF as _NG_MOD_DEF, Injector, ChangeDetectorRef, Test
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.1+sha-0788b0c');
+const VERSION = new Version('20.0.0-next.2+sha-bc472b6');
 
 function noNg() {
     throw new Error('AngularJS v1.x is not loaded!');
