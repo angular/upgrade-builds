5a4386106f30823485af9fcdf3cfbb7701d0992c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-19 15:01:07 +0000

    13a8709 fix(core): catch hydration marker with implicit body tag (#60429)

--- fesm2022/upgrade_helper-DMR5pJ--.mjs
+++ fesm2022/upgrade_helper-DMR5pJ--.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.2+sha-9fea601
+ * @license Angular v19.2.2+sha-13a8709
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version, ɵNG_MOD_DEF as _NG_MOD_DEF, Injector, ChangeDetectorRef, Test
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.2+sha-9fea601');
+const VERSION = new Version('19.2.2+sha-13a8709');
 
 function noNg() {
     throw new Error('AngularJS v1.x is not loaded!');
