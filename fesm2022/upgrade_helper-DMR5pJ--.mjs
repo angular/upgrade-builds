2a59278ed8427edb275cbe40bae8fb48b3ec8d7d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-19 18:17:29 +0000

    2466fc9 refactor(platform-browser-dynamic): relocate `DOMTestComponentRenderer` to `@angular/platform-browser` (#60453)

--- fesm2022/upgrade_helper-DMR5pJ--.mjs
+++ fesm2022/upgrade_helper-DMR5pJ--.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.2+sha-d306e31
+ * @license Angular v19.2.2+sha-2466fc9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version, ɵNG_MOD_DEF as _NG_MOD_DEF, Injector, ChangeDetectorRef, Test
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.2+sha-d306e31');
+const VERSION = new Version('19.2.2+sha-2466fc9');
 
 function noNg() {
     throw new Error('AngularJS v1.x is not loaded!');
