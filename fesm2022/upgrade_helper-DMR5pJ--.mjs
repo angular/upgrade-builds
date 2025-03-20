a195bf71cd51a005a9dc19f45c1f397ea86040f3
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-03-20 07:48:27 +0000

    9426e83 build: disable RBE for compliance partial goldens (#60473)

--- fesm2022/upgrade_helper-DMR5pJ--.mjs
+++ fesm2022/upgrade_helper-DMR5pJ--.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.3+sha-6df6aa1
+ * @license Angular v19.2.3+sha-9426e83
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version, ɵNG_MOD_DEF as _NG_MOD_DEF, Injector, ChangeDetectorRef, Test
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.3+sha-6df6aa1');
+const VERSION = new Version('19.2.3+sha-9426e83');
 
 function noNg() {
     throw new Error('AngularJS v1.x is not loaded!');
