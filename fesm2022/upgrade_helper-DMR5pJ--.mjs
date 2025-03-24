6d9d3d3a315ce3ce6c9568b37c79e2867f80cc46
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-03-24 14:14:09 +0000

    997836e refactor(core): specify reactive node kind for linked signal (#60451)

--- fesm2022/upgrade_helper-DMR5pJ--.mjs
+++ fesm2022/upgrade_helper-DMR5pJ--.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.3+sha-dcfa8a0
+ * @license Angular v20.0.0-next.3+sha-997836e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version, ɵNG_MOD_DEF as _NG_MOD_DEF, Injector, ChangeDetectorRef, Test
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.3+sha-dcfa8a0');
+const VERSION = new Version('20.0.0-next.3+sha-997836e');
 
 function noNg() {
     throw new Error('AngularJS v1.x is not loaded!');
