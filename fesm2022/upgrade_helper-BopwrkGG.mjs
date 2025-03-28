556d1db347e889c0629688e2323f38c7b2a03bc7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-28 17:31:59 +0000

    d15fda3 Revert "refactor(core): allow multiple DI profilers (#60562)" (#60611)

--- fesm2022/upgrade_helper-BopwrkGG.mjs
+++ fesm2022/upgrade_helper-BopwrkGG.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-e40b5c9
+ * @license Angular v20.0.0-next.4+sha-d15fda3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { e as element, q as $ROOT_ELEMENT, c as $ROOT_SCOPE, D as DOWNGRADED_MOD
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-e40b5c9');
+const VERSION = new Version('20.0.0-next.4+sha-d15fda3');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
