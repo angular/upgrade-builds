80300d88cf58d13c1537940ae88969772b1f6b69
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-28 15:13:24 +0000

    882f96f refactor(core): produce a message about `@defer` behavior when HMR is enabled (#60533)

--- fesm2022/upgrade_helper-BopwrkGG.mjs
+++ fesm2022/upgrade_helper-BopwrkGG.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.4+sha-b18215d
+ * @license Angular v19.2.4+sha-882f96f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { e as element, q as $ROOT_ELEMENT, c as $ROOT_SCOPE, D as DOWNGRADED_MOD
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.4+sha-b18215d');
+const VERSION = new Version('19.2.4+sha-882f96f');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
