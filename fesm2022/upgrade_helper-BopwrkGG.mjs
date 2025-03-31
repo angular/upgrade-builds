dd1b5a93e053656bf668869f8aadbe96749800bf
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 16:46:16 +0000

    599c45c build: update lock files for integration test apps (#60594)

--- fesm2022/upgrade_helper-BopwrkGG.mjs
+++ fesm2022/upgrade_helper-BopwrkGG.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-52dfce6
+ * @license Angular v20.0.0-next.4+sha-599c45c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { e as element, q as $ROOT_ELEMENT, c as $ROOT_SCOPE, D as DOWNGRADED_MOD
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-52dfce6');
+const VERSION = new Version('20.0.0-next.4+sha-599c45c');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
