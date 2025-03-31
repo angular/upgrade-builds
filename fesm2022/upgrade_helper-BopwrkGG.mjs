c78c816ebb40f4223f16b2d1de0203a3dffee1d4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 20:39:41 +0000

    79d502f docs: update Angular CLI help [main] (#60647)

--- fesm2022/upgrade_helper-BopwrkGG.mjs
+++ fesm2022/upgrade_helper-BopwrkGG.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-4e478af
+ * @license Angular v20.0.0-next.4+sha-79d502f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { e as element, q as $ROOT_ELEMENT, c as $ROOT_SCOPE, D as DOWNGRADED_MOD
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-4e478af');
+const VERSION = new Version('20.0.0-next.4+sha-79d502f');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
