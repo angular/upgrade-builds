01c971cbc6d33d1f00009ec3c95c1cc1edceebf2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 17:17:21 +0000

    c5d841a fix(devtools): catch firefox non-top level frame error case when using utilities APIs (#60430)

--- fesm2022/upgrade_helper-BopwrkGG.mjs
+++ fesm2022/upgrade_helper-BopwrkGG.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-8b529de
+ * @license Angular v20.0.0-next.4+sha-c5d841a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { e as element, q as $ROOT_ELEMENT, c as $ROOT_SCOPE, D as DOWNGRADED_MOD
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-8b529de');
+const VERSION = new Version('20.0.0-next.4+sha-c5d841a');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
