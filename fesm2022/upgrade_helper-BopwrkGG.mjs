0df3473f202d78edaf138d419aac73792d320241
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-01 12:26:28 +0000

    b3c7282 Revert "refactor(core): add `ApplicationRef.prototype.bootstrapImpl` with an `injector` parameter (#60622)" (#60669)

--- fesm2022/upgrade_helper-BopwrkGG.mjs
+++ fesm2022/upgrade_helper-BopwrkGG.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.4+sha-e816d2d
+ * @license Angular v19.2.4+sha-b3c7282
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { e as element, q as $ROOT_ELEMENT, c as $ROOT_SCOPE, D as DOWNGRADED_MOD
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.4+sha-e816d2d');
+const VERSION = new Version('19.2.4+sha-b3c7282');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
