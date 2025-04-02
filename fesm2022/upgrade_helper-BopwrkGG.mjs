da1e62bb4755d1395d226ae9fac22e1038f38bd3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 13:44:02 +0000

    d3fec85 docs: Use `inject` for DI (#59835)

--- fesm2022/upgrade_helper-BopwrkGG.mjs
+++ fesm2022/upgrade_helper-BopwrkGG.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-590b502
+ * @license Angular v20.0.0-next.4+sha-d3fec85
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { e as element, q as $ROOT_ELEMENT, c as $ROOT_SCOPE, D as DOWNGRADED_MOD
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-590b502');
+const VERSION = new Version('20.0.0-next.4+sha-d3fec85');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
