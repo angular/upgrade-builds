9ce3881a9c87918c0ebff512ff273fd3653a8d6c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 15:22:17 +0000

    6acce7c fix(core): fixes #592882 ng generate @angular/core:signal-queries-migration (#60688)

--- fesm2022/upgrade_helper-BopwrkGG.mjs
+++ fesm2022/upgrade_helper-BopwrkGG.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-59d40f2
+ * @license Angular v20.0.0-next.4+sha-6acce7c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { e as element, q as $ROOT_ELEMENT, c as $ROOT_SCOPE, D as DOWNGRADED_MOD
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-59d40f2');
+const VERSION = new Version('20.0.0-next.4+sha-6acce7c');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
