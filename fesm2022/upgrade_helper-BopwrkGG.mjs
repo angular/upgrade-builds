75b11376f6648103c4a8796f24446d5661930e77
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 14:34:18 +0000

    8f68d1b fix(core): fix ng generate @angular/core:output-migration (#60626)

--- fesm2022/upgrade_helper-BopwrkGG.mjs
+++ fesm2022/upgrade_helper-BopwrkGG.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.4+sha-0c5b697
+ * @license Angular v19.2.4+sha-8f68d1b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { e as element, q as $ROOT_ELEMENT, c as $ROOT_SCOPE, D as DOWNGRADED_MOD
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.4+sha-0c5b697');
+const VERSION = new Version('19.2.4+sha-8f68d1b');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
