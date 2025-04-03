2ff01d20645ae4a9a12627013166dcbe80d745b6
Andrew Scott <atscott@google.com>
2025-04-03 17:44:04 +0000

    0e82d42 fix(language-service): Do not provide element completions in end tag (#60616)

--- fesm2022/upgrade_helper-BopwrkGG.mjs
+++ fesm2022/upgrade_helper-BopwrkGG.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.5+sha-8d183ae
+ * @license Angular v19.2.5+sha-0e82d42
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { e as element, q as $ROOT_ELEMENT, c as $ROOT_SCOPE, D as DOWNGRADED_MOD
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.5+sha-8d183ae');
+const VERSION = new Version('19.2.5+sha-0e82d42');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
