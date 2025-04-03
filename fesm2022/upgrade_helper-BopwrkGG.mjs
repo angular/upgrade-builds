b1db7f1206f8d4b3c49ef8a928dd63b4286ae9ed
Andrew Scott <atscott@google.com>
2025-04-03 19:59:16 +0000

    5795e03 fix(http): Delay stabilization until next app synchronization (#60656)

--- fesm2022/upgrade_helper-BopwrkGG.mjs
+++ fesm2022/upgrade_helper-BopwrkGG.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-2348f26
+ * @license Angular v20.0.0-next.5+sha-5795e03
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { e as element, q as $ROOT_ELEMENT, c as $ROOT_SCOPE, D as DOWNGRADED_MOD
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.5+sha-2348f26');
+const VERSION = new Version('20.0.0-next.5+sha-5795e03');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
