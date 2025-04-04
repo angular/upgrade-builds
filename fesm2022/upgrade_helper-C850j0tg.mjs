ad713505de9d43759c29ace5c91f267256c5a028
Andrew Scott <atscott@google.com>
2025-04-04 18:53:21 +0000

    9228a73 feat(docs-infra): Add Navigation API adapter to adev (#60722)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-92c4123
+ * @license Angular v20.0.0-next.5+sha-9228a73
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.5+sha-92c4123');
+const VERSION = new Version('20.0.0-next.5+sha-9228a73');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
