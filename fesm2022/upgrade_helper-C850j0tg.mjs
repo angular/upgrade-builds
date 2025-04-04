88d00c7d03f78ab74a192ce801e6b3ee5784b41a
Andrew Scott <atscott@google.com>
2025-04-04 17:42:09 +0000

    8d050b5 feat(core): stabilize linkedSignal API (#60741)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-3ea0163
+ * @license Angular v20.0.0-next.5+sha-8d050b5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.5+sha-3ea0163');
+const VERSION = new Version('20.0.0-next.5+sha-8d050b5');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
