a1318916c9a99a920e87a6c00d4be200dabef8de
Andrew Scott <atscott@google.com>
2025-04-07 16:33:34 +0000

    00bbd9b fix(core): fix docs for output migration (#60764)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.5+sha-e9a719e
+ * @license Angular v19.2.5+sha-00bbd9b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.5+sha-e9a719e');
+const VERSION = new Version('19.2.5+sha-00bbd9b');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
