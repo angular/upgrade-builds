69f71c84864f9f8e8b5744dbf9177f03eab1b6da
Andrew Scott <atscott@google.com>
2025-04-07 18:38:59 +0000

    0ae1889 fix(core): run `ApplicationRef.prototype.bootstrap` in `NgZone` (#60720)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-5039ed0
+ * @license Angular v20.0.0-next.5+sha-0ae1889
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.5+sha-5039ed0');
+const VERSION = new Version('20.0.0-next.5+sha-0ae1889');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
