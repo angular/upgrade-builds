6ba5f502f1b05a2007d160ca78667a32b4545ec5
Andrew Scott <atscott@google.com>
2025-04-07 17:38:02 +0000

    5039ed0 build: update cross-repo angular dependencies (#60747)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-d8e941b
+ * @license Angular v20.0.0-next.5+sha-5039ed0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.5+sha-d8e941b');
+const VERSION = new Version('20.0.0-next.5+sha-5039ed0');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
