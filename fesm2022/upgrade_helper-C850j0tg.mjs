435d5a4c944b0ad69931ee47d4ffbc94bdda20dd
Andrew Scott <atscott@google.com>
2025-04-08 17:30:28 +0000

    0506a0e docs: update Angular CLI help [main] (#60788)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-80a3258
+ * @license Angular v20.0.0-next.5+sha-0506a0e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.5+sha-80a3258');
+const VERSION = new Version('20.0.0-next.5+sha-0506a0e');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
