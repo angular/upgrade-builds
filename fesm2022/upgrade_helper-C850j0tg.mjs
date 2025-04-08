6c637dbd01d2d4f128479b4692e20fa5bc558d85
Andrew Scott <atscott@google.com>
2025-04-08 23:21:04 +0000

    9241615 fix(core): reduce total memory usage of various migration schematics (#60776)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.5+sha-dc193a7
+ * @license Angular v19.2.5+sha-9241615
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.5+sha-dc193a7');
+const VERSION = new Version('19.2.5+sha-9241615');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
