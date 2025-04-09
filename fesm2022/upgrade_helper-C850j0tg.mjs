c5fe5d71f83b9e57cea6fcf7ff242c999bf703ae
Andrew Scott <atscott@google.com>
2025-04-09 16:39:49 +0000

    e50f8a2 Revert "refactor(core): use stream in rxResource instead of loader (#59910)" (#60803)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-2b4fd9b
+ * @license Angular v20.0.0-next.5+sha-e50f8a2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.5+sha-2b4fd9b');
+const VERSION = new Version('20.0.0-next.5+sha-e50f8a2');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
