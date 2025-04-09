e0b487b241dc65b9409ac0a219ff3019f163ca39
Andrew Scott <atscott@google.com>
2025-04-09 21:33:29 +0000

    d39e09d fix(http): Include HTTP status code and headers when HTTP requests errored in `httpResource` (#60802)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.6+sha-54f9269
+ * @license Angular v19.2.6+sha-d39e09d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.6+sha-54f9269');
+const VERSION = new Version('19.2.6+sha-d39e09d');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
