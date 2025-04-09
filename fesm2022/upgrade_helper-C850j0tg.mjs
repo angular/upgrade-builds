8bab02c3847b4ff4a6b5eb1cd6f5f904b850de4d
Andrew Scott <atscott01@gmail.com>
2025-04-09 19:37:04 +0000

    c3113b9 docs: release notes for the v19.2.6 release

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-8744c9a
+ * @license Angular v20.0.0-next.5+sha-c3113b9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.5+sha-8744c9a');
+const VERSION = new Version('20.0.0-next.5+sha-c3113b9');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
