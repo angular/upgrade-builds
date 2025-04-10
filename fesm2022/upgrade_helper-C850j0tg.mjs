31d40b8caf1bed5ef67bb4522e99be98f5b8d4f4
kirjs <kirjs@users.noreply.github.com>
2025-04-10 15:29:33 +0000

    2021f64 refactor(devtools): reduce material form field density globally (#60633)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-abe9691
+ * @license Angular v20.0.0-next.6+sha-2021f64
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.6+sha-abe9691');
+const VERSION = new Version('20.0.0-next.6+sha-2021f64');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
