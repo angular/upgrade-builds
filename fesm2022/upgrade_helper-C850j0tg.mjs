ae4548f8c21a837b9f668b7f009a24fad2d5f243
kirjs <kirjs@users.noreply.github.com>
2025-04-10 15:27:55 +0000

    77c6041 fix(docs-infra): update docs-alert and tests (#60829)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.6+sha-d39e09d
+ * @license Angular v19.2.6+sha-77c6041
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.6+sha-d39e09d');
+const VERSION = new Version('19.2.6+sha-77c6041');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
