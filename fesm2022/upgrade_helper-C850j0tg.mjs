c4d69d171562a6c33ff624ef65391d1b210e3a2e
kirjs <kirjs@users.noreply.github.com>
2025-04-11 18:59:55 +0000

    72dd133 refactor(router): Use `NavigationTrigger` more consistently and move events helper next to definitions (#60736)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-1522e6c
+ * @license Angular v20.0.0-next.6+sha-72dd133
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.6+sha-1522e6c');
+const VERSION = new Version('20.0.0-next.6+sha-72dd133');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
