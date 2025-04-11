f4b400783d5c0cb80554df26516a372d7e969922
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-04-11 10:31:07 +0000

    cda922d build: update post-approval dev-infra action

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-2b59031
+ * @license Angular v20.0.0-next.6+sha-cda922d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.6+sha-2b59031');
+const VERSION = new Version('20.0.0-next.6+sha-cda922d');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
