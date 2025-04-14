262384e4f18e90c099ae0c18c68cc47722f55b3f
kirjs <kirjs@users.noreply.github.com>
2025-04-14 13:45:57 +0000

    84d359c refactor(compiler): add support for the new search element (#54945)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-c5ca597
+ * @license Angular v20.0.0-next.6+sha-84d359c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.6+sha-c5ca597');
+const VERSION = new Version('20.0.0-next.6+sha-84d359c');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
