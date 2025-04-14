060cebfe90dbc81e137de19323a2694fb4902947
kirjs <kirjs@users.noreply.github.com>
2025-04-14 13:28:48 +0000

    c5ca597 refactor(devtools): switch to components tab whenever the inspector is used (#60694)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-c0ef923
+ * @license Angular v20.0.0-next.6+sha-c5ca597
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.6+sha-c0ef923');
+const VERSION = new Version('20.0.0-next.6+sha-c5ca597');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
