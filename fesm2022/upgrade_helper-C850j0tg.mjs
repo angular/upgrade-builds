1f129381cbab2881ecf0ed2a8fff534639145fb1
kirjs <kirjs@users.noreply.github.com>
2025-04-14 17:47:24 +0000

    e24b6d5 build: update cross-repo angular dependencies (#60847)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-c1bcae9
+ * @license Angular v20.0.0-next.6+sha-e24b6d5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.6+sha-c1bcae9');
+const VERSION = new Version('20.0.0-next.6+sha-e24b6d5');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
