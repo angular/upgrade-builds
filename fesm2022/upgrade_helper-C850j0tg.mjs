42f88d95bc6e36bfc6ff6308d3a237d37c08a183
kirjs <kirjs@users.noreply.github.com>
2025-04-14 15:08:28 +0000

    c80f24d docs: fix missing card-title tag in example of fallback-content section (#60855)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.6+sha-c0e4ff1
+ * @license Angular v19.2.6+sha-c80f24d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.6+sha-c0e4ff1');
+const VERSION = new Version('19.2.6+sha-c80f24d');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
