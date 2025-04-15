73583de8266ee3cbf212ccf4604e8483936b4308
kirjs <kirjs@users.noreply.github.com>
2025-04-15 13:28:11 +0000

    9171401 refactor(devtools): update release process to commit before publishing (#60646)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.6+sha-9ebe59f
+ * @license Angular v19.2.6+sha-9171401
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.6+sha-9ebe59f');
+const VERSION = new Version('19.2.6+sha-9171401');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
