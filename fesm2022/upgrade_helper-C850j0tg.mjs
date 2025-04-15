0aa0ec53d009c57e874c656cb15e13c28fd132cb
kirjs <kirjs@users.noreply.github.com>
2025-04-15 22:47:43 +0000

    e90daf3 refactor(core): Update Transition finished promise to not throw unhandled rejection (#60816)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-388c749
+ * @license Angular v20.0.0-next.6+sha-e90daf3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.6+sha-388c749');
+const VERSION = new Version('20.0.0-next.6+sha-e90daf3');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
