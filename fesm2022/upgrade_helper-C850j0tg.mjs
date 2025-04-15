84349b711a7d4d34c1d3c71fe4c433a0017ec606
kirjs <kirjs@users.noreply.github.com>
2025-04-15 21:43:10 +0000

    aaf07d8 docs: Add error handling documentation (#60848)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.6+sha-9171401
+ * @license Angular v19.2.6+sha-aaf07d8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.6+sha-9171401');
+const VERSION = new Version('19.2.6+sha-aaf07d8');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
