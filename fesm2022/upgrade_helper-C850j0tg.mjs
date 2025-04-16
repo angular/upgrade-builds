536e2279bf3599f8901c5954592bb7b28552eea6
kirjs <kirjs@users.noreply.github.com>
2025-04-16 16:54:46 +0000

    c90eea2 refactor(common): eliminate redundant iterator-to-array conversion (#60884)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.6+sha-37ab681
+ * @license Angular v19.2.6+sha-c90eea2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.6+sha-37ab681');
+const VERSION = new Version('19.2.6+sha-c90eea2');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
