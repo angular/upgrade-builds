90859b0930221fd9e871a78dfa5844e97e15880a
kirjs <kirjs@users.noreply.github.com>
2025-04-16 14:31:04 +0000

    f22c72b docs(docs-infra): first-app tutorial - step-14-http src-final correction (#60738)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-e90daf3
+ * @license Angular v20.0.0-next.6+sha-f22c72b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.6+sha-e90daf3');
+const VERSION = new Version('20.0.0-next.6+sha-f22c72b');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
