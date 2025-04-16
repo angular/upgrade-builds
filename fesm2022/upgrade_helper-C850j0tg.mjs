4fde2cc4563fd22ad7eed4b62ca934930b1a5833
kirjs <kirjs@users.noreply.github.com>
2025-04-16 17:08:07 +0000

    5675223 feat(core): stabilize incremental hydration api (#60888)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-bf8492b
+ * @license Angular v20.0.0-next.6+sha-5675223
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.6+sha-bf8492b');
+const VERSION = new Version('20.0.0-next.6+sha-5675223');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
