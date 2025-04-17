6f3b5bbbd94042b71e102e2097981aa7c566c0bf
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-17 09:34:04 +0000

    763e3ca release: bump Angular DevTools version to 1.0.28 (#60893)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.7+sha-2c1f348
+ * @license Angular v19.2.7+sha-763e3ca
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.7+sha-2c1f348');
+const VERSION = new Version('19.2.7+sha-763e3ca');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
