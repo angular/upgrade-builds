4d5ee68fcb35d076a798e69dbe92e1c640ad6d47
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-18 13:22:37 +0000

    1694739 build: update cross-repo angular dependencies (#60897)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.7+sha-2f3f8d6
+ * @license Angular v20.0.0-next.7+sha-1694739
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.7+sha-2f3f8d6');
+const VERSION = new Version('20.0.0-next.7+sha-1694739');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
