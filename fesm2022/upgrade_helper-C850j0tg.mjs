49bfab5892b6755cdec7ea5c19a24ef6fe4b8641
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-22 10:09:24 +0000

    9d08525 docs: format `RouterUpgradeInitializer` usage notes (#60930)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.7+sha-5555e9f
+ * @license Angular v19.2.7+sha-9d08525
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.7+sha-5555e9f');
+const VERSION = new Version('19.2.7+sha-9d08525');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
