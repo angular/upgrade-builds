2340644204b741f782df0a1ad0332ca718c96082
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-22 09:22:10 +0000

    b43fe29 docs: update examples for i18n tokens (#60938)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.7+sha-0162ceb
+ * @license Angular v20.0.0-next.7+sha-b43fe29
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.7+sha-0162ceb');
+const VERSION = new Version('20.0.0-next.7+sha-b43fe29');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
