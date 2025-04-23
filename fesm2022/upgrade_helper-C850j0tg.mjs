09994a8023e0f389bb594de72ea385a90f04bc26
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-23 06:53:23 +0000

    c990265 docs(docs-infra): Improve doc support for pipes. (#60926)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.7+sha-1b8e7ab
+ * @license Angular v20.0.0-next.7+sha-c990265
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.7+sha-1b8e7ab');
+const VERSION = new Version('20.0.0-next.7+sha-c990265');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
