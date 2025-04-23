7dd26340cf74a728032c2f0e788a3ecf8bbccd33
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-23 14:37:18 +0000

    94b2a8c release: cut the v19.2.8 release

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.7+sha-b557618
+ * @license Angular v19.2.8+sha-94b2a8c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.7+sha-b557618');
+const VERSION = new Version('19.2.8+sha-94b2a8c');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
