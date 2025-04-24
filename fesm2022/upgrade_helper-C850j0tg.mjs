831f3f9dcc9d0983566ca5fbcf45c1f96cc4e1af
Miles Malerba <mmalerba@google.com>
2025-04-24 16:40:04 +0000

    9e3665a docs(docs-infra): fix github links for for previews (#60994)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.8+sha-5b0ec4a
+ * @license Angular v19.2.8+sha-9e3665a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.8+sha-5b0ec4a');
+const VERSION = new Version('19.2.8+sha-9e3665a');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
