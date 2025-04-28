7874cf3c2265b40bb55b57c6471553e46c451fc4
Miles Malerba <mmalerba@google.com>
2025-04-28 17:23:25 +0000

    7680a27 refactor(core): Ignore errors related to updating exhaustive check value (#61027)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.8+sha-d8204bf
+ * @license Angular v20.0.0-next.8+sha-7680a27
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.8+sha-d8204bf');
+const VERSION = new Version('20.0.0-next.8+sha-7680a27');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
