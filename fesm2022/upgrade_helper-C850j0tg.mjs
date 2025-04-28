ea8a45fa512420a9cb31c2f1a848880d0a3f53be
Miles Malerba <mmalerba@google.com>
2025-04-28 21:44:19 +0000

    9c4591c refactor(core): Update FakeNavigation to extract some Angular quirks (#60961)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.8+sha-2c9bdc3
+ * @license Angular v19.2.8+sha-9c4591c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.8+sha-2c9bdc3');
+const VERSION = new Version('19.2.8+sha-9c4591c');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
