15d9c977d629ece1f39ab3ae43b2c266891c94d3
Miles Malerba <mmalerba@google.com>
2025-04-29 17:15:49 +0000

    09cc394 build: update cross-repo angular dependencies (#61021)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.8+sha-888f9c2
+ * @license Angular v20.0.0-next.8+sha-09cc394
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.8+sha-888f9c2');
+const VERSION = new Version('20.0.0-next.8+sha-09cc394');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
