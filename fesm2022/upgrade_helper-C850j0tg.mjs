c7d0c32527ae907c6786613a2f272f9c10eeff67
Miles Malerba <mmalerba@google.com>
2025-04-29 19:25:53 +0000

    222ae18 build(devtools): cleanup dependencies (#60936)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.8+sha-46c1ade
+ * @license Angular v19.2.8+sha-222ae18
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.8+sha-46c1ade');
+const VERSION = new Version('19.2.8+sha-222ae18');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
