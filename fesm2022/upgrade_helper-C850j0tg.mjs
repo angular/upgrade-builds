6224cfbb3b77c94ed31ecbc29886c8cc87ac6a2d
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-04-30 10:29:15 +0000

    6b76bca Revert "build: Run browsers tests without `platform-browser-dynamic` (#60937)" (#61059)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.8+sha-946b844
+ * @license Angular v19.2.8+sha-6b76bca
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.8+sha-946b844');
+const VERSION = new Version('19.2.8+sha-6b76bca');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
