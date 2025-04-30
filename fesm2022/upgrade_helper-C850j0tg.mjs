23fbebccad30ff1328a8274f11f0a043e47b061c
Andrew Kushnir <akushnir@google.com>
2025-04-30 19:11:02 +0000

    400dbc5 fix(core): properly handle app stabilization with defer blocks (#61056)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.9+sha-a2eaea0
+ * @license Angular v19.2.9+sha-400dbc5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.9+sha-a2eaea0');
+const VERSION = new Version('19.2.9+sha-400dbc5');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
