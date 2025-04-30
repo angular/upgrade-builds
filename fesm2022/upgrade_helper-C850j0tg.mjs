60fb00545e82b9b40e52c6b4acfa92d0a900ee13
Miles Malerba <mmalerba@google.com>
2025-04-30 17:38:33 +0000

    829379e docs: release notes for the v19.2.9 release

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.8+sha-bb00b93
+ * @license Angular v20.0.0-next.8+sha-829379e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.8+sha-bb00b93');
+const VERSION = new Version('20.0.0-next.8+sha-829379e');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
