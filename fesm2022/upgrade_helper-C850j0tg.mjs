021d878d5cf7df8802c201affc14dc6e19f83d2a
Andrew Kushnir <akushnir@google.com>
2025-04-30 19:51:08 +0000

    59ca539 refactor(service-worker): drop error messages in production (#59702)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.9+sha-f4d60ff
+ * @license Angular v20.0.0-next.9+sha-59ca539
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.9+sha-f4d60ff');
+const VERSION = new Version('20.0.0-next.9+sha-59ca539');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
