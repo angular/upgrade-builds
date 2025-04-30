b7181080ebf932559de4f74c11a37e342472d175
Miles Malerba <mmalerba@google.com>
2025-04-30 15:33:41 +0000

    2bddce2 docs: update Angular CLI help [main] (#61048)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.8+sha-867f389
+ * @license Angular v20.0.0-next.8+sha-2bddce2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.8+sha-867f389');
+const VERSION = new Version('20.0.0-next.8+sha-2bddce2');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
