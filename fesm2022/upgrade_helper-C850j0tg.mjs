796c835f77657e5e1d11b3c45a21d8187444ea8f
Miles Malerba <mmalerba@google.com>
2025-04-30 05:21:07 +0000

    8d82a39 fix(core): async EventEmitter error should not prevent stability (#61028)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.8+sha-3261d14
+ * @license Angular v20.0.0-next.8+sha-8d82a39
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.8+sha-3261d14');
+const VERSION = new Version('20.0.0-next.8+sha-8d82a39');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
