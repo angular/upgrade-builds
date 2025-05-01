aaac872cc5d39839398a98a7980f977cbfb8dbfa
Andrew Kushnir <akushnir@google.com>
2025-05-01 17:19:55 +0000

    6bc9d45 refactor(core): drop computation error messages in production (#60700)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-d2cb0b9
+ * @license Angular v20.1.0-next.0+sha-6bc9d45
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-d2cb0b9');
+const VERSION = new Version('20.1.0-next.0+sha-6bc9d45');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
