21054734593ad4cd748b65ed368dd1873df02ef4
Andrew Kushnir <akushnir@google.com>
2025-05-01 18:50:02 +0000

    d4dc7cb refactor(zone.js): drop passive supported check (#58010)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.9+sha-181cc17
+ * @license Angular v20.0.0-next.9+sha-d4dc7cb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.9+sha-181cc17');
+const VERSION = new Version('20.0.0-next.9+sha-d4dc7cb');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
