3161636ed7a92eac6fd89719907f434ad9603ba1
Andrew Kushnir <akushnir@google.com>
2025-05-01 15:53:45 +0000

    d2cb0b9 refactor(core): tree-shake `getDirectiveDef` error (#61075)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-33a336f
+ * @license Angular v20.1.0-next.0+sha-d2cb0b9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-33a336f');
+const VERSION = new Version('20.1.0-next.0+sha-d2cb0b9');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
