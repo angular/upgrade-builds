f0d0776d77b097accfc3cd104e07a05347442cf1
Andrew Kushnir <akushnir@google.com>
2025-05-02 15:05:53 +0000

    782c6e1 docs: ViewContainerRef.createComponent jsdoc update (#61097)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.9+sha-bbdb166
+ * @license Angular v20.0.0-next.9+sha-782c6e1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.9+sha-bbdb166');
+const VERSION = new Version('20.0.0-next.9+sha-782c6e1');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
