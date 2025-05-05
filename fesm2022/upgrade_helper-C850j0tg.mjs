b7d2bc412544573f83c911cf81b16070a01d0a9a
Andrew Kushnir <akushnir@google.com>
2025-05-05 19:03:12 +0000

    63d10dc build: fix the lockfiles (#61127)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.9+sha-782c6e1
+ * @license Angular v20.0.0-next.9+sha-63d10dc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.9+sha-782c6e1');
+const VERSION = new Version('20.0.0-next.9+sha-63d10dc');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
