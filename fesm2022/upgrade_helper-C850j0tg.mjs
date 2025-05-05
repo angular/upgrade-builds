d0b5fa116f96089af734d8413a64ae180b4a8a16
Andrew Kushnir <akushnir@google.com>
2025-05-05 16:33:18 +0000

    ba5a3c0 build: update rules_angular digest to 3ba9d67 (#61124)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.9+sha-cc48424
+ * @license Angular v19.2.9+sha-ba5a3c0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.9+sha-cc48424');
+const VERSION = new Version('19.2.9+sha-ba5a3c0');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
