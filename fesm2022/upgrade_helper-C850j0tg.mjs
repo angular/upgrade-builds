8ee3c3281f29827e4b81f73da0bd5f0d67992e12
Andrew Kushnir <akushnir@google.com>
2025-05-05 21:45:37 +0000

    2c17145 refactor(compiler): element references not resolved when selectorless matcher is passed in (#61100)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-310e5ff
+ * @license Angular v20.1.0-next.0+sha-2c17145
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-310e5ff');
+const VERSION = new Version('20.1.0-next.0+sha-2c17145');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
