a7f563c339af776879dc2a5c93be4cdb6ec63700
Andrew Kushnir <akushnir@google.com>
2025-05-06 17:34:42 +0000

    267bc0e docs: fix whitespace after a link (#61136)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.9+sha-453b1d9
+ * @license Angular v19.2.9+sha-267bc0e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.9+sha-453b1d9');
+const VERSION = new Version('19.2.9+sha-267bc0e');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
