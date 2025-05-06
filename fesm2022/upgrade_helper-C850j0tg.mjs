47000d8122c3e12767d21752c39fa655d2442698
Andrew Kushnir <akushnir@google.com>
2025-05-06 21:20:56 +0000

    c081e04 docs: fix non-working link  (#61131)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.9+sha-e9e1c43
+ * @license Angular v19.2.9+sha-c081e04
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.9+sha-e9e1c43');
+const VERSION = new Version('19.2.9+sha-c081e04');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
