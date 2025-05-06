d4b1043ee84696ce81ed46207e4eec98f9e9a41a
Andrew Kushnir <akushnir@google.com>
2025-05-06 16:19:16 +0000

    453b1d9 build: update github/codeql-action action to v3.28.17 (#61134)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.9+sha-8772236
+ * @license Angular v19.2.9+sha-453b1d9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.9+sha-8772236');
+const VERSION = new Version('19.2.9+sha-453b1d9');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
