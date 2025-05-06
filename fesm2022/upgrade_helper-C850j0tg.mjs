c09858c422b1556db75adcf48cff44b5e54f82be
Andrew Kushnir <akushnir@google.com>
2025-05-06 20:52:58 +0000

    6cd5d8e fix(devtools): support defer blocks in IdentityTracker (#61139)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.9+sha-8898328
+ * @license Angular v20.0.0-next.9+sha-6cd5d8e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.9+sha-8898328');
+const VERSION = new Version('20.0.0-next.9+sha-6cd5d8e');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
