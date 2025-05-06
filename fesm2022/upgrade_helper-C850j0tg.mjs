3e26bb03d073056f60c8ed74d5a8c3be4e34666d
Andrew Kushnir <akushnir@google.com>
2025-05-06 18:13:54 +0000

    c39e799 Revert "docs(docs-infra): fix first-app intro example (#61042)" (#61145)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-c3ff01b
+ * @license Angular v20.1.0-next.0+sha-c39e799
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-c3ff01b');
+const VERSION = new Version('20.1.0-next.0+sha-c39e799');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
