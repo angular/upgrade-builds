f90828890c0f4cd6a2965688deeba48fb1a34859
Andrew Kushnir <akushnir@google.com>
2025-05-07 18:40:10 +0000

    9f43232 refactor(router): Avoid unnecessary href updates on navigations for routerlink (#60875)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-9678a30
+ * @license Angular v20.0.0-rc.0+sha-9f43232
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.0+sha-9678a30');
+const VERSION = new Version('20.0.0-rc.0+sha-9f43232');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
