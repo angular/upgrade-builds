094cea723461301925c9be6ccee2cff66a8d0eda
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-07 08:52:12 +0000

    803c94c ci: enable Renovate on `main` and `20.0.x` branches (#61160)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.9+sha-4b365c7
+ * @license Angular v20.0.0-next.9+sha-803c94c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.9+sha-4b365c7');
+const VERSION = new Version('20.0.0-next.9+sha-803c94c');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
