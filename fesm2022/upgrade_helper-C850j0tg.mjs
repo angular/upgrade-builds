57407ced8a9fa6615160778091fb081cfddb1f1c
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-08 14:40:03 +0000

    66753dc ci: use bash syntax for `CURRENT_BRANCH` in GitHub Actions (#61212)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-6f4307f
+ * @license Angular v20.1.0-next.0+sha-66753dc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-6f4307f');
+const VERSION = new Version('20.1.0-next.0+sha-66753dc');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
