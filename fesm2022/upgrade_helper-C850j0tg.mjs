663eeb3435071f4f1ac0b1eb3eaeeb4b74468b17
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-08 18:43:03 +0000

    7820f4e ci: use ` --force-with-lease`  when pushing to remote branch (#61222)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-5b60442
+ * @license Angular v19.2.10+sha-7820f4e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.10+sha-5b60442');
+const VERSION = new Version('19.2.10+sha-7820f4e');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
