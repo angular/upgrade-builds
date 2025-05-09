ae255cd4b77f7fb5c0ee15cc9a2c1eeffc239acf
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-09 14:33:41 +0000

    83c98ad ci: restore `.npmrc` before executing `postUpgradeTasks` (#61239)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-5cecf0d
+ * @license Angular v20.0.0-rc.0+sha-83c98ad
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.0+sha-5cecf0d');
+const VERSION = new Version('20.0.0-rc.0+sha-83c98ad');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
