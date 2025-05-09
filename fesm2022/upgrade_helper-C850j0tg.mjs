640a3f1802145cc47d5b3455a753aea9245ba449
Joey Perrott <josephperrott@gmail.com>
2025-05-09 16:53:06 +0000

    f0dd8f8 build: migrate upgrade package to use ts_project (#61245)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-84b66f3
+ * @license Angular v19.2.10+sha-f0dd8f8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.10+sha-84b66f3');
+const VERSION = new Version('19.2.10+sha-f0dd8f8');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
