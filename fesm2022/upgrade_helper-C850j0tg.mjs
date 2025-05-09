9c79ca1e50d8945e160a62375164041b6f3ae592
Joey Perrott <josephperrott@gmail.com>
2025-05-09 16:39:39 +0000

    345d3d8 build: migrate adev shared-docs package to use ts_project (#61218)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-69bfece
+ * @license Angular v20.0.0-rc.0+sha-345d3d8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.0+sha-69bfece');
+const VERSION = new Version('20.0.0-rc.0+sha-345d3d8');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
