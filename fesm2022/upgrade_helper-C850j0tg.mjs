e210a2ebd60108b0ed55abea78d98d0cafd5b090
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-13 22:34:42 +0000

    dd25f3d refactor(core): remove `USE_RUNTIME_DEPS_TRACKER_FOR_JIT` flag. (#61265)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-b4c383b
+ * @license Angular v20.1.0-next.0+sha-dd25f3d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-b4c383b');
+const VERSION = new Version('20.1.0-next.0+sha-dd25f3d');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
