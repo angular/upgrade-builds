99554ca028be6264a91546ca69151d9aa68da4a1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-13 09:04:21 +0000

    ed9a1d4 build: do not auto install peer dependencies (#61295)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-8d90626
+ * @license Angular v20.0.0-rc.0+sha-ed9a1d4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.0+sha-8d90626');
+const VERSION = new Version('20.0.0-rc.0+sha-ed9a1d4');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
