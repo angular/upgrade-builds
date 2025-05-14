6b903e29559f4887dd08f0e47594d90a75d89166
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-14 16:06:30 +0000

    f7bfdc7 refactor(router): add return types to exported functions (#61310)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-d7dbabe
+ * @license Angular v20.0.0-rc.0+sha-f7bfdc7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.0+sha-d7dbabe');
+const VERSION = new Version('20.0.0-rc.0+sha-f7bfdc7');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
