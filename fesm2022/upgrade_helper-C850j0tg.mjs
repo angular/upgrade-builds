5cf8fb28455b7cbe339a864cbbb1f8b90c74e024
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-14 13:44:53 +0000

    c028918 docs: document the use of `this` in template expression syntax (#61250)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-48dc810
+ * @license Angular v20.0.0-rc.0+sha-c028918
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.0+sha-48dc810');
+const VERSION = new Version('20.0.0-rc.0+sha-c028918');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
