b8a7bda2905681b2cab9f674f19062f0b615d91b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-15 17:22:19 +0000

    82e5449 refactor: replace port `4206` usage with `4209` (#61362)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-1191e62
+ * @license Angular v19.2.11+sha-82e5449
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-1191e62');
+const VERSION = new Version('19.2.11+sha-82e5449');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
