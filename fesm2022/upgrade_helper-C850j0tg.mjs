afedcfd6b8b36d02b97ff6427ad7d89d7c4b8333
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-15 17:34:26 +0000

    f699476 build: update dependency node to v22.15.1 (#61358)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-ca6295e
+ * @license Angular v20.0.0-rc.1+sha-f699476
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-ca6295e');
+const VERSION = new Version('20.0.0-rc.1+sha-f699476');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
