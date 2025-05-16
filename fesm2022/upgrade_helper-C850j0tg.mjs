49f7b570eccf20210976a788b056a55b052b6778
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 09:23:17 +0000

    85e0baf docs: fix Routed components example (#61278)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-8edafd0
+ * @license Angular v19.2.11+sha-85e0baf
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-8edafd0');
+const VERSION = new Version('19.2.11+sha-85e0baf');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
