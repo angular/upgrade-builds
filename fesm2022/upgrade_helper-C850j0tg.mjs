037ca93e35beb4f58d4387c403da741e77ed2f3b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 07:49:28 +0000

    bd2a5b9 docs: update Angular CDK apis [19.2.x] (#61347)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-82e5449
+ * @license Angular v19.2.11+sha-bd2a5b9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-82e5449');
+const VERSION = new Version('19.2.11+sha-bd2a5b9');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
