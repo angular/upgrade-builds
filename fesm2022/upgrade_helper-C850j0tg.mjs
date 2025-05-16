4bb774d431992fadec1309ef870236b53d806e7a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 14:07:16 +0000

    35a40cb build: fix golden approval script (#61407)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-dce4978
+ * @license Angular v20.1.0-next.0+sha-35a40cb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-dce4978');
+const VERSION = new Version('20.1.0-next.0+sha-35a40cb');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
