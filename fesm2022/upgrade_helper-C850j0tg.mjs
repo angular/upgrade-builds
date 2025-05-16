3b17ec07fd1b8d3044fe4582c336bb5883366465
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 09:45:18 +0000

    57df361 docs: fix incomplete sentence (#61391)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-85e0baf
+ * @license Angular v19.2.11+sha-57df361
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-85e0baf');
+const VERSION = new Version('19.2.11+sha-57df361');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
