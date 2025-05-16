16b88742e35f69c304049f1cac939c76132b1b48
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 08:52:45 +0000

    11589d4 ci: streamline adev CDK APIs and CLI help workflows (#61396)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-94f5a4b
+ * @license Angular v19.2.11+sha-11589d4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-94f5a4b');
+const VERSION = new Version('19.2.11+sha-11589d4');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
