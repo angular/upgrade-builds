943a2a01d2cdecb82e9b46581440e50b144bc2c2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 08:40:03 +0000

    333a0f3 ci: update step name in workflow (#61393)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-d3f667a
+ * @license Angular v20.1.0-next.0+sha-333a0f3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-d3f667a');
+const VERSION = new Version('20.1.0-next.0+sha-333a0f3');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
