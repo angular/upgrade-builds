759cc9371cfcb2d445469b71cb96a27bb5a1de02
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 10:28:25 +0000

    a90c720 docs: fix typos (#61257)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-f0e961c
+ * @license Angular v20.0.0-rc.1+sha-a90c720
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-f0e961c');
+const VERSION = new Version('20.0.0-rc.1+sha-a90c720');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
