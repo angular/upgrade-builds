7b68e64209c76b22c652028383bd6e785f36f517
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 09:14:53 +0000

    8edafd0 perf(platform-server): speed up resolution of base (#61392)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-11589d4
+ * @license Angular v19.2.11+sha-8edafd0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-11589d4');
+const VERSION = new Version('19.2.11+sha-8edafd0');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
