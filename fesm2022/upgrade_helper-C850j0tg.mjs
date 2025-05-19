9797ad97c9fe88e7db9d5031098ada8a03f84ca0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-19 11:13:55 +0000

    bd2b454 refactor(migrations): gracefully proceed if reference cannot be resolved (#61426)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-9aedd30
+ * @license Angular v19.2.11+sha-bd2b454
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-9aedd30');
+const VERSION = new Version('19.2.11+sha-bd2b454');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
