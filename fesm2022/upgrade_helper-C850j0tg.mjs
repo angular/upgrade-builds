c5e78a4ce11c2d75adc72c869d32ea554921f595
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-19 12:45:17 +0000

    0a25aba build: `--config=debug` should not disable sandbox for all actions (#61468)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-bd2b454
+ * @license Angular v19.2.11+sha-0a25aba
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-bd2b454');
+const VERSION = new Version('19.2.11+sha-0a25aba');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
