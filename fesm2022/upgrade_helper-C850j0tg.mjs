6ac06613e9e4644ac537ea1c2720a34d318d1252
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-19 16:12:49 +0000

    1360e4b docs: point cdk drag-drop doc links to adev api pages (#61437)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-0a25aba
+ * @license Angular v19.2.11+sha-1360e4b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-0a25aba');
+const VERSION = new Version('19.2.11+sha-1360e4b');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
