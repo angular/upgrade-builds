20f9ad54c6df6fa5f216a05171f45ef565ec1394
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 15:20:43 +0000

    efda872 fix(common): prevent reading chunks if app is destroyed (#61354)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-c0c69a5
+ * @license Angular v19.2.11+sha-efda872
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-c0c69a5');
+const VERSION = new Version('19.2.11+sha-efda872');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
