82e71222f68437476d7876b0a2d0f6fba14dcb32
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 12:49:25 +0000

    f6b4f72 Revert "test: replace `platform-browser-dynamic` with  `platform-browser` (#61495)" (#61510)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-4430423
+ * @license Angular v19.2.11+sha-f6b4f72
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-4430423');
+const VERSION = new Version('19.2.11+sha-f6b4f72');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
