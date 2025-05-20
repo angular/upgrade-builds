cdf92da1ee2c0c74b25de5a7fc098fcd3e7575e0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 14:35:49 +0000

    43588f3 docs(docs-infra): fix substitution regex (#61515)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-30e0812
+ * @license Angular v20.0.0-rc.1+sha-43588f3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-30e0812');
+const VERSION = new Version('20.0.0-rc.1+sha-43588f3');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
