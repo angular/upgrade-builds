5cb6ab01f4abd5067eda7269e8722fb619334700
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 10:10:38 +0000

    4430423 test: replace `platform-browser-dynamic` with  `platform-browser` (#61495)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-dbd66ec
+ * @license Angular v19.2.11+sha-4430423
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-dbd66ec');
+const VERSION = new Version('19.2.11+sha-4430423');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
