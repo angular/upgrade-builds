77e43d64034b78ceeed8cd87f0bb670f3d6f9d31
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 09:02:47 +0000

    dbd66ec refactor(devtools): switch default font to material symbols (#61281)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-1360e4b
+ * @license Angular v19.2.11+sha-dbd66ec
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-1360e4b');
+const VERSION = new Version('19.2.11+sha-dbd66ec');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
