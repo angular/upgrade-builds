2a5a044184683abec7a09866afe64c9e58995d1c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 13:48:53 +0000

    b641107 docs: replace material.a.io with material.a.dev (#61513)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-f6b4f72
+ * @license Angular v19.2.11+sha-b641107
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-f6b4f72');
+const VERSION = new Version('19.2.11+sha-b641107');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
