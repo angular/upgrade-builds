78e6cae7004a22188665efaf1d08a87cbe6a9180
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 12:39:27 +0000

    44bb328 fix(compiler): avoid conflicts between HMR code and local symbols (#61550)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-8f9b05e
+ * @license Angular v19.2.11+sha-44bb328
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-8f9b05e');
+const VERSION = new Version('19.2.11+sha-44bb328');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
