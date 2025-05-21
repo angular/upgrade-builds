170b73d16f876432290d16e9882c0cd7c7f72b51
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 11:23:39 +0000

    0104a69 ci: temporarily disable saucelabs for push jobs (#61559)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-e4943c6
+ * @license Angular v19.2.11+sha-0104a69
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-e4943c6');
+const VERSION = new Version('19.2.11+sha-0104a69');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
