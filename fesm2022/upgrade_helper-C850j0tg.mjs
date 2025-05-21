038d1277bdd05ac97317aba9afb7fa5c63c29904
Andrew Scott <atscott01@gmail.com>
2025-05-21 19:58:48 +0000

    ac76549 release: cut the v19.2.12 release

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-b6d7b4b
+ * @license Angular v19.2.12+sha-ac76549
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-b6d7b4b');
+const VERSION = new Version('19.2.12+sha-ac76549');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
