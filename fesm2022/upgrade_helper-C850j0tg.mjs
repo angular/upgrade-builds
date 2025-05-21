e1794386ed7f3ea9de45061289e53348f2fc4a69
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 12:52:52 +0000

    47f4a8e refactor(core): drop `rxResource` error messages (#61565)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-e74266b
+ * @license Angular v20.0.0-rc.1+sha-47f4a8e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-e74266b');
+const VERSION = new Version('20.0.0-rc.1+sha-47f4a8e');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
