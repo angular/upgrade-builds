6b204494cdd73b075eb5577aa6eb22083fd1e55a
Andrew Scott <atscott@google.com>
2025-05-21 19:14:45 +0000

    905194f fix(core): reading resource value after reload in the error state (#61441)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-bde1b5e
+ * @license Angular v20.1.0-next.0+sha-905194f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-bde1b5e');
+const VERSION = new Version('20.1.0-next.0+sha-905194f');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
