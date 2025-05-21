62ec11c9431fa25c103c864e6c95eacdbb1dd0b9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 13:52:06 +0000

    545bd3e docs: fix capitalization error (#61554) PR Close #61554

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-47f4a8e
+ * @license Angular v20.0.0-rc.1+sha-545bd3e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-47f4a8e');
+const VERSION = new Version('20.0.0-rc.1+sha-545bd3e');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
