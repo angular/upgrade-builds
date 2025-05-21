53063e95f23796b958bb48fed14f30774506ecf1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 09:14:36 +0000

    0a82e58 docs: fix info about `descendants`  for content queries. (#61534)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-3b564e7
+ * @license Angular v20.0.0-rc.1+sha-0a82e58
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-3b564e7');
+const VERSION = new Version('20.0.0-rc.1+sha-0a82e58');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
