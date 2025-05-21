14be36e3727ab89df9524eae12eb99b3ce3364e6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 10:17:34 +0000

    4e5d6de refactor(core): Merge R3TemplateRef implementation and TemplateRef interface (#61455)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-3cc0910
+ * @license Angular v20.0.0-rc.1+sha-4e5d6de
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-3cc0910');
+const VERSION = new Version('20.0.0-rc.1+sha-4e5d6de');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
