be6ac30677396a4a2b21f6ee87d9efaa1020ad2c
Andrew Scott <atscott@google.com>
2025-05-22 03:50:33 +0000

    1a3313a fix(zone.js): classes that extend Error should retain cause property (#61599)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.2+sha-e7cb313
+ * @license Angular v20.0.0-rc.2+sha-1a3313a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.2+sha-e7cb313');
+const VERSION = new Version('20.0.0-rc.2+sha-1a3313a');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
