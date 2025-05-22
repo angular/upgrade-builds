5e51660eaa52669f8a2cbb1735aa7b92d998325f
Andrew Scott <atscott@google.com>
2025-05-22 22:01:44 +0000

    e38cc41 release: cut the zone.js-0.15.1 release (#61632)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.12+sha-d4d4be8
+ * @license Angular v19.2.12+sha-e38cc41
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.12+sha-d4d4be8');
+const VERSION = new Version('19.2.12+sha-e38cc41');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
