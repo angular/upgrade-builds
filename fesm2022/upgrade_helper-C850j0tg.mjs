4b76678b122c8822fc45d046a030bcfefa5c8a95
Andrew Scott <atscott@google.com>
2025-05-22 22:42:50 +0000

    a6d5479 build: migrate platform-server to rules_js (#61619)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.12+sha-e38cc41
+ * @license Angular v19.2.12+sha-a6d5479
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.12+sha-e38cc41');
+const VERSION = new Version('19.2.12+sha-a6d5479');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
