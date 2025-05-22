7a1070743f10b685deb36ee3b134f4d42efbcd84
Andrew Scott <atscott@google.com>
2025-05-22 17:10:59 +0000

    959258d fix(service-worker): do not register service worker if app is destroyed before it is ready to register (#61101)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.2+sha-1a3313a
+ * @license Angular v20.0.0-rc.2+sha-959258d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.2+sha-1a3313a');
+const VERSION = new Version('20.0.0-rc.2+sha-959258d');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
