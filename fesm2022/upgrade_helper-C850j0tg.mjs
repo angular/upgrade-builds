7a316d317812f3a15265f3b853797b1e280a7a95
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-26 09:32:38 +0000

    556413d refactor(compiler-cli): remove hardcoded config for unused standalone imports rule enablement (#61622)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.2+sha-2e5362a
+ * @license Angular v20.0.0-rc.2+sha-556413d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.2+sha-2e5362a');
+const VERSION = new Version('20.0.0-rc.2+sha-556413d');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
