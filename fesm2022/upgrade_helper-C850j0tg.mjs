b9a1a1618d3f8c14effc2690be69ea4e62f23199
GitHub <noreply@github.com>
2025-05-28 17:44:14 +0000

    bf7816e build: bump peer deps for angular-in-memory-web-api (#61730)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-55306bd
+ * @license Angular v20.0.0+sha-bf7816e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-55306bd');
+const VERSION = new Version('20.0.0+sha-bf7816e');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
