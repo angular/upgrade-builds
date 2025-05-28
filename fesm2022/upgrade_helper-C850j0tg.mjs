7b0f308cce7dec8e046948995d92b0fdfa68a1fc
Andrew Scott <atscott@google.com>
2025-05-28 18:01:26 +0000

    449c540 release: bump version of in memory web api (#61731)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-d5ac380
+ * @license Angular v20.1.0-next.0+sha-449c540
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-d5ac380');
+const VERSION = new Version('20.1.0-next.0+sha-449c540');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
