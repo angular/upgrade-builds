df55e6d532734770dc657e334db9c26d5416c7bb
Andrew Scott <atscott01@gmail.com>
2025-05-28 14:00:48 +0000

    40e2b5b release: cut the v20.0.0 release

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.3+sha-0502cf2
+ * @license Angular v20.0.0+sha-40e2b5b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.3+sha-0502cf2');
+const VERSION = new Version('20.0.0+sha-40e2b5b');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
