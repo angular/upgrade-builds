4fc4586b8e9378573ee967f01bf57f20d6aba066
Andrew Scott <atscott@google.com>
2025-05-28 16:49:36 +0000

    8cf7e23 docs: update tutorials and playground to v20 (#61726)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-62be7fa
+ * @license Angular v20.1.0-next.0+sha-8cf7e23
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-62be7fa');
+const VERSION = new Version('20.1.0-next.0+sha-8cf7e23');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
