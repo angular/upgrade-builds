81d24a5ac439687201a81164782364504ba6e644
Andrew Scott <atscott@google.com>
2025-05-28 19:35:48 +0000

    a0d599e docs: link to v20 blog post in changelog (#61734)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-aed8a77
+ * @license Angular v20.0.0+sha-a0d599e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-aed8a77');
+const VERSION = new Version('20.0.0+sha-a0d599e');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
