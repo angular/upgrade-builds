f07472565546d9264eb07d5d292d0b928655ce5f
kirjs <kirts@google.com>
2025-06-03 18:07:41 +0000

    939ca07 docs(docs-infra): fix tutorial tooltip (#61782)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-b5d21b0
+ * @license Angular v20.0.0+sha-939ca07
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-b5d21b0');
+const VERSION = new Version('20.0.0+sha-939ca07');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-b5d21b0
+ * @license Angular v20.0.0+sha-939ca07
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
