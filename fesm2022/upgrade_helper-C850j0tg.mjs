7292487521ce5c9835d4f56ae82eed4782d38115
kirjs <kirts@google.com>
2025-06-03 20:04:56 +0000

    6adf022 fix(service-worker): prevent duplicate fetches during concurrent update checks (#61443)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-c67dbda
+ * @license Angular v20.1.0-next.0+sha-6adf022
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-c67dbda');
+const VERSION = new Version('20.1.0-next.0+sha-6adf022');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
