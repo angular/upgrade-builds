7c1d55701a9c11b094004bacbdcfd5559cb6858e
kirjs <kirts@google.com>
2025-06-03 11:23:40 +0000

    0ea831c refactor(migrations): keep the control flow migration as `ng generate`. (#61773)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-32230e6
+ * @license Angular v20.0.0+sha-0ea831c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-32230e6');
+const VERSION = new Version('20.0.0+sha-0ea831c');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
