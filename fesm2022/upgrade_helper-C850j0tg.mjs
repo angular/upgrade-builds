b1a82753e79afd008eacc9cec45b431b02995300
kirjs <kirts@google.com>
2025-06-04 13:46:50 +0000

    5c31e7e fix(core): unregister `onDestroy` when observable errors in `toSignal` (#61596)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-eb62a58
+ * @license Angular v20.0.0+sha-5c31e7e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-eb62a58');
+const VERSION = new Version('20.0.0+sha-5c31e7e');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
