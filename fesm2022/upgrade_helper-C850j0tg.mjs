61eadbf7ff35ca23288180c4f55af9713dde8b38
kirjs <kirts@google.com>
2025-06-04 18:33:03 +0000

    ddd22be fix(core): unregister `onDestroy` in `ResourceImpl` when `destroy()` is called (#61870)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-bdf882c
+ * @license Angular v20.0.0+sha-ddd22be
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-bdf882c');
+const VERSION = new Version('20.0.0+sha-ddd22be');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
