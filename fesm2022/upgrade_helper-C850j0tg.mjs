fe6c6f907096ae85d1f6d365e45d5b1067b217cc
kirjs <kirts@google.com>
2025-06-04 00:28:45 +0000

    2bf7249 docs: update resource docs (#61846)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-b93fa22
+ * @license Angular v20.0.0+sha-2bf7249
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-b93fa22');
+const VERSION = new Version('20.0.0+sha-2bf7249');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
