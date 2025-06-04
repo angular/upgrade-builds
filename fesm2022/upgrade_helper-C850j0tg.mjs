b2ec28cdca0f683b2e354330b56f9cfd6be9e646
kirjs <kirts@google.com>
2025-06-04 00:42:13 +0000

    3a9a70d refactor(compiler-cli): implement transform to determine debugName from signal functions (#57348)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-e5d9ea2
+ * @license Angular v20.1.0-next.0+sha-3a9a70d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-e5d9ea2');
+const VERSION = new Version('20.1.0-next.0+sha-3a9a70d');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
