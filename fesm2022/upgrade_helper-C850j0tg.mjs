e8d015ea9a28950da9f0acd5fa67e106e651f4a9
Andrew Kushnir <akushnir@google.com>
2025-06-05 16:21:28 +0000

    8ccd496 build: remove unnecessary zone.js dep from various build targets (#61903)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.1+sha-1e8158b
+ * @license Angular v20.0.1+sha-8ccd496
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.1+sha-1e8158b');
+const VERSION = new Version('20.0.1+sha-8ccd496');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
