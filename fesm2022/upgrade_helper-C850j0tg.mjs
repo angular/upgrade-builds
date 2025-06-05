24fe05e6454aa7145590636705f4df3e10399050
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-05 11:09:52 +0000

    f562fd4 refactor(core): export signal setter and updater types for wiz (#61714)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.1+sha-ccdbdf7
+ * @license Angular v20.0.1+sha-f562fd4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.1+sha-ccdbdf7');
+const VERSION = new Version('20.0.1+sha-f562fd4');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
