036011dabca28c21fcf075b0aecd72845a370eab
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-05 11:42:18 +0000

    ddfb94c ci: atomatically add target labels based on base branch (#61894)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-935ce0e
+ * @license Angular v20.1.0-next.0+sha-ddfb94c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-935ce0e');
+const VERSION = new Version('20.1.0-next.0+sha-ddfb94c');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
