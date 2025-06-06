68014cb0c8cd04918de6ef225874d5e973994602
Andrew Kushnir <akushnir@google.com>
2025-06-06 00:54:32 +0000

    cfb0dc2 fix(docs-infra): Fix import name for house location (#61914)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-9354efc
+ * @license Angular v20.1.0-next.0+sha-cfb0dc2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-9354efc');
+const VERSION = new Version('20.1.0-next.0+sha-cfb0dc2');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
