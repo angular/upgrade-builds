ac31b5abaeaec076279617c2aeebb9e902cb791a
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-06-06 16:33:48 +0000

    321c8a3 release: cut the v20.0.2 release

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.1+sha-bb6ec10
+ * @license Angular v20.0.2+sha-321c8a3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.1+sha-bb6ec10');
+const VERSION = new Version('20.0.2+sha-321c8a3');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
