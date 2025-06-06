1a8f4e3cbbc092b8c986f996e2651cae77370b8d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-06 11:28:20 +0000

    8ab0028 docs: update first-app tutorial to signal inputs (#61876)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-e01b4c2
+ * @license Angular v20.1.0-next.0+sha-8ab0028
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-e01b4c2');
+const VERSION = new Version('20.1.0-next.0+sha-8ab0028');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
