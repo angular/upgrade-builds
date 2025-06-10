2c11fe0a9d979fbfcda3d6f97dc76605396a7b67
Andrew Kushnir <akushnir@google.com>
2025-06-10 21:51:21 +0000

    9d6db41 docs: fix incorrect destructuring in resource loader example in signals guide (#61988)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.2+sha-135c594
+ * @license Angular v20.0.2+sha-9d6db41
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.2+sha-135c594');
+const VERSION = new Version('20.0.2+sha-9d6db41');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
