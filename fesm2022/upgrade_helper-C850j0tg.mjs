2002c982c5183c3e8ee391e3027d45c66cff917a
Andrew Kushnir <akushnir@google.com>
2025-06-10 21:51:49 +0000

    b9141da refactor(devtools): use import type to improve performance (#61966)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-4f0221e
+ * @license Angular v20.1.0-next.0+sha-b9141da
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-4f0221e');
+const VERSION = new Version('20.1.0-next.0+sha-b9141da');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
