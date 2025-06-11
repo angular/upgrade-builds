9d3bb6a66e286aa67669533f0eab1fc52fc07b6a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-11 09:47:07 +0000

    06979c6 docs: navigate-to-routes.md to include RouterLink in imports. (#61965)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.2+sha-e73f248
+ * @license Angular v20.0.2+sha-06979c6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.2+sha-e73f248');
+const VERSION = new Version('20.0.2+sha-06979c6');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
