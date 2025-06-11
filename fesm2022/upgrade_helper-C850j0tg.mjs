c8b1608d9b265d7962c69b001143ce2161eea8e3
Andrew Kushnir <akushnir@google.com>
2025-06-11 00:39:49 +0000

    229c6b4 build: update dependency typescript to v5.8.3 (#61958)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.2+sha-eef6faf
+ * @license Angular v20.0.2+sha-229c6b4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.2+sha-eef6faf');
+const VERSION = new Version('20.0.2+sha-229c6b4');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
