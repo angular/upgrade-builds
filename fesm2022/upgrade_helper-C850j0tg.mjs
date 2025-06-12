4ed527fe0b7ac129eabe9569021463176578d4a2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-12 11:52:25 +0000

    b24d540 build: update dependency google-closure-compiler to v20250609 (#62009)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.3+sha-e343cdf
+ * @license Angular v20.0.3+sha-b24d540
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.3+sha-e343cdf');
+const VERSION = new Version('20.0.3+sha-b24d540');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
