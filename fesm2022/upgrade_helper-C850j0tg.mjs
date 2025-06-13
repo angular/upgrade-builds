350a40b4c34bad4d223cd26dfa751c2f9abebb99
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-13 10:45:05 +0000

    9b33089 docs: remove developer preview hint for withI18nSupport (#62039)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.3+sha-67f657e
+ * @license Angular v20.0.3+sha-9b33089
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.3+sha-67f657e');
+const VERSION = new Version('20.0.3+sha-9b33089');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
