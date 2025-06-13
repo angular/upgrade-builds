109d7d0529d5a9be5edb69dec93f45d066f4d65a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-13 10:46:50 +0000

    be2747e fix(devtools): errors caused by the injector tree (#62029)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.1+sha-31da435
+ * @license Angular v20.1.0-next.1+sha-be2747e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.1+sha-31da435');
+const VERSION = new Version('20.1.0-next.1+sha-be2747e');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
