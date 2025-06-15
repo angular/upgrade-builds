da00db4a94d861089f47e229088a7d1c7f985a37
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-15 10:52:02 +0000

    f69ec36 refactor(localize): add tripleslash type of Application builder (#62037)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.3+sha-9b33089
+ * @license Angular v20.0.3+sha-f69ec36
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.3+sha-9b33089');
+const VERSION = new Version('20.0.3+sha-f69ec36');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
