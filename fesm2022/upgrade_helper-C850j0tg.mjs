4288d9911d4713588bcb75bc881647e40514a5c4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-16 09:40:17 +0000

    deaa292 build: lock file maintenance (#62063)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.3+sha-8403bc7
+ * @license Angular v20.0.3+sha-deaa292
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.3+sha-8403bc7');
+const VERSION = new Version('20.0.3+sha-deaa292');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
