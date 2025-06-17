6105e0f1e1e10fade83bcba516c0c3141a654101
Joey Perrott <josephperrott@gmail.com>
2025-06-17 23:54:09 +0000

    04e0d4f build: update the ts_integrity expected for the typescript version in WORKSPACE (#62109)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.3+sha-1c73f54
+ * @license Angular v20.0.3+sha-04e0d4f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.3+sha-1c73f54');
+const VERSION = new Version('20.0.3+sha-04e0d4f');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
