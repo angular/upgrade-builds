e80c9202216ce89384ec89c4ff57d45ba5b3b4ba
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-17 09:04:04 +0000

    132fd3c refactor(devtools): reorganize profiler components (#62073)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.1+sha-1182fe7
+ * @license Angular v20.1.0-next.1+sha-132fd3c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.1+sha-1182fe7');
+const VERSION = new Version('20.1.0-next.1+sha-132fd3c');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
