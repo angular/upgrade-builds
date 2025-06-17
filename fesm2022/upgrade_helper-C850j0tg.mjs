c07a9ca282efa22c3e285e642b73cf631a77f46f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-17 09:31:37 +0000

    fb56d51 docs(router): improve discoverability of route guard section (#62060)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.3+sha-734c13a
+ * @license Angular v20.0.3+sha-fb56d51
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.3+sha-734c13a');
+const VERSION = new Version('20.0.3+sha-fb56d51');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
