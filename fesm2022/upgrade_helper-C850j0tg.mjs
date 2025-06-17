c62155c75a46b0932bf7557ec270fa774686d0f7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-17 17:03:29 +0000

    a6c5742 build: migrate back to using the @npm// sourced benchmark_test macro (#62101)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.3+sha-fb56d51
+ * @license Angular v20.0.3+sha-a6c5742
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.3+sha-fb56d51');
+const VERSION = new Version('20.0.3+sha-a6c5742');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
