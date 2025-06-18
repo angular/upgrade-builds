583d3cb7e7f40c646136550e7ea8c69f2da87352
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-06-18 21:05:06 +0000

    127120a docs: update best-practices prompt styling (#62128)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-6a4fdba
+ * @license Angular v20.0.4+sha-127120a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.4+sha-6a4fdba');
+const VERSION = new Version('20.0.4+sha-127120a');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
