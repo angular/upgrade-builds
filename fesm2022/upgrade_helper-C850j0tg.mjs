c82c4c50baf191c446c38ca9dce7918635006275
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-18 07:54:17 +0000

    2e1e9aa docs: remove duplicate entry for tagged template literals (#62082)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.3+sha-24b4484
+ * @license Angular v20.0.3+sha-2e1e9aa
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.3+sha-24b4484');
+const VERSION = new Version('20.0.3+sha-2e1e9aa');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
