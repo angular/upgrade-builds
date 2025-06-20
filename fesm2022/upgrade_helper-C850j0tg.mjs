7c4ebd6124ab370c7db3c72299b972b42e71a606
Andrew Kushnir <akushnir@google.com>
2025-06-20 21:55:19 +0000

    9c41449 build: update cross-repo angular dependencies (#62163)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-2020005
+ * @license Angular v20.0.4+sha-9c41449
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.4+sha-2020005');
+const VERSION = new Version('20.0.4+sha-9c41449');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
