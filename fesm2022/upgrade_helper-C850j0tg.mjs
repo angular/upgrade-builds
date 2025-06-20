543360dea4918e9b85bab75cb9cc3b152439b38d
Andrew Kushnir <akushnir@google.com>
2025-06-20 19:20:10 +0000

    f8a90c9 build: update dependency rollup to v4.44.0 (#62144)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.2+sha-3ecfa9c
+ * @license Angular v20.1.0-next.2+sha-f8a90c9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.2+sha-3ecfa9c');
+const VERSION = new Version('20.1.0-next.2+sha-f8a90c9');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
