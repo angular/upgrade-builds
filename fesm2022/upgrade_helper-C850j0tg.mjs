ed16427da70cbd184d6f372c0d7b384df8da1a68
Andrew Kushnir <akushnir@google.com>
2025-06-21 01:37:46 +0000

    35ce544 docs: fix link to http-resource guide (#62180)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-2ae2076
+ * @license Angular v20.0.4+sha-35ce544
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.4+sha-2ae2076');
+const VERSION = new Version('20.0.4+sha-35ce544');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
