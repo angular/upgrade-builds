dcb31e1ce7b07b1746c2becc5a72e204af68f75e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-24 15:16:00 +0000

    9d20d6f Revert "test(devtools): revive cypress tests (#61972)" (#62243)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-a42169a
+ * @license Angular v20.0.4+sha-9d20d6f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.4+sha-a42169a');
+const VERSION = new Version('20.0.4+sha-9d20d6f');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
