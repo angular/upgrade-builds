3348e25ef71ac1d5aa173decd2282cf528e9a27e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-24 12:36:51 +0000

    65c59dd fix(router): handle scrollRestoration error in restricted environments (#62186)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-9b01773
+ * @license Angular v20.0.4+sha-65c59dd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.4+sha-9b01773');
+const VERSION = new Version('20.0.4+sha-65c59dd');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
