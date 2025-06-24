e416054622b8d83efb690d1a2cc354fa992e0e17
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-24 10:58:54 +0000

    4f1a5ab build: move dependencies on npm package of dev-infra to workspace dependency (#62083)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-76dbd72
+ * @license Angular v20.0.4+sha-4f1a5ab
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.4+sha-76dbd72');
+const VERSION = new Version('20.0.4+sha-4f1a5ab');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
