0044712f896c95f4798f9b494db9c26622108b12
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-24 09:04:58 +0000

    1cff7cf build: update cross-repo angular dependencies to db18d71 (#62199)

--- fesm2022/upgrade_helper-C850j0tg.mjs
+++ fesm2022/upgrade_helper-C850j0tg.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.2+sha-8ce6ca9
+ * @license Angular v20.1.0-next.2+sha-1cff7cf
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.2+sha-8ce6ca9');
+const VERSION = new Version('20.1.0-next.2+sha-1cff7cf');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
