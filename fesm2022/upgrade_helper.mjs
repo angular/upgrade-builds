77fe83e3af9c0c4ceecb7ade4448dffd50c15b66
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-24 11:02:10 +0000

    8d049f5 build: update jest monorepo to v30 (#62234)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.2+sha-07df4e1
+ * @license Angular v20.1.0-next.2+sha-8d049f5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.2+sha-07df4e1');
+const VERSION = new Version('20.1.0-next.2+sha-8d049f5');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
