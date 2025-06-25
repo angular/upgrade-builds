37d736ed059d9a59d28475ea5d4cf067ba1c15a0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-25 12:58:56 +0000

    17a8ca3 ci: Update pullapprove ownership for compiler and zonejs (#62261)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-c43f632
+ * @license Angular v20.0.4+sha-17a8ca3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.4+sha-c43f632');
+const VERSION = new Version('20.0.4+sha-17a8ca3');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
