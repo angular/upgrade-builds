1afd48dc2dc2568233be7ada3fff6892e8aa7c93
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-26 10:36:57 +0000

    92ecd69 build: update dependency rollup to v4.44.1 (#62286)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-029043c
+ * @license Angular v20.0.5+sha-92ecd69
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.5+sha-029043c');
+const VERSION = new Version('20.0.5+sha-92ecd69');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
