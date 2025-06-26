90066c88ac6bda6b249060ac51cec9d81569dc3f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-26 08:32:25 +0000

    c5c9b0b build: update rules_browsers digest to 0952071 (#62279)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-85fe323
+ * @license Angular v20.1.0-next.3+sha-c5c9b0b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.3+sha-85fe323');
+const VERSION = new Version('20.1.0-next.3+sha-c5c9b0b');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
