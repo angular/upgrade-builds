43b0971dc3daf741e65291a1109a0808fd525655
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-26 09:10:55 +0000

    19eaf71 docs: Update documentation httpResource with keepalive feature (#62278)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-c39b80f
+ * @license Angular v20.0.5+sha-19eaf71
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.5+sha-c39b80f');
+const VERSION = new Version('20.0.5+sha-19eaf71');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
