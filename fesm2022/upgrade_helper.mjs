7ed1398814279eff380af183199a97dc78637810
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-26 12:15:35 +0000

    a3c246a refactor(common): remove redundant `ɵloadImpl` check in client bundles (#62191)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-8b861e2
+ * @license Angular v20.0.5+sha-a3c246a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.5+sha-8b861e2');
+const VERSION = new Version('20.0.5+sha-a3c246a');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
