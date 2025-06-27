dbae1100a8fa241f81d78ad3bf1cd01c0b0b9ed3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 13:00:09 +0000

    6ec2900 docs: Update documentation httpResource with cache & priority feature (#62334)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-7077520
+ * @license Angular v20.1.0-next.3+sha-6ec2900
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.3+sha-7077520');
+const VERSION = new Version('20.1.0-next.3+sha-6ec2900');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
