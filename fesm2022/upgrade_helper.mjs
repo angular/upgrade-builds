687c4ba4772b1b265deb13003f7aa4dacbac7f88
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 13:27:33 +0000

    f10e1bc fix(devtools): profiler records only the first frames from the stream (#62335)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-f943505
+ * @license Angular v20.1.0-next.3+sha-f10e1bc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.3+sha-f943505');
+const VERSION = new Version('20.1.0-next.3+sha-f10e1bc');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
