84bc7f5424dd2b6b4d58808f0cbf2bd34dc72c2d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 11:32:28 +0000

    82cdd5c build: update github/codeql-action action to v3.29.1 (#62332)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-74004f6
+ * @license Angular v20.0.5+sha-82cdd5c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.5+sha-74004f6');
+const VERSION = new Version('20.0.5+sha-82cdd5c');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
