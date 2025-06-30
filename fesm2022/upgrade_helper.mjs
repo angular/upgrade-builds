e7f495ed5b10e913c107fd2969e775aded63f848
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-30 08:13:53 +0000

    e1856da docs: update router read-route-state.md file (#62367)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-08e2d9d
+ * @license Angular v20.1.0-next.3+sha-e1856da
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.3+sha-08e2d9d');
+const VERSION = new Version('20.1.0-next.3+sha-e1856da');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
