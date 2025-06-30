6114a404f4ce97af2e5c688d6dd3115aae794dea
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-30 12:15:11 +0000

    35032a6 refactor(core): Make it so that injecting with just a token guarantees an instance of the type provided. (#62338)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-f0965c7
+ * @license Angular v20.1.0-next.3+sha-35032a6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.3+sha-f0965c7');
+const VERSION = new Version('20.1.0-next.3+sha-35032a6');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
