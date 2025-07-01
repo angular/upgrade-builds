12d7b66f7769e62330efaf397ccbacff838a2f8f
Andrew Kushnir <akushnir@google.com>
2025-07-01 19:29:08 +0000

    fb1bc99 ci: fix incorrect base branch in renovate config (#62406)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-4ac0147
+ * @license Angular v20.2.0-next.0+sha-fb1bc99
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-4ac0147');
+const VERSION = new Version('20.2.0-next.0+sha-fb1bc99');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
