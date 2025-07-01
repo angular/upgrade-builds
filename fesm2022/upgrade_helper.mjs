e775c8041045510c44f0a0538f1a827b6c4e5052
Andrew Kushnir <akushnir@google.com>
2025-07-01 21:02:57 +0000

    40214ac release: bump Angular DevTools version to 1.0.37 (#62409)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-fb1bc99
+ * @license Angular v20.2.0-next.0+sha-40214ac
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-fb1bc99');
+const VERSION = new Version('20.2.0-next.0+sha-40214ac');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
