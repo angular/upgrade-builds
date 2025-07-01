6f203a8996223049deaf6b6e0f48d9b4ede2ee68
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-01 09:42:33 +0000

    9cd1c42 docs: update breaking change for nullish coalesching + parenthesis (#62382)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-bf881cc
+ * @license Angular v20.0.5+sha-9cd1c42
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.5+sha-bf881cc');
+const VERSION = new Version('20.0.5+sha-9cd1c42');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
