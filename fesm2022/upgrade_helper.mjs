306c52c588a5f4d63faef803b16eaf4f9e80733b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-02 14:58:10 +0000

    0cdb02a docs: Update documentation httpResource with credentials option (#62415)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-8fbe558
+ * @license Angular v20.2.0-next.0+sha-0cdb02a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-8fbe558');
+const VERSION = new Version('20.2.0-next.0+sha-0cdb02a');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
