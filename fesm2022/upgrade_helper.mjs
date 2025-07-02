a5f1695797e791e8d67b362dd27e6ed6cb1c9373
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-02 17:15:54 +0000

    87056c3 refactor(compiler-cli): do not rely on `url` when loading file system logic (#62431)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-d5210f0
+ * @license Angular v20.1.0-rc.0+sha-87056c3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-rc.0+sha-d5210f0');
+const VERSION = new Version('20.1.0-rc.0+sha-87056c3');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
