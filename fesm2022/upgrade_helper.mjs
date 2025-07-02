0e387eb6cb116d535f6d3d9fe35d24d28ef5beea
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-02 07:58:17 +0000

    f9abb55 Revert "fix(service-worker): update service worker to handle seeking better for videos (#60029)" (#62422)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-40214ac
+ * @license Angular v20.2.0-next.0+sha-f9abb55
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-40214ac');
+const VERSION = new Version('20.2.0-next.0+sha-f9abb55');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
