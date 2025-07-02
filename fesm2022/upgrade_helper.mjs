f3476d4a8144b1cc07aae107fba596b0b35cb248
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-02 16:19:29 +0000

    d1d131a build: migrate devtools to use packaged version of @angular/* packages (#62413)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-42a3dbb
+ * @license Angular v20.1.0-rc.0+sha-d1d131a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-rc.0+sha-42a3dbb');
+const VERSION = new Version('20.1.0-rc.0+sha-d1d131a');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
