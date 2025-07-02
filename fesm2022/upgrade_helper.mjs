0383c68f85387fb3a34272869e6ebe8e797ef252
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-02 17:06:17 +0000

    4c332c3 docs: fix formatting (#62400)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.6+sha-e0e78fe
+ * @license Angular v20.0.6+sha-4c332c3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.6+sha-e0e78fe');
+const VERSION = new Version('20.0.6+sha-4c332c3');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
