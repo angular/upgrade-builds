905052ecd9e475d6f4b9c8d0c2003c6933b37a83
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-03 14:54:52 +0000

    9ed9375 refactor(devtools): remove `version_name` from `manifest.json` (#62351)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-5c54cb8
+ * @license Angular v20.2.0-next.0+sha-9ed9375
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-5c54cb8');
+const VERSION = new Version('20.2.0-next.0+sha-9ed9375');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
