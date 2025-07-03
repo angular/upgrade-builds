0f2c848101a76eb1c542fdbac024a65542f4aa4d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-03 09:46:03 +0000

    fca058a refactor(migrations): ensure tsurge can properly emit references in g3 (#62447)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-f5f80d8
+ * @license Angular v20.1.0-rc.0+sha-fca058a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-rc.0+sha-f5f80d8');
+const VERSION = new Version('20.1.0-rc.0+sha-fca058a');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
