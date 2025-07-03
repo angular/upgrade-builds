6e493fa931893f43134f6e664d84dd08437e06f1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-03 09:24:08 +0000

    ade8ddd ci: update pullapprove reviewers (#62448)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.6+sha-5901a23
+ * @license Angular v20.0.6+sha-ade8ddd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.6+sha-5901a23');
+const VERSION = new Version('20.0.6+sha-ade8ddd');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
