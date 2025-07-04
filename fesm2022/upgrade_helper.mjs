e0f1f1ea3264ff4879e2122f147d24f11cc9072a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-04 07:30:50 +0000

    720717e docs: Update documentation http with integrity & referrer (#62462)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-9ed9375
+ * @license Angular v20.2.0-next.0+sha-720717e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-9ed9375');
+const VERSION = new Version('20.2.0-next.0+sha-720717e');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
