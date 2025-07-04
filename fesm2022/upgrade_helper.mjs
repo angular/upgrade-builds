725bba75591081c8d41bc4efda88ca27c1ccbbbd
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-04 08:56:30 +0000

    9ba5903 refactor(devtools): remove the standalone demo (#62455)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-720717e
+ * @license Angular v20.2.0-next.0+sha-9ba5903
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-720717e');
+const VERSION = new Version('20.2.0-next.0+sha-9ba5903');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
