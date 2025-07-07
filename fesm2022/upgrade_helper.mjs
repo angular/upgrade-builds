23c3f6cd6eeeca05c404ba1aa9575dbf3b44c38f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-07 15:44:05 +0000

    c6a720b build: update dependency aspect_rules_ts to v3.6.2 (#62475)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-bd6d043
+ * @license Angular v20.2.0-next.0+sha-c6a720b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-bd6d043');
+const VERSION = new Version('20.2.0-next.0+sha-c6a720b');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
