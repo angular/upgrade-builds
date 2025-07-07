2500c41c0a7c2f60932d45fd2aab71b5ce3f4757
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-07 16:23:56 +0000

    633d01b refactor(compiler): ensure compiler passes closure conformance checks (#62500)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-c6a720b
+ * @license Angular v20.2.0-next.0+sha-633d01b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-c6a720b');
+const VERSION = new Version('20.2.0-next.0+sha-633d01b');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
