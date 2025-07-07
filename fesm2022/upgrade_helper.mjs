5e6f7f53ddb5e8a9e0227c63ba27b791c2edc660
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-07-07 11:08:18 +0000

    d774314 refactor(compiler-cli): ensure mock file system can be loaded in the browser (#62493)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-da99b9a
+ * @license Angular v20.1.0-rc.0+sha-d774314
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-rc.0+sha-da99b9a');
+const VERSION = new Version('20.1.0-rc.0+sha-d774314');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
