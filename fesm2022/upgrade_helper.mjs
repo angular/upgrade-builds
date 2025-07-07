6dfc652722c501aa47e75fcf32e4c53ea6b2c725
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-07 17:01:39 +0000

    fec1206 docs: update comment (#62482) PR Close #62482

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-98ecf80
+ * @license Angular v20.1.0-rc.0+sha-fec1206
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-rc.0+sha-98ecf80');
+const VERSION = new Version('20.1.0-rc.0+sha-fec1206');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
