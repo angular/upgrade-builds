db5b4c4314c0a169475113d899bd19695ef11301
Andrew Kushnir <akushnir@google.com>
2025-07-08 20:36:34 +0000

    365ca13 test: rename legacy animations integration tests (#62527)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-6953166
+ * @license Angular v20.2.0-next.0+sha-365ca13
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-6953166');
+const VERSION = new Version('20.2.0-next.0+sha-365ca13');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
