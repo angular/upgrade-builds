7906e702c305da2b0c910a243ed032e58dfc831b
Andrew Kushnir <akushnir@google.com>
2025-07-09 19:53:18 +0000

    75a5d08 fix(http): do not display warnings `Angular detected that a `HttpClient` request with the `keepalive` option was sent using XHR` when option is not true (#62536)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-1b895e1
+ * @license Angular v20.2.0-next.0+sha-75a5d08
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-1b895e1');
+const VERSION = new Version('20.2.0-next.0+sha-75a5d08');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
