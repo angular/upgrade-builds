627f282fe34a38a43b321bac81ed33d3ef5a4d01
Andrew Kushnir <akushnir@google.com>
2025-07-09 21:12:10 +0000

    6274ea7 docs: update Angular CLI help (#62519)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0+sha-276836e
+ * @license Angular v20.1.0+sha-6274ea7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0+sha-276836e');
+const VERSION = new Version('20.1.0+sha-6274ea7');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
