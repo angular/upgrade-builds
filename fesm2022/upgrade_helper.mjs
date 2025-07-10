cfd127f26f78349083a2f83d830c462447a840ab
Andrew Kushnir <akushnir@google.com>
2025-07-10 20:00:46 +0000

    6241957 ci: adjust renovate teams (#62578)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-96014b5
+ * @license Angular v20.2.0-next.0+sha-6241957
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-96014b5');
+const VERSION = new Version('20.2.0-next.0+sha-6241957');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
