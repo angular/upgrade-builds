219559977e6e2bd029f4189692282b1a1075d02e
Andrew Kushnir <akushnir@google.com>
2025-07-10 17:46:23 +0000

    6549418 refactor(core): include DI path into cyclic dependency error message (#50902)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0+sha-66a85ab
+ * @license Angular v20.1.0+sha-6549418
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0+sha-66a85ab');
+const VERSION = new Version('20.1.0+sha-6549418');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
