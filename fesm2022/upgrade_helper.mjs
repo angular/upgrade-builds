87ec490776280da26c8479286ac8e927a9fe4653
Andrew Kushnir <akushnir@google.com>
2025-07-10 20:53:07 +0000

    4f1e88e build: use esbuild from aspect rules (#62568)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0+sha-aea0a70
+ * @license Angular v20.1.0+sha-4f1e88e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0+sha-aea0a70');
+const VERSION = new Version('20.1.0+sha-4f1e88e');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
