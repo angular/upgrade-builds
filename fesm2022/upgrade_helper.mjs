34c17f7e0439fc4edc519c2c035f04329380c644
Andrew Kushnir <akushnir@google.com>
2025-07-11 17:53:26 +0000

    bece163 ci: add to public-api approvers list (#62591)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-15bb258
+ * @license Angular v20.2.0-next.0+sha-bece163
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-15bb258');
+const VERSION = new Version('20.2.0-next.0+sha-bece163');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
