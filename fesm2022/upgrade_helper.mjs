7b5a2de829a26c13182e18b32963a8cadbb8e4ec
Andrew Kushnir <akushnir@google.com>
2025-07-11 21:55:01 +0000

    db11cdd build: update all non-major dependencies (#62583)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0+sha-02dce8e
+ * @license Angular v20.1.0+sha-db11cdd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0+sha-02dce8e');
+const VERSION = new Version('20.1.0+sha-db11cdd');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
