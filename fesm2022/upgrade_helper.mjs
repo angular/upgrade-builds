99d817f8d21d76f8efa6a862c075d33ab00d8939
Andrew Kushnir <akushnir@google.com>
2025-07-14 17:11:35 +0000

    1cff7ca docs: update forgotten change to input signals (#62612) PR Close #62612

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-aa52cd4
+ * @license Angular v20.2.0-next.0+sha-1cff7ca
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-aa52cd4');
+const VERSION = new Version('20.2.0-next.0+sha-1cff7ca');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
