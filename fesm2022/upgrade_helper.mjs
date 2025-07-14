18b026c7a7a51ee2a16e27c34d3ea3bd09cc6e5b
Andrew Kushnir <akushnir@google.com>
2025-07-14 23:01:34 +0000

    e8c5603 build: move all rule/macro loading into adev/defaults.bzl and fix intellisense loading (#62627)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-eca2334
+ * @license Angular v20.2.0-next.0+sha-e8c5603
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-eca2334');
+const VERSION = new Version('20.2.0-next.0+sha-e8c5603');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
