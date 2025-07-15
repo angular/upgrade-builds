84d5b486569b2b401053819ca054da5b279c8dac
Andrew Kushnir <akushnir@google.com>
2025-07-15 15:09:08 +0000

    72605a4 docs: fix missing 'of' in Style Guide sentence (#62628)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0+sha-a81f0fa
+ * @license Angular v20.1.0+sha-72605a4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0+sha-a81f0fa');
+const VERSION = new Version('20.1.0+sha-72605a4');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
