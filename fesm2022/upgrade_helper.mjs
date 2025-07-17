5b34b5d5e730db55a98c00269671233ec0452dcf
Kirill Cherkashin <kirjs@google.com>
2025-07-17 18:30:21 +0000

    8ad10fd fix(compiler): fix detection of directive deps in JIT (#62666)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.1+sha-dcab8c9
+ * @license Angular v20.1.1+sha-8ad10fd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.1+sha-dcab8c9');
+const VERSION = new Version('20.1.1+sha-8ad10fd');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
