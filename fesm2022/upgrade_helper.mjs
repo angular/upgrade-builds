dd29d027372045914b28c8d7f56e75d74a57ab5a
Kirill Cherkashin <kirjs@google.com>
2025-07-18 11:29:36 +0000

    ba8a5d8 refactor(core): Use promise.finally for task removal (#62674)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-1e9bfe5
+ * @license Angular v20.1.2+sha-ba8a5d8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-1e9bfe5');
+const VERSION = new Version('20.1.2+sha-ba8a5d8');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
