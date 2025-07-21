88ff4103a8a7f73d8c530a672c68537f6caa93b1
Kirill Cherkashin <kirjs@google.com>
2025-07-21 14:55:34 +0000

    8b5f814 build: update all non-major dependencies (#62701)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-a6b2d7d
+ * @license Angular v20.2.0-next.1+sha-8b5f814
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-a6b2d7d');
+const VERSION = new Version('20.2.0-next.1+sha-8b5f814');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
