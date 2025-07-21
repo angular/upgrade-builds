38adcb2e1477910630c97826cc73a4c0e23bdb6a
Kirill Cherkashin <kirjs@google.com>
2025-07-21 13:45:49 +0000

    cf48b13 docs: fix code snippet (#62720)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-c36de9e
+ * @license Angular v20.1.2+sha-cf48b13
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-c36de9e');
+const VERSION = new Version('20.1.2+sha-cf48b13');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
