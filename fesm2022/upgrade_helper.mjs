12d208e21b819c21ecbe1287dcff37d2520b6a1c
Kirill Cherkashin <kirjs@google.com>
2025-07-21 16:13:42 +0000

    f8c8ff2 docs: update Angular CLI help (#62729)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-8b5f814
+ * @license Angular v20.2.0-next.1+sha-f8c8ff2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-8b5f814');
+const VERSION = new Version('20.2.0-next.1+sha-f8c8ff2');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
