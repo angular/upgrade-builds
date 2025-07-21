f8efd09463ea150f3c4c6b5dde68f9b819d1f3dd
Kirill Cherkashin <kirjs@google.com>
2025-07-21 17:47:18 +0000

    74e4778 docs: fix minor typos in styleguide (#62706) PR Close #62706

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-b388f1e
+ * @license Angular v20.1.2+sha-74e4778
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-b388f1e');
+const VERSION = new Version('20.1.2+sha-74e4778');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
