bc6e2464ed323fd46c1317a50d59c49d0333f0cd
Kirill Cherkashin <kirjs@google.com>
2025-07-21 21:10:29 +0000

    38b7ab2 refactor(core): add a `getTransferState` for the devtools (#62722)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-45d3eef
+ * @license Angular v20.1.2+sha-38b7ab2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-45d3eef');
+const VERSION = new Version('20.1.2+sha-38b7ab2');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
