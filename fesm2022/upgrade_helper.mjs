da0d6c900df6a102a06ae078645f76a5b86fa2d9
Kirill Cherkashin <kirjs@google.com>
2025-07-21 15:02:41 +0000

    bca8a3c build: update dependency node to v22.17.1 (#62715)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-a1a78b2
+ * @license Angular v20.1.2+sha-bca8a3c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-a1a78b2');
+const VERSION = new Version('20.1.2+sha-bca8a3c');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
