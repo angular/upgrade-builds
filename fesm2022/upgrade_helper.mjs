074e95dd4eb0eb735338cb1bc6d7ee093d6d00ab
Kirill Cherkashin <kirjs@google.com>
2025-07-21 17:25:27 +0000

    6780d16 build: update cross-repo angular dependencies (#62728)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-8bf97d1
+ * @license Angular v20.2.0-next.1+sha-6780d16
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-8bf97d1');
+const VERSION = new Version('20.2.0-next.1+sha-6780d16');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
