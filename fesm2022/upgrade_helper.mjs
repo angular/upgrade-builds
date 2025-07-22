b69099104d22df277a4df3b643aaa0a87cac146f
Kirill Cherkashin <kirjs@google.com>
2025-07-22 14:22:46 +0000

    db8f7ce build: move generating doc files for @angular/ssr to using an intermediate typescript file (#62734)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-38b7ab2
+ * @license Angular v20.1.2+sha-db8f7ce
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-38b7ab2');
+const VERSION = new Version('20.1.2+sha-db8f7ce');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
