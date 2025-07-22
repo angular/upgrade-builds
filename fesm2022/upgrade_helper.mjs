555b782eb150da537ad2b9b55a24364453d5b78f
Kirill Cherkashin <kirjs@google.com>
2025-07-22 18:27:32 +0000

    0a9f55d build: update dependency angular-split to v20 (#62740)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-92e03e1
+ * @license Angular v20.2.0-next.1+sha-0a9f55d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-92e03e1');
+const VERSION = new Version('20.2.0-next.1+sha-0a9f55d');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
