1db0f4893b03290b2fb95dab35bfa4ae44eb1c4d
Kirill Cherkashin <kirjs@google.com>
2025-07-22 12:50:22 +0000

    a2f366f feat(devtools): add transfer state tab (#62465)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-4138aca
+ * @license Angular v20.2.0-next.1+sha-a2f366f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-4138aca');
+const VERSION = new Version('20.2.0-next.1+sha-a2f366f');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
