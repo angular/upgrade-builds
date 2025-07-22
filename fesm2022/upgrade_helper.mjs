9d1139dfe7d37891d6392431bb802099fb736426
Kirill Cherkashin <kirjs@google.com>
2025-07-22 16:40:01 +0000

    91bdd17 build: update @angular/ng-dev digest to 75a2aee (#62745)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-db8f7ce
+ * @license Angular v20.1.2+sha-91bdd17
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-db8f7ce');
+const VERSION = new Version('20.1.2+sha-91bdd17');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
