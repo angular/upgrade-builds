1812966d31a9d3837c4721cca695c57e631fc9bd
Kirill Cherkashin <kirjs@google.com>
2025-07-23 17:03:52 +0000

    593cc8a fix(core): checks if body exists before continuing (#62768)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-a4a4f7f
+ * @license Angular v20.2.0-next.1+sha-593cc8a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-a4a4f7f');
+const VERSION = new Version('20.2.0-next.1+sha-593cc8a');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
