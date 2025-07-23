555b46f621db268c4f8344e1b28dfbe54b8683fa
Kirill Cherkashin <kirjs@google.com>
2025-07-23 19:50:54 +0000

    006eaf3 build: update bazel rules_browsers dependency (#62772)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-155dbee
+ * @license Angular v20.1.2+sha-006eaf3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-155dbee');
+const VERSION = new Version('20.1.2+sha-006eaf3');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
