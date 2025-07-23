b72a2aea791f1b446649894a14f8d3017db4b111
Jan Martin <jan.krems@gmail.com>
2025-07-23 16:33:46 +0000

    6398baa release: bump Angular DevTools version to 1.0.40 (#62767)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-2fd1f7b
+ * @license Angular v20.1.2+sha-6398baa
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-2fd1f7b');
+const VERSION = new Version('20.1.2+sha-6398baa');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
