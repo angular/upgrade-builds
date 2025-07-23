ea700d6baa3a8b7b4c41ee134adbb4a91be9359b
Kirill Cherkashin <kirill@kcherkashin.org>
2025-07-23 21:04:19 +0000

    8344572 docs: release notes for the v20.1.3 release

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-7f9d37d
+ * @license Angular v20.2.0-next.1+sha-8344572
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-7f9d37d');
+const VERSION = new Version('20.2.0-next.1+sha-8344572');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
