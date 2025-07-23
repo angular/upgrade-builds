b97df709ff4a0edde018a63384f0de590e484305
Kirill Cherkashin <kirjs@google.com>
2025-07-23 11:33:15 +0000

    b7c745b docs: create MCP server setup guide (#62746)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-91bdd17
+ * @license Angular v20.1.2+sha-b7c745b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-91bdd17');
+const VERSION = new Version('20.1.2+sha-b7c745b');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
