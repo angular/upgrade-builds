1e2738dab3e6f48dc17d4973b7f5ce8a40fc9514
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-24 13:33:44 +0000

    206d153 refactor(forms): Removes redundant destroy ref usage in value accessor in select control (#62738)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.3+sha-5aaf726
+ * @license Angular v20.1.3+sha-206d153
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.3+sha-5aaf726');
+const VERSION = new Version('20.1.3+sha-206d153');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
