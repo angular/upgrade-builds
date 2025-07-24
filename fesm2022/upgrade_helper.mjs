bbaa5635004e2a69cc1ade044f113b40797e93f7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-24 16:21:11 +0000

    b11e050 docs(docs-infra): correct typos in adev and wording in resource api (#62801) PR Close #62801

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-61eeb9b
+ * @license Angular v20.2.0-next.2+sha-b11e050
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.2+sha-61eeb9b');
+const VERSION = new Version('20.2.0-next.2+sha-b11e050');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
