265fc778d0e6a3c76364b891467c8d5ce1d122f2
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-07-25 11:46:30 +0000

    632eba5 build: update to the TypeScript 5.9 RC (#62815)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-c19d0ae
+ * @license Angular v20.2.0-next.2+sha-632eba5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.2+sha-c19d0ae');
+const VERSION = new Version('20.2.0-next.2+sha-632eba5');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
