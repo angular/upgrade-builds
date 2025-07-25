f69afb08d63c9fbe2e75b4cbdcc89549e926dd95
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-07-25 08:16:30 +0000

    c19d0ae build: use ts_config rule from rules_ts only and always load from common location (#62808)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-b11e050
+ * @license Angular v20.2.0-next.2+sha-c19d0ae
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.2+sha-b11e050');
+const VERSION = new Version('20.2.0-next.2+sha-c19d0ae');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
