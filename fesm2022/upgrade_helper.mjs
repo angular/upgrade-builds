dbdacde34441cd7b56b0f5fee5d09add4e9718a4
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-07-28 12:28:38 +0000

    3b759db docs: Update VersionFailedEvent in Service Worker docs (#62813)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-c66e9fe
+ * @license Angular v20.2.0-next.2+sha-3b759db
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.2+sha-c66e9fe');
+const VERSION = new Version('20.2.0-next.2+sha-3b759db');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
