f7379330595cfab39e74a71d94dd64dcc3855a06
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-07-28 15:12:53 +0000

    92c2d2a fix(core): update symbols for new signals api (#62284)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-3b759db
+ * @license Angular v20.2.0-next.2+sha-92c2d2a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.2+sha-3b759db');
+const VERSION = new Version('20.2.0-next.2+sha-92c2d2a');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
