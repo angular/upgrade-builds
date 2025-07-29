0ba11867977dbc0b727e6b8855455034f26d297d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-29 10:06:21 +0000

    dc49c2c build: update all non-major dependencies (#62862)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-d55a706
+ * @license Angular v20.2.0-next.2+sha-dc49c2c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.2+sha-d55a706');
+const VERSION = new Version('20.2.0-next.2+sha-dc49c2c');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
