0267735132efeba27fe2e1c377e9c4dba0298524
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-07-29 04:50:41 +0000

    3a1a6cc test(devtools): write some e2e tests for the transfer state tab (#62843)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-ecf2ad1
+ * @license Angular v20.2.0-next.2+sha-3a1a6cc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.2+sha-ecf2ad1');
+const VERSION = new Version('20.2.0-next.2+sha-3a1a6cc');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
