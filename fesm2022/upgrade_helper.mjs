c3fc26eb24be7b0f56a25256ef34416330f05789
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-29 16:59:53 +0000

    793ff35 build: move http_server and generate_api_docs into defaults2.bzl (#62878)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-af86624
+ * @license Angular v20.2.0-next.2+sha-793ff35
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.2+sha-af86624');
+const VERSION = new Version('20.2.0-next.2+sha-793ff35');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
