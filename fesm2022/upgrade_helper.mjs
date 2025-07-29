b4fed3128ce19508aa87b82e256f3a768a5f7d21
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-29 12:24:36 +0000

    0b9f53a docs: fix ActivateRoute example (#62855)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.3+sha-f0fca61
+ * @license Angular v20.1.3+sha-0b9f53a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.3+sha-f0fca61');
+const VERSION = new Version('20.1.3+sha-0b9f53a');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
