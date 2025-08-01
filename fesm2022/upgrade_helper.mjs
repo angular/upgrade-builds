cb1eddefab807de7b8b8fc049d273b3fff25379d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-01 14:28:58 +0000

    3e6e1c1 docs: Add `MAX_ANIMATION_TIMEOUT` to the animate docs (#62949)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.3+sha-857675f
+ * @license Angular v20.2.0-next.3+sha-3e6e1c1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.3+sha-857675f');
+const VERSION = new Version('20.2.0-next.3+sha-3e6e1c1');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
