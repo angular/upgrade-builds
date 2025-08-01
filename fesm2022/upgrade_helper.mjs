10f02628c94b60b6c37e092acf253ec29b2a168d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-01 08:18:15 +0000

    e5d6fb5 fix(http): Reset headers, progress, and statusCode when using `set()` in `HttpResource` (#62873)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.3+sha-d05138a
+ * @license Angular v20.2.0-next.3+sha-e5d6fb5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.3+sha-d05138a');
+const VERSION = new Version('20.2.0-next.3+sha-e5d6fb5');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
