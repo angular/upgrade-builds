8f17b01b0256d8681e7bbd11db76dd6c7f495cb3
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-04 09:48:50 +0000

    cbd300d docs: Adds docs on handling redirect info in interceptors (#62968)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.3+sha-3e6e1c1
+ * @license Angular v20.2.0-next.3+sha-cbd300d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.3+sha-3e6e1c1');
+const VERSION = new Version('20.2.0-next.3+sha-cbd300d');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
