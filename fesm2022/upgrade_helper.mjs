db4810b3c333d96e130bcb6cec7c2d36a1c4437d
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-04 09:49:42 +0000

    e0d436a docs: Adds docs on handling redirect info in interceptors (#62968)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.4+sha-c2b48be
+ * @license Angular v20.1.4+sha-e0d436a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.4+sha-c2b48be');
+const VERSION = new Version('20.1.4+sha-e0d436a');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
