d90b372c799e030fb0b9361b3aeea4413e215182
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-05 16:08:14 +0000

    ddf779e Revert "feat(router): add a `currentNavigation` signal to the `Router` service. (#62971)" (#63010)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.3+sha-5802f80
+ * @license Angular v20.2.0-next.3+sha-ddf779e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.3+sha-5802f80');
+const VERSION = new Version('20.2.0-next.3+sha-ddf779e');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
