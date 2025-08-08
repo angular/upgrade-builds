5f7801f6acf0d9bb3cf5226c6db8229e21458e0a
Andrew Scott <atscott@google.com>
2025-08-08 21:23:24 +0000

    660444f fix(router): attempt to resolve component resources in JIT mode (#63062)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.6+sha-3b214d2
+ * @license Angular v20.2.0-next.6+sha-660444f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.6+sha-3b214d2');
+const VERSION = new Version('20.2.0-next.6+sha-660444f');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
