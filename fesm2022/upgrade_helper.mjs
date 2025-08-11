f43a783bc7f71663d59d0df2ba6e6ffe2925a717
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-08-11 08:46:47 +0000

    57184d0 docs: release notes for the v20.2.0-rc.0 release

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.6+sha-660444f
+ * @license Angular v20.3.0-next.0+sha-57184d0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.6+sha-660444f');
+const VERSION = new Version('20.3.0-next.0+sha-57184d0');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
