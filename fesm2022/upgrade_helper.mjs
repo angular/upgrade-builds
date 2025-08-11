9841c8e56108ecf8bb7f6ddcd59ac32811f6808e
Andrew Scott <atscott@google.com>
2025-08-11 22:50:56 +0000

    1dd6fd0 refactor(core): remove temporary resource setter function. (#63097)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.6+sha-fdce7e2
+ * @license Angular v20.1.6+sha-1dd6fd0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.6+sha-fdce7e2');
+const VERSION = new Version('20.1.6+sha-1dd6fd0');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
