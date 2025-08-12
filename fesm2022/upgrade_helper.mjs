4b4c6e3a25a4a12124b6279b3edeed2ae2b6f914
Andrew Scott <atscott01@gmail.com>
2025-08-12 20:35:32 +0000

    ada3c37 refactor(compiler-cli): ensure JIT transforms with queries work with closure property renaming (#63123)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.0+sha-4635095
+ * @license Angular v20.2.0-rc.0+sha-ada3c37
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.0+sha-4635095');
+const VERSION = new Version('20.2.0-rc.0+sha-ada3c37');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
