f8ffb678371c33ddb1f3575bb03a646f61c4085b
Andrew Scott <atscott@google.com>
2025-08-12 16:44:44 +0000

    0e3465d docs: add missing supported `!=` operator (#63108)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.0+sha-856dd50
+ * @license Angular v20.2.0-rc.0+sha-0e3465d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.0+sha-856dd50');
+const VERSION = new Version('20.2.0-rc.0+sha-0e3465d');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
