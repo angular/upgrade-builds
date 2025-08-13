44c5f54f3bae6d10adda58321958d53a7fcecfde
Andrew Scott <atscott01@gmail.com>
2025-08-13 20:23:11 +0000

    74e1796 docs: release notes for the v20.1.7 release

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-4e0fc81
+ * @license Angular v21.0.0-next.0+sha-74e1796
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-4e0fc81');
+const VERSION = new Version('21.0.0-next.0+sha-74e1796');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
