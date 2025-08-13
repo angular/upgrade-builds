0a4eab005b3355ed6d2fada68a9f78051dd33f52
Andrew Scott <atscott01@gmail.com>
2025-08-13 16:34:59 +0000

    774b225 docs: add rxjs-interop section for `rxResource` (#63125)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.6+sha-2c12f2a
+ * @license Angular v20.1.6+sha-774b225
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.6+sha-2c12f2a');
+const VERSION = new Version('20.1.6+sha-774b225');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
