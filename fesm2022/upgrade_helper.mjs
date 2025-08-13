667fa99fbda3fae27bcb7a66fb0a99c4b4b4d536
Andrew Scott <atscott01@gmail.com>
2025-08-13 18:33:38 +0000

    4e0fc81 feat(router): convert `lastSuccessfulNavigation` to signal (#63057)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-55cd8c2
+ * @license Angular v21.0.0-next.0+sha-4e0fc81
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-55cd8c2');
+const VERSION = new Version('21.0.0-next.0+sha-4e0fc81');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
