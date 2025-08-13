fca0e253cc3acaa6fc209d2ab2b38a032912a0b1
Andrew Scott <atscott01@gmail.com>
2025-08-13 02:13:10 +0000

    91b3466 build: update actions/checkout action to v5 (#63114)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-657061e
+ * @license Angular v21.0.0-next.0+sha-91b3466
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-657061e');
+const VERSION = new Version('21.0.0-next.0+sha-91b3466');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
