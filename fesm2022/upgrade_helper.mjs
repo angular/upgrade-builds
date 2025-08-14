c2fcba08b90da45852ccf8d34f25997241e2995b
Joey Perrott <josephperrott@gmail.com>
2025-08-14 21:19:57 +0000

    8b02aa0 ci: redirect adev to `index.csr.html` (#63168)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-c68a099
+ * @license Angular v20.2.0-rc.1+sha-8b02aa0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.1+sha-c68a099');
+const VERSION = new Version('20.2.0-rc.1+sha-8b02aa0');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
