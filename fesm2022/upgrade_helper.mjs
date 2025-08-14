0e8f32c8530abe6f19b4d087e15e02d89289b222
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-14 11:17:13 +0000

    4bed062 feat(http): Provide http services in root (#56212)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-e7cc89e
+ * @license Angular v21.0.0-next.0+sha-4bed062
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-e7cc89e');
+const VERSION = new Version('21.0.0-next.0+sha-4bed062');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
