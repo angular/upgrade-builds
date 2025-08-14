6c1aaf62cb01cce114b6985e7ffabdf5ad17b4df
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-14 20:08:32 +0000

    5e084e4 docs(docs-infra): enhance method name following the guideline (#63164)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.7+sha-7ee4965
+ * @license Angular v20.1.7+sha-5e084e4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.7+sha-7ee4965');
+const VERSION = new Version('20.1.7+sha-5e084e4');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
