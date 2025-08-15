08855d13477460dcea15c925eafce18ad457b9be
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-15 10:36:48 +0000

    d3a0c25 docs: add --yes option to mcp commands (#63173)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-47b09f8
+ * @license Angular v21.0.0-next.0+sha-d3a0c25
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-47b09f8');
+const VERSION = new Version('21.0.0-next.0+sha-d3a0c25');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
