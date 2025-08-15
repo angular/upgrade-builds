4af568d01209f47bb6a6c343794caf5094e30bbc
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-15 07:59:42 +0000

    5b3933f fix(compiler): error when ng-content fallback has translated children (#63156)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-5129dae
+ * @license Angular v21.0.0-next.0+sha-5b3933f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-5129dae');
+const VERSION = new Version('21.0.0-next.0+sha-5b3933f');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
