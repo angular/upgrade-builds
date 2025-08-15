29964d2e9c10d37e56a233c7006f1ca8bf68b42d
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-15 09:46:29 +0000

    124d44f build: update cross-repo angular dependencies (#63179)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-19ed12c
+ * @license Angular v20.2.0-rc.1+sha-124d44f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.1+sha-19ed12c');
+const VERSION = new Version('20.2.0-rc.1+sha-124d44f');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
