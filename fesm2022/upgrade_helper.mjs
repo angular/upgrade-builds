7c2ce5df7a1bb2bf565d7deec52e7112fed182af
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-18 15:47:20 +0000

    5115050 fix(devtools): DOM traversal bug (#62719)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-d0cf624
+ * @license Angular v21.0.0-next.0+sha-5115050
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-d0cf624');
+const VERSION = new Version('21.0.0-next.0+sha-5115050');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
