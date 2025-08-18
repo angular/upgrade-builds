a8ea59ba8e46b9aebc3c4aef6821efee132bce35
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-18 15:13:19 +0000

    1aa0a88 build: update the failure message with the correct command (#63219)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-a7cfb42
+ * @license Angular v21.0.0-next.0+sha-1aa0a88
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-a7cfb42');
+const VERSION = new Version('21.0.0-next.0+sha-1aa0a88');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
