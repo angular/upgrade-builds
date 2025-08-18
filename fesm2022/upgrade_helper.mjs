2790255ae702e6155e75c35a767ebb5ccf7d7e2f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-18 15:13:55 +0000

    b413b62 build: update the failure message with the correct command (#63219)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.7+sha-bc3798d
+ * @license Angular v20.1.7+sha-b413b62
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.7+sha-bc3798d');
+const VERSION = new Version('20.1.7+sha-b413b62');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
