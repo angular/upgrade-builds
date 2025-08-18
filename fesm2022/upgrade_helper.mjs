d5d467a949c8fcf0a58627e36ded5deafdf09ab1
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-18 11:03:33 +0000

    726d602 ci: mark myself as unavailable during OOO (#63214)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-9e828d5
+ * @license Angular v20.2.0-rc.1+sha-726d602
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.1+sha-9e828d5');
+const VERSION = new Version('20.2.0-rc.1+sha-726d602');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
