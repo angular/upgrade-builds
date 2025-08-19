df314a2dd5b58a4a281ed476a9620b364732fc92
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-19 08:06:29 +0000

    d81baf6 docs: fix typo in SSR section (#63224) PR Close #63224

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.7+sha-b413b62
+ * @license Angular v20.1.7+sha-d81baf6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.7+sha-b413b62');
+const VERSION = new Version('20.1.7+sha-d81baf6');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
