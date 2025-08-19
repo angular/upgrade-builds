0ce4eb9ac4598c558dbae26fbf3b4f914b53dc73
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-19 08:31:46 +0000

    6712456 refactor(core): update NG0303 error message (#63222) Ensure consistency in error message wording by aligning the NG0303 error with other Angular error strings. This improves clarity and maintains a uniform developer experience

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-101f072
+ * @license Angular v21.0.0-next.0+sha-6712456
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-101f072');
+const VERSION = new Version('21.0.0-next.0+sha-6712456');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
