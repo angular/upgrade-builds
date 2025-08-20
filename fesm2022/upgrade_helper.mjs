18805e406ea6294f924e63cbe4b97a67fdd22825
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 11:19:41 +0000

    3d7b31c build: update dependency playwright-core to v1.55.0 (#63272)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-086ae5e
+ * @license Angular v21.0.0-next.0+sha-3d7b31c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-086ae5e');
+const VERSION = new Version('21.0.0-next.0+sha-3d7b31c');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
