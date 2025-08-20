ba171cb3567fe0ab246115e4b76babea31e13b7e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 16:04:55 +0000

    1c5fb1e build: enforce frozen lockfile mode (#63260)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-d8bdf56
+ * @license Angular v21.0.0-next.0+sha-1c5fb1e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-d8bdf56');
+const VERSION = new Version('21.0.0-next.0+sha-1c5fb1e');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
