4c94b51e613a311fc8688b77c67833ec71dd7aab
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 11:36:50 +0000

    878b99a fix(compiler-cli): account for expression with type arguments during HMR extraction (#63261)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-9655ea7
+ * @license Angular v20.2.0-rc.1+sha-878b99a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.1+sha-9655ea7');
+const VERSION = new Version('20.2.0-rc.1+sha-878b99a');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
