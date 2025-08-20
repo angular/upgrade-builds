20f6b1b1d502abbff18eafb6537b23a7d040b040
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 16:21:16 +0000

    65c5a73 build: update @angular/ng-dev digest to c93dfc8 (#63290)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0+sha-9bdb4fe
+ * @license Angular v20.2.0+sha-65c5a73
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0+sha-9bdb4fe');
+const VERSION = new Version('20.2.0+sha-65c5a73');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
