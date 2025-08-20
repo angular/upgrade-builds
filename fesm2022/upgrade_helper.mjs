524e7e79515db5c12d9f26628087999d2d924304
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-08-20 12:35:12 +0000

    d1df128 docs: release notes for the v20.2.0 release

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-e6fea45
+ * @license Angular v21.0.0-next.0+sha-d1df128
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-e6fea45');
+const VERSION = new Version('21.0.0-next.0+sha-d1df128');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
