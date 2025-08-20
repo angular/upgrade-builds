622defd51df6243b9114b2d6121994c4f863983d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 09:37:24 +0000

    6a6cb01 docs: update `NG_VALIDATORS` examples to use `forwardRef` (#63247)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-85468ef
+ * @license Angular v21.0.0-next.0+sha-6a6cb01
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-85468ef');
+const VERSION = new Version('21.0.0-next.0+sha-6a6cb01');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
