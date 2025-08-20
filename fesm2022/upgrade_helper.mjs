4a4344384b6b87e8d3558aa689582e0d3f91cde7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 08:35:06 +0000

    1feadba docs: remove redundant Angular docs link in tailwind guide (#63205)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-8a6cc39
+ * @license Angular v21.0.0-next.0+sha-1feadba
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-8a6cc39');
+const VERSION = new Version('21.0.0-next.0+sha-1feadba');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
