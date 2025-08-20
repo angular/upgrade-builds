5a4bdbe2f9163eed38c9c05242c3c6325957971e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 09:48:49 +0000

    eeeaadc fix(language-service): Support to resolve the re-export component. (#62585)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-6a6cb01
+ * @license Angular v21.0.0-next.0+sha-eeeaadc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-6a6cb01');
+const VERSION = new Version('21.0.0-next.0+sha-eeeaadc');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
