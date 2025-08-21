c8a9fd25e4e0117a224c34685975c1a87d6321f1
Andrew Kushnir <akushnir@google.com>
2025-08-21 16:52:41 +0000

    0dd95c5 feat(forms): Add FormArrayDirective (#55880)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-1b0c84a
+ * @license Angular v21.0.0-next.0+sha-0dd95c5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-1b0c84a');
+const VERSION = new Version('21.0.0-next.0+sha-0dd95c5');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
