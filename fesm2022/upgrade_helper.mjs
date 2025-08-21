732d70257ac11f6b52a638ae8047078a8d25524f
Andrew Kushnir <akushnir@google.com>
2025-08-21 18:47:36 +0000

    69df2c0 build: update dependency cypress to v15 (#63303)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-7d6ae95
+ * @license Angular v21.0.0-next.0+sha-69df2c0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-7d6ae95');
+const VERSION = new Version('21.0.0-next.0+sha-69df2c0');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
