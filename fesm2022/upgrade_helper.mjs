7b8a8d6c5604705e0317a662598c91427060e168
Andrew Kushnir <akushnir@google.com>
2025-08-22 21:49:28 +0000

    3df1dcc refactor: various build and import specificer fixes for strict deps (#63323)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-983cc2f
+ * @license Angular v21.0.0-next.0+sha-3df1dcc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-983cc2f');
+const VERSION = new Version('21.0.0-next.0+sha-3df1dcc');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
