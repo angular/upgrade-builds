26f5b70922a42c3456b52980b3dbf443120a0453
Andrew Kushnir <akushnir@google.com>
2025-08-25 15:45:23 +0000

    d025a07 build: update all non-major dependencies (#63344)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-5af3222
+ * @license Angular v21.0.0-next.0+sha-d025a07
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-5af3222');
+const VERSION = new Version('21.0.0-next.0+sha-d025a07');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
