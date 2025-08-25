1b8e093b1c3a0338e66be5cdde2c250fe38eab6c
Andrew Kushnir <akushnir@google.com>
2025-08-25 23:34:35 +0000

    15395a5 build: update cross-repo angular dependencies (#63380)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.1+sha-9a27c5b
+ * @license Angular v20.2.1+sha-15395a5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.1+sha-9a27c5b');
+const VERSION = new Version('20.2.1+sha-15395a5');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
