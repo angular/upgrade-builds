6522662e6ad83ced946d873c55e0e27620c73fd1
Andrew Kushnir <akushnir@google.com>
2025-08-26 16:46:19 +0000

    aff8bb2 refactor(devtools): provide signalGraphEnabled via settings service (#63374)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-67d4449
+ * @license Angular v21.0.0-next.0+sha-aff8bb2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-67d4449');
+const VERSION = new Version('21.0.0-next.0+sha-aff8bb2');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
