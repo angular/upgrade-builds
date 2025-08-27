dc99b0a393d59f72575063d08bde0b7177c49523
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-08-27 18:04:15 +0000

    478eebe release: cut the v21.0.0-next.1 release

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-01f44ee
+ * @license Angular v21.0.0-next.1+sha-478eebe
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-01f44ee');
+const VERSION = new Version('21.0.0-next.1+sha-478eebe');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
