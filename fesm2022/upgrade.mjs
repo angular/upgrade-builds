170a2a65e270a3e1a88ff69b5b387a593874764a
Miles Malerba <mmalerba@google.com>
2025-08-27 23:22:45 +0000

    059e0d3 docs: add customizing route behavior guide for router (#63398)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-1bb30c7
+ * @license Angular v21.0.0-next.1+sha-059e0d3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-1bb30c7');
+const VERSION = new Version('21.0.0-next.1+sha-059e0d3');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
