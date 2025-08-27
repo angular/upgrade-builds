0d34d220d3e75f43a43ea3d8dca9b71982154658
Miles Malerba <mmalerba@google.com>
2025-08-27 23:37:30 +0000

    0d57dfb docs: update Angular CLI help (#63419)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-059e0d3
+ * @license Angular v21.0.0-next.1+sha-0d57dfb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-059e0d3');
+const VERSION = new Version('21.0.0-next.1+sha-0d57dfb');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
