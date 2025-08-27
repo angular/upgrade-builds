17e602a9ed4ba189272514e31c862f1f705b2675
Andrew Kushnir <akushnir@google.com>
2025-08-27 18:44:56 +0000

    fce5602 refactor(core): mark `VERSION` as `@__PURE__` for better tree-shaking (#63400)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.2+sha-06c5367
+ * @license Angular v20.2.2+sha-fce5602
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.2+sha-06c5367');
+const VERSION = new Version('20.2.2+sha-fce5602');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
