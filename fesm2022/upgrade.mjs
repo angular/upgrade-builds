94e2d564fd74973cc30d9da88aaaa228f88acb9c
Miles Malerba <mmalerba@google.com>
2025-08-28 16:58:11 +0000

    c9b215b build: update cross-repo angular dependencies (#63443)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-2f5f975
+ * @license Angular v21.0.0-next.1+sha-c9b215b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-2f5f975');
+const VERSION = new Version('21.0.0-next.1+sha-c9b215b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
