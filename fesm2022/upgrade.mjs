ab117762e78cbcfae1feb24bd2e918e9cb104852
Miles Malerba <mmalerba@google.com>
2025-08-28 19:00:01 +0000

    1a26fd3 build: update all non-major dependencies (#63412)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-147c743
+ * @license Angular v21.0.0-next.1+sha-1a26fd3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-147c743');
+const VERSION = new Version('21.0.0-next.1+sha-1a26fd3');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
