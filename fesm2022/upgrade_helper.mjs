ee09975790b50bb900a9dccadb568c309214234a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-28 10:07:07 +0000

    421edfe build: update @bazel/ibazel to `0.26.10` (#63435)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.2+sha-87bfe16
+ * @license Angular v20.2.2+sha-421edfe
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.2+sha-87bfe16');
+const VERSION = new Version('20.2.2+sha-421edfe');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
