64a19aaa4333d984e3bd2559fcb37c32aa0247bd
Miles Malerba <mmalerba@google.com>
2025-08-28 15:54:29 +0000

    a1d1cdf build: remove `rulesJsInteropMode` (#63448)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-07e6788
+ * @license Angular v21.0.0-next.1+sha-a1d1cdf
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-07e6788');
+const VERSION = new Version('21.0.0-next.1+sha-a1d1cdf');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
