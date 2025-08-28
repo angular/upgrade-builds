32803cada1818c3e69d1f50371e9174322d4389b
Miles Malerba <mmalerba@google.com>
2025-08-28 00:00:54 +0000

    a6a46e9 docs(docs-infra): tooltip on hover and message after copied (#63167)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.2+sha-9de2f07
+ * @license Angular v20.2.2+sha-a6a46e9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.2+sha-9de2f07');
+const VERSION = new Version('20.2.2+sha-a6a46e9');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
