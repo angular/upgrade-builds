eeef9c33713f21084bc680d02bf77468e37d61d7
Miles Malerba <mmalerba@google.com>
2025-08-28 15:40:30 +0000

    07e6788 feat(http): Add reponseType property to HttpResponse and HttpErrorResponse (#63043)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-ed3d1f2
+ * @license Angular v21.0.0-next.1+sha-07e6788
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-ed3d1f2');
+const VERSION = new Version('21.0.0-next.1+sha-07e6788');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
