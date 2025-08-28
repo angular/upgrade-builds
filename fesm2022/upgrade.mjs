ad628bdb3310a93c3abcf3c6bb5e21bbf9431e20
Miles Malerba <mmalerba@google.com>
2025-08-28 00:02:41 +0000

    b2ed21b build: remove unused deps (#63417)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-0d57dfb
+ * @license Angular v21.0.0-next.1+sha-b2ed21b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-0d57dfb');
+const VERSION = new Version('21.0.0-next.1+sha-b2ed21b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
