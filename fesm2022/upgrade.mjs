4755f6a3a5254dd7d708adfed6bc126837a74bc6
Doug Parker <dgp1130@users.noreply.github.com>
2025-08-28 17:40:28 +0000

    147c743 release: bump Angular DevTools to 1.0.44 (#63452)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-c9b215b
+ * @license Angular v21.0.0-next.1+sha-147c743
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-c9b215b');
+const VERSION = new Version('21.0.0-next.1+sha-147c743');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
