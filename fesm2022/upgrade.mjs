26c9426f00d5e364cb54ad6b4672b37b7c48ecc9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-29 13:06:07 +0000

    a3938d5 docs(docs-infra): makes signal readonly (#63449)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-1875d5c
+ * @license Angular v21.0.0-next.1+sha-a3938d5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-1875d5c');
+const VERSION = new Version('21.0.0-next.1+sha-a3938d5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
