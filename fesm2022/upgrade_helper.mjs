77422c6510914f0c922b028c0e36ff9dfd97e7c9
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-08-29 12:37:56 +0000

    64ddde2 release: cut the v20.2.3 release

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.2+sha-479a919
+ * @license Angular v20.2.3+sha-64ddde2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.2+sha-479a919');
+const VERSION = new Version('20.2.3+sha-64ddde2');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
