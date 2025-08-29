d1045d9d4f816888446b1eee4b4b1a3f98b266d4
Miles Malerba <mmalerba@google.com>
2025-08-29 22:16:24 +0000

    dddce83 ci: add more team members as approvers (#63495)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-e9096b9
+ * @license Angular v20.2.3+sha-dddce83
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.3+sha-e9096b9');
+const VERSION = new Version('20.2.3+sha-dddce83');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
