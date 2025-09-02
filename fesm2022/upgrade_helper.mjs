82a0049ba0187a02cc0289f8f688d38d89523c5a
Miles Malerba <mmalerba@google.com>
2025-09-02 18:04:40 +0000

    dc64f3e fix(core): Fixed inject migration schematics for migrate destructured properties (#62832)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-957b367
+ * @license Angular v20.2.3+sha-dc64f3e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.3+sha-957b367');
+const VERSION = new Version('20.2.3+sha-dc64f3e');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
