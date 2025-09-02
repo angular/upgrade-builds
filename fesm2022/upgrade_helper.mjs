dab8b5fba51b7ba14559efd28ec6d1b5862e471f
Miles Malerba <mmalerba@google.com>
2025-09-02 19:44:56 +0000

    7bbcb54 build: lock file maintenance (#63523)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-1e47a87
+ * @license Angular v20.2.3+sha-7bbcb54
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.3+sha-1e47a87');
+const VERSION = new Version('20.2.3+sha-7bbcb54');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
