802df440db827347247e9d1ade5aa9f79e085ff7
Miles Malerba <mmalerba@google.com>
2025-09-02 16:41:50 +0000

    3b42314 docs(docs-infra): export class (#63506)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-dddce83
+ * @license Angular v20.2.3+sha-3b42314
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.3+sha-dddce83');
+const VERSION = new Version('20.2.3+sha-3b42314');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
