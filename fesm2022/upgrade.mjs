d1ab701e4eaf0f082be0303d67527cd96e8d87ae
Miles Malerba <mmalerba@google.com>
2025-09-02 19:48:34 +0000

    6148a40 build: lock file maintenance (#63522)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-4fb8c46
+ * @license Angular v21.0.0-next.1+sha-6148a40
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-4fb8c46');
+const VERSION = new Version('21.0.0-next.1+sha-6148a40');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
