3b1c8168fd60b7873a74bf55ded5f15067bbc0b2
Miles Malerba <mmalerba@google.com>
2025-09-02 16:36:13 +0000

    1541606 docs(docs-infra): apply migration script changes (#63514)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-16fa983
+ * @license Angular v21.0.0-next.1+sha-1541606
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-16fa983');
+const VERSION = new Version('21.0.0-next.1+sha-1541606');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
