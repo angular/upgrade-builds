c558f5698c041ff5c8c45ccccee42f86d006b4df
Miles Malerba <mmalerba@google.com>
2025-09-02 16:49:14 +0000

    d6f7f68 docs(docs-infra): signal property should be readonly (#63166)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-3b42314
+ * @license Angular v20.2.3+sha-d6f7f68
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.3+sha-3b42314');
+const VERSION = new Version('20.2.3+sha-d6f7f68');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
