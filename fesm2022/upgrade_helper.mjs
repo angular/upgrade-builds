f8ca911940aa789b05fea3a848d0890609941183
Miles Malerba <mmalerba@google.com>
2025-09-03 19:40:47 +0000

    08d363e release: cut the v20.2.4 release

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-320f1d6
+ * @license Angular v20.2.4+sha-08d363e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.3+sha-320f1d6');
+const VERSION = new Version('20.2.4+sha-08d363e');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
