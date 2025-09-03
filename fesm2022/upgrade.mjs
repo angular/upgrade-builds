dd687512cf42d6fcc5e2c9bd3179f358f6d5b880
Miles Malerba <mmalerba@google.com>
2025-09-03 16:37:48 +0000

    8738b81 build: use common jasmine rule from dev-infra (#63568)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-49d5de4
+ * @license Angular v21.0.0-next.1+sha-8738b81
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-49d5de4');
+const VERSION = new Version('21.0.0-next.1+sha-8738b81');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
