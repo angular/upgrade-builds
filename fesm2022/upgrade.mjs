98b5c95138cc627135f6721d9ab5513a9a7ea7c5
Miles Malerba <mmalerba@google.com>
2025-09-03 16:20:11 +0000

    49d5de4 refactor(platform-server): simplify transfer state serialization tracking (#63525)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-e6949ad
+ * @license Angular v21.0.0-next.1+sha-49d5de4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-e6949ad');
+const VERSION = new Version('21.0.0-next.1+sha-49d5de4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
