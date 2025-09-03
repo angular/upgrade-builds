00592d6644f1fe8d17a3b8afe757c189d89895a1
Miles Malerba <mmalerba@google.com>
2025-09-03 16:05:12 +0000

    e6949ad docs: fix #63555 (#63560)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-d006721
+ * @license Angular v21.0.0-next.1+sha-e6949ad
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-d006721');
+const VERSION = new Version('21.0.0-next.1+sha-e6949ad');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
