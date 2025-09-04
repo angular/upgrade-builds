3365e908a051c90db95f6d187dbb387de6731252
Andrew Scott <atscott@google.com>
2025-09-04 22:25:12 +0000

    e1dd3b1 docs: uses signal, implement TODOs, and fix typos (#63603)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-a3f808d
+ * @license Angular v20.2.4+sha-e1dd3b1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.4+sha-a3f808d');
+const VERSION = new Version('20.2.4+sha-e1dd3b1');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
