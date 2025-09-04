9c998cdd6d2607c972fec5c5adbaa963ec15a50d
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-09-04 11:51:33 +0000

    948b811 release: bump Angular DevTools version to 1.1.44 (#63596)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-835aad7
+ * @license Angular v21.0.0-next.2+sha-948b811
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-835aad7');
+const VERSION = new Version('21.0.0-next.2+sha-948b811');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
