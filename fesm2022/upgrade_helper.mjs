4496a207d727bef24c9df52333d8457c4b074c7a
Andrew Scott <atscott@google.com>
2025-09-05 19:58:11 +0000

    c3303a5 test: increase mermaid spec timeout to reduce flakes (#63610)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-f697995
+ * @license Angular v20.2.4+sha-c3303a5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.4+sha-f697995');
+const VERSION = new Version('20.2.4+sha-c3303a5');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
