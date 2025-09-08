c0955d85bcb504b96378bb9783681f441a4219fe
Andrew Scott <atscott@google.com>
2025-09-08 19:14:59 +0000

    d124fba docs: improve headings of docs (#63604)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-a6640e3
+ * @license Angular v20.2.4+sha-d124fba
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.4+sha-a6640e3');
+const VERSION = new Version('20.2.4+sha-d124fba');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
