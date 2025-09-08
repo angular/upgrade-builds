44986b24c823a1f3ec641b1e3fa91a2227f52107
Andrew Scott <atscott@google.com>
2025-09-08 21:57:34 +0000

    efb9474 docs: should not conditionally include `<ng-content>` in IMPORTANT block (#63580)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-c8c4399
+ * @license Angular v21.0.0-next.2+sha-efb9474
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-c8c4399');
+const VERSION = new Version('21.0.0-next.2+sha-efb9474');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
