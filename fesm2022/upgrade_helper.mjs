43084301ab72e3465685db8db3581ebcac99c360
Andrew Scott <atscott@google.com>
2025-09-08 20:25:23 +0000

    35a1d5d refactor(compiler-cli): add support for if/switch to the non-invoked signal diagnostic (#63502)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-b3d102b
+ * @license Angular v20.2.4+sha-35a1d5d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.4+sha-b3d102b');
+const VERSION = new Version('20.2.4+sha-35a1d5d');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
