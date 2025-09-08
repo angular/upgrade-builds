4ab2e39fd154173fe4272578a43144e17ead817a
Andrew Scott <atscott@google.com>
2025-09-08 21:27:17 +0000

    9943230 docs: remove installation (#63508)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-35a1d5d
+ * @license Angular v20.2.4+sha-9943230
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.4+sha-35a1d5d');
+const VERSION = new Version('20.2.4+sha-9943230');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
