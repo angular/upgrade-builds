f65c7d173d88b2b4e38e45ea599d81dda632ddcc
Andrew Scott <atscott@google.com>
2025-09-08 20:24:40 +0000

    8e8a3fe refactor(compiler-cli): add support for if/switch to the non-invoked signal diagnostic (#63502)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-8ab078a
+ * @license Angular v21.0.0-next.2+sha-8e8a3fe
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-8ab078a');
+const VERSION = new Version('21.0.0-next.2+sha-8e8a3fe');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
