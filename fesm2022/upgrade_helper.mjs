92dc9d73c347cf5eb9fbc05cf993ff8598a47ad5
Andrew Scott <atscott@google.com>
2025-09-08 23:36:16 +0000

    c7b84f5 docs: Update Language Service IDE list (#63647)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-a857f72
+ * @license Angular v20.2.4+sha-c7b84f5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.4+sha-a857f72');
+const VERSION = new Version('20.2.4+sha-c7b84f5');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
