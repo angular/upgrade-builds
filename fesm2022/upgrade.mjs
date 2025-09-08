a876e62c3000058666ddf179cafeb9d3ea9216d8
Andrew Scott <atscott@google.com>
2025-09-08 19:56:46 +0000

    8ab078a docs: update template examples to use signal-based viewChild instead of decorator-based @ViewChild (#63630)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-1078954
+ * @license Angular v21.0.0-next.2+sha-8ab078a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-1078954');
+const VERSION = new Version('21.0.0-next.2+sha-8ab078a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
