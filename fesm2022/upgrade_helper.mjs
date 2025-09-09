f82d7e65e5744e84b9120b37963211af301f0ff4
Andrew Scott <atscott@google.com>
2025-09-09 21:33:36 +0000

    650cd6b docs: add missing component harness docs (#63620)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0-next.0+sha-d0f0c08
+ * @license Angular v20.3.0-next.0+sha-650cd6b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0-next.0+sha-d0f0c08');
+const VERSION = new Version('20.3.0-next.0+sha-650cd6b');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
