a4e618a8971bda3e625151b628e5db02a78983c6
Andrew Scott <atscott@google.com>
2025-09-09 22:49:49 +0000

    97d3615 build: create a script and ci setup for managing and uploading known synonyms for algolia (#63663)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-c357650
+ * @license Angular v21.0.0-next.2+sha-97d3615
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-c357650');
+const VERSION = new Version('21.0.0-next.2+sha-97d3615');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
