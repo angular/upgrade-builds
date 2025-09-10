72489838307be39c48fcefcc847f91b269c42cf5
Andrew Scott <atscott01@gmail.com>
2025-09-10 15:06:42 +0000

    9096d45 release: cut the v20.3.0 release

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0-rc.0+sha-34c5a03
+ * @license Angular v20.3.0+sha-9096d45
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0-rc.0+sha-34c5a03');
+const VERSION = new Version('20.3.0+sha-9096d45');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
