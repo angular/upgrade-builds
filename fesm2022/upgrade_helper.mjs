d481aab1c54306f8fe077cf3c611ad7d61d1aff9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-10 22:35:51 +0000

    5f5828f refactor(core): dispatch enter and leave animations at the right times (#63710)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-8dc3496
+ * @license Angular v20.3.0+sha-5f5828f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-8dc3496');
+const VERSION = new Version('20.3.0+sha-5f5828f');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
