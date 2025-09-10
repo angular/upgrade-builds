b774a93d26c80a2c5ca94023991a504a8acafae1
Andrew Scott <atscott@google.com>
2025-09-10 17:35:06 +0000

    9487dc6 build: update `@angular/ng-dev` to latest SHA (#63696)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-48fee39
+ * @license Angular v21.0.0-next.3+sha-9487dc6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-48fee39');
+const VERSION = new Version('21.0.0-next.3+sha-9487dc6');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
