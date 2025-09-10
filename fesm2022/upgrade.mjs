de14e4ff9c88f439f87d8ae596e57bd03985084c
Andrew Scott <atscott@google.com>
2025-09-10 20:34:39 +0000

    fb3082f build: fix symbol tests (#63709)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-f642fcf
+ * @license Angular v21.0.0-next.3+sha-fb3082f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-f642fcf');
+const VERSION = new Version('21.0.0-next.3+sha-fb3082f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
