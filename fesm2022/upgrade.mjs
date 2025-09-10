ea021722947566c934335371f4a8ad80cbccffef
Andrew Scott <atscott01@gmail.com>
2025-09-10 15:12:15 +0000

    9586d48 docs: release notes for the v20.3.0 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-0102575
+ * @license Angular v21.0.0-next.2+sha-9586d48
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-0102575');
+const VERSION = new Version('21.0.0-next.2+sha-9586d48');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
