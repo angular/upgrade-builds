299fbd1d683114bcaec66b4355362973a7b43352
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 15:26:08 +0000

    a4001c4 fix(core): Prevent leave animations on a move operation (#63745)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-464bff9
+ * @license Angular v21.0.0-next.3+sha-a4001c4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-464bff9');
+const VERSION = new Version('21.0.0-next.3+sha-a4001c4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
