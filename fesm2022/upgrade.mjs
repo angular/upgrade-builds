6f920ed892fb994ac669ef40469f3475ec9bb2af
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 21:49:43 +0000

    85994fb build: update branches and versioning information (#63772)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-62ad4db
+ * @license Angular v21.0.0-next.3+sha-85994fb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-62ad4db');
+const VERSION = new Version('21.0.0-next.3+sha-85994fb');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
