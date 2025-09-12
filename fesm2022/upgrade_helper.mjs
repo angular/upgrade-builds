ab7d7b56c542adbbe6ecce2f28ca51481b356547
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 15:28:08 +0000

    3ec8a5c fix(core): Prevent leave animations on a move operation (#63745)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-4ee3993
+ * @license Angular v20.3.0+sha-3ec8a5c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-4ee3993');
+const VERSION = new Version('20.3.0+sha-3ec8a5c');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
