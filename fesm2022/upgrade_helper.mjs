0fc79e9ff1ed07d63fc11738dcf6d60db24037b2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 17:49:37 +0000

    9def492 build: update pnpm to v10.16.0 (#63770)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-15b6e1a
+ * @license Angular v20.3.0+sha-9def492
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-15b6e1a');
+const VERSION = new Version('20.3.0+sha-9def492');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
