070716b9f5da7b8718705e78cf9d6b77f0a936f2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 15:55:24 +0000

    895f0fb docs: update Creating service with CLI (#63714)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-3ec8a5c
+ * @license Angular v20.3.0+sha-895f0fb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-3ec8a5c');
+const VERSION = new Version('20.3.0+sha-895f0fb');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
