4ee7d92ff4d642380593435bca8f370f496e9906
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 18:52:11 +0000

    7fb5a80 fix(compiler): Add support for `aria-invalid` (#63748)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-a2ea2ec
+ * @license Angular v20.3.0+sha-7fb5a80
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-a2ea2ec');
+const VERSION = new Version('20.3.0+sha-7fb5a80');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
