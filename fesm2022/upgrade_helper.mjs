90e1232d5320182075c1969352d810e2d7aeb876
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 17:13:19 +0000

    a2ea2ec docs: Update outputs.md to demonstrate how to use event data (#63732) PR Close #63732

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-cd36189
+ * @license Angular v20.3.0+sha-a2ea2ec
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-cd36189');
+const VERSION = new Version('20.3.0+sha-a2ea2ec');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
