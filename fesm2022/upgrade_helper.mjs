ad172df9adfb26f6077c0105c958c47465fba44e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 17:08:04 +0000

    cd36189 ci: update pullapprove public-api rules (#63737)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-38c9921
+ * @license Angular v20.3.0+sha-cd36189
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-38c9921');
+const VERSION = new Version('20.3.0+sha-cd36189');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
