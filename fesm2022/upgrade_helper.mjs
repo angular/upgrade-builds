d4d352dd9bca307f4899954af2a1cf055801cf94
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 15:44:18 +0000

    b05552f docs: add documentation for NG02802 warning about HttpTransferCache headers (#63802)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-13e4255
+ * @license Angular v20.3.0+sha-b05552f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-13e4255');
+const VERSION = new Version('20.3.0+sha-b05552f');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
