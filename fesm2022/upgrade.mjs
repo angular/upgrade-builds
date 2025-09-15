27ed8d11481b38f9cbfc5c081b34d8944632d94a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 18:30:25 +0000

    a5e5dbb refactor(core): remove `moduleId` from Component metadata (#63482)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-2473931
+ * @license Angular v21.0.0-next.3+sha-a5e5dbb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-2473931');
+const VERSION = new Version('21.0.0-next.3+sha-a5e5dbb');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
