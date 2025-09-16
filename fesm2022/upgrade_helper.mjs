ab162a3e116e2f1c747d4c880a71e5a1c8ace259
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 14:58:24 +0000

    7eb5f9a docs: remove leading `$` from script examples (#63795)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-4a23f6e
+ * @license Angular v20.3.0+sha-7eb5f9a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-4a23f6e');
+const VERSION = new Version('20.3.0+sha-7eb5f9a');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
