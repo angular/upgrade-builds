28acbfed1c79f22363e302f2cbad968f22823b67
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 16:26:09 +0000

    049b672 docs(docs-infra): new home page (#63662)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-6e54bdf
+ * @license Angular v20.3.0+sha-049b672
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-6e54bdf');
+const VERSION = new Version('20.3.0+sha-049b672');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
