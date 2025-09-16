d746c22426b0fa0bb4309a3f29a04806631db225
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 15:05:37 +0000

    c80d9ba docs: update doc to clear up difference between `toObservable`/ `outputToObservable` (#63836)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-7eb5f9a
+ * @license Angular v20.3.0+sha-c80d9ba
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-7eb5f9a');
+const VERSION = new Version('20.3.0+sha-c80d9ba');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
