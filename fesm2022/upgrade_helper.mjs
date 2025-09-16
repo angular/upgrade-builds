19e438e811818a635732334bd51e48718e32e0fc
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 15:22:44 +0000

    444fd01 docs(docs-infra): Use port 4201 for adev (#63834)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-27b2a4a
+ * @license Angular v20.3.0+sha-444fd01
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-27b2a4a');
+const VERSION = new Version('20.3.0+sha-444fd01');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
