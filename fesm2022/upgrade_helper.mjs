1f60e4d8085860799149d744cfd68c4f7fd6b811
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 15:05:31 +0000

    423cc17 refactor(common): trim undefined from locale data. (#63520)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-8cd2d8b
+ * @license Angular v20.3.0+sha-423cc17
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-8cd2d8b');
+const VERSION = new Version('20.3.0+sha-423cc17');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
