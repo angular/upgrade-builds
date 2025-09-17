aec2b298273d1bc4873cd1effdc76d72fb990099
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 17:46:13 +0000

    dee6ef8 refactor(core): tree-shake `HOST_TAG_NAME` in prod (#63861)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-32c98e0
+ * @license Angular v21.0.0-next.3+sha-dee6ef8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-32c98e0');
+const VERSION = new Version('21.0.0-next.3+sha-dee6ef8');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
