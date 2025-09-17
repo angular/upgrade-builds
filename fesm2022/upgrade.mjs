f13822c8efcb93b65634abd62fbb2559e62ed2e7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 15:01:25 +0000

    e523384 refactor(common): trim undefined from locale data. (#63520)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-5fa2c05
+ * @license Angular v21.0.0-next.3+sha-e523384
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-5fa2c05');
+const VERSION = new Version('21.0.0-next.3+sha-e523384');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
