bdfd51450ab6af5da3d779762688f72c54a9e8e8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 16:17:17 +0000

    8843707 fix(compiler-cli): only bind inputs that are part of microsyntax to a structural directive (#52453)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-dcdca98
+ * @license Angular v20.3.0+sha-8843707
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-dcdca98');
+const VERSION = new Version('20.3.0+sha-8843707');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
