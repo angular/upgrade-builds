b49c7de6b6c78e12388aa5acb3b6136025b34b52
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-18 20:22:56 +0000

    307e4ea refactor(migrations): remove common module if there are no references (#63892)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-e426302
+ * @license Angular v21.0.0-next.4+sha-307e4ea
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-e426302');
+const VERSION = new Version('21.0.0-next.4+sha-307e4ea');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
