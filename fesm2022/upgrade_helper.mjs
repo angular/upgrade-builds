1d58baef148258ea2de4d24b5cca923bee4931e5
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-18 15:05:30 +0000

    205d527 build: update cross-repo angular dependencies (#63908)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-ef57d61
+ * @license Angular v20.3.1+sha-205d527
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.1+sha-ef57d61');
+const VERSION = new Version('20.3.1+sha-205d527');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
