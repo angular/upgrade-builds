4814486de1e3c28d50b997c6764ac82c52675bf3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-18 14:44:14 +0000

    950ffd0 build: remove yarn from integration tests, switch to pnpm (#63902)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-8ad26ed
+ * @license Angular v21.0.0-next.4+sha-950ffd0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-8ad26ed');
+const VERSION = new Version('21.0.0-next.4+sha-950ffd0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
