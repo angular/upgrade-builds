c81e15c6a80d13b0f83f01b2075eb969a69bb49f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 21:34:49 +0000

    e78451c fix(core): prevent animations renderer from impacting `animate.leave` (#63921)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-b04e6b1
+ * @license Angular v20.3.1+sha-e78451c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.1+sha-b04e6b1');
+const VERSION = new Version('20.3.1+sha-e78451c');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
