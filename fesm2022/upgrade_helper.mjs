c5a2c10f5a0501872bafa7377567787b801e839b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 21:39:51 +0000

    dbb8dbc docs: callout for HMR+Defer (#63958)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-e78451c
+ * @license Angular v20.3.1+sha-dbb8dbc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.1+sha-e78451c');
+const VERSION = new Version('20.3.1+sha-dbb8dbc');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
