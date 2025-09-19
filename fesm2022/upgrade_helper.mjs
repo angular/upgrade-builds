fd24b12b58ae8bd1eed366406329d3bd5135503a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 19:09:57 +0000

    8858e3c Revert "refactor(compiler-cli): track member metadata using output AST (#63904)" (#63952)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-150ed2b
+ * @license Angular v20.3.1+sha-8858e3c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.1+sha-150ed2b');
+const VERSION = new Version('20.3.1+sha-8858e3c');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
