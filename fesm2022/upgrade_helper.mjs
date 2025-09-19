b8463ea96e4253c28a3d6334afba5222d7b4c6c1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 14:55:36 +0000

    d0c7ea8 refactor(core): fix MockRenderer in unit tests. (#63936)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-ba40153
+ * @license Angular v20.3.1+sha-d0c7ea8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.1+sha-ba40153');
+const VERSION = new Version('20.3.1+sha-d0c7ea8');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
