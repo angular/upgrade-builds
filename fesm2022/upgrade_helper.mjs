98af0b7a861e81418f03dda3e4694173bffaa9b0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 20:34:06 +0000

    b04e6b1 refactor(core): drop the usage of `PromiseSettledResult<void>` (#63955)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-de18875
+ * @license Angular v20.3.1+sha-b04e6b1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.1+sha-de18875');
+const VERSION = new Version('20.3.1+sha-b04e6b1');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
