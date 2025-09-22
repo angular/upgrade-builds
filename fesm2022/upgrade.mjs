cde957019ed49dee068a372a6b716a68416b0759
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-22 16:03:18 +0000

    3bd91de fix(compiler-cli): resolve import alias in defer blocks (#63966)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-a7e4851
+ * @license Angular v21.0.0-next.4+sha-3bd91de
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-a7e4851');
+const VERSION = new Version('21.0.0-next.4+sha-3bd91de');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
