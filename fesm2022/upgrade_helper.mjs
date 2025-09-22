8f0877333b33379b94424bc4139d40598e41cd08
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-22 14:54:22 +0000

    2d21524 build: lock file maintenance (#63985)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-ca104b3
+ * @license Angular v20.3.1+sha-2d21524
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.1+sha-ca104b3');
+const VERSION = new Version('20.3.1+sha-2d21524');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
