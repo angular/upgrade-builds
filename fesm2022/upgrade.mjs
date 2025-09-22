16479a423d0c47b68ebd7f5d8768a0c1a65faecd
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-22 16:59:34 +0000

    5406e1a refactor(core): use version>0 instead of hasRun (#62467)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-3bd91de
+ * @license Angular v21.0.0-next.4+sha-5406e1a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-3bd91de');
+const VERSION = new Version('21.0.0-next.4+sha-5406e1a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
