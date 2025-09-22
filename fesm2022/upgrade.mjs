ffb6f7ef79ed6cf5be2b55c5b528a4d91da1b134
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-22 14:45:55 +0000

    a2d98cc docs: add missing `signal` function (#63974)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-6782f71
+ * @license Angular v21.0.0-next.4+sha-a2d98cc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-6782f71');
+const VERSION = new Version('21.0.0-next.4+sha-a2d98cc');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
