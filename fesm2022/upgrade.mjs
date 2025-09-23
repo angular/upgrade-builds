356a68c9f3ac13f5e39e98ff09d6871388211d15
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-23 21:44:11 +0000

    f36a504 build: update pnpm to v10.17.1 (#64015)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-4230442
+ * @license Angular v21.0.0-next.4+sha-f36a504
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-4230442');
+const VERSION = new Version('21.0.0-next.4+sha-f36a504');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
