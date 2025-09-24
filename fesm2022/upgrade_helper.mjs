37c49e39aa975df8244fc315e68db4d23bc29cd3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 16:53:33 +0000

    1fd8d5d fix(forms): Emit `FormResetEvent` when resetting control (#64034)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-3ebaecc
+ * @license Angular v20.3.1+sha-1fd8d5d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.1+sha-3ebaecc');
+const VERSION = new Version('20.3.1+sha-1fd8d5d');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
