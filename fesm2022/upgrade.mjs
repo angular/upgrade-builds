8524a80d2bdbf5171b675292b99ca6c94517ac67
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 18:35:47 +0000

    0b2018d docs(docs-infra): align docs-step number to the heading (#64035)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-c0d88c3
+ * @license Angular v21.0.0-next.4+sha-0b2018d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-c0d88c3');
+const VERSION = new Version('21.0.0-next.4+sha-0b2018d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
