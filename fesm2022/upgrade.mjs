5a12c8f7586424ada791c1aa94579d33c7dc41e0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 16:08:29 +0000

    fffc7cf refactor(migrations): migrate usages of `getTestBed.initTestEnvironment` (#63997)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-7e8e310
+ * @license Angular v21.0.0-next.4+sha-fffc7cf
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-7e8e310');
+const VERSION = new Version('21.0.0-next.4+sha-fffc7cf');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
