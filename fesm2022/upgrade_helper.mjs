bed082477b0c80866a62dae47557d9b8cec4f9f4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 18:36:08 +0000

    ce6c7d9 docs(docs-infra): align docs-step number to the heading (#64035)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-1fd8d5d
+ * @license Angular v20.3.1+sha-ce6c7d9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.1+sha-1fd8d5d');
+const VERSION = new Version('20.3.1+sha-ce6c7d9');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
