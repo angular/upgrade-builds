1dbd53a8b9df83ac2e098e0d7fac79f90da27055
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 14:55:25 +0000

    7a7bc27 ci: use release config when deploying adev (#64030)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-3a9b8d8
+ * @license Angular v21.0.0-next.4+sha-7a7bc27
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-3a9b8d8');
+const VERSION = new Version('21.0.0-next.4+sha-7a7bc27');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
