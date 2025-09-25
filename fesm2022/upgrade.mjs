21db083f2bab3c7233859c6abde9c545edd017b3
kirjs <kirts@google.com>
2025-09-25 20:34:37 +0000

    e2fd79b docs: add warning to `AbstractControl.setErrors` (#64063)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-6fee508
+ * @license Angular v21.0.0-next.5+sha-e2fd79b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-6fee508');
+const VERSION = new Version('21.0.0-next.5+sha-e2fd79b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
