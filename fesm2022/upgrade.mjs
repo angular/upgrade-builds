bb3b685c7d7a51e6e0b762baea94f1513bb0b781
kirjs <kirts@google.com>
2025-09-25 19:07:43 +0000

    a45e6fd fix(devtools): signal graph effect node (#64076)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-497dce9
+ * @license Angular v21.0.0-next.5+sha-a45e6fd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-497dce9');
+const VERSION = new Version('21.0.0-next.5+sha-a45e6fd');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
