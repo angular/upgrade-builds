1f653c1bdc5461bd838c4fc6dc3dcb8ea0ea5cc8
kirjs <kirts@google.com>
2025-09-25 14:30:48 +0000

    95fa943 refactor: update dependencies and build configurations (#64073)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-0e8e7ba
+ * @license Angular v21.0.0-next.5+sha-95fa943
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-0e8e7ba');
+const VERSION = new Version('21.0.0-next.5+sha-95fa943');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
