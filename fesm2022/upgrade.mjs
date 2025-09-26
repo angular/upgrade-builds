967da4667dd27278a658e701d930d1a399dce596
kirjs <kirts@google.com>
2025-09-26 13:42:04 +0000

    597d6e7 docs: add StateOfJS survey banner (#64083)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-14d5b0e
+ * @license Angular v21.0.0-next.5+sha-597d6e7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-14d5b0e');
+const VERSION = new Version('21.0.0-next.5+sha-597d6e7');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
