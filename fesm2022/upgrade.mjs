89078fba570ca6ab3974a7e9ca3c38486df2ef9e
kirjs <kirts@google.com>
2025-09-29 16:05:57 +0000

    78cee8e ci: correctly check for outputPaths to exist before deleting them (#64137)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-768a09d
+ * @license Angular v21.0.0-next.5+sha-78cee8e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-768a09d');
+const VERSION = new Version('21.0.0-next.5+sha-78cee8e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
