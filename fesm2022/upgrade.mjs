ec9c390e43ce48b92ff64559ee72c5f29e783247
kirjs <kirts@google.com>
2025-09-29 19:41:34 +0000

    04462ed refactor(compiler): Remove the interpolation config (#64071)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-fc643c9
+ * @license Angular v21.0.0-next.5+sha-04462ed
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-fc643c9');
+const VERSION = new Version('21.0.0-next.5+sha-04462ed');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
