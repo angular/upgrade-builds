9550b6d31a3194121cded00914523b842a002327
kirjs <kirts@google.com>
2025-09-29 20:52:58 +0000

    9a913dc docs(docs-infra): clean up code and use inferred types where possible (#64139)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-04462ed
+ * @license Angular v21.0.0-next.5+sha-9a913dc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-04462ed');
+const VERSION = new Version('21.0.0-next.5+sha-9a913dc');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
