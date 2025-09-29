41daaf71d0049083cbe44b2276d6847e4c8211e3
kirjs <kirts@google.com>
2025-09-29 17:56:25 +0000

    5c50e0b build: update cross-repo angular dependencies (#64115)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.2+sha-aa28aa2
+ * @license Angular v20.3.2+sha-5c50e0b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.2+sha-aa28aa2');
+const VERSION = new Version('20.3.2+sha-5c50e0b');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
