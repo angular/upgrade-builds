c17ffa5df0ae1b54d72c0bc213bf4043f9675e27
kirjs <kirts@google.com>
2025-09-29 17:15:14 +0000

    4b3f7a0 refactor(devtools): guard value reads in the serializer (#64096)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.2+sha-633db24
+ * @license Angular v20.3.2+sha-4b3f7a0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.2+sha-633db24');
+const VERSION = new Version('20.3.2+sha-4b3f7a0');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
