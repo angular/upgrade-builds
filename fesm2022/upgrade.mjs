f306f119f904f1ab30c0d5b6531456f6198009ff
kirjs <kirts@google.com>
2025-09-30 19:01:33 +0000

    c3a949a refactor(compiler-cli): include linkedSignal in the signal debugName transformer (#64106)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-2aca6da
+ * @license Angular v21.0.0-next.5+sha-c3a949a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-2aca6da');
+const VERSION = new Version('21.0.0-next.5+sha-c3a949a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
