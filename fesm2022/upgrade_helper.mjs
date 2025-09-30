e6f8fb6bf28f911319d309bd9290b9588822941f
kirjs <kirts@google.com>
2025-09-30 17:56:15 +0000

    8e80083 refactor(core): Add custom formatters for Signals (#64000)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.2+sha-c1a922a
+ * @license Angular v20.3.2+sha-8e80083
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.2+sha-c1a922a');
+const VERSION = new Version('20.3.2+sha-8e80083');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
