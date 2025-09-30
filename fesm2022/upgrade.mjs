f800b431172430e838844cd595c6eca61c2e54a6
kirjs <kirts@google.com>
2025-09-30 17:41:00 +0000

    899c25f docs: add mention of `host-context()` being spec deprecated but still supported (#64153)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-20bb459
+ * @license Angular v21.0.0-next.5+sha-899c25f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-20bb459');
+const VERSION = new Version('21.0.0-next.5+sha-899c25f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
