0d995df7238811c74f2de6b3a0731f7d1fe14a02
Miles Malerba <mmalerba@google.com>
2025-10-01 22:47:14 +0000

    d892aab Revert "fix(core): Fixes animations in conjunction with content projection (#63776)" (#64189)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-be0455a
+ * @license Angular v21.0.0-next.5+sha-d892aab
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-be0455a');
+const VERSION = new Version('21.0.0-next.5+sha-d892aab');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
