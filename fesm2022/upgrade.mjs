2fe6d9e49c8363ccd739cf761cddb7b72ea693fd
kirjs <kirts@google.com>
2025-10-01 13:46:19 +0000

    3bd4362 docs: update APF docs for new `.d.ts` layout (#64168)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-1cb16fd
+ * @license Angular v21.0.0-next.5+sha-3bd4362
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-1cb16fd');
+const VERSION = new Version('21.0.0-next.5+sha-3bd4362');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
