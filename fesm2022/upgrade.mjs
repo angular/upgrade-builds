badeff64430799084ce98a39ceda6d20a9d9fdd9
kirjs <kirts@google.com>
2025-10-01 17:18:13 +0000

    ee7d49a docs: update Angular CLI help (#64174)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-0537083
+ * @license Angular v21.0.0-next.5+sha-ee7d49a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-0537083');
+const VERSION = new Version('21.0.0-next.5+sha-ee7d49a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
