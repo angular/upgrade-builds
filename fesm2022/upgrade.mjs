75daa4a18ffd6becec3e5df9b635ae4a768eeb1f
kirjs <kirts@google.com>
2025-10-01 17:49:51 +0000

    c0e1c41 build: update ossf/scorecard-action action to v2.4.3 (#64164)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-851c07b
+ * @license Angular v21.0.0-next.5+sha-c0e1c41
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-851c07b');
+const VERSION = new Version('21.0.0-next.5+sha-c0e1c41');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
