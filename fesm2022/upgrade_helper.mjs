85ea820afa0db1f8be5b342ebf6c68b6b8a58718
Miles Malerba <mmalerba@google.com>
2025-10-01 22:47:58 +0000

    a453b1d Revert "fix(core): Fixes animations in conjunction with content projection (#63776)" (#64189)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.2+sha-952e3b1
+ * @license Angular v20.3.2+sha-a453b1d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.2+sha-952e3b1');
+const VERSION = new Version('20.3.2+sha-a453b1d');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
