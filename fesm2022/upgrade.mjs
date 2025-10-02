37359e5b488bfac977e601ee8d3b6e9bdbc6e33e
Miles Malerba <mmalerba@google.com>
2025-10-02 20:52:56 +0000

    e23815b build: update dependency typescript to v5.9.3 (#64207)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-4704863
+ * @license Angular v21.0.0-next.6+sha-e23815b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.6+sha-4704863');
+const VERSION = new Version('21.0.0-next.6+sha-e23815b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
