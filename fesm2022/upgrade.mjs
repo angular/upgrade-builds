d7f2bc2f208203e3a21d46f428b18c6b10605f38
Miles Malerba <mmalerba@google.com>
2025-10-02 17:36:07 +0000

    5e61e8d fix(router): Fix memory leak through Navigation.abort and canDeactivate guards (#64141)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-9eac43c
+ * @license Angular v21.0.0-next.6+sha-5e61e8d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.6+sha-9eac43c');
+const VERSION = new Version('21.0.0-next.6+sha-5e61e8d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
