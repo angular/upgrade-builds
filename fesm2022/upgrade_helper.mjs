6932db5a2aaccc3f4a71ea7d300505220d9aac52
Miles Malerba <mmalerba@google.com>
2025-10-02 17:35:32 +0000

    e2820d1 docs(docs-infra): keep file name visible when code block is hidden (#64204)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.3+sha-6fed986
+ * @license Angular v20.3.3+sha-e2820d1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.3+sha-6fed986');
+const VERSION = new Version('20.3.3+sha-e2820d1');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
