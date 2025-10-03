7a9269e0cbf6d3794c872e967b87fab60d4ae2c3
Miles Malerba <mmalerba@google.com>
2025-10-03 17:23:37 +0000

    ab780a7 docs: add info about disabling request caching (#64216)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-e403078
+ * @license Angular v21.0.0-next.6+sha-ab780a7
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.6+sha-e403078');
+const VERSION = new Version('21.0.0-next.6+sha-ab780a7');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
