88d7acbb2ee5457864e42bfb220f4dfaad155d71
Miles Malerba <mmalerba@google.com>
2025-10-06 19:16:01 +0000

    1ffc643 docs(docs-infra): fix @example in class members (#64249)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-fd8383a
+ * @license Angular v21.0.0-next.6+sha-1ffc643
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.6+sha-fd8383a');
+const VERSION = new Version('21.0.0-next.6+sha-1ffc643');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
