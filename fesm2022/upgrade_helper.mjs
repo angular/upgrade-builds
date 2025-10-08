f5965d88cb266244565fed6388f2df9bf692c7f3
Miles Malerba <mmalerba@google.com>
2025-10-08 00:50:32 +0000

    11d83d1 build: update cross-repo angular dependencies (#64272)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.3+sha-67bffa2
+ * @license Angular v20.3.3+sha-11d83d1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.3+sha-67bffa2');
+const VERSION = new Version('20.3.3+sha-11d83d1');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
