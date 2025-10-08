2b809592827cede03ac41873bdd8a9cdd9c429fc
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-10-08 08:23:56 +0000

    47b66b1 release: bump Angular DevTools version to 1.4.0 (#64287)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-5cbdefc
+ * @license Angular v21.0.0-next.6+sha-47b66b1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.6+sha-5cbdefc');
+const VERSION = new Version('21.0.0-next.6+sha-47b66b1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
