6aae3a3d88dabbd5aaeae8b4a1b2d6a6cf564f0c
Miles Malerba <mmalerba@google.com>
2025-10-08 15:03:42 +0000

    808d6ae release: cut the v21.0.0-next.7 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-ec4775d
+ * @license Angular v21.0.0-next.7+sha-808d6ae
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.6+sha-ec4775d');
+const VERSION = new Version('21.0.0-next.7+sha-808d6ae');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
