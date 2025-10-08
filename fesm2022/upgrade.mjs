a5e0324d75bed5791463da3509f3224b8640b8ba
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-10-08 10:16:32 +0000

    76c8d04 build: update `rules_sass` to `1184a80751a21af8348f308abc5b38a41f26850e` (#64288)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-47b66b1
+ * @license Angular v21.0.0-next.6+sha-76c8d04
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.6+sha-47b66b1');
+const VERSION = new Version('21.0.0-next.6+sha-76c8d04');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
