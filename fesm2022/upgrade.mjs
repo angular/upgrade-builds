c2e707331df4c8224533e47072983e5ee0cefb86
Andrew Kushnir <akushnir@google.com>
2025-10-09 18:18:51 +0000

    e1da41f fix(router): Scroll restoration should use instant scroll behavior for traversals (#64299)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-9af3cf7
+ * @license Angular v21.0.0-next.7+sha-e1da41f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.7+sha-9af3cf7');
+const VERSION = new Version('21.0.0-next.7+sha-e1da41f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
