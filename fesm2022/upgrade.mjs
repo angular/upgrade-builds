6bedd5df8e741df632ab6d6ac3fc5a780b8849bb
Andrew Kushnir <akushnir@google.com>
2025-10-09 17:26:33 +0000

    9af3cf7 build: migrate to in-repo ts_project with strict deps (#64306)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-22a1001
+ * @license Angular v21.0.0-next.7+sha-9af3cf7
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.7+sha-22a1001');
+const VERSION = new Version('21.0.0-next.7+sha-9af3cf7');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
