8a7af13cd3bc11e77b8ac3ac7e64368b2dcfd9b1
Andrew Kushnir <akushnir@google.com>
2025-10-10 15:18:40 +0000

    da50733 docs: add a callout that adding/removing to formArray doesn't not mark dirty (#64337)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.4+sha-da38cd9
+ * @license Angular v20.3.4+sha-da50733
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.4+sha-da38cd9');
+const VERSION = new Version('20.3.4+sha-da50733');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
