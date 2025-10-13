a23aeb415bf7a6523cbed83e4fb1bbf8433b9437
Andrew Kushnir <akushnir@google.com>
2025-10-13 15:47:28 +0000

    84f6e36 build: update pnpm to v10.18.2 (#64358)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-e34776a
+ * @license Angular v21.0.0-next.7+sha-84f6e36
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.7+sha-e34776a');
+const VERSION = new Version('21.0.0-next.7+sha-84f6e36');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
