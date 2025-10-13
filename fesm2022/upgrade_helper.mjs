3bfef47afb8b0fb155af4301b0df57b999c6d667
Andrew Kushnir <akushnir@google.com>
2025-10-13 15:49:45 +0000

    9cbe5b6 build: update pnpm to v10.18.2 (#64359)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.4+sha-da50733
+ * @license Angular v20.3.4+sha-9cbe5b6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.4+sha-da50733');
+const VERSION = new Version('20.3.4+sha-9cbe5b6');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
