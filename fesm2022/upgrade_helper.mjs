3234e9c1ade885ea159fa60a7cda82e9df96239b
Andrew Kushnir <akushnir@google.com>
2025-10-13 16:06:53 +0000

    b4b19b6 refactor(compiler): Improve pipe in track expression error (#64321)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.4+sha-9cbe5b6
+ * @license Angular v20.3.4+sha-b4b19b6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.4+sha-9cbe5b6');
+const VERSION = new Version('20.3.4+sha-b4b19b6');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
