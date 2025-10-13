a648eb637958eebd1498838b34e9887b9ff7f9f5
Andrew Kushnir <akushnir@google.com>
2025-10-13 16:09:19 +0000

    62cda78 refactor(compiler): Improve pipe in track expression error (#64321)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-84f6e36
+ * @license Angular v21.0.0-next.7+sha-62cda78
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.7+sha-84f6e36');
+const VERSION = new Version('21.0.0-next.7+sha-62cda78');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
