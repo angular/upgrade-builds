7ca06d3a60665664178e2619bc7cb04cf856c7a3
Andrew Kushnir <akushnir@google.com>
2025-10-13 20:09:56 +0000

    32f86d3 perf(forms): optimize `[field]` binding instructions (#64351)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-6a96844
+ * @license Angular v21.0.0-next.7+sha-32f86d3
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.7+sha-6a96844');
+const VERSION = new Version('21.0.0-next.7+sha-32f86d3');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
