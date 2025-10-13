dbea5822470462bb748773cc4eb1566daa4afa34
Andrew Kushnir <akushnir@google.com>
2025-10-13 17:05:39 +0000

    eee8eab docs: improve clarity of self-closing tag migration example (#64388)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-09a927a
+ * @license Angular v21.0.0-next.7+sha-eee8eab
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.7+sha-09a927a');
+const VERSION = new Version('21.0.0-next.7+sha-eee8eab');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
