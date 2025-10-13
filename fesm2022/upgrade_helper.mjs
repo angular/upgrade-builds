59142f276fad330762930dcfdb59c65e03fbcbfb
Andrew Kushnir <akushnir@google.com>
2025-10-13 17:05:36 +0000

    e54f41e docs: improve clarity of self-closing tag migration example (#64388)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.4+sha-51066cc
+ * @license Angular v20.3.4+sha-e54f41e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.4+sha-51066cc');
+const VERSION = new Version('20.3.4+sha-e54f41e');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
