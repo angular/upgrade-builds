a8f71a2b5c25945d7a50873b839b2c8dbbf685c4
Andrew Kushnir <akushnir@google.com>
2025-10-14 16:42:58 +0000

    11b006e refactor(compiler-cli): add a resource debugName transform (#64172)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.4+sha-a2773b5
+ * @license Angular v20.3.4+sha-11b006e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.4+sha-a2773b5');
+const VERSION = new Version('20.3.4+sha-11b006e');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
