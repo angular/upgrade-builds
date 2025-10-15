a948334532941d4edc5f861acc02500187f9e004
Andrew Kushnir <akushnir@google.com>
2025-10-15 18:51:36 +0000

    9b9ae42 docs: update Angular CLI help (#64433)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-3e8e48c
+ * @license Angular v21.0.0-next.8+sha-9b9ae42
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.8+sha-3e8e48c');
+const VERSION = new Version('21.0.0-next.8+sha-9b9ae42');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
