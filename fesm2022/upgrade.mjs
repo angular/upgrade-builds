7b3b1afdb2583d81e5568529fe5a5b83aac77d57
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-16 14:47:06 +0000

    6af231e refactor(devtools): Replace console warnings with snackbar notifications (#64458)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-a7ba2f5
+ * @license Angular v21.0.0-next.8+sha-6af231e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.8+sha-a7ba2f5');
+const VERSION = new Version('21.0.0-next.8+sha-6af231e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
