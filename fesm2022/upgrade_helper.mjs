1396331d8af25e7651c2106754dad1d2d1cee464
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-16 17:49:03 +0000

    911d682 fix(core): update animation scheduling (#64441)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.5+sha-2d159af
+ * @license Angular v20.3.5+sha-911d682
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.5+sha-2d159af');
+const VERSION = new Version('20.3.5+sha-911d682');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
