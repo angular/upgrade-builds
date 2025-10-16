70965578fe05aa00462b15106114ac43c0c5e7ff
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-16 18:57:51 +0000

    b99d028 refactor(platform-browser): remove unecessary export. (#64469)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.5+sha-d342601
+ * @license Angular v20.3.5+sha-b99d028
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.5+sha-d342601');
+const VERSION = new Version('20.3.5+sha-b99d028');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
