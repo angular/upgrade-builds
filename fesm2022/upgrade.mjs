ea01da1ed8adbe4ee33d1900ca9eed9aeeac9093
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-20 14:45:54 +0000

    810505f build: update github/codeql-action action to v4.30.9 (#64520)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-c60ab33
+ * @license Angular v21.0.0-next.8+sha-810505f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.8+sha-c60ab33');
+const VERSION = new Version('21.0.0-next.8+sha-810505f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
