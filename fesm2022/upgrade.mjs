6b27019a18f301ad0c2819bda0131a75c2f8ff8c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-20 15:44:57 +0000

    e464aac build: bump leftover dependency of typescript (#64543)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-9c7029c
+ * @license Angular v21.0.0-next.8+sha-e464aac
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.8+sha-9c7029c');
+const VERSION = new Version('21.0.0-next.8+sha-e464aac');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
