ec10d3e8e0fbe019062ca2d2f5be4ca3f062a81c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-20 17:57:28 +0000

    182fe78 fix(router): Surface parse errors in Router.parseUrl (#64503)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.6+sha-4063082
+ * @license Angular v20.3.6+sha-182fe78
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.6+sha-4063082');
+const VERSION = new Version('20.3.6+sha-182fe78');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
