811f2131b5da04447b9d75738686cc334a80b2a7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-20 16:20:39 +0000

    fad6e13 build: update all non-major dependencies (#64514)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-aca2f75
+ * @license Angular v21.0.0-next.8+sha-fad6e13
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.8+sha-aca2f75');
+const VERSION = new Version('21.0.0-next.8+sha-fad6e13');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
