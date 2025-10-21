491340a5124122f3492fbef0244344d329f3cea7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-21 16:48:27 +0000

    50e2c54 build: update rules_browsers digest to 6a699bf (#64559)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.6+sha-e568ddb
+ * @license Angular v20.3.6+sha-50e2c54
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.6+sha-e568ddb');
+const VERSION = new Version('20.3.6+sha-50e2c54');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
