c1562d2725db388092b484f1559329e609bdc9f7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-22 19:11:54 +0000

    81bd671 fix(core): prevent duplicate nodes from being retained with fast `animate.leave`` calls (#64592)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-a3e91b2
+ * @license Angular v21.0.0-next.8+sha-81bd671
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.8+sha-a3e91b2');
+const VERSION = new Version('21.0.0-next.8+sha-81bd671');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
