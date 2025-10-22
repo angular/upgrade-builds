24f9cac22cadf861b918b239d163bfb806fc10a0
Andrew Scott <atscott01@gmail.com>
2025-10-22 23:31:16 +0000

    292b9e3 refactor(zone.js): Improve missing proxy zone error for jest imported (#64497) test functions

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.9+sha-de2ee6e
+ * @license Angular v21.0.0-next.9+sha-292b9e3
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.9+sha-de2ee6e');
+const VERSION = new Version('21.0.0-next.9+sha-292b9e3');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
