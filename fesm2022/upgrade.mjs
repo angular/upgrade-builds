5345c1056b4b570c3098ad9d6b37d795ca8ad230
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-22 22:02:02 +0000

    de2ee6e docs: update Angular CDK apis (#64599)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.9+sha-d183153
+ * @license Angular v21.0.0-next.9+sha-de2ee6e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.9+sha-d183153');
+const VERSION = new Version('21.0.0-next.9+sha-de2ee6e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
