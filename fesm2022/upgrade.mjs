ea4452d327c22d1f6356b4cd06b03a1a0cff53d3
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-23 16:17:50 +0000

    ef34e39 refactor(forms): rename files related to metadata (#64603)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-6a3e881
+ * @license Angular v21.1.0-next.0+sha-ef34e39
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.1.0-next.0+sha-6a3e881');
+const VERSION = new Version('21.1.0-next.0+sha-ef34e39');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
