6cd016e782fc025fce64836829da00a3bd8c20a3
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-23 16:34:24 +0000

    e47ef3e docs: update Angular CLI help (#64621)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-ef34e39
+ * @license Angular v21.1.0-next.0+sha-e47ef3e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.1.0-next.0+sha-ef34e39');
+const VERSION = new Version('21.1.0-next.0+sha-e47ef3e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
