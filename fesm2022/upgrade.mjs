2071386a11f323ec5ae1bf0cd6dec39578b8f0f2
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-24 19:33:31 +0000

    b5f8d0b fix(docs-infra): fix broken adev tests

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-620e2c4
+ * @license Angular v21.1.0-next.0+sha-b5f8d0b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.1.0-next.0+sha-620e2c4');
+const VERSION = new Version('21.1.0-next.0+sha-b5f8d0b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
