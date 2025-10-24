e1451c4d6636e9380c7ec32c906350919d35da34
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-24 17:07:51 +0000

    6daabb0 Revert "build: refactor location of best-practices.md"

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.7+sha-07d91d7
+ * @license Angular v20.3.7+sha-6daabb0
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.7+sha-07d91d7');
+const VERSION = new Version('20.3.7+sha-6daabb0');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
