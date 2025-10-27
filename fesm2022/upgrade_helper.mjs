f5e408c8921f2e706d4ea836b46530a0d333850b
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-27 18:50:38 +0000

    ca3fd78 fix(core): Clear lView from IcuIteratorState when stack is empty to prevent memory leak

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.7+sha-14b5590
+ * @license Angular v20.3.7+sha-ca3fd78
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.7+sha-14b5590');
+const VERSION = new Version('20.3.7+sha-ca3fd78');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
