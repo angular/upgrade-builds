8a5e40eca76d0198373134ecd8670b18c674a3bf
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-27 08:34:32 +0000

    14b5590 fix(docs-infra): preserve shell class for multifile code blocks

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.7+sha-6daabb0
+ * @license Angular v20.3.7+sha-14b5590
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.7+sha-6daabb0');
+const VERSION = new Version('20.3.7+sha-14b5590');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
