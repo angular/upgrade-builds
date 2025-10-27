b680555cb4fdbb8517e8623072ab4e16a66f2de9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-27 08:40:58 +0000

    25e8bcb build: refactor location of best-practices.md

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-b5f8d0b
+ * @license Angular v21.1.0-next.0+sha-25e8bcb
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.1.0-next.0+sha-b5f8d0b');
+const VERSION = new Version('21.1.0-next.0+sha-25e8bcb');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
