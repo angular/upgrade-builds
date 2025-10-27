dff62568f911aad548e144312f51a7463df44977
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-27 18:50:18 +0000

    59e6489 fix(core): Clear lView from IcuIteratorState when stack is empty to prevent memory leak

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-b8c8bc6
+ * @license Angular v21.1.0-next.0+sha-59e6489
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-b8c8bc6');
+const VERSION = new Version('21.1.0-next.0+sha-59e6489');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
