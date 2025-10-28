44cffc034c86eff11673d0011627f5759e58e964
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-28 20:02:53 +0000

    6e004ca refactor(docs-infra): switch to unit test builder for unit tests

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-next.9+sha-b41a070
+ * @license Angular v21.0.0-next.9+sha-6e004ca
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-next.9+sha-b41a070');
+const VERSION = new Version('21.0.0-next.9+sha-6e004ca');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
