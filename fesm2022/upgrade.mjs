206bd990b5680f66fb55024a587b0a6b5e5eb4f7
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-28 16:00:57 +0000

    d1d5c72 docs(docs-infra): allow cors for json files

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-next.9+sha-4f21ea7
+ * @license Angular v21.0.0-next.9+sha-d1d5c72
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-next.9+sha-4f21ea7');
+const VERSION = new Version('21.0.0-next.9+sha-d1d5c72');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
