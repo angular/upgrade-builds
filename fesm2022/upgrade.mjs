911beecb3fc06c9f054a28480145388b702eb86f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-28 15:08:40 +0000

    8e50cdb refactor(compiler-cli): Remove deep imports of `compiler-cli` in angular/core

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-07b8e95
+ * @license Angular v21.1.0-next.0+sha-8e50cdb
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-07b8e95');
+const VERSION = new Version('21.1.0-next.0+sha-8e50cdb');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
