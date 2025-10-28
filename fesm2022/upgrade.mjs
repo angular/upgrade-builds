8d7f278c8c16926332a59e155b32472314a672bd
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-28 17:40:31 +0000

    5a93eeb Revert "feat(router): Support wildcard params with segments trailing"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-3032332
+ * @license Angular v21.1.0-next.0+sha-5a93eeb
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-3032332');
+const VERSION = new Version('21.1.0-next.0+sha-5a93eeb');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
