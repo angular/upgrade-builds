7408138029b4d2f3c017f0ce5429d548170cf5b2
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-28 15:02:47 +0000

    766559e fix(docs-infra): update guide navigation to remain active with query params

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.7+sha-92d1897
+ * @license Angular v20.3.7+sha-766559e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.7+sha-92d1897');
+const VERSION = new Version('20.3.7+sha-766559e');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
