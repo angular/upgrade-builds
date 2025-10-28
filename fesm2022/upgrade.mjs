d2cbd410bd30716e96d9bd9bce4ef4f1004eac42
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-28 14:14:40 +0000

    3fbaaa0 refactor(http): add hooks for propagating traces across XHR callbacks.

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-next.9+sha-81b8765
+ * @license Angular v21.0.0-next.9+sha-3fbaaa0
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-next.9+sha-81b8765');
+const VERSION = new Version('21.0.0-next.9+sha-3fbaaa0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
