0896f65cbf6faf0050635eb1b6fb10ef5d8fc9bd
Andrew Scott <atscott@google.com>
2025-10-29 20:43:25 +0000

    81b18fc ci: add workflow to copy and generate aria api pages

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-20b4b2c
+ * @license Angular v21.0.0-rc.0+sha-81b18fc
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.0+sha-20b4b2c');
+const VERSION = new Version('21.0.0-rc.0+sha-81b18fc');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
