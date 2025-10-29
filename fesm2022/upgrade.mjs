1d0b09ba8eace4e50fbabe4b65a4e5d56823b788
Andrew Scott <atscott@google.com>
2025-10-29 20:37:53 +0000

    20b4b2c refactor(core): expose isWritableSignal to the publicApi

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-981fabc
+ * @license Angular v21.0.0-rc.0+sha-20b4b2c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.0+sha-981fabc');
+const VERSION = new Version('21.0.0-rc.0+sha-20b4b2c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
