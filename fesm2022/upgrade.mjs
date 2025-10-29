16d160f251182aab440922aeeeef02be981e0acc
GitHub <noreply@github.com>
2025-10-29 20:27:54 +0000

    662f0e5 feat(docs-infra): add support for namespaces

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-6cc6d93
+ * @license Angular v21.1.0-next.0+sha-662f0e5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-6cc6d93');
+const VERSION = new Version('21.1.0-next.0+sha-662f0e5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
