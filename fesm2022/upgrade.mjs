ba7a8ed0ff197678b82ce37cee1e413700c7bd2d
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-29 09:55:49 +0000

    95344c1 fix(migrations): Do not remove a template if it is referenced even with a trailing semilocon

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-next.9+sha-6e004ca
+ * @license Angular v21.0.0-next.9+sha-95344c1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-next.9+sha-6e004ca');
+const VERSION = new Version('21.0.0-next.9+sha-95344c1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
