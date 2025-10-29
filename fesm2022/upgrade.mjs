790e0f06e428ba9d2a6d0005f67211a05b8d73ee
Andrew Scott <atscott@google.com>
2025-10-29 20:53:45 +0000

    366392b refactor(compiler): desambiguate and export `RegularExpressionLiteralExpr`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-81b18fc
+ * @license Angular v21.0.0-rc.0+sha-366392b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.0+sha-81b18fc');
+const VERSION = new Version('21.0.0-rc.0+sha-366392b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
