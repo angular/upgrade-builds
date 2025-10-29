9b619af5dc2c9035ddc13108372ca4cd0d03ec5c
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-29 13:16:19 +0000

    9123e3d fix(docs-infra): fix broken adev tests (#64761)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.7+sha-2edc82a
+ * @license Angular v20.3.7+sha-9123e3d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.7+sha-2edc82a');
+const VERSION = new Version('20.3.7+sha-9123e3d');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
