e8ddf0c246628e796998fc4a1d06f9ec9ab90948
Andrew Scott <atscott01@gmail.com>
2025-10-29 21:02:43 +0000

    fb569ef refactor(core): Move patch toggles inside functions to allow tree shaking

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-cfba831
+ * @license Angular v21.1.0-next.0+sha-fb569ef
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-cfba831');
+const VERSION = new Version('21.1.0-next.0+sha-fb569ef');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
