bc61a795f65bd7c5c774c086df68691d0073d323
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-29 17:54:06 +0000

    54ad3f6 docs: Rename TodoListItem to UserProfile and fix usage of `isValidUserId`

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.9+sha-b517c9c
+ * @license Angular v20.3.9+sha-54ad3f6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.9+sha-b517c9c');
+const VERSION = new Version('20.3.9+sha-54ad3f6');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
