3ba9dbbcd3ca4c4efd6f4a57e3437d3fcfff5554
Andrew Scott <atscott@google.com>
2025-10-29 20:30:32 +0000

    981fabc feat(docs-infra): add support for namespaces

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-2a06e2b
+ * @license Angular v21.0.0-rc.0+sha-981fabc
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.0+sha-2a06e2b');
+const VERSION = new Version('21.0.0-rc.0+sha-981fabc');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
