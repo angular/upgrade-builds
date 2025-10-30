48e471dc8daea86b5ec3b5173b10517fa3adec46
Andrew Scott <atscott01@gmail.com>
2025-10-30 18:21:17 +0000

    29d6eba docs: update Angular Aria apis (#64782)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-56f26f1
+ * @license Angular v21.1.0-next.0+sha-29d6eba
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-56f26f1');
+const VERSION = new Version('21.1.0-next.0+sha-29d6eba');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
