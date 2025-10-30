1af70964a4f4986e5be0305ace6337f638b22aa3
Andrew Scott <atscott01@gmail.com>
2025-10-30 19:27:14 +0000

    fee4baf docs: update dependency injection guides (part 1) (#63652)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-d23bb3c
+ * @license Angular v21.0.0-rc.0+sha-fee4baf
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.0+sha-d23bb3c');
+const VERSION = new Version('21.0.0-rc.0+sha-fee4baf');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
