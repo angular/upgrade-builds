b8fd13eaeedd03581e6ac41e7a878cd3d0dd82c5
Andrew Scott <atscott01@gmail.com>
2025-10-30 19:27:17 +0000

    f321e22 docs: update dependency injection guides (part 1) (#63652)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-29d6eba
+ * @license Angular v21.1.0-next.0+sha-f321e22
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-29d6eba');
+const VERSION = new Version('21.1.0-next.0+sha-f321e22');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
