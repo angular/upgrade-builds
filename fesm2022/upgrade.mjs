53c45c39b1c55f01fa8dd0e288175662073beacf
Andrew Scott <atscott01@gmail.com>
2025-10-30 19:38:39 +0000

    a4fe078 refactor(common): remove unused import (#64699)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-fee4baf
+ * @license Angular v21.0.0-rc.0+sha-a4fe078
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.0+sha-fee4baf');
+const VERSION = new Version('21.0.0-rc.0+sha-a4fe078');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
