4beace0d12e45a152cf9fe99af3ea5ac4fe2df5e
Andrew Scott <atscott@google.com>
2025-10-30 19:45:16 +0000

    a44083a docs(devtools): Removes comment about enum deprecation

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-a4fe078
+ * @license Angular v21.0.0-rc.0+sha-a44083a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.0+sha-a4fe078');
+const VERSION = new Version('21.0.0-rc.0+sha-a44083a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
