e71103399d0707ed21d0d853c13f4b7e3a38f335
Andrew Scott <atscott01@gmail.com>
2025-10-30 17:54:55 +0000

    56f26f1 docs(docs-infra): allow cors for json files (#64783)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-fd09a69
+ * @license Angular v21.1.0-next.0+sha-56f26f1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-fd09a69');
+const VERSION = new Version('21.1.0-next.0+sha-56f26f1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
