b13d295d5897b5bc6b3c0f2168d14f6a1279f0e8
Andrew Scott <atscott01@gmail.com>
2025-10-30 15:49:07 +0000

    c84d372 feat(router): Support wildcard params with segments trailing (#64737)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-b0c9c63
+ * @license Angular v21.1.0-next.0+sha-c84d372
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-b0c9c63');
+const VERSION = new Version('21.1.0-next.0+sha-c84d372');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
