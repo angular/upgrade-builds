14832fb52b71d353a5a38dd839cb0668302f9172
Andrew Scott <atscott01@gmail.com>
2025-10-30 17:29:51 +0000

    58f921d docs: add essential signal forms guide (#64665)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-90452f5
+ * @license Angular v21.0.0-rc.0+sha-58f921d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.0+sha-90452f5');
+const VERSION = new Version('21.0.0-rc.0+sha-58f921d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
