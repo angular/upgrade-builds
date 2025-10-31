8ae8748386847777443fdd9a718578e9955c3b91
Andrew Scott <atscott01@gmail.com>
2025-10-31 22:00:18 +0000

    334f856 build: update dependency bazel to v7.7.0

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-a0fe177
+ * @license Angular v21.1.0-next.0+sha-334f856
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-a0fe177');
+const VERSION = new Version('21.1.0-next.0+sha-334f856');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
