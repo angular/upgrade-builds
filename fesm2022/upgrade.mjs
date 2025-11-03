dcca48799bb09414fad11740d18e6a61ef57c2c9
Andrew Scott <atscott@google.com>
2025-11-03 21:06:44 +0000

    dc99f4c docs(docs-infra): ensure all redirections are absolute

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-32a166b
+ * @license Angular v21.0.0-rc.0+sha-dc99f4c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.0+sha-32a166b');
+const VERSION = new Version('21.0.0-rc.0+sha-dc99f4c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
