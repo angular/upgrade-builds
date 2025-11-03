866d55295868d576c77e2c63736acb9bc6d4ba45
Andrew Scott <atscott01@gmail.com>
2025-11-03 20:04:36 +0000

    bfdb652 docs: remove feature from changelog.

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-86d05eb
+ * @license Angular v21.1.0-next.0+sha-bfdb652
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-86d05eb');
+const VERSION = new Version('21.1.0-next.0+sha-bfdb652');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
