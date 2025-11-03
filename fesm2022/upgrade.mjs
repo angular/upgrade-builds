bf1ba1b53c87e7b9441dd1663f77a8159480743a
Andrew Scott <atscott@google.com>
2025-11-03 20:12:16 +0000

    5d75d0e refactor(core): Replace usages of the `Function` type for animations

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-303bff6
+ * @license Angular v21.0.0-rc.0+sha-5d75d0e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.0+sha-303bff6');
+const VERSION = new Version('21.0.0-rc.0+sha-5d75d0e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
