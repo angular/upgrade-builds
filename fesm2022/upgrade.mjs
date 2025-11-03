de0dd464ef36eae909646c896b14a8760c0c0a10
Andrew Scott <atscott01@gmail.com>
2025-11-03 19:49:36 +0000

    0cb1ade docs: add the version of angular being used to the footer

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-2922b41
+ * @license Angular v21.1.0-next.0+sha-0cb1ade
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-2922b41');
+const VERSION = new Version('21.1.0-next.0+sha-0cb1ade');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
