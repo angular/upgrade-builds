06f45eb16f9943c088e9e2c2a3cac8c8773af170
Andrew Scott <atscott01@gmail.com>
2025-11-04 00:32:06 +0000

    c4c7fb8 docs: remove standalone: true since it's now the default

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-7a422c2
+ * @license Angular v21.1.0-next.0+sha-c4c7fb8
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-7a422c2');
+const VERSION = new Version('21.1.0-next.0+sha-c4c7fb8');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
