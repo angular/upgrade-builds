7aa6e362aaba8b5eac308084be22ff300e344bc8
Andrew Scott <atscott01@gmail.com>
2025-11-04 23:02:54 +0000

    b5e37ff docs(docs-infra): add missing meta tags for author, keywords, and Twitter

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-b7839e8
+ * @license Angular v21.1.0-next.0+sha-b5e37ff
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-b7839e8');
+const VERSION = new Version('21.1.0-next.0+sha-b5e37ff');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
