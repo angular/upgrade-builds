fadfebeb9c6886641839a6f03b1a515967c39f88
Andrew Scott <atscott@google.com>
2025-11-04 04:27:05 +0000

    36f4c9b refactor(core): Removes unused flag for onDestroy

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-3140b87
+ * @license Angular v21.0.0-rc.0+sha-36f4c9b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.0+sha-3140b87');
+const VERSION = new Version('21.0.0-rc.0+sha-36f4c9b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
