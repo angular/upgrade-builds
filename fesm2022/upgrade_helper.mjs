efe527ec775c3a3470331e4e78454e54a30932de
Andrew Scott <atscott@google.com>
2025-11-04 17:44:24 +0000

    1b6d0cf docs(router): uniformize lazy loading syntax

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.9+sha-7438b9c
+ * @license Angular v20.3.9+sha-1b6d0cf
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.9+sha-7438b9c');
+const VERSION = new Version('20.3.9+sha-1b6d0cf');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
