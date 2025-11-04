a1d6e3c4f46eaf28f63c4d74453327f6cf9eb57f
Andrew Scott <atscott01@gmail.com>
2025-11-04 00:05:44 +0000

    a702c87 build: update cross-repo angular dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-6b3fdf3
+ * @license Angular v21.0.0-rc.0+sha-a702c87
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.0+sha-6b3fdf3');
+const VERSION = new Version('21.0.0-rc.0+sha-a702c87');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
