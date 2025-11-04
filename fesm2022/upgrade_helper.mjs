8222057306196a4e3cc255b31fdbcfc73ae42170
Andrew Scott <atscott@google.com>
2025-11-04 00:39:02 +0000

    4f7b97c docs: simplify tutorial signals with self-closing component tags

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.9+sha-3d6a206
+ * @license Angular v20.3.9+sha-4f7b97c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.9+sha-3d6a206');
+const VERSION = new Version('20.3.9+sha-4f7b97c');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
