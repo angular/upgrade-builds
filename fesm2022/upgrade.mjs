10e4d82bd1e1de678022e9bfff5c4f324b4e4e48
Andrew Scott <atscott@google.com>
2025-11-04 00:43:54 +0000

    3140b87 docs: simplify tutorial signals with self-closing component tags

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-34f53aa
+ * @license Angular v21.0.0-rc.0+sha-3140b87
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.0+sha-34f53aa');
+const VERSION = new Version('21.0.0-rc.0+sha-3140b87');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
