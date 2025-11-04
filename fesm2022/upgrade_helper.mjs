205fcf19e5fb68630bb43ae71399d0525faadbba
Andrew Scott <atscott@google.com>
2025-11-04 00:31:22 +0000

    3d6a206 docs: remove standalone: true since it's now the default

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.9+sha-192eb35
+ * @license Angular v20.3.9+sha-3d6a206
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.9+sha-192eb35');
+const VERSION = new Version('20.3.9+sha-3d6a206');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
