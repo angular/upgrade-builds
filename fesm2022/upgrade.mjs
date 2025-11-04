e2228fed6bb491aa8641ff14814cb6d33ffb3729
Andrew Scott <atscott@google.com>
2025-11-04 17:44:40 +0000

    3ccda42 docs(router): uniformize lazy loading syntax

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-36f4c9b
+ * @license Angular v21.0.0-rc.0+sha-3ccda42
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.0+sha-36f4c9b');
+const VERSION = new Version('21.0.0-rc.0+sha-3ccda42');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
