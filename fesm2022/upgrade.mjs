9fff0e6d1750eff680a8831ee98ce5639c73d535
Andrew Scott <atscott@google.com>
2025-11-04 23:31:03 +0000

    e1f933f fix(migrations): Prevent removal of templates referenced with preceding whitespace characters

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-9c59ea8
+ * @license Angular v21.0.0-rc.0+sha-e1f933f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.0+sha-9c59ea8');
+const VERSION = new Version('21.0.0-rc.0+sha-e1f933f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
