8dee54186ce364f8e85c0f764a3af04e65797ddc
Andrew Scott <atscott01@gmail.com>
2025-11-04 18:01:32 +0000

    7f16902 refactor(router): Update pre/post wildcard matching to retain 0 or more segment match meaning

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-8f49af9
+ * @license Angular v21.1.0-next.0+sha-7f16902
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-8f49af9');
+const VERSION = new Version('21.1.0-next.0+sha-7f16902');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
