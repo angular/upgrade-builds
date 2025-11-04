b0403a7c00186d61c2b18e0a09f74e96ea8e7d7e
Andrew Scott <atscott01@gmail.com>
2025-11-04 19:22:29 +0000

    9596020 docs: update cross-repo adev docs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-0b1339e
+ * @license Angular v21.0.0-rc.0+sha-9596020
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.0+sha-0b1339e');
+const VERSION = new Version('21.0.0-rc.0+sha-9596020');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
