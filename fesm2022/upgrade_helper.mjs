624a42794f98285efc8b22677f47673bea842e96
Andrew Scott <atscott@google.com>
2025-11-04 23:03:08 +0000

    c374e80 docs(docs-infra): add missing meta tags for author, keywords, and Twitter

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.9+sha-43f7f41
+ * @license Angular v20.3.9+sha-c374e80
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.9+sha-43f7f41');
+const VERSION = new Version('20.3.9+sha-c374e80');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
