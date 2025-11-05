138e55cfd59ee8bd4ba8f1c44922608305b6feb0
Andrew Scott <atscott@google.com>
2025-11-05 17:17:55 +0000

    47578e0 refactor(common): remove redundant `providedIn: 'root'` from injection tokens

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-507b346
+ * @license Angular v21.0.0-rc.0+sha-47578e0
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.0+sha-507b346');
+const VERSION = new Version('21.0.0-rc.0+sha-47578e0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
