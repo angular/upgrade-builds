7680c06b2b55f8f1349cff352ba632bb26b9ac07
Andrew Scott <atscott@google.com>
2025-11-05 19:52:10 +0000

    bf19fd4 docs: Add the missing function call to use the signal's actual value

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-24f9d76
+ * @license Angular v21.0.0-rc.1+sha-bf19fd4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.1+sha-24f9d76');
+const VERSION = new Version('21.0.0-rc.1+sha-bf19fd4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
