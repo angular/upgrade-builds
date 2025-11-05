014504d5f4abe4483c6297bd981334edba4675a3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-05 22:50:27 +0000

    4bdd317 docs: improve docs generation for namespaces (#64747)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-bf19fd4
+ * @license Angular v21.0.0-rc.1+sha-4bdd317
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.1+sha-bf19fd4');
+const VERSION = new Version('21.0.0-rc.1+sha-4bdd317');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
