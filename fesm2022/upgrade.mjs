8ee520ff7dbc99db9278c9b91d830f16b9717ea4
Alan Agius <alan.agius4@gmail.com>
2025-11-05 14:56:55 +0000

    1cd5b44 release: bump Angular DevTools version to 1.5.0

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-41be02d
+ * @license Angular v21.1.0-next.0+sha-1cd5b44
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-41be02d');
+const VERSION = new Version('21.1.0-next.0+sha-1cd5b44');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
