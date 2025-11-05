1ea4323fd353b41206b937d93ac9f7985ebb5f13
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-05 23:29:31 +0000

    de1f22c build: update diff release package script

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-9ae1f44
+ * @license Angular v21.1.0-next.0+sha-de1f22c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-9ae1f44');
+const VERSION = new Version('21.1.0-next.0+sha-de1f22c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
