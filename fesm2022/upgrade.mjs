5fa10eb55ad3427d0e56be6d51adfa862e40e0e3
Andrew Scott <atscott01@gmail.com>
2025-11-05 19:34:16 +0000

    24f9d76 release: cut the v21.0.0-rc.1 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-01c5ba5
+ * @license Angular v21.0.0-rc.1+sha-24f9d76
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.0+sha-01c5ba5');
+const VERSION = new Version('21.0.0-rc.1+sha-24f9d76');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
