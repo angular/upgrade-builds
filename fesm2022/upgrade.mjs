e86aa0757e61c6dbc3d9495992e0104456b76a2a
Andrew Scott <atscott01@gmail.com>
2025-11-05 17:41:36 +0000

    748caf9 test(compiler-cli): set up TCB tests for signal forms

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-7c49c53
+ * @license Angular v21.1.0-next.0+sha-748caf9
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-7c49c53');
+const VERSION = new Version('21.1.0-next.0+sha-748caf9');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
