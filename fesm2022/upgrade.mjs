6a394fb94ebba1bb24407ed69741a1f0bff4f2a4
Andrew Scott <atscott@google.com>
2025-11-05 17:54:48 +0000

    01c5ba5 docs(docs-infra): fix heading layout on mobile

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-d3d2a34
+ * @license Angular v21.0.0-rc.0+sha-01c5ba5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.0+sha-d3d2a34');
+const VERSION = new Version('21.0.0-rc.0+sha-01c5ba5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
