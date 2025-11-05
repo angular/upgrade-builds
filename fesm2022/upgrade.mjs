5ed299e9ae502598b70e7ce6d5e5b7f94ca7733c
Andrew Scott <atscott01@gmail.com>
2025-11-05 00:16:03 +0000

    41be02d perf(forms): implement change detection for field control bindings

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-5b210e9
+ * @license Angular v21.1.0-next.0+sha-41be02d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-5b210e9');
+const VERSION = new Version('21.1.0-next.0+sha-41be02d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
