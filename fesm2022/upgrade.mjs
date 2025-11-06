8d2d1635bd8a9e6f6c409723ff337791fdc9a1f8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-06 20:13:22 +0000

    acaa1e6 build: restrain visibility to compiler-cli subpackages (#64732)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-e90f9e4
+ * @license Angular v21.0.0-rc.1+sha-acaa1e6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.1+sha-e90f9e4');
+const VERSION = new Version('21.0.0-rc.1+sha-acaa1e6');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
