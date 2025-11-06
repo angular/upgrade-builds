d793771369def3e05aed40916c9c79a6fb797bdb
GitHub <noreply@github.com>
2025-11-06 18:10:28 +0000

    26fed34 build: format md files

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-08a84e0
+ * @license Angular v21.1.0-next.0+sha-26fed34
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-08a84e0');
+const VERSION = new Version('21.1.0-next.0+sha-26fed34');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
