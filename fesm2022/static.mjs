8cd4a7338481f3fed249fb3a3f1484ba3f710708
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-06 19:06:05 +0000

    e90f9e4 refactor(forms): tree-shake `ngControlStatusHost` and `ngGroupStatusHost`

--- fesm2022/static.mjs
+++ fesm2022/static.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-3bed9f0
+ * @license Angular v21.0.0-rc.1+sha-e90f9e4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1008,7 +1008,7 @@ class UpgradeComponent {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.1+sha-3bed9f0",
+    version: "21.0.0-rc.1+sha-e90f9e4",
     ngImport: i0,
     type: UpgradeComponent,
     deps: "invalid",
@@ -1016,7 +1016,7 @@ class UpgradeComponent {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "14.0.0",
-    version: "21.0.0-rc.1+sha-3bed9f0",
+    version: "21.0.0-rc.1+sha-e90f9e4",
     type: UpgradeComponent,
     isStandalone: true,
     usesOnChanges: true,
@@ -1025,7 +1025,7 @@ class UpgradeComponent {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.1+sha-3bed9f0",
+  version: "21.0.0-rc.1+sha-e90f9e4",
   ngImport: i0,
   type: UpgradeComponent,
   decorators: [{
@@ -1139,7 +1139,7 @@ class UpgradeModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.1+sha-3bed9f0",
+    version: "21.0.0-rc.1+sha-e90f9e4",
     ngImport: i0,
     type: UpgradeModule,
     deps: [{
@@ -1153,13 +1153,13 @@ class UpgradeModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.0.0-rc.1+sha-3bed9f0",
+    version: "21.0.0-rc.1+sha-e90f9e4",
     ngImport: i0,
     type: UpgradeModule
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.1+sha-3bed9f0",
+    version: "21.0.0-rc.1+sha-e90f9e4",
     ngImport: i0,
     type: UpgradeModule,
     providers: [angular1Providers, _internalProvideZoneChangeDetection({})]
@@ -1167,7 +1167,7 @@ class UpgradeModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.1+sha-3bed9f0",
+  version: "21.0.0-rc.1+sha-e90f9e4",
   ngImport: i0,
   type: UpgradeModule,
   decorators: [{
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-3bed9f0
+ * @license Angular v21.0.0-rc.1+sha-e90f9e4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.1+sha-3bed9f0');
+const VERSION = new Version('21.0.0-rc.1+sha-e90f9e4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
