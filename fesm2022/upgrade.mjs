12adbc725ba43ce4a4de8d64010addb3e5721e07
GitHub <noreply@github.com>
2025-11-06 17:10:21 +0000

    25e13e9 build: add a dev-app

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-ca3ef38
+ * @license Angular v21.1.0-next.0+sha-25e13e9
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-ca3ef38');
+const VERSION = new Version('21.1.0-next.0+sha-25e13e9');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
