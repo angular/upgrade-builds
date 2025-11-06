be32bd9bc9c436626bdc4348bc6c3beedc93689a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-06 17:47:33 +0000

    08a84e0 refactor(router): simplify imports and improve option access in RouterScroller tests

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-25e13e9
+ * @license Angular v21.1.0-next.0+sha-08a84e0
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-25e13e9');
+const VERSION = new Version('21.1.0-next.0+sha-08a84e0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
