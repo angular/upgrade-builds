8bb78b767a2e1657cc43ddfdac6a13984b8f3a30
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-06 17:46:06 +0000

    60ce31f test(forms): re-enable some disabled tests

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-a3c2fe8
+ * @license Angular v21.0.0-rc.1+sha-60ce31f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.1+sha-a3c2fe8');
+const VERSION = new Version('21.0.0-rc.1+sha-60ce31f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
