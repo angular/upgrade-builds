fd7ea6139777e6bf723fa23a9ce4e4c46cb0d5ae
GitHub <noreply@github.com>
2025-11-06 22:29:16 +0000

    cf47ce2 refactor(core): move profile_types.ts to primtives

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-faccf03
+ * @license Angular v21.1.0-next.0+sha-cf47ce2
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-faccf03');
+const VERSION = new Version('21.1.0-next.0+sha-cf47ce2');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
