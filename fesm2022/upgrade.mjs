634b1c6f94c430c3bd843890249b28a5ec3b1dd3
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-06 17:09:33 +0000

    a3c2fe8 Revert "refactor(common): Removes unused imports to clean up dependencies"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-26995e2
+ * @license Angular v21.0.0-rc.1+sha-a3c2fe8
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.1+sha-26995e2');
+const VERSION = new Version('21.0.0-rc.1+sha-a3c2fe8');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
