445283a1fd863d42ecea8b72acbde585b616c911
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-06 17:51:12 +0000

    dcdd1bc fix(core): skip leave animations on view swaps

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.10+sha-d6ef181
+ * @license Angular v20.3.10+sha-dcdd1bc
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.10+sha-d6ef181');
+const VERSION = new Version('20.3.10+sha-dcdd1bc');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
