b4935075b01af0ac4a28d5ac398f6f347c5e40c1
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-06 15:57:55 +0000

    e87f423 refactor(forms): add better support for Object.keys and Symbol.iterator

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-de1f22c
+ * @license Angular v21.1.0-next.0+sha-e87f423
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-de1f22c');
+const VERSION = new Version('21.1.0-next.0+sha-e87f423');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
