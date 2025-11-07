b4fcd4cdc6bb027343ed906f8b3574b9e1fc60d6
Andrew Kushnir <akushnir@google.com>
2025-11-07 18:51:50 +0000

    f54cc4f fix(compiler): support commas in :host() argument

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-0552e72
+ * @license Angular v21.0.0-rc.1+sha-f54cc4f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.1+sha-0552e72');
+const VERSION = new Version('21.0.0-rc.1+sha-f54cc4f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
