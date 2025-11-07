ac72a830d80651faa92b3523148dc17f78df8d7d
Andrew Kushnir <akushnir@google.com>
2025-11-07 19:42:06 +0000

    1e53f4e refactor: exclude tsconfig.json from VSIX package contents (#64991)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-f54cc4f
+ * @license Angular v21.0.0-rc.1+sha-1e53f4e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.1+sha-f54cc4f');
+const VERSION = new Version('21.0.0-rc.1+sha-1e53f4e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
