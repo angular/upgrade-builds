27dc13cb96998bcdc738380c700ed8a79bc6b4a7
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-07 21:00:22 +0000

    8bccd86 refactor(core): Extract NavigateEvent cancel steps based on recent spec update

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-850f0d6
+ * @license Angular v21.1.0-next.0+sha-8bccd86
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-850f0d6');
+const VERSION = new Version('21.1.0-next.0+sha-8bccd86');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
