1876fdf6b574eb421b402daa5ee9161fb537c6a7
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-07 19:49:13 +0000

    aada582 build: update `rules_angular` to latest

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-1e53f4e
+ * @license Angular v21.0.0-rc.1+sha-aada582
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.1+sha-1e53f4e');
+const VERSION = new Version('21.0.0-rc.1+sha-aada582');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
