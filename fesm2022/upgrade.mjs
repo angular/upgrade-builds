c9cdbe2178075b93b73fdccfd08a1be3a553585a
Andrew Kushnir <akushnir@google.com>
2025-11-07 18:19:12 +0000

    46c1a80 docs(docs-infra): refactor update to use host property and refactor isWindows

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-a3cb1df
+ * @license Angular v21.0.0-rc.1+sha-46c1a80
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.1+sha-a3cb1df');
+const VERSION = new Version('21.0.0-rc.1+sha-46c1a80');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
