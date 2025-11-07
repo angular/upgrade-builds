af326e8328202ea932ed1aef9a4e19de47467b2c
Andrew Kushnir <akushnir@google.com>
2025-11-07 23:33:43 +0000

    590244a docs(core): add a11y considerations related to `@defer()`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-647e640
+ * @license Angular v21.0.0-rc.1+sha-590244a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.1+sha-647e640');
+const VERSION = new Version('21.0.0-rc.1+sha-590244a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
