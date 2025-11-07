9e94f42bb8745a59f66aacd866b7ff36164d9355
Andrew Kushnir <akushnir@google.com>
2025-11-07 18:29:59 +0000

    0552e72 docs(docs-infra): use self-closing syntax for component examples

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-46c1a80
+ * @license Angular v21.0.0-rc.1+sha-0552e72
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.1+sha-46c1a80');
+const VERSION = new Version('21.0.0-rc.1+sha-0552e72');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
