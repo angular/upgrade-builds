dd4cda4eec53873bb96c164ef48421e0e16f9737
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-07 15:53:36 +0000

    0efb3f6 refactor(forms): nicer type errors

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-ff3013c
+ * @license Angular v21.1.0-next.0+sha-0efb3f6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.1.0-next.0+sha-ff3013c');
+const VERSION = new Version('21.1.0-next.0+sha-0efb3f6');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
