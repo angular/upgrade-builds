d8ea597fdad967f104328f073a4312a9c3db8264
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-08 00:07:46 +0000

    b47054c Revert "fix(compiler): support commas in :host() argument"

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.10+sha-143883a
+ * @license Angular v20.3.10+sha-b47054c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.10+sha-143883a');
+const VERSION = new Version('20.3.10+sha-b47054c');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
