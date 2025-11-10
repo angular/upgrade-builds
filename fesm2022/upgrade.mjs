66d68b2854823fd38f112b3ee4f54bf5ea44cecb
Andrew Kushnir <akushnir@google.com>
2025-11-10 16:07:38 +0000

    5092a89 docs: add shell language to CLI installation examples

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-590244a
+ * @license Angular v21.0.0-rc.1+sha-5092a89
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.1+sha-590244a');
+const VERSION = new Version('21.0.0-rc.1+sha-5092a89');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
