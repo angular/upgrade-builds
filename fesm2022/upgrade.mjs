1c0f2fa51cb22a2b7a87b9561228024ddf227bdd
Andrew Kushnir <akushnir@google.com>
2025-11-10 22:21:24 +0000

    2019940 docs: add links to SDUI example and livestream

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-ab98e71
+ * @license Angular v21.0.0-rc.1+sha-2019940
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-ab98e71');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-2019940');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
