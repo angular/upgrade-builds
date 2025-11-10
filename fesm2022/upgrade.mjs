ba5067b93c586fc7de1ea6c7bbc7bfba1799a98f
Andrew Kushnir <akushnir@google.com>
2025-11-10 20:16:40 +0000

    ab98e71 fix(common): remove placeholder image listeners once view is removed

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-63418fb
+ * @license Angular v21.0.0-rc.1+sha-ab98e71
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = new Version('21.0.0-rc.1+sha-63418fb');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-ab98e71');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
