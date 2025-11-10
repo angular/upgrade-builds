cc8cf17d10d257c9ec5487916c6d592eab646f68
Andrew Kushnir <akushnir@google.com>
2025-11-10 16:06:02 +0000

    b20bf5d docs: add shell language to CLI installation examples

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.10+sha-b47054c
+ * @license Angular v20.3.10+sha-b20bf5d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.10+sha-b47054c');
+const VERSION = new Version('20.3.10+sha-b20bf5d');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
