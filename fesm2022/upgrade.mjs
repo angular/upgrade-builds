2472f132019119cab3b4618d9effb1ceec2ef6bc
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-11 22:16:43 +0000

    c3e82fb refactor(core): render additional WAI-ARIA 1.3 property bindings as attributes

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-4ed8781
+ * @license Angular v21.1.0-next.0+sha-c3e82fb
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-4ed8781');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-c3e82fb');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
