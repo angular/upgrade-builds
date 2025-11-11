2755009bf6fb890dcc13d638c2d7d16593a3eb2d
Andrew Kushnir <akushnir@google.com>
2025-11-11 20:06:23 +0000

    a278ee3 feat(forms): add `debounce()` rule for signal forms

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-fc3a28e
+ * @license Angular v21.0.0-rc.1+sha-a278ee3
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-fc3a28e');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-a278ee3');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
