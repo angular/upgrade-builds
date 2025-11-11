05f80d64d69067d4b6f4fff77c79d54d9689e07b
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-11 16:58:12 +0000

    91dbb4b build: lock file maintenance

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-557213e
+ * @license Angular v21.0.0-rc.1+sha-91dbb4b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-557213e');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-91dbb4b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
