0d46dd93db03ba2dc811c9b2c2fad240ca9bcaea
Andrew Kushnir <akushnir@google.com>
2025-11-11 20:59:01 +0000

    7811d95 refactor(core): improve resource loading with async/await

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-a278ee3
+ * @license Angular v21.0.0-rc.1+sha-7811d95
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-a278ee3');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-7811d95');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
