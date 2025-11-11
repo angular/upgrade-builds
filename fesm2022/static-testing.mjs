8c4f2d51e3efc6900366f1f205db874ef12bb58e
Andrew Kushnir <akushnir@google.com>
2025-11-11 16:42:13 +0000

    557213e docs: correct mermaid code block formatting

--- fesm2022/static-testing.mjs
+++ fesm2022/static-testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-2019940
+ * @license Angular v21.0.0-rc.1+sha-557213e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -20,7 +20,7 @@ class AngularTestingModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.1+sha-2019940",
+    version: "21.0.0-rc.1+sha-557213e",
     ngImport: i0,
     type: AngularTestingModule,
     deps: [{
@@ -30,13 +30,13 @@ class AngularTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.0.0-rc.1+sha-2019940",
+    version: "21.0.0-rc.1+sha-557213e",
     ngImport: i0,
     type: AngularTestingModule
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.1+sha-2019940",
+    version: "21.0.0-rc.1+sha-557213e",
     ngImport: i0,
     type: AngularTestingModule,
     providers: [{
@@ -47,7 +47,7 @@ class AngularTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.1+sha-2019940",
+  version: "21.0.0-rc.1+sha-557213e",
   ngImport: i0,
   type: AngularTestingModule,
   decorators: [{
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-2019940
+ * @license Angular v21.0.0-rc.1+sha-557213e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-2019940');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-557213e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
