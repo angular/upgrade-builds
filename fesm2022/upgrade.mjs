80b7740bec883dcd0d5f7b8cab238dd18be5ec53
Andrew Kushnir <akushnir@google.com>
2025-11-11 18:20:10 +0000

    c9f9778 fix(core): skip Angular formatting when formatting signals recursively

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-91dbb4b
+ * @license Angular v21.0.0-rc.1+sha-c9f9778
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-91dbb4b');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-c9f9778');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
