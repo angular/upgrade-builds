eb0d23e198acf781ab4572bf9ff9bbe8e4992ac9
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-12 21:18:15 +0000

    f7e5857 refactor(compiler-cli): rework type checking for signal forms

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-48d3954
+ * @license Angular v21.1.0-next.0+sha-f7e5857
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-48d3954');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-f7e5857');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
