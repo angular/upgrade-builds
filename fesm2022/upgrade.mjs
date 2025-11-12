202b9459df18a793c6146be85f7bda8d56923f83
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-12 19:39:56 +0000

    05ef2a5 docs: correct code block language in error handling and signals interop

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-8e81d0a
+ * @license Angular v21.1.0-next.0+sha-05ef2a5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-8e81d0a');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-05ef2a5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
