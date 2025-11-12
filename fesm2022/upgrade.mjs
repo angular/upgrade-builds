8871f23420f127018ae20686f1c0a14547f7cc21
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-12 20:01:13 +0000

    eb9034d build: update cross-repo angular dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-dc11db1
+ * @license Angular v21.0.0-rc.2+sha-eb9034d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-dc11db1');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-eb9034d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
