d23cdabcc0d0d6f289aef1a9c2e12236253fc174
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-12 19:54:20 +0000

    dc11db1 docs: correct example for RouteReuseStrategy getRouteKey

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-129e16d
+ * @license Angular v21.0.0-rc.2+sha-dc11db1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-129e16d');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-dc11db1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
