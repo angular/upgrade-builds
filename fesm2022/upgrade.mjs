960263e9c94c7678387ed1091f41639d25a69c12
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-12 17:48:32 +0000

    18cc260 build: update bazel dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-909588e
+ * @license Angular v21.0.0-rc.2+sha-18cc260
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-909588e');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-18cc260');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
