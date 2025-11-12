46b4ad65e0dfbc97fd35c9c5f45b85e38ba849dd
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-12 16:47:22 +0000

    cc095d4 docs: release notes for the v21.0.0-rc.2 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-1f6de17
+ * @license Angular v21.1.0-next.0+sha-cc095d4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-1f6de17');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-cc095d4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
