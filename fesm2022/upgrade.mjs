c60b1fb6b0c802320ba0ffe687101003d3d7438c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-13 18:08:53 +0000

    16d51f6 refactor(core): flip ngDevMode condition for formatter

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-2406172
+ * @license Angular v21.0.0-rc.2+sha-16d51f6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-2406172');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-16d51f6');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
