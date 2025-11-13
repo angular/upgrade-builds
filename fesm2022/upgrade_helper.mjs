494a359ffddcd6358c5247d1a29c46cc2a42f070
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-13 18:05:07 +0000

    06bc146 docs: adds guide references to router APIs

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.11+sha-3a8fb09
+ * @license Angular v20.3.11+sha-06bc146
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.11+sha-3a8fb09');
+const VERSION = /* @__PURE__ */ new Version('20.3.11+sha-06bc146');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
