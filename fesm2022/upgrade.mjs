372fce169326c910faf0fbcc2e007e4cd0352763
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-13 23:18:39 +0000

    4b0f208 ci: use hardcoded global approvers group

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-482e137
+ * @license Angular v21.0.0-rc.2+sha-4b0f208
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-482e137');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-4b0f208');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
