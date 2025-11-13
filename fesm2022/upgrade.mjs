9323c1ff89bd3e996447e47bdec5bc7fcf3a844d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-13 17:41:14 +0000

    4f6f9b8 docs(docs-infra): Add support for metadata on fenced code blocks

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-67fbd5f
+ * @license Angular v21.0.0-rc.2+sha-4f6f9b8
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-67fbd5f');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-4f6f9b8');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
