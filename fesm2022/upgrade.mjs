e6e83c47479e88558631c187977db1f2d35825cd
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-13 18:09:38 +0000

    1b895fe refactor(core): flip ngDevMode condition for formatter

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-1a94d71
+ * @license Angular v21.1.0-next.0+sha-1b895fe
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-1a94d71');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-1b895fe');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
