0ba31484be2d102d330023c194bcb1a3d8f27f51
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 20:28:57 +0000

    3228cce docs(docs-infra): enable tabs layout

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-8a5b284
+ * @license Angular v21.0.0-rc.2+sha-3228cce
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-8a5b284');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-3228cce');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
