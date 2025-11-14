9ead6c90c99d54873da0064ddb66f9ac68c88cbb
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-14 00:02:20 +0000

    6d8c3fc docs(docs-infra): hide file tabs when code example is hidden

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-024c939
+ * @license Angular v21.1.0-next.0+sha-6d8c3fc
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-024c939');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-6d8c3fc');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
