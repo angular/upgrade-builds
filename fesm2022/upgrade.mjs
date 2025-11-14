4c1711967591f16197e74518948550ccc10647af
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-14 23:35:20 +0000

    c1057c6 docs: add formArray clear() example clarifies array manipulation

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-722292f
+ * @license Angular v21.1.0-next.0+sha-c1057c6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-722292f');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-c1057c6');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
