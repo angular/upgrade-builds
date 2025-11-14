9a49cbadc0efad6c886ea66719df1245c86de8bd
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 17:28:37 +0000

    966bfae build(forms): expose signal forms compat package

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-911999e
+ * @license Angular v21.0.0-rc.2+sha-966bfae
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-911999e');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-966bfae');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
