a9b374e795b12255800c16688f892e68a8fefc4b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 22:10:09 +0000

    e1a7c35 refactor(forms): improve typing on min & max (#65212)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-3228cce
+ * @license Angular v21.0.0-rc.2+sha-e1a7c35
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-3228cce');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-e1a7c35');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
