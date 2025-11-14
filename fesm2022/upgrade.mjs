bb5cc022c0f92a6e74a02126db9b00d1bc053dc7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 16:48:49 +0000

    f550f4a docs: ExperimentalIsolatedShadowDom mentions

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-c2a52fd
+ * @license Angular v21.0.0-rc.2+sha-f550f4a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-c2a52fd');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-f550f4a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
