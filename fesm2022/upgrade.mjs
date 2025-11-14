f6c805a2e47d1e3e348b2de64ca87838e8e50e39
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-14 00:07:14 +0000

    c2a52fd docs: add aria toolbar component guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-750da17
+ * @license Angular v21.0.0-rc.2+sha-c2a52fd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-750da17');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-c2a52fd');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
