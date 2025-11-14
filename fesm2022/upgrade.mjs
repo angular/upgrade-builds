c5be17c735451e5d0a87c03d444a172a33fb5505
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-14 17:45:20 +0000

    1464d02 docs(docs-infra): backport docs changes from `21.0.x`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-fc1ef79
+ * @license Angular v21.1.0-next.0+sha-1464d02
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-fc1ef79');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-1464d02');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
