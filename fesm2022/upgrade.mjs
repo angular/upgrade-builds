e3b499bc6f04579ad191f45d2d808994780c0022
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-14 16:47:08 +0000

    c137f1f docs: ExperimentalIsolatedShadowDom mentions

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-8989536
+ * @license Angular v21.1.0-next.0+sha-c137f1f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-8989536');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-c137f1f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
