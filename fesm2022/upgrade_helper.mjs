0a914ea409689882f0e0b3fef041d248170919cf
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 16:37:12 +0000

    e0a6dad docs: Uses the self-closing tag syntax

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.12+sha-39f9e23
+ * @license Angular v20.3.12+sha-e0a6dad
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.12+sha-39f9e23');
+const VERSION = /* @__PURE__ */ new Version('20.3.12+sha-e0a6dad');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
