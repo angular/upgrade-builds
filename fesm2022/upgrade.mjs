65ba4aabb97003da191a52c30738000317f2864c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 16:16:41 +0000

    59543d6 docs: add bun package manager and standardize order in testing guides

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-74b95de
+ * @license Angular v21.0.0-rc.2+sha-59543d6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-74b95de');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-59543d6');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
