af5122b69a0634451e059728679e01208d722e43
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 19:22:26 +0000

    6551b65 docs: update examples to latest Angular

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-b7e9183
+ * @license Angular v21.0.0-rc.2+sha-6551b65
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-b7e9183');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-6551b65');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
