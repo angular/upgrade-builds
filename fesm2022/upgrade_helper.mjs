321d5c479addbe0bf42e5abb1c52ad1f66f168a6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 22:38:05 +0000

    967e111 docs: fix markdown table in custom elements guide

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.12+sha-5e15553
+ * @license Angular v20.3.12+sha-967e111
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.12+sha-5e15553');
+const VERSION = /* @__PURE__ */ new Version('20.3.12+sha-967e111');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
