91997b1335595e2f05e472d770069ecc12d2fb14
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 18:44:11 +0000

    cb481a5 docs: aria listbox examples (#65273)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-88dfd96
+ * @license Angular v21.1.0-next.0+sha-cb481a5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-88dfd96');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-cb481a5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
