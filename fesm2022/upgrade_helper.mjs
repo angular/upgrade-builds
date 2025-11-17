0c8f89998f815989ca22bc94a27985e919a53656
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 23:13:23 +0000

    72b19d8 docs: replace @HostListener with host event bindings in attribute directives

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.12+sha-81b6e45
+ * @license Angular v20.3.12+sha-72b19d8
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.12+sha-81b6e45');
+const VERSION = /* @__PURE__ */ new Version('20.3.12+sha-72b19d8');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
