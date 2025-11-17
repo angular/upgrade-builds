4a6a08590cdcb7d75a0ae46c3c7b9b5744d6494e
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-17 22:49:45 +0000

    e7a6e31 fix(devtools): refine when signal graph button is shown

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-c757fd6
+ * @license Angular v21.1.0-next.0+sha-e7a6e31
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-c757fd6');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-e7a6e31');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
