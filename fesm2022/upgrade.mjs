501ac70d9e01e28a058441a24168dc19767dd771
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 23:14:19 +0000

    9723748 docs: replace @HostListener with host event bindings in attribute directives

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.3+sha-91de094
+ * @license Angular v21.0.0-rc.3+sha-9723748
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-91de094');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-9723748');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
