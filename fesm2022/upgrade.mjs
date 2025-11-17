dd871b480b83b594f68376037866efa46985cab6
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-17 17:51:41 +0000

    9e7ddca fix(compiler): don't choke on unbalanced parens in declaration block

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-3760feb
+ * @license Angular v21.1.0-next.0+sha-9e7ddca
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-3760feb');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-9e7ddca');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
