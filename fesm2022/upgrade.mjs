0446d240f7e80cd73f606ee3623583ae98ca21dc
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-17 22:44:16 +0000

    c757fd6 docs(docs-infra): add advanced recommendations

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-f13159f
+ * @license Angular v21.1.0-next.0+sha-c757fd6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-f13159f');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-c757fd6');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
