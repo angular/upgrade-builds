6d808ae8333db22644d0f25fd30cfbb016882941
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 17:55:04 +0000

    ecea909 fix(compiler): don't choke on unbalanced parens in declaration block

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-cc621ea
+ * @license Angular v21.0.0-rc.2+sha-ecea909
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-cc621ea');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-ecea909');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
