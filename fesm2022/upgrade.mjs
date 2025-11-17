41e5d23b3b5dff29a3e7e700e503624cd192bb42
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-17 16:54:51 +0000

    0432e76 docs: Adds links to relevant guides for APIs in core package

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-599b72e
+ * @license Angular v21.1.0-next.0+sha-0432e76
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-599b72e');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-0432e76');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
