adbebb73b4798e46be0a8018e3ccc3e8982346d7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 19:13:56 +0000

    b7e9183 Revert "ci: remove cache usage from pr.yml"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-652d719
+ * @license Angular v21.0.0-rc.2+sha-b7e9183
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-652d719');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-b7e9183');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
