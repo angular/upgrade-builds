25d7d00c91a9c9eb2089b20bdf25acc4aee7a5f5
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-17 18:15:14 +0000

    88dfd96 Revert "refactor(core): let the profiler handle asymmetric events leniently"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-9e7ddca
+ * @license Angular v21.1.0-next.0+sha-88dfd96
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-9e7ddca');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-88dfd96');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
