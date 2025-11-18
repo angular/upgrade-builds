43a13bb002ffe558bd1130e21add42ee5751c961
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-18 16:34:17 +0000

    ed0647d refactor(devtools): add matcher and runGuardsAndResolvers support in router viewer

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-f799fc8
+ * @license Angular v21.1.0-next.0+sha-ed0647d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-f799fc8');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-ed0647d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
