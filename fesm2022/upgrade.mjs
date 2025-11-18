d43b7429f80ca3a142f91a581f1e222feef7f536
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-18 23:00:28 +0000

    114c83e docs: aria combobox examples

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-27fa993
+ * @license Angular v21.1.0-next.0+sha-114c83e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-27fa993');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-114c83e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
