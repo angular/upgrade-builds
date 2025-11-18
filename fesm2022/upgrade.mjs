278c95ae4de7ea5e37256e21841bd21479bde3e3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-18 23:35:28 +0000

    012326e docs: update angular aria guides structure, subnav menu, and examples

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.3+sha-21e2889
+ * @license Angular v21.0.0-rc.3+sha-012326e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-21e2889');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-012326e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
