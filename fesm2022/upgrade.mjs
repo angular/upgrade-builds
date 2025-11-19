c8bd48e566f7fb412e63626304f8c748e9bf8425
GitHub <noreply@github.com>
2025-11-19 23:29:27 +0000

    34e1fe2 refactor(core): export profile event as enum and move profile_types.ts and framework to shared devtools folder

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-4c31f8e
+ * @license Angular v21.1.0-next.0+sha-34e1fe2
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-4c31f8e');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-34e1fe2');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
