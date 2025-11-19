411c3f36db61e0077f3f4f199836dbe4e9f74cad
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 16:32:04 +0000

    ac34824 fix(docs-infra): add moduleResolution to TypeScript compiler options for playground

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.3+sha-e69855a
+ * @license Angular v21.0.0-rc.3+sha-ac34824
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-e69855a');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-ac34824');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
