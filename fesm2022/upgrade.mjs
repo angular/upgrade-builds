ee2a1b612897989d0845f78b1843b5f05aa31803
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-19 01:05:52 +0000

    7a910e6 docs: add signal forms comparison guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-47eb77f
+ * @license Angular v21.1.0-next.0+sha-7a910e6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-47eb77f');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-7a910e6');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
