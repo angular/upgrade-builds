6eacdebccb7f55fe9cec893d890f67aa3bdf6379
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 23:30:27 +0000

    2bf0c52 refactor(core): export profile event as enum and move profile_types.ts and framework to shared devtools folder

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-e0026c8
+ * @license Angular v21.0.0+sha-2bf0c52
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-e0026c8');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-2bf0c52');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
