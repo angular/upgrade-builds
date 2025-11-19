31bac02ea1e080267d85ddecc4c4022964a18e05
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-19 22:57:57 +0000

    38f4ea1 docs: add signal forms validation guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-e9ba63a
+ * @license Angular v21.1.0-next.0+sha-38f4ea1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-e9ba63a');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-38f4ea1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
