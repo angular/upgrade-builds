3ce8ecf39c012b2f762bb3058bb954f45e7a6810
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-19 22:18:35 +0000

    d2fe867 docs: update aria overview page

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-c231794
+ * @license Angular v21.1.0-next.0+sha-d2fe867
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-c231794');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-d2fe867');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
