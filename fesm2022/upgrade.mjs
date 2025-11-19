9c4c758f7c89c642f04d7627df13187a5ed9eb6d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 22:28:08 +0000

    47fc721 refactor: improve release process instructions and `vsce login` handling

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-d9017a4
+ * @license Angular v21.0.0+sha-47fc721
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-d9017a4');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-47fc721');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
