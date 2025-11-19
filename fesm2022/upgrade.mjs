eb5c307cbb54d4487ad0b46c876b0d332c3994e3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 16:52:49 +0000

    8b492ce docs: update the actively supported versions

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.3+sha-ac34824
+ * @license Angular v21.0.0-rc.3+sha-8b492ce
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-ac34824');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-8b492ce');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
