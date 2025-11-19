09b1bf9a715a8d6af79a74be183766893abc9ba6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 16:24:43 +0000

    e69855a docs: aria grid replace overview example with a playful one

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.3+sha-28c1a2d
+ * @license Angular v21.0.0-rc.3+sha-e69855a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-28c1a2d');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-e69855a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
