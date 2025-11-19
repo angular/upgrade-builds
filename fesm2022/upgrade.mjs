e964f0ca8ef96374ef73a85620b851661be9ab9c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 22:19:32 +0000

    d9017a4 docs: update aria overview page

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-4e14a53
+ * @license Angular v21.0.0+sha-d9017a4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-4e14a53');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-d9017a4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
