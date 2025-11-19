21848de4dc8c4f78827c15e0da7a689e6b3f281d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 20:14:47 +0000

    28810e0 build: bump adev version

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-63a95c7
+ * @license Angular v21.0.0+sha-28810e0
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-63a95c7');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-28810e0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
