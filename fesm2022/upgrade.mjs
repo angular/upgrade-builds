d849a846dc96dab77f4cedb5e953abec1cbdb2eb
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 01:07:08 +0000

    28c1a2d docs: add signal forms comparison guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0-rc.3+sha-012326e
+ * @license Angular v21.0.0-rc.3+sha-28c1a2d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-012326e');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-28c1a2d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
