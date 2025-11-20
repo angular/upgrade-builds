00c40378135ffc038bbf2033bed1a685cf9dcf2a
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-20 21:52:50 +0000

    2ecdb22 docs: correct v21 LTS end date and v20 active end date

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-11da7a6
+ * @license Angular v21.1.0-next.0+sha-2ecdb22
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-11da7a6');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-2ecdb22');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
