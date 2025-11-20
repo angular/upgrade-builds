7ebe5a66fb689d9ad72506666875d8812f79e367
kirjs <kirts@google.com>
2025-11-20 22:08:33 +0000

    70fb1d6 ci: try to resolve vscode extension e2e flakes

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-da97099
+ * @license Angular v21.0.0+sha-70fb1d6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-da97099');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-70fb1d6');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
