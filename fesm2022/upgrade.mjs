5aa97a9e92127cc47a4668dc84b0862013ac2912
kirjs <kirts@google.com>
2025-11-20 18:32:50 +0000

    7219759 docs: add v21 event game

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-cd86de1
+ * @license Angular v21.0.0+sha-7219759
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-cd86de1');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-7219759');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
