e4bf50c598ef450d5df775c1d1f343b2ed8f8678
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-20 15:39:46 +0000

    fc2e414 docs: add signal forms custom controls guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-8904142
+ * @license Angular v21.1.0-next.0+sha-fc2e414
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-8904142');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-fc2e414');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
