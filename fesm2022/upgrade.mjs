63234eb58aa14ec1f6cc3cfe6bf35ba4e584969a
kirjs <kirts@google.com>
2025-11-20 20:06:19 +0000

    b9181f1 docs: prioritize ng add for Tailwind CSS and clarify manual steps

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-82c085d
+ * @license Angular v21.0.0+sha-b9181f1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-82c085d');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-b9181f1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
