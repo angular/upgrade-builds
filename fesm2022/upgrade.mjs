db577da5a9d1a7a07d78c676a6fffc51818903db
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-20 00:45:48 +0000

    8904142 docs: adds a link to the signal forms example

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-34e1fe2
+ * @license Angular v21.1.0-next.0+sha-8904142
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-34e1fe2');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-8904142');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
