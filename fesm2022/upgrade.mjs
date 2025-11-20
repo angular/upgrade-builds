05005ebc582a47b669d522367f1a69e3ec7a1de9
kirjs <kirts@google.com>
2025-11-20 15:56:24 +0000

    630a3b2 refactor(core): add debugName option to rxjs-interop toSignal

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-cf7579f
+ * @license Angular v21.0.0+sha-630a3b2
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-cf7579f');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-630a3b2');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
