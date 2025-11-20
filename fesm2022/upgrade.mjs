49b5ed6de71f1126318fb01946959104281f0778
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-20 17:45:37 +0000

    be6f2f0 docs: consolidate patterns and iterate on conclusion for angular aria overview

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-913cde8
+ * @license Angular v21.1.0-next.0+sha-be6f2f0
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-913cde8');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-be6f2f0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
