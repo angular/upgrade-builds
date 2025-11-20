ffb3e775b0419ff19fa25183458385a3263810c1
kirjs <kirts@google.com>
2025-11-20 21:47:45 +0000

    65c39ea docs(core): remove outdated regex template restriction in best-practices.md

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-b9181f1
+ * @license Angular v21.0.0+sha-65c39ea
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-b9181f1');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-65c39ea');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
