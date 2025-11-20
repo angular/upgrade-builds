e49e8dbf35fda8965ac24b0b28ffa1467bd25315
kirjs <kirts@google.com>
2025-11-20 21:52:42 +0000

    da97099 docs: correct v21 LTS end date and v20 active end date

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-65c39ea
+ * @license Angular v21.0.0+sha-da97099
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-65c39ea');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-da97099');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
