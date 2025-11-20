5a7edef14eabe113bea1766421623839c577d4c7
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-20 22:09:38 +0000

    1ab461c refactor(router): Store route injector on ActivatedRoute instance

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-2ecdb22
+ * @license Angular v21.1.0-next.0+sha-1ab461c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-2ecdb22');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-1ab461c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
