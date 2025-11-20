2b31b18888024412f0b094f81cf482951f2b9bf2
kirjs <kirts@google.com>
2025-11-20 16:27:19 +0000

    f349a91 docs: fix standlone menu overlay

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-630a3b2
+ * @license Angular v21.0.0+sha-f349a91
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-630a3b2');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-f349a91');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
