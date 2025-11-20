701c3c95fec429c9e479274aa57217b969adcba5
kirjs <kirts@google.com>
2025-11-20 17:46:15 +0000

    cd86de1 docs: consolidate patterns and iterate on conclusion for angular aria overview

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-eadc8de
+ * @license Angular v21.0.0+sha-cd86de1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-eadc8de');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-cd86de1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
