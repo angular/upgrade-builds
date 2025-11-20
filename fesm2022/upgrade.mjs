175282d2fdbe17785c1275ec42687a83fed40581
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-20 21:47:28 +0000

    11da7a6 docs(core): remove outdated regex template restriction in best-practices.md

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-396c25e
+ * @license Angular v21.1.0-next.0+sha-11da7a6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-396c25e');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-11da7a6');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
