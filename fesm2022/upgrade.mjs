d7319803dfb731bd2fb524d6be02ac9d3c332436
kirjs <kirts@google.com>
2025-11-20 15:40:12 +0000

    cf7579f docs: add signal forms custom controls guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-f31fbdd
+ * @license Angular v21.0.0+sha-cf7579f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-f31fbdd');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-cf7579f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
