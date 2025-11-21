2a80f0fcb8005f564d9cf8cecfdf01b01bca548c
kirjs <kirts@google.com>
2025-11-21 16:45:47 +0000

    bc34083 fix(compiler-cli): ignore non-existent files

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-1cdcd5a
+ * @license Angular v21.0.0+sha-bc34083
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-1cdcd5a');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-bc34083');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
