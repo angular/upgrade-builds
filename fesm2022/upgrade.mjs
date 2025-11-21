ac16f10adc90a12dae29aa8c7a9ce9d6bf30bbb4
kirjs <kirts@google.com>
2025-11-21 17:31:09 +0000

    1dfce39 docs: move aria tabs into content organization category

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-bc34083
+ * @license Angular v21.0.0+sha-1dfce39
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-bc34083');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-1dfce39');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
