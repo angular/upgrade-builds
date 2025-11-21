ab700c515245a3b541e66c7929dab45f8b40cf61
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-21 17:30:30 +0000

    8f346ad docs: move aria tabs into content organization category

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-1628125
+ * @license Angular v21.1.0-next.0+sha-8f346ad
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-1628125');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-8f346ad');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
