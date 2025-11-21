168eb281ca39dfa35fb2c445f3dcab6a99335d99
kirjs <kirts@google.com>
2025-11-21 15:53:18 +0000

    053bf98 docs: iterate on signal forms essentials guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-2f44ede
+ * @license Angular v21.0.0+sha-053bf98
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-2f44ede');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-053bf98');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
