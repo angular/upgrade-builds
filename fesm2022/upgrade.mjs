b83bbd1d785c5eec13f642c26fa8cb13ceb3d646
kirjs <kirts@google.com>
2025-11-21 21:39:41 +0000

    a70a191 docs: Adds signal type checking documentation

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-b8da1d4
+ * @license Angular v21.0.0+sha-a70a191
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-b8da1d4');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-a70a191');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
