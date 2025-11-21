3d20f1d12c97649bf43c99dafbfad9f73a67d6ed
kirjs <kirts@google.com>
2025-11-21 16:01:40 +0000

    1cdcd5a docs: fix incorrect Signal Forms link

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-053bf98
+ * @license Angular v21.0.0+sha-1cdcd5a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-053bf98');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-1cdcd5a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
