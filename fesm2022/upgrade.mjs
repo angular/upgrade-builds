34237feaf610ddf7aa46c720f8180d16d1b06168
kirjs <kirts@google.com>
2025-11-21 00:26:33 +0000

    1925721 refactor(docs-infra): automate algolia index name based on package version

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-70fb1d6
+ * @license Angular v21.0.0+sha-1925721
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-70fb1d6');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-1925721');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
