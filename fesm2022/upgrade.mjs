df7e1bdf76f13b309a3139ce801b2ec5cded6952
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-21 00:25:59 +0000

    2ddf26e refactor(docs-infra): automate algolia index name based on package version

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-1ab461c
+ * @license Angular v21.1.0-next.0+sha-2ddf26e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-1ab461c');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-2ddf26e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
