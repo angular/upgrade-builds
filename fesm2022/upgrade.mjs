73603d0007a2ad8ab92d7ff7170d5e3f55fa0e01
kirjs <kirts@google.com>
2025-11-21 18:30:52 +0000

    df1d129 docs: consistent casing for onpush zoneless migration MCP tool name

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-1dfce39
+ * @license Angular v21.0.0+sha-df1d129
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-1dfce39');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-df1d129');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
