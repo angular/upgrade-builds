17bb1de43ea8f1f4e8bcaadfbc3bcde02a0ed210
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-21 21:39:02 +0000

    38a354f docs: Adds signal type checking documentation

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-f884c18
+ * @license Angular v21.1.0-next.0+sha-38a354f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-f884c18');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-38a354f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
