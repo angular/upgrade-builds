99ef0cdceaee8444fbd93cced66d97beeea91b2a
kirjs <kirts@google.com>
2025-11-21 20:58:21 +0000

    b8da1d4 docs: format JSON examples and replace <docs-code> with fenced code blocks

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-16babdd
+ * @license Angular v21.0.0+sha-b8da1d4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-16babdd');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-b8da1d4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
