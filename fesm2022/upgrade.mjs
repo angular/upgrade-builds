cb02e99a0b8efb5de4715b092f198118ee503176
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-22 14:15:49 +0000

    72b1f7f docs: fix router event subscription in error handler example

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-38a354f
+ * @license Angular v21.1.0-next.0+sha-72b1f7f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-38a354f');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-72b1f7f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
