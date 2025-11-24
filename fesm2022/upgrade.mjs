17588f5b536efb839a60990ead70ac9959fc0e4d
kirjs <kirts@google.com>
2025-11-24 15:30:23 +0000

    1f20b54 docs(docs-infra): simplify tab selection

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-2d658b3
+ * @license Angular v21.0.0+sha-1f20b54
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-2d658b3');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-1f20b54');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
