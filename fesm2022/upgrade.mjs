5f65f860ff6484e07f7c6e22476b0521580bb9cb
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 18:54:34 +0000

    f97a1d4 refactor(forms): support custom control directives

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-d442c72
+ * @license Angular v21.1.0-next.0+sha-f97a1d4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-d442c72');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-f97a1d4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
