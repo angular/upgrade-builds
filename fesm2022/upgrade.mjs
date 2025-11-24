8fc55af90c7633287c31d60c288c03af6e9188b4
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 15:43:17 +0000

    0203dad docs: html examples and replace <docs-code> with fenced code blocks

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-119c6dd
+ * @license Angular v21.1.0-next.0+sha-0203dad
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-119c6dd');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-0203dad');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
