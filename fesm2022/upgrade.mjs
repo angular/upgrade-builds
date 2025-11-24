7ab03912c5d3f7784a565106762ac8b02de0081f
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 17:45:10 +0000

    a4f50bd fix(migrations): detect structural ngTemplateOutlet and ngComponentOutlet

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-b26fe73
+ * @license Angular v21.1.0-next.0+sha-a4f50bd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-b26fe73');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-a4f50bd');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
