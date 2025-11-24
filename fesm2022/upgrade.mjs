57eeff728044cc1637371afd0946b7f46d190916
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 14:12:47 +0000

    48a05dd docs: fix v21 event header image

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-72b1f7f
+ * @license Angular v21.1.0-next.0+sha-48a05dd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-72b1f7f');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-48a05dd');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
