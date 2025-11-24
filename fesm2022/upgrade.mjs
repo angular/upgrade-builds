4eaa305505e4298c35b89c90f56a21f6a37d80eb
kirjs <kirts@google.com>
2025-11-24 15:43:48 +0000

    4303f51 docs: html examples and replace <docs-code> with fenced code blocks

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-1f20b54
+ * @license Angular v21.0.0+sha-4303f51
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-1f20b54');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-4303f51');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
