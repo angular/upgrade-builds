cfa71f26f084d761de4e7c62ec01966eb70e9305
kirjs <kirts@google.com>
2025-11-24 16:35:57 +0000

    0f4b11c refactor(compiler-cli): add a resource debugName transform (#64172)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-b5ae62c
+ * @license Angular v21.1.0-next.0+sha-0f4b11c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-b5ae62c');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-0f4b11c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
