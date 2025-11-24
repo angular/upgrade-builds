2d73391f1b25f70b80c44e2eabb2fa14a65fcafd
kirjs <kirts@google.com>
2025-11-24 17:28:27 +0000

    70bc311 docs(docs-infra): keep 'Show code' button aligned right and prevent metadata wrapping

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-9852033
+ * @license Angular v21.0.0+sha-70bc311
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-9852033');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-70bc311');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
