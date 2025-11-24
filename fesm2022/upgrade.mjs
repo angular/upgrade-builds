451fe50ecd17c63d396b6c73fa6c9bf74800e749
GitHub <noreply@github.com>
2025-11-24 15:07:23 +0000

    f7f9707 build: run e2e in sandbox

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-5961e96
+ * @license Angular v21.1.0-next.0+sha-f7f9707
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-5961e96');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-f7f9707');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
