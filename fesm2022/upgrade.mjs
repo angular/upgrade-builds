964eed2d1b3bc4246dc54220aaf7b2c464594ea0
kirjs <kirts@google.com>
2025-11-24 18:54:57 +0000

    4845a33 refactor(forms): support custom control directives

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-534cc3d
+ * @license Angular v21.0.0+sha-4845a33
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-534cc3d');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-4845a33');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
