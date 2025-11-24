31eb8a2fb31cba53addde6de74d61c324f1a7fb3
kirjs <kirts@google.com>
2025-11-24 16:16:45 +0000

    102cb87 docs: update devtools guide - splits the injector inspector into its own guide - update the screenshots - add defer block section - add hydration section

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-4303f51
+ * @license Angular v21.0.0+sha-102cb87
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-4303f51');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-102cb87');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
