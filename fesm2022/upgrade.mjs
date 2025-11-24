83989396b5b54f55179eeba160b684b015a49c62
kirjs <kirts@google.com>
2025-11-24 20:35:35 +0000

    70507b8 fix(core): debug data causing memory leak for root effects

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-4845a33
+ * @license Angular v21.0.0+sha-70507b8
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-4845a33');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-70507b8');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
