2e60f63ede15deaf3187896ae09981df8c8681cb
kirjs <kirts@google.com>
2025-11-24 18:24:00 +0000

    a62162d docs: Update router docs to add references and components input fixed syntaxis

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-f394215
+ * @license Angular v21.0.0+sha-a62162d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-f394215');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-a62162d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
