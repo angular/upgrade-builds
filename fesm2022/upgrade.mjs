7749692f07ad0f318649def3574b3c5b53b0e403
kirjs <kirts@google.com>
2025-11-24 22:04:31 +0000

    0e458c7 refactor(core): Remove toggles used for zoneless by default migration

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-70507b8
+ * @license Angular v21.0.0+sha-0e458c7
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-70507b8');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-0e458c7');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
