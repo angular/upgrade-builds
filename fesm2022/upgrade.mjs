253812d565233590efee898f11be8ab5ff053167
kirjs <kirts@google.com>
2025-11-24 17:45:47 +0000

    f394215 fix(migrations): detect structural ngTemplateOutlet and ngComponentOutlet

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-70bc311
+ * @license Angular v21.0.0+sha-f394215
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-70bc311');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-f394215');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
