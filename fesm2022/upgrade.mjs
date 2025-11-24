97ea9c5cf07f5ed2b8cbb82513f9c94a46339d6a
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 14:39:17 +0000

    c7ae6f3 build: update all github actions

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-92f0b66
+ * @license Angular v21.0.0+sha-c7ae6f3
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-92f0b66');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-c7ae6f3');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
