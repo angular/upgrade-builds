1d29038edebeba7734b6665fb262ed8dd01b763e
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 20:34:38 +0000

    ca6ab6c fix(core): debug data causing memory leak for root effects

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-f97a1d4
+ * @license Angular v21.1.0-next.0+sha-ca6ab6c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-f97a1d4');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-ca6ab6c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
