9034016940df2d915fdda1f21150d938b846dedd
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 14:21:04 +0000

    3a1eb07 fix(forms): allow dynamic `type` bindings on signal form controls

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-a294e3b
+ * @license Angular v21.1.0-next.0+sha-3a1eb07
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-a294e3b');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-3a1eb07');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
