3a24873f491dba7f5a30d897a386a6cb5cf16f2c
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 23:34:44 +0000

    ef034c1 docs(docs-infra): renders inline code snippets in docs pills

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-4c0ba06
+ * @license Angular v21.1.0-next.0+sha-ef034c1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-4c0ba06');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-ef034c1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
