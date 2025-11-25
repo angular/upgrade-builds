667a550b60dde2ca2103c74f4046660c392a875d
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 20:20:42 +0000

    6aaddba docs: release notes for the v20.3.14 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-6b1fb02
+ * @license Angular v21.1.0-next.0+sha-6aaddba
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-6b1fb02');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-6aaddba');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
