63e74fcab3c2b5ad2285769eaec0242b0fc766e9
kirjs <kirts@google.com>
2025-11-25 16:44:42 +0000

    49ad7c6 fix(core): use injected `DOCUMENT` for `CSP_NONCE`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-4634b46
+ * @license Angular v21.0.0+sha-49ad7c6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-4634b46');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-49ad7c6');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
