a8e992d3b13f17a4b16e0bd41eb14e26c76ac541
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 12:48:30 +0000

    a294e3b docs: fix example imports and missing event URL reference

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-51ebe9a
+ * @license Angular v21.1.0-next.0+sha-a294e3b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-51ebe9a');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-a294e3b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
