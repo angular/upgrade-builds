392fec43e93cd60c8955831869e338eccc02b563
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 18:10:36 +0000

    400fc82 refactor: replace `getDocument()` with `inject(DOCUMENT)`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-3b1fa82
+ * @license Angular v21.1.0-next.0+sha-400fc82
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-3b1fa82');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-400fc82');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
