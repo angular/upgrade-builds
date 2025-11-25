b909e11049f830e361712dd5dbdd3e1532c6f272
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 16:39:16 +0000

    5bfa027 fix(compiler-cli): escape angular control flow in jsdoc

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-346caef
+ * @license Angular v21.1.0-next.0+sha-5bfa027
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-346caef');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-5bfa027');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
