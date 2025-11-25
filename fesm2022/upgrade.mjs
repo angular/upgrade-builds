6e865c22bb2ca0515d188538ddb3a14eff89c7cb
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 18:53:49 +0000

    40790ef fix(http): prevent XSRF token leakage to protocol-relative URLs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-400fc82
+ * @license Angular v21.1.0-next.0+sha-40790ef
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-400fc82');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-40790ef');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
