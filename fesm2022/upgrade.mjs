1315775f7c8941a26fb36fff383961456543feab
kirjs <kirts@google.com>
2025-11-25 15:56:37 +0000

    e3f5f34 refactor(forms): Make reset take value

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-9aa3e92
+ * @license Angular v21.0.0+sha-e3f5f34
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-9aa3e92');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-e3f5f34');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
