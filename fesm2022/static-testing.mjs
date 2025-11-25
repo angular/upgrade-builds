d04d2c512718932bcf9f0ed9a19565c39a2e8d39
kirjs <kirts@google.com>
2025-11-25 18:10:40 +0000

    908b5a4 refactor: replace `getDocument()` with `inject(DOCUMENT)`

--- fesm2022/static-testing.mjs
+++ fesm2022/static-testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-49ad7c6
+ * @license Angular v21.0.0+sha-908b5a4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -20,7 +20,7 @@ class AngularTestingModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0+sha-49ad7c6",
+    version: "21.0.0+sha-908b5a4",
     ngImport: i0,
     type: AngularTestingModule,
     deps: [{
@@ -30,13 +30,13 @@ class AngularTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.0.0+sha-49ad7c6",
+    version: "21.0.0+sha-908b5a4",
     ngImport: i0,
     type: AngularTestingModule
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.0.0+sha-49ad7c6",
+    version: "21.0.0+sha-908b5a4",
     ngImport: i0,
     type: AngularTestingModule,
     providers: [{
@@ -47,7 +47,7 @@ class AngularTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0+sha-49ad7c6",
+  version: "21.0.0+sha-908b5a4",
   ngImport: i0,
   type: AngularTestingModule,
   decorators: [{
