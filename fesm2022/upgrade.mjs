5f8ece499c4d0fa224a7b4af1c193aabbb0800d4
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 20:30:06 +0000

    4c0ba06 release: cut the v21.1.0-next.0 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-6aaddba
+ * @license Angular v21.1.0-next.0+sha-4c0ba06
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-6aaddba');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-4c0ba06');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
