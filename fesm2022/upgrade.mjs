c03a97c68b79f7c2a886718a4a3e59fafa756b37
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 15:41:30 +0000

    9aa3e92 build: lock file maintenance

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-477df38
+ * @license Angular v21.0.0+sha-9aa3e92
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-477df38');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-9aa3e92');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
