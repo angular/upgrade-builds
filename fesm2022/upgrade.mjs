3e978f36cbce49ad829a6052503daa32205cd65c
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 15:24:38 +0000

    67d41e9 docs: improve core package API documentation with additional reference links

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-3a1eb07
+ * @license Angular v21.1.0-next.0+sha-67d41e9
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-3a1eb07');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-67d41e9');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
