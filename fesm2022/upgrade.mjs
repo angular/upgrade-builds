6fbf72d469f050bda95bb1a8ac35e228b2dbbbbb
kirjs <kirts@google.com>
2025-11-25 15:26:15 +0000

    477df38 docs: improve core package API documentation with additional reference links

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.0+sha-8acf5d2
+ * @license Angular v21.0.0+sha-477df38
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-8acf5d2');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-477df38');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
