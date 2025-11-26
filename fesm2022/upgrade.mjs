ff66b0dc6275f063857849dd908701d90fbcdcab
kirjs <kirts@google.com>
2025-11-26 18:16:45 +0000

    1260d1a refactor(devtools): Use the same error message as the signal custom formatter for signal errors

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.1+sha-0ae14cb
+ * @license Angular v21.0.1+sha-1260d1a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.1+sha-0ae14cb');
+const VERSION = /* @__PURE__ */new Version('21.0.1+sha-1260d1a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
