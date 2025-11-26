5c9103b7d2b11c5fd93a0b512e0bfc03abbc157a
kirjs <kirts@google.com>
2025-11-26 18:48:32 +0000

    4207d89 docs(docs-infra): fix visible lines/regions

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.1+sha-1260d1a
+ * @license Angular v21.0.1+sha-4207d89
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.1+sha-1260d1a');
+const VERSION = /* @__PURE__ */new Version('21.0.1+sha-4207d89');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
