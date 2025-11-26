09b5f7f3b64bfa9d2159c27be93e41d465f7548c
kirjs <kirts@google.com>
2025-11-26 20:53:27 +0000

    99b6341 docs(docs-infra): fix form custom control example uses syntax

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.1+sha-4207d89
+ * @license Angular v21.0.1+sha-99b6341
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.1+sha-4207d89');
+const VERSION = /* @__PURE__ */new Version('21.0.1+sha-99b6341');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
