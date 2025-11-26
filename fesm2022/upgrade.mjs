632eec3d4e8e6ca647d9d4a457955cc15fa7fd6d
kirjs <kirts@google.com>
2025-11-26 17:02:47 +0000

    0ae14cb test(core): test bundling of dynamic component creation and bindings

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.1+sha-10201b6
+ * @license Angular v21.0.1+sha-0ae14cb
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.1+sha-10201b6');
+const VERSION = /* @__PURE__ */new Version('21.0.1+sha-0ae14cb');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
