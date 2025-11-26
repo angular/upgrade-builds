5f7d0282fa3736eba93594c1d9a81ae07721836c
Alan Agius <alan.agius4@gmail.com>
2025-11-26 08:13:29 +0000

    04101e6 docs: release notes for the v19.2.16 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-ef034c1
+ * @license Angular v21.1.0-next.0+sha-04101e6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-ef034c1');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-04101e6');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
