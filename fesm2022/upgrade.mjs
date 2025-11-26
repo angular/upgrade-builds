de3df0963ed489d1e2ca0de978d7eb77a8c2a3b4
GitHub <noreply@github.com>
2025-11-26 17:02:13 +0000

    add8c41 test(core): test bundling of dynamic component creation and bindings

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-04101e6
+ * @license Angular v21.1.0-next.0+sha-add8c41
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-04101e6');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-add8c41');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
