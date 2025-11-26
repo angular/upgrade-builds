144e1e0ddae88ce53579bb14d5adcc228df53d66
kirjs <kirts@google.com>
2025-11-26 21:07:23 +0000

    fec1f1a docs: update code snippets in reference doocumentation

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.1+sha-99b6341
+ * @license Angular v21.0.1+sha-fec1f1a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.1+sha-99b6341');
+const VERSION = /* @__PURE__ */new Version('21.0.1+sha-fec1f1a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
