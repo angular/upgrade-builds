182453b527a26f0563472e666d1e234c92944f72
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-26 18:18:09 +0000

    a0930e1 refactor(devtools): Use the same error message as the signal custom formatter for signal errors

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-add8c41
+ * @license Angular v21.1.0-next.0+sha-a0930e1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-add8c41');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-a0930e1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
