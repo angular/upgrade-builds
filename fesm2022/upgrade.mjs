00d76f99c899b8d7b8cc061bb5e7f2379c120709
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-12-01 11:56:15 +0000

    8c4a152 release: cut the v21.0.2 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.1+sha-78fd159
+ * @license Angular v21.0.2+sha-8c4a152
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.1+sha-78fd159');
+const VERSION = /* @__PURE__ */new Version('21.0.2+sha-8c4a152');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
