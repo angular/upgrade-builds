92743a34310d75d42d67351294ac9442ef83ebb5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-01 18:03:16 +0000

    721080a docs: fix npm link URL in creating-libraries guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-242d332
+ * @license Angular v21.1.0-next.0+sha-721080a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-242d332');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-721080a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
