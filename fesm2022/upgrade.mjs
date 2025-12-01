37e43b3b4995c4596401112fc13ab0d12c8b4f98
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-01 18:38:17 +0000

    aea2465 docs: update notes to use consistent formatting in various guides

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-25bf417
+ * @license Angular v21.1.0-next.0+sha-aea2465
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-25bf417');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-aea2465');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
