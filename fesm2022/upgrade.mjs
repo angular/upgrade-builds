0407e5d0bf89f3ad43eafead9b420c01ed6235ee
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 18:35:44 +0000

    e95d2e3 docs: Update adev/src/content/guide/forms/signals/models.md

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.2+sha-c2a8934
+ * @license Angular v21.0.2+sha-e95d2e3
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.2+sha-c2a8934');
+const VERSION = /* @__PURE__ */new Version('21.0.2+sha-e95d2e3');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
