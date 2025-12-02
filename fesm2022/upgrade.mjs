98d72ccaae9ac0b863637d04bfefc7870cee127c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 15:50:57 +0000

    a0ad5d4 test(router): Add tests for support of path params before/after a wildcard

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-e30e61b
+ * @license Angular v21.1.0-next.0+sha-a0ad5d4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-e30e61b');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-a0ad5d4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
