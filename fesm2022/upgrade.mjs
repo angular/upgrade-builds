e5a6cca8aa6dd964f35137c470dbe54b2974a59c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 18:18:01 +0000

    c2a8934 Revert "refactor(router): compress synchronous end to router navigation to single operator"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.2+sha-5a80a48
+ * @license Angular v21.0.2+sha-c2a8934
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.2+sha-5a80a48');
+const VERSION = /* @__PURE__ */new Version('21.0.2+sha-c2a8934');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
