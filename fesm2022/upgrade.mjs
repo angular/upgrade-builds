d10da4646d6c07f7ea7d7e3d71e2b61f0a992ad0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 11:18:01 +0000

    a784995 docs(docs-infra): Show examples on function overloads

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-c5a3bbb
+ * @license Angular v21.1.0-next.0+sha-a784995
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-c5a3bbb');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-a784995');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
