b98c8f22483ff5e465605517ae792b7b76bc79e5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 11:11:38 +0000

    c0e0010 fix(devtools): hide profiler node details when the frame is changed

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.2+sha-2935fef
+ * @license Angular v21.0.2+sha-c0e0010
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.2+sha-2935fef');
+const VERSION = /* @__PURE__ */new Version('21.0.2+sha-c0e0010');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
