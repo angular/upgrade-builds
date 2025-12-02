1c10e26fc0489c1b2fb87b5554a0941e96c80fa8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 14:17:27 +0000

    5a80a48 fix(compiler-cli): avoid allocating an object for signals in production mode

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.2+sha-017672f
+ * @license Angular v21.0.2+sha-5a80a48
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.2+sha-017672f');
+const VERSION = /* @__PURE__ */new Version('21.0.2+sha-5a80a48');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
