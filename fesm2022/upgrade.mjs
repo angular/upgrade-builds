8dd5953ee15e559d85916226073a387f01341af9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-03 19:30:48 +0000

    34caae5 ci: use defined ng-dev config for deploying docs site

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.3+sha-2e5d187
+ * @license Angular v21.0.3+sha-34caae5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.3+sha-2e5d187');
+const VERSION = /* @__PURE__ */new Version('21.0.3+sha-34caae5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
