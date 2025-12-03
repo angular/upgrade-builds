f0ba191d2e27087d419aaa1cd09e590a76aa71e3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-03 20:38:16 +0000

    565c022 build: disable assistant_to_the_branch_manager on forks

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.3+sha-34caae5
+ * @license Angular v21.0.3+sha-565c022
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.3+sha-34caae5');
+const VERSION = /* @__PURE__ */new Version('21.0.3+sha-565c022');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
