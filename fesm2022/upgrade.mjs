1a709ae86b220542f891078a3b3c12549ba9ebaa
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 12:29:42 +0000

    6491df9 build: lock file maintenance

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.2+sha-20474d3
+ * @license Angular v21.0.2+sha-6491df9
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.2+sha-20474d3');
+const VERSION = /* @__PURE__ */new Version('21.0.2+sha-6491df9');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
