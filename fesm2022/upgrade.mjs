2f9b269dd9bc8fd8548064c18b29e34922af5b44
GitHub <noreply@github.com>
2025-12-03 11:28:40 +0000

    0659d11 fix(http): enable XSRF protection for same-origin absolute URLs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-819b640
+ * @license Angular v21.1.0-next.0+sha-0659d11
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-819b640');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-0659d11');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
