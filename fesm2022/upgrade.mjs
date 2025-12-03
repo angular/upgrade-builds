2f51db35b9e0327e5c6c7dfb4848421a60e6df3c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 11:31:45 +0000

    20474d3 fix(http): enable XSRF protection for same-origin absolute URLs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.2+sha-0f50ea8
+ * @license Angular v21.0.2+sha-20474d3
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.2+sha-0f50ea8');
+const VERSION = /* @__PURE__ */new Version('21.0.2+sha-20474d3');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
