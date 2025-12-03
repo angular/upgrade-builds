7729d8856675fda94098f1ee73d0908662dc1089
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 10:58:57 +0000

    819b640 docs: prevent showing typescript diagnostic errors until the node runtime reaches a ready state

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.0+sha-13353bf
+ * @license Angular v21.1.0-next.0+sha-819b640
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-13353bf');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-819b640');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
