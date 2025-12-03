1db32af8ddea10966f2dd10547023bc85cf78c0c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 10:39:50 +0000

    c112ef1 Revert "build: update cross-repo angular dependencies"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.2+sha-42fbb64
+ * @license Angular v21.0.2+sha-c112ef1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.2+sha-42fbb64');
+const VERSION = /* @__PURE__ */new Version('21.0.2+sha-c112ef1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
