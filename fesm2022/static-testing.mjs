1154592100ba024455851cf2b5dbfd5ce013def7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 15:35:44 +0000

    2e5d187 fix(docs-infra): prevent readonly TypeScript keyword from being linked

--- fesm2022/static-testing.mjs
+++ fesm2022/static-testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.3+sha-c355ffd
+ * @license Angular v21.0.3+sha-2e5d187
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -20,7 +20,7 @@ class AngularTestingModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.3+sha-c355ffd",
+    version: "21.0.3+sha-2e5d187",
     ngImport: i0,
     type: AngularTestingModule,
     deps: [{
@@ -30,13 +30,13 @@ class AngularTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.0.3+sha-c355ffd",
+    version: "21.0.3+sha-2e5d187",
     ngImport: i0,
     type: AngularTestingModule
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.0.3+sha-c355ffd",
+    version: "21.0.3+sha-2e5d187",
     ngImport: i0,
     type: AngularTestingModule,
     providers: [{
@@ -47,7 +47,7 @@ class AngularTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.3+sha-c355ffd",
+  version: "21.0.3+sha-2e5d187",
   ngImport: i0,
   type: AngularTestingModule,
   decorators: [{
