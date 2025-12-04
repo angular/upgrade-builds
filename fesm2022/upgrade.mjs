fab5d2ab9749f14e1276f75ecc67bca2ddd0dfe4
GitHub <noreply@github.com>
2025-12-04 19:31:56 +0000

    c3bd22d build: update cross-repo angular dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.3+sha-bf1c12c
+ * @license Angular v21.0.3+sha-c3bd22d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.3+sha-bf1c12c');
+const VERSION = /* @__PURE__ */new Version('21.0.3+sha-c3bd22d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
