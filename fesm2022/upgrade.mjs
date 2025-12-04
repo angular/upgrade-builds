1586d5d7cafbc36fb7f6a69ba16fcc5a3ce955be
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-04 19:51:43 +0000

    28e7546 feat(docs-infra): forward preview error locations

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.3+sha-c3bd22d
+ * @license Angular v21.0.3+sha-28e7546
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.3+sha-c3bd22d');
+const VERSION = /* @__PURE__ */new Version('21.0.3+sha-28e7546');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
