05d8c15ecbac256ebcebd02206a2a09f2e86ac85
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-08 16:57:42 +0000

    5b911d6 docs(docs-infra): fix hideCopy option to code snippets

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.3+sha-28e7546
+ * @license Angular v21.0.3+sha-5b911d6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.3+sha-28e7546');
+const VERSION = /* @__PURE__ */new Version('21.0.3+sha-5b911d6');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
