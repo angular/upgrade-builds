05ec6adba437d0576c6b532da8e224e2d351e6df
Jan Olaf Martin <jan.krems@gmail.com>
2025-12-10 19:44:28 +0000

    954c546 release: bump Angular DevTools version to 1.6.3

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.1+sha-5988bde
+ * @license Angular v21.1.0-next.1+sha-954c546
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-5988bde');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-954c546');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
