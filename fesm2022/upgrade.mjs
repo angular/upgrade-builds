eeb36f597474f727fccb2785f5b4eafb51e6722a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-10 18:25:58 +0000

    497c0e3 refactor(core): Add transition.to property to FakeNavigation

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.3+sha-2dd971b
+ * @license Angular v21.0.3+sha-497c0e3
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.3+sha-2dd971b');
+const VERSION = /* @__PURE__ */new Version('21.0.3+sha-497c0e3');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
