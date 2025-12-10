a0f0c8b456ed4a530e29e3b8b42db669fcd25660
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-10 20:46:22 +0000

    c63ef9c docs: fix typos and clarify root effect

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.1+sha-af6c01f
+ * @license Angular v21.1.0-next.1+sha-c63ef9c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-af6c01f');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-c63ef9c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
