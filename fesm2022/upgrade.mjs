568365f898e153beb92ab01dc90c5c22c7ba0217
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-10 17:41:40 +0000

    834593b docs: use fenced code blocks and format examples

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.3+sha-01ab257
+ * @license Angular v21.0.3+sha-834593b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.3+sha-01ab257');
+const VERSION = /* @__PURE__ */new Version('21.0.3+sha-834593b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
