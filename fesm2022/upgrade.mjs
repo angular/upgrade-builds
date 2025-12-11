2720693a19c95ca598f1d9ab8d8a9be9d3c3f61d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 00:55:42 +0000

    d343ff3 docs: release notes for the v21.0.4 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.1+sha-a52c834
+ * @license Angular v21.1.0-next.1+sha-d343ff3
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-a52c834');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-d343ff3');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
