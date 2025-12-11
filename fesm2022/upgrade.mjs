677d82da6c2aed2d4926e2c8472876ce5e6c134d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 00:49:04 +0000

    a52c834 build: update ng-dev to latest version

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.1+sha-d0c3806
+ * @license Angular v21.1.0-next.1+sha-a52c834
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-d0c3806');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-a52c834');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
