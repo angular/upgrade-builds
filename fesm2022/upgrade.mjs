99b350d1360ea3b8c1ce42b03e8bc5df4f4b82e9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 19:29:17 +0000

    69d243a fix(core): avoid false-positive deprecation when using `InjectionToken` with factory only

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.4+sha-fa4e6e6
+ * @license Angular v21.0.4+sha-69d243a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.4+sha-fa4e6e6');
+const VERSION = /* @__PURE__ */new Version('21.0.4+sha-69d243a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
