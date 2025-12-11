ebfb4c4e701f22074c812f5098dc9a8ed9d96f37
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 23:38:39 +0000

    4fd2b72 fix(forms): fix signal forms type error

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.4+sha-69d243a
+ * @license Angular v21.0.4+sha-4fd2b72
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.4+sha-69d243a');
+const VERSION = /* @__PURE__ */new Version('21.0.4+sha-4fd2b72');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
