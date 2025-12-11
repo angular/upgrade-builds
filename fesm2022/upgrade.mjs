419eb2bc68c5c7b3f9b96e801dd56e143af9fbc3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 23:54:19 +0000

    7c23448 release: cut the v21.1.0-next.3 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.2+sha-bf86f53
+ * @license Angular v21.1.0-next.3+sha-7c23448
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.2+sha-bf86f53');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-7c23448');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
