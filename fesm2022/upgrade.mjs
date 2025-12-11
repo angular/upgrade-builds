7d7ab50376f69ba5e732314084de761b15d9feb2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 23:47:07 +0000

    5a21465 release: cut the v21.0.5 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.4+sha-4fd2b72
+ * @license Angular v21.0.5+sha-5a21465
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.4+sha-4fd2b72');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-5a21465');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
