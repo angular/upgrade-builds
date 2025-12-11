180a94e8b415054009fe5cf8a0eb266cb3233e90
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 00:48:04 +0000

    c937da4 build: update cross-repo angular dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.3+sha-67a5a65
+ * @license Angular v21.0.3+sha-c937da4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.3+sha-67a5a65');
+const VERSION = /* @__PURE__ */new Version('21.0.3+sha-c937da4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
