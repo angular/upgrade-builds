7472c6007fba9dd12b32d5f2a1166eb61d8a2025
Andrew Kushnir <akushnir@google.com>
2025-12-12 18:16:10 +0000

    8cabd67 docs: update `guidelines.md` to match `best-practices.md`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.5+sha-27bb376
+ * @license Angular v21.0.5+sha-8cabd67
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.5+sha-27bb376');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-8cabd67');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
