be2af6c5b066237db624800c84d93ff7c1b97549
Andrew Kushnir <akushnir@google.com>
2025-12-12 21:05:34 +0000

    534b1b5 Revert "refactor(compiler): remove unnecessary sanitization for safe attributes"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.5+sha-88e1829
+ * @license Angular v21.0.5+sha-534b1b5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.5+sha-88e1829');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-534b1b5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
