6f01937721fd19aaec21ff2dcd9ab03053c9448e
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-12 17:57:15 +0000

    16ab98f build: update dependency @actions/core to v2

--- fesm2022/static-testing.mjs
+++ fesm2022/static-testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.3+sha-348f149
+ * @license Angular v21.1.0-next.3+sha-16ab98f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -20,7 +20,7 @@ class AngularTestingModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.3+sha-348f149",
+    version: "21.1.0-next.3+sha-16ab98f",
     ngImport: i0,
     type: AngularTestingModule,
     deps: [{
@@ -30,13 +30,13 @@ class AngularTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.1.0-next.3+sha-348f149",
+    version: "21.1.0-next.3+sha-16ab98f",
     ngImport: i0,
     type: AngularTestingModule
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.1.0-next.3+sha-348f149",
+    version: "21.1.0-next.3+sha-16ab98f",
     ngImport: i0,
     type: AngularTestingModule,
     providers: [{
@@ -47,7 +47,7 @@ class AngularTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.3+sha-348f149",
+  version: "21.1.0-next.3+sha-16ab98f",
   ngImport: i0,
   type: AngularTestingModule,
   decorators: [{
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.3+sha-348f149
+ * @license Angular v21.1.0-next.3+sha-16ab98f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-348f149');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-16ab98f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
