fb7d36b818ef6b83e12b858cbb5502d0bf4ca7b6
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-12 16:17:56 +0000

    348f149 feat(forms): pass field directive to class config

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.3+sha-23547f0
+ * @license Angular v21.1.0-next.3+sha-348f149
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-23547f0');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-348f149');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
