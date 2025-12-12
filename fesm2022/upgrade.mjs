4bd9c9b758a1ecaab8efac4ca56f1c61d36e6be4
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-12 19:46:40 +0000

    88e1829 build: update cross-repo angular dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.5+sha-8cabd67
+ * @license Angular v21.0.5+sha-88e1829
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.5+sha-8cabd67');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-88e1829');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
