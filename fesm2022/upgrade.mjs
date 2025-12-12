a20371a0bfa204551766af9cdbdcfe972279f228
Andrew Kushnir <akushnir@google.com>
2025-12-12 01:17:02 +0000

    25f8007 docs(docs-infra): add primary color vars for tutorial button

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.5+sha-5a21465
+ * @license Angular v21.0.5+sha-25f8007
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.5+sha-5a21465');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-25f8007');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
