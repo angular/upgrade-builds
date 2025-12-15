acf297615ea84a0b5a0e79805ec98a452e3c3218
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-15 23:25:58 +0000

    b4f584c fix(core): return `StaticProvider` for `providePlatformInitializer`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.3+sha-11e271c
+ * @license Angular v21.1.0-next.3+sha-b4f584c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-11e271c');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-b4f584c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
