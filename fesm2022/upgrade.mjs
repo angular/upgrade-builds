3e3c31e7f18b1136c9d9dd2bc47989879520a54f
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-15 17:50:18 +0000

    b5205c7 docs(docs-infra): throw when rendering absolute links to adev in our guides

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.3+sha-8243bb3
+ * @license Angular v21.1.0-next.3+sha-b5205c7
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-8243bb3');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-b5205c7');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
