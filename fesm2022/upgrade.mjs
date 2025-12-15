9a4c727696eab0fef335e65c85a75bf9089d9335
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-15 17:51:09 +0000

    fe59d9a ci: fix deployement of previews

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.5+sha-534b1b5
+ * @license Angular v21.0.5+sha-fe59d9a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.5+sha-534b1b5');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-fe59d9a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
