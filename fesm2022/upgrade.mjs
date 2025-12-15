4117e900853de6947ac6d423ff6a38c7db96c8c0
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-15 19:50:22 +0000

    44d4439 refactor(forms): add signal forms to the type tests

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.3+sha-a2aa8fe
+ * @license Angular v21.1.0-next.3+sha-44d4439
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-a2aa8fe');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-44d4439');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
