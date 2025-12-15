f58e4611af288adfd681e9ed6f6cdb160fd17f36
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-15 21:32:55 +0000

    356092a docs: update cross-repo adev docs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.3+sha-44d4439
+ * @license Angular v21.1.0-next.3+sha-356092a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-44d4439');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-356092a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
