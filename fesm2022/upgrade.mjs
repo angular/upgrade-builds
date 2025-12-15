b0d0d240ac07b3f968f32c0afc8cc0cda2a589dd
Andrew Kushnir <akushnir@google.com>
2025-12-15 22:08:48 +0000

    5afb1eb docs(forms): improve JSDoc for Signal Forms Schema types

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.5+sha-0905eb6
+ * @license Angular v21.0.5+sha-5afb1eb
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.5+sha-0905eb6');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-5afb1eb');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
