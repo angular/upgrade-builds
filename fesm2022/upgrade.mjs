46447be545e79896ddfa1e69d1334f8e45a9cba7
GitHub <noreply@github.com>
2025-12-15 22:07:45 +0000

    193aa33 docs(forms): improve JSDoc for Signal Forms Schema types

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.3+sha-356092a
+ * @license Angular v21.1.0-next.3+sha-193aa33
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-356092a');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-193aa33');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
