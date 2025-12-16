92ab8332338af34b40a0910d8fd2b4978f800260
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-16 01:49:00 +0000

    3ff20a5 refactor(docs-infra): remove deprecated h.JSX.HTMLAttributes usage

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.3+sha-b4f584c
+ * @license Angular v21.1.0-next.3+sha-3ff20a5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-b4f584c');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-3ff20a5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
