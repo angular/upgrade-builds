118ef0977a75e09cba842179b713e76897b85f37
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-16 21:40:10 +0000

    06be803 fix(core): Microtask scheduling should be used after any application synchronization

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.3+sha-ae0c590
+ * @license Angular v21.1.0-next.3+sha-06be803
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-ae0c590');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-06be803');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
