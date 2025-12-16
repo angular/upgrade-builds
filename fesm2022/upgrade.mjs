c26e5e456f20edd6ac203856e307dc06208707cc
GitHub <noreply@github.com>
2025-12-16 18:31:13 +0000

    ae0c590 refactor(forms): rename field to fieldTree in FieldContext and ValidationError

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.3+sha-bf2e508
+ * @license Angular v21.1.0-next.3+sha-ae0c590
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-bf2e508');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-ae0c590');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
