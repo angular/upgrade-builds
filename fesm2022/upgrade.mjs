9ac096352691678eb4e964085b4b64ec901c751c
Andrew Kushnir <akushnir@google.com>
2025-12-16 17:47:25 +0000

    0a5fd6b refactor(core): conditionally include debug names based on `ngDevMode`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.5+sha-c250bd0
+ * @license Angular v21.0.5+sha-0a5fd6b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.5+sha-c250bd0');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-0a5fd6b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
