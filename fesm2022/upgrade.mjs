1e84535a7d7644069641c1c7123764405815cc89
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-16 22:50:04 +0000

    6270bba ci: reformat files

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.3+sha-06be803
+ * @license Angular v21.1.0-next.3+sha-6270bba
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-06be803');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-6270bba');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
