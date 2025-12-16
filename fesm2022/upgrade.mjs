55df8eda1e46ed4c28eeae9bcf72ddc0e3a41065
Andrew Kushnir <akushnir@google.com>
2025-12-16 23:04:20 +0000

    feed89e ci: reformat files

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.5+sha-729b964
+ * @license Angular v21.0.5+sha-feed89e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.5+sha-729b964');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-feed89e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
