6108fafb9718eaa4a349de3351624bd2e6742280
Andrew Kushnir <akushnir@google.com>
2025-12-17 23:47:45 +0000

    59b3790 refactor(forms): rename server errors to submission errors

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.6+sha-0edbee4
+ * @license Angular v21.0.6+sha-59b3790
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-0edbee4');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-59b3790');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
