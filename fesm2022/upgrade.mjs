08bdcbb1eb65c38f34cd1593d85b19a480952e9e
Andrew Kushnir <akushnir@google.com>
2025-12-17 01:44:08 +0000

    a4d2de8 Revert "fix(core): explicitly cast signal node value to String"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.5+sha-0eb623c
+ * @license Angular v21.0.5+sha-a4d2de8
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.5+sha-0eb623c');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-a4d2de8');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
