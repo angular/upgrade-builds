c4a82ca472ff371c639468e248b6fb97fdd6fd47
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 18:19:54 +0000

    868cb7c build: migrate Bazel ignore rules from .bazelignore to REPO.bazel

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.3+sha-2e58b66
+ * @license Angular v21.1.0-next.3+sha-868cb7c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-2e58b66');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-868cb7c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
