0c64c0327d3d210d14e99dd3234971b863beab7b
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 17:45:28 +0000

    d23ce76 docs(docs-infra): leverage the search on 404

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.3+sha-9fa77af
+ * @license Angular v21.1.0-next.3+sha-d23ce76
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-9fa77af');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-d23ce76');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
