144dbb813a26e2af6c7611d43a57c7750ac2d43b
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 01:42:32 +0000

    9fa77af Revert "fix(core): explicitly cast signal node value to String"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.3+sha-c501b25
+ * @license Angular v21.1.0-next.3+sha-9fa77af
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-c501b25');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-9fa77af');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
