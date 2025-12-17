edbcd321248ed73275bd9409e1e6b42280501bdd
Andrew Kushnir <akushnir@google.com>
2025-12-17 17:58:34 +0000

    3b3b982 docs: add documentation for schema validation with Signal Forms

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.5+sha-14b529e
+ * @license Angular v21.0.5+sha-3b3b982
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.5+sha-14b529e');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-3b3b982');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
