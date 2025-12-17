48436cfe44aef53288de72a666c2f26ac4712898
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 21:24:07 +0000

    664fa39 release: cut the v21.1.0-next.4 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.3+sha-868cb7c
+ * @license Angular v21.1.0-next.4+sha-664fa39
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-868cb7c');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-664fa39');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
