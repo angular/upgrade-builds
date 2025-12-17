be54619e76a8718da1bd65e988601a0920bebaa0
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 23:49:57 +0000

    99ad18a feat(core): Add stability debugging utility

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.4+sha-e2a9938
+ * @license Angular v21.1.0-next.4+sha-99ad18a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-e2a9938');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-99ad18a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
