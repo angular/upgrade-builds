0024b2b73257543026fa08bda2cf29ca67ff0174
Andrew Kushnir <akushnir@google.com>
2025-12-17 22:32:46 +0000

    c8d14d7 docs: fix incorrect line highlighting in tutorials

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.6+sha-eaa68c4
+ * @license Angular v21.0.6+sha-c8d14d7
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-eaa68c4');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-c8d14d7');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
