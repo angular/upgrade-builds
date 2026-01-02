c00d89eca88ad36312a4543fb7555781bbc3a220
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-02 07:43:33 +0000

    d868a5b build: update bazel dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.4+sha-5a146b3
+ * @license Angular v21.1.0-next.4+sha-d868a5b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-5a146b3');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-d868a5b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
