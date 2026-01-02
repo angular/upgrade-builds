4a66e354aa819d7696f73d2a468524b51063c188
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-02 13:29:40 +0000

    71a4373 build: update cross-repo angular dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.6+sha-f8b6a3f
+ * @license Angular v21.0.6+sha-71a4373
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-f8b6a3f');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-71a4373');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
