bfc9db563f86f9c62fb50ba6ce8f1f47e1058ddf
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-01-02 10:44:19 +0000

    f8b6a3f Revert "refactor(core): Add `ngDevMode` guards and new sanitization error codes"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.6+sha-d370c4b
+ * @license Angular v21.0.6+sha-f8b6a3f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-d370c4b');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-f8b6a3f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
