47b587733d475eb4cc6f8d4c8f7605808b0f7d08
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-05 20:55:28 +0000

    a6b8cb6 feat(common): support custom transformations in ImageKit and Imgix loaders

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.4+sha-14f5cc4
+ * @license Angular v21.1.0-next.4+sha-a6b8cb6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-14f5cc4');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-a6b8cb6');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
