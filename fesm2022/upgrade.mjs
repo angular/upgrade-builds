c483a2e73ae0ada0513864a3919af5fadc4c7b03
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-05 22:07:18 +0000

    df2aaaa docs(vscode-extension): Update developer docs to match current setup

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.4+sha-1532be9
+ * @license Angular v21.1.0-next.4+sha-df2aaaa
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-1532be9');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-df2aaaa');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
