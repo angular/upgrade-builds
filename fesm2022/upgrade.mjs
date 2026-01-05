eb315939bf70a8e728f127e6b172281ba0310140
kirjs <kirts@google.com>
2026-01-05 17:43:15 +0000

    9494b56 docs(docs-infra): sync heading colors between docs and API pages

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.6+sha-7e9dc56
+ * @license Angular v21.0.6+sha-9494b56
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-7e9dc56');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-9494b56');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
