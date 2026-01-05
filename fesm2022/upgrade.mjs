fa35e4339342a5f26de08fe6219456bf2c8f9afd
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-05 17:42:33 +0000

    e6631f0 docs(docs-infra): sync heading colors between docs and API pages

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.4+sha-6a94300
+ * @license Angular v21.1.0-next.4+sha-e6631f0
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-6a94300');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-e6631f0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
