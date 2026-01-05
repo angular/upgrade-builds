f9784e1ac502b23c1c44f9c98c4a6676779408ee
kirjs <kirts@google.com>
2026-01-05 21:59:18 +0000

    8f26a52 refactor(core): introduce tree-shakeable runtime error codes for `NgModule` handling and `ViewContainerRef` errors

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.6+sha-1748c89
+ * @license Angular v21.0.6+sha-8f26a52
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-1748c89');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-8f26a52');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
