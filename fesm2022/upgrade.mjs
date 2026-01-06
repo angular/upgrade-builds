303d8f017465e8fe4ed4945da84d20daec04fc06
kirjs <kirts@google.com>
2026-01-06 22:31:04 +0000

    e3fba18 feat(forms): add `[formField]` directive

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.6+sha-defc924
+ * @license Angular v21.0.6+sha-e3fba18
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-defc924');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-e3fba18');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
