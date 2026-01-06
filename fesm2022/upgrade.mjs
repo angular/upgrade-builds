279dfcefcf7a790aec94998e71063c37e14efb15
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-06 15:17:40 +0000

    db470f0 refactor(router): Remove internal rxjs recognize implementation

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.4+sha-71cde39
+ * @license Angular v21.1.0-next.4+sha-db470f0
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-71cde39');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-db470f0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
