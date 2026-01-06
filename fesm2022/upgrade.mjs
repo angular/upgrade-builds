963f21cf787027456a1cbe8704c28267c1fe297f
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-06 00:31:29 +0000

    456ca35 docs(forms): fix duplicate validator reference titles in AbstractControl

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.4+sha-9184146
+ * @license Angular v21.1.0-next.4+sha-456ca35
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-9184146');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-456ca35');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
