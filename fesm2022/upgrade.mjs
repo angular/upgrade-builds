0d5d755d1eead0a0f79e24c6a29b2d1c96e571d2
kirjs <kirts@google.com>
2026-01-06 18:24:58 +0000

    af1d4ac refactor(forms): bind native properties on interop controls

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.6+sha-8e80874
+ * @license Angular v21.0.6+sha-af1d4ac
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-8e80874');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-af1d4ac');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
