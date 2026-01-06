66ed0133cdfdad7415bfccc4c1e168a6fa0b0b0f
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-06 15:39:53 +0000

    b93d5ec docs(forms): add documentation links to Signal Forms API

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.4+sha-f05d0fe
+ * @license Angular v21.1.0-next.4+sha-b93d5ec
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-f05d0fe');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-b93d5ec');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
