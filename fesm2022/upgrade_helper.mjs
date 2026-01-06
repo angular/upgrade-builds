f603ba26a6d48db10327ce8de2d621ae9870c56b
GitHub <noreply@github.com>
2026-01-06 20:59:44 +0000

    c2c2b4a fix(core): sanitize sensitive attributes on SVG script elements

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.15+sha-4755bbd
+ * @license Angular v20.3.15+sha-c2c2b4a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.15+sha-4755bbd');
+const VERSION = /* @__PURE__ */ new Version('20.3.15+sha-c2c2b4a');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
