29f0bc470a65309d376ba2b08433ee109d0ffa46
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-06 18:24:48 +0000

    82a2de2 refactor(forms): bind native properties on interop controls

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.4+sha-10da2f9
+ * @license Angular v21.1.0-next.4+sha-82a2de2
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-10da2f9');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-82a2de2');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
