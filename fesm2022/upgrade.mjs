39a6584c8abc1295795465d8294444205078b0f7
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-06 17:27:27 +0000

    10da2f9 fix(compiler): better types for a few expression AST nodes

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.4+sha-b93d5ec
+ * @license Angular v21.1.0-next.4+sha-10da2f9
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-b93d5ec');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-10da2f9');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
