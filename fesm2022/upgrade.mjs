31e7a2cd4def3d0934e8ea884abd58f6a0b1d8ab
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-06 00:43:50 +0000

    71cde39 docs: rewrite testing docs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.4+sha-456ca35
+ * @license Angular v21.1.0-next.4+sha-71cde39
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-456ca35');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-71cde39');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
