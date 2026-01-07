cb57460fc74e8282c43675c9da0e7406a8fd08e3
kirjs <kirts@google.com>
2026-01-07 19:25:12 +0000

    c3adc5a docs(docs-infra): Fix combobox dialog styles for dark theme

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.6+sha-61370dc
+ * @license Angular v21.0.6+sha-c3adc5a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-61370dc');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-c3adc5a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
