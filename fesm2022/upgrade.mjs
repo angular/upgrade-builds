9d236c457590cea81db9d426f08e491e881d014f
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-07 22:49:33 +0000

    bb0dc8c refactor(core): typo in stability debugging warning message

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.4+sha-5c75d29
+ * @license Angular v21.1.0-next.4+sha-bb0dc8c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-5c75d29');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-bb0dc8c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
