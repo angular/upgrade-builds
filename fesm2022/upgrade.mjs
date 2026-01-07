c1ad48704c2f440f6e543c9bee92ce024b8080f3
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 14:29:12 +0000

    640693d feat(compiler): Add support for multiple swich cases matching consecutive `@case` blocks are now supported:

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.4+sha-f811c6c
+ * @license Angular v21.1.0-next.4+sha-640693d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-f811c6c');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-640693d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
