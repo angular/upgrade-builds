12d5da3565f4a59fb3a5e1271a015eace76a9235
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 18:47:01 +0000

    241bdeb feat(devtools): create an info snackbar when a user logs a property

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.4+sha-80b0fbb
+ * @license Angular v21.1.0-next.4+sha-241bdeb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-80b0fbb');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-241bdeb');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
