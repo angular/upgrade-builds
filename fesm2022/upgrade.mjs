97e0185d9510826f7d32a9edc83c8a13aef8cb3f
kirjs <kirts@google.com>
2026-01-07 14:28:50 +0000

    c1e9627 docs(core): fix Ivy view data docs tables

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.6+sha-0ff2d5a
+ * @license Angular v21.0.6+sha-c1e9627
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-0ff2d5a');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-c1e9627');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
