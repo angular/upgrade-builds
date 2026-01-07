2da49e00ff6d83b4889f9ddf307f54f9ef66e8d5
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 17:48:31 +0000

    80b0fbb fix(core): avoid leaking view data in animations

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.4+sha-97fd1de
- * (c) 2010-2025 Google LLC. https://angular.dev/
+ * @license Angular v21.1.0-next.4+sha-80b0fbb
+ * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-97fd1de');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-80b0fbb');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
