9986068d1ddf1ce99ca3826e6f88622a75df8b92
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 20:15:21 +0000

    46dbd18 refactor(forms): remove `customError()`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.4+sha-090484c
+ * @license Angular v21.1.0-next.4+sha-46dbd18
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-090484c');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-46dbd18');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
