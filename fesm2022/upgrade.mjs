8c2e2c5b61fd0b6d4d55d09a7e0c3477ac8529b0
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 14:41:29 +0000

    b17f93b docs(docs-infra): Use category for signal forms guides

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.4+sha-640693d
+ * @license Angular v21.1.0-next.4+sha-b17f93b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-640693d');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-b17f93b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
