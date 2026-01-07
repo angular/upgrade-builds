3c47b36fb96fca9ca9391b9a6e4c635e11b672eb
kirjs <kirts@google.com>
2026-01-07 14:16:15 +0000

    0ff2d5a docs: fixed incorrect link in migrating-to-vitest.md

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.6+sha-4161161
+ * @license Angular v21.0.6+sha-0ff2d5a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-4161161');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-0ff2d5a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
