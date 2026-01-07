cf37547a7ec8d879f799016cf6f987233e3430bb
kirjs <kirts@google.com>
2026-01-07 17:45:09 +0000

    7c74674 fix(core): avoid leaking view data in animations

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.6+sha-7738eb9
+ * @license Angular v21.0.6+sha-7c74674
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-7738eb9');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-7c74674');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
