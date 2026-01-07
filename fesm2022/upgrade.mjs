e5d2330aba9b5b8cfda799c537baa7f6f5aab056
kirjs <kirts@google.com>
2026-01-07 20:15:44 +0000

    ff53175 refactor(forms): remove `customError()`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.6+sha-c3adc5a
+ * @license Angular v21.0.6+sha-ff53175
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-c3adc5a');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-ff53175');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
