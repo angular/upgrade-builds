13568fcb2b22f181add88ab08686aa99bcffbe98
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-07 00:34:20 +0000

    4161161 Revert "fix(router): Ensure createUrlTree does not reuse segments of input"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.6+sha-e3fba18
+ * @license Angular v21.0.6+sha-4161161
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-e3fba18');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-4161161');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
