d1ddc805a71537f0b655fc96af4fc665f29bcb6a
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 22:26:47 +0000

    af4e521 docs: release notes for the v20.3.16 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.4+sha-f8db6ad
+ * @license Angular v21.1.0-next.4+sha-af4e521
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-f8db6ad');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-af4e521');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
