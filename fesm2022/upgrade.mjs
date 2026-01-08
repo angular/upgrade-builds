6266ba3d048e9d078448d1654015df3a1f6ac957
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-08 20:25:04 +0000

    a2b9429 Revert "feat(router): add trailingSlash config option"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.4+sha-86dc128
+ * @license Angular v21.1.0-next.4+sha-a2b9429
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-86dc128');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-a2b9429');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
