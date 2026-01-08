54a94e4985768dfcf9908bc470c37f7d07cf9555
GitHub <noreply@github.com>
2026-01-08 16:32:18 +0000

    12fccc5 feat(router): add trailingSlash config option

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.4+sha-7003e8d
+ * @license Angular v21.1.0-next.4+sha-12fccc5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-7003e8d');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-12fccc5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
