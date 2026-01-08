0a17d83198e3898143d27f337b5e71d729d36e8c
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-08 18:08:39 +0000

    2b83091 docs(forms): make onSubmit method async for form submission

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.7+sha-32ff1a0
+ * @license Angular v21.0.7+sha-2b83091
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.7+sha-32ff1a0');
+const VERSION = /* @__PURE__ */new Version('21.0.7+sha-2b83091');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
