5e21897e781069fc77f647f6b337ebdb14b1a00b
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-08 17:12:50 +0000

    32ff1a0 docs(docs-infra): Fix toggle button in dark mode Fix visibility toggle button in dark mode

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.7+sha-8aab227
+ * @license Angular v21.0.7+sha-32ff1a0
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.7+sha-8aab227');
+const VERSION = /* @__PURE__ */new Version('21.0.7+sha-32ff1a0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
