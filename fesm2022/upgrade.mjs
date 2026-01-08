ca57ede901ec78801d808966ffb80cb51ffa6e39
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-08 16:41:38 +0000

    1488c2e docs: fix broken anchor link for Angular versioning section

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.7+sha-f5bad8e
+ * @license Angular v21.0.7+sha-1488c2e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.7+sha-f5bad8e');
+const VERSION = /* @__PURE__ */new Version('21.0.7+sha-1488c2e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
