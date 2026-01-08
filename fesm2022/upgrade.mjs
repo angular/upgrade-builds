f9cd25d406b2923a03edb7402ede5ae9bcde45aa
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-08 00:20:40 +0000

    7003e8d feat(router): Publish Router's integration with platform Navigation API as experimental

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-next.4+sha-0ad3adc
+ * @license Angular v21.1.0-next.4+sha-7003e8d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-0ad3adc');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-7003e8d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
