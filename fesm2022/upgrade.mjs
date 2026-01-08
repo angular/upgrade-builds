9010ddfe00c6fc7b5a7e79df28f53c39c9f707de
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-08 21:33:46 +0000

    74af7d8 refactor(core): Use the provided Document value rather than global in FakeNavigation

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-6ad2ef6
+ * @license Angular v21.1.0-rc.0+sha-74af7d8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-6ad2ef6');
+const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-74af7d8');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
