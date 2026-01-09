12ed1722da8d7acf0640bae601b537c68e0b7af6
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-09 22:45:24 +0000

    615a77b refactor(compiler-cli): update error message links to versioned docs (#66374)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.8+sha-5bc072e
+ * @license Angular v21.0.8+sha-615a77b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-5bc072e');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-615a77b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
