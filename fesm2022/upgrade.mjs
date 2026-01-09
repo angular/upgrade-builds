d26fd5c2cb19054c37f3750a76c279ada30a7a99
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-09 16:04:03 +0000

    5dd2f2b docs(docs-infra): improve label fallback logic in search results

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-4391c44
+ * @license Angular v21.1.0-rc.0+sha-5dd2f2b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-4391c44');
+const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-5dd2f2b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
