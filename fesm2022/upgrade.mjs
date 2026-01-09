083fac0d1d708db86d7d97b1384bbab4edd321d6
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-09 18:47:24 +0000

    99db2e9 ci: add pnpm and typescript exclusions to a Renovate package rule.

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-4fb24ca
+ * @license Angular v21.2.0-next.0+sha-99db2e9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-4fb24ca');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-99db2e9');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
