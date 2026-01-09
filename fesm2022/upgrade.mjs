4f3fcf801ccc3a0f547fac106bbaf87e83fe72a3
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-09 18:04:51 +0000

    4fb24ca refactor(compiler): switch `Binary.isAssignmentOperation` to type guard function

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-94b7079
+ * @license Angular v21.2.0-next.0+sha-4fb24ca
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-94b7079');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-4fb24ca');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
