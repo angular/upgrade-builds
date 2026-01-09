283b3ec031d3890cba18c7f9c3aa9877469f09f5
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-09 17:01:31 +0000

    24fa4a2 docs(forms): Clarify returning errors from submit functions

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.8+sha-491a156
+ * @license Angular v21.0.8+sha-24fa4a2
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-491a156');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-24fa4a2');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
