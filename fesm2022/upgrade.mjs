c612bd10a05e44eab039ffca2811c9298c959378
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-09 16:28:15 +0000

    e66aeac refactor(devtools): style the profiler dialogs to match the current design

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-3a65814
+ * @license Angular v21.2.0-next.0+sha-e66aeac
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-3a65814');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-e66aeac');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
