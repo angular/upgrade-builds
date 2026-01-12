45b55b10f2ebcbe3840ae196ec3f2a11747918fb
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 18:12:22 +0000

    d1f6bcd docs: remove Component suffix in animations, HTTP, and SSR guides

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-481557e
+ * @license Angular v21.1.0-rc.0+sha-d1f6bcd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-481557e');
+const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-d1f6bcd');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
