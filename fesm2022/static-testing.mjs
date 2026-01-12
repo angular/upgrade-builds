8ce4343126335ae0ef424955472590b9d685a48e
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-12 16:37:07 +0000

    66b40d4 docs: update testing guide to include Vitest and improve authGuard test setup

--- fesm2022/static-testing.mjs
+++ fesm2022/static-testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-7f1b790
+ * @license Angular v21.2.0-next.0+sha-66b40d4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -20,7 +20,7 @@ class AngularTestingModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.0+sha-7f1b790",
+    version: "21.2.0-next.0+sha-66b40d4",
     ngImport: i0,
     type: AngularTestingModule,
     deps: [{
@@ -30,13 +30,13 @@ class AngularTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.2.0-next.0+sha-7f1b790",
+    version: "21.2.0-next.0+sha-66b40d4",
     ngImport: i0,
     type: AngularTestingModule
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.2.0-next.0+sha-7f1b790",
+    version: "21.2.0-next.0+sha-66b40d4",
     ngImport: i0,
     type: AngularTestingModule,
     providers: [{
@@ -47,7 +47,7 @@ class AngularTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.0+sha-7f1b790",
+  version: "21.2.0-next.0+sha-66b40d4",
   ngImport: i0,
   type: AngularTestingModule,
   decorators: [{
