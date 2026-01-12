697341f0418a5e5f6264eb6a083e046447c3f319
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 16:24:23 +0000

    5b0e705 docs: improve separation between enter and leave example

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.8+sha-615a77b
+ * @license Angular v21.0.8+sha-5b0e705
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-615a77b');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-5b0e705');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
