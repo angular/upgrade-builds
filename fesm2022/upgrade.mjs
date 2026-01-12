abd92ef0aa961b410e94f15420a3b55ea71f2d1d
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 16:37:42 +0000

    204aaca docs: update testing guide to include Vitest and improve authGuard test setup

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-4871131
+ * @license Angular v21.1.0-rc.0+sha-204aaca
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-4871131');
+const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-204aaca');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
