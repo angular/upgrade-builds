45ec8039f15210532880059af0025b6142e28f91
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 21:59:09 +0000

    82d556a fix(forms): Ensure the control instruction comes after the other bindings

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.8+sha-7902344
+ * @license Angular v21.0.8+sha-82d556a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-7902344');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-82d556a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
