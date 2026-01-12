bc4ad17d2649261a45c7518d9e3d975c27575bbd
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-12 17:55:17 +0000

    b228355 docs(docs-infra): prevent from showing duplicate usage blocks in functions API docs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-a056413
+ * @license Angular v21.2.0-next.0+sha-b228355
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-a056413');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-b228355');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
