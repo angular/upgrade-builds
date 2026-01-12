141d3167da2b6bb8f82f539a0866ba151c1a5e11
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 17:35:29 +0000

    addccea docs: change language section to "community translations"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-8f451b5
+ * @license Angular v21.1.0-rc.0+sha-addccea
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-8f451b5');
+const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-addccea');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
