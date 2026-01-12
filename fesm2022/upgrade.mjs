ba08aadd955d9e381305433ebd16a81954663ade
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-12 21:57:15 +0000

    1ba9b7a feat(core): resource composition via snapshots

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-1b4dcc0
+ * @license Angular v21.2.0-next.0+sha-1ba9b7a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-1b4dcc0');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-1ba9b7a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
