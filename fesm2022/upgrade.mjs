9f04277a76ac2f59b491a0810819fd1d12de8785
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 21:48:21 +0000

    5ad2e1b docs: replace old https://angular.io/license with https://angular.dev/license

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-1f47e88
+ * @license Angular v21.1.0-rc.0+sha-5ad2e1b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-1f47e88');
+const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-5ad2e1b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
