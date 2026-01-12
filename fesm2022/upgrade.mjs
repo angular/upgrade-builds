56f50f83b6ada5c4f08fdbf6435a6a4818558847
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 22:58:33 +0000

    d5caac6 build: fix changelog generation for last rev

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.8+sha-82d556a
+ * @license Angular v21.0.8+sha-d5caac6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-82d556a');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-d5caac6');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
