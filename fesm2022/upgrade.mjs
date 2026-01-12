ba92c25e65e46521c4af308b8742b37db4367610
GitHub <noreply@github.com>
2026-01-12 17:01:30 +0000

    89d47d8 refactor(router): Change RouterLink internals to use signals

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-405b056
+ * @license Angular v21.2.0-next.0+sha-89d47d8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-405b056');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-89d47d8');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
