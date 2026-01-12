4f3bc37e91217df48ca47657bd6d4a277628f739
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-12 17:47:15 +0000

    a056413 docs(docs-infra): Show API decorators docs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-ad699ce
+ * @license Angular v21.2.0-next.0+sha-a056413
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-ad699ce');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-a056413');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
