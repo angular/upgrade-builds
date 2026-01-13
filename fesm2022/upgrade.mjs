e32fffd64ff1b86a90fcc83b2679fd6ce0596a84
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-13 19:51:08 +0000

    492b106 refactor(core): remove ng when invoking `enableProdMode`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.8+sha-99b10e4
+ * @license Angular v21.0.8+sha-492b106
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-99b10e4');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-492b106');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
