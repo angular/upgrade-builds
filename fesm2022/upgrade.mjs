48792ab6a6ff21f76c6b3d44c3e391a6e1471c0f
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-13 21:13:43 +0000

    227acdd build: Update vscode release script

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-cb5879f
+ * @license Angular v21.2.0-next.0+sha-227acdd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-cb5879f');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-227acdd');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
