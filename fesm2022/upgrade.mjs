53bdd2840186f7e6022bb84c8a564619d0c6a550
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-13 16:38:11 +0000

    33de839 docs: update hierarchical-dependency-injection.md

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.8+sha-6e8f142
+ * @license Angular v21.0.8+sha-33de839
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-6e8f142');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-33de839');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
