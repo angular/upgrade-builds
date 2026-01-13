fb615d666225aaffd2a307bdc6cc86d956e63d0f
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-13 17:11:09 +0000

    01e5bab docs: update OnChanges API reference w/ signal input

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.8+sha-8f4cc84
+ * @license Angular v21.0.8+sha-01e5bab
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-8f4cc84');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-01e5bab');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
