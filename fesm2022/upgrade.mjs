14e98e3f49312b45f6b23abe0240fa9087117bce
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-13 16:43:46 +0000

    e8f8ac2 build: remove `@angular-devkit/architect-cli` from dev dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-69da639
+ * @license Angular v21.2.0-next.0+sha-e8f8ac2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-69da639');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-e8f8ac2');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
