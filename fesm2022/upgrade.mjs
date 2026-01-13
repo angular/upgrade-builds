c966a9783e6ec83205be8c9a5ae13cf37a0f3972
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-13 19:26:14 +0000

    99b10e4 build: update cherrypick changelog to stash

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.8+sha-01e5bab
+ * @license Angular v21.0.8+sha-99b10e4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-01e5bab');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-99b10e4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
