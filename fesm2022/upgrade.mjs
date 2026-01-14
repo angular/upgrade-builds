e4b407cccab2781717eb2695776ca2fc409ecd60
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-14 17:26:10 +0000

    79273c1 Revert "docs(docs-infra): use Signals Forms focusBoundControl"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.8+sha-d710ebe
+ * @license Angular v21.0.8+sha-79273c1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-d710ebe');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-79273c1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
