823f4962cc15a0d98aec20934391b3914c20694f
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-14 16:17:59 +0000

    b0b341f docs: add IMPORTANT prefix to pipe usage warning and

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.8+sha-79e6dfd
+ * @license Angular v21.0.8+sha-b0b341f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-79e6dfd');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-b0b341f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
