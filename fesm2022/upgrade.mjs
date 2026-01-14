2b4e93986050ae6768f040d0d7c4c54da36f75ef
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-14 16:31:14 +0000

    d710ebe docs(docs-infra): Exempts animation-related symbols from linking

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.0.8+sha-b0b341f
+ * @license Angular v21.0.8+sha-d710ebe
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-b0b341f');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-d710ebe');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
