d3f55cc1e7b5bc3f4023d800c14dfb3e5a561de4
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-14 17:31:32 +0000

    3f13db8 ci: update algolia synonym updater to use node directly rather than tsx

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-724e917
+ * @license Angular v21.2.0-next.0+sha-3f13db8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-724e917');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-3f13db8');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
