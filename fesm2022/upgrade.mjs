84aee5e03a893b01e55c7e2ccb03139230c48df5
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-14 19:37:13 +0000

    d12e6f0 docs: release notes for the v21.0.9 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-3f13db8
+ * @license Angular v21.2.0-next.0+sha-d12e6f0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-3f13db8');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-d12e6f0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
