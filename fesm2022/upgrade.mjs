42cffa0219840179e7d42a840a9ea073a70928ed
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-14 16:33:26 +0000

    1ea5c97 feat(forms): allow focusing bound control from field state

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-654ed65
+ * @license Angular v21.1.0-rc.0+sha-1ea5c97
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-654ed65');
+const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-1ea5c97');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
