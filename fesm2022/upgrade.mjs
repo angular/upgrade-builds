3c0f05017f3ee33bc7803a601be0526182c82061
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-15 19:16:03 +0000

    87f4797 refactor(devtools): drop dagre graph type

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-a792315
+ * @license Angular v21.2.0-next.0+sha-87f4797
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-a792315');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-87f4797');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
