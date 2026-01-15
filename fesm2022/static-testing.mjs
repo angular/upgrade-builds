20ef552a1481a799fc1d4e02d86ff0e45fcfeb57
Alon Mishne <amishne@users.noreply.github.com>
2026-01-15 21:48:53 +0000

    ded654d build: initial test of TypeScript 6

--- fesm2022/static-testing.mjs
+++ fesm2022/static-testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0+sha-aa39f87
+ * @license Angular v21.1.0+sha-ded654d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -20,7 +20,7 @@ class AngularTestingModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0+sha-aa39f87",
+    version: "21.1.0+sha-ded654d",
     ngImport: i0,
     type: AngularTestingModule,
     deps: [{
@@ -30,13 +30,13 @@ class AngularTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.1.0+sha-aa39f87",
+    version: "21.1.0+sha-ded654d",
     ngImport: i0,
     type: AngularTestingModule
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.1.0+sha-aa39f87",
+    version: "21.1.0+sha-ded654d",
     ngImport: i0,
     type: AngularTestingModule,
     providers: [{
@@ -47,7 +47,7 @@ class AngularTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0+sha-aa39f87",
+  version: "21.1.0+sha-ded654d",
   ngImport: i0,
   type: AngularTestingModule,
   decorators: [{
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0+sha-aa39f87
+ * @license Angular v21.1.0+sha-ded654d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-aa39f87');
+const VERSION = /* @__PURE__ */new Version('21.1.0+sha-ded654d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
