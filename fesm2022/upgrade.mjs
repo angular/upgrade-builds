4cb8b90c0db8607ecb063c2579ff57627a494f5a
Alon Mishne <amishne@users.noreply.github.com>
2026-01-15 19:46:30 +0000

    aa39f87 docs: update directive name from `[field]` to `[formField]` in signal-forms documentation

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0+sha-0643f54
+ * @license Angular v21.1.0+sha-aa39f87
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-0643f54');
+const VERSION = /* @__PURE__ */new Version('21.1.0+sha-aa39f87');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
