65ef3a71d4e54134b38637d5d5b425a37152dfc6
Alon Mishne <amishne@users.noreply.github.com>
2026-01-15 19:14:25 +0000

    0643f54 docs: clarify that FESM can depend on shared chunks

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0+sha-c0446da
+ * @license Angular v21.1.0+sha-0643f54
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-c0446da');
+const VERSION = /* @__PURE__ */new Version('21.1.0+sha-0643f54');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
