dde0adacb8641456275941013db3e859d60f0126
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-15 17:04:44 +0000

    a792315 docs(docs-infra): prevent heading from linking symbols

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-61614f6
+ * @license Angular v21.2.0-next.0+sha-a792315
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-61614f6');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-a792315');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
