be4c093f944f7e67cce024fcc9ef13ca417408e8
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-16 19:11:54 +0000

    e55260f build: update lock file

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-52bc020
+ * @license Angular v21.2.0-next.0+sha-e55260f-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-52bc020');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-e55260f-with-local-changes');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
