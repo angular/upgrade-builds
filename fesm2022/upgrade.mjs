a309f9b08409cbcc3e0d920efb0bec50111a5643
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-16 21:33:46 +0000

    5d46554 docs: Add section on `animate.leave` for clarity

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-e55260f-with-local-changes
+ * @license Angular v21.2.0-next.0+sha-5d46554-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-e55260f-with-local-changes');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-5d46554-with-local-changes');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
