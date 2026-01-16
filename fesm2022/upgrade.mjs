796dc08b5570b1bce0360441f557a3b585b9fdd0
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-16 17:33:43 +0000

    52bc020 build: move zone.js build off deprecated flag

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-d9c980a
+ * @license Angular v21.2.0-next.0+sha-52bc020
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-d9c980a');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-52bc020');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
