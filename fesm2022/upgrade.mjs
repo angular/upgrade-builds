3729a15ac76cfe5607d5e099f91753acbef1dd6a
Alon Mishne <amishne@users.noreply.github.com>
2026-01-20 18:30:12 +0000

    e664249 ci: add template for vscode dedicated issues

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0+sha-9cc0f4d
+ * @license Angular v21.1.0+sha-e664249
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-9cc0f4d');
+const VERSION = /* @__PURE__ */new Version('21.1.0+sha-e664249');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
