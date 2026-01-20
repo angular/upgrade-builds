5d5725d9ce4b12aa91e77b6bd85d78417969df4e
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-20 18:09:23 +0000

    9cc0f4d docs: update cross-repo adev docs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0+sha-82593de
+ * @license Angular v21.1.0+sha-9cc0f4d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-82593de');
+const VERSION = /* @__PURE__ */new Version('21.1.0+sha-9cc0f4d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
