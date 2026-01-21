37735bfb06a091ca3a68ce34d6aa52fd0d0cf1fb
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-21 22:06:54 +0000

    f577f8c release: cut the v21.1.1 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0+sha-643f0f4
+ * @license Angular v21.1.1+sha-f577f8c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-643f0f4');
+const VERSION = /* @__PURE__ */new Version('21.1.1+sha-f577f8c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
