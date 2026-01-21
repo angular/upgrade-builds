c03e92c5be685220f19075e4a6df6502ec267f3e
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-21 01:22:05 +0000

    b275206 fix(docs-infra): use focus-visible for focus styling

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-6284126
+ * @license Angular v21.2.0-next.0+sha-b275206
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-6284126');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-b275206');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
