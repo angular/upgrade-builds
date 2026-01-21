11bb47ce93402fdd9b0a239f7ce5ca32d37e8dd5
Alon Mishne <amishne@users.noreply.github.com>
2026-01-21 19:45:29 +0000

    1884226 docs(docs-infra): Handle additional description format

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0+sha-223ff74
+ * @license Angular v21.1.0+sha-1884226
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-223ff74');
+const VERSION = /* @__PURE__ */new Version('21.1.0+sha-1884226');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
