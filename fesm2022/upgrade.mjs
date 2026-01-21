eb78b3a8686b170684e5ec403b81f84ef52a2daf
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-21 00:20:18 +0000

    94a8f79 fix(docs-infra): don't use private symbol

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-ed78fa0
+ * @license Angular v21.2.0-next.0+sha-94a8f79
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-ed78fa0');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-94a8f79');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
