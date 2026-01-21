db9579ed025427340e514f3f4e23523fb7e10134
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-21 20:51:49 +0000

    2c53243 build: update cross-repo angular dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-f0b1061
+ * @license Angular v21.2.0-next.0+sha-2c53243
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-f0b1061');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-2c53243');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
