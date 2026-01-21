6b054480f93f7da45b60f258e6aec7a7d477df9d
Alon Mishne <amishne@users.noreply.github.com>
2026-01-21 18:33:03 +0000

    b9c995e refactor: Delete counterproductive aria-label

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0+sha-6a56204
+ * @license Angular v21.1.0+sha-b9c995e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-6a56204');
+const VERSION = /* @__PURE__ */new Version('21.1.0+sha-b9c995e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
