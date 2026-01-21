1df1d95a02a2110942144c101ae594db81cf7faa
Alon Mishne <amishne@users.noreply.github.com>
2026-01-21 18:43:54 +0000

    223ff74 build: don't substitute binary files

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0+sha-b9c995e
+ * @license Angular v21.1.0+sha-223ff74
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-b9c995e');
+const VERSION = /* @__PURE__ */new Version('21.1.0+sha-223ff74');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
