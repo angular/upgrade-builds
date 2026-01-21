0f213aaed0c2ead1888d4df608cf63fce21103cd
Alon Mishne <amishne@users.noreply.github.com>
2026-01-21 00:37:08 +0000

    62d8473 refactor(common): cleanup unused code in doc regions

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.0+sha-3c8d697
+ * @license Angular v21.1.0+sha-62d8473
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-3c8d697');
+const VERSION = /* @__PURE__ */new Version('21.1.0+sha-62d8473');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
