d52b6ea9c169dbb3330775e169854ccf6f3dd978
Andrew Scott <atscott01@gmail.com>
2026-01-22 23:36:59 +0000

    d1135f5 build: update dependency aspect_rules_js to v2.9.2

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-30b50e7
+ * @license Angular v21.2.0-next.0+sha-d1135f5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-30b50e7');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-d1135f5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
