bbdc48c0cf3151a258652dccc0d03327d576b744
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-22 00:49:00 +0000

    bfc2afd release: cut the v21.2.0-next.0 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-e35eea6
+ * @license Angular v21.2.0-next.0+sha-bfc2afd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-e35eea6');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-bfc2afd');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
