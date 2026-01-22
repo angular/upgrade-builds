9bdba7521ed65c8421f7ac950c23482dfbd1e5eb
GitHub <noreply@github.com>
2026-01-22 18:38:27 +0000

    27eeff9 release: bump VSCode extension version to 21.1.1 (#66714)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.1+sha-f577f8c
+ * @license Angular v21.1.1+sha-27eeff9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.1+sha-f577f8c');
+const VERSION = /* @__PURE__ */new Version('21.1.1+sha-27eeff9');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
