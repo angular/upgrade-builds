5bdc9ba2b4ab8947787a785de4238150178ba605
Andrew Scott <atscott01@gmail.com>
2026-01-22 23:06:03 +0000

    30b50e7 docs(docs-infra): Exempts more symbols from automatic linking

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-ebae211
+ * @license Angular v21.2.0-next.0+sha-30b50e7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-ebae211');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-30b50e7');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
