4512a4f034b00c063e2a94a71e171dac9473931e
GitHub <noreply@github.com>
2026-01-22 18:50:14 +0000

    405fa59 docs: release notes for the vscode extension 21.1.1 release (#66715)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-bfc2afd
+ * @license Angular v21.2.0-next.0+sha-405fa59
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-bfc2afd');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-405fa59');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
