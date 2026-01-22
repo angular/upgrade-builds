6d034308bf0b0cb3fe6600a2a5edc58734fb1a57
Andrew Scott <atscott01@gmail.com>
2026-01-22 21:11:24 +0000

    dbc1452 refactor(compiler-cli): remove deprecated signature usage

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-405fa59
+ * @license Angular v21.2.0-next.0+sha-dbc1452
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-405fa59');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-dbc1452');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
