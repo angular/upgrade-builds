c2038d56cd1c06f678ef6a09e883eed54a1a2b09
Andrew Scott <atscott01@gmail.com>
2026-01-22 21:19:10 +0000

    e4c9ff7 build: Update prettier to 3.8

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.1+sha-c9043fb
+ * @license Angular v21.1.1+sha-e4c9ff7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.1+sha-c9043fb');
+const VERSION = /* @__PURE__ */new Version('21.1.1+sha-e4c9ff7');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
