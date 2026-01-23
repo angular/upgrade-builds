b9fa4ccc427f989d1abc154a9edfea9b7d35cd44
Andrew Scott <atscott01@gmail.com>
2026-01-23 20:15:11 +0000

    8bbe6dc feat(common): Add Location strategies to manage trailing slash on write

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-6fb39d9
+ * @license Angular v21.2.0-next.0+sha-8bbe6dc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-6fb39d9');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-8bbe6dc');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
