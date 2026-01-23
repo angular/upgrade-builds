5b6fa2b3cac6cd90a16c17815bc167730ca9d484
Andrew Scott <atscott01@gmail.com>
2026-01-23 01:07:47 +0000

    ddd3198 docs: update signal form tutorials to use `FormField`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-d1135f5
+ * @license Angular v21.2.0-next.0+sha-ddd3198
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-d1135f5');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-ddd3198');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
