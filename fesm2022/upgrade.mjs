9bea4d160a243e239e709ecbbd2b4f5b5e276cff
Andrew Scott <atscott01@gmail.com>
2026-01-23 19:51:34 +0000

    85122cb docs: update `bootstrapApplication` docs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-ddd3198
+ * @license Angular v21.2.0-next.0+sha-85122cb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-ddd3198');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-85122cb');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
