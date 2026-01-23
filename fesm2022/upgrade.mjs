4c0ac50d1757f0377a00fc5e03b179e6f2bf03bc
Andrew Scott <atscott01@gmail.com>
2026-01-23 19:59:02 +0000

    6fb39d9 feat(language-server): Support client-side file watching via `onDidChangeWatchedFiles`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-85122cb
+ * @license Angular v21.2.0-next.0+sha-6fb39d9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-85122cb');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-6fb39d9');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
