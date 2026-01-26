a201db6b01a62ce716ebaf944297dd84bd0d344c
GitHub <noreply@github.com>
2026-01-26 17:11:34 +0000

    d306737 docs: add resource composition with snapshots section

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-8bbe6dc
+ * @license Angular v21.2.0-next.0+sha-d306737
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-8bbe6dc');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-d306737');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
