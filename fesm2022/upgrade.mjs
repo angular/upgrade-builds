cc72c57c8c02bab5a99f842dee4a381d7b80bd34
Andrew Scott <atscott01@gmail.com>
2026-01-26 23:25:25 +0000

    06952b4 docs(docs-infra): remove unused examples

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.1+sha-c133a2c
+ * @license Angular v21.1.1+sha-06952b4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.1+sha-c133a2c');
+const VERSION = /* @__PURE__ */new Version('21.1.1+sha-06952b4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
