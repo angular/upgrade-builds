6fa2cf0aa594c2356e7d5618c9d1e8c1bcd0459f
Andrew Scott <atscott01@gmail.com>
2026-01-26 22:51:22 +0000

    c921786 docs: Updates form documentation (#66732)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-8efee1c
+ * @license Angular v21.2.0-next.0+sha-c921786
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-8efee1c');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-c921786');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
