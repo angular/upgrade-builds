62059b78f6306647030be9675c0c9596c72f60ce
Andrew Scott <atscott01@gmail.com>
2026-01-26 22:40:26 +0000

    b4227fb docs: apply prefer/avoid blocks in UI with comments

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.1+sha-21ecdc0
+ * @license Angular v21.1.1+sha-b4227fb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.1+sha-21ecdc0');
+const VERSION = /* @__PURE__ */new Version('21.1.1+sha-b4227fb');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
