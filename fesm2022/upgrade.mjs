b92fe0526d23a92ca6843dcc8064a1b4c4db0ff3
Andrew Scott <atscott01@gmail.com>
2026-01-27 00:02:18 +0000

    160a8b4 docs: fix wrong line highlights

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-b51bab5
+ * @license Angular v21.2.0-next.0+sha-160a8b4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-b51bab5');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-160a8b4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
