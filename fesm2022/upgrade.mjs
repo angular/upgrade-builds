12b6ca5d6a3619ddfb5735c16fff0c644fa4c839
GitHub <noreply@github.com>
2026-01-28 00:14:06 +0000

    4448356 test(common): enables zoneless change detection in tests

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-960de62
+ * @license Angular v21.2.0-next.0+sha-4448356
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-960de62');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-4448356');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
