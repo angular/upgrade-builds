3ea646e75d9837b4f2673003f8c7bfb263db446a
Andrew Scott <atscott01@gmail.com>
2026-01-28 20:44:33 +0000

    571747d docs: release notes for the v21.1.2 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-7bb241f
+ * @license Angular v21.2.0-next.0+sha-571747d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-7bb241f');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-571747d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
