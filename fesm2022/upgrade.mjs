6bd2d7f1e77a27a2c1d23346dbc82c92b416ddc8
Andrew Scott <atscott01@gmail.com>
2026-01-28 18:03:20 +0000

    bea4cb7 build: update dependency @actions/github to v8

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.0+sha-4123ebf
+ * @license Angular v21.2.0-next.0+sha-bea4cb7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-4123ebf');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-bea4cb7');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
