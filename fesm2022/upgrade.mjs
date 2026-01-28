64469e748cd78d07875e0b8c1add7fa8bed622be
Andrew Scott <atscott01@gmail.com>
2026-01-28 00:21:37 +0000

    31e6f0d test(forms): submit behavior while validation is pending

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.1+sha-6601f06
+ * @license Angular v21.1.1+sha-31e6f0d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.1+sha-6601f06');
+const VERSION = /* @__PURE__ */new Version('21.1.1+sha-31e6f0d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
