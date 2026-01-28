2a62c63f6ea140726ef1738c6a1b20363aacd986
Andrew Scott <atscott01@gmail.com>
2026-01-28 21:02:27 +0000

    cfe7340 refactor(core): remove unused restriction parameter

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.2+sha-4f0d29d
+ * @license Angular v21.1.2+sha-cfe7340
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.2+sha-4f0d29d');
+const VERSION = /* @__PURE__ */new Version('21.1.2+sha-cfe7340');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
