a8b953503f01c90a52e7d57ec3a95f53d40afdc6
Andrew Scott <atscott01@gmail.com>
2026-01-28 19:05:01 +0000

    9f99b14 fix(forms): only touch visible, interactive fields on submit

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.1+sha-3de87e1
+ * @license Angular v21.1.1+sha-9f99b14
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.1+sha-3de87e1');
+const VERSION = /* @__PURE__ */new Version('21.1.1+sha-9f99b14');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
