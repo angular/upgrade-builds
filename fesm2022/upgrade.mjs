e018910ef95fdeebcfe474eb874c5d688f53d8e1
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-29 20:37:22 +0000

    8dfbb8b build: update cross-repo angular dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.2+sha-0a4c647
+ * @license Angular v21.1.2+sha-8dfbb8b-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.2+sha-0a4c647');
+const VERSION = /* @__PURE__ */new Version('21.1.2+sha-8dfbb8b-with-local-changes');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
