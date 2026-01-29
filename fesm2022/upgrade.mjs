c8d1ba00ee23455ac01838698c0a6b644976d215
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-29 21:27:21 +0000

    a67e007 refactor(forms): move control logic into `FormField` directive

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.1+sha-6990f88
+ * @license Angular v21.2.0-next.1+sha-a67e007
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-6990f88');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-a67e007');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
