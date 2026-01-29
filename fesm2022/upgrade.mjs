dc3b14708ff557cf1d8935aa35ef3da21da39b63
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-29 20:13:41 +0000

    0a4c647 refactor(core): Remove unsued properties in differ factories

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.2+sha-cfe7340
+ * @license Angular v21.1.2+sha-0a4c647
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.2+sha-cfe7340');
+const VERSION = /* @__PURE__ */new Version('21.1.2+sha-0a4c647');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
