3d0bebac827d3460bb990e5678703359e37836ba
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-29 21:27:31 +0000

    efb64bf build(language-service): remove local development build script

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.2+sha-8dfbb8b-with-local-changes
+ * @license Angular v21.1.2+sha-efb64bf-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.2+sha-8dfbb8b-with-local-changes');
+const VERSION = /* @__PURE__ */new Version('21.1.2+sha-efb64bf-with-local-changes');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
