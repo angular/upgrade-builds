9d0e632dac4d264c7c778777e317947f91544ca9
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-29 20:32:59 +0000

    6990f88 feat(docs-infra): increase table-of-contents width for large screens

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.1+sha-0e04233
+ * @license Angular v21.2.0-next.1+sha-6990f88
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-0e04233');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-6990f88');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
