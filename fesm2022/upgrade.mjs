f905b9b6cdc9fc8a0b276ecc83c4abc07a4a4475
GitHub <noreply@github.com>
2026-01-30 17:19:33 +0000

    26d1215 refactor(forms): convert `FieldState.controlValue` to a `WritableSignal`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.1+sha-af76b9d
+ * @license Angular v21.2.0-next.1+sha-26d1215
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-af76b9d');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-26d1215');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
