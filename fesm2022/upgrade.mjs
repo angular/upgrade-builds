a7e2e982b14c4d92fd36abc1a469e2e25f1324c2
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-30 17:09:34 +0000

    af76b9d build: update dependency @actions/github to v9

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.1+sha-b327d72
+ * @license Angular v21.2.0-next.1+sha-af76b9d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-b327d72');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-af76b9d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
