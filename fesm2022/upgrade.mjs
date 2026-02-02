9f7c74a278cce7deb983c374552926ed07dab337
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-02 18:24:01 +0000

    575f8a3 docs(animations): fix small typo in animations migrations docs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.2+sha-eb28032
+ * @license Angular v21.1.2+sha-575f8a3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.2+sha-eb28032');
+const VERSION = /* @__PURE__ */new Version('21.1.2+sha-575f8a3');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
