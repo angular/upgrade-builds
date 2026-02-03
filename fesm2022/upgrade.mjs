ec815f6a2abed764404e9fc488114a41f9ea2c3b
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-03 20:40:49 +0000

    f15d33d docs(docs-infra): extract call signature return type.

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.2+sha-6505a26
+ * @license Angular v21.1.2+sha-f15d33d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.2+sha-6505a26');
+const VERSION = /* @__PURE__ */new Version('21.1.2+sha-f15d33d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
