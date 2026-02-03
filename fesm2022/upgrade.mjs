90322d34399c44628dfa6709d672611ee2004c4f
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-03 20:50:36 +0000

    cab5ddd fix(core): hold constructors weakly in DepsTracker cache

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.1+sha-43d61ec
+ * @license Angular v21.2.0-next.1+sha-cab5ddd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-43d61ec');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-cab5ddd');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
