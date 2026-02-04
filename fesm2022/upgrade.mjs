767182bef69c4dba2af87805d1b4b62fdc6daff4
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-04 23:50:53 +0000

    8c21866 feat(language-service): add linked editing ranges for HTML tag synchronization

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.1+sha-da1f61c
+ * @license Angular v21.2.0-next.1+sha-8c21866
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-da1f61c');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-8c21866');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
