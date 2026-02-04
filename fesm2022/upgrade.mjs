20dd2a0fd82d4df6f7df0f27997b07ac01606710
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-04 23:50:32 +0000

    767c6b4 fix(vscode-extension): support highlighting for class bindings with brackets

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.2+sha-1bf6bfc
+ * @license Angular v21.1.2+sha-767c6b4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.2+sha-1bf6bfc');
+const VERSION = /* @__PURE__ */new Version('21.1.2+sha-767c6b4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
