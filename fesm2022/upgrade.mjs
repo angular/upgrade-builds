d684e7c671092bcf0c5a25995f6943e01e1f5321
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-04 22:31:36 +0000

    04d518b refactor(forms): work around internal issue

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.2+sha-2cf4da0
+ * @license Angular v21.1.2+sha-04d518b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.2+sha-2cf4da0');
+const VERSION = /* @__PURE__ */new Version('21.1.2+sha-04d518b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
