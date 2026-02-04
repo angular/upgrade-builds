c3823b48337c4644bf84e88cd7179c5df6f3f5bd
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-04 23:43:22 +0000

    1bf6bfc test(forms): remove zone-based testing utilities

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.2+sha-04d518b
+ * @license Angular v21.1.2+sha-1bf6bfc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.2+sha-04d518b');
+const VERSION = /* @__PURE__ */new Version('21.1.2+sha-1bf6bfc');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
