a58fe21d635a1d44a9c602b93a27ee82068ffc15
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-05 00:20:22 +0000

    c3229b3 release: cut the v21.2.0-next.2 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.1+sha-8fe34e1
+ * @license Angular v21.2.0-next.2+sha-c3229b3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-8fe34e1');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-c3229b3');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
