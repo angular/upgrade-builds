9ba21dcf63727788ba0d97e58f8a21e4c6dd1dea
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-05 00:12:33 +0000

    8fe34e1 docs: release notes for the v21.1.3 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.1+sha-8c21866
+ * @license Angular v21.2.0-next.1+sha-8fe34e1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-8c21866');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-8fe34e1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
