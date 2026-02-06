9ad0bd3cd0c54d42b9947f0eb62e0742dc46c818
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-06 18:25:51 +0000

    460dbbb docs: remove non-existant method

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.2+sha-15c71fb
+ * @license Angular v21.2.0-next.2+sha-460dbbb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-15c71fb');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-460dbbb');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
