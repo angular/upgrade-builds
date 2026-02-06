6dc9fca903df4406b836d0aa1e64bced109df24d
Andrew Kushnir <akushnir@google.com>
2026-02-06 01:03:31 +0000

    0729181 test(compiler): remove zone-based testing utilities

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.3+sha-70668be
+ * @license Angular v21.1.3+sha-0729181
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.3+sha-70668be');
+const VERSION = /* @__PURE__ */new Version('21.1.3+sha-0729181');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
