84a5d1923c427c6f57f7cb2f9bb8bbd5e4b085c9
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-06 15:48:10 +0000

    24c0c5a feat(forms): support signal-based schemas in validateStandardSchema

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.2+sha-6fa2270
+ * @license Angular v21.2.0-next.2+sha-24c0c5a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-6fa2270');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-24c0c5a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
