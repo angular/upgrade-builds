3f0cf2d575798dc35888db9dc586701c6100e12f
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-06 01:22:35 +0000

    be4ffb6 build: update rules_browsers digest to e08ae33

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.3+sha-0729181
+ * @license Angular v21.1.3+sha-be4ffb6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.3+sha-0729181');
+const VERSION = /* @__PURE__ */new Version('21.1.3+sha-be4ffb6');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
