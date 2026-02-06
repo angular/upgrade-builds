54a1efc1a407019bf99ce1007852fc34599d5528
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-06 17:54:38 +0000

    15c71fb build: update all non-major dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.2+sha-24c0c5a
+ * @license Angular v21.2.0-next.2+sha-15c71fb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-24c0c5a');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-15c71fb');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
