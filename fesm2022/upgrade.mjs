ecd5a78ea665017219eeb399b426aba0f1e27da0
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-06 01:01:46 +0000

    2a0241a test(compiler): remove zone-based testing utilities

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.2+sha-c3229b3
+ * @license Angular v21.2.0-next.2+sha-2a0241a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-c3229b3');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-2a0241a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
