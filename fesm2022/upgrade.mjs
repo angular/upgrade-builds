ade7b3f633435845864dd3e74856b2cfe4532aea
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-06 19:19:56 +0000

    c633697 Revert "fix(compiler-cli): update diagnostic to flag no-op arrow functions in listeners"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.3+sha-be4ffb6
+ * @license Angular v21.1.3+sha-c633697
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.3+sha-be4ffb6');
+const VERSION = /* @__PURE__ */new Version('21.1.3+sha-c633697');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
