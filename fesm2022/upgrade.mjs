ba87e3be8b0ae8ab701c41693b2e937a0890853a
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-09 20:41:24 +0000

    cb1163e fix(http): correctly parse ArrayBuffer and Blob in transfer cache

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.2+sha-4b3b149
+ * @license Angular v21.2.0-next.2+sha-cb1163e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-4b3b149');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-cb1163e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
