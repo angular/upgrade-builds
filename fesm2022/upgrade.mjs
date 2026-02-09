8a4dd7a436ddf07fdb03c4f28fc44ee1ee9b7643
Andrew Kushnir <akushnir@google.com>
2026-02-09 22:53:48 +0000

    19542a3 test(common): remove zone-based testing utilities Removes usages of zone-based helpers such as `fakeAsync` , `tick` `waitForAsync` as part of the migration to zoneless tests.

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.3+sha-d6aeac5
+ * @license Angular v21.1.3+sha-19542a3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.3+sha-d6aeac5');
+const VERSION = /* @__PURE__ */new Version('21.1.3+sha-19542a3');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
