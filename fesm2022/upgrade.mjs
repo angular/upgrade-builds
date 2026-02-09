d5c1f0bb8e77923322fdb00607cae9ab83d8b98c
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-09 22:21:53 +0000

    e53c8ab fix(core): Fix flakey test due to document injection

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.2+sha-cb1163e
+ * @license Angular v21.2.0-next.2+sha-e53c8ab
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-cb1163e');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-e53c8ab');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
