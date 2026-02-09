3a05334080168f7255d6764c5a15bd9b2f3e939b
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-09 22:56:37 +0000

    51cc914 feat(common): support height in ImageLoaderConfig and built-in loaders

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.2+sha-e53c8ab
+ * @license Angular v21.2.0-next.2+sha-51cc914
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-e53c8ab');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-51cc914');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
