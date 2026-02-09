5365cca97273b9ab80a1cb70523a499cf098bd79
Andrew Kushnir <akushnir@google.com>
2026-02-09 20:42:01 +0000

    3905015 fix(http): correctly parse ArrayBuffer and Blob in transfer cache

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.3+sha-c298694
+ * @license Angular v21.1.3+sha-3905015
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.3+sha-c298694');
+const VERSION = /* @__PURE__ */new Version('21.1.3+sha-3905015');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
