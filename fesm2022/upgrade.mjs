98f33dd5ad5ee4bd9d559aa0caca294095d2627b
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-10 00:12:48 +0000

    760910e Revert "refactor(service-worker): remove zone-based testing utilities"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.2+sha-51cc914
+ * @license Angular v21.2.0-next.2+sha-760910e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-51cc914');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-760910e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
