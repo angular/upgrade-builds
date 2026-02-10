c551a33853a8ae50ad2d1175a580a9da262334e2
Andrew Kushnir <akushnir@google.com>
2026-02-10 21:41:58 +0000

    2b99eaa fix(core): capture animation dependencies eagerly to avoid destroyed injector

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.3+sha-083f0fa
+ * @license Angular v21.1.3+sha-2b99eaa
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.3+sha-083f0fa');
+const VERSION = /* @__PURE__ */new Version('21.1.3+sha-2b99eaa');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
