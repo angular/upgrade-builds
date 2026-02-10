ee1d2cad4beffcd119516460545518ae9e493c62
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-10 00:03:46 +0000

    c6bbf40 docs(forms): replace 'legacy' with 'existing' in migration guide and navigation

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.3+sha-19542a3
+ * @license Angular v21.1.3+sha-c6bbf40
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.3+sha-19542a3');
+const VERSION = /* @__PURE__ */new Version('21.1.3+sha-c6bbf40');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
