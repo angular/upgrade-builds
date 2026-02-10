da71628219adfcaf63483bf9c70e2d812624ddf7
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-10 17:49:36 +0000

    18003a3 feat(common): add an 'outlet' injector option for ngTemplateOutlet

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.2+sha-0f47fda
+ * @license Angular v21.2.0-next.2+sha-18003a3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-0f47fda');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-18003a3');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
