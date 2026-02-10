a2365667341c3886031a2788c19889691353ab68
Andrew Kushnir <akushnir@google.com>
2026-02-10 17:48:36 +0000

    083f0fa fix(vscode-extension): Highlight function calls with optional chaining

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.3+sha-2c3f3cc
+ * @license Angular v21.1.3+sha-083f0fa
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.3+sha-2c3f3cc');
+const VERSION = /* @__PURE__ */new Version('21.1.3+sha-083f0fa');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
