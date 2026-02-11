fc368e296654620e7d62617a30a3504b489d7d55
GitHub <noreply@github.com>
2026-02-11 22:53:09 +0000

    d75046b fix(forms): warn when showing hidden field state

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.3+sha-f67818a
+ * @license Angular v21.2.0-next.3+sha-d75046b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-f67818a');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-d75046b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
