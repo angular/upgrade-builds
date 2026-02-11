e28f8941d341867e5c6b1878bed842f559964325
Matthew Beck <me@mattrb.com>
2026-02-11 23:29:06 +0000

    4ef0f1f build: add workspace rule for antigravity

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.4+sha-7150df2
+ * @license Angular v21.1.4+sha-4ef0f1f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-7150df2');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-4ef0f1f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
