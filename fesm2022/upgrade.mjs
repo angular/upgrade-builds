daaccdd8bf480ca36c6cf3ab73c02e8441570f2a
GitHub <noreply@github.com>
2026-02-11 19:52:10 +0000

    3606902 refactor(forms): relax `[formField]` input type from `FieldTree` to `Field`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.3+sha-39cff9c
+ * @license Angular v21.2.0-next.3+sha-3606902
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-39cff9c');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-3606902');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
