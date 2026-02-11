dd31f39d4a5c5e40bdf193474991ddc8bf9c2b83
Matthew Beck (Berry) <me@mattrb.com>
2026-02-11 22:50:09 +0000

    7150df2 build: update cross-repo angular dependencies to v21.1.4

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.4+sha-6060aad
+ * @license Angular v21.1.4+sha-7150df2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-6060aad');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-7150df2');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
