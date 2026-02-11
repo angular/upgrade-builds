1cbdd7be005699618b3cbb6bd4c1af22f1bcef2c
Matthew Beck (Berry) <me@mattrb.com>
2026-02-11 23:44:47 +0000

    937e98f build: update pnpm to v10.29.2

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.4+sha-4ef0f1f
+ * @license Angular v21.1.4+sha-937e98f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-4ef0f1f');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-937e98f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
