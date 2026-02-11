70edd070b6c16147ffd3022daff1b3f5ed95add1
Matthew Beck <me@mattrb.com>
2026-02-11 22:08:37 +0000

    aa03843 ci: update pullapprove reviewer list

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.4+sha-8868130
+ * @license Angular v21.1.4+sha-aa03843
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-8868130');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-aa03843');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
