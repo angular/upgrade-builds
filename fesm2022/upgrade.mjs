1a087bbd245f37455c3f233de0fc6d6d229f45b8
Matthew Beck (Berry) <me@mattrb.com>
2026-02-12 18:24:32 +0000

    5b59dbd build: lock file maintenance

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.3+sha-06d94ac
+ * @license Angular v21.2.0-next.3+sha-5b59dbd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-06d94ac');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-5b59dbd');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
