eb9b4bba0383ebeecf457cf3dc1f380fe368525f
Matthew Beck (Berry) <me@mattrb.com>
2026-02-12 17:46:03 +0000

    65a8df1 build: update bazel dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.4+sha-937e98f
+ * @license Angular v21.1.4+sha-65a8df1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-937e98f');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-65a8df1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
