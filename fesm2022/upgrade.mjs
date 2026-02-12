545252279c88f202160c73f51901c5bca4e3c731
Matthew Beck (Berry) <me@mattrb.com>
2026-02-12 17:00:45 +0000

    b386f95 Revert "refactor(compiler): Add info about unclosed element."

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.3+sha-6245333
+ * @license Angular v21.2.0-next.3+sha-b386f95
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-6245333');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-b386f95');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
