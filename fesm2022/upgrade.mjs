24cce211db7b51ba94b2e0ce7127ba3b7bb377b0
Matthew Beck (Berry) <me@mattrb.com>
2026-02-13 01:34:24 +0000

    d5b35b1 refactor(core): place Eager value first

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.3+sha-ddfc833
+ * @license Angular v21.2.0-next.3+sha-d5b35b1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-ddfc833');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-d5b35b1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
