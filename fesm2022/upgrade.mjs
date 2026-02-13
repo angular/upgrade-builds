e61a1699fa291574fabd2bfe2809a5fa29f4529e
Matthew Beck (Berry) <me@mattrb.com>
2026-02-13 17:50:14 +0000

    e10a634 refactor(compiler-cli): use phaseModifier for type-only import detection

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.3+sha-dfd5e3a
+ * @license Angular v21.2.0-next.3+sha-e10a634
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-dfd5e3a');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-e10a634');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
