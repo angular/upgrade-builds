214fb60a7d55c4e4a323607655aad0e6dfb80393
Matthew Beck (Berry) <me@mattrb.com>
2026-02-13 23:47:27 +0000

    a37b6c5 docs: fix formatting in pipes.md example

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.3+sha-a6a0347
+ * @license Angular v21.2.0-next.3+sha-a37b6c5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-a6a0347');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-a37b6c5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
