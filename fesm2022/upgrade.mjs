ae207940cdbed3935301822d0c3f63d8aceb88de
Matthew Beck (Berry) <me@mattrb.com>
2026-02-13 00:59:52 +0000

    ddfc833 refactor(common): improve image directive typings

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.3+sha-5b59dbd
+ * @license Angular v21.2.0-next.3+sha-ddfc833
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-5b59dbd');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-ddfc833');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
