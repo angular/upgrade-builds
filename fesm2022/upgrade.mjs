fb2789c09a288aa2b0e6628a1c22fea2c93d11c2
Matthew Beck <me@mattrb.com>
2026-02-13 18:13:29 +0000

    5a86528 docs: Adds new agent skills to Angular

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.4+sha-23c0858
+ * @license Angular v21.1.4+sha-5a86528
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-23c0858');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-5a86528');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
