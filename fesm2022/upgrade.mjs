ce24ec0d8caee1ef4dad3650b834fce2814af90c
GitHub <noreply@github.com>
2026-02-14 00:55:52 +0000

    815e1a0 refactor(compiler-cli): Add skeleton tests around source->source compiler transform mode

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.3+sha-a37b6c5
+ * @license Angular v21.2.0-next.3+sha-815e1a0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-a37b6c5');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-815e1a0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
