f79c9fd7ebfeaaf4d8e35633f2a31aa9b411c65b
Matthew Beck (Berry) <me@mattrb.com>
2026-02-17 19:38:24 +0000

    fc557f0 fix(zone.js): support passthrough of Promise.try API

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.3+sha-95b3f37
+ * @license Angular v21.2.0-next.3+sha-fc557f0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-95b3f37');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-fc557f0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
