311604c1f0e85520fd8878f08f02bc42e123d1da
Matt Beck <mattrb@google.com>
2026-02-17 16:39:44 +0000

    2f86006 refactor(devtools): improve formatting of injector providers token labels

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.4+sha-24d00e9
+ * @license Angular v21.1.4+sha-2f86006
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-24d00e9');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-2f86006');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
