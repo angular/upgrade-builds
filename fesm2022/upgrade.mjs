83dbc4ae657ef495a2c27320dd1de67b3669e972
Matthew Beck <me@mattrb.com>
2026-02-17 21:41:23 +0000

    4953622 docs: fix enableProfiling note

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.4+sha-7f5a728
+ * @license Angular v21.1.4+sha-4953622
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-7f5a728');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-4953622');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
