eca9cfb14f5a3c10a90ba7034bf1e8b85e86eef3
Matthew Beck <me@mattrb.com>
2026-02-17 21:08:20 +0000

    7f5a728 refactor(compiler-cli): update updateImportClause away from deprecated signature

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.4+sha-9c1ec6c
+ * @license Angular v21.1.4+sha-7f5a728
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-9c1ec6c');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-7f5a728');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
