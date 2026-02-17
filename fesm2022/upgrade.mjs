ec47bda97fddf98957faf14dcf4a51d73581fc63
Matthew Beck (Berry) <me@mattrb.com>
2026-02-17 22:41:42 +0000

    8de0d25 docs(docs-infra): Signal forms docs Update docs signal forms errors input wrong typing

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.3+sha-064749e
+ * @license Angular v21.2.0-next.3+sha-8de0d25
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-064749e');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-8de0d25');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
