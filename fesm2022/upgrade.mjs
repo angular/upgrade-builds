2168e0f5eb61799625cdadffe057063af747625b
GitHub <noreply@github.com>
2026-02-18 23:20:38 +0000

    2eb6b0d build: update vscode version on main to target v22 (#67133)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.3.0-next.0+sha-2d776f6
+ * @license Angular v21.3.0-next.0+sha-2eb6b0d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.3.0-next.0+sha-2d776f6');
+const VERSION = /* @__PURE__ */new Version('21.3.0-next.0+sha-2eb6b0d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
