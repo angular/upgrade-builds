03fba8c9611189ec70e4b8dfb4fe9e3c9bece154
GitHub <noreply@github.com>
2026-02-18 19:45:12 +0000

    9a9f4e7 docs: update best practice performances nav structure to be more comprehensive

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.3+sha-b8811b4
+ * @license Angular v21.2.0-next.3+sha-9a9f4e7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-b8811b4');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-9a9f4e7');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
