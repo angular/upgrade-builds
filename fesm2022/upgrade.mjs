2ed1dd1e97b28b584ae196d89a7dceced86904c4
Matthew Beck (Berry) <me@mattrb.com>
2026-02-18 15:22:38 +0000

    680e1c0 docs: fix custom control metadata example in signal forms guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.3+sha-8de0d25
+ * @license Angular v21.2.0-next.3+sha-680e1c0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-8de0d25');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-680e1c0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
