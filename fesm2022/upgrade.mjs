ecf7e64a69110fe9d2b036bd7b05dd0cb6fae25c
GitHub <noreply@github.com>
2026-02-18 19:11:21 +0000

    b8811b4 docs: add signal forms async operations guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.3+sha-47486f8
+ * @license Angular v21.2.0-next.3+sha-b8811b4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-47486f8');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-b8811b4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
