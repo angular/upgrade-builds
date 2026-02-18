1d4fc251dbc1d7407ce1e87f05b2e9410f3ea12f
Matthew Beck (Berry) <me@mattrb.com>
2026-02-18 17:34:56 +0000

    c36385c refactor(docs-infra): Add writing guide skill

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-next.3+sha-22a2090
+ * @license Angular v21.2.0-next.3+sha-c36385c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-22a2090');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-c36385c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
