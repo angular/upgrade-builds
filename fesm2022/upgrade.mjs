01ffb42b9e4a20ddddcbaef0220dba155c7dde9d
Matthew Beck <me@mattrb.com>
2026-02-18 19:46:24 +0000

    b90a0fc docs: update best practice performances nav structure to be more comprehensive

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.4+sha-a4eb347
+ * @license Angular v21.1.4+sha-b90a0fc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-a4eb347');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-b90a0fc');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
