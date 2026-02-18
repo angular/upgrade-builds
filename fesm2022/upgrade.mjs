5fb856e51ff3ff78267bc8a3e7df8afb1d8443a7
GitHub <noreply@github.com>
2026-02-18 23:20:34 +0000

    dd630ef build: update version target to prep for 21.2.0 release for vscode extension (#67132)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-13fd2b1
+ * @license Angular v21.2.0-rc.0+sha-dd630ef
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-13fd2b1');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-dd630ef');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
