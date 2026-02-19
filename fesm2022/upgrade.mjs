0aab854e91dc7d40ea31f66ade0d91f52ebc8506
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-19 19:29:46 +0000

    5cea357 docs: update changelog to note no changes in 21.1.5 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-79f1cd4
+ * @license Angular v21.2.0-rc.0+sha-5cea357
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-79f1cd4');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-5cea357');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
