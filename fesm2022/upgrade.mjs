13d16990e41de8dd499777985d989a6490c2d322
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-19 17:17:29 +0000

    a5a1829 docs: improve lazy loading of homepage examples

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.3.0-next.0+sha-a3731a1
+ * @license Angular v21.3.0-next.0+sha-a5a1829
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.3.0-next.0+sha-a3731a1');
+const VERSION = /* @__PURE__ */new Version('21.3.0-next.0+sha-a5a1829');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
