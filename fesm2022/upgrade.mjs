9cf4fdecb8e58220f744f34e5bf38a72470f1086
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-19 17:00:21 +0000

    4ea3caf docs(docs-infra): fixed missing gray color palette definitions for light and dark modes

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-f58f9c8
+ * @license Angular v21.2.0-rc.0+sha-4ea3caf
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-f58f9c8');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-4ea3caf');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
