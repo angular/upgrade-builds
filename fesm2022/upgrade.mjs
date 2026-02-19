fa437ec0b80ab1cdef804897763b0caa5367eb32
GitHub <noreply@github.com>
2026-02-19 17:06:55 +0000

    a3731a1 refactor(devtools): rename directive-explorer component names and folder structure

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.3.0-next.0+sha-1771d54
+ * @license Angular v21.3.0-next.0+sha-a3731a1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.3.0-next.0+sha-1771d54');
+const VERSION = /* @__PURE__ */new Version('21.3.0-next.0+sha-a3731a1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
