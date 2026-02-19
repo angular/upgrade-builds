8ca302086e5a235cfcef4fdaa3d200ec0cf58d96
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-19 21:36:13 +0000

    5729456 build: update cross-repo angular dependencies

--- fesm2022/static-testing.mjs
+++ fesm2022/static-testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.3.0-next.0+sha-d501506
+ * @license Angular v21.3.0-next.0+sha-5729456
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -20,7 +20,7 @@ class AngularTestingModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.3.0-next.0+sha-d501506",
+    version: "21.3.0-next.0+sha-5729456",
     ngImport: i0,
     type: AngularTestingModule,
     deps: [{
@@ -30,13 +30,13 @@ class AngularTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.3.0-next.0+sha-d501506",
+    version: "21.3.0-next.0+sha-5729456",
     ngImport: i0,
     type: AngularTestingModule
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.3.0-next.0+sha-d501506",
+    version: "21.3.0-next.0+sha-5729456",
     ngImport: i0,
     type: AngularTestingModule,
     providers: [{
@@ -47,7 +47,7 @@ class AngularTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.3.0-next.0+sha-d501506",
+  version: "21.3.0-next.0+sha-5729456",
   ngImport: i0,
   type: AngularTestingModule,
   decorators: [{
