da408cecd7ef4a22bd1c3d98412692d721ad1682
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-19 20:46:51 +0000

    eb7c593 ci: add `zone.js` to `minimumReleaseAgeExclude`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-5cea357
+ * @license Angular v21.2.0-rc.0+sha-eb7c593
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-5cea357');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-eb7c593');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
