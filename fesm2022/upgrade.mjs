40f037ba884f902436f747c0b5683b231d6890e8
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-19 23:55:15 +0000

    bc473cf docs(docs-infra): preserve content before docs-card-container in adev

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.3.0-next.0+sha-5729456
+ * @license Angular v21.3.0-next.0+sha-bc473cf
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.3.0-next.0+sha-5729456');
+const VERSION = /* @__PURE__ */new Version('21.3.0-next.0+sha-bc473cf');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
