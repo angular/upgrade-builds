54906329d085be806eb783490ad106d72a647418
Andrew Scott <atscott@google.com>
2026-02-19 00:31:51 +0000

    f58f9c8 release: cut the zone.js-0.16.1 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-7f33cbe
+ * @license Angular v21.2.0-rc.0+sha-f58f9c8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-7f33cbe');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-f58f9c8');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
