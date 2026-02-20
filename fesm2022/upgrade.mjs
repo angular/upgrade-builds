5449d6d6f506d7161301d2c162feef9f1b88d8cc
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 21:36:04 +0000

    35f0663 refactor(core): guards stringify calls with ngDevMode

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-8737289
+ * @license Angular v21.2.0-rc.0+sha-35f0663
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-8737289');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-35f0663');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
