4d42f84436cb8153e75ba4f9ea47e0c1d9c47432
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 17:23:00 +0000

    3c4deaa refactor(common): log a warning when a JsonPipe receives a signal

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-c0cb604
+ * @license Angular v21.2.0-rc.0+sha-3c4deaa
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-c0cb604');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-3c4deaa');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
