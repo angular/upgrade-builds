114fec1d17cb66a462729724e205c8ebd7ecfa1e
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 18:34:40 +0000

    631f60d fix(forms): preserve parse errors when parse returns value

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-390efd5
+ * @license Angular v21.2.0-rc.0+sha-631f60d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-390efd5');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-631f60d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
