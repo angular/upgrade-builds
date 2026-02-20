aaec5d39b5a47c83d3da2ea2a93461a4486da4d3
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 21:33:59 +0000

    31d3d56 fix(common): fix LCP image detection with duplicate URLs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.5+sha-0f4ba4a
+ * @license Angular v21.1.5+sha-31d3d56
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.5+sha-0f4ba4a');
+const VERSION = /* @__PURE__ */new Version('21.1.5+sha-31d3d56');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
