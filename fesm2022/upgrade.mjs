d34e4fbf670555559de2ad5fcc9d5c268f9676d6
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 00:11:55 +0000

    9ee99c7 docs: update NgTemplateOutlet documentation with injector

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-5b8a403
+ * @license Angular v21.2.0-rc.0+sha-9ee99c7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-5b8a403');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-9ee99c7');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
