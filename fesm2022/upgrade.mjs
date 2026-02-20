a1bae0bf1dd56ac65f0832aeaaffc903dc49272e
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-20 16:50:04 +0000

    cfdbb7c ci: bump target version to target 22.0.0-next.0

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.3.0-next.0+sha-c672f92
+ * @license Angular v22.0.0-next.0+sha-cfdbb7c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.3.0-next.0+sha-c672f92');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-cfdbb7c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
