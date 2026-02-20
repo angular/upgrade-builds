bdf936ad09737ae64fd09935b3fd09197a33ff8b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-20 17:50:00 +0000

    66b472e refactor(compiler-cli): improve diagnostic with help link

--- fesm2022/static-testing.mjs
+++ fesm2022/static-testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-246a984
+ * @license Angular v22.0.0-next.0+sha-66b472e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -20,7 +20,7 @@ class AngularTestingModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.0+sha-246a984",
+    version: "22.0.0-next.0+sha-66b472e",
     ngImport: i0,
     type: AngularTestingModule,
     deps: [{
@@ -30,13 +30,13 @@ class AngularTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "22.0.0-next.0+sha-246a984",
+    version: "22.0.0-next.0+sha-66b472e",
     ngImport: i0,
     type: AngularTestingModule
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "22.0.0-next.0+sha-246a984",
+    version: "22.0.0-next.0+sha-66b472e",
     ngImport: i0,
     type: AngularTestingModule,
     providers: [{
@@ -47,7 +47,7 @@ class AngularTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.0+sha-246a984",
+  version: "22.0.0-next.0+sha-66b472e",
   ngImport: i0,
   type: AngularTestingModule,
   decorators: [{
