fb4c073c1ae0bb4f66c317346d9455da0d2dd973
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-20 00:05:43 +0000

    f68078b docs: add section on reactive context and async operations

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.3.0-next.0+sha-bc473cf
+ * @license Angular v21.3.0-next.0+sha-f68078b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.3.0-next.0+sha-bc473cf');
+const VERSION = /* @__PURE__ */new Version('21.3.0-next.0+sha-f68078b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
