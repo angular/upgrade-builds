bff86269d3c5b0ef36265436338cd888fb1b323f
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 18:08:30 +0000

    390efd5 docs: add new debugging and troubleshooting di guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-c2cedd1
+ * @license Angular v21.2.0-rc.0+sha-390efd5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-c2cedd1');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-390efd5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
