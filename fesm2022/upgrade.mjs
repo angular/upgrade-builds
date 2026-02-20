b3edac39561160872c761fd37b50607b586f5d93
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 17:12:07 +0000

    24b578c fix(compiler-cli): detect uninvoked functions in defer trigger expressions

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.5+sha-64b1a79
+ * @license Angular v21.1.5+sha-24b578c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.5+sha-64b1a79');
+const VERSION = /* @__PURE__ */new Version('21.1.5+sha-24b578c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
