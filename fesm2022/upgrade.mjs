10d9eada8db34e56c12c40333642e43694bbbc52
GitHub <noreply@github.com>
2026-02-20 21:36:00 +0000

    c89d94b refactor(core): guards stringify calls with ngDevMode

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.0+sha-d7ddebc
+ * @license Angular v22.0.0-next.0+sha-c89d94b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-d7ddebc');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-c89d94b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
