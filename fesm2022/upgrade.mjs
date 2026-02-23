f0efd43d33bce8e2d32b603dff0b025b1f9785ea
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-23 23:22:22 +0000

    30e486a fix(docs-infra): use shared heading ID generation logic

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-a73d543
+ * @license Angular v21.2.0-rc.0+sha-30e486a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-a73d543');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-30e486a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
