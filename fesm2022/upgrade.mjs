3795c2ca46034edcad7482bd380437cf6ed7f7c1
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-23 23:17:02 +0000

    a73d543 docs: Add a compiler entry to the roadmap

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-b9d3fe3
+ * @license Angular v21.2.0-rc.0+sha-a73d543
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-b9d3fe3');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-a73d543');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
