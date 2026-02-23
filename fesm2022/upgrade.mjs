c271663d400543cd29ab37ff2085b0ed81fd1a4b
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-23 21:35:19 +0000

    d3479f1 docs: update and reformat the roadmap

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.5+sha-a8dcb28
+ * @license Angular v21.1.5+sha-d3479f1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.5+sha-a8dcb28');
+const VERSION = /* @__PURE__ */new Version('21.1.5+sha-d3479f1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
