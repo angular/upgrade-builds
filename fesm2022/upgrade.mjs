fb12db10a2dc7e53695d6518ba1f1efce65e0d4f
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-23 17:19:40 +0000

    0596a5b docs: add Route Loading Strategies section and update navigation links

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.5+sha-31d3d56
+ * @license Angular v21.1.5+sha-0596a5b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.5+sha-31d3d56');
+const VERSION = /* @__PURE__ */new Version('21.1.5+sha-0596a5b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
