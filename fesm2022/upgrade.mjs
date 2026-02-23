d7b0720492371eba08c62a9c938fc8f9b5c28e95
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-23 23:35:40 +0000

    eb6bce0 docs: Add image loader config with height parameter

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-30e486a
+ * @license Angular v21.2.0-rc.0+sha-eb6bce0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-30e486a');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-eb6bce0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
