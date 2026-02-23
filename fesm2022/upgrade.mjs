921f010b30f911a102e3eb896955102156b73e70
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-23 18:32:23 +0000

    2f213de refactor(core): remove outdated TODO comments

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-31bf694
+ * @license Angular v21.2.0-rc.0+sha-2f213de
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-31bf694');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-2f213de');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
