13af1466137e7931c4628c1e6c8f136f7121041c
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-24 22:18:32 +0000

    63cb3f4 build: update bazel dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-787cd87
+ * @license Angular v21.2.0-rc.0+sha-63cb3f4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-787cd87');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-63cb3f4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
