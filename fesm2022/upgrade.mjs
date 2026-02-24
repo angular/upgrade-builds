f7bd604e28909c4ebbfea970385f5a972af69226
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-24 17:32:11 +0000

    5c4d9b2 docs: consolidate validation rules for `Host` and `X-Forwarded-Host` headers into a single point.

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.5+sha-4e6ac9c
+ * @license Angular v21.1.5+sha-5c4d9b2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.5+sha-4e6ac9c');
+const VERSION = /* @__PURE__ */new Version('21.1.5+sha-5c4d9b2');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
