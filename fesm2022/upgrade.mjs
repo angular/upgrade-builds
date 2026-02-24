a99dd8812bde7a49a48a1366cc450ad7c3376123
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-24 22:31:10 +0000

    706cfec docs: update README to clarify app setup confirmation

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-63cb3f4
+ * @license Angular v21.2.0-rc.0+sha-706cfec
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-63cb3f4');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-706cfec');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
