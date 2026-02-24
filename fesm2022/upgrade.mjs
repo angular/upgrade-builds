96954137e23146b4b542a0b62fe1d4647d51cf8d
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-24 22:29:21 +0000

    bdbb7cc docs: update README to clarify app setup confirmation

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.0+sha-bb2745d
+ * @license Angular v22.0.0-next.0+sha-bdbb7cc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-bb2745d');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-bdbb7cc');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
