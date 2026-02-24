a59792c43618685500bf7e8fad0add069c6fd8a2
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-24 23:28:51 +0000

    c235b57 ci: update pullapprove for manual_api docs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.5+sha-24b39ab
+ * @license Angular v21.1.5+sha-c235b57
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.5+sha-24b39ab');
+const VERSION = /* @__PURE__ */new Version('21.1.5+sha-c235b57');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
