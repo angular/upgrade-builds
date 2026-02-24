797b25330ca9d93314409d2be618bf466c979dd0
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-24 19:08:17 +0000

    1a151f2 docs: add docs for `@switch` Exhaustive type checking

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-ab1c84e
+ * @license Angular v21.2.0-rc.0+sha-1a151f2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-ab1c84e');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-1a151f2');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
