314d0c18bc795a35302059361306e5f2b30f9ff9
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-24 19:30:32 +0000

    787cd87 refactor(vscode-extension): Add support for exhaustive type check in the syntax

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-1a151f2
+ * @license Angular v21.2.0-rc.0+sha-787cd87
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-1a151f2');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-787cd87');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
