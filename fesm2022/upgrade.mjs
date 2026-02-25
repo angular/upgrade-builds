c1862fad826b64302e2f7f2f75c62778b47ac788
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-25 20:51:14 +0000

    de0cc81 docs(forms): fix casing of 'FormField' to 'formField' in templates

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.0+sha-f9a92bf
+ * @license Angular v22.0.0-next.0+sha-de0cc81
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-f9a92bf');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-de0cc81');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
