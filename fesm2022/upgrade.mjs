1a61f07df76401ce8bb877fd5fd517cb93154dd5
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-25 19:36:24 +0000

    50a9ed1 release: cut the v21.2.0 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-e437980
+ * @license Angular v21.2.0+sha-50a9ed1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-e437980');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-50a9ed1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
