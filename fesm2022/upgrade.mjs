9916b18f6ba54ba07b5c4d4a76bad448ec058b9f
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-25 23:40:11 +0000

    661e800 release: bump VSCode extension version to 21.2.1

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0+sha-c960a6e
+ * @license Angular v21.2.0+sha-661e800
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-c960a6e');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-661e800');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
