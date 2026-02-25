9a49968bda0c1a135dceef33417adc7d8155f867
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-25 16:19:28 +0000

    89e8661 refactor(common): log a warning when a KeyValuePipe receives a signal

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.1.5+sha-c235b57
+ * @license Angular v21.1.5+sha-89e8661
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.1.5+sha-c235b57');
+const VERSION = /* @__PURE__ */new Version('21.1.5+sha-89e8661');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
