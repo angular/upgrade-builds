aa0f9d10af11b546802d11106768c4987a577542
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-26 18:04:19 +0000

    3d3e6cb build: update repository locations for bazel rules

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0+sha-2158bf8
+ * @license Angular v21.2.0+sha-3d3e6cb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-2158bf8');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-3d3e6cb');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
