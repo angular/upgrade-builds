75247c13cce615a26a4b61e3b08775ee6d387ccc
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-26 21:57:47 +0000

    03db2ae fix(compiler): throw on duplicate input/outputs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.0+sha-d1ebbbe
+ * @license Angular v22.0.0-next.0+sha-03db2ae
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-d1ebbbe');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-03db2ae');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
