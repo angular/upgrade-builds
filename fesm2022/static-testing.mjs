f1d02a80918cc9200ea5a46b71d8c600be109953
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-26 16:27:10 +0000

    1b46b6d docs: add info around route level services

--- fesm2022/static-testing.mjs
+++ fesm2022/static-testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-f23f3e7
+ * @license Angular v21.2.0+sha-1b46b6d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -20,7 +20,7 @@ class AngularTestingModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0+sha-f23f3e7",
+    version: "21.2.0+sha-1b46b6d",
     ngImport: i0,
     type: AngularTestingModule,
     deps: [{
@@ -30,13 +30,13 @@ class AngularTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.2.0+sha-f23f3e7",
+    version: "21.2.0+sha-1b46b6d",
     ngImport: i0,
     type: AngularTestingModule
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.2.0+sha-f23f3e7",
+    version: "21.2.0+sha-1b46b6d",
     ngImport: i0,
     type: AngularTestingModule,
     providers: [{
@@ -47,7 +47,7 @@ class AngularTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0+sha-f23f3e7",
+  version: "21.2.0+sha-1b46b6d",
   ngImport: i0,
   type: AngularTestingModule,
   decorators: [{
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0+sha-f23f3e7
+ * @license Angular v21.2.0+sha-1b46b6d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-f23f3e7');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-1b46b6d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
