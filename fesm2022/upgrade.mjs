b089e6fc810da7a97165b66ebca342dd1ff2a04d
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-26 20:49:58 +0000

    9c0587f build: update to latest sha of dev-infra repo

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0+sha-ac06941
+ * @license Angular v21.2.0+sha-9c0587f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-ac06941');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-9c0587f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
