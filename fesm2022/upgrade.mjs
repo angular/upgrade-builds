32afad35b7ccafcd9f1b8062cb02d643cecc7698
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-03-02 21:27:57 +0000

    dd77363 build: update shiki monorepo to v4

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.0+sha-c90c8a1
+ * @license Angular v22.0.0-next.0+sha-dd77363
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-c90c8a1');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-dd77363');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
