758ac717a3e8ac8986b83016fc02b94991200723
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-03-02 16:54:21 +0000

    23cb9a4 refactor(forms): address feedback

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0+sha-498d9cd
+ * @license Angular v21.2.0+sha-23cb9a4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-498d9cd');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-23cb9a4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
