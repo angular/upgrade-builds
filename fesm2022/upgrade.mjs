a2e34d2760f663ba6be8e5b8cf353cf47e20360d
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-03-02 19:16:02 +0000

    a79e8af docs: update doc for `provideZoneChangeDetection`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0+sha-23cb9a4
+ * @license Angular v21.2.0+sha-a79e8af
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-23cb9a4');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-a79e8af');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
