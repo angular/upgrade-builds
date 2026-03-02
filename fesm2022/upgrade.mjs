31a3e7b5c2774c6d836149e44e8a0710b48a71d7
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-03-02 19:48:08 +0000

    f5e9a86 refactor(core): ignore infinite animations in animate api

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0+sha-0c8a285
+ * @license Angular v21.2.0+sha-f5e9a86
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-0c8a285');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-f5e9a86');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
