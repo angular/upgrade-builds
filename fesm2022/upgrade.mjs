9feba7f438ed1a99e8620602f0a2474a0a66e4a1
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-03-03 22:53:25 +0000

    d9e40cb build: update dependency jasmine to v6

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.0+sha-0e9d58e
+ * @license Angular v22.0.0-next.0+sha-d9e40cb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-0e9d58e');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-d9e40cb');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
