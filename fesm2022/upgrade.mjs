3f563068fa4714cfc24d73853a64062884e7fb4b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-03-03 22:55:40 +0000

    657d532 build: update dependency preact-render-to-string to v6.6.6

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0+sha-796b9a7
+ * @license Angular v21.2.0+sha-657d532
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-796b9a7');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-657d532');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
