b057403a660744213daa83531765066565180eec
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-03-03 22:30:00 +0000

    0e9d58e refactor(compiler-cli): update old angular.io references to angular.dev

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.0+sha-778b748
+ * @license Angular v22.0.0-next.0+sha-0e9d58e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-778b748');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-0e9d58e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
