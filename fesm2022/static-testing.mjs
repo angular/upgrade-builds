1c2f90fc5261cb000f0880089ae62b1be4cb8847
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-03-04 17:28:38 +0000

    93c6dc6 Revert "refactor(http): Improves base64 encoding/decoding with feature detection (#67002)"

--- fesm2022/static-testing.mjs
+++ fesm2022/static-testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-b4ec3cc
+ * @license Angular v21.2.0+sha-93c6dc6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -20,7 +20,7 @@ class AngularTestingModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0+sha-b4ec3cc",
+    version: "21.2.0+sha-93c6dc6",
     ngImport: i0,
     type: AngularTestingModule,
     deps: [{
@@ -30,13 +30,13 @@ class AngularTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.2.0+sha-b4ec3cc",
+    version: "21.2.0+sha-93c6dc6",
     ngImport: i0,
     type: AngularTestingModule
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.2.0+sha-b4ec3cc",
+    version: "21.2.0+sha-93c6dc6",
     ngImport: i0,
     type: AngularTestingModule,
     providers: [{
@@ -47,7 +47,7 @@ class AngularTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0+sha-b4ec3cc",
+  version: "21.2.0+sha-93c6dc6",
   ngImport: i0,
   type: AngularTestingModule,
   decorators: [{
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0+sha-b4ec3cc
+ * @license Angular v21.2.0+sha-93c6dc6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-b4ec3cc');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-93c6dc6');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
