f0e03a172d2abcd96bc5dbe84e720c7696e26a47
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-03-04 17:27:25 +0000

    5338b59 Revert "refactor(http): Improves base64 encoding/decoding with feature detection (#67002)"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.0+sha-9e64147
+ * @license Angular v22.0.0-next.0+sha-5338b59
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-9e64147');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-5338b59');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
