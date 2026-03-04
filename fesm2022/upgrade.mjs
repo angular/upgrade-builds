7370218390339041aa2e0b9cc20ff49191d8687c
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-03-04 18:25:12 +0000

    6d7446b refactor(core): account for shadow roots in animations

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0+sha-f4160c2
+ * @license Angular v21.2.0+sha-6d7446b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-f4160c2');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-6d7446b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
