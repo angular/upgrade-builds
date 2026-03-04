dfc96e3f40cab7f5e35fd8273b1e7f7d78945855
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-03-04 18:08:39 +0000

    f4160c2 build: update cross-repo angular dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0+sha-93c6dc6
+ * @license Angular v21.2.0+sha-f4160c2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-93c6dc6');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-f4160c2');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
