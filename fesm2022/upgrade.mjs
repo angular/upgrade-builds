dd6b062fd52215592eee297b00017827ab4c30f9
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-03-04 16:10:48 +0000

    84e79f5 build: update pnpm to v10.30.3

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0+sha-657d532
+ * @license Angular v21.2.0+sha-84e79f5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-657d532');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-84e79f5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
