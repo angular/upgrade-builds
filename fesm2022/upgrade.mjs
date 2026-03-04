b0f0c5611ff356e14d83893daead45858f698bdf
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-03-04 16:28:26 +0000

    b4ec3cc fix(core): prevent child animation elements from being orphaned

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.0+sha-84e79f5
+ * @license Angular v21.2.0+sha-b4ec3cc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-84e79f5');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-b4ec3cc');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
