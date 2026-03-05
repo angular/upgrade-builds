c3e61c02fb94b6c298d9d89a3a9288c6459cf360
Andrew Scott <atscott01@gmail.com>
2026-03-05 18:26:54 +0000

    0964b2c release: cut the v22.0.0-next.1 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.0+sha-c767d67
+ * @license Angular v22.0.0-next.1+sha-0964b2c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-c767d67');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-0964b2c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
