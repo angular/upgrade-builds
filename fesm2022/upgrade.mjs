101717438913f3855fc66d598f1c492aa4d6f294
Andrew Scott <atscott01@gmail.com>
2026-03-05 22:42:57 +0000

    ac8418d refactor(compiler-cli): export TCB types and methods

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.1+sha-dd464e5
+ * @license Angular v22.0.0-next.1+sha-ac8418d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-dd464e5');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-ac8418d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
