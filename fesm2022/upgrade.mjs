4c30732c3907a0997c5b514399d367e439a7679c
Andrew Scott <atscott01@gmail.com>
2026-03-05 22:20:00 +0000

    c822bf8 fix(compiler-cli): always parenthesize object literals in TCB

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.1+sha-fd3b441
+ * @license Angular v21.2.1+sha-c822bf8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.1+sha-fd3b441');
+const VERSION = /* @__PURE__ */new Version('21.2.1+sha-c822bf8');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
