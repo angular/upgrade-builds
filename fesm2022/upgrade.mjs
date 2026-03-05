1b9e08c9f4602ff23b44b5ab28baeba9f7910c90
Andrew Scott <atscott01@gmail.com>
2026-03-05 20:08:54 +0000

    3693687 refactor(core): remove `createNgModuleRef`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.1+sha-9fe820d
+ * @license Angular v22.0.0-next.1+sha-3693687
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-9fe820d');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-3693687');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
