222ad1aa9260d34b9d65d5c0e0ec2e9fdaf529b6
Andrew Scott <atscott01@gmail.com>
2026-03-06 23:41:56 +0000

    276c12a build: update dependency node to v22.22.1

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.1+sha-104ac11
+ * @license Angular v21.2.1+sha-276c12a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.1+sha-104ac11');
+const VERSION = /* @__PURE__ */new Version('21.2.1+sha-276c12a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
