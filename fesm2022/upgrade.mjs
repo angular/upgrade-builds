59e7894b997a198a777433780d88fa8c6761114f
GitHub <noreply@github.com>
2026-03-06 20:19:58 +0000

    786ea44 build: force external sourcemaps for esbuild targets

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.1+sha-759ee22
+ * @license Angular v22.0.0-next.1+sha-786ea44
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-759ee22');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-786ea44');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
