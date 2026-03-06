2aabf43afe107917837ee661c3a92c3bddd8fa44
Andrew Scott <atscott01@gmail.com>
2026-03-06 19:06:14 +0000

    759ee22 build: update all github actions to v7

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.1+sha-cc64def
+ * @license Angular v22.0.0-next.1+sha-759ee22
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-cc64def');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-759ee22');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
