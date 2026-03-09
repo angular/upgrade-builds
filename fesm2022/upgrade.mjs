66f7086981e7b04e9707cb29a5b43f2f6a6d3ea7
Andrew Scott <atscott01@gmail.com>
2026-03-09 23:48:02 +0000

    4a9b715 test(forms): cover transformedValue without FormField context

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.2+sha-c499c57
+ * @license Angular v21.2.2+sha-4a9b715
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.2+sha-c499c57');
+const VERSION = /* @__PURE__ */new Version('21.2.2+sha-4a9b715');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
