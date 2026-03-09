675c739774482503dea08f85ac47600d24489cf8
Andrew Scott <atscott@google.com>
2026-03-09 17:54:07 +0000

    bd72571 refactor(devtools): prevent spamming the message bus

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.1+sha-628f3f9
+ * @license Angular v21.2.1+sha-bd72571
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.1+sha-628f3f9');
+const VERSION = /* @__PURE__ */new Version('21.2.1+sha-bd72571');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
