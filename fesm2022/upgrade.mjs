d0a35dc63e70fde09f407e5fc48448ecb13eda6e
Andrew Scott <atscott@google.com>
2026-03-09 17:29:14 +0000

    79ac570 refactor(devtools): fix browser-specific styles infra

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.1+sha-276c12a
+ * @license Angular v21.2.1+sha-79ac570
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.1+sha-276c12a');
+const VERSION = /* @__PURE__ */new Version('21.2.1+sha-79ac570');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
