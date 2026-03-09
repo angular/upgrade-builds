f7757e5b69e488d7f9b91b346a31de2a83ef077c
Andrew Scott <atscott01@gmail.com>
2026-03-09 17:36:37 +0000

    0346536 docs: add Turkish community translation of Angular documentation

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.1+sha-786ea44
+ * @license Angular v22.0.0-next.1+sha-0346536
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-786ea44');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-0346536');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
