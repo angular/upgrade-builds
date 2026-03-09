52349b7db0df33efdd1907698f6333ee3b46a917
Andrew Scott <atscott01@gmail.com>
2026-03-09 17:54:14 +0000

    263b819 refactor(devtools): prevent spamming the message bus

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.1+sha-0346536
+ * @license Angular v22.0.0-next.1+sha-263b819
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-0346536');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-263b819');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
