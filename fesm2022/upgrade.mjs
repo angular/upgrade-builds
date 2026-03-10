18ea3fe1555fda1c3431a9e0610ccaeed796f572
Andrew Scott <atscott01@gmail.com>
2026-03-10 22:15:59 +0000

    57ba621 test(forms): read only context prevents writing to field value

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.1+sha-02a617e
+ * @license Angular v22.0.0-next.1+sha-57ba621
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-02a617e');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-57ba621');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
