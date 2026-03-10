5bcf157bff31c803ed93476b6d26f1eb1031eb05
Andrew Scott <atscott01@gmail.com>
2026-03-10 20:16:26 +0000

    02a617e build: update dependency @rollup/plugin-babel to v7

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.1+sha-a675950
+ * @license Angular v22.0.0-next.1+sha-02a617e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-a675950');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-02a617e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
