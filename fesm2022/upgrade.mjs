5fb6d02a87b34524f6829e3701841224142c920d
Andrew Scott <atscott01@gmail.com>
2026-03-10 17:12:00 +0000

    ab35383 docs: fix first-app code getHousingLocationById housing.service.ts

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.2+sha-106cf4e
+ * @license Angular v21.2.2+sha-ab35383
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.2+sha-106cf4e');
+const VERSION = /* @__PURE__ */new Version('21.2.2+sha-ab35383');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
