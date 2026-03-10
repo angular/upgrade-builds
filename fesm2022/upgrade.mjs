e57be2761028c25946d6270cacb20585002fe6ba
Andrew Scott <atscott01@gmail.com>
2026-03-10 19:57:30 +0000

    575f302 refactor(core): interface cleanup

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.2+sha-224e60e
+ * @license Angular v21.2.2+sha-575f302
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.2+sha-224e60e');
+const VERSION = /* @__PURE__ */new Version('21.2.2+sha-575f302');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
