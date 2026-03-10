9f334561c5cc2afcf2149148af712d34ffb35b26
Andrew Scott <atscott01@gmail.com>
2026-03-10 17:34:31 +0000

    09638ec docs(core): clarify provideZoneChangeDetection usage in v21+

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.2+sha-ab35383
+ * @license Angular v21.2.2+sha-09638ec
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.2+sha-ab35383');
+const VERSION = /* @__PURE__ */new Version('21.2.2+sha-09638ec');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
