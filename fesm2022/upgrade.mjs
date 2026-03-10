cdc8ade5ab7e1a0095b72ea72595169dadb3491e
Andrew Scott <atscott01@gmail.com>
2026-03-10 00:09:39 +0000

    024e307 build: update pnpm to v10.31.0

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.2+sha-4a9b715
+ * @license Angular v21.2.2+sha-024e307
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.2+sha-4a9b715');
+const VERSION = /* @__PURE__ */new Version('21.2.2+sha-024e307');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
