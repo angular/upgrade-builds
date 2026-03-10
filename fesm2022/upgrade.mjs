823cc542feb705eaaed660e4cd532a87ddec7ef1
Andrew Scott <atscott01@gmail.com>
2026-03-10 18:27:01 +0000

    224e60e fix(core): sanitize translated attribute bindings with interpolations

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.2+sha-09638ec
+ * @license Angular v21.2.2+sha-224e60e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.2+sha-09638ec');
+const VERSION = /* @__PURE__ */new Version('21.2.2+sha-224e60e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
