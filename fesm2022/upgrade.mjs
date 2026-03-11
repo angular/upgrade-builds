1056b40de349e74293f1ef61c2ab1c2c817b968d
Andrew Scott <atscott@google.com>
2026-03-11 21:20:15 +0000

    74698d5 docs(forms): clarify zoneless change detection for reactive FormArray updates

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.3+sha-7907e98
+ * @license Angular v21.2.3+sha-74698d5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.3+sha-7907e98');
+const VERSION = /* @__PURE__ */new Version('21.2.3+sha-74698d5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
