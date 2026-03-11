f0baaa9f4fb6528ea4e6610ce806fc9ada47066e
Andrew Scott <atscott01@gmail.com>
2026-03-11 22:01:43 +0000

    0ab344c docs: release notes for the vscode extension 21.2.3 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.2+sha-3d25953
+ * @license Angular v22.0.0-next.2+sha-0ab344c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.2+sha-3d25953');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.2+sha-0ab344c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
