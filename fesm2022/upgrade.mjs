2689c0fd4e41da230de479f902c64e76c6301f8a
Andrew Scott <atscott01@gmail.com>
2026-03-11 17:10:42 +0000

    d45fced docs: remove duplicate aria-hidden attribute in angular aria ngTree example

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.2+sha-ed48c41
+ * @license Angular v21.2.2+sha-d45fced
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.2+sha-ed48c41');
+const VERSION = /* @__PURE__ */new Version('21.2.2+sha-d45fced');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
