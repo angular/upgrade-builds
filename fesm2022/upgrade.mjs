ceb82e914c649ab429a6da26ab9b1ce490345c51
Andrew Scott <atscott@google.com>
2026-03-11 20:44:50 +0000

    6672192 test: remove duplicate tests (#67518)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.2+sha-cfb8aff
+ * @license Angular v22.0.0-next.2+sha-6672192
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.2+sha-cfb8aff');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.2+sha-6672192');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
