feed97271c8527929ab0692013f9276dbf520271
Andrew Scott <atscott01@gmail.com>
2026-03-11 21:38:33 +0000

    8ba2844 docs: document coverageExclude option for excluding files from coverage

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.3+sha-74698d5
+ * @license Angular v21.2.3+sha-8ba2844
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.3+sha-74698d5');
+const VERSION = /* @__PURE__ */new Version('21.2.3+sha-8ba2844');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
