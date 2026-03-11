274313d930b4301e0196c59428f1ed3693cc9515
Andrew Scott <atscott01@gmail.com>
2026-03-11 17:21:08 +0000

    4c532bd build: lock file maintenance

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.1+sha-66e72ef
+ * @license Angular v22.0.0-next.1+sha-4c532bd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-66e72ef');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-4c532bd');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
