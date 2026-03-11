bc6904dbf7622e33042f3bba5dfffc2510b2650d
Andrew Scott <atscott01@gmail.com>
2026-03-11 16:40:09 +0000

    7513558 docs: combine multiple documentation improvements into one PR

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.2+sha-575f302
+ * @license Angular v21.2.2+sha-7513558
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.2+sha-575f302');
+const VERSION = /* @__PURE__ */new Version('21.2.2+sha-7513558');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
