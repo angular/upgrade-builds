1fce01d322ec78db613d1d6c5df640e705b0b94e
Andrew Scott <atscott01@gmail.com>
2026-03-11 20:16:56 +0000

    cfb8aff release: cut the v22.0.0-next.2 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.1+sha-dea3241
+ * @license Angular v22.0.0-next.2+sha-cfb8aff
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-dea3241');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.2+sha-cfb8aff');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
