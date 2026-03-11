15611e90fbecc8cd9e23a227cd6095c3d1e20ea3
Andrew Scott <atscott01@gmail.com>
2026-03-11 16:49:57 +0000

    ed48c41 refactor(compiler-cli): simplifies `IncrementalCompilation.fresh`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.2+sha-7513558
+ * @license Angular v21.2.2+sha-ed48c41
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.2+sha-7513558');
+const VERSION = /* @__PURE__ */new Version('21.2.2+sha-ed48c41');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
