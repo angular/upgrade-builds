865e9c0ac3b24a79929b91715ed468a96ed3d481
Andrew Scott <atscott01@gmail.com>
2026-03-11 19:17:14 +0000

    dea3241 docs: release notes for the v21.2.3 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.1+sha-3c2d95a
+ * @license Angular v22.0.0-next.1+sha-dea3241
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-3c2d95a');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-dea3241');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
