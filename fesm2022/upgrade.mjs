887a01be127ed78f75e76ff4aa9c905b42de087c
Andrew Scott <atscott01@gmail.com>
2026-03-11 18:01:53 +0000

    6c73aac refactor(common): Removes unused generic type parameters from `KeyValueDiffers`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.2+sha-d45fced
+ * @license Angular v21.2.2+sha-6c73aac
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.2+sha-d45fced');
+const VERSION = /* @__PURE__ */new Version('21.2.2+sha-6c73aac');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
