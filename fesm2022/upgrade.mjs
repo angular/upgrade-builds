475fdbbb97485cf8b1ce4422b1aa63369399463a
Andrew Scott <atscott01@gmail.com>
2026-03-11 21:47:56 +0000

    c06e3a0 build: update bazel dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.3+sha-8ba2844
+ * @license Angular v21.2.3+sha-c06e3a0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.3+sha-8ba2844');
+const VERSION = /* @__PURE__ */new Version('21.2.3+sha-c06e3a0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
