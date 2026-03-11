e5393e2720bdf32642af84b4bcc6dcdc2ac24337
Alan Agius <alan.agius4@gmail.com>
2026-03-11 13:28:34 +0000

    71c9c6d release: bump Angular DevTools version to 1.13.0

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.1+sha-57ba621
+ * @license Angular v22.0.0-next.1+sha-71c9c6d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-57ba621');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-71c9c6d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
