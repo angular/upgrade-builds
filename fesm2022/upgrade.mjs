0ae24640f842c0d5da57e606b46d4d1e53f0d69b
Matthew Beck (Berry) <me@mattrb.com>
2026-03-12 22:19:03 +0000

    a929494 docs: update cross-repo adev docs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.3+sha-bb628d8
+ * @license Angular v22.0.0-next.3+sha-a929494
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-bb628d8');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-a929494');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
