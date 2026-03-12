4a26a7c82404a36d87817b303cef209edc662f5e
Matthew Beck (Berry) <me@mattrb.com>
2026-03-12 17:10:10 +0000

    78dea55 fix(compiler): disallow translations of iframe src

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.2+sha-0ab344c
+ * @license Angular v22.0.0-next.2+sha-78dea55
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.2+sha-0ab344c');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.2+sha-78dea55');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
