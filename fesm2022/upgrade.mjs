b850cf0dd2614bde187e3f52bf9558fa0140fbb5
Matthew Beck (Berry) <me@mattrb.com>
2026-03-12 19:06:28 +0000

    8017617 release: cut the v21.2.4 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.3+sha-ed2d324
+ * @license Angular v21.2.4+sha-8017617
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.3+sha-ed2d324');
+const VERSION = /* @__PURE__ */new Version('21.2.4+sha-8017617');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
