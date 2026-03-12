9b57a04288dfc1c71ba068f015c272d9834ee4b0
Matthew Beck (Berry) <me@mattrb.com>
2026-03-12 21:04:53 +0000

    bb628d8 refactor: ensure modules with 'declare global' are correctly handled by Rollup

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.3+sha-432a3de
+ * @license Angular v22.0.0-next.3+sha-bb628d8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-432a3de');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-bb628d8');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
