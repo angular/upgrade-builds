c87dba350aea6309dab5a156578a311a25d3c728
Matthew Beck (Berry) <me@mattrb.com>
2026-03-12 22:29:27 +0000

    8edcf41 refactor(compiler-cli): add warning for matching viewport @defer trigger options

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.3+sha-a929494
+ * @license Angular v22.0.0-next.3+sha-8edcf41
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-a929494');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-8edcf41');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
