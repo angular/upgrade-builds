1d3852ed01edf08423fb3a08718e022c100efad6
Matthew Beck (Berry) <me@mattrb.com>
2026-03-12 22:32:31 +0000

    bfb8b17 refactor(compiler-cli): remove more unused code

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.4+sha-5d755be
+ * @license Angular v21.2.4+sha-bfb8b17
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.4+sha-5d755be');
+const VERSION = /* @__PURE__ */new Version('21.2.4+sha-bfb8b17');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
