5b946f1d3420479a83251f1e1bd47d74bbee63dd
Matthew Beck <me@mattrb.com>
2026-03-12 23:46:23 +0000

    6f41efc refactor(core): Update determineLongestAnimation to account for playback rate

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.4+sha-bfb8b17
+ * @license Angular v21.2.4+sha-6f41efc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.4+sha-bfb8b17');
+const VERSION = /* @__PURE__ */new Version('21.2.4+sha-6f41efc');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
