506a60fa946ef63f4623a5a064dcb341ce7b8f5f
Matthew Beck <me@mattrb.com>
2026-03-13 19:23:29 +0000

    21cab20 docs: combine multiple documentation typo fixes

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.4+sha-334ae10
+ * @license Angular v21.2.4+sha-21cab20
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.4+sha-334ae10');
+const VERSION = /* @__PURE__ */new Version('21.2.4+sha-21cab20');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
