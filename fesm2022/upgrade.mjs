70037ae00dc7d86f8b0af004e5f32c5d4c9b8c68
Matthew Beck (Berry) <me@mattrb.com>
2026-03-13 19:00:28 +0000

    334ae10 fix(compiler): ensure generated code compiles

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.4+sha-6f41efc
+ * @license Angular v21.2.4+sha-334ae10
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.4+sha-6f41efc');
+const VERSION = /* @__PURE__ */new Version('21.2.4+sha-334ae10');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
