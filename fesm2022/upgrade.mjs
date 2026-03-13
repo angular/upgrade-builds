35199057384310bebd5e525cc41856cb7f9841b5
Matthew Beck (Berry) <me@mattrb.com>
2026-03-13 22:34:12 +0000

    ada150c build: update cross-repo angular dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.3+sha-e127927
+ * @license Angular v22.0.0-next.3+sha-ada150c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-e127927');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-ada150c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
