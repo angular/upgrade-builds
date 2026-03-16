9df35030960e41c18f7e1802b9c21314669e5440
Matthew Beck (Berry) <me@mattrb.com>
2026-03-16 22:10:51 +0000

    eeba51c refactor(forms): make `markAsTouched()` touch all descendants by default

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.3+sha-a94958b
+ * @license Angular v22.0.0-next.3+sha-eeba51c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-a94958b');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-eeba51c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
