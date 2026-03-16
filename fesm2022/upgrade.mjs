378db0a41a67c7ed5edff26333b36b9b7b18f551
Matthew Beck (Berry) <me@mattrb.com>
2026-03-16 21:32:20 +0000

    a94958b refactor(forms): Address more feedback

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.3+sha-470cf43
+ * @license Angular v22.0.0-next.3+sha-a94958b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-470cf43');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-a94958b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
