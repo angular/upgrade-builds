9c7f7e924450e8d1b2635746b1fda8791b28d1fb
Matthew Beck (Berry) <me@mattrb.com>
2026-03-16 20:58:15 +0000

    470cf43 test(language-service): optimize language service test environment and flatten test setup

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.3+sha-2e34924
+ * @license Angular v22.0.0-next.3+sha-470cf43
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-2e34924');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-470cf43');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
