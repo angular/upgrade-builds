aaef04316acfd7e1928839b2c14b83c888ba045e
Matthew Beck <me@mattrb.com>
2026-03-16 16:20:27 +0000

    1890c30 fix(core): clean up dehydrated views during HMR component replacement

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.4+sha-f39896d
+ * @license Angular v21.2.4+sha-1890c30
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.4+sha-f39896d');
+const VERSION = /* @__PURE__ */new Version('21.2.4+sha-1890c30');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
