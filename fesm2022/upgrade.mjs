b94f3030486cb3cf9a9cab49c5f30fd96317d1b4
Matthew Beck (Berry) <me@mattrb.com>
2026-03-16 16:20:33 +0000

    dc04465 fix(core): clean up dehydrated views during HMR component replacement

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.3+sha-ada150c
+ * @license Angular v22.0.0-next.3+sha-dc04465
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-ada150c');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-dc04465');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
