b02832b61e679a8b138069121fcaa0ac74ece072
Matthew Beck <me@mattrb.com>
2026-03-17 20:07:53 +0000

    a8f80c1 refactor(core): declare explicit reactive node prototypes types

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.4+sha-67e0ba7
+ * @license Angular v21.2.4+sha-a8f80c1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.4+sha-67e0ba7');
+const VERSION = /* @__PURE__ */new Version('21.2.4+sha-a8f80c1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
