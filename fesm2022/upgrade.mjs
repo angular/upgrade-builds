95e6f139d2bb6fd49e5c73db9fede6e99a733d35
Matthew Beck <me@mattrb.com>
2026-03-17 19:33:36 +0000

    67e0ba7 fix(compiler-cli): generic types not filled out correctly in type check block

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.4+sha-ca328ea
+ * @license Angular v21.2.4+sha-67e0ba7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.4+sha-ca328ea');
+const VERSION = /* @__PURE__ */new Version('21.2.4+sha-67e0ba7');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
