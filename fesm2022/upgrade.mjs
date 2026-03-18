678ae8d46564e1ecc0ac57183769b2dd27f6d37f
Matthew Beck (Berry) <me@mattrb.com>
2026-03-18 00:10:00 +0000

    730684b fix(migrations): prevent trailing comma syntax errors after removing NgStyle

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.3+sha-1eaf920
+ * @license Angular v22.0.0-next.3+sha-730684b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-1eaf920');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-730684b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
