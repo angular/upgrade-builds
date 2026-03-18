ab9a158cf8fda0e78e365b3f75270e13686f3688
Matthew Beck <me@mattrb.com>
2026-03-18 00:46:57 +0000

    737777e docs: add Angular Developer Skill updates

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.4+sha-076d41c
+ * @license Angular v21.2.4+sha-737777e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.4+sha-076d41c');
+const VERSION = /* @__PURE__ */new Version('21.2.4+sha-737777e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
