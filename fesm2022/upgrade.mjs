4244a49ab753a1a79797db7fce1973d051f3b091
Matthew Beck (Berry) <me@mattrb.com>
2026-03-19 02:19:26 +0000

    557f6e7 docs: release notes for the v21.2.5 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.3+sha-785735b
+ * @license Angular v22.0.0-next.3+sha-557f6e7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-785735b');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-557f6e7');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
