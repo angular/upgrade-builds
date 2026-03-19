f7784b39fb32c224f5d1089015ff0bba7d06ee58
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-19 23:14:28 +0000

    9182b62 docs(forms): add section on managing validators dynamically in form validation guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.5+sha-ad0156e
+ * @license Angular v21.2.5+sha-9182b62
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.5+sha-ad0156e');
+const VERSION = /* @__PURE__ */new Version('21.2.5+sha-9182b62');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
