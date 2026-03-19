25a7f6850d6b7a3d8102242438ce0114470bb7d1
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-19 23:25:57 +0000

    50e599e fix(core): lazy-initialize debounced state to prevent computation cycle

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.4+sha-836bf20
+ * @license Angular v22.0.0-next.4+sha-50e599e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-836bf20');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-50e599e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
