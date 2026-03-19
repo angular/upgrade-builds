c8f702158f4a5a33c82f4acc6658e28c022590e9
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-19 23:00:47 +0000

    ad0156e fix(core): fixes a regression with animate.leave and reordering

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.5+sha-aa3220a
+ * @license Angular v21.2.5+sha-ad0156e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.5+sha-aa3220a');
+const VERSION = /* @__PURE__ */new Version('21.2.5+sha-ad0156e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
