9f84a3b14ffe0f5d7b137f0311ee5a4ad1653138
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-20 23:00:54 +0000

    a21be36 refactor: prepare for required `changeDetection` prop on G3.

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.5+sha-7aaa8b9
+ * @license Angular v21.2.5+sha-a21be36
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.5+sha-7aaa8b9');
+const VERSION = /* @__PURE__ */new Version('21.2.5+sha-a21be36');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
