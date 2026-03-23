7da3cf7b9135e6cc52061182fd155175a857c045
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-23 21:57:17 +0000

    c7a1892 fix(devtools): fix incorrect logic in destroy function for ChromeMessageBus

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.5+sha-b658acc
+ * @license Angular v21.2.5+sha-c7a1892
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.5+sha-b658acc');
+const VERSION = /* @__PURE__ */new Version('21.2.5+sha-c7a1892');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
