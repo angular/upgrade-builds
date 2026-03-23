fc1e0f463f6760b7ad9ff1008bef4022d9f6b267
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-23 16:56:20 +0000

    293ac66 ci: support publishing dev-skills to snapshot repository

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.4+sha-d2054c7
+ * @license Angular v22.0.0-next.4+sha-293ac66
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-d2054c7');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-293ac66');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
