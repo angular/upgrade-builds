0c485eb6278585cda8dc44bec21e6edb09120a5c
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-23 18:43:32 +0000

    f46e8bd refactor(compiler): remove `fullTemplateTypeCheck` compiler option.

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.4+sha-2615f35
+ * @license Angular v22.0.0-next.4+sha-f46e8bd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-2615f35');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-f46e8bd');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
