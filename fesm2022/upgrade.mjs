af5bf08b6f7ac8bb9daa87fe6483ad2df9589611
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-24 23:36:52 +0000

    eae8f7e feat(core): Set default Component changeDetection strategy to OnPush

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.4+sha-050b14b
+ * @license Angular v22.0.0-next.4+sha-eae8f7e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-050b14b');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-eae8f7e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
