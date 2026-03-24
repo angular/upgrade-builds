729cb7e471170bbda9e22fa4daea59b8893bf2b6
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-24 21:56:52 +0000

    e731bbd docs: clarify size budget configuration

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.5+sha-c7a1892
+ * @license Angular v21.2.5+sha-e731bbd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.5+sha-c7a1892');
+const VERSION = /* @__PURE__ */new Version('21.2.5+sha-e731bbd');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
