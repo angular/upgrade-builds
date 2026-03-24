91803e047f832372f03b84bbb65beaae7f4708de
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-24 21:59:02 +0000

    ee8d209 fix(forms): change FieldState optional properties to non-optional | undefined

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.4+sha-0eeb1b5
+ * @license Angular v22.0.0-next.4+sha-ee8d209
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-0eeb1b5');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-ee8d209');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
