51cb15e2ce3fc8d9881a4057c4d0f43aec53aa26
GitHub <noreply@github.com>
2026-03-25 20:45:12 +0000

    b5af15a test(forms): support directive composition with `FormField`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.4+sha-eecfa4c
+ * @license Angular v22.0.0-next.4+sha-b5af15a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-eecfa4c');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-b5af15a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
