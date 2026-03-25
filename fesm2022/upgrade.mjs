7bccfcc406c85c0462e5d46b54962f7f402e9623
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-25 21:27:21 +0000

    24e52d4 feat(forms): add debounce option to validateAsync and validateHttp

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.4+sha-b5af15a
+ * @license Angular v22.0.0-next.4+sha-24e52d4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-b5af15a');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-24e52d4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
