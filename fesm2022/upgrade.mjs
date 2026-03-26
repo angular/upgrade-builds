7f5d0c0cdaccb3f0fe7247cf875259c8c42d4742
Andrew Scott <atscott01@gmail.com>
2026-03-26 21:42:37 +0000

    b453c0c refactor(language-service): Update getTcbNodesOfTemplateAtPosition to be usable without compiler (#67898)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.6+sha-807cabd
+ * @license Angular v21.2.6+sha-b453c0c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.6+sha-807cabd');
+const VERSION = /* @__PURE__ */new Version('21.2.6+sha-b453c0c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
