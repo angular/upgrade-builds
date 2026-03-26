0a05997463808400076d64dd409d26d0da899563
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-26 17:54:40 +0000

    807cabd fix(dev-infra): improve PR review skill handling of git worktrees

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.6+sha-13f050d
+ * @license Angular v21.2.6+sha-807cabd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.6+sha-13f050d');
+const VERSION = /* @__PURE__ */new Version('21.2.6+sha-807cabd');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
