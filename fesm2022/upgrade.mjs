d5ce84819f5c310a566c2cde4b07ff1f8edde79a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-26 17:42:53 +0000

    13f050d test: construct local `Date` objects to fix timezone flakiness

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.6+sha-d8239c7
+ * @license Angular v21.2.6+sha-13f050d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.6+sha-d8239c7');
+const VERSION = /* @__PURE__ */new Version('21.2.6+sha-13f050d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
