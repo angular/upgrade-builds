4697e1962a254f69bd3ea94c714539be9eb47c6a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-26 17:46:04 +0000

    d268df6 build: remove unused `@nginfra/angular-linking` dependency

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.5+sha-d4c085d
+ * @license Angular v22.0.0-next.5+sha-d268df6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-d4c085d');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-d268df6');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
