af6f839fdc8c03cf58074b935a34f38e0fbe26d1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-27 14:43:41 +0000

    c1261b0 refactor: extract comments utilities to private

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.5+sha-9aa315c
+ * @license Angular v22.0.0-next.5+sha-c1261b0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-9aa315c');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-c1261b0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
