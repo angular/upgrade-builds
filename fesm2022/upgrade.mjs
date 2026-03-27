31cf458962c14b3ebf21d514b754b513fb042aec
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-27 16:25:51 +0000

    9b48cb2 docs(docs-infra): introduce a custom UrlSerializer

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.6+sha-bba5ed8
+ * @license Angular v21.2.6+sha-9b48cb2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.6+sha-bba5ed8');
+const VERSION = /* @__PURE__ */new Version('21.2.6+sha-9b48cb2');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
