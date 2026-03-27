c895103df963275f1f7f6f11f217faff4ec877a0
Andrew Scott <atscott01@gmail.com>
2026-03-27 18:48:04 +0000

    86bbc94 docs: add agent skills documentation and new readme

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.5+sha-9d79ec6
+ * @license Angular v22.0.0-next.5+sha-86bbc94
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-9d79ec6');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-86bbc94');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
