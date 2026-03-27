ddb72587352c2919f5223dd54fd5ff328543e7e6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-27 14:50:53 +0000

    43ef717 docs: update change detection strategy references

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.5+sha-c1261b0
+ * @license Angular v22.0.0-next.5+sha-43ef717
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-c1261b0');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-43ef717');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
