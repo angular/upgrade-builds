11f0111784509e1d755d5e4cd2d8588e2f8d8e50
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-27 15:17:28 +0000

    bba5ed8 fix(compiler-cli): prevent recursive scope checks for invalid NgModule imports

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.6+sha-56c872d
+ * @license Angular v21.2.6+sha-bba5ed8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.6+sha-56c872d');
+const VERSION = /* @__PURE__ */new Version('21.2.6+sha-bba5ed8');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
