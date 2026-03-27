8f2893466443b866ca55f8064d0d86cdd15c7eb5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-27 15:07:29 +0000

    12b3a1a refactor(migrations):  keep the `http-xhr-backend` migration idempotent

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.5+sha-43ef717
+ * @license Angular v22.0.0-next.5+sha-12b3a1a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-43ef717');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-12b3a1a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
