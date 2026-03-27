7c8f72c338abafe3c1996ff444077d6bc533ea5c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-27 13:20:19 +0000

    56c872d docs: update cross-repo adev docs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.6+sha-c8902de
+ * @license Angular v21.2.6+sha-56c872d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.6+sha-c8902de');
+const VERSION = /* @__PURE__ */new Version('21.2.6+sha-56c872d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
