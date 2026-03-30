5a3a36bb20ccfd6732eaf9f346fd8c5a4ab28a9c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-30 08:49:27 +0000

    c6f73f4 docs: update url from `<base href>`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.5+sha-86bbc94
+ * @license Angular v22.0.0-next.5+sha-c6f73f4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-86bbc94');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-c6f73f4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
