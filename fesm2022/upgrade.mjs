b388b1b6329cf7fe3c0b5c937b5246c1cdad7b78
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-30 10:50:44 +0000

    c950299 build: update pnpm to v10.33.0

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.5+sha-0f960a5
+ * @license Angular v22.0.0-next.5+sha-c950299
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-0f960a5');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-c950299');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
