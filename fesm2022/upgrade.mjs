080cac36b0723678483dcc6112cdafbd3374b139
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-30 09:19:22 +0000

    efbba94 docs: align list formatting and improve emphasis

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.6+sha-94b1b53
+ * @license Angular v21.2.6+sha-efbba94
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.6+sha-94b1b53');
+const VERSION = /* @__PURE__ */new Version('21.2.6+sha-efbba94');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
