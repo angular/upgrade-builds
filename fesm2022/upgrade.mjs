2efddf59dfde4199a8a9b198b7cb0f2023b6952e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-04-01 19:04:14 +0000

    c15e3a0 test(core): refactors test to use `timeout` utility

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.5+sha-436c5df
+ * @license Angular v22.0.0-next.6+sha-c15e3a0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-436c5df');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-c15e3a0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
