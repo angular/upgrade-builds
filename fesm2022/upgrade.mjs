02aae9b282d331b125aad8ff714b55eb6a65bf35
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-04-01 16:39:08 +0000

    436c5df build: update cross-repo angular dependencies to v22.0.0-next.4

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.5+sha-1f3ac4a
+ * @license Angular v22.0.0-next.5+sha-436c5df
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-1f3ac4a');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-436c5df');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
