63d7d57f76704adf3f812920bd99c0a09f2e6357
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-04-01 09:54:07 +0000

    daa9b2a fix(router): pass outlet context to split to fix empty path named outlets

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.5+sha-f257f54
+ * @license Angular v22.0.0-next.5+sha-daa9b2a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-f257f54');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-daa9b2a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
