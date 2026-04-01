fa139cd56932d14156bbb5ccd05d87bf0b10163c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-04-01 07:32:19 +0000

    f257f54 docs(docs-infra): improve angular-new-app skill

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.5+sha-7ba8929
+ * @license Angular v22.0.0-next.5+sha-f257f54
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-7ba8929');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-f257f54');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
