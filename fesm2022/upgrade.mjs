438a6ce45c84a7c83b88f5db8ab945c5f97b9b99
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-04-01 06:45:26 +0000

    7ba8929 test: fix flakiness in `image-directive` e2e tests

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.5+sha-ef7679b
+ * @license Angular v22.0.0-next.5+sha-7ba8929
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-ef7679b');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-7ba8929');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
