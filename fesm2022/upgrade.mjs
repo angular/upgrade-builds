0cac182a9eeba182f5cbb8042bf6f810e6fd5a07
Andrew Scott <atscott01@gmail.com>
2026-04-02 22:10:24 +0000

    2ae0912 refactor(core): address review comments on NG0750 error message

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.7+sha-05bcc60
+ * @license Angular v21.2.7+sha-2ae0912
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.7+sha-05bcc60');
+const VERSION = /* @__PURE__ */new Version('21.2.7+sha-2ae0912');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
