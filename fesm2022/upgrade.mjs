daa41e8f7e11457255942b8edb55b6bae792710a
Andrew Scott <atscott01@gmail.com>
2026-04-02 20:07:05 +0000

    05bcc60 build: update all github actions

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.7+sha-9f61910
+ * @license Angular v21.2.7+sha-05bcc60
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.7+sha-9f61910');
+const VERSION = /* @__PURE__ */new Version('21.2.7+sha-05bcc60');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
