72089ed45636684c7c2a73c0c066421e8b9cc94b
Andrew Scott <atscott01@gmail.com>
2026-04-03 18:30:38 +0000

    b0dc2fb docs(forms): clarify disabled FormArray value behavior

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.7+sha-ef204bd
+ * @license Angular v21.2.7+sha-b0dc2fb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.7+sha-ef204bd');
+const VERSION = /* @__PURE__ */new Version('21.2.7+sha-b0dc2fb');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
