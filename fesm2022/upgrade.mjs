20d0ef341894c24529aebb9326b3b6be6c94daa0
Andrew Scott <atscott01@gmail.com>
2026-04-03 17:25:43 +0000

    394ad0c fix(forms): allow late-bound input types for signals forms

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.6+sha-9c55fcb
+ * @license Angular v22.0.0-next.6+sha-394ad0c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-9c55fcb');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-394ad0c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
