a0a4170d11cfec4e27d622e8cb2a9d44bbc285cb
Andrew Scott <atscott01@gmail.com>
2026-04-06 18:03:13 +0000

    d95e856 fix(docs-infra): align search result icon with text on mobile

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.6+sha-ecae525
+ * @license Angular v22.0.0-next.6+sha-d95e856
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-ecae525');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-d95e856');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
