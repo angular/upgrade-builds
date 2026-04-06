5e94b6124978fb66d03b56e638bca73b2c708a20
Andrew Scott <atscott01@gmail.com>
2026-04-06 20:33:51 +0000

    e40d378 fix(compiler): handle nested brackets in host object bindings

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.7+sha-d715c36-with-local-changes
+ * @license Angular v21.2.7+sha-e40d378-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.7+sha-d715c36-with-local-changes');
+const VERSION = /* @__PURE__ */new Version('21.2.7+sha-e40d378-with-local-changes');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
