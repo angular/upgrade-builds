0ce5185ab482d441291f20cbc3da4ab6b508e623
Andrew Scott <atscott01@gmail.com>
2026-04-07 16:41:59 +0000

    236d6d4 refactor(devtools): pass `node` to `logValue` to log the appropriate prop to the console

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.6+sha-2ce0e98
+ * @license Angular v22.0.0-next.6+sha-236d6d4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-2ce0e98');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-236d6d4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
