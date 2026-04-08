d598b8d2a3efe9fe62f0ae59030f1f063545e2da
Andrew Scott <atscott01@gmail.com>
2026-04-08 16:50:11 +0000

    c70b4fe docs: fix typos and formatting in spam policy and saved replies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.6+sha-30c950f
+ * @license Angular v22.0.0-next.6+sha-c70b4fe
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-30c950f');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-c70b4fe');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
