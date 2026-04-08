853a00fa01e31ef30847901476756faf5bab8c5b
Andrew Scott <atscott01@gmail.com>
2026-04-08 15:26:29 +0000

    33a30e0 refactor(compiler-cli): Fix regressions caused by ts.typechecker removal

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.7+sha-a385743
+ * @license Angular v21.2.7+sha-33a30e0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.7+sha-a385743');
+const VERSION = /* @__PURE__ */new Version('21.2.7+sha-33a30e0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
