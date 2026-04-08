6dbf8cd3e846ee2adb95a993468b539192314f10
Andrew Scott <atscott01@gmail.com>
2026-04-08 18:40:02 +0000

    057cc6d fix(core): remove obsolete iOS cursor pointer hack in event delegation

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.7+sha-dea76c7
+ * @license Angular v21.2.7+sha-057cc6d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.7+sha-dea76c7');
+const VERSION = /* @__PURE__ */new Version('21.2.7+sha-057cc6d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
