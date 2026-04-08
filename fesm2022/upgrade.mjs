809a816b03e7a6a8febf4daa6e2b2f8379f33927
Andrew Scott <atscott01@gmail.com>
2026-04-08 19:08:49 +0000

    d4c8a9a refactor(compiler-cli): decouple SymbolBuilder from BoundTarget and minimize adapter surface

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.7+sha-057cc6d
+ * @license Angular v21.2.7+sha-d4c8a9a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.7+sha-057cc6d');
+const VERSION = /* @__PURE__ */new Version('21.2.7+sha-d4c8a9a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
