80aa398b12f7d1452e5caf6c525cde2de4d5e0b4
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-09 15:19:18 +0000

    b9265cc docs: update typescript version requirement for angular 22

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.7+sha-1a16a40
+ * @license Angular v22.0.0-next.7+sha-b9265cc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-1a16a40');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-b9265cc');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
