b8491c256f1db390fa3f8cc8ab26e477d31c503d
GitHub <noreply@github.com>
2026-04-09 18:56:55 +0000

    836094c fix(service-worker): resolve TS 6.0 compatibility for messageerror listener

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.7+sha-68628dd
+ * @license Angular v22.0.0-next.7+sha-836094c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-68628dd');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-836094c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
