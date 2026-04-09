0620a79d7faf16d48d7848b5e55da858331f3902
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-09 15:32:52 +0000

    47fcbc4 feat(compiler): allow safe navigation to correctly narrow down nullables

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.7+sha-b9265cc
+ * @license Angular v22.0.0-next.7+sha-47fcbc4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-b9265cc');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-47fcbc4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
