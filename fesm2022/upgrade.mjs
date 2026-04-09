e82c1d72f5c91aeaf8cdbab045117c9202b4fb72
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-09 12:47:27 +0000

    db9f9a8 build: update all non-major dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.7+sha-5516769
+ * @license Angular v22.0.0-next.7+sha-db9f9a8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-5516769');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-db9f9a8');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
