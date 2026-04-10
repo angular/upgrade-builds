5b01e76feee0e2fb0087c42e490b4013c61dce51
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-10 17:30:57 +0000

    1415d86 fix(migrations): Fix typo for strict-template migration Fix typo for strict-template migration

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.7+sha-a268547
+ * @license Angular v22.0.0-next.7+sha-1415d86
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-a268547');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-1415d86');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
