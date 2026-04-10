2b99931855dfde0e21aad2cda3f31a540edeaa01
kirjs <kirts@google.com>
2026-04-10 17:28:33 +0000

    b1407e1 build: update rules_angular setup in MODULE.bazel

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.8+sha-b4a747a
+ * @license Angular v21.2.8+sha-b1407e1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.8+sha-b4a747a');
+const VERSION = /* @__PURE__ */new Version('21.2.8+sha-b1407e1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
