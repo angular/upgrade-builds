dc0803ee3517bb5a69dc6261f8ed7d6b7c24fde4
kirjs <kirts@google.com>
2026-04-10 08:04:50 +0000

    dc95814 docs: add documentation for NG1002

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.8+sha-05d9b97
+ * @license Angular v21.2.8+sha-dc95814
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.8+sha-05d9b97');
+const VERSION = /* @__PURE__ */new Version('21.2.8+sha-dc95814');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
