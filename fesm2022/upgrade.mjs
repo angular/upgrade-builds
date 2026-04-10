f720f115b28164ffc4066bc07e4c9fea523f067b
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-10 14:47:31 +0000

    843f425 docs(docs-infra): fix homepage nav overlay and banner visibility between 701–900px

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.7+sha-1391b68
+ * @license Angular v22.0.0-next.7+sha-843f425
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-1391b68');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-843f425');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
