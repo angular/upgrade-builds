873eeaaf204dceb6d7c27574647dfa6e5568d7bb
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-10 14:09:39 +0000

    1391b68 docs: update cross-repo adev docs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.7+sha-337e6e7
+ * @license Angular v22.0.0-next.7+sha-1391b68
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-337e6e7');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-1391b68');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
