2377a4f3abeba8e32089150c3b82135c453eb25d
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-10 08:04:16 +0000

    0304228 docs: add documentation for NG1002

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.7+sha-836094c
+ * @license Angular v22.0.0-next.7+sha-0304228
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-836094c');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-0304228');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
