2c02c53d2fbdd79b9963658956dcc0c74dcbb402
kirjs <kirts@google.com>
2026-04-13 10:31:53 +0000

    b72b6b4 docs(forms): update signal forms migration guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.8+sha-3ed14c6
+ * @license Angular v21.2.8+sha-b72b6b4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.8+sha-3ed14c6');
+const VERSION = /* @__PURE__ */new Version('21.2.8+sha-b72b6b4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
