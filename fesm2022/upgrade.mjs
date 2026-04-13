75ccb91dd952f5ddf9ea5f2baf98f1cd04e5762b
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 11:18:50 +0000

    8ce9cc4 feat(core): register AI runtime debugging tools

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.7+sha-70368ea
+ * @license Angular v22.0.0-next.7+sha-8ce9cc4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-70368ea');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-8ce9cc4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
