1757211c05dd5eefd1ce8f9fe20504ee228632c2
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 10:30:52 +0000

    f25c7ce docs(forms): update signal forms migration guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.7+sha-5f74e7e
+ * @license Angular v22.0.0-next.7+sha-f25c7ce
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-5f74e7e');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-f25c7ce');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
