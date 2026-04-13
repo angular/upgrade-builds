10cc32c49cb02322fea3d96300deebc2a474b365
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 07:56:39 +0000

    fda8d20 docs(docs-infra): fix essentials next-step navigation pills

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.7+sha-a89b565
+ * @license Angular v22.0.0-next.7+sha-fda8d20
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-a89b565');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-fda8d20');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
