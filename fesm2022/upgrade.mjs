9d0fafa5a9f4fe68f5d768e0c069cab1a3809d2e
kirjs <kirts@google.com>
2026-04-13 08:30:52 +0000

    3ed14c6 refactor: Mobile layout api reference Fix mobile layout shift in API reference

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.8+sha-245bcdd
+ * @license Angular v21.2.8+sha-3ed14c6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.8+sha-245bcdd');
+const VERSION = /* @__PURE__ */new Version('21.2.8+sha-3ed14c6');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
