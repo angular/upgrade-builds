012f1048b5e49f21250bb8259d5ff306c5430913
GitHub <noreply@github.com>
2026-04-13 19:47:49 +0000

    2f5ab54 feat(core): enhance profiling with documentation URLs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.7+sha-f9b74e9
+ * @license Angular v22.0.0-next.7+sha-2f5ab54
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-f9b74e9');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-2f5ab54');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
