43375dcbfaad319f46eeb5648b05e95ff604ad9a
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 08:24:29 +0000

    59513b7 docs: add dev-app section to contributing docs docs: link directly to dev-app `README.md`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.7+sha-c3d4be4
+ * @license Angular v22.0.0-next.7+sha-59513b7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-c3d4be4');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-59513b7');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
