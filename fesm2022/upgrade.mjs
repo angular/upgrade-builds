1b4ca8cfba0051be7efe6f53b8c64362033d5149
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 19:18:17 +0000

    1fa8bb7 refactor(vscode-extension): allow toggle attributes via LS.

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.7+sha-cb19c69
+ * @license Angular v22.0.0-next.7+sha-1fa8bb7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-cb19c69');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-1fa8bb7');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
