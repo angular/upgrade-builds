a496d020dcd1a6239ba9dbdc495b692c41226883
kirjs <kirts@google.com>
2026-04-13 18:29:09 +0000

    17ffa19 docs(devtools): create router tree documentation

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.8+sha-aa5d237
+ * @license Angular v21.2.8+sha-17ffa19
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.8+sha-aa5d237');
+const VERSION = /* @__PURE__ */new Version('21.2.8+sha-17ffa19');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
