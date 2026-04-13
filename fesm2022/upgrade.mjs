0e31c766784df86cba305dc66003329968f03873
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 18:04:39 +0000

    eb2b06f docs: draft PR spam policy addendum

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.7+sha-0454d4c
+ * @license Angular v22.0.0-next.7+sha-eb2b06f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-0454d4c');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-eb2b06f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
