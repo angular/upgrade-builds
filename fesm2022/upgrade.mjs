3e34a71e16baba7d817917f8049b4e1b552b6057
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 16:00:18 +0000

    0454d4c refactor(core): deprecate `withIncrementalHydration`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.7+sha-39e382a
+ * @license Angular v22.0.0-next.7+sha-0454d4c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-39e382a');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-0454d4c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
