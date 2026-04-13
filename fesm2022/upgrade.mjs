ab722a4c086198be61fc549f263a9b021aa63e9c
kirjs <kirts@google.com>
2026-04-13 13:10:32 +0000

    540536c fix(http): add CSP nonce support to JsonpClientBackend

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.8+sha-f603d47
+ * @license Angular v21.2.8+sha-540536c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.8+sha-f603d47');
+const VERSION = /* @__PURE__ */new Version('21.2.8+sha-540536c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
