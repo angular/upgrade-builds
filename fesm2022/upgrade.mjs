7f4079b12acefab53ec3f2c4647d729f5669dfa2
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 11:03:45 +0000

    70368ea refactor(http): Make `BrowserXhr`/`XhrFactory` tree-shakable

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.7+sha-f25c7ce
+ * @license Angular v22.0.0-next.7+sha-70368ea
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-f25c7ce');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-70368ea');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
