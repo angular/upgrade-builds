6b737cb4b9d3a8c019531ae06b4e754c148693b5
kirjs <kirts@google.com>
2026-04-14 15:37:12 +0000

    c3d69ae fix(docs-infra): prevent inline code wrapping in CLI reference table

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.8+sha-684e9fd
+ * @license Angular v21.2.8+sha-c3d69ae
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.8+sha-684e9fd');
+const VERSION = /* @__PURE__ */new Version('21.2.8+sha-c3d69ae');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
