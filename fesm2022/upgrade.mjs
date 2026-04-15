b5ad72b9bffcd2a01b825491417cecf0ecda3a04
kirjs <kirts@google.com>
2026-04-15 16:54:57 +0000

    32a8302 docs: add new signal forms - form submission guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.8+sha-4f65bb3
+ * @license Angular v21.2.8+sha-32a8302
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.8+sha-4f65bb3');
+const VERSION = /* @__PURE__ */new Version('21.2.8+sha-32a8302');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
