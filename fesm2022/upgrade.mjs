101bb9ccd331a9b4ad7f667498797946621bb15d
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-15 21:42:47 +0000

    e14d5ea release: cut the v21.2.9 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.8+sha-528a93a
+ * @license Angular v21.2.9+sha-e14d5ea
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.8+sha-528a93a');
+const VERSION = /* @__PURE__ */new Version('21.2.9+sha-e14d5ea');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
