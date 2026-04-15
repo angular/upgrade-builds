f431d8fbd5a2276939eac64fbaddc8e4258db131
kirjs <kirts@google.com>
2026-04-15 14:40:43 +0000

    4f65bb3 docs: correct "What to learn more about Angular?" to "Want to learn more about Angular?"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.8+sha-c3d69ae
+ * @license Angular v21.2.8+sha-4f65bb3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.8+sha-c3d69ae');
+const VERSION = /* @__PURE__ */new Version('21.2.8+sha-4f65bb3');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
