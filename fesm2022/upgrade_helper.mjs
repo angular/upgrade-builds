f1a64eef0c7f000a97183376c0957b1f1fec79d4
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-15 22:17:59 +0000

    b9ec542 release: cut the v20.3.19 release

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.18+sha-303d4cd
+ * @license Angular v20.3.19+sha-b9ec542
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.18+sha-303d4cd');
+const VERSION = /* @__PURE__ */ new Version('20.3.19+sha-b9ec542');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
