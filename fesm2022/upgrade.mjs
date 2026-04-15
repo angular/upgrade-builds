84d2b71a49f67ff513bb09c250ceb6a9b699e066
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-15 17:16:28 +0000

    281a2db docs: Add guide for debounced signals

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.7+sha-8b4581d
+ * @license Angular v22.0.0-next.7+sha-281a2db
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-8b4581d');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-281a2db');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
