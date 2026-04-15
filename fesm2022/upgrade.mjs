fc5591a16f2b0d5c859dbcec33136e74cfa6bfbe
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-15 19:48:43 +0000

    79c9818 docs(router): document .. traversal and relativeTo pitfalls in router.navigate()

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.7+sha-034b12f
+ * @license Angular v22.0.0-next.7+sha-79c9818
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-034b12f');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-79c9818');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
