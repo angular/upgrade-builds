7774c1796e39d3ab00ca1b93e2c87f3c9ff419a3
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-15 21:45:31 +0000

    05a851e docs: release notes for the v21.2.9 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.7+sha-4e33106
+ * @license Angular v22.0.0-next.7+sha-05a851e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-4e33106');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-05a851e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
