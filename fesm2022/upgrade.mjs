57491c58cef554b521bfea8fff047ff8f3e4a64b
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-15 21:24:00 +0000

    4e33106 feat(core): allow synchronous values for stream Resources

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.7+sha-79c9818
+ * @license Angular v22.0.0-next.7+sha-4e33106
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-79c9818');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-4e33106');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
