c90749e8ccf6498d798e0339eec30fdc2b3fdd4e
kirjs <kirts@google.com>
2026-04-15 19:49:11 +0000

    528a93a docs(router): document .. traversal and relativeTo pitfalls in router.navigate()

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.8+sha-32a8302
+ * @license Angular v21.2.8+sha-528a93a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.8+sha-32a8302');
+const VERSION = /* @__PURE__ */new Version('21.2.8+sha-528a93a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
