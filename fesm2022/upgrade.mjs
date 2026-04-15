520016a65b379c0308726c9633b1cfa726ee1aab
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-15 14:40:43 +0000

    e32159b docs: correct "What to learn more about Angular?" to "Want to learn more about Angular?"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.7+sha-75f2cb8
+ * @license Angular v22.0.0-next.7+sha-e32159b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-75f2cb8');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-e32159b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
