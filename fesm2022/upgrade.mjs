3e0547e85bee680418c6e058cb2af8fa891e76e0
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-15 22:18:48 +0000

    9bead58 docs: release notes for the v20.3.19 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.8+sha-c326548
+ * @license Angular v22.0.0-next.8+sha-9bead58
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-c326548');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-9bead58');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
