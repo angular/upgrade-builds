162383e59f8018e929305967eeb04ac62d2deef5
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-15 22:07:18 +0000

    c326548 release: cut the v22.0.0-next.8 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.7+sha-05a851e
+ * @license Angular v22.0.0-next.8+sha-c326548
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-05a851e');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-c326548');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
