d2d643897b1b6c48651d6bd4273ec2a8ff2e24c8
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-17 21:32:20 +0000

    6d10b8b docs: add inject to structural directive imports

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.9+sha-c738d45
+ * @license Angular v21.2.9+sha-6d10b8b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.9+sha-c738d45');
+const VERSION = /* @__PURE__ */new Version('21.2.9+sha-6d10b8b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
