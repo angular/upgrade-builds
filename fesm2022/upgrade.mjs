af952dc229b421a9c6c91e76c3a41ad6e32a6e32
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 16:59:57 +0000

    c610425 docs: fix wording in reactive forms guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.8+sha-a718e18
+ * @license Angular v22.0.0-next.8+sha-c610425
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-a718e18');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-c610425');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
