fc9b2f8b7bccce604eebec7b0e38bcd4d8b15cdc
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 20:39:51 +0000

    a9ddc5d docs: warn against storing secrets in environment files

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.9+sha-c9215b3
+ * @license Angular v21.2.9+sha-a9ddc5d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.9+sha-c9215b3');
+const VERSION = /* @__PURE__ */new Version('21.2.9+sha-a9ddc5d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
