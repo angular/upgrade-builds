875a390937dcc0141bdc1d9ae6e8469da1bdaa06
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 23:55:47 +0000

    6eff439 fix(router): restore internal URL on popstate when `browserUrl` is used

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.8+sha-d27e2c2
+ * @license Angular v22.0.0-next.8+sha-6eff439
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-d27e2c2');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-6eff439');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
