5dccfd69f0ee441e1b0ba2ca8de1627d5fb5146c
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 20:29:04 +0000

    c04c0b9 docs(docs-infra): guard sandbox reset before initialization in playground

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.8+sha-3ae40e6
+ * @license Angular v22.0.0-next.8+sha-c04c0b9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-3ae40e6');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-c04c0b9');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
