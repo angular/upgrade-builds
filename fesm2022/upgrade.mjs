410e1cccc8a14ac1f3a24271fe7bf38ac6db94c7
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 16:37:52 +0000

    a718e18 docs: improve clarity in dynamic forms guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.8+sha-a0d4563
+ * @license Angular v22.0.0-next.8+sha-a718e18
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-a0d4563');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-a718e18');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
