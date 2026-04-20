ee35c027e074cc060311a295e592269dce6c6dba
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 20:04:06 +0000

    c9215b3 Revert "refactor(core): complete removal of deprecated `createNgModuleRef` alias"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.9+sha-e5b93ea
+ * @license Angular v21.2.9+sha-c9215b3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.9+sha-e5b93ea');
+const VERSION = /* @__PURE__ */new Version('21.2.9+sha-c9215b3');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
