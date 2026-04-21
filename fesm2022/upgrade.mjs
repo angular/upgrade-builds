7b9e2af4dbb3d6e6c47a6ac2015292a5b3a98c8a
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-21 16:09:09 +0000

    fa6a3d2 docs: update builder docs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.8+sha-6eff439
+ * @license Angular v22.0.0-next.8+sha-fa6a3d2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-6eff439');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-fa6a3d2');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
