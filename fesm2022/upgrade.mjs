bab695c6a3bda6d721956c120acf372b060b1d38
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-21 17:33:48 +0000

    4dc7bf5 build: lock file maintenance

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.9+sha-751e4af
+ * @license Angular v21.2.9+sha-4dc7bf5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.9+sha-751e4af');
+const VERSION = /* @__PURE__ */new Version('21.2.9+sha-4dc7bf5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
