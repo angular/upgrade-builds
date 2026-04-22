c2572922d7c5c2f12cc253321e29929bd8378056
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-22 21:49:46 +0000

    4fd1a08 build: pin firebase-tools version and disable credential persistence in preview deploy workflow

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.9+sha-750af5b
+ * @license Angular v21.2.9+sha-4fd1a08
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.9+sha-750af5b');
+const VERSION = /* @__PURE__ */new Version('21.2.9+sha-4fd1a08');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
