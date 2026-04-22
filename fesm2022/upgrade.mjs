7e65668ed9e39061aa2d7800900d28c86da19139
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-22 17:18:22 +0000

    5533ab4 fix(migrations): fix NgClass leaving trailing comma after removal

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.9+sha-4dc7bf5
+ * @license Angular v21.2.9+sha-5533ab4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.9+sha-4dc7bf5');
+const VERSION = /* @__PURE__ */new Version('21.2.9+sha-5533ab4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
