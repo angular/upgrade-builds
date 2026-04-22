22f0bf1038c90e2f5b7557ecdcdc93550b33faf5
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-22 18:22:29 +0000

    2c141c0 refactor(compiler-cli): simplify Angular decorator stripping

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.8+sha-b395173
+ * @license Angular v22.0.0-next.8+sha-2c141c0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-b395173');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.8+sha-2c141c0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
