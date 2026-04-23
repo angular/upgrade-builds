89ed91e7713b9b30fe4fc756fd7b86c9f148cc1d
Alon Mishne <amishne@users.noreply.github.com>
2026-04-23 18:49:10 +0000

    a8dd801 docs: link Angular CLI in installation prerequisites

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.10+sha-2ec753c
+ * @license Angular v21.2.10+sha-a8dd801
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.10+sha-2ec753c');
+const VERSION = /* @__PURE__ */new Version('21.2.10+sha-a8dd801');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
