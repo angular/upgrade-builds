7053ae8f6a46bf2254d83aa35f3488f2e68b89a9
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-24 17:18:54 +0000

    adbd1ab docs: update service documentation to include `@Service` decorator details

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.9+sha-65c205d
+ * @license Angular v22.0.0-next.9+sha-adbd1ab
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-65c205d');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-adbd1ab');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
