d0ac37a8ae51f2ea7d1398e0858d53cda8d4b85a
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-24 17:18:14 +0000

    8ce8b93 build: update pnpm to v10.33.2

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.10+sha-f3308dc
+ * @license Angular v21.2.10+sha-8ce8b93
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.10+sha-f3308dc');
+const VERSION = /* @__PURE__ */new Version('21.2.10+sha-8ce8b93');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
