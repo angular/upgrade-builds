6d09c80b560fa33e5b9af2f25f16c472e56e776a
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-24 17:41:24 +0000

    29081f7 docs(docs-infra): improve Playground card on installation page

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.9+sha-adbd1ab
+ * @license Angular v22.0.0-next.9+sha-29081f7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-adbd1ab');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-29081f7');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
