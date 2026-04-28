ba49426c8728c4776968ea31dc2a3bb3711e9401
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-28 00:26:17 +0000

    c8aad6a docs: remove duplicated text

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.9+sha-357cb15
+ * @license Angular v22.0.0-next.9+sha-c8aad6a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-357cb15');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-c8aad6a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
