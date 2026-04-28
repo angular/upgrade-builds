46e621653f70c652188f8c61418bd0b7cfb1cc27
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-28 16:51:25 +0000

    9bcbf37 refactor(core): fix bundling symbol test

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.10+sha-a3d14fc
+ * @license Angular v21.2.10+sha-9bcbf37
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.10+sha-a3d14fc');
+const VERSION = /* @__PURE__ */new Version('21.2.10+sha-9bcbf37');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
