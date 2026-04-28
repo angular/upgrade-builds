8fcb15fe14fe36387d1ffe5ecfb838c51fbf2266
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-28 17:17:01 +0000

    1bfdae9 docs: document NG_TRUST_PROXY_HEADERS environment variable for AngularNodeAppEngine

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.9+sha-c8aad6a
+ * @license Angular v22.0.0-next.9+sha-1bfdae9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-c8aad6a');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-1bfdae9');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
