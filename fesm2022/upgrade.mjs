5a5745a72610c76e3546d4c0dcde79e70449e88c
Alon Mishne <amishne@users.noreply.github.com>
2026-04-28 19:49:09 +0000

    10ad3c0 fix(common): prevent focus from scrollToAnchor

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.10+sha-d07f502
+ * @license Angular v21.2.10+sha-10ad3c0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.10+sha-d07f502');
+const VERSION = /* @__PURE__ */new Version('21.2.10+sha-10ad3c0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
