104d6c6ff3e0ca624a1ba8102f3e33fb0a4dec49
GitHub <noreply@github.com>
2026-04-28 22:48:55 +0000

    2896c93 feat(compiler): Angular expressions with optional chaining returns `undefined`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.9+sha-ef38017
+ * @license Angular v22.0.0-next.9+sha-2896c93
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-ef38017');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-2896c93');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
