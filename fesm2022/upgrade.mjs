64c5cc58edc0abcfe71adb6d26f506b64b17ea0d
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-28 20:14:29 +0000

    ef38017 build: move devtools only deps out of the workspace

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.9+sha-8f8972b
+ * @license Angular v22.0.0-next.9+sha-ef38017
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-8f8972b');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-ef38017');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
