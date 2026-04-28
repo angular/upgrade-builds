68302fecbdeb6fa506cd01f60ac756187ee334fa
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-28 23:48:36 +0000

    42d57c3 refactor(common): fix viewport tests

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.10+sha-10ad3c0
+ * @license Angular v21.2.10+sha-42d57c3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.10+sha-10ad3c0');
+const VERSION = /* @__PURE__ */new Version('21.2.10+sha-42d57c3');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
