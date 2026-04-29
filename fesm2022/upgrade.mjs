850a2b43c40769a37fdd2fe1355d7e10dce71f37
Alon Mishne <amishne@users.noreply.github.com>
2026-04-29 21:08:38 +0000

    6c6db5b docs(docs-infra): open update guide external links in new tabs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.10+sha-631e303
+ * @license Angular v21.2.10+sha-6c6db5b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.10+sha-631e303');
+const VERSION = /* @__PURE__ */new Version('21.2.10+sha-6c6db5b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
