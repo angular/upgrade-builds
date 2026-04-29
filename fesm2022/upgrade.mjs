d733174938197a4b92c464ff6c2d80e34167794d
Alon Mishne <amishne@users.noreply.github.com>
2026-04-29 20:51:27 +0000

    631e303 docs: improve clarity in dependency injection guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.10+sha-42d57c3
+ * @license Angular v21.2.10+sha-631e303
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.10+sha-42d57c3');
+const VERSION = /* @__PURE__ */new Version('21.2.10+sha-631e303');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
