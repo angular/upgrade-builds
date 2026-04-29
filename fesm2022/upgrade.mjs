ea8af607682a21eef5b53ec7f0410a4ebfd5ce69
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-29 20:51:25 +0000

    af24a4a fix(vscode-extension): Look for tsdk override in the new js/ts.tsdk.path setting

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.9+sha-37ec63e
+ * @license Angular v22.0.0-next.9+sha-af24a4a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-37ec63e');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-af24a4a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
