ae061bd246861073e6d74165203e5e1b94ff0a79
Alon Mishne <amishne@users.noreply.github.com>
2026-04-29 23:21:39 +0000

    1aeebbe fix(core): respect ngSkipHydration on components with projectable nodes in LContainers

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.11+sha-ef9669c
+ * @license Angular v21.2.11+sha-1aeebbe
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.11+sha-ef9669c');
+const VERSION = /* @__PURE__ */new Version('21.2.11+sha-1aeebbe');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
