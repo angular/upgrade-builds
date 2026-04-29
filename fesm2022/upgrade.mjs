d35cfeb03f5521862678327e8ba70d31eb08f309
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-29 17:11:27 +0000

    37ec63e refactor(compiler): introduce default constant for legacyOptionalChaining flag

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.9+sha-2896c93
+ * @license Angular v22.0.0-next.9+sha-37ec63e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-2896c93');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-37ec63e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
