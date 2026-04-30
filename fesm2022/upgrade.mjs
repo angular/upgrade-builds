391168ddc8d8cb475d97681969b2ce7570e83ec8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-04-30 23:04:30 +0000

    ebffd80 build: update all github actions

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.10+sha-4c9afb6
+ * @license Angular v22.0.0-next.10+sha-ebffd80
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.10+sha-4c9afb6');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.10+sha-ebffd80');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
