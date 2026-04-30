86189a829820ff80d33704db61538d146ffea15a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-04-30 23:20:48 +0000

    2b62458 ci: update PullApprove config

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.10+sha-ebffd80
+ * @license Angular v22.0.0-next.10+sha-2b62458
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.10+sha-ebffd80');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.10+sha-2b62458');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
