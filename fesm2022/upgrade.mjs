8bb456066e07001dafcc2b2410d390fe303a050b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-01 18:13:49 +0000

    a4145de Revert "fix(language-service): Add support for `@Input` with transforms"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.10+sha-2b62458
+ * @license Angular v22.0.0-next.10+sha-a4145de
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.10+sha-2b62458');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.10+sha-a4145de');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
