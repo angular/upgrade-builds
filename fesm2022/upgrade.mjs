3b63d1dd80670c7159ef95115e9788252768fc36
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-01 23:12:10 +0000

    c84642a feat(router): add unmatchedInputBehavior option to componentInputBinding

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.10+sha-a4145de
+ * @license Angular v22.0.0-next.10+sha-c84642a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.10+sha-a4145de');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.10+sha-c84642a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
