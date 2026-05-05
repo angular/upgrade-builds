a5ac3d582ce8c20b638ab9b05ef39ce082c1f531
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-05 19:53:53 +0000

    8a7f955 docs: correct "Angular JS" to "AngularJS"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.10+sha-708631f
+ * @license Angular v22.0.0-next.10+sha-8a7f955
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.10+sha-708631f');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.10+sha-8a7f955');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
