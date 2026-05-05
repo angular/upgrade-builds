39ac9d277eb47e2671e6a7bf46763df86cc53974
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-05 18:24:12 +0000

    708631f fix(forms): prohibit concurrent submits in signal forms

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.10+sha-fc52633
+ * @license Angular v22.0.0-next.10+sha-708631f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.10+sha-fc52633');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.10+sha-708631f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
