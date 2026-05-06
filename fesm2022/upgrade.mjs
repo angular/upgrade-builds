f54d9d56cb3dcc00042f8e0a7e308b7db059edf3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-06 23:51:31 +0000

    e3dc87c release: cut the zone.js-0.16.2 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-853a91e
+ * @license Angular v22.1.0-next.0+sha-e3dc87c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-853a91e');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-e3dc87c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
