38192de04a20bb20128b9d1f1a72d796e20207ac
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-06 22:57:11 +0000

    5316753 release: cut the v21.2.12 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.11+sha-9e38ed7
+ * @license Angular v21.2.12+sha-5316753
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.11+sha-9e38ed7');
+const VERSION = /* @__PURE__ */new Version('21.2.12+sha-5316753');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
