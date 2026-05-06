d3140eb098d14b45d262e636b9d35cb62f1802aa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-06 21:51:05 +0000

    25e4e07 fix(platform-server): ensure origin has a trailing slash when parsing url (#68468)

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.19+sha-b9ec542
+ * @license Angular v20.3.19+sha-25e4e07
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.19+sha-b9ec542');
+const VERSION = /* @__PURE__ */ new Version('20.3.19+sha-25e4e07');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
