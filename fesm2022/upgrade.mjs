9619846c28093528ef3bdf89df46e92f8bb848de
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-06 22:57:40 +0000

    8b3973a docs: release notes for the v21.2.12 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.10+sha-a02ac76
+ * @license Angular v22.0.0-next.10+sha-8b3973a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.10+sha-a02ac76');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.10+sha-8b3973a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
