b68b682168c9ad8a7a04b9158227ab07a9de0601
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-06 00:15:51 +0000

    9e38ed7 fix(core): sanitizer typings

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.11+sha-1aeebbe
+ * @license Angular v21.2.11+sha-9e38ed7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.11+sha-1aeebbe');
+const VERSION = /* @__PURE__ */new Version('21.2.11+sha-9e38ed7');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
