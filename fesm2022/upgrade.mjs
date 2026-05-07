eb11a6ddd46b4888d4bf2bccb6fa15691f48e419
Matthew Beck <me@mattrb.com>
2026-05-07 22:38:22 +0000

    629905d fix(platform-server): add `allowedHosts` option to `renderModule` and `renderApplication`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.12+sha-5316753
+ * @license Angular v21.2.12+sha-629905d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.12+sha-5316753');
+const VERSION = /* @__PURE__ */new Version('21.2.12+sha-629905d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
