370c6d8eefec376d332f2fc647914b500df23de4
Matthew Beck <me@mattrb.com>
2026-05-08 21:17:54 +0000

    119a19e fix(platform-server): forward BEFORE_APP_SERIALIZED errors to ErrorHandler

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.12+sha-1d621ea
+ * @license Angular v22.0.0-next.12+sha-119a19e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-1d621ea');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-119a19e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
