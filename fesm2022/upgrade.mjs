78783a06bab462d533651877ad36ec2bfe6bddbf
Matthew Beck (Berry) <me@mattrb.com>
2026-05-08 15:30:51 +0000

    38b2bed docs: release notes for the v22.0.0-next.12 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-c72ebcb
+ * @license Angular v22.1.0-next.0+sha-38b2bed
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-c72ebcb');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-38b2bed');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
