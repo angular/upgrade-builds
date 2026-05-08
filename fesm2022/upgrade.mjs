96edf382af13aef85becc7785c9c1bd5df91432d
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-05-08 08:24:28 +0000

    8ebae1d fix(core): allow service with factory on abstract classes

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.11+sha-609f930
+ * @license Angular v22.0.0-next.11+sha-8ebae1d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.11+sha-609f930');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.11+sha-8ebae1d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
