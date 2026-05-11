1ec21483b866d734dfcb1ef6c936f2edf4a147e1
Matthew Beck (Berry) <me@mattrb.com>
2026-05-11 19:17:38 +0000

    ef134ac docs: add new signal forms field metadata guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-538d0a8
+ * @license Angular v22.1.0-next.0+sha-ef134ac
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-538d0a8');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-ef134ac');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
