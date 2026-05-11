dc4691a916fcdd1f941ad329c1bcbf839633b9a9
Matthew Beck <me@mattrb.com>
2026-05-11 19:52:56 +0000

    49a133a refactor(compiler-cli): drop @ts-ignore around jsDocParsingMode

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.12+sha-c93d158
+ * @license Angular v21.2.12+sha-49a133a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.12+sha-c93d158');
+const VERSION = /* @__PURE__ */new Version('21.2.12+sha-49a133a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
