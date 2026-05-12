86a094087c53c5827d04ff638d0d9864ef045652
Matthew Beck (Berry) <me@mattrb.com>
2026-05-12 19:20:11 +0000

    cdef73a build: update @angular/ng-dev

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-66b9259
+ * @license Angular v22.1.0-next.0+sha-cdef73a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-66b9259');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-cdef73a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
