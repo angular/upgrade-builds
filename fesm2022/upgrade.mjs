c05f3b25ac2229d8a819449aab1a6cc8fd284931
Matthew Beck (Berry) <me@mattrb.com>
2026-05-12 20:31:50 +0000

    e0593ec refactor(compiler-cli): drop @ts-ignore around fs.readFileSync

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-cdef73a
+ * @license Angular v22.1.0-next.0+sha-e0593ec
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-cdef73a');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-e0593ec');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
