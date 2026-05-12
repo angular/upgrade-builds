548f43dff12a22427f107e764585d98f04c7cbcb
Matthew Beck (Berry) <me@mattrb.com>
2026-05-12 17:59:46 +0000

    a617967 refactor(http): update HTTP resource options APIs to stable

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-62710ad
+ * @license Angular v22.1.0-next.0+sha-a617967
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-62710ad');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-a617967');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
