05c87505d6a69528cdad0dc32bdc0b5d966bac15
Matthew Beck <me@mattrb.com>
2026-05-12 01:41:25 +0000

    e9d1c7e refactor(router): Move target RouterState creation before 'blocking' stage

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.12+sha-8391cf0
+ * @license Angular v22.0.0-next.12+sha-e9d1c7e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-8391cf0');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-e9d1c7e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
