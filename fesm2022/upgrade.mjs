043a23366d3faba266159e02557ca0a18ff5db6a
Matthew Beck <me@mattrb.com>
2026-05-12 17:56:49 +0000

    7ab78d5 ci: mark devversion as unavailable

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.12+sha-49a133a
+ * @license Angular v21.2.12+sha-7ab78d5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.12+sha-49a133a');
+const VERSION = /* @__PURE__ */new Version('21.2.12+sha-7ab78d5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
