3a63105dfbd424423b5c6eb99ccd13e23a1c4376
Matthew Beck (Berry) <me@mattrb.com>
2026-05-13 23:33:09 +0000

    8282c09 release: cut the v21.2.13 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.12+sha-1e079a8
+ * @license Angular v21.2.13+sha-8282c09
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.12+sha-1e079a8');
+const VERSION = /* @__PURE__ */new Version('21.2.13+sha-8282c09');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
