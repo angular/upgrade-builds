2412e0e62c6c25332d6be4c54591dcf92cc6aa79
Matthew Beck <me@mattrb.com>
2026-05-13 19:31:12 +0000

    1f0c1ab refactor(core): patch special provider classes with __NG_ELEMENT_ID__

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.12+sha-ccb7d42
+ * @license Angular v22.0.0-next.12+sha-1f0c1ab
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-ccb7d42');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-1f0c1ab');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
