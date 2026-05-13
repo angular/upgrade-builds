79f057be3cee51094e04a4b0718963f99b247baf
Matthew Beck <me@mattrb.com>
2026-05-13 22:45:53 +0000

    639a785 docs: add version 22 to the browser support reference table

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-next.12+sha-1f0c1ab
+ * @license Angular v22.0.0-next.12+sha-639a785
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-1f0c1ab');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-639a785');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
