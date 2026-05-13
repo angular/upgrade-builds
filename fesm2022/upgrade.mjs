2737a970ac1b9d589dcc4b3223067b3afed46a5d
Matthew Beck (Berry) <me@mattrb.com>
2026-05-13 23:04:40 +0000

    7557b33 refactor(core): mark resource() overloads as @publicApi 22.0

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-dbf3853
+ * @license Angular v22.1.0-next.0+sha-7557b33
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-dbf3853');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-7557b33');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
