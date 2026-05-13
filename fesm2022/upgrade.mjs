f5513a58cd7f8efcba95f51898cdbadf9ee128e5
Matthew Beck (Berry) <me@mattrb.com>
2026-05-13 18:40:43 +0000

    06f6dec fix(compiler): type check invalid for loops

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-739bd37
+ * @license Angular v22.1.0-next.0+sha-06f6dec
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-739bd37');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-06f6dec');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
