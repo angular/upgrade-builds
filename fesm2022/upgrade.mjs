dbb6d3b03e8626e61e6c0e41cd8262b42ff072de
Matthew Beck (Berry) <me@mattrb.com>
2026-05-13 22:44:56 +0000

    dbf3853 docs: add version 22 to the browser support reference table

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-ec63947
+ * @license Angular v22.1.0-next.0+sha-dbf3853
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-ec63947');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-dbf3853');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
