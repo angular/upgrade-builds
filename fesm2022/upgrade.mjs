ffa5dedfd2323a1f0a69833f10f57367b5b69ab4
leonsenft <leonsenft@users.noreply.github.com>
2026-05-15 19:26:18 +0000

    08d5017 docs: use when in signal forms rule examples

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-rc.0+sha-defb172
+ * @license Angular v22.0.0-rc.0+sha-08d5017
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.0+sha-defb172');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.0+sha-08d5017');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
