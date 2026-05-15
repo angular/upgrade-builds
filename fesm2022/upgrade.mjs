9e07c4c73c92d5c301b29cbafd0641c3b622183f
leonsenft <leonsenft@users.noreply.github.com>
2026-05-15 17:54:04 +0000

    4dd9839 docs: move Angular 19 to unsupported versions

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-rc.0+sha-5ca0c5a
+ * @license Angular v22.0.0-rc.0+sha-4dd9839
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.0+sha-5ca0c5a');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.0+sha-4dd9839');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
