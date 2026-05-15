b6b1017021d351b9aa3513588fe117ca272a9035
leonsenft <leonsenft@users.noreply.github.com>
2026-05-15 17:52:07 +0000

    fc434c1 refactor(compiler-cli): Remove unused properties of IndexedComponent interface

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.13+sha-8282c09
+ * @license Angular v21.2.13+sha-fc434c1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.13+sha-8282c09');
+const VERSION = /* @__PURE__ */new Version('21.2.13+sha-fc434c1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
