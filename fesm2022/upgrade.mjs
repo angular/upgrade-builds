4df8f184df808a99f94eb1f02cdc881e9e4abdae
leonsenft <leonsenft@users.noreply.github.com>
2026-05-18 20:42:16 +0000

    c0f5227 fix(core): do not insert todo when migrating void @Output

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.13+sha-fc434c1
+ * @license Angular v21.2.13+sha-c0f5227
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.13+sha-fc434c1');
+const VERSION = /* @__PURE__ */new Version('21.2.13+sha-c0f5227');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
