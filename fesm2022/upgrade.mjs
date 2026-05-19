8962373977e88b38d1104948c4ba0af3843abace
leonsenft <leonsenft@users.noreply.github.com>
2026-05-19 21:10:02 +0000

    1f71ebd fix(core): visit ICU expressions in signal migration schematics

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-rc.0+sha-0e1ec0c
+ * @license Angular v22.0.0-rc.0+sha-1f71ebd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.0+sha-0e1ec0c');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.0+sha-1f71ebd');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
