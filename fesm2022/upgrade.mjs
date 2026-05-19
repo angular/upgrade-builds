39bb3f9683c8aafdc74197396f6be6af990cdc4a
leonsenft <leonsenft@users.noreply.github.com>
2026-05-19 20:50:18 +0000

    027c7c0 docs: update HTTP testing setup guidance

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.13+sha-7f444e1
+ * @license Angular v21.2.13+sha-027c7c0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.13+sha-7f444e1');
+const VERSION = /* @__PURE__ */new Version('21.2.13+sha-027c7c0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
