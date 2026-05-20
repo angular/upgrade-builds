161d69b6464efd1f59cc5377572ef09a2751465f
leonsenft <leonsenft@users.noreply.github.com>
2026-05-20 20:43:08 +0000

    232b21d docs: document content projection limitations

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.13+sha-1d6e71d
+ * @license Angular v21.2.13+sha-232b21d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.13+sha-1d6e71d');
+const VERSION = /* @__PURE__ */new Version('21.2.13+sha-232b21d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
