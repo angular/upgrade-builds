eaeee1314a8ef3c6a7bd821ae9382135be40c23e
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-20 21:19:21 +0000

    04f31cc docs: link to ng new reference from installation guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-de9e3d1
+ * @license Angular v22.1.0-next.0+sha-04f31cc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-de9e3d1');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-04f31cc');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
