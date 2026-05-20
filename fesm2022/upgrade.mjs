d9224232cd64d9698974ac55dad0b6363acde55a
leonsenft <leonsenft@users.noreply.github.com>
2026-05-20 21:20:18 +0000

    9627e02 docs: link to ng new reference from installation guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.13+sha-1b6f780
+ * @license Angular v21.2.13+sha-9627e02
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.13+sha-1b6f780');
+const VERSION = /* @__PURE__ */new Version('21.2.13+sha-9627e02');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
