20fa88daf61c2af3ef2b906dbcbaef275f5cb0a8
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-20 23:00:10 +0000

    fd6ef34 docs: release notes for the v22.0.0-rc.1 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-16195d0
+ * @license Angular v22.1.0-next.0+sha-fd6ef34
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-16195d0');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-fd6ef34');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
