a6d9dbfd3123e7faeff8169b5c0c1c58dd9cd2a5
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-20 22:43:02 +0000

    3fe8562 release: cut the v21.2.14 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.13+sha-9627e02
+ * @license Angular v21.2.14+sha-3fe8562
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.13+sha-9627e02');
+const VERSION = /* @__PURE__ */new Version('21.2.14+sha-3fe8562');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
