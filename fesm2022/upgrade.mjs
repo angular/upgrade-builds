5bd73ff1b591eb7d200d7a2eabc44ccecfec0a3d
leonsenft <leonsenft@users.noreply.github.com>
2026-05-20 17:42:40 +0000

    83026cf fix(docs-infra): make absolute angular.dev hrefs relative in CLI option descriptions

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-rc.0+sha-3ff5536
+ * @license Angular v22.0.0-rc.0+sha-83026cf
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.0+sha-3ff5536');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.0+sha-83026cf');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
