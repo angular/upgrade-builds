f6960fbb3ff0932e515045c0d7648c9491edce9b
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-20 17:58:37 +0000

    cb95723 build: update bazel dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-745ee71
+ * @license Angular v22.1.0-next.0+sha-cb95723
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-745ee71');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-cb95723');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
