a720aabdc2773364e0b0d85921c5085da220fbf8
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-20 01:41:08 +0000

    5993c47 test(core): remove obsolete SVG script sanitization translation test

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-rc.0+sha-1f71ebd
+ * @license Angular v22.0.0-rc.0+sha-5993c47
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.0+sha-1f71ebd');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.0+sha-5993c47');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
