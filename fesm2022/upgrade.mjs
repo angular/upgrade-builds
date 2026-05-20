52ee197c1308e371493ebdd60047853381d474b5
leonsenft <leonsenft@users.noreply.github.com>
2026-05-20 18:31:32 +0000

    1d6e71d docs: clarify ngDoCheck invocation behavior with OnPush strategy

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.13+sha-0c7f70e
+ * @license Angular v21.2.13+sha-1d6e71d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.13+sha-0c7f70e');
+const VERSION = /* @__PURE__ */new Version('21.2.13+sha-1d6e71d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
