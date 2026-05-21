d2477d249ef2132527d3b040f9e87ecd2f02f641
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-21 18:10:30 +0000

    3b4ef1e perf(forms): avoid redundant invalidations in parser errors signal

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-rc.1+sha-d8a848b
+ * @license Angular v22.0.0-rc.1+sha-3b4ef1e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-d8a848b');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-3b4ef1e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
