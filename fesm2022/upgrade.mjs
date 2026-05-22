0dadff5e95b275f9a5e5984daae9c654d3241c02
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-22 21:32:32 +0000

    525e160 release: bump VSCode extension version to 21.2.4

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.14+sha-4a41831
+ * @license Angular v21.2.14+sha-525e160
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.14+sha-4a41831');
+const VERSION = /* @__PURE__ */new Version('21.2.14+sha-525e160');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
