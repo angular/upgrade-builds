84751de532d46c8961ed9f9c45e4aa05be248ac8
GitHub <noreply@github.com>
2026-05-22 21:11:46 +0000

    06b004e refactor(compiler): add support for compiling NgModules under isolatedDeclarations

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-df68a96
+ * @license Angular v22.1.0-next.0+sha-06b004e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-df68a96');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-06b004e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
