0ef64f6336aad20c64872537974e80c605c6fd24
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-22 17:07:41 +0000

    c08f22a refactor(vscode-extension): Remove effectively dead code

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-rc.1+sha-3b4ef1e
+ * @license Angular v22.0.0-rc.1+sha-c08f22a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-3b4ef1e');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-c08f22a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
