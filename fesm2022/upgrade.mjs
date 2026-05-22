f6786bc7b3e27aa0f153a25e7d38ed0806af1832
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-22 21:16:12 +0000

    2200b4a refactor(compiler): add support for compiling NgModules under isolatedDeclarations

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-rc.1+sha-b25964b
+ * @license Angular v22.0.0-rc.1+sha-2200b4a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-b25964b');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-2200b4a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
