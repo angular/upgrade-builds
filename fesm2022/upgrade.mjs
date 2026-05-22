6dc33cf4e53b3d71e3cb26174fec3a4371681e16
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-22 19:32:00 +0000

    b25964b fix(vscode-extension): disable language server in untrusted workspaces

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-rc.1+sha-c08f22a
+ * @license Angular v22.0.0-rc.1+sha-b25964b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-c08f22a');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-b25964b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
