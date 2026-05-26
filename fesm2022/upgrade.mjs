bf2ef4f3412218b6a074f1907958834b198d9669
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-26 23:17:26 +0000

    4f9ee3c refactor: add g3-only and 3p-only markers to replace specific g3 patches

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-06b004e
+ * @license Angular v22.1.0-next.0+sha-4f9ee3c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-06b004e');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-4f9ee3c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
