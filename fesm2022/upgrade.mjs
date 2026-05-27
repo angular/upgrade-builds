04d5720eee063c2b5b2b3ed18e2e8901d51b7c9e
GitHub <noreply@github.com>
2026-05-27 17:39:08 +0000

    140c4d0 fix(platform-server): prevent SSRF bypasses via backslash URLs in HttpClient

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-4f9ee3c
+ * @license Angular v22.1.0-next.0+sha-140c4d0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-4f9ee3c');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-140c4d0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
