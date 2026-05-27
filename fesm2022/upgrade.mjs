52ca26405a0e18b2e20c5db42a990fb5490670b1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 20:26:02 +0000

    e6cfaf5 fix(http): prevent `httpResource` from leaking a subscription

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-rc.1+sha-d9c38e5
+ * @license Angular v22.0.0-rc.1+sha-e6cfaf5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-d9c38e5');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-e6cfaf5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
