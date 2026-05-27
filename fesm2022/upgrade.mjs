a61b11b40c2dafcb9c97feffefc882cd76ac2f7c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 21:27:04 +0000

    34090cb fix(http): exclude withCredentials requests from transfer cache

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-6388675
+ * @license Angular v22.1.0-next.0+sha-34090cb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-6388675');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-34090cb');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
