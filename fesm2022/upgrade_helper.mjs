70772287e857a76f26371a961c5a02d276d153de
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 18:14:18 +0000

    e617fa0 fix(service-worker): Preserves HTTP cache mode in asset group requests

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.21+sha-3d135ce
+ * @license Angular v20.3.21+sha-e617fa0
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.21+sha-3d135ce');
+const VERSION = /* @__PURE__ */ new Version('20.3.21+sha-e617fa0');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
