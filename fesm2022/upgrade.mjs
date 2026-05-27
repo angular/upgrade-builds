edd8cb902953f3f2d14d62a2b96edb7ad933f970
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 18:39:56 +0000

    6f56202 docs: fix typos in source code comments

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-140c4d0
+ * @license Angular v22.1.0-next.0+sha-6f56202
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-140c4d0');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-6f56202');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
