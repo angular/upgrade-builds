91ea9ed438d6dc32010ed4434bb7dbd9211b68a6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-28 14:15:34 +0000

    6b8202e refactor(docs-infra): extract magic 27 in navigation-list tooltip threshold

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.14+sha-eb1cbbf
+ * @license Angular v21.2.14+sha-6b8202e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.14+sha-eb1cbbf');
+const VERSION = /* @__PURE__ */new Version('21.2.14+sha-6b8202e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
