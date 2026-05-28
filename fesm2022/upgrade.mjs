97145780c14ed4112fceb8a0ed624353e34a72ef
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-28 17:01:02 +0000

    cea6588 release: cut the v21.2.15 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.14+sha-6b8202e
+ * @license Angular v21.2.15+sha-cea6588
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.14+sha-6b8202e');
+const VERSION = /* @__PURE__ */new Version('21.2.15+sha-cea6588');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
