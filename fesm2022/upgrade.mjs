4c0763aa6a0ca6dc8423728aabb3567edecb4e6b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-28 14:20:22 +0000

    c1cefe9 build: update bazel dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-rc.1+sha-94d520f
+ * @license Angular v22.0.0-rc.1+sha-c1cefe9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-94d520f');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-c1cefe9');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
