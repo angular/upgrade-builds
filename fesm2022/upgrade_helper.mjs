8e379184283201369046eaaa26f72d8ca9cab36e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-28 14:34:30 +0000

    a1eeeb8 release: cut the v20.3.23 release

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.22+sha-d40acc6
+ * @license Angular v20.3.23+sha-a1eeeb8
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.22+sha-d40acc6');
+const VERSION = /* @__PURE__ */ new Version('20.3.23+sha-a1eeeb8');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
