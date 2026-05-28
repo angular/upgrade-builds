7e7d105c0dfe2a8c8a5e27f3059e8f98cfb5be3b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-28 11:54:53 +0000

    7b4b067 docs: fix grammar issues in resource guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-rc.1+sha-d88b796
+ * @license Angular v22.0.0-rc.1+sha-7b4b067
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-d88b796');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-7b4b067');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
