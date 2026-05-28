46ad56fe6bda1c63e227fc1984acf82a880fd3af
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-28 14:52:36 +0000

    e2ccea9 docs: release notes for the v19.2.24 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-25922d1
+ * @license Angular v22.1.0-next.0+sha-e2ccea9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-25922d1');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-e2ccea9');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
