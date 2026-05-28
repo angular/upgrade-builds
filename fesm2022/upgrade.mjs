8bee244763df00bc7fd221b7442db2571ed11c2a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-28 14:15:54 +0000

    4f79cfb build: update all github actions

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-ec138c3
+ * @license Angular v22.1.0-next.0+sha-4f79cfb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-ec138c3');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-4f79cfb');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
