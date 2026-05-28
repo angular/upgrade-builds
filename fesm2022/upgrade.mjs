c0a8dca8038a237a94826814970458328b3b8352
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-28 11:54:21 +0000

    8538bdc docs: fix grammar issues in resource guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.14+sha-582a417
+ * @license Angular v21.2.14+sha-8538bdc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.14+sha-582a417');
+const VERSION = /* @__PURE__ */new Version('21.2.14+sha-8538bdc');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
