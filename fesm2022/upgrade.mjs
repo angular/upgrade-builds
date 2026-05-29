f99ba1e6c7601c48534220fb89525cbb1cd8da13
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-29 11:27:18 +0000

    f6d8e64 fix(common): only strip a literal /index.html suffix from URLs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.15+sha-9b7d0e5
+ * @license Angular v21.2.15+sha-f6d8e64
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.15+sha-9b7d0e5');
+const VERSION = /* @__PURE__ */new Version('21.2.15+sha-f6d8e64');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
