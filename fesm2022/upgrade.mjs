f8a39325dea49faf6eb6c5b6b69694a4f6d4e524
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-29 10:00:33 +0000

    9b7d0e5 docs: document i18n object forms for sourceLocale and locales in angular.json

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.15+sha-cea6588
+ * @license Angular v21.2.15+sha-9b7d0e5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.15+sha-cea6588');
+const VERSION = /* @__PURE__ */new Version('21.2.15+sha-9b7d0e5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
