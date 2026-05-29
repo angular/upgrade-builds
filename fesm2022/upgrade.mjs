f58d5645d3eb6c2fef3fd0cf5dd870033db1739c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-29 13:07:38 +0000

    7e38336 fix(core): use Object.create(null) for LOCALE_DATA as a hardening measure

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.15+sha-f6d8e64
+ * @license Angular v21.2.15+sha-7e38336
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.15+sha-f6d8e64');
+const VERSION = /* @__PURE__ */new Version('21.2.15+sha-7e38336');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
