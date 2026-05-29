363575c686e23fce056bbf6409841b962d6a49e4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-29 13:08:58 +0000

    e81c7e8 refactor(forms): type built-in getError results

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-rc.2+sha-4795b35
+ * @license Angular v22.0.0-rc.2+sha-e81c7e8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.2+sha-4795b35');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.2+sha-e81c7e8');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
