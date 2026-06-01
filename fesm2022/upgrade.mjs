6c357c3674963cbc3dac594eae2024f1a71db588
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 10:05:05 +0000

    d82c364 fix(docs-infra): readable contrast for DEV/EXP api badges in light mode

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-rc.2+sha-8061ca4
+ * @license Angular v22.0.0-rc.2+sha-d82c364
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.2+sha-8061ca4');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.2+sha-d82c364');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
