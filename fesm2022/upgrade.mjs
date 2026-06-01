c820b3b0b72841e5677841ac69cfc8f1ba85d8e0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 18:16:18 +0000

    0b19c92 fix(platform-server): throw on suspicious URLs and restrict protocol-relative URLs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-ac47524
+ * @license Angular v22.1.0-next.0+sha-0b19c92
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-ac47524');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-0b19c92');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
