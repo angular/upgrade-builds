945e4f89f5b46984186ee813fcbac62f51116672
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 16:33:06 +0000

    5215f73 build: update dependency @mcp-b/webmcp-polyfill to v3

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-f0b28f6
+ * @license Angular v22.1.0-next.0+sha-5215f73
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-f0b28f6');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-5215f73');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
