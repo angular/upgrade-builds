8bd61d2c1693f404b50d54160a825b5c235ebf42
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 10:25:56 +0000

    9f6fda6 docs: fix esbuild and TensorFlow branding on overview page

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.15+sha-1cd4f54
+ * @license Angular v21.2.15+sha-9f6fda6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.15+sha-1cd4f54');
+const VERSION = /* @__PURE__ */new Version('21.2.15+sha-9f6fda6');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
