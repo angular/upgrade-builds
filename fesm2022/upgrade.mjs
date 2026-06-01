dc2454289a7b142873b0f41135e969a4c3eb9ae3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 16:43:24 +0000

    b74fb76 docs(docs-infra): simplify code block styles

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.15+sha-ae1c8a1
+ * @license Angular v21.2.15+sha-b74fb76
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.15+sha-ae1c8a1');
+const VERSION = /* @__PURE__ */new Version('21.2.15+sha-b74fb76');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
