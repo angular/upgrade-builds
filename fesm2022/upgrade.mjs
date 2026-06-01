f53d58aa385a00b7ca0dda7019a49a8b96729379
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 17:04:17 +0000

    ac47524 docs(core): fix typos and grammar in dependency injection guides

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-ec4f08b
+ * @license Angular v22.1.0-next.0+sha-ac47524
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-ec4f08b');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-ac47524');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
