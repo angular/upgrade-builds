28f26fd32bdde6d0e3d69eaedf727498ae79c13c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 09:40:59 +0000

    69c0d48 fix(docs-infra): round up media queries

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.15+sha-7e38336
+ * @license Angular v21.2.15+sha-69c0d48
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.15+sha-7e38336');
+const VERSION = /* @__PURE__ */new Version('21.2.15+sha-69c0d48');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
