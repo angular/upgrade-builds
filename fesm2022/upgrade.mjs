68b1a143ddc00abfd5e02ae6f71013ed7211f04e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-02 11:38:07 +0000

    e695379 fix(core): harden inherit definition feature against polluted prototypes

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-ebc7dbe
+ * @license Angular v22.1.0-next.0+sha-e695379
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-ebc7dbe');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-e695379');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
