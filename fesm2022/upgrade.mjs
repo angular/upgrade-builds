53fb06a3f25686c95d8f00388ea1252ef10e8144
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-02 12:24:08 +0000

    736c4ab refactor(core): fix broken unit test.

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.15+sha-3fd6897
+ * @license Angular v21.2.15+sha-736c4ab
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.15+sha-3fd6897');
+const VERSION = /* @__PURE__ */new Version('21.2.15+sha-736c4ab');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
