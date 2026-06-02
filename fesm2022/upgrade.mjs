626f7c39ce16142c50380a44fdf47541e1698840
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-02 09:29:53 +0000

    db157e4 docs: fix `min/max` form template examples

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.15+sha-66821c4
+ * @license Angular v21.2.15+sha-db157e4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.15+sha-66821c4');
+const VERSION = /* @__PURE__ */new Version('21.2.15+sha-db157e4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
