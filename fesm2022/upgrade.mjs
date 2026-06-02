dfa7214b76fb85ddfc318c2f1a3be7bf28c5a29d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-02 09:31:45 +0000

    04028c7 docs: add testing guides to Angular Aria

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-rc.3+sha-7584e35
+ * @license Angular v22.0.0-rc.3+sha-04028c7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.3+sha-7584e35');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.3+sha-04028c7');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
