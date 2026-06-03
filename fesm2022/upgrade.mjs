5e814da06ed4ac19f48c77524387b3ff4361f2de
Andrew Scott <atscott01@gmail.com>
2026-06-03 22:54:19 +0000

    592b121 refactor(devtools): Prevent cross-tab privilege escalation

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0+sha-8e4fd59
+ * @license Angular v22.0.0+sha-592b121
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-8e4fd59');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-592b121');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
