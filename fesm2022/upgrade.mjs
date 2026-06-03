88058cc2ff5c759bc00e498c72f74e7dd0b79bd9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-03 15:19:50 +0000

    595660d docs: update tutorials and playground to v22.0.0

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-09c03a2
+ * @license Angular v22.1.0-next.0+sha-595660d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-09c03a2');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-595660d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
