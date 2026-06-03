16aa5564652813f294c6821e4ff380b32068031e
Andrew Scott <atscott01@gmail.com>
2026-06-03 20:16:54 +0000

    dc9c996 fix(compiler): sanitize two-way properties

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.16+sha-1523061
+ * @license Angular v21.2.16+sha-dc9c996
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.16+sha-1523061');
+const VERSION = /* @__PURE__ */new Version('21.2.16+sha-dc9c996');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
