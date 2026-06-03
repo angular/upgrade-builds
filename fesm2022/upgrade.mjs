c0719c3ecee77d75b4a7cd13017f30fbdbcbf1bf
Andrew Scott <atscott01@gmail.com>
2026-06-03 19:17:57 +0000

    3d9ca2f fix(compiler-cli): bind switch exhaustive check expressions

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0+sha-32b348a
+ * @license Angular v22.0.0+sha-3d9ca2f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-32b348a');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-3d9ca2f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
