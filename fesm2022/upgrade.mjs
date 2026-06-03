d6c1c2e93850711ce4af948b056d01555d2434f5
Andrew Scott <atscott01@gmail.com>
2026-06-03 23:11:28 +0000

    6bde84f fix(core): harden TransferState restoration against DOM clobbering

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-7896e74
+ * @license Angular v22.1.0-next.0+sha-6bde84f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-7896e74');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-6bde84f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
