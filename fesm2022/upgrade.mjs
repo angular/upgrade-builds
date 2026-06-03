3e5f5fb0df5d9275eea80a7f0c614db9b628edd8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-03 12:39:25 +0000

    03188dd release: cut the v21.2.16 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.15+sha-736c4ab
+ * @license Angular v21.2.16+sha-03188dd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.15+sha-736c4ab');
+const VERSION = /* @__PURE__ */new Version('21.2.16+sha-03188dd');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
