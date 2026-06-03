1b56efda0e6ec932ab0f872d83e40b75e9656dbf
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-03 14:44:37 +0000

    3151330 release: bump version of in memory web api

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0+sha-1cb0524
+ * @license Angular v22.0.0+sha-3151330
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-1cb0524');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-3151330');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
