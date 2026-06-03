bae18a4188f4345830b77b05802ad4c3cc9a69fb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-03 13:10:27 +0000

    1cb0524 release: cut the v22.0.0 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0-rc.3+sha-3401189
+ * @license Angular v22.0.0+sha-1cb0524
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.3+sha-3401189');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-1cb0524');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
