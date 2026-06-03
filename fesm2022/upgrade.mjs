56f8bca1bb9cf64e7fe1b789c6fa20309787092f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-03 17:10:26 +0000

    32b348a docs: link to v22 blog post in changelog

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0+sha-da07b3c
+ * @license Angular v22.0.0+sha-32b348a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-da07b3c');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-32b348a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
