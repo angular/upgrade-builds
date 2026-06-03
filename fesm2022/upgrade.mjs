8117596167029bd38ff007a46be5f91382f242f1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-03 16:03:08 +0000

    2052495 docs: update landing page for v22

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-595660d
+ * @license Angular v22.1.0-next.0+sha-2052495
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-595660d');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-2052495');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
