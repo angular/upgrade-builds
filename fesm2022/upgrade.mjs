35279e2f3eb0cadbaea04643ef3f706885da5e4e
Andrew Scott <atscott01@gmail.com>
2026-06-04 22:34:32 +0000

    d846326 fix(common): skip transfer cache for uncacheable HTTP traffic

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.16+sha-dc9c996
+ * @license Angular v21.2.16+sha-d846326
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.16+sha-dc9c996');
+const VERSION = /* @__PURE__ */new Version('21.2.16+sha-d846326');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
