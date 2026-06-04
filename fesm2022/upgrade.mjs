56fb697150da43dccc620f0d6039efdf6f64d7e3
Andrew Scott <atscott01@gmail.com>
2026-06-04 17:13:00 +0000

    302cb60 refactor(core): Also throw an error on iframe attributes set to `undefined`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0+sha-fa546f3
+ * @license Angular v22.0.0+sha-302cb60
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-fa546f3');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-302cb60');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
