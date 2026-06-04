3fc4c7186b057ed6fba17123947175e127ef14ba
Andrew Scott <atscott01@gmail.com>
2026-06-04 19:40:15 +0000

    c00387b docs: mention `parse` of `validateHttp`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0+sha-669146b
+ * @license Angular v22.0.0+sha-c00387b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-669146b');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-c00387b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
