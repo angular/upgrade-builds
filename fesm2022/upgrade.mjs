ec3e067fc0634c8efb566c1f75d102f62fb4cdfa
Andrew Scott <atscott01@gmail.com>
2026-06-04 21:27:44 +0000

    55b7b5a fix(forms): set `additionalProperties: false` on generated WebMCP form

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0+sha-ffb06c0
+ * @license Angular v22.0.0+sha-55b7b5a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-ffb06c0');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-55b7b5a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
