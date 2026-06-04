44edc70e28acb0873b314aa3a0011b6805fdb015
Andrew Scott <atscott01@gmail.com>
2026-06-04 18:50:47 +0000

    e50d47a fix(core): disable WebMCP during SSR

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-8da1198
+ * @license Angular v22.1.0-next.0+sha-e50d47a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-8da1198');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-e50d47a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
