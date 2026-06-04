68936105f52e3622bc8df9987f71362b1313b810
Andrew Scott <atscott01@gmail.com>
2026-06-04 21:12:34 +0000

    cb8ceb1 fix(http): ensure query parameters are inserted before URL fragments

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-e100c75
+ * @license Angular v22.1.0-next.0+sha-cb8ceb1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-e100c75');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-cb8ceb1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
