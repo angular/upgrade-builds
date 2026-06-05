36645a4429abcbd6edfef5fa3344e67768a42f91
Andrew Scott <atscott01@gmail.com>
2026-06-05 16:56:27 +0000

    bc55749 fix(common): use cryptographically secure SHA-256 for transfer cache key generation

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.16+sha-d846326
+ * @license Angular v21.2.16+sha-bc55749
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.16+sha-d846326');
+const VERSION = /* @__PURE__ */new Version('21.2.16+sha-bc55749');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
