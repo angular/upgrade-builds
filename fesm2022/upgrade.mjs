7ceced6426a6afbc9f2deddc75dcfdf910ac5b32
GitHub <noreply@github.com>
2026-06-05 21:33:52 +0000

    a810a31 fix(http): Rejects non-HTTP(S) URLs in JSONP requests

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.16+sha-3551074
+ * @license Angular v21.2.16+sha-a810a31
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.16+sha-3551074');
+const VERSION = /* @__PURE__ */new Version('21.2.16+sha-a810a31');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
