c61bb32a2f4cfa8d7e574cd48c2685cbdba981c3
Andrew Scott <atscott01@gmail.com>
2026-06-05 17:59:09 +0000

    3551074 fix(platform-server): harden platform location origin validation during SSR

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.16+sha-bc55749
+ * @license Angular v21.2.16+sha-3551074
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.16+sha-bc55749');
+const VERSION = /* @__PURE__ */new Version('21.2.16+sha-3551074');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
