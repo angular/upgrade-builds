3b5de7407fa68795a0b92ece49eee4d35666d1ce
Andrew Scott <atscott01@gmail.com>
2026-06-05 17:50:05 +0000

    1a62130 fix(common): use cryptographically secure SHA-256 for transfer cache key generation

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.24+sha-49368c1
+ * @license Angular v20.3.24+sha-1a62130
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.24+sha-49368c1');
+const VERSION = /* @__PURE__ */ new Version('20.3.24+sha-1a62130');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
