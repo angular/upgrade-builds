b682ec5b7e1408991a66d9961e87d7956d29b786
Andrew Scott <atscott01@gmail.com>
2026-06-05 17:31:07 +0000

    652b76b docs: bump schematics-for-libraries example peer deps to v22

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0+sha-8f06057
+ * @license Angular v22.0.0+sha-652b76b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-8f06057');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-652b76b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
