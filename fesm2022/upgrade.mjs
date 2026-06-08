a122920dc4abebbd3677b70467bd483535c42e61
Andrew Scott <atscott01@gmail.com>
2026-06-08 21:32:21 +0000

    e7dae12 fix(docs-infra): avoid code copy button overlap

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0+sha-167bd4c
+ * @license Angular v22.0.0+sha-e7dae12
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-167bd4c');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-e7dae12');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
