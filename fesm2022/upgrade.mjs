93421be7fce644a789d16c3fac8739824d920c64
Andrew Scott <atscott01@gmail.com>
2026-06-08 20:08:43 +0000

    60d88b1 docs: fix malformed prettier-ignore comment in whitespace guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0+sha-a99b7e4
+ * @license Angular v22.0.0+sha-60d88b1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-a99b7e4');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-60d88b1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
