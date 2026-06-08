60e0e62791930f182cbc9c15725fb260646626b7
Andrew Scott <atscott01@gmail.com>
2026-06-08 18:59:06 +0000

    51724b4 ci: add myself to `size-tracking`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-a7d6845
+ * @license Angular v22.1.0-next.0+sha-51724b4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-a7d6845');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-51724b4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
