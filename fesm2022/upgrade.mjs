3e350c6759353cc6afd4c322018690da59a0e41a
Andrew Scott <atscott01@gmail.com>
2026-06-08 18:07:05 +0000

    a816848 ci: update devtools reviewers list

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-11b206b
+ * @license Angular v22.1.0-next.0+sha-a816848
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-11b206b');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-a816848');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
