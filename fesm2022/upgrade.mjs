79edf79e8dc1bbc4051136edc47108d37eed28ba
Andrew Scott <atscott01@gmail.com>
2026-06-08 20:31:34 +0000

    86a56dc fix(common): Limits date format string length

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.16+sha-b9d2938
+ * @license Angular v21.2.16+sha-86a56dc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.16+sha-b9d2938');
+const VERSION = /* @__PURE__ */new Version('21.2.16+sha-86a56dc');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
