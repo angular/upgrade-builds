6f532e46889a43feecb8225bec88fc4cb5834f1e
Andrew Scott <atscott01@gmail.com>
2026-06-08 20:27:53 +0000

    3c70270 fix(compiler): sanitize two-way properties

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-422e689
+ * @license Angular v22.1.0-next.0+sha-3c70270
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-422e689');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-3c70270');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
