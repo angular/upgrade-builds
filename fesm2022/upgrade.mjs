623901279b1ce4960a41e4bb88a00424c692d061
Andrew Scott <atscott01@gmail.com>
2026-06-08 19:54:50 +0000

    06db706 feat(devtools): improve transfer state tab

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-fe50a7e
+ * @license Angular v22.1.0-next.0+sha-06db706
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-fe50a7e');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-06db706');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
