45f9b38efe0a29a5a64680eb286167a80ff72663
Andrew Scott <atscott01@gmail.com>
2026-06-08 21:11:35 +0000

    167bd4c fix(http): Rejects non-HTTP(S) URLs in JSONP requests

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0+sha-393b84c
+ * @license Angular v22.0.0+sha-167bd4c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-393b84c');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-167bd4c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
