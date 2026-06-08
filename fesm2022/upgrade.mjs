2da77dbd5e8a4bab1e250288db6e4b8ea9bef072
GitHub <noreply@github.com>
2026-06-08 19:46:05 +0000

    fe50a7e perf(devtools): optimize signal graph nodes value inspection

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-b3748e9
+ * @license Angular v22.1.0-next.0+sha-fe50a7e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-b3748e9');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-fe50a7e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
