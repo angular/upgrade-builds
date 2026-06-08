d09628986b7b82b20059574396295a88145fd7c5
Andrew Scott <atscott01@gmail.com>
2026-06-08 17:24:48 +0000

    11b206b fix(core): introduce disposal mechanism for Angular views in foreign `@content`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-255151a
+ * @license Angular v22.1.0-next.0+sha-11b206b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-255151a');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-11b206b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
