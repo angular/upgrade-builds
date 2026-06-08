81559611b3138921ddf19e24dd1f986c3021e42e
Andrew Scott <atscott01@gmail.com>
2026-06-08 22:49:47 +0000

    06be298 fix(http): preserve empty referrer option in HttpRequest

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.24+sha-9f443bc
+ * @license Angular v20.3.24+sha-06be298
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.24+sha-9f443bc');
+const VERSION = /* @__PURE__ */ new Version('20.3.24+sha-06be298');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
