db20e893bd4ac6892ee565e314b9b9a2f5d8b183
Andrew Scott <atscott01@gmail.com>
2026-06-08 22:00:40 +0000

    cf97b1f fix(service-worker): Strips sensitive headers on cross-origin redirects

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0+sha-35a8c28
+ * @license Angular v22.0.0+sha-cf97b1f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-35a8c28');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-cf97b1f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
