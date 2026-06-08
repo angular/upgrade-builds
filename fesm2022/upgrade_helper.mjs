abe83d356fba6f42332f1f7867931bec2e08905b
Andrew Scott <atscott01@gmail.com>
2026-06-08 20:22:04 +0000

    d65a5f4 fix(service-worker): Strips sensitive headers on cross-origin redirects

--- fesm2022/upgrade_helper.mjs
+++ fesm2022/upgrade_helper.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.24+sha-1a62130
+ * @license Angular v20.3.24+sha-d65a5f4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ import { element, $ROOT_ELEMENT, $ROOT_SCOPE, DOWNGRADED_MODULE_COUNT_KEY, UPGRA
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.24+sha-1a62130');
+const VERSION = /* @__PURE__ */ new Version('20.3.24+sha-d65a5f4');
 
 /**
  * A `PropertyBinding` represents a mapping between a property name
