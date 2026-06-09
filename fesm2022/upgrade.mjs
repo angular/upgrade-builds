5dec933acb5ec05d28ad3d9f51f8013b08f428c2
GitHub <noreply@github.com>
2026-06-09 17:14:19 +0000

    13fb0af refactor(platform-server): deprecate ServerXhr (#69255)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.16+sha-86a56dc
+ * @license Angular v21.2.16+sha-13fb0af
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.16+sha-86a56dc');
+const VERSION = /* @__PURE__ */new Version('21.2.16+sha-13fb0af');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
