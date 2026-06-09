94948de16bb651fb5b194c18bef97216da057a8f
Andrew Scott <atscott01@gmail.com>
2026-06-09 20:40:58 +0000

    43a0e28 fix(language-service): prevent external template inlay hints from appearing in TS files

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0+sha-4289c4c
+ * @license Angular v22.0.0+sha-43a0e28
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-4289c4c');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-43a0e28');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
