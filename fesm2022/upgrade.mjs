c4e065b70d16e54bbf85381017d160bd1705cfb1
Andrew Scott <atscott01@gmail.com>
2026-06-09 17:49:28 +0000

    c4b5fa3 fix(common): escape CSS string-terminating characters in escapeCssUrl

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0+sha-cdcea80
+ * @license Angular v22.0.0+sha-c4b5fa3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-cdcea80');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-c4b5fa3');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
