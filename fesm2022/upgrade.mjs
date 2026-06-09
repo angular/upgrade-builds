be3a52a984173fc0e509776e2ef9570514874e06
Andrew Scott <atscott01@gmail.com>
2026-06-09 18:22:12 +0000

    fe72186 fix(router): use native URL object for navigation boundary and comparison

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-3dd35c2
+ * @license Angular v22.1.0-next.0+sha-fe72186
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-3dd35c2');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-fe72186');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
