ecccb708d7e14aa00fc82773663c127f4c94edfc
Andrew Scott <atscott01@gmail.com>
2026-06-09 17:36:18 +0000

    c121407 fix(core): require WebMCP tool descriptions

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-bff084f
+ * @license Angular v22.1.0-next.0+sha-c121407
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-bff084f');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-c121407');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
