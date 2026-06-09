d2cd9985a478340fea1e933b367748f692487a29
Andrew Scott <atscott01@gmail.com>
2026-06-09 17:04:20 +0000

    7d92cc8 fix(docs-infra): improve version picker dropdown positioning

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-547d85a
+ * @license Angular v22.1.0-next.0+sha-7d92cc8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-547d85a');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-7d92cc8');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
