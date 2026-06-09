9452ed77d25b875cce20d682cc434159939d0d21
Andrew Scott <atscott01@gmail.com>
2026-06-09 20:23:00 +0000

    4289c4c fix(core): update comment for Default change detection

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0+sha-562a566
+ * @license Angular v22.0.0+sha-4289c4c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-562a566');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-4289c4c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
