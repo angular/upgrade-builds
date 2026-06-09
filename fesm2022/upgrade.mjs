ecd9280078e3c03a86471c147a25f3c3e0c01c5f
Andrew Scott <atscott01@gmail.com>
2026-06-09 17:38:01 +0000

    cdcea80 fix(core): require WebMCP tool descriptions

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0+sha-f908a3e
+ * @license Angular v22.0.0+sha-cdcea80
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-f908a3e');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-cdcea80');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
