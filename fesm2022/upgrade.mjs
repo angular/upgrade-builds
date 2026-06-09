df50a6fcb148afda8322f5efb66ebc21eaa410e6
Andrew Scott <atscott01@gmail.com>
2026-06-09 16:54:20 +0000

    547d85a ci: run benchmark comparison in isolated worktree and harden security

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-e6ae250
+ * @license Angular v22.1.0-next.0+sha-547d85a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-e6ae250');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-547d85a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
