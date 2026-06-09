75702b346dc9907b5ca6593bbc56e906673c783d
Andrew Scott <atscott01@gmail.com>
2026-06-09 21:07:05 +0000

    a704b08 docs: add Signal Forms and v22 guidance to AI best-practices and llms.txt

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0+sha-43a0e28
+ * @license Angular v22.0.0+sha-a704b08
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-43a0e28');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-a704b08');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
