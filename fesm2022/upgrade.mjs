205def55a008d4fb9e9be878dc157bac2bbbe803
Andrew Scott <atscott01@gmail.com>
2026-06-09 20:32:40 +0000

    86cd166 fix(docs-infra): load cross-origin video embeds under COEP credentialless

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-5829177
+ * @license Angular v22.1.0-next.0+sha-86cd166
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-5829177');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-86cd166');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
