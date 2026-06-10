e29d632d9292955c9946d3cb24def5ab198e848b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-10 22:48:47 +0000

    71bb19d build: lock file maintenance

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-6f3e832
+ * @license Angular v22.1.0-next.0+sha-71bb19d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-6f3e832');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-71bb19d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
