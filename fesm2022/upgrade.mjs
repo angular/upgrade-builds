25de7690e75f0754ecf3cf3807bba22f89c9b771
Andrew Scott <atscott01@gmail.com>
2026-06-10 20:25:19 +0000

    9f4b3dc docs: release notes for the v22.0.1 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-7e94ab9
+ * @license Angular v22.1.0-next.0+sha-9f4b3dc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-7e94ab9');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-9f4b3dc');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
