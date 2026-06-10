bf56dd9433125db1ba487bfc97d0a1ea1182ccd0
Andrew Scott <atscott01@gmail.com>
2026-06-10 18:48:49 +0000

    2dd65d2 fix(http): pass down the `reportUploadProgress` and `reportDownloadProgress` on post/patch requests

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0+sha-b416da6
+ * @license Angular v22.0.0+sha-2dd65d2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-b416da6');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-2dd65d2');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
