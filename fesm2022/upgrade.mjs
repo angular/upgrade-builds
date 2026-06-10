12b4253cb7a39e8ddfd1277698f77554aabac881
Andrew Scott <atscott01@gmail.com>
2026-06-10 18:09:40 +0000

    4645850 refactor(compiler): Remove 80 char limit on AbstractEmitterVisitor

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0+sha-e51ad37
+ * @license Angular v22.0.0+sha-4645850
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-e51ad37');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-4645850');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
