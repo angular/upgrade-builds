271dd2ae0bf5411f4758a20d7a835dfb8b148d46
GitHub <noreply@github.com>
2026-06-10 16:57:58 +0000

    88832c8 fix(core): validate lowercase SVG animation attribute names (#69269)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.16+sha-13fb0af
+ * @license Angular v21.2.16+sha-88832c8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.16+sha-13fb0af');
+const VERSION = /* @__PURE__ */new Version('21.2.16+sha-88832c8');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
