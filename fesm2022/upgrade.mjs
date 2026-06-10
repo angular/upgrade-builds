f9f462183ff8868b66a5701c432883742fc66bd4
Andrew Scott <atscott01@gmail.com>
2026-06-10 18:17:07 +0000

    fbe807c fix(vscode-extension): resolve relative workspace tsdk paths to absolute

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-54112d9
+ * @license Angular v22.1.0-next.0+sha-fbe807c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-54112d9');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-fbe807c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
