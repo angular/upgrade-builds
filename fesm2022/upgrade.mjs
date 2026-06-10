8b51f2303253e132c4cb571df413eb3be06e30c5
Andrew Scott <atscott01@gmail.com>
2026-06-10 20:02:01 +0000

    31c25e2 release: cut the v21.2.17 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.16+sha-88832c8
+ * @license Angular v21.2.17+sha-31c25e2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.16+sha-88832c8');
+const VERSION = /* @__PURE__ */new Version('21.2.17+sha-31c25e2');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
