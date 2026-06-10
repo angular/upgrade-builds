042d50522262c6f1971d8dc61940f8053c555fe1
Andrew Scott <atscott01@gmail.com>
2026-06-10 02:34:01 +0000

    f06b96d fix(forms): harden FormGroup control lookups against prototype shadowing

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-6b0150f
+ * @license Angular v22.1.0-next.0+sha-f06b96d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-6b0150f');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-f06b96d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
