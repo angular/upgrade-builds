df2997b4077ce70e3cfed479080be11df37f84bb
Andrew Scott <atscott01@gmail.com>
2026-06-10 00:18:08 +0000

    6e3d51d refactor(migrations): Improve `safeNavigationMigration` heuristic

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.0+sha-01ea640
+ * @license Angular v22.0.0+sha-6e3d51d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-01ea640');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-6e3d51d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
