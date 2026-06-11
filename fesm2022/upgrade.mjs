f88ad63455d105626e57f87d693162df1aebdb24
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-11 17:11:10 +0000

    6867f77 fix(http): distinguish repeated transfer cache params

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.1+sha-21bdff5
+ * @license Angular v22.0.1+sha-6867f77
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.1+sha-21bdff5');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-6867f77');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
