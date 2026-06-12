52799fb647e3999e7d1e1031882e233acb5234e6
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-12 16:17:13 +0000

    b7aa8dc fix(docs-infra): add explicit font styles to docs-primary-btn

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.1+sha-d45e318
+ * @license Angular v22.0.1+sha-b7aa8dc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.1+sha-d45e318');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-b7aa8dc');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
