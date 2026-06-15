02b649167343fe5ff260ab61f132faf604785c8e
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-15 16:36:42 +0000

    417a407 fix(compiler): restrict possible event handler check to property names longer than 2 characters

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-50e7f3a
+ * @license Angular v22.1.0-next.0+sha-417a407
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-50e7f3a');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-417a407');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
