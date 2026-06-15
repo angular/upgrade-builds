52356b76dfb92a7cf731d9a682c08ef724fa94f3
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-15 18:15:04 +0000

    be45e8e refactor(compiler-cli): Change indexComponent file to be fileUrl

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.1+sha-6f11719
+ * @license Angular v22.0.1+sha-be45e8e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.1+sha-6f11719');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-be45e8e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
