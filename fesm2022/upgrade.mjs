b0b01120bfea71b939d252beaedc2a269ee25c5f
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-15 18:26:15 +0000

    01e99d8 docs: update cross-repo adev docs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-21fccd4
+ * @license Angular v22.1.0-next.0+sha-01e99d8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-21fccd4');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-01e99d8');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
