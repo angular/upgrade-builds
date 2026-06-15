878980f6900816a99d3683958bb1c4aa3a59e3f9
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-15 16:09:24 +0000

    fe7f9ed fix(docs-infra): center social and theme menus under their triggers on tablet

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.1+sha-1d97355
+ * @license Angular v22.0.1+sha-fe7f9ed
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.1+sha-1d97355');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-fe7f9ed');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
