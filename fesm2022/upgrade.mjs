3f5a8688bd9f1df9456fd7c2ca6e27a3965d830d
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-16 16:16:25 +0000

    0152e3c fix(core): treat iframe credentialless as security-sensitive

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-cbccd36
+ * @license Angular v22.1.0-next.0+sha-0152e3c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-cbccd36');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-0152e3c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
