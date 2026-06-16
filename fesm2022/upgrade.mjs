c33e57e2ee0905894a9ebc72477a98e655cddb01
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-16 15:39:55 +0000

    93d0a5f fix(core): prevent unsubscribe during emit from throwing off other listeners

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.1+sha-fef8bcf
+ * @license Angular v22.0.1+sha-93d0a5f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.1+sha-fef8bcf');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-93d0a5f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
