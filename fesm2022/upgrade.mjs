ba0abc78507bcb9101dda194d865aa01944d6a65
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-16 17:36:43 +0000

    ffbbbf9 refactor(compiler): add support for @Input transforms under isolatedDeclarations

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.1+sha-b32ee7c
+ * @license Angular v22.0.1+sha-ffbbbf9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.1+sha-b32ee7c');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-ffbbbf9');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
