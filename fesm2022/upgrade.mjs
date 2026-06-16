a632d5e1bafc9ce56d0822f72751e3037698e8b7
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-16 15:56:05 +0000

    cbccd36 docs: use the @Service decorator in the learn-angular DI tutorial

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-28cb15a
+ * @license Angular v22.1.0-next.0+sha-cbccd36
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-28cb15a');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-cbccd36');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
