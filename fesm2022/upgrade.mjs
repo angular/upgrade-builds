e54647f5fd1ea3728bf0d5b460a120dd940a04e8
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-17 15:24:34 +0000

    a5ee50b refactor(compiler): correct TcbInvalidReferenceOp initializer

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-86ade07
+ * @license Angular v22.1.0-next.0+sha-a5ee50b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-86ade07');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-a5ee50b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
