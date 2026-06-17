17dd4f5c2e5f745a0a2183124103569669a38ae2
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-17 17:40:44 +0000

    e3d392f ci: add id-token: write permission to release workflow

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.1+sha-74f2b91
+ * @license Angular v22.0.1+sha-e3d392f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.1+sha-74f2b91');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-e3d392f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
