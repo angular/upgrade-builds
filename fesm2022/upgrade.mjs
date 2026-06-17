c6f37c60a7576f85865c567eb862cb3a7ba5839d
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-17 17:08:51 +0000

    a89ab78 docs(docs-infra): update environment configuration examples and remove production flag

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-2d2d5fc
+ * @license Angular v22.1.0-next.0+sha-a89ab78
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-2d2d5fc');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-a89ab78');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
