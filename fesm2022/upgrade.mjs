8f227557fa74d13033eccc4c109631323427f2a7
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-17 20:18:48 +0000

    52f5501 ci: update dev-infra reference to new commit SHA

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.1+sha-81cb457
+ * @license Angular v22.0.1+sha-52f5501
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.1+sha-81cb457');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-52f5501');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
