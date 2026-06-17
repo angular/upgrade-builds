1770c4f53ee1eb01b90fd21c6d27a9ebaf23f662
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-17 18:51:43 +0000

    1e79dd3 refactor(router): Add handling for ActivatedRoute-scoped injector

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.0+sha-ea18ab2
+ * @license Angular v22.1.0-next.0+sha-1e79dd3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-ea18ab2');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-1e79dd3');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
