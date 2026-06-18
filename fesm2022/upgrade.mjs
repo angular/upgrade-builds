9a8064fb7279933c4737cacd938ad27bd08dc779
kirjs <kirts@google.com>
2026-06-18 18:17:06 +0000

    c6b0b87 fix(docs-infra): align the page title with its edit action

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.2+sha-3e7c83b
+ * @license Angular v22.0.2+sha-c6b0b87
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.2+sha-3e7c83b');
+const VERSION = /* @__PURE__ */new Version('22.0.2+sha-c6b0b87');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
