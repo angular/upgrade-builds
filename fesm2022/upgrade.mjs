5c14902006444b8fc3798087c1ed0419e94caf5a
kirjs <kirts@google.com>
2026-06-18 14:58:49 +0000

    73880c5 fix(docs-infra): correct select dropdown scrolling, sizing, and selection

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.2+sha-291d574
+ * @license Angular v22.0.2+sha-73880c5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.2+sha-291d574');
+const VERSION = /* @__PURE__ */new Version('22.0.2+sha-73880c5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
