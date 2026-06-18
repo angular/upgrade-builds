6cd7d488000309e6d1deba1d90b72423afac4805
kirjs <kirts@google.com>
2026-06-18 16:40:45 +0000

    3e7c83b Revert "fix(core): escape overlapping comment delimiters in escapeCommentText"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.2+sha-73880c5
+ * @license Angular v22.0.2+sha-3e7c83b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.2+sha-73880c5');
+const VERSION = /* @__PURE__ */new Version('22.0.2+sha-3e7c83b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
