83627243ca23fc663ef072e469e1520566cb188b
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-06-18 13:35:44 +0000

    291d574 docs(docs-infra): remove deprecated ts flags

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.2+sha-aac8577
+ * @license Angular v22.0.2+sha-291d574
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.2+sha-aac8577');
+const VERSION = /* @__PURE__ */new Version('22.0.2+sha-291d574');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
