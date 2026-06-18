86365ff14be95fd0810f91ba428e8e3857607c38
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-18 18:31:32 +0000

    7e1fcd4 build: update all github actions to v6.0.9

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.1+sha-e77a8a0
+ * @license Angular v22.1.0-next.1+sha-7e1fcd4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-e77a8a0');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-7e1fcd4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
