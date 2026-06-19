47a7ec4c08ffa78ae58b5550a4cebbe7db0dd741
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-06-19 09:42:19 +0000

    aa7d9fc fix(devtools): restore build under stricter ts_project deps

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.1+sha-7e1fcd4
+ * @license Angular v22.1.0-next.1+sha-aa7d9fc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-7e1fcd4');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-aa7d9fc');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
