83ef874deb51584cc188f86ca5e1112bbbc29249
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-22 20:17:33 +0000

    a286a32 build: update cross-repo angular dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.1+sha-aa7d9fc
+ * @license Angular v22.1.0-next.1+sha-a286a32
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-aa7d9fc');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-a286a32');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
