765d8649ce95ca37cd32fea8251937d2efc78d45
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-22 20:19:39 +0000

    a477a69 build: update dependency node to v22.23.0

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.2+sha-aaac1bc
+ * @license Angular v22.0.2+sha-a477a69
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.2+sha-aaac1bc');
+const VERSION = /* @__PURE__ */new Version('22.0.2+sha-a477a69');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
