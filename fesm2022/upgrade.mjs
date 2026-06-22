322912b92dd784afd0b8bc9c54e5931dec3547ea
kirjs <kirts@google.com>
2026-06-22 20:56:00 +0000

    d817e56 docs: show selected menu item visually in context menu example

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.2+sha-59ee51e
+ * @license Angular v22.0.2+sha-d817e56
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.2+sha-59ee51e');
+const VERSION = /* @__PURE__ */new Version('22.0.2+sha-d817e56');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
