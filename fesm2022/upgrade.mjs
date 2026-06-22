1f85b38113cc78b77f7f9e925697aae16a3de58b
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-22 20:43:08 +0000

    792edab refactor(migrations): account for inheritance in service migration

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.1+sha-a286a32
+ * @license Angular v22.1.0-next.1+sha-792edab
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-a286a32');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-792edab');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
