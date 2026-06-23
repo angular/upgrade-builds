97468a83956ae1f891f3f3184be52dc63d065965
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-23 15:37:06 +0000

    dead64f docs: add error guide for NG05101

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.1+sha-e5d7b3a
+ * @license Angular v22.1.0-next.1+sha-dead64f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-e5d7b3a');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-dead64f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
