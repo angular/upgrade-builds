7638cab660904dd884aa8a3875f088c9c8155262
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-23 13:54:27 +0000

    a4e9ff1 docs: add missing space before decorator link in components guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.1+sha-69ac1d5
+ * @license Angular v22.1.0-next.1+sha-a4e9ff1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-69ac1d5');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-a4e9ff1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
