e95de40f16655e4e5f95c7fe96e3670efe838250
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-24 18:56:52 +0000

    ecd0475 fix(compiler): account for NgModule dependencies in JIT-compiled partial declarations

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.1+sha-4744bab
+ * @license Angular v22.1.0-next.1+sha-ecd0475
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-4744bab');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-ecd0475');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
