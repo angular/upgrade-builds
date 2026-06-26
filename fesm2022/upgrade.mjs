66d21c74e78b83f9e5628336e60bf4655ad70411
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-26 16:25:58 +0000

    fd37f09 fix(migrations): resolve migration failure when tsconfig specifies rootDir

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.3+sha-5ba6443
+ * @license Angular v22.0.3+sha-fd37f09
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.3+sha-5ba6443');
+const VERSION = /* @__PURE__ */new Version('22.0.3+sha-fd37f09');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
