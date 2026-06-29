1664a848239fee30106a3cda6a5b212a309fb83d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-29 21:30:16 +0000

    0c18ccb docs(docs-infra): fix typo and malformed markdown bullet in angular-developer skill

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.4+sha-32c1f30
+ * @license Angular v22.0.4+sha-0c18ccb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.4+sha-32c1f30');
+const VERSION = /* @__PURE__ */new Version('22.0.4+sha-0c18ccb');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
