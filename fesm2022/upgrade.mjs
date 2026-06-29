a15bf7a8fd41b78c6d534c0b64fb4773697cefd6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-29 21:56:41 +0000

    ca76b2c docs(forms): correct signal field access in form model example

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.4+sha-baf09a9
+ * @license Angular v22.0.4+sha-ca76b2c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.4+sha-baf09a9');
+const VERSION = /* @__PURE__ */new Version('22.0.4+sha-ca76b2c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
