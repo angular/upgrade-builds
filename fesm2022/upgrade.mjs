5bb8b920942d22ea11cd60c83aae7812a9736953
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-29 21:41:31 +0000

    baf09a9 fix(compiler-cli): include toSignal in debugName transform

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.4+sha-0c18ccb
+ * @license Angular v22.0.4+sha-baf09a9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.4+sha-0c18ccb');
+const VERSION = /* @__PURE__ */new Version('22.0.4+sha-baf09a9');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
