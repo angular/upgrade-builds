e5736b34e3d2ee8ff0a09ccd8f6302118bf4cd9c
GitHub <noreply@github.com>
2026-06-29 21:39:47 +0000

    1659952 fix(compiler-cli): include toSignal in debugName transform

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.3+sha-d109cc5
+ * @license Angular v22.1.0-next.3+sha-1659952
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.3+sha-d109cc5');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.3+sha-1659952');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
