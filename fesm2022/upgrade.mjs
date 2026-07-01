91ee0c82802bdc9c91508047eb06b0f41fd4709c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-01 01:06:44 +0000

    0554bf5 docs: improve introductory resource example

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.4+sha-ced0180
+ * @license Angular v22.0.4+sha-0554bf5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.4+sha-ced0180');
+const VERSION = /* @__PURE__ */new Version('22.0.4+sha-0554bf5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
