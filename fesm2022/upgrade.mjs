fb43191cb908faac6bdc21be510048778abe346c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-01 21:15:42 +0000

    b126dc9 release: cut the v22.1.0-next.4 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.3+sha-c4fa24a
+ * @license Angular v22.1.0-next.4+sha-b126dc9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.3+sha-c4fa24a');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.4+sha-b126dc9');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
