bfa0ed3246c30dea058b6171cf76b60930f33a5b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-01 00:49:46 +0000

    5a693ba fix(core): reject dynamic script host elements

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.17+sha-6bcce11
+ * @license Angular v21.2.17+sha-5a693ba
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.17+sha-6bcce11');
+const VERSION = /* @__PURE__ */new Version('21.2.17+sha-5a693ba');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
