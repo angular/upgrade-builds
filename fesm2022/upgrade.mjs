bdab2f20a1f406717341ffb77c218d4ae9c38601
leonsenft <leonsenft@users.noreply.github.com>
2026-07-06 21:13:14 +0000

    8006412 docs: fix layout shift when refreshing the stagger example

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.5+sha-2b3277f
+ * @license Angular v22.0.5+sha-8006412
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.5+sha-2b3277f');
+const VERSION = /* @__PURE__ */new Version('22.0.5+sha-8006412');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
