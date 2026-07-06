95a9e05d480e898654e496949d5c6ec6be5272cf
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-06 21:09:13 +0000

    8d22cc9 fix(compiler-cli): update babel dependencies to latest v7

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.17+sha-5a693ba
+ * @license Angular v21.2.17+sha-8d22cc9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.17+sha-5a693ba');
+const VERSION = /* @__PURE__ */new Version('21.2.17+sha-8d22cc9');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
