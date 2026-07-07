b90a86767e502fba61912f43e640b4bf69c8e1ea
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-07 17:26:40 +0000

    56847dc fix(devtools): highlight SVG and directive hosts correctly

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.4+sha-6edabc6
+ * @license Angular v22.1.0-next.4+sha-56847dc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.4+sha-6edabc6');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.4+sha-56847dc');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
