d31b1fb837d754b32530059ab3875a4139335f2b
leonsenft <leonsenft@users.noreply.github.com>
2026-07-07 17:26:47 +0000

    c91a951 refactor(core): move devtools ai tool definition types to primitives

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.5+sha-ab721b4
+ * @license Angular v22.0.5+sha-c91a951
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.5+sha-ab721b4');
+const VERSION = /* @__PURE__ */new Version('22.0.5+sha-c91a951');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
