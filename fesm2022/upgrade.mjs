a930d9e83c38da1aa9fd883070cf95eee3d6a4e4
leonsenft <leonsenft@users.noreply.github.com>
2026-07-07 16:33:50 +0000

    2a05de4 docs: add signal forms dynamic forms with json guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.5+sha-8006412
+ * @license Angular v22.0.5+sha-2a05de4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.5+sha-8006412');
+const VERSION = /* @__PURE__ */new Version('22.0.5+sha-2a05de4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
