a1219af6c99d432895337493a8977a94f1972761
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-07 17:00:48 +0000

    5ec0b16 fix(docs-infra): disable nav-list tooltip on mobile

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.4+sha-e91046f
+ * @license Angular v22.1.0-next.4+sha-5ec0b16
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.4+sha-e91046f');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.4+sha-5ec0b16');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
