6f990e3bfc529d7b59219216ec9cbc8402ad69da
leonsenft <leonsenft@users.noreply.github.com>
2026-07-07 17:17:49 +0000

    ab721b4 refactor(vscode-extension): remove duplicated getTokenAtPosition in completions handler

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.5+sha-e839891
+ * @license Angular v22.0.5+sha-ab721b4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.5+sha-e839891');
+const VERSION = /* @__PURE__ */new Version('22.0.5+sha-ab721b4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
