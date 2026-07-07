59fbdbbcb4612a667674b19efbf9cb3253279ee7
GitHub <noreply@github.com>
2026-07-07 16:33:43 +0000

    d837d62 docs: add signal forms dynamic forms with json guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.4+sha-731d665
+ * @license Angular v22.1.0-next.4+sha-d837d62
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.4+sha-731d665');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.4+sha-d837d62');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
