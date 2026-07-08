bc2b3c760365978123eb7ead65cf434f0458b4af
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 18:35:31 +0000

    e86c31b fix(service-worker): preserve referrer policy in asset requests

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.17+sha-91df739
+ * @license Angular v21.2.17+sha-e86c31b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.17+sha-91df739');
+const VERSION = /* @__PURE__ */new Version('21.2.17+sha-e86c31b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
