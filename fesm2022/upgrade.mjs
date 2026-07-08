fe126991974f9d7d2efce87f683aeb9c8c38d2a3
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 22:20:30 +0000

    e3ac727 docs: release notes for the v20.3.26 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.5+sha-57616af
+ * @license Angular v22.1.0-next.5+sha-e3ac727
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-57616af');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-e3ac727');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
