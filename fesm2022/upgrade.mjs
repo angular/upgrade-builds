cc07f1a77c78df1efc4ecab9dc91f47d7b8b2052
leonsenft <leonsenft@users.noreply.github.com>
2026-07-08 17:52:56 +0000

    2d52d53 docs: update optional chaining behavior

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.5+sha-748faa4
+ * @license Angular v22.0.5+sha-2d52d53
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.5+sha-748faa4');
+const VERSION = /* @__PURE__ */new Version('22.0.5+sha-2d52d53');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
