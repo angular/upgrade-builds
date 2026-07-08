b332ca79382242aed7b16bfad98e957a0687f0ec
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 17:52:22 +0000

    39ea1c5 docs: fix grammar and punctuation issues in guides

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.4+sha-c1829f6
+ * @license Angular v22.1.0-next.4+sha-39ea1c5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.4+sha-c1829f6');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.4+sha-39ea1c5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
