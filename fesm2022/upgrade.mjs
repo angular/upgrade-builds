574ca4c180772191a9d95ff8282d733c09b5d0b4
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 21:57:30 +0000

    248d4e5 release: cut the v22.1.0-next.5 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.4+sha-099c5c6
+ * @license Angular v22.1.0-next.5+sha-248d4e5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.4+sha-099c5c6');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-248d4e5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
