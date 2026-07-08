bd592a70a1fa4e2dc80ee57c355f17016302c12b
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 19:27:56 +0000

    eac04cf docs: clarify runtime component comparison in elements guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.4+sha-c0eaaed
+ * @license Angular v22.1.0-next.4+sha-eac04cf
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.4+sha-c0eaaed');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.4+sha-eac04cf');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
