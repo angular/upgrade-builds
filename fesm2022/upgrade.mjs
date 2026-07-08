e138e96b3b58b669c4b43df64ae154d9ba8d4b4e
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 19:52:42 +0000

    3ac2c7b feat(docs-infra): add Angie mascot to the not found page

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.4+sha-eac04cf
+ * @license Angular v22.1.0-next.4+sha-3ac2c7b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.4+sha-eac04cf');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.4+sha-3ac2c7b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
