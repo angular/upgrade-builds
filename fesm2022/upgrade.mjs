da7190e56b2b6d90202163f8b2708de2b08198a2
leonsenft <leonsenft@users.noreply.github.com>
2026-07-08 18:35:16 +0000

    ab18f5f refactor(core): cleanup Meta service

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.5+sha-2d52d53
+ * @license Angular v22.0.5+sha-ab18f5f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.5+sha-2d52d53');
+const VERSION = /* @__PURE__ */new Version('22.0.5+sha-ab18f5f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
