16adca1886f0d8022e3e4ccb78e541b01c26cebd
Matthew Beck <me@mattrb.com>
2026-07-09 17:06:33 +0000

    53ca8c2 test(language-service): empty template literal interpolation

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.5+sha-5bd00ad
+ * @license Angular v22.1.0-next.5+sha-53ca8c2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-5bd00ad');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-53ca8c2');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
