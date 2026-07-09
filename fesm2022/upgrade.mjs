c71791bf91460267a848f5a9e1c5f16ecf7be31f
Matthew Beck <me@mattrb.com>
2026-07-09 19:11:02 +0000

    cd6b903 feat(docs-infra): add Angie to the embedded editor loading steps

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.6+sha-aa03cb9
+ * @license Angular v22.0.6+sha-cd6b903
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.6+sha-aa03cb9');
+const VERSION = /* @__PURE__ */new Version('22.0.6+sha-cd6b903');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
