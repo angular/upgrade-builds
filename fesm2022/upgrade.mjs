23cc232bc12c3f7db4128c9ec5593474eaeb2294
Matthew Beck <me@mattrb.com>
2026-07-09 23:17:59 +0000

    97c830b ci: fix workflows broken by renamed command

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.6+sha-8ce1fcf
+ * @license Angular v22.0.6+sha-97c830b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.6+sha-8ce1fcf');
+const VERSION = /* @__PURE__ */new Version('22.0.6+sha-97c830b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
