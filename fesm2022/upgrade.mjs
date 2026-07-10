63e66d3ebbcb7b95fc746d17bb0a25a11eb419ab
GitHub <noreply@github.com>
2026-07-10 21:34:11 +0000

    f48d276 refactor(devtools): add static nodes for html-only block content in the component tree

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.5+sha-72cc548
+ * @license Angular v22.1.0-next.5+sha-f48d276
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-72cc548');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-f48d276');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
