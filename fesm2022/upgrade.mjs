60493b780298e379364bfbfd47b1d141fffb761c
Matthew Beck <me@mattrb.com>
2026-07-10 01:06:06 +0000

    c82ae67 Revert "fix(core): allow static attributes for explicit input transforms"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.6+sha-97c830b
+ * @license Angular v22.0.6+sha-c82ae67
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.6+sha-97c830b');
+const VERSION = /* @__PURE__ */new Version('22.0.6+sha-c82ae67');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
