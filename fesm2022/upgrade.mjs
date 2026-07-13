5aeed86610a8ea691d6e3f6ff2df0cd28c1525ba
Matthew Beck <me@mattrb.com>
2026-07-13 20:07:33 +0000

    7658a81 docs: Add mention that about signal narrowing on `@switch` blocks

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.6+sha-ac5f476
+ * @license Angular v22.0.6+sha-7658a81
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.6+sha-ac5f476');
+const VERSION = /* @__PURE__ */new Version('22.0.6+sha-7658a81');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
