0d9b9604a8e59352df67dcb9aba5ded8dea4f228
Matthew Beck <me@mattrb.com>
2026-07-13 19:00:56 +0000

    ac5f476 build: update pnpm to v10.34.5

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.6+sha-cfb77d6
+ * @license Angular v22.0.6+sha-ac5f476
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.6+sha-cfb77d6');
+const VERSION = /* @__PURE__ */new Version('22.0.6+sha-ac5f476');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
