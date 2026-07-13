28ec51456cf29d3a4cf0b50f48da9ef5eb602b0a
Matthew Beck <me@mattrb.com>
2026-07-13 21:53:58 +0000

    d5e0b13 feat(docs-infra): show Angie in the error snack bar

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.5+sha-d55f5c4
+ * @license Angular v22.1.0-next.5+sha-d5e0b13
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-d55f5c4');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-d5e0b13');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
