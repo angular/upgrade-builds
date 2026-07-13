f84cf292ca6d6255511dfb2e8025393e27eb4cc6
Matthew Beck <me@mattrb.com>
2026-07-13 15:18:54 +0000

    13d9cc3 refactor: remove obsolete @types/babel__core dependency

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.5+sha-f48d276
+ * @license Angular v22.1.0-next.5+sha-13d9cc3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-f48d276');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-13d9cc3');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
