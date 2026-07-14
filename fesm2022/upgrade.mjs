5c777d1e5a500c0612e97c82ac31c0cefbf9f047
Matthew Beck <me@mattrb.com>
2026-07-14 15:07:54 +0000

    2695290 docs: add native html validation clarification in signal forms docs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.6+sha-d726aa8
+ * @license Angular v22.0.6+sha-2695290
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.6+sha-d726aa8');
+const VERSION = /* @__PURE__ */new Version('22.0.6+sha-2695290');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
