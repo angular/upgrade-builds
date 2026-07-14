54a27d4b2ad9894cd81f4e31c04d5a9b015a2a9d
Matthew Beck <me@mattrb.com>
2026-07-14 16:29:16 +0000

    6d043f8 fix(http): prevent interceptor signal reads from leaking into calling reactive contexts

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.5+sha-ca37790
+ * @license Angular v22.1.0-next.5+sha-6d043f8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-ca37790');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-6d043f8');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
