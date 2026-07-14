04ae225b2afedb0e663c473d747ff9c9e6601649
Matthew Beck <me@mattrb.com>
2026-07-14 18:19:15 +0000

    e881b96 refactor(core): move signal debug graph interfaces to primitives/devtools

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.6+sha-20b7dc3
+ * @license Angular v22.0.6+sha-e881b96
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.6+sha-20b7dc3');
+const VERSION = /* @__PURE__ */new Version('22.0.6+sha-e881b96');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
