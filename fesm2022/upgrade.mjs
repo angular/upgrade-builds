de07e1b1deb22d77e58267fbc2c987a31af62072
Matthew Beck <me@mattrb.com>
2026-07-15 21:57:45 +0000

    f034c02 build: update cross-repo angular dependencies to v22.1.0-next.4

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.5+sha-d7a13b9
+ * @license Angular v22.1.0-next.5+sha-f034c02
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-d7a13b9');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-f034c02');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
