19c3bba28ad1d4a117740c69ec9a8f18cdfdebf1
Matthew Beck <me@mattrb.com>
2026-07-15 19:11:51 +0000

    0ae6d81 fix(core): preserve explicit input transform write type

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.5+sha-d997a96
+ * @license Angular v22.1.0-next.5+sha-0ae6d81
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-d997a96');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-0ae6d81');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
