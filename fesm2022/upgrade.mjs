cbd055e6768478dfd5156b591d3eca3670bce652
Matthew Beck <me@mattrb.com>
2026-07-16 00:50:54 +0000

    d43acaf ci: harden workflow against expression injection

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.5+sha-2e32872
+ * @license Angular v22.1.0-next.5+sha-d43acaf
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-2e32872');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-d43acaf');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
