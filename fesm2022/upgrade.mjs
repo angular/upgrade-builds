34d2ee767e5d62e4d1e1bc300117a5cbc28d422b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-20 12:46:39 +0000

    9d40f8a fix(http): prevent transfer cache key collisions

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.7+sha-e02a0de
+ * @license Angular v22.0.7+sha-9d40f8a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.7+sha-e02a0de');
+const VERSION = /* @__PURE__ */new Version('22.0.7+sha-9d40f8a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
