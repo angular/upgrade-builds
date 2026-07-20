8b0056dd9e48378f38e5a7880d59c98a2f7351ac
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-20 08:47:14 +0000

    e14ead1 Revert "fix(forms): preserve intermediate number values in signal forms"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.7+sha-f8df06d
+ * @license Angular v22.0.7+sha-e14ead1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.7+sha-f8df06d');
+const VERSION = /* @__PURE__ */new Version('22.0.7+sha-e14ead1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
