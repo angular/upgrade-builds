2dc41bffce6d310baa4a2e5671a870250a4ac5c2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-20 14:10:24 +0000

    79fe1c3 fix(docs-infra): only claim a search on the 404 page when results exist

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.6+sha-3192dcc
+ * @license Angular v22.1.0-next.6+sha-79fe1c3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-3192dcc');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-79fe1c3');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
