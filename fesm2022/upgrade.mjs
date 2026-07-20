67318005ab1d7eec81fa676f51a38708a89ea0f5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-20 09:56:43 +0000

    d6cad90 docs: fix broken link to Angular AI Skills page

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.6+sha-f032f5a
+ * @license Angular v22.1.0-next.6+sha-d6cad90
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-f032f5a');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-d6cad90');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
