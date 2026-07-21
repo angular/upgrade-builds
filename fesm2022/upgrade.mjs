6cca4b1ffc3e7ccd3fc4658095b07890d5a22b19
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-21 11:58:27 +0000

    3497c9b fix(forms):  ensure `pending` status propagates to the root form in signal forms

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.6+sha-22aecf5-with-local-changes
+ * @license Angular v22.1.0-next.6+sha-3497c9b-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-22aecf5-with-local-changes');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-3497c9b-with-local-changes');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
