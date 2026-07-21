abe4c1af1a01ea3ccdeecc24036af1c962a596d7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-21 09:17:13 +0000

    7eec520 test(compiler-cli): cover DOM-only vs full instruction set across compilation modes

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.7+sha-f542a09
+ * @license Angular v22.0.7+sha-7eec520
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.7+sha-f542a09');
+const VERSION = /* @__PURE__ */new Version('22.0.7+sha-7eec520');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
