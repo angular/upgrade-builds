819eee7cffb2cda1fe7d30e70b4d58010c4fffe1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-21 17:34:27 +0000

    49672c4 fix(migrations): correctly migrate ngClass with mixed space-separated keys

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.6+sha-9bf8b8c
+ * @license Angular v22.1.0-next.6+sha-49672c4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-9bf8b8c');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-49672c4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
