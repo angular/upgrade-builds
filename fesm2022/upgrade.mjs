a3e344e6715bd3b964f9a6573251f83d67a77590
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 12:32:47 +0000

    45eef01 fix(docs-infra): fail the guide build when a markdown file starts with a BOM

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.7+sha-bc3ff3b
+ * @license Angular v22.0.7+sha-45eef01
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.7+sha-bc3ff3b');
+const VERSION = /* @__PURE__ */new Version('22.0.7+sha-45eef01');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
