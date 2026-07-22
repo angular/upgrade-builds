f89d7c544780da8f5ccef68418740f46b9a06b2f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 10:07:57 +0000

    e62905e docs: update stale Twitter reference to X in the URL matcher guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.7+sha-66f27a4
+ * @license Angular v22.0.7+sha-e62905e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.7+sha-66f27a4');
+const VERSION = /* @__PURE__ */new Version('22.0.7+sha-e62905e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
