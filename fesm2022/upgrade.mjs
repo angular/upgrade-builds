670468b710d99f47ea2ff6f09c84d5c5b54c9028
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 10:40:02 +0000

    e23541b fix(zone.js): vitest patching of describe/it curried calls

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-next.6+sha-6557df5
+ * @license Angular v22.1.0-next.6+sha-e23541b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-6557df5');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-e23541b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
