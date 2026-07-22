915cb76c5e1032d62a9f40cc80ab687ae382e89b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 09:58:53 +0000

    66f27a4 docs: add skills for Angular pipes

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.7+sha-388daea
+ * @license Angular v22.0.7+sha-66f27a4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.7+sha-388daea');
+const VERSION = /* @__PURE__ */new Version('22.0.7+sha-66f27a4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
