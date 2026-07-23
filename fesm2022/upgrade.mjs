d0c64d873d516c931ad96bc572168b58156001da
Douglas Parker <dgp1130@users.noreply.github.com>
2026-07-23 19:09:43 +0000

    e779309 release: bump Angular DevTools version to 1.18.0

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.0+sha-3bf2430
+ * @license Angular v22.2.0-next.0+sha-e779309
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-3bf2430');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-e779309');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
