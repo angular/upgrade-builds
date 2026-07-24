2106a3e765450c73185b9f6557cb3d8a02790a07
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-24 15:34:33 +0000

    125a794 docs: use https for external links in adev

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0-rc.0+sha-1b85b3c
+ * @license Angular v22.1.0-rc.0+sha-125a794
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-rc.0+sha-1b85b3c');
+const VERSION = /* @__PURE__ */new Version('22.1.0-rc.0+sha-125a794');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
