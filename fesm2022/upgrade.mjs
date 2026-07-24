6ca945e37a8cb0128fd4d4368cddac4934acae19
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-24 20:53:45 +0000

    232cfea fix(language-service): account for strictTemplates being enabled by default

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.8+sha-0650966
+ * @license Angular v22.0.8+sha-232cfea
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.8+sha-0650966');
+const VERSION = /* @__PURE__ */new Version('22.0.8+sha-232cfea');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
