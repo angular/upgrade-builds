c0eb3440164feeb283b880c9f0cf809b2c1fd388
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-24 17:42:26 +0000

    1784530 docs: modernize directives guides (#69822)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.0+sha-28d59e8
+ * @license Angular v22.2.0-next.0+sha-1784530
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-28d59e8');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-1784530');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
