024ebf6d324f5989dbdade95640f6e34a03b83cf
Alan Agius <alan.agius4@gmail.com>
2026-07-29 20:59:09 +0000

    c6e4d5a release: cut the v22.2.0-next.0 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.0+sha-9386c89
+ * @license Angular v22.2.0-next.0+sha-c6e4d5a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-9386c89');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-c6e4d5a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
