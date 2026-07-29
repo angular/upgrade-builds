7c1bcb3946fffd1cc004de5e08ba1d784d43bf9e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-29 17:21:06 +0000

    0d26130 docs: invoke signal inputs in adev examples and tutorials

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.0+sha-21eed5f
+ * @license Angular v22.2.0-next.0+sha-0d26130
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-21eed5f');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-0d26130');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
