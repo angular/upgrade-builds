7e5c40464bb047e2752002a83cd70ea288dd3a2f
Douglas Parker <dgp1130@users.noreply.github.com>
2026-07-29 23:58:58 +0000

    1a2bcb2 fix(devtools): avoid initializing profiler in production mode

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.0+sha-c6e4d5a
+ * @license Angular v22.2.0-next.0+sha-1a2bcb2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-c6e4d5a');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-1a2bcb2');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
