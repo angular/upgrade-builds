e84d4e07efc0baa30d5a7bebc5c363b7ec6ab855
Alan Agius <alan.agius4@gmail.com>
2026-07-29 20:56:04 +0000

    07cb840 release: cut the v21.2.19 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.18+sha-7b884f5
+ * @license Angular v21.2.19+sha-07cb840
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.18+sha-7b884f5');
+const VERSION = /* @__PURE__ */new Version('21.2.19+sha-07cb840');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
