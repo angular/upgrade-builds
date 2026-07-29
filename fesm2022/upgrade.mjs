466250731c3854b1961b2016d3154b9346d7985e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-29 16:56:10 +0000

    f90690f docs: fix self-referential link in defer block description

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.0.8+sha-45d7de4
+ * @license Angular v22.0.8+sha-f90690f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.0.8+sha-45d7de4');
+const VERSION = /* @__PURE__ */new Version('22.0.8+sha-f90690f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
