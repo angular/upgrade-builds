c93f5772cf6e642e8b8bb04799657b6129c56fd3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-29 16:18:36 +0000

    9182253 fix(docs-infra): fix malformed --webgl-page-background declaration

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.0+sha-5ad8231
+ * @license Angular v22.2.0-next.0+sha-9182253
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-5ad8231');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-9182253');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
