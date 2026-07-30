1395df61f86e5f8b306747f513ec26f3f1589224
GitHub <noreply@github.com>
2026-07-30 15:36:54 +0000

    6afe6fa fix(core): sanitize host bindings on concrete hosts

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.19+sha-07cb840
+ * @license Angular v21.2.19+sha-6afe6fa
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.19+sha-07cb840');
+const VERSION = /* @__PURE__ */new Version('21.2.19+sha-6afe6fa');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
