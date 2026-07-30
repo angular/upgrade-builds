95b8a61261bf03642346b0068b3c4c5c74f5713b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-07-30 22:29:23 +0000

    a37c79d docs: remove duplicate aria-hidden attributes in aria examples

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.0+sha-b63b903
+ * @license Angular v22.2.0-next.0+sha-a37c79d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-b63b903');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-a37c79d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
