b5a48bb4a4eea9343a789c9f7c7e9fdcbbb8e65e
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-07-30 16:15:22 +0000

    949de9c refactor(forms): modernize reactive forms tests to rely on `whenStable`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0+sha-eb10e93
+ * @license Angular v22.1.0+sha-949de9c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0+sha-eb10e93');
+const VERSION = /* @__PURE__ */new Version('22.1.0+sha-949de9c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
