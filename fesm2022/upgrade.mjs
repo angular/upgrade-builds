6b556182fc4e328d93e8535a0f36d3df6640afdc
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-07-30 15:28:55 +0000

    c4b1432 docs: document overlay stacking change in v21 update guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0+sha-d366e4b
+ * @license Angular v22.1.0+sha-c4b1432
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0+sha-d366e4b');
+const VERSION = /* @__PURE__ */new Version('22.1.0+sha-c4b1432');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
