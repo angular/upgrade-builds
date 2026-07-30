89307f031d8ff2903b1d81c7f34785b3f7a4bd63
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-07-30 16:04:47 +0000

    eb10e93 refactor(common): modernize pipes & non bindable tests to rely on `whenStable`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0+sha-644455b
+ * @license Angular v22.1.0+sha-eb10e93
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0+sha-644455b');
+const VERSION = /* @__PURE__ */new Version('22.1.0+sha-eb10e93');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
