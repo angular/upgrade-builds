8dc691356acd3f5452f3a378985db1499d7d96f8
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-07-31 18:26:25 +0000

    3a29329 fix(devtools): Extension name for non googlers.

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.0+sha-690eecd
+ * @license Angular v22.2.0-next.0+sha-3a29329
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-690eecd');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-3a29329');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
