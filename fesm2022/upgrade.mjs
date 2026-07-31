ea3860cf81db9853da167a98ac5b53c4b5d0c405
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-07-31 22:42:00 +0000

    3d0067a refactor(animations): remove outdated comment

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0+sha-aa6d318
+ * @license Angular v22.1.0+sha-3d0067a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0+sha-aa6d318');
+const VERSION = /* @__PURE__ */new Version('22.1.0+sha-3d0067a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
