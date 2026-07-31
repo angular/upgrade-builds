af3325b162c9d6b85fb5f88790f0479923234b15
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-07-31 15:47:17 +0000

    d6ec7c7 docs: document deprecated allowSignalWrites option in v19 update guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0+sha-a13b968
+ * @license Angular v22.1.0+sha-d6ec7c7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0+sha-a13b968');
+const VERSION = /* @__PURE__ */new Version('22.1.0+sha-d6ec7c7');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
