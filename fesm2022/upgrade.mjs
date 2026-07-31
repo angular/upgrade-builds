cd6b2ca48f9aba4d392553819ebdc6c87c153fce
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-07-31 22:23:11 +0000

    caf6166 fix(http): run root interceptors in the terminal request chain

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.19+sha-6afe6fa
+ * @license Angular v21.2.19+sha-caf6166
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.19+sha-6afe6fa');
+const VERSION = /* @__PURE__ */new Version('21.2.19+sha-caf6166');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
