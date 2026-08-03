ee291f88bfa4655b8bb52ef5f94c16859507e34d
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-03 16:27:05 +0000

    baff4d0 fix(docs-infra): keep inline code in links as an atomic inline box

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0+sha-6b643ab
+ * @license Angular v22.1.0+sha-baff4d0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0+sha-6b643ab');
+const VERSION = /* @__PURE__ */new Version('22.1.0+sha-baff4d0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
