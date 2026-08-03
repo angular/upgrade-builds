5fc7e6cf267c9fa8e8e92b41c4a2385bf445fedd
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-03 16:06:01 +0000

    f2f1292 docs: fix roadmap link text and preview-site link

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.0+sha-1b13db7
+ * @license Angular v22.2.0-next.0+sha-f2f1292
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-1b13db7');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-f2f1292');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
