09f2f779f9c4adf247a244e6509b63c7077ba0d5
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-03 16:52:00 +0000

    3478df7 refactor(compiler): remove unused code and exports

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0+sha-832ff2a
+ * @license Angular v22.1.0+sha-3478df7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0+sha-832ff2a');
+const VERSION = /* @__PURE__ */new Version('22.1.0+sha-3478df7');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
