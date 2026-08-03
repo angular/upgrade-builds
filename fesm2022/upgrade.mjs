83332cb36d12982f83999e87f9fded0ae7f96af2
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-03 16:38:52 +0000

    832ff2a test(platform-server): add SSR integration test for null and normal input values

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0+sha-baff4d0
+ * @license Angular v22.1.0+sha-832ff2a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0+sha-baff4d0');
+const VERSION = /* @__PURE__ */new Version('22.1.0+sha-832ff2a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
