d5fabbf6bf2117f1bd1bf37df6198c2f3afa0548
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-04 16:50:25 +0000

    ac3728e fix(http): avoid aborting completed requests in FetchBackend

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0+sha-f99e916
+ * @license Angular v22.1.0+sha-ac3728e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0+sha-f99e916');
+const VERSION = /* @__PURE__ */new Version('22.1.0+sha-ac3728e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
