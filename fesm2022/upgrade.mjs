ca293e07d41534b4f3e797b67c79c8d680584ef2
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-04 17:40:14 +0000

    6f86d66 docs: fix supported Node.js version in installation prerequisites

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0+sha-ac3728e
+ * @license Angular v22.1.0+sha-6f86d66
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0+sha-ac3728e');
+const VERSION = /* @__PURE__ */new Version('22.1.0+sha-6f86d66');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
