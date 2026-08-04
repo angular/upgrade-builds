fa519837a7b06ad19e7784a4ac688e6831ff4746
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-04 15:51:43 +0000

    f99e916 refactor(common): modernize directive tests to rely on whenStable

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0+sha-32af9b5
+ * @license Angular v22.1.0+sha-f99e916
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0+sha-32af9b5');
+const VERSION = /* @__PURE__ */new Version('22.1.0+sha-f99e916');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
