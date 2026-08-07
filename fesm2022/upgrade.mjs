656cc5ada0c8c3421c5f49133006066951419b4b
Alon Mishne <amishne@users.noreply.github.com>
2026-08-07 23:42:51 +0000

    4d985a1 fix(core): incorrect loop in defer blocks

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.1+sha-7c15737
+ * @license Angular v22.1.1+sha-4d985a1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.1+sha-7c15737');
+const VERSION = /* @__PURE__ */new Version('22.1.1+sha-4d985a1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
