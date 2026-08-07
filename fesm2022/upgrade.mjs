14976c389bdf5363563976fee0c87e2685a2c605
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-07 23:29:53 +0000

    4631a6e refactor(forms): modernize Signal Forms tests

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.1+sha-adb770b
+ * @license Angular v22.2.0-next.1+sha-4631a6e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-adb770b');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-4631a6e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
