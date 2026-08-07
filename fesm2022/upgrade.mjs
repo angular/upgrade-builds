6d6fced7299609a4779d3eee07a936a287cc66eb
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-07 16:37:56 +0000

    7c15737 release: cut the v22.1.1 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.0+sha-6f86d66
+ * @license Angular v22.1.1+sha-7c15737
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0+sha-6f86d66');
+const VERSION = /* @__PURE__ */new Version('22.1.1+sha-7c15737');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
