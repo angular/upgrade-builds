33e3c765229d0f46f008f6ea033d6a4fe83451f3
Joey Perrott <josephperrott@gmail.com>
2026-08-07 17:29:18 +0000

    8a40fbe release: cut the v22.2.0-next.1 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.0+sha-96b8042
+ * @license Angular v22.2.0-next.1+sha-8a40fbe
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-96b8042');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-8a40fbe');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
