023f2f5aad1b523cedf57fd70ff375d56d25a38f
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-07 17:38:10 +0000

    cf1844b refactor(devtools): use JavaScriptTransformer for Angular optimization esbuild plugin

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.1+sha-8a40fbe
+ * @license Angular v22.2.0-next.1+sha-cf1844b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-8a40fbe');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-cf1844b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
