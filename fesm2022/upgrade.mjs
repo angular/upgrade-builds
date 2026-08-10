daf0c17d882bde959ca5376c02b78dfb7c220cd1
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-10 22:43:56 +0000

    8925a4e fix(core): ensure i18n_util hasOwnProperty checks are safe for property renaming

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.1+sha-4fc6e31
+ * @license Angular v22.2.0-next.1+sha-8925a4e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-4fc6e31');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-8925a4e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
