3ec122f0db9f64693e17dc7e35ba68c94f57d6ea
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-10 16:49:27 +0000

    596356e docs: update cross-repo adev docs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.1+sha-4d985a1
+ * @license Angular v22.1.1+sha-596356e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.1+sha-4d985a1');
+const VERSION = /* @__PURE__ */new Version('22.1.1+sha-596356e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
