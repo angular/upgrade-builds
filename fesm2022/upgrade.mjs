ec05db29817f0cb5c7c507af755384374cf3a459
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-11 19:16:23 +0000

    435f8b2 fix(router): limit protocol-relative URL handling to serialization

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.1+sha-b65dea4
+ * @license Angular v22.2.0-next.1+sha-435f8b2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-b65dea4');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-435f8b2');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
