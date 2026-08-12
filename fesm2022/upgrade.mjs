86782b042a1f0164ccfd7bf2c1baf6ec7a550bde
Alon Mishne <amishne@users.noreply.github.com>
2026-08-12 23:24:58 +0000

    e9660b1 fix(compiler-cli): correctly resolve symbol for SafePropertyRead in chained optional navigation

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.1+sha-3032e7f
+ * @license Angular v22.1.1+sha-e9660b1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.1+sha-3032e7f');
+const VERSION = /* @__PURE__ */new Version('22.1.1+sha-e9660b1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
