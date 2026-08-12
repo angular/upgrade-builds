08bc94f2a59a464418987a883d8eda56afb48b75
Alon Mishne <amishne@users.noreply.github.com>
2026-08-12 18:19:35 +0000

    299607d docs: clarify animate.leave child animation behavior

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.1+sha-2f82601
+ * @license Angular v22.1.1+sha-299607d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.1+sha-2f82601');
+const VERSION = /* @__PURE__ */new Version('22.1.1+sha-299607d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
