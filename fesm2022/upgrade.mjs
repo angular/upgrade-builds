1ef54b46cfe2337baae6d763fb2d0da28c775228
GitHub <noreply@github.com>
2026-08-12 22:45:04 +0000

    3032e7f release: bump VSCode extension version to 22.1.0 (#70171)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.1+sha-299607d
+ * @license Angular v22.1.1+sha-3032e7f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.1+sha-299607d');
+const VERSION = /* @__PURE__ */new Version('22.1.1+sha-3032e7f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
