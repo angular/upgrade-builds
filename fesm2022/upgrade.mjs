3bd71a3a692d5bd27e3eaec63763bec21c440b46
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-13 17:23:18 +0000

    044e69c docs: release notes for the v20.3.28 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.1+sha-16e42c3
+ * @license Angular v22.2.0-next.1+sha-044e69c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-16e42c3');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-044e69c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
