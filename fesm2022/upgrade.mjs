a2dd814dd52b1591328c2d293773ba6764e26f91
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-13 16:05:18 +0000

    0a708e9 docs: note SafeResourceUrl audio src change

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.1+sha-910f391
+ * @license Angular v22.1.1+sha-0a708e9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.1+sha-910f391');
+const VERSION = /* @__PURE__ */new Version('22.1.1+sha-0a708e9');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
