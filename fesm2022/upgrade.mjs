94979a33f9079ba8d3c663fac16c23d2b45f36ce
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-13 19:14:51 +0000

    5ed071d docs: release notes for the v22.1.2 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.2+sha-40d23fa
+ * @license Angular v22.2.0-next.2+sha-5ed071d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-40d23fa');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-5ed071d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
