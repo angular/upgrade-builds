cd1238dcbd06bc3d32dbcb3b2c6b9210d4edd350
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-13 16:17:08 +0000

    16e42c3 build: update all non-major dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.1+sha-647bf8e
+ * @license Angular v22.2.0-next.1+sha-16e42c3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-647bf8e');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-16e42c3');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
