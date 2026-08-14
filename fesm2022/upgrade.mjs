35e8b5f36289df64d50b49b3689a7418bfdbab63
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-14 15:30:07 +0000

    0cd243d build: resolve CI failures

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.2+sha-a9f8fb2
+ * @license Angular v22.1.2+sha-0cd243d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.2+sha-a9f8fb2');
+const VERSION = /* @__PURE__ */new Version('22.1.2+sha-0cd243d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
