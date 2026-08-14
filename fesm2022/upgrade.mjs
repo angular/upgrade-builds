b765c95077b85076d888c22940ea02b8cbc4de1d
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-14 16:36:53 +0000

    ec678c0 build: update dependency ng-packagr to v22.2.0-next.3

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.2+sha-c6e4a36
+ * @license Angular v22.2.0-next.2+sha-ec678c0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-c6e4a36');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-ec678c0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
