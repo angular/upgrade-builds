837598906c7da09dd92e4477ed59c1b47c3041db
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-14 15:40:40 +0000

    c6e4a36 fix(core): throw a descriptive error instead of crashing when a hydration node is missing

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.2+sha-d3d3bc6
+ * @license Angular v22.2.0-next.2+sha-c6e4a36
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-d3d3bc6');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-c6e4a36');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
