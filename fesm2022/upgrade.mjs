552aed76ccfb1f7d1f7a645636923eb1dd55a7c9
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-17 22:17:44 +0000

    b3c78a5 fix(common): preserve literal key union in KeyValuePipe.transform()

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.2+sha-5cb4ea7
+ * @license Angular v22.1.2+sha-b3c78a5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.2+sha-5cb4ea7');
+const VERSION = /* @__PURE__ */new Version('22.1.2+sha-b3c78a5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
