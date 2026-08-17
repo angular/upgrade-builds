df81340702312dfa49ecd47c0ba6226d93e46ad0
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-17 17:54:01 +0000

    2d87ed9 docs: update Angular MCP skill reference

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.2+sha-c04931c
+ * @license Angular v22.1.2+sha-2d87ed9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.2+sha-c04931c');
+const VERSION = /* @__PURE__ */new Version('22.1.2+sha-2d87ed9');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
