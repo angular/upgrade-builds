2cd0ce961e52332c69ba0420af4112e7508baff8
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-17 20:37:48 +0000

    0849fac docs: add example for conditional redirects in routing

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.2+sha-2d87ed9
+ * @license Angular v22.1.2+sha-0849fac
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.2+sha-2d87ed9');
+const VERSION = /* @__PURE__ */new Version('22.1.2+sha-0849fac');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
