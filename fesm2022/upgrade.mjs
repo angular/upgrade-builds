d8f04971629b9a575c6b8b02b4e4b3061803cbf2
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-17 20:42:32 +0000

    1c2b057 feat(docs-infra): add Angie to the roadmap illustration

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.2+sha-789c9c2
+ * @license Angular v22.2.0-next.2+sha-1c2b057
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-789c9c2');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-1c2b057');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
--- types/static-testing.d.ts
+++ types/static-testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.2+sha-789c9c2
+ * @license Angular v22.2.0-next.2+sha-1c2b057
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
