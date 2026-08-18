37b53862434407c4398ddfaa6648d3ea571b03f4
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-18 16:11:12 +0000

    85f12a5 fix(core): stop running further effects once one destroys the view mid-flush

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.2+sha-b3c78a5
+ * @license Angular v22.1.2+sha-85f12a5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.2+sha-b3c78a5');
+const VERSION = /* @__PURE__ */new Version('22.1.2+sha-85f12a5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
