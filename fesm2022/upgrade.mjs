47f79e543927d80a6ecc34fa0b34cff1aaf7a37c
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-18 16:53:26 +0000

    d364c83 ci: exclude markdown files from requiring dev-infra review

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.2+sha-732e505
+ * @license Angular v22.2.0-next.2+sha-d364c83
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-732e505');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-d364c83');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
