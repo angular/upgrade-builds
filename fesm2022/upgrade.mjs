fba680babdd385fc181b872b5d8a17b9279cd614
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-18 19:16:17 +0000

    2720362 feat(router): add containsTree as public API

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.2+sha-d364c83
+ * @license Angular v22.2.0-next.2+sha-2720362
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-d364c83');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-2720362');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
