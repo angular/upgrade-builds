df7d8f229c49fe66bdb907460f3778010f33a539
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-18 23:04:53 +0000

    53fc371 fix(compiler): preserve &ngsp; between sibling control flow blocks

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.2+sha-f1a4c85
+ * @license Angular v22.2.0-next.2+sha-53fc371
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-f1a4c85');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-53fc371');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
