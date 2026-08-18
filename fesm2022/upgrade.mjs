e64c74537753c922c351b9dd255eec62b7ccf09a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-18 22:15:06 +0000

    f1a4c85 fix(router): pass correct component to canDeactivate for named outlets in componentless parent routes

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.2+sha-2720362
+ * @license Angular v22.2.0-next.2+sha-f1a4c85
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-2720362');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-f1a4c85');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
