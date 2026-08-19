19661d547aa370353cd43361a99c58421abea70d
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-19 19:54:29 +0000

    e041f94 refactor(core): pass AbortSignal to WebMCP tools to handle cancellation

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.2+sha-7e39b4a
+ * @license Angular v22.2.0-next.2+sha-e041f94
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-7e39b4a');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-e041f94');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
