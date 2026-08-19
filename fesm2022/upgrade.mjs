734003c2b65f18abc5494090da1f806311f72ede
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-19 15:38:04 +0000

    84a210e refactor(router): add support for non-blocking router resources (#70211)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.2+sha-83450d2
+ * @license Angular v22.1.2+sha-84a210e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.2+sha-83450d2');
+const VERSION = /* @__PURE__ */new Version('22.1.2+sha-84a210e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
