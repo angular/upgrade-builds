e65ac28020ae0343c9db6ef249b60a5dbc669b6f
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-19 00:18:32 +0000

    83450d2 fix(forms): report forbidden 2way bindings on when `FormField` is applied

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.2+sha-94f0b9a
+ * @license Angular v22.1.2+sha-83450d2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.2+sha-94f0b9a');
+const VERSION = /* @__PURE__ */new Version('22.1.2+sha-83450d2');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
