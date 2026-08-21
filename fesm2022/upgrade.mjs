601df29cf78a0649f355b062beae1b85b46480a7
leonsenft <leonsenft@users.noreply.github.com>
2026-08-21 19:16:12 +0000

    ee63c0a refactor(docs-infra): hold editor diagnostics in a signal

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.3+sha-004cf3a
+ * @license Angular v22.1.3+sha-ee63c0a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.3+sha-004cf3a');
+const VERSION = /* @__PURE__ */new Version('22.1.3+sha-ee63c0a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
