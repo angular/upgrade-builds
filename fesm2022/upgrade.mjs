e09a0a54a2db9eedd87592866b2fbfa259b58b36
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-21 18:23:01 +0000

    2e2c426 fix(compiler-cli): deduplicate deferred imports across multiple blocks

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.3+sha-d609cf6
+ * @license Angular v22.2.0-next.3+sha-2e2c426
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-d609cf6');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-2e2c426');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
