32a1a21bc0dd173a6488344ec35addc4878808ec
leonsenft <leonsenft@users.noreply.github.com>
2026-08-24 22:40:30 +0000

    7c752d4 fix(core): prevent TransferState prototype pollution

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.3+sha-ba3bc47
+ * @license Angular v22.1.3+sha-7c752d4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.3+sha-ba3bc47');
+const VERSION = /* @__PURE__ */new Version('22.1.3+sha-7c752d4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
