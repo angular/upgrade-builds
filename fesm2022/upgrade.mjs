c221f0729f0deda4f00ad2908dbd6f9f12e9c77c
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-24 22:43:30 +0000

    82a7675 feat(devtools): add "Watch signal" button to signal-details component

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.3+sha-671d8d3
+ * @license Angular v22.2.0-next.3+sha-82a7675
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-671d8d3');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-82a7675');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
