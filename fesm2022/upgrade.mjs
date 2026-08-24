7a460717380a6749afd27e7ca48327eccfc3d91e
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-24 17:52:59 +0000

    73d8bbd fix(platform-server): update domino to latest version

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.21+sha-992d627
+ * @license Angular v21.2.21+sha-73d8bbd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.21+sha-992d627');
+const VERSION = /* @__PURE__ */new Version('21.2.21+sha-73d8bbd');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
