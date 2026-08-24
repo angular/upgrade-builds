8b24ce73758dc4ba3ddfc236040db99dcdaff76c
leonsenft <leonsenft@users.noreply.github.com>
2026-08-24 17:16:31 +0000

    87af7a4 docs: add self-closing tags guideline to components reference

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.3+sha-64e02d8
+ * @license Angular v22.1.3+sha-87af7a4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.3+sha-64e02d8');
+const VERSION = /* @__PURE__ */new Version('22.1.3+sha-87af7a4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
