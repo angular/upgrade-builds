9eca17dc026c8ae83f4d2165427289a51a2bbfa3
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-25 23:28:28 +0000

    a46292a fix(compiler-cli): default template diagnostic related message source file to template

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.3+sha-ddfd8a8
+ * @license Angular v22.2.0-next.3+sha-a46292a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-ddfd8a8');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-a46292a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
