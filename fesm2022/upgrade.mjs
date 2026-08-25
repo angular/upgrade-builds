bb03f22302b58def818a148abcb1262c6581fa7a
leonsenft <leonsenft@users.noreply.github.com>
2026-08-25 22:29:38 +0000

    bf69e85 refactor(docs-infra): remove example configs for tooling that no longer exists

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.3+sha-e3bf968
+ * @license Angular v22.1.3+sha-bf69e85
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.3+sha-e3bf968');
+const VERSION = /* @__PURE__ */new Version('22.1.3+sha-bf69e85');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
