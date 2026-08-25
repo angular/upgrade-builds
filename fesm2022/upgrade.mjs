d3a38054789cdafe0ca9f660b52095db79f2e60a
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-25 17:34:09 +0000

    90fc947 refactor(docs-infra): remove example code that nothing references

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.3+sha-1426f5e
+ * @license Angular v22.2.0-next.3+sha-90fc947
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-1426f5e');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-90fc947');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
