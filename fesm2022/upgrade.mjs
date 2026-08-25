316e05ba553fab3fa1f7bd8f6ec3e2fe013bc271
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-25 23:37:19 +0000

    915a03a fix(core): explicitly reject foreign components in JIT mode

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.3+sha-a46292a
+ * @license Angular v22.2.0-next.3+sha-915a03a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-a46292a');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-915a03a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
