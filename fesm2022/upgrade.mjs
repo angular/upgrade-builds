67385cd2096289a0d6906c1dc266c36977598de9
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-25 17:15:15 +0000

    5aa6d97 fix(platform-server): avoid stripping unicode whitespace during url resolution

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.21+sha-73d8bbd
+ * @license Angular v21.2.21+sha-5aa6d97
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.21+sha-73d8bbd');
+const VERSION = /* @__PURE__ */new Version('21.2.21+sha-5aa6d97');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
