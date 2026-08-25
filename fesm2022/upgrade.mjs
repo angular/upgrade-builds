767011dc76e006b8ce76a8aee4e3d53acf27f64d
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-25 18:36:26 +0000

    3848b7f docs: clarify onSameUrlNavigation reload behavior

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.3+sha-07605ba
+ * @license Angular v22.2.0-next.3+sha-3848b7f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-07605ba');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-3848b7f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
