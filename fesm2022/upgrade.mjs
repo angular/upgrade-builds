dab3d6f86f4a455f663b33c4b81861e8620f62b9
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-26 22:58:36 +0000

    2e6a057 docs: update cross-repo adev docs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.3+sha-c1ac4b7
+ * @license Angular v22.2.0-next.3+sha-2e6a057
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-c1ac4b7');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-2e6a057');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
