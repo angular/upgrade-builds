2326fa913f03889a4e5b240c22b57bcde1cdd2c6
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-26 22:49:49 +0000

    c1ac4b7 refactor(devtools): convert remaining console calls to the custom log

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.3+sha-66d505e
+ * @license Angular v22.2.0-next.3+sha-c1ac4b7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-66d505e');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-c1ac4b7');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
