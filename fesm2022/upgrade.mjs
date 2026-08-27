8d663ec589cf92087db85f27cf22fe17bd7b3d2a
Matthew Beck <me@mattrb.com>
2026-08-27 15:12:43 +0000

    6f51be6 build: pin `vscode-languageserver-textdocument` version

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.4+sha-8983809
+ * @license Angular v22.1.4+sha-6f51be6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.4+sha-8983809');
+const VERSION = /* @__PURE__ */new Version('22.1.4+sha-6f51be6');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
