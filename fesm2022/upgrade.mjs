3bea5bfa6eb1183f6b21494fab2a750e67f2ae5a
Matthew Beck <me@mattrb.com>
2026-08-27 15:10:41 +0000

    c2d4070 build: update all non-major dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.4+sha-133cafd
+ * @license Angular v22.2.0-next.4+sha-c2d4070
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-133cafd');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-c2d4070');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
