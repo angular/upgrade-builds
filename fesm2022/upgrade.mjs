d90c0f6493f77b41b69973e339c46b5008301eee
Matthew Beck <me@mattrb.com>
2026-08-27 20:54:16 +0000

    ad22e89 refactor(devtools): point the .gitignore comment at live GitHub docs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.4+sha-6f1dc26
+ * @license Angular v22.2.0-next.4+sha-ad22e89
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-6f1dc26');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-ad22e89');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
