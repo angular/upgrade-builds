52af9fe48af7665844cd91be4b290c9155327294
Matthew Beck <me@mattrb.com>
2026-08-27 17:39:06 +0000

    4fc45a9 refactor(compiler-cli): index bound directive inputs and outputs in templates

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.4+sha-c2d4070
+ * @license Angular v22.2.0-next.4+sha-4fc45a9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-c2d4070');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-4fc45a9');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
