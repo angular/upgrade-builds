88776c7996005313340f4b96f7a7a1330c51b67d
GitHub <noreply@github.com>
2026-08-27 19:54:19 +0000

    6f1dc26 docs(router): Add router resource docs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.4+sha-4fc45a9
+ * @license Angular v22.2.0-next.4+sha-6f1dc26
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-4fc45a9');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-6f1dc26');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
