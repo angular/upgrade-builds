66d0b37c2872df55b56b64177a744c52c7f3a5b6
Matthew Beck <me@mattrb.com>
2026-08-28 04:15:59 +0000

    34817da refactor(core): defer foreign component rendering to post-update pass

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.4+sha-ad22e89
+ * @license Angular v22.2.0-next.4+sha-34817da
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-ad22e89');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-34817da');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
