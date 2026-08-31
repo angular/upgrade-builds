268c3cc701fa665854a533301738c11ddfe9d715
Matthew Beck <me@mattrb.com>
2026-08-31 22:20:11 +0000

    2ceeb27 fix(core): cancel stale debounce timers to prevent timer leaks

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.4+sha-3b8723c
+ * @license Angular v22.1.4+sha-2ceeb27
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.4+sha-3b8723c');
+const VERSION = /* @__PURE__ */new Version('22.1.4+sha-2ceeb27');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
