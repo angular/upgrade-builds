df7a3dd858c4224ae9120a60a54b5011385a7b13
Matthew Beck <me@mattrb.com>
2026-08-31 18:38:39 +0000

    d9afca0 fix(docs-infra): reject unclosed paired docs elements

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.4+sha-563850e
+ * @license Angular v22.2.0-next.4+sha-d9afca0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-563850e');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-d9afca0');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
