30b2aa962ccee01687a08e13c868adc43badd7b1
Matthew Beck <me@mattrb.com>
2026-08-31 20:37:18 +0000

    3b8723c fix(platform-server): resolve HTTP(S) URLs without authority as relative during SSR

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.4+sha-341bc0d
+ * @license Angular v22.1.4+sha-3b8723c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.4+sha-341bc0d');
+const VERSION = /* @__PURE__ */new Version('22.1.4+sha-3b8723c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
