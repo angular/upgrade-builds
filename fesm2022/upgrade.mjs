6887c80f708ec484b9a2c71b0bab2a0565a178a9
Matthew Beck <me@mattrb.com>
2026-08-31 22:31:09 +0000

    d457cc9 fix(zone.js): preserve accessor and non-enumerable event listener options

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.4+sha-2ceeb27
+ * @license Angular v22.1.4+sha-d457cc9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.4+sha-2ceeb27');
+const VERSION = /* @__PURE__ */new Version('22.1.4+sha-d457cc9');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
