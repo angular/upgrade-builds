b7169611807aefd534c986efec66764941f60193
Matthew Beck <me@mattrb.com>
2026-09-01 03:42:51 +0000

    d8e74e7 docs(docs-infra): improve update checkbox accessibility

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.4+sha-87d38ec
+ * @license Angular v22.2.0-next.4+sha-d8e74e7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-87d38ec');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-d8e74e7');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
