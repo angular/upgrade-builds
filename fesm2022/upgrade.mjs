d08713a567c2f8b2fda648f9b246fe5fe0b46ac0
Matthew Beck <me@mattrb.com>
2026-09-01 21:53:16 +0000

    b2903de ci: move `packages/private` under `fw-general`

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.4+sha-d90698d
+ * @license Angular v22.1.4+sha-b2903de
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.4+sha-d90698d');
+const VERSION = /* @__PURE__ */new Version('22.1.4+sha-b2903de');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
