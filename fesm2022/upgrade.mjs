6b2f30fbfbf53d95555e9ceb3e8f40596a986855
Matthew Beck <me@mattrb.com>
2026-09-02 21:29:16 +0000

    0a1bbbe release: cut the zone.js-0.16.3 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.4+sha-b2903de
+ * @license Angular v22.1.4+sha-0a1bbbe
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.4+sha-b2903de');
+const VERSION = /* @__PURE__ */new Version('22.1.4+sha-0a1bbbe');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
