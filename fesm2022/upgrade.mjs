4742b0adacc68bb035c89c0de6c01fe502c8c9ce
Matthew Beck <me@mattrb.com>
2026-09-02 23:17:15 +0000

    69c8730 fix(vscode-extension): prevent JSDoc link provider injection

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.4+sha-a66fd64
+ * @license Angular v22.2.0-next.4+sha-69c8730
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-a66fd64');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-69c8730');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
