4f0dd66b3f0fbac1fdeea55caf57d1b9d24f75b2
Matthew Beck <me@mattrb.com>
2026-09-03 02:47:04 +0000

    81c3f3a release: cut the v22.2.0-next.5 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.4+sha-502fa13
+ * @license Angular v22.2.0-next.5+sha-81c3f3a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-502fa13');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.5+sha-81c3f3a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
