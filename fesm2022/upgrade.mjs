484b03c3be840ee3efd3d723d316c7d2213b803e
Matthew Beck <me@mattrb.com>
2026-09-03 02:13:14 +0000

    502fa13 fix(docs-infra): ignore external links when mapping navigation items to routes

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.4+sha-69c8730
+ * @license Angular v22.2.0-next.4+sha-502fa13
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-69c8730');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-502fa13');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
