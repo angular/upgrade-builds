df50186680b796e5fcfbc718af4f68c75e8d4701
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-09-04 14:57:04 +0000

    ef48630 test(core): replace act with actAsync

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.5+sha-6f5406c
+ * @license Angular v22.1.5+sha-ef48630-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.5+sha-6f5406c');
+const VERSION = /* @__PURE__ */new Version('22.1.5+sha-ef48630-with-local-changes');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
