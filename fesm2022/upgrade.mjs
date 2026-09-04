7aa768f4584f082b9f1db5aed63533a56b41b7cb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-09-04 14:33:13 +0000

    6f5406c fix(docs-infra): correct example viewer tab state and line numbering

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.5+sha-dfe7be4
+ * @license Angular v22.1.5+sha-6f5406c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.5+sha-dfe7be4');
+const VERSION = /* @__PURE__ */new Version('22.1.5+sha-6f5406c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
