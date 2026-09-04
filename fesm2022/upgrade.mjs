9d01f089c7f11f0bfb490fbef833f4c220b67241
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-09-04 14:33:47 +0000

    bae170d build: update bazel dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.5+sha-74c1716
+ * @license Angular v22.2.0-next.5+sha-bae170d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.5+sha-74c1716');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.5+sha-bae170d');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
