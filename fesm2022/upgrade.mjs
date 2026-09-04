6e5550a4c735ab0e39b39e8749357182aa1023e8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-09-04 14:58:21 +0000

    9a58353 build: update cross-repo angular dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.5+sha-bae170d
+ * @license Angular v22.2.0-next.5+sha-9a58353
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.5+sha-bae170d');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.5+sha-9a58353');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
