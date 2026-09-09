e70e875fc37759c7503ed70c82f55ce1d2d8601f
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-09-09 20:42:58 +0000

    da8dac6 fix(docs-infra): highlight home page code for the resolved theme

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.6+sha-356adf7
+ * @license Angular v22.1.6+sha-da8dac6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.6+sha-356adf7');
+const VERSION = /* @__PURE__ */new Version('22.1.6+sha-da8dac6');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
