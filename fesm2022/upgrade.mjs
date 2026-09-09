0cfb7f9df438350f1dd5193d01e0061cf627a7a2
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-09 20:02:57 +0000

    81385ea release: cut the v21.2.23 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.22+sha-fc2e8fb
+ * @license Angular v21.2.23+sha-81385ea
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.22+sha-fc2e8fb');
+const VERSION = /* @__PURE__ */new Version('21.2.23+sha-81385ea');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
