182d48c67460e584d02be1e67cee771ce5637d2d
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-09 14:36:11 +0000

    fc2e8fb fix(platform-server): update domino to latest version

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.22+sha-4c0bc43
+ * @license Angular v21.2.22+sha-fc2e8fb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.22+sha-4c0bc43');
+const VERSION = /* @__PURE__ */new Version('21.2.22+sha-fc2e8fb');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
