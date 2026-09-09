9807cfa5d7d3b7963a6e76f178d9bb0ff2ceab06
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-09 20:19:38 +0000

    356adf7 release: cut the v22.1.6 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.5+sha-05a05f5
+ * @license Angular v22.1.6+sha-356adf7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.5+sha-05a05f5');
+const VERSION = /* @__PURE__ */new Version('22.1.6+sha-356adf7');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
