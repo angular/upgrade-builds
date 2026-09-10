62ac5c824db1aba68fa72e7f2d22e71de809b781
Andrew Scott <atscott01@gmail.com>
2026-09-10 21:51:06 +0000

    66de56b refactor(compiler-cli): relax nullish coalescing non nullable diagnostics on indexed access

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.6+sha-5e632b6
+ * @license Angular v22.1.6+sha-66de56b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.6+sha-5e632b6');
+const VERSION = /* @__PURE__ */new Version('22.1.6+sha-66de56b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
