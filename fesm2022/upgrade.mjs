327692290f4754e16416b78f5346121e17008cd6
Andrew Scott <atscott01@gmail.com>
2026-09-10 22:03:38 +0000

    b9fb456 docs(forms): close the unterminated code fences in the compatForm docs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.7+sha-9090a75
+ * @license Angular v22.2.0-next.7+sha-b9fb456
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-9090a75');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-b9fb456');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
