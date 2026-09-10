c105f87f38ebeb9d024525ca21db30c7c154c68b
Andrew Scott <atscott01@gmail.com>
2026-09-10 22:15:24 +0000

    29f9dd5 refactor(devtools): update angular optimization plugin for JavaScriptTransformer API

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.7+sha-b9fb456
+ * @license Angular v22.2.0-next.7+sha-29f9dd5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-b9fb456');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-29f9dd5');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
