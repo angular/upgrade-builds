02b16169052eb8afdcdd9ba530a7859aca16d7cb
Andrew Scott <atscott01@gmail.com>
2026-09-10 20:33:33 +0000

    14dbbf9 fix(compiler): wrap `@for` collection expression before appending non-null assertion

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.7+sha-8ca879e
+ * @license Angular v22.2.0-next.7+sha-14dbbf9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-8ca879e');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-14dbbf9');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
