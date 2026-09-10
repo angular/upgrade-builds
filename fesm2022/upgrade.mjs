70b658c7888c32bad4088a5094fbae8f42112c7e
Andrew Scott <atscott01@gmail.com>
2026-09-10 18:22:32 +0000

    4b3f8c2 docs: update cross-repo adev docs

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.7+sha-8576f16
+ * @license Angular v22.2.0-next.7+sha-4b3f8c2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-8576f16');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-4b3f8c2');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
