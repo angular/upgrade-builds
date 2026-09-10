d40aa2e8cf2ebdefe5ac36ba8dcb917a59a994e5
Andrew Scott <atscott01@gmail.com>
2026-09-10 18:44:32 +0000

    8ca879e refactor(forms): expose `consequentialHint` annotation

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.7+sha-4b3f8c2
+ * @license Angular v22.2.0-next.7+sha-8ca879e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-4b3f8c2');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-8ca879e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
