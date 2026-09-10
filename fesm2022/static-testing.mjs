50ca3954fda6ac2af32fe96a11d75736938f5ebe
Andrew Scott <atscott01@gmail.com>
2026-09-10 22:43:38 +0000

    e205075 docs: add error boundaries entries

--- fesm2022/static-testing.mjs
+++ fesm2022/static-testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.7+sha-29f9dd5
+ * @license Angular v22.2.0-next.7+sha-e205075-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -20,7 +20,7 @@ class AngularTestingModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.2.0-next.7+sha-29f9dd5",
+    version: "22.2.0-next.7+sha-e205075-with-local-changes",
     ngImport: i0,
     type: AngularTestingModule,
     deps: [{
@@ -30,13 +30,13 @@ class AngularTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "22.2.0-next.7+sha-29f9dd5",
+    version: "22.2.0-next.7+sha-e205075-with-local-changes",
     ngImport: i0,
     type: AngularTestingModule
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "22.2.0-next.7+sha-29f9dd5",
+    version: "22.2.0-next.7+sha-e205075-with-local-changes",
     ngImport: i0,
     type: AngularTestingModule,
     providers: [{
@@ -47,7 +47,7 @@ class AngularTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.2.0-next.7+sha-29f9dd5",
+  version: "22.2.0-next.7+sha-e205075-with-local-changes",
   ngImport: i0,
   type: AngularTestingModule,
   decorators: [{
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.7+sha-29f9dd5
+ * @license Angular v22.2.0-next.7+sha-e205075-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-29f9dd5');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-e205075-with-local-changes');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
