bba949e0273d8a497b5fb8d703cd9a4bac762732
Andrew Scott <atscott01@gmail.com>
2026-09-10 23:35:50 +0000

    23e5d30 build: update pnpm to v11.26.0

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.6+sha-14d6999
+ * @license Angular v22.1.6+sha-23e5d30
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.6+sha-14d6999');
+const VERSION = /* @__PURE__ */new Version('22.1.6+sha-23e5d30');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
