59f4b4fb99da793efc9e78c045bafabfb2a1f00f
Andrew Scott <atscott01@gmail.com>
2026-09-11 18:10:20 +0000

    c4bd3e2 docs: fix the broken housing location template in the first-app tutorial

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.6+sha-17d6c1b
+ * @license Angular v22.1.6+sha-c4bd3e2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.6+sha-17d6c1b');
+const VERSION = /* @__PURE__ */new Version('22.1.6+sha-c4bd3e2');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
