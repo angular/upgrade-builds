a0533819d2a67c9e77b812655bb790bf0f18c0df
Andrew Scott <atscott01@gmail.com>
2026-09-11 20:21:29 +0000

    c097c01 docs: import bootstrapApplication from platform-browser in the CSP example

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.7+sha-a823323
+ * @license Angular v22.2.0-next.7+sha-c097c01
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-a823323');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-c097c01');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
