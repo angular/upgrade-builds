13913bbbe0bbd6d5dcb20dc400f47eac6d48f409
Andrew Scott <atscott01@gmail.com>
2026-09-11 17:13:31 +0000

    37ca679 Revert "fix(compiler): wrap `@for` collection expression before appending non-null assertion"

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.7+sha-ae33a5f-with-local-changes
+ * @license Angular v22.2.0-next.7+sha-37ca679-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-ae33a5f-with-local-changes');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-37ca679-with-local-changes');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
