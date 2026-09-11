eb8f977fb881741d68d96fa1aedad4b8148d451b
Andrew Scott <atscott01@gmail.com>
2026-09-11 22:26:05 +0000

    f2cf65a fix(vscode-extension): handle template literals when detecting supported decorator fields

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.7+sha-2dcdf9a
+ * @license Angular v22.2.0-next.7+sha-f2cf65a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-2dcdf9a');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-f2cf65a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
