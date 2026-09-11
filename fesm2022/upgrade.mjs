64cdcd324d12f37a454cd7f4acd1e89814c3957e
Andrew Scott <atscott01@gmail.com>
2026-09-11 17:50:30 +0000

    17d6c1b fix(docs-infra): raise SSR fetch limit so the menubar guide prerenders

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.6+sha-8a837f6
+ * @license Angular v22.1.6+sha-17d6c1b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.6+sha-8a837f6');
+const VERSION = /* @__PURE__ */new Version('22.1.6+sha-17d6c1b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
