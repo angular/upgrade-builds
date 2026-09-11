181bfc1c013bd275d16d39b51241f7e47e621b49
Andrew Scott <atscott01@gmail.com>
2026-09-11 22:25:09 +0000

    376b71e docs(forms): document what required() considers empty

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.6+sha-4b5571c
+ * @license Angular v22.1.6+sha-376b71e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.6+sha-4b5571c');
+const VERSION = /* @__PURE__ */new Version('22.1.6+sha-376b71e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
