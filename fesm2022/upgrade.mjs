704bf76574e947fd601f8290f3f7615a2e7eafc9
GitHub <noreply@github.com>
2026-09-11 19:52:26 +0000

    a823323 fix(core): update FakeNavigation to match WHATWG HTML spec

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.7+sha-d0ed76c
+ * @license Angular v22.2.0-next.7+sha-a823323
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-d0ed76c');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-a823323');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
