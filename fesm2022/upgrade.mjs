89bc7175c1a4cfe7336410042ea6f10d0820e121
Andrew Scott <atscott01@gmail.com>
2026-09-13 21:27:52 +0000

    16c8064 test(core): reduce fakeAsync usage in animation tests

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.6+sha-8509045
+ * @license Angular v22.1.6+sha-16c8064
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.6+sha-8509045');
+const VERSION = /* @__PURE__ */new Version('22.1.6+sha-16c8064');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
