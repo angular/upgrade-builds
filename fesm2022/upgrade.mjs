008224d444dbe9c546d68136441a2299df462d80
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-09-16 13:21:10 +0000

    47afc90 docs: close the heading tag in the ng-template output example

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.6+sha-75cbcde
+ * @license Angular v22.1.6+sha-47afc90
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.6+sha-75cbcde');
+const VERSION = /* @__PURE__ */new Version('22.1.6+sha-47afc90');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
