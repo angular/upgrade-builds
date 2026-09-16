f2121c41b9df7284787960a4450e7273203721d0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-09-16 14:12:34 +0000

    24ca9d4 docs: remove outdated useFactory and expression lowering guidance

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.6+sha-5ffc381
+ * @license Angular v22.1.6+sha-24ca9d4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.6+sha-5ffc381');
+const VERSION = /* @__PURE__ */new Version('22.1.6+sha-24ca9d4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
