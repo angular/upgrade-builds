e029502d0ca8fe5a5be9ed35cd8646f3665b6126
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-09-16 13:54:06 +0000

    430a8eb docs: add the missing heading to the effects guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.7+sha-0de811d
+ * @license Angular v22.2.0-next.7+sha-430a8eb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-0de811d');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-430a8eb');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
