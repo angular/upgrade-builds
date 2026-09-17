a51fc01a24255e655ca8a87824affdc6666da20e
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-17 17:36:28 +0000

    3a54e92 docs(core): restore the PipeTransform code examples

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-rc.0+sha-2cf2473
+ * @license Angular v22.2.0-rc.0+sha-3a54e92
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-rc.0+sha-2cf2473');
+const VERSION = /* @__PURE__ */new Version('22.2.0-rc.0+sha-3a54e92');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
