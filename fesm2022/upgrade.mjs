b7b390b623fa0968180fe27a20515d78e1fc07b6
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-17 21:16:36 +0000

    a783c4e refactor(platform-browser): type shadow DOM renderer fields

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.3.0-next.0+sha-7e678e0
+ * @license Angular v22.3.0-next.0+sha-a783c4e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-7e678e0');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-a783c4e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
