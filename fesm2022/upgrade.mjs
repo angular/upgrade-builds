307fd048a763589f6a74236ee7a2571e3051ffe6
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-17 15:59:18 +0000

    80f9e3b test(core): restore longer durations in animation tests

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-rc.0+sha-379bdbe
+ * @license Angular v22.2.0-rc.0+sha-80f9e3b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-rc.0+sha-379bdbe');
+const VERSION = /* @__PURE__ */new Version('22.2.0-rc.0+sha-80f9e3b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
