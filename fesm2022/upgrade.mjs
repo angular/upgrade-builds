e14407a38c96ff335ad3e6298baa7185f38bfa59
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-17 17:54:17 +0000

    03754b3 docs: use the remote-cache config in the bazel caching guide

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.7+sha-c0ed0ad
+ * @license Angular v22.1.7+sha-03754b3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.7+sha-c0ed0ad');
+const VERSION = /* @__PURE__ */new Version('22.1.7+sha-03754b3');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
