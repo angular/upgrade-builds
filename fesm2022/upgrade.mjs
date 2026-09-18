3f7a2bb45318d1f485298b91c955385363c18ba7
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-18 16:55:31 +0000

    c08ba52 docs(core): add error guide for NG0600

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.7+sha-7bf58f9
+ * @license Angular v22.1.7+sha-c08ba52
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.7+sha-7bf58f9');
+const VERSION = /* @__PURE__ */new Version('22.1.7+sha-c08ba52');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
