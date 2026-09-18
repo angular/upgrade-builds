7fc9f6c4127b35447406bc5933e34e967d087a58
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-18 16:14:40 +0000

    1151eb9 fix(docs-infra): render the signature of decorators without members

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.3.0-next.0+sha-a783c4e
+ * @license Angular v22.3.0-next.0+sha-1151eb9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-a783c4e');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-1151eb9');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
