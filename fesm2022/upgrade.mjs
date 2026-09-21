273667b3529990ee69082e27058d2a77beacfadf
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-21 19:40:38 +0000

    4bff265 fix(dev-infra): handle missing arguments and gh failures in PR review script

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.7+sha-805aa91
+ * @license Angular v22.1.7+sha-4bff265
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.7+sha-805aa91');
+const VERSION = /* @__PURE__ */new Version('22.1.7+sha-4bff265');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
