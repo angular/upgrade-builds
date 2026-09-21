475002d802175cb659ec54101acc46d2584e4221
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-21 16:57:44 +0000

    f39db77 docs: fix stray markup in the image optimization and zoneless guides

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.7+sha-c08ba52
+ * @license Angular v22.1.7+sha-f39db77
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.7+sha-c08ba52');
+const VERSION = /* @__PURE__ */new Version('22.1.7+sha-f39db77');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
