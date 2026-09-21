5372195691ca1879c15fd85684a6f1b81ddbfb03
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-21 17:18:10 +0000

    aa2cabb refactor(compiler): add method to emitter context

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.7+sha-f39db77
+ * @license Angular v22.1.7+sha-aa2cabb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.7+sha-f39db77');
+const VERSION = /* @__PURE__ */new Version('22.1.7+sha-aa2cabb');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
