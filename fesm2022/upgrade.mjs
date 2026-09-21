ac9c2e4ef463abd7894d3c452b9999ab3a94fb27
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-21 19:16:31 +0000

    6647a07 docs: update `disabledReasons` and `transformedValue` examples

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.3.0-next.0+sha-b0d4010-with-local-changes
+ * @license Angular v22.3.0-next.0+sha-6647a07
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-b0d4010-with-local-changes');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-6647a07');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
