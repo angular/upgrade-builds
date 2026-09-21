fff927da4ab8bd93825442bb189f3769a614d47e
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-21 17:17:38 +0000

    51f056f docs(router): clarify route resource parameters

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-rc.0+sha-adea161
+ * @license Angular v22.2.0-rc.0+sha-51f056f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-rc.0+sha-adea161');
+const VERSION = /* @__PURE__ */new Version('22.2.0-rc.0+sha-51f056f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
