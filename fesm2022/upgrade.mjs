a6452594d317495a46ae28c7eb8bc164004cdee7
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-22 23:12:01 +0000

    efe48df docs(core): fix three JSDoc rendering bugs on the API pages

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-rc.0+sha-8287b3f
+ * @license Angular v22.2.0-rc.0+sha-efe48df
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-rc.0+sha-8287b3f');
+const VERSION = /* @__PURE__ */new Version('22.2.0-rc.0+sha-efe48df');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
