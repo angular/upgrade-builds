9af3faff1e0fc5cb49be8d5ebc89bd1e4d365cbd
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-22 21:27:33 +0000

    726368b docs: correct the adev writing guide skill

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.7+sha-71f84a8
+ * @license Angular v22.1.7+sha-726368b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.7+sha-71f84a8');
+const VERSION = /* @__PURE__ */new Version('22.1.7+sha-726368b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
