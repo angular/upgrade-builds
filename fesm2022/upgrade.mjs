80ad842ecff91571f658768631ec27408df99ce4
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-22 16:17:55 +0000

    bc402cf fix(core): don't use plain objects as maps for jsaction data

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-rc.0+sha-378ecfc
+ * @license Angular v22.2.0-rc.0+sha-bc402cf
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-rc.0+sha-378ecfc');
+const VERSION = /* @__PURE__ */new Version('22.2.0-rc.0+sha-bc402cf');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
