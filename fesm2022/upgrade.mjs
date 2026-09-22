2e6a42f7b2c85495f8fbbb237be95f768421b67a
Alan Agius <alan.agius4@gmail.com>
2026-09-22 18:05:35 +0000

    6cae42c build: update all non-major dependencies

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.3.0-next.0+sha-aa1cc47
+ * @license Angular v22.3.0-next.0+sha-6cae42c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-aa1cc47');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-6cae42c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
