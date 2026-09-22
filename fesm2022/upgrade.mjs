f14b76c2b3dc5f6e676a6517844e30b8c0cd7dc8
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-22 21:55:38 +0000

    afcda88 docs(service-worker): render the SwPush and SwRegistrationOptions examples

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.3.0-next.0+sha-249f55f
+ * @license Angular v22.3.0-next.0+sha-afcda88
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-249f55f');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-afcda88');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
