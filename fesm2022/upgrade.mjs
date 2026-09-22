ac9bd5dee202359eac0d1ff60b5e8aee178a25ed
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-22 21:25:34 +0000

    249f55f docs: correct the adev writing guide skill

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.3.0-next.0+sha-6cae42c
+ * @license Angular v22.3.0-next.0+sha-249f55f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-6cae42c');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-249f55f');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
