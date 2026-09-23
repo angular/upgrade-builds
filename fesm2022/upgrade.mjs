3d7be7311902a2b0718b2b6aca0bbe2eb6843eb4
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-23 17:31:54 +0000

    03872a8 fix(router): avoid dense elements allocation for numeric URL keys

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.23+sha-81385ea
+ * @license Angular v21.2.23+sha-03872a8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.23+sha-81385ea');
+const VERSION = /* @__PURE__ */new Version('21.2.23+sha-03872a8');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
