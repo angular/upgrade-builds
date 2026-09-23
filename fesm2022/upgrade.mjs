8fcdb822472b530b00f8daa5aaa906cc6904e3d3
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-23 17:53:48 +0000

    e65f114 release: cut the v21.2.24 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v21.2.23+sha-03872a8
+ * @license Angular v21.2.24+sha-e65f114
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('21.2.23+sha-03872a8');
+const VERSION = /* @__PURE__ */new Version('21.2.24+sha-e65f114');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
