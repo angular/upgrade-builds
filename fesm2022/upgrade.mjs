32e42cfb4a653925decbd805d2ae0349ae486bec
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-23 18:02:17 +0000

    fc187d4 release: cut the v22.2.0 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-rc.0+sha-52c33bc
+ * @license Angular v22.2.0+sha-fc187d4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-rc.0+sha-52c33bc');
+const VERSION = /* @__PURE__ */new Version('22.2.0+sha-fc187d4');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
