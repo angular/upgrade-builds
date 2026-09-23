ffefccf4f2bda0d3364bef0a240fcd6acdb530b7
Andrew Scott <atscott01@gmail.com>
2026-09-23 20:04:14 +0000

    1ba9b86 refactor(router): give internal error classes an explicit name

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0+sha-84975ce
+ * @license Angular v22.2.0+sha-1ba9b86
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0+sha-84975ce');
+const VERSION = /* @__PURE__ */new Version('22.2.0+sha-1ba9b86');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
