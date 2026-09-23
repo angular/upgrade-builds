c5ae375bacb64ad326e809b5e39e23e8acde30b5
Andrew Scott <atscott01@gmail.com>
2026-09-23 19:39:20 +0000

    0737c4e test(router): add test coverage for route resource reloading and input binding

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.3.0-next.0+sha-4d3f502
+ * @license Angular v22.3.0-next.0+sha-0737c4e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-4d3f502');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-0737c4e');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
