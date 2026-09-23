11579c0bb5afc5040b7ad38879067a5120b111dd
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-23 16:01:46 +0000

    52c33bc refactor(router): clean up route resource implementation and test helpers

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-rc.0+sha-369d1f4
+ * @license Angular v22.2.0-rc.0+sha-52c33bc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-rc.0+sha-369d1f4');
+const VERSION = /* @__PURE__ */new Version('22.2.0-rc.0+sha-52c33bc');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
