6fc0bec17f7d9dfd5c84abf0cae07cf3c2c13f69
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-23 17:28:58 +0000

    573293c build: update dependency mermaid to v12

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.3.0-next.0+sha-344b10f
+ * @license Angular v22.3.0-next.0+sha-573293c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-344b10f');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-573293c');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
