289b1e0b02fdef8b2cf21863bdda1d74d7fbf258
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-23 15:50:45 +0000

    a2fedc1 docs(upgrade): add maintenance mode note

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.1.7+sha-282f9fe
+ * @license Angular v22.1.7+sha-a2fedc1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.1.7+sha-282f9fe');
+const VERSION = /* @__PURE__ */new Version('22.1.7+sha-a2fedc1');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
