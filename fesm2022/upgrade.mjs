c9b79e34fd9d7b8ae360ca117c109310ee27a983
Andrew Scott <atscott01@gmail.com>
2026-09-23 20:09:36 +0000

    28ad39a release: bump Angular DevTools version to 1.22.0

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.3.0-next.0+sha-0737c4e
+ * @license Angular v22.3.0-next.0+sha-28ad39a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-0737c4e');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-28ad39a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
