4dd02e3dd3686fa40cd60e972f6447ea280f6ab0
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-23 17:54:16 +0000

    123729b docs: release notes for the v21.2.24 release

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.3.0-next.0+sha-573293c
+ * @license Angular v22.3.0-next.0+sha-123729b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-573293c');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-123729b');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
