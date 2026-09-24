a9dd463704d3c050930699fc338b6ed3be3139ea
Andrew Scott <atscott01@gmail.com>
2026-09-24 19:45:37 +0000

    21851fc fix(core): add debugName on afterRenderEffect

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0+sha-7f9c14d
+ * @license Angular v22.2.0+sha-21851fc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0+sha-7f9c14d');
+const VERSION = /* @__PURE__ */new Version('22.2.0+sha-21851fc');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
