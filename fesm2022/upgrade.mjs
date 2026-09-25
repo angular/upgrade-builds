bda7b268c84c4f51fd90ff902cad6c86c306a105
Andrew Scott <atscott01@gmail.com>
2026-09-25 22:19:21 +0000

    d3eaf9a fix(docs-infra): render one card per API member

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.3.0-next.0+sha-e4fc745
+ * @license Angular v22.3.0-next.0+sha-d3eaf9a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-e4fc745');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-d3eaf9a');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
