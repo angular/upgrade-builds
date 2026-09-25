af04cc1a5093f0f4ad87e1df548547db1c760aa3
GitHub <noreply@github.com>
2026-09-25 18:05:29 +0000

    e4fc745 docs: release notes for the vscode extension 22.2.0 release (#70951)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.3.0-next.0+sha-846c73d
+ * @license Angular v22.3.0-next.0+sha-e4fc745
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-846c73d');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-e4fc745');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
