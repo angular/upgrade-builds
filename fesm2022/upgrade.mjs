289f6f190b015df4cdeaae8b0d4204ad61accaff
Andrew Scott <atscott01@gmail.com>
2026-09-26 00:04:52 +0000

    14793bf perf(router): do not retain UrlTree instances in RouterLink

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0+sha-b9a8605
+ * @license Angular v22.2.0+sha-14793bf
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { Version } from '@angular/core';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0+sha-b9a8605');
+const VERSION = /* @__PURE__ */new Version('22.2.0+sha-14793bf');
 
 export { VERSION };
 //# sourceMappingURL=upgrade.mjs.map
